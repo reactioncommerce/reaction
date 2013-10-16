96cc3bac40a65b1064aac5fac7bd96d6aeb7f2b9
aaronjudd <aaronjudd@me.com>
2013-10-16 07:35:33 -0700

    Adding some initial package manager tile layout, additional tile colors, package properties

--- client/helpers/reaction.js
+++ client/helpers/reaction.js
@@ -10,3 +10,21 @@ getObjects = function getObjects(obj, key, val) {
     }
     return objects;
 };
+
+
+//
+//
+// returns a random item from any array,
+// if you pass a non-falsey argument,
+// it removes the item from the array.
+// ex: alert(colors.getRandom());
+// where colors in an array.
+//
+//
+Array.prototype.getRandom = function(cut){
+    var i= Math.floor(Math.random()*this.length);
+    if(cut && i in this){
+        return this.splice(i, 1)[0];
+    }
+    return this[i];
+}
