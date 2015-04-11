6f2e9364b4de6fcaed0f8f976835a2114c062569
aaronjudd <aaronjudd@me.com>
2015-04-10 19:03:58 -0700

    Add reaction-analytics, updated Dockerfile
    **Implements https://github.com/reactioncommerce/reaction/issues/351**
    
    - add new analytics packages from @spencern
        - spencern:reaction-analytics-libs
        - spencern:reaction-analytics
        - related default app configuration
        -
        - removes reaction-google-analytics
    
    **Strategic updates for issue
    https://github.com/reactioncommerce/reaction/issues/249**
    
    - refactor Dockerfile so that the reaction docker image loads without
    any additional configuration when used with Kitematic, and is testable.
    - entrypoint.sh functionality which was used to build meteor from a
    remote repository has been removed from Dockerfile. There are other
    (better) meteor Dockerfile examples out there, if you need that
    functionality.
    - remove themes from master
    - replaced bin/entrypoint.sh (now just starts mongo db and reaction app)
    - adds bin/build-meteor.sh (builds the current app)
    - adds bin/build-themes.sh (builds less themes in development mode)
    - adds bin/build-packages.sh (builds app with bin/docker/packages)
    - add initial shippable.yml

---
+++ bin/docker/build-themes.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# this is currently a work around for the way that meteor is compiling less
+#
+# we need to make sure .less files created by the bootstrap build plugin
+# this can only be done by running development mode meteor
+# anxiously waiting for
+# https://meteor.hackpad.com/Improvements-to-LESS-and-other-CSS-preprocessors-fqDPbgOH8Fn
+#
+if [ ! -f client/themes/custom.reaction.less ]; then
+    echo "warning: less theme files not found!!"
+    echo "info: using development mode in an attempt to create missing files."
+    # startup development server
+    command="env -i HOME="$HOME" LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" PATH="$PATH" USER="$USER" /usr/local/bin/meteor"
+    log=".build.log"
+    match="=> Client modified -- refreshing"
+
+    # wait for meteor to modify client files
+    $command > "$log" 2>&1 &
+    pid=$!
+    while sleep 10
+    do
+        if fgrep --quiet "$match" "$log"
+        then
+            kill $pid
+            echo "happiness: we've created new theme files"
+            exit 0
+        fi
+    done
+fi
