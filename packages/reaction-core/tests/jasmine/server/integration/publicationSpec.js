fee8f002e52610caa2ebd48ff24de642c098f0c5
Josh Owens <joshua.owens@gmail.com>
2015-07-24 10:22:38 -0500

    WIP.

--- packages/reaction-core/tests/jasmine/server/integration/publicationSpec.js
+++ packages/reaction-core/tests/jasmine/server/integration/publicationSpec.js
@@ -1,21 +1,49 @@
 describe("Publication", function() {
 
+  var shop;
+
+  beforeEach(function() {
+    // reset
+    Products.remove({});
+    Shops.remove({});
+    // insert products and shops
+    Shops.insert({name: 'Cookie Swirl C', currency: 'USD', currencies: {}, locales: { continents: {}, countries: {}}, timezone: 'US/Pacific'});
+    shop = Shops.findOne();
+    Products.insert({title: 'My Little Pony', shopId: shop._id, productType: 'Simple', variants: [], isVisible: false});
+    Products.insert({title: 'Shopkins - Peachy', shopId: shop._id, productType: 'Simple', variants: [], isVisible: true});
+  });
+
   describe("Products", function() {
-    fit("should return all products to admins", function() {
+    it("should return all products to admins", function() {
       // setup
-      var shop = {_id: "123"};
-      spyOn(Match, "Optional");
       spyOn(ReactionCore, "getCurrentShop").and.returnValue(shop);
       spyOn(Roles, "userIsInRole").and.returnValue(true);
-      spyOn(Products, "find").and.returnValue('cursor');
       // execute
       cursor = Meteor.server.publish_handlers["Products"]();
       // verify
-      expect(Products.find).toHaveBeenCalledWith({shopId: shop._id});
-      expect(cursor).toEqual('cursor');
+      data = cursor.fetch()[0];
+      expect(data.title).toEqual('My Little Pony');
     });
-    xit("should return only visible products to visitors");
-    xit("should return products from all shops when multiple shops are provided");
-    xit("should return products from current shop when no shops are provided");
+    it("should return only visible products to visitors", function() {
+      // setup
+      spyOn(ReactionCore, "getCurrentShop").and.returnValue(shop);
+      spyOn(Roles, "userIsInRole").and.returnValue(false);
+      // execute
+      cursor = Meteor.server.publish_handlers["Products"]();
+      // verify
+      data = cursor.fetch()[0];
+      expect(data.title).toEqual('Shopkins - Peachy');
+    });
+    it("should return products from all shops when multiple shops are provided", function() {
+      // setup
+      var shopIds = [shop._id];
+      spyOn(ReactionCore, "getCurrentShop").and.returnValue({_id: '123'});
+      spyOn(Roles, "userIsInRole").and.returnValue(true);
+      // execute
+      cursor = Meteor.server.publish_handlers["Products"](shopIds);
+      // verify
+      data = cursor.fetch()[0];
+      expect(data.title).toEqual('My Little Pony');
+    });
   });
 });
