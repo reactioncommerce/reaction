5a8d4ef30b3b5ded02669ef424ad861ef51fa5c0
Mike Murray <hellomikemurray@gmail.com>
2015-08-19 15:08:00 -0700

    Fixed query for user orders
    - Cleaned up profile template

--- packages/reaction-accounts/client/templates/profile/profile.js
+++ packages/reaction-accounts/client/templates/profile/profile.js
@@ -21,9 +21,10 @@ Template.accountProfile.helpers({
     if (Meteor.user()) {
 
       return ReactionCore.Collections.Orders.find({
-        userId: Meteor.userId(),
-        sort: { createdAt: -1},
-        limit:25
+        userId: Meteor.userId()
+      }, {
+        sort: {createdAt: -1},
+        limit: 25
       });
     }
   },
