b5cc05cae5d6d1ea37ef134e5ff538538dbf2638
aaronjudd <aaron@ongoworks.com>
2015-08-23 16:47:02 -0700

    development localhost:* browser policy

--- packages/reaction-core/server/browserPolicy.js
+++ packages/reaction-core/server/browserPolicy.js
@@ -6,10 +6,9 @@
 
 if (process.env.NODE_ENV === 'development') {
   BrowserPolicy.content.allowOriginForAll('localhost:*');
-  BrowserPolicy.content.allowConnectOrigin('ws://localhost:5000');
-  BrowserPolicy.content.allowConnectOrigin('ws://localhost:63580');
-  BrowserPolicy.content.allowConnectOrigin('http://localhost:5000');
-  BrowserPolicy.content.allowConnectOrigin('http://localhost:63580');
+  BrowserPolicy.content.allowConnectOrigin('ws://localhost:*');
+  BrowserPolicy.content.allowConnectOrigin('http://localhost:*');
+  BrowserPolicy.content.allowConnectOrigin('https://localhost:*');
   BrowserPolicy.framing.allowAll();
 }
 
