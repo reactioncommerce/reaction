1fcf631ba589fb5e13e4072f38a3094b5cd6f462
aaronjudd <aaron@ongoworks.com>
2015-09-11 11:18:45 -0700

    updated to use reactioncommerce org packages

--- packages/reaction-analytics/package.js
+++ packages/reaction-analytics/package.js
@@ -1,15 +1,15 @@
 Package.describe({
   summary: "Reaction Analytics - Integrate third-party analytics libraries",
-  name: "spencern:reaction-analytics",
-  version: "0.0.3",
-  git: "https://github.com/spencern/reaction-analytics.git"
+  name: "reactioncommerce:reaction-analytics",
+  version: "1.0.0",
+  git: "https://github.com/reactioncommerce/reaction-analytics.git"
 });
 
 Package.registerBuildPlugin({
   name: 'analyticsConfigurator',
   use: [
     'underscore@1.0.3',
-    'spencern:reaction-analytics-libs@0.0.1'
+    'reactioncommerce:reaction-analytics-libs@1.0.0'
   ],
   sources: [
     'server/buildtools/analyticsSources.js',
@@ -21,13 +21,13 @@ Package.registerBuildPlugin({
 
 Package.on_use(function (api, where) {
   api.versionsFrom('METEOR@1.0');
-  api.use("meteor-platform@1.2.1");
+  api.use("meteor-platform@1.2.2");
   api.use("less");
   api.use("jquery");
   api.use('browser-policy-content', 'server');
-  api.use('iron:router@1.0.7', 'client');
-  api.use("reactioncommerce:core@0.4.1");
-  api.use("spencern:reaction-analytics-libs@0.0.1", 'client');
+  api.use('iron:router@1.0.9', 'client');
+  api.use("reactioncommerce:core@0.7.0");
+  api.use("reactioncommerce:reaction-analytics-libs@1.0.0", 'client');
 
   api.addFiles([
     "common/routing.js",
