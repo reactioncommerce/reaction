f3cae3854a8458884deacf41b4c0423310fa59d9
aaronjudd <aaron@ongoworks.com>
2015-09-11 11:19:27 -0700

    updated to use reactioncommerce org packages

--- packages/reaction-analytics-libs/package.js
+++ packages/reaction-analytics-libs/package.js
@@ -1,11 +1,11 @@
 Package.describe({
-  name: "spencern:reaction-analytics-libs",
-  summary: "provides access to static analytics libs on server",
-  version: "0.0.1",
-  git: "https://github.com/spencern/reaction-analytics-libs.git"
+  summary: "Reaction Commerce - Analytics Libraries",
+  name: "reactioncommerce:reaction-analytics-libs",
+  version: "1.0.0",
+  git: "https://github.com/reactioncommerce/reaction-analytics-libs.git"
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
   api.addFiles('analytics-lib.js', 'server');
   api.export(['GetAnalyticsLib']);
@@ -13,5 +13,7 @@ Package.onUse(function(api) {
     'analytics/googleAnalytics.js',
     'analytics/mixpanel.js',
     'analytics/segmentio.js'
-  ], 'server', {isAsset: true});
+  ], 'server', {
+    isAsset: true
+  });
 })
