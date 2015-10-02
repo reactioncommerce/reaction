c7895cd5473acd392e2879f8ccebbe16e306a72e
aaronjudd <aaron@ongoworks.com>
2015-10-02 12:37:50 -0700

    update to cart/methods in accounts

--- packages/reaction-accounts/client/templates/addressBook/grid/grid.js
+++ packages/reaction-accounts/client/templates/addressBook/grid/grid.js
@@ -2,60 +2,51 @@
  * handles display of addressBook grid
  */
 Template.addressBookGrid.helpers({
-
-  selectedBilling: function() {
-
-    var cart = ReactionCore.Collections.Cart.findOne();
+  selectedBilling: function () {
+    let cart = ReactionCore.Collections.Cart.findOne();
 
     if (cart) {
       if (cart.payment) {
-        if ( cart.payment.address) {
+        if (cart.payment.address) {
           if (this._id === cart.payment.address._id) {
             return "active";
           }
         }
       }
     }
-
   },
 
-  selectedShipping: function() {
-
-    var cart = ReactionCore.Collections.Cart.findOne();
+  selectedShipping: function () {
+    let cart = ReactionCore.Collections.Cart.findOne();
 
     if (cart) {
       if (cart.shipping) {
-        if ( cart.shipping.address) {
+        if (cart.shipping.address) {
           if (this._id === cart.shipping.address._id) {
             return "active";
           }
         }
       }
     }
-
   },
-
-  account: function() {
-
+  account: function () {
     return ReactionCore.Collections.Accounts.findOne({
       userId: Meteor.userId()
     });
-
   }
 });
-
 
 /*
  * events
  */
 
 Template.addressBookGrid.events({
-  "click [data-event-action=selectShippingAddress]": function(event, template) {
+  "click [data-event-action=selectShippingAddress]": function (event, template) {
     cart = ReactionCore.Collections.Cart.findOne();
-    return Meteor.call("setShipmentAddress", cart._id, this);
+    return Meteor.call("cart/setShipmentAddress", cart._id, this);
   },
-  "click [data-event-action=selectBillingAddress]": function(event, template) {
+  "click [data-event-action=selectBillingAddress]": function (event, template) {
     cart = ReactionCore.Collections.Cart.findOne();
-    return Meteor.call("setPaymentAddress", cart._id, this);
+    return Meteor.call("cart/setPaymentAddress", cart._id, this);
   }
 });
