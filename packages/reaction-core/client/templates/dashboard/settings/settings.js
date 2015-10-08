0e2f0aeb34b6dbe6d2c30bc48470e54fc1df9c9f
Mike Murray <hellomikemurray@gmail.com>
2015-10-08 14:28:07 -0700

    Cleanup of settings view code

--- packages/reaction-core/client/templates/dashboard/settings/settings.js
+++ packages/reaction-core/client/templates/dashboard/settings/settings.js
@@ -1,35 +1,39 @@
+/**
+ * Helpers for Settings Header (actionView)
+ */
 Template.settingsHeader.helpers({
 
-  "registry": function () {
+  /**
+   * Data pased to action view
+   * @return {Object} Registry entry for item
+   */
+  registry: function () {
     // just some handle little helpers for default package i18nKey/i18nLabel
-    var registry = ReactionCore.getActionView() || {};
+    let registry = ReactionCore.getActionView() || {};
     registry.nameSpace = registry.name || registry.template || "app";
     registry.i18nLabel = registry.label || registry.provides || "Settings";
     registry.i18nKey = registry.nameSpace.toCamelCase() + "." + registry.i18nLabel.toCamelCase();
     return registry;
-
   },
 
-  "thisApp": function () {
-    var fields = {
-      'enabled': 1,
-      'registry': 1,
-      'name': 1
-    };
-
-    var reactionApp = ReactionCore.Collections.Packages.findOne({
+  /**
+   * thisApp
+   * @return {Object} Registry entry for item
+   */
+  thisApp() {
+    let reactionApp = ReactionCore.Collections.Packages.findOne({
       "registry.provides": "settings",
       "registry.route": Router.current().route.getName()
     }, {
-      'enabled': 1,
-      'registry': 1,
-      'name': 1,
-      'route': 1
+      enabled: 1,
+      registry: 1,
+      name: 1,
+      route: 1
     });
 
     if (reactionApp) {
-      var settingsData = _.find(reactionApp.registry, function (item) {
-        return item.route == Router.current().route.getName() && item.provides == "settings";
+      let settingsData = _.find(reactionApp.registry, function (item) {
+        return item.route === Router.current().route.getName() && item.provides === "settings";
       });
 
       return settingsData;
@@ -39,36 +43,53 @@ Template.settingsHeader.helpers({
 
 });
 
-
+/**
+ * Events for Setting Header (actionView)
+ */
 Template.settingsHeader.events({
-  "click [data-event-action=closeSettings]": function () {
+  "click [data-event-action=closeSettings]": () => {
     ReactionCore.hideActionView();
   }
 });
 
-
-// Templte.settingsSidebar.inheritsHelpersFrom("packagesGrid");
-
+/**
+ * Helpers for Settings Sidebar (actionView)
+ */
 Template.settingsSidebar.helpers({
-  pkgPermissions: function () {
-    if (ReactionCore.hasPermission('dashboard')) {
+
+  /**
+   * pkgPermissions Check package permissions
+   * @return {Boolean} user has permission to see settings for this package
+   */
+  pkgPermissions() {
+    if (ReactionCore.hasPermission("dashboard")) {
       if (this.route) {
         return ReactionCore.hasPermission(this.route);
-      } else {
-        return ReactionCore.hasPermission(this.name);
       }
-    } else {
-      return false;
+
+      return ReactionCore.hasPermission(this.name);
     }
+
+    return false;
   }
 });
 
 Template.settingsSidebarItem.helpers({
-  "label": function () {
+
+  /**
+   * label
+   * @return {String} Label for item
+   */
+  label() {
     return Template.parentData(1).label || this.label;
   },
 
-  "active": function (route) {
+  /**
+   * active
+   * @param {String} route Route for item
+   * @return {String} "active" || ""
+   */
+  active(route) {
     if (route === Router.current().route.getName()) {
       return "active";
     }
