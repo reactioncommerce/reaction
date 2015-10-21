e027b718b7a6bdfd310497a6dbc15f7171cea25a
MichaelJenny <michael.jenny@projekt-und-partner.de>
2015-10-21 10:03:48 +0200

    Add cart item tests.

--- packages/reaction-core/tests/jasmine/server/integration/cart.js
+++ packages/reaction-core/tests/jasmine/server/integration/cart.js
@@ -3,33 +3,70 @@ describe("cart methods", function () {
 
   //let userId = Factory.get("user");
   let userId = Meteor.users.insert({}); // Somehow Factory does not work
-  // Create shop so that ReactionCore.getShopId() doesn't fail
-  Factory.create("shop");
   ReactionCore.sessionId = "deadbeef"; // Required for creating a cart
   let cartId;
 
   describe("cart/createCart", function () {
-    beforeEach(function () {
-    });
-    it("should create a cart", function (done) {
+    it("should create a test cart", function (done) {
       cartId = Meteor.call("cart/createCart", userId);
       let cart = Cart.findOne({'userId': userId});
       expect(cartId).toEqual(cart._id);
       done();
     });
   });
-  describe("cart/addToCart", function () {
-    it("should add item to cart", function (done) {
-      let product = Factory.create("product");
-      let productId = product._id;
-      let variantData = product.variants[0];
-      let quantity = "1";
-      Meteor.call("cart/addToCart", cartId, productId, variantData, quantity);
-      let foundCarts = Cart.find({_id: cartId}, {items: product}).fetch();
-      expect(_.size(foundCarts)).toEqual(1);
-      expect(_.size(foundCarts[0].items)).toEqual(1);
-      expect(foundCarts[0].items[0].productId).toEqual(productId);
-      done();
+  describe("cart items", function () {
+    beforeEach(function () {
+      // Empty test cart
+      Cart.update({
+          _id: cartId
+        }, {
+          $pull: { 'items': {
+            }
+          }
+      });
+    });
+    describe("cart/addToCart", function () {
+      it("should add item to cart", function (done) {
+        let product = Factory.create("product");
+        let productId = product._id;
+        let variantData = product.variants[0];
+        let quantity = "1";
+        Meteor.call("cart/addToCart", cartId, productId, variantData, quantity);
+        let carts = Cart.find({_id: cartId}, {items: product}).fetch();
+        expect(_.size(carts)).toEqual(1);
+        expect(_.size(carts[0].items)).toEqual(1);
+        expect(carts[0].items[0].productId).toEqual(productId);
+        done();
+      });
+      it("should merge all items of same variant in cart", function (done) {
+        let product = Factory.create("product");
+        let productId = product._id;
+        let variantData = product.variants[0];
+        let quantity = "1";
+        Meteor.call("cart/addToCart", cartId, productId, variantData, quantity);
+        // add a second item of same variant
+        Meteor.call("cart/addToCart", cartId, productId, variantData, quantity);
+        let carts = Cart.find({_id: cartId}, {items: product}).fetch();
+        expect(_.size(carts)).toEqual(1);
+        expect(_.size(carts[0].items)).toEqual(1);
+        expect(carts[0].items[0].quantity).toEqual(2);
+        done();
+      });
+    });
+    describe("cart/removeFromCart", function () {
+      it("should remove item from cart", function (done) {
+        let product = Factory.create("product");
+        let productId = product._id;
+        let variantData = product.variants[0];
+        let quantity = "1";
+        Meteor.call("cart/addToCart", cartId, productId, variantData, quantity);
+        let carts = Cart.find({_id: cartId}, {items: product}).fetch();
+        let cartItem = carts[0].items[0];
+        Meteor.call("cart/removeFromCart", cartId, cartItem);
+        carts = Cart.find({_id: cartId}, {items: product}).fetch();
+        expect(_.size(carts[0].items)).toEqual(0);
+        done();
+      });
     });
   });
 });
