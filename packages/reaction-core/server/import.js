7433903899e66a0810d44d895d1322ecb318d0d9
Tom De Caluwé <decaluwe.t@gmail.com>
2015-11-04 16:03:40 +0100

    Use the mongo bulk operation data structure directly as the import buffer.

--- packages/reaction-core/server/import.js
+++ packages/reaction-core/server/import.js
@@ -1,88 +1,26 @@
-/**
- * @summary Commit all objects currently in the import buffer.
- *
- * Importing a product consists of the following steps:
- *
- * * Upsert the parent product, so it surely exists in the system.
- * * Pull the variant from non-matching parent products.
- * * Push the variant if it doesn't exist.
- * * Update the variant.
- */
-Import = function () {
-  let contexts = {};
-  let query = {};
-  let update = {};
-  let bulk = {};
-  contexts.tag = ReactionCore.Schemas.Tag.newContext();
-  contexts.product = ReactionCore.Schemas.Product.newContext();
-  contexts.variant = ReactionCore.Schemas.ProductVariant.newContext();
-  // Initialize bulk operation for the tags collection.
-  bulk.tags = ReactionCore.Collections.Tags.rawCollection().initializeUnorderedBulkOp();
-  while (Import._tags.length > 0) {
-    var tag = Import._tags.pop();
-    // Upsert tags.
-    ReactionCore.Schemas.Tag.clean(tag.value);
-    ReactionCore.Collections.Tags.before.update(Meteor.userId(), tag.value);
-    bulk.tags.find(tag.key).upsert().update({
-      $set: tag.value,
-      $setOnInsert: { createdAt: new Date() }
-    });
-  }
-  try {
-    // Can throw an `Invalid Operation, No operations in bulk` error if no
-    // queries were added.
-    bulk.tags.execute(function(error, result) {});
-  } finally {}
-  bulk.products = ReactionCore.Collections.Products.rawCollection().initializeUnorderedBulkOp();
-  while (Import._products.length > 0) {
-    var product = Import._products.pop();
-    // Upsert products.
-    ReactionCore.Schemas.Product.clean(product.value);
-    contexts.product.validate(product.value);
-    bulk.products.find(product.key).upsert().update({
-      $set: product.value,
-      $setOnInsert: { createdAt: new Date() }
-    });
-  }
-  while (Import._variants.length > 0) {
-    var variant = Import._variants.pop();
-    //ReactionCore.Schemas.ProductVariant.clean(Import._variants[i].value, {});
-    //contexts.variant.validate(Import._variants[i].value, {});
-    // Remove variants with the same key from other parents.
-    bulk.products.find({
-      'variants': { $elemMatch: variant.key },
-      $nor: [ variant.parent ]
-    }).update({ $pull: { 'variants': { $elemMatch: variant.key } } });
-    // Make sure the variant exists.
-    query = { $nor: [ { 'variants': { $elemMatch: variant.key } } ] };
-    for (let key of Object.keys(variant.parent)) {
-      query[key] = variant.parent[key];
-    }
-    bulk.products.find(query).update({ $push: { 'variants': variant.key } });
-    // Upsert the variant.
-    ReactionCore.Schemas.ProductVariant.clean(variant.value, {});
-    query = { 'variants': { $elemMatch: variant.key } };
-    for (let key of Object.keys(variant.parent)) {
-      query[key] = variant.parent[key];
-    }
-    update = {};
-    for (let key of Object.keys(variant.value)) {
-      update['variants.$.' + key] = variant.value[key];
-    }
-    bulk.products.find(query).update({ $set: update });
-  }
-  try {
-    // Can throw an `Invalid Operation, No operations in bulk` error if no
-    // queries were added.
-    bulk.products.execute(function(error, result) {});
-  } finally {}
+ReactionImport = {};
+
+ReactionImport._collections = ['Tags', 'Products'];
+ReactionImport._buffers = {};
+ReactionImport._contexts = {};
+ReactionImport._count = 0;
+ReactionImport._limit = 1000;
+
+for (let collection of ReactionImport._collections) {
+  ReactionImport._buffers[collection] = ReactionCore.Collections[collection].rawCollection().initializeUnorderedBulkOp();
 }
 
-Import._variants = [];
-Import._products = [];
-Import._relations = [];
-Import._tags = [];
-Import._orders = [];
+/**
+ * @summary Commit all buffers to the database.
+ */
+ReactionImport.flush = function () {
+  for (let collection of ReactionImport._collections) {
+    try {
+      ReactionImport._buffers[collection].execute(function(error, result) {});
+    } catch (error) {}
+    ReactionImport._buffers[collection] = ReactionCore.Collections[collection].rawCollection().initializeUnorderedBulkOp();
+  }
+}
 
 /**
  * @summary Store a product in the import buffer.
@@ -91,20 +29,78 @@ Import._orders = [];
  * @param {Object} parent A key to identify the parent product
  * 
  * When processing the import buffer, the product and the parent will be
- * inserted if needed.
+ * inserted if needed. Importing a variant currently consists of the following
+ * steps:
+ *
+ * * Pull the variant from non-matching parent products.
+ * * Push the variant if it doesn't exist.
+ * * Update the variant.
  */
-Import.product = function (key, product, parent) {
+ReactionImport.product = function (key, product, parent) {
   if (parent) {
-    Import._variants.push({ key: key, value: product || {}, parent: parent });
+    ReactionCore.Schemas.ProductVariant.clean(product, {});
+    if (ReactionImport._contexts.ProductVariant) {
+      ReactionImport._contexts.ProductVariant.validate(product, {});
+    } else {
+      ReactionImport._contexts.ProductVariant = ReactionCore.Schemas.ProductVariant.newContext();
+    }
+    // Remove variants with the same key from other parents.
+    ReactionImport._buffers['Products'].find({
+      'variants': { $elemMatch: key },
+      $nor: [ parent ]
+    }).update({ $pull: { 'variants': { $elemMatch: key } } });
+    // Make sure the variant exists.
+    query = { $nor: [ { 'variants': { $elemMatch: key } } ] };
+    for (let key of Object.keys(parent)) {
+      query[key] = parent[key];
+    }
+    ReactionImport._buffers['Products'].find(query).update({ $push: { 'variants': key } });
+    // Upsert the variant.
+    ReactionCore.Schemas.ProductVariant.clean(product, {});
+    query = { 'variants': { $elemMatch: key } };
+    for (let key of Object.keys(parent)) {
+      query[key] = parent[key];
+    }
+    update = {};
+    for (let key of Object.keys(product)) {
+      update['variants.$.' + key] = product[key];
+    }
+    ReactionImport._buffers['Products'].find(query).update({ $set: update });
   } else {
-    Import._products.push({ key: key, value: product || {} });
+    ReactionCore.Schemas.Product.clean(product);
+    if (ReactionImport._contexts.Product) {
+      ReactionImport._contexts.Product.validate(product, {});
+    } else {
+      ReactionImport._contexts.Product = ReactionCore.Schemas.Product.newContext();
+    }
+    // Upsert products.
+    ReactionImport._buffers['Products'].find(key).upsert().update({
+      $set: product,
+      $setOnInsert: { createdAt: new Date() }
+    });
   }
 }
 
-Import.tag = function (key, tag) {
-  Import._tags.push({ key: key, value: tag || {} });
+/**
+ * @summary Store a tag in the import buffer.
+ * @param {Object} key A key to look up the tag
+ * @param {Object} tag The tag data to be updated
+ */
+ReactionImport.tag = function (key, tag) {
+  ReactionCore.Schemas.Tag.clean(tag);
+  if (ReactionImport._contexts.Tag) {
+    ReactionImport._contexts.Tag.validate(tag, {});
+  } else {
+    ReactionImport._contexts.Tag = ReactionCore.Schemas.Tag.newContext();
+  }
+  // Upsert tags.
+  ReactionCore.Collections.Tags.before.update(Meteor.userId(), tag);
+  ReactionImport._buffers['Tags'].find(key).upsert().update({
+    $set: tag,
+    $setOnInsert: { createdAt: new Date() }
+  });
 }
 
-Import.image = function (key, image, links) {}
+ReactionImport.image = function (key, image, links) {}
 
-Import.relation = function (key, relation) {}
+ReactionImport.relation = function (key, relation) {}
