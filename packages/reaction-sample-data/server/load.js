6610e2296b44ac0e65d85bc3556851c5be75a7dd
Tom De Caluwé <decaluwe.t@gmail.com>
2015-11-06 17:01:31 +0100

    Load sample data on startup.

--- packages/reaction-sample-data/server/load.js
+++ packages/reaction-sample-data/server/load.js
@@ -3,24 +3,25 @@
  */
 
 Meteor.startup(function () {
-  try {
-    Fixtures.loadData(ReactionCore.Collections.Shops, Assets.getText("private/data/Shops.json"));
-    // start checking once per second if Shops collection is ready,
-    // then load the rest of the fixtures when it is
-    let wait = Meteor.setInterval(function () {
-      if (!!ReactionCore.Collections.Shops.find().count()) {
-        Meteor.clearInterval(wait);
-        Fixtures.loadI18n(ReactionCore.Collections.Translations);
-        Fixtures.loadData(ReactionCore.Collections.Products, Assets.getText("private/data/Products.json"));
-        Fixtures.loadData(ReactionCore.Collections.Tags, Assets.getText("private/data/Tags.json"));
-        // create default admin user
-        ReactionRegistry.createDefaultAdminUser();
-        // we've finished all reaction core initialization
-        ReactionCore.Log.info("Reaction Core initialization finished.");
-      }
-    }, 1000);
-  } catch (error) {
-    ReactionCore.Log.error("loadFixtures: ", error.message);
+  ReactionImport.process(Assets.getText("private/data/Shops.json"), ["name"], ReactionImport.shop);
+  ReactionImport.flush();
+  // start checking once per second if Shops collection is ready,
+  // then load the rest of the fixtures when it is
+  let wait = Meteor.setInterval(function () {
+    if (!!ReactionCore.Collections.Shops.find().count()) {
+      Meteor.clearInterval(wait);
+      ReactionImport.fixture().process(Assets.getText("private/data/Tags.json"), ["name"], ReactionImport.load);
+      ReactionImport.fixture().process(Assets.getText("private/data/Products.json"), ["title"], ReactionImport.load);
+      ReactionImport.flush();
+      ReactionRegistry.createDefaultAdminUser();
+      // we've finished all reaction core initialization
+      ReactionCore.Log.info("Reaction Core initialization finished.");
+    }
+  }, 1000);
+  // load package configurations
+  if (ReactionCore.Collections.Shops.find().count()) {
+    ReactionRegistry.setDomain();
+    ReactionRegistry.loadPackages();
   }
   return true;
 });
