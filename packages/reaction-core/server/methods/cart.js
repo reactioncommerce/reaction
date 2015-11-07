a17dc7c2809ea5043ac4cdd3cfb33eb5c52297c0
Mike Murray <hellomikemurray@gmail.com>
2015-11-06 19:45:01 -0800

    semicolon;

--- packages/reaction-core/server/methods/cart.js
+++ packages/reaction-core/server/methods/cart.js
@@ -43,7 +43,7 @@ Meteor.methods({
         }
       }]
     });
-    ReactionCore.Log.debug("sessionCarts", sessionCarts.fetch())
+    ReactionCore.Log.debug("sessionCarts", sessionCarts.fetch());
     ReactionCore.Log.debug(
       `merge cart: begin merge processing of session ${sessionId} into: ${currentCart._id}`
     );
