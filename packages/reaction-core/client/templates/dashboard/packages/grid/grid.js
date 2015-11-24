20dcad9993ab31298f5161a8ad44542f0cee7009
aaronjudd <aaron@ongoworks.com>
2015-11-23 23:45:56 -0800

    dashboard grid cleanup, labels updated

--- packages/reaction-core/client/templates/dashboard/packages/grid/grid.js
+++ packages/reaction-core/client/templates/dashboard/packages/grid/grid.js
@@ -1,17 +1,15 @@
 /**
-* packagesGrid helpers
-*
-*/
+ * packagesGrid helpers
+ *
+ */
 Template.packagesGrid.helpers({
-  pkgPermissions: function() {
-    if (ReactionCore.hasPermission('dashboard')) {
+  pkgPermissions: function () {
+    if (ReactionCore.hasPermission("dashboard")) {
       if (this.route) {
         return ReactionCore.hasPermission(this.route);
-      } else {
-        return ReactionCore.hasPermission(this.name);
       }
-    } else {
-      return false;
+      return ReactionCore.hasPermission(this.name);
     }
+    return false;
   }
 });
