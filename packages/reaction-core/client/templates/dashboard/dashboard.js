7554dd821ee656460cad7106ef9069df1c0a2b0a
aaronjudd <aaron@ongoworks.com>
2015-12-14 20:28:12 -0800

    Shop settings form patch
    - resolves https://github.com/reactioncommerce/reaction/issues/583

--- packages/reaction-core/client/templates/dashboard/dashboard.js
+++ packages/reaction-core/client/templates/dashboard/dashboard.js
@@ -5,16 +5,18 @@
  */
 
 Template.dashboard.helpers({
-  route: function() {
+  route: function () {
     return Router.current().route.getName();
   },
-  displayConsoleNavBar: function() {
-    if (ReactionCore.hasPermission('console') && Session.get("displayConsoleNavBar")) {
+  displayConsoleNavBar: function () {
+    if (ReactionCore.hasPermission("console") && Session.get(
+        "displayConsoleNavBar")) {
       return true;
     }
   },
-  displayConsoleDrawer: function() {
-    if (ReactionCore.hasPermission('console') && Session.get("displayConsoleDrawer")) {
+  displayConsoleDrawer: function () {
+    if (ReactionCore.hasPermission("console") && Session.get(
+        "displayConsoleDrawer")) {
       return true;
     }
   }
@@ -27,15 +29,14 @@ Template.dashboard.helpers({
  */
 
 Template.dashboard.events({
-  'click .dashboard-navbar-package': function(event, template) {
+  "click .dashboard-navbar-package": function (event) {
     Session.set("currentPackage", this.route);
-    if (this.route != null) {
+    if (this.route !== null) {
       event.preventDefault();
       return Router.go(this.route);
     }
   }
 });
-
 
 Template.dashboardHeader.helpers({
   showHeader: function () {
@@ -48,23 +49,20 @@ Template.dashboardHeader.helpers({
 
   "registry": function () {
     // just some handle little helpers for default package i18nKey/i18nLabel
-    var registry = ReactionCore.getRegistryForCurrentRoute("dashboard") || {};
+    let registry = ReactionCore.getRegistryForCurrentRoute("dashboard") || {};
     registry.nameSpace = registry.name || registry.template || "app";
-    registry.i18nLabel = registry.label || registry.provides || "Settings";
-    registry.i18nKey = registry.nameSpace.toCamelCase() + "." + registry.i18nLabel.toCamelCase();
+    registry.i18nLabel = registry.label || registry.provides ||
+      "Settings";
+    registry.i18nKey = registry.nameSpace.toCamelCase() + "." + registry.i18nLabel
+      .toCamelCase();
     return registry;
   },
 
   thisDashboard: function () {
-
     return ReactionCore.getRegistryForCurrentRoute("dashboard");
-
   }
 
-
-
 });
-
 
 Template.dashboardHeader.events({
   "click [data-event-action=showPackageSettings]": function () {
