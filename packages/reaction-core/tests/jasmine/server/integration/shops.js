4ecb1e245708962663106b6f699a2facbbc84a63
newsiberian <newsiberian2015@yandex.ru>
2016-01-30 14:59:19 +0600

    - fixed bug in shop tests;

--- packages/reaction-core/tests/jasmine/server/integration/shops.js
+++ packages/reaction-core/tests/jasmine/server/integration/shops.js
@@ -1,6 +1,4 @@
 describe("core shop schema", function () {
-  let shop;
-
   beforeEach(function () {
     return ReactionCore.Collections.Shops.remove({});
   });
@@ -15,6 +13,7 @@ describe("core shop schema", function () {
 });
 
 describe("core shop methods", function () {
+  let shop;
   beforeEach(function () {
     shop = Factory.create("shop");
   });
