eb3bf522b5bd9e427e98555b78d6e22c0ad3001b
Mike Murray <hellomikemurray@gmail.com>
2016-02-15 18:18:45 -0800

    Added core ui workflow
    added z-index to admin-controls for times when it overlaps with the
    main navigation bar

--- packages/reaction-ui/server/register.js
+++ packages/reaction-ui/server/register.js
@@ -8,30 +8,32 @@ ReactionCore.registerPackage({
     route: "/dashboard/ui",
     name: "reaction-ui/uiDashboard",
     provides: "dashboard",
+    workflow: "coreUIWorkflow",
     container: "appearance",
-    label: "UI",
+    label: "Themes",
     description: "Themes and UI Components",
     icon: "fa fa-html5",
     priority: 1,
     template: "uiDashboard"
   }, {
     route: "/dashboard/ui/:id",
+    workflow: "coreUIWorkflow",
     name: "dashboard/uiThemeDetails",
     template: "uiThemeDetails"
   }],
   layout: [{
     layout: "coreLayout",
-    workflow: "coreAccountsWorkflow",
+    workflow: "coreUIWorkflow",
     collection: "Themes",
     theme: "default",
     enabled: true,
     structure: {
-      template: "accountsDashboard",
+      template: "uiDashboard",
       layoutHeader: "layoutHeader",
       layoutFooter: "",
       notFound: "notFound",
       dashboardHeader: "dashboardHeader",
-      dashboardControls: "accountsDashboardControls",
+      dashboardControls: "",
       dashboardHeaderControls: "",
       adminControlsFooter: "adminControlsFooter"
     }
