6e562bc6a492343cc917c18fb7250a3e0619f607
Mike Murray <hellomikemurray@gmail.com>
2016-03-04 14:20:20 -0800

    Updated import to merge new properties from fixture data

--- packages/reaction-core/server/registry/loadPackages.js
+++ packages/reaction-core/server/registry/loadPackages.js
@@ -1,3 +1,5 @@
+const merge = Npm.require("lodash.merge");
+
 /**
  *  ReactionRegistry.loadPackages
  *  insert Reaction packages into registry
@@ -8,6 +10,24 @@
  *  @return {String} returns insert result
  */
 ReactionRegistry.loadPackages = function () {
+  let settingsJSONAsset;
+  let settingsFromJSON;
+  const packages = ReactionCore.Collections.Packages.find({}).fetch();
+
+  // Attempt to load reaction.json fixture data
+  try {
+    settingsJSONAsset = AppAssets.getText("settings/reaction-poop.json");
+    const validatedJson = EJSON.parse(settingsJSONAsset);
+
+    if (!_.isArray(validatedJson[0])) {
+      ReactionCore.Log.warn("Load Settings is not an array. Failed to load settings.");
+    } else {
+      settingsFromJSON = validatedJson;
+    }
+  } catch (error) {
+    ReactionCore.Log.warn("loadSettings reaction.json not loaded.", error);
+  }
+
   // for each shop, we're loading packages a unique registry
   _.each(ReactionRegistry.Packages, (config, pkgName) => {
     return ReactionCore.Collections.Shops.find().forEach((shop) => {
@@ -15,17 +35,46 @@ ReactionRegistry.loadPackages = function () {
       if (!shopId) return [];
       // existing registry will be upserted with changes, perhaps we should add:
       ReactionRegistry.assignOwnerRoles(shopId, pkgName, config.registry);
-      ReactionImport.package({
+
+      // Settings from the package registry.js
+      const settingsFromPackage = {
         name: pkgName,
         icon: config.icon,
         enabled: !!config.autoEnable,
         settings: config.settings,
         registry: config.registry,
         layout: config.layout
-      }, shopId);
+      };
+
+      // Setting from a fixture file, most likely reaction.json
+      let settingsFromFixture;
+      if (settingsFromJSON) {
+        settingsFromFixture = _.find(settingsFromJSON[0], (packageSetting) => {
+          return config.name === packageSetting.name;
+        });
+      }
+
+      // Setting already imported into the packages collection
+      const settingsFromDB = _.find(packages, (ps) => {
+        if (config.name === ps.name && shopId === ps.shopId) {
+          return true;
+        }
+      });
+
+      const combinedSettings = merge(
+        {},
+        settingsFromPackage,
+        settingsFromFixture || {},
+        settingsFromDB || {}
+      );
+
+      // Import package data
+      ReactionImport.package(combinedSettings, shopId);
+
       ReactionCore.Log.info(`Initializing ${shop.name} ${pkgName}`);
     });
   });
+
   //
   // package cleanup
   //
