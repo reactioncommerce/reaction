46428e4980b3728b79df815aed6da135840a5a20
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-03-08 17:33:17 -0500

    refactor Docker build to be configurable and lean

---
+++ bin/docker/install-phantom.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ "${INSTALL_PHANTOMJS}" = "true" ]; then
+
+  : ${PHANTOMJS_VERSION:=2.1.3}
+
+  printf "\n[-] Installing Phantom.js $PHANTOMJS_VERSION...\n"
+
+  npm install --silent -g phantomjs@$PHANTOMJS_VERSION
+
+  npm cache clear
+  
+fi
