225f5bfe5a321b6fba3845dc44f9887ffde07757
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-03-09 19:55:59 -0500

    install specific Meteor version based on app

--- docker/scripts/install-meteor.sh
+++ docker/scripts/install-meteor.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-printf "\n[-] Installing Meteor.js...\n\n"
+set -e
 
-apt-get update -qq -y
+if [ "$DEV_BUILD" = "true" ]; then
+  curl https://install.meteor.com/ | sh
+else
+  # download installer script
+  curl https://install.meteor.com -o /tmp/install_meteor.sh
 
-DEBIAN_FRONTEND=noninteractive apt-get install -qq -y curl git
+  # read in the release version in the app
+  METEOR_VERSION=$(head $APP_SOURCE_DIR/.meteor/release | cut -d "@" -f 2)
 
-curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
+  # set the release version in the install script
+  sed -i "s/RELEASE=.*/RELEASE=\"$METEOR_VERSION\"/g" /tmp/install_meteor.sh
+
+  # install
+  printf "\n[-] Installing Meteor $METEOR_VERSION...\n\n"
+  sh /tmp/install_meteor.sh
+fi
