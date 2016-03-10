8a74848975c9727926a32ccd87499224f6743c17
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-03-09 19:51:34 -0500

    update phantom dependency

--- docker/scripts/install-phantom.sh
+++ docker/scripts/install-phantom.sh
@@ -4,11 +4,11 @@ set -e
 
 if [ "${INSTALL_PHANTOMJS}" = "true" ]; then
 
-  : ${PHANTOMJS_VERSION:=2.1.3}
+  : ${PHANTOMJS_VERSION:=2.1.1}
 
-  printf "\n[-] Installing Phantom.js $PHANTOMJS_VERSION...\n"
+  printf "\n[-] Installing Phantom.js $PHANTOMJS_VERSION...\n\n"
 
-  npm install --silent -g phantomjs@$PHANTOMJS_VERSION
+  npm install --silent -g phantomjs-prebuilt@$PHANTOMJS_VERSION
 
   npm cache clear
 
