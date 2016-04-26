44f9cd8b7d902952d3cfa4b7a3df8325738e8bc2
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-04-26 12:49:02 -0400

    combine server side ReactionCore methods

--- server/api/core.js
+++ server/api/core.js
@@ -1,11 +1,154 @@
 import { Shops } from "/lib/collections";
+import { Hooks, Logger } from "./logger";
 
 /**
  * ReactionCore methods (server)
  */
+export class ReactionCore {
 
-_.extend(ReactionCore, {
-  getCurrentShopCursor: function () {
+  init() {
+    // run onCoreInit hooks
+    Hooks.Events.run("onCoreInit", this);
+    // start job server
+    Logger.info("JobServer started:", Jobs.startJobServer());
+    // uncomment for JobCollection debug
+    // Jobs.setLogStream(process.stdout);
+    ReactionRegistry.loadPackages();
+    // process imports from packages and any hooked imports
+    ReactionImport.flush();
+    // timing is important, packages are rqd
+    // for initilial permissions configuration.
+    ReactionRegistry.createDefaultAdminUser();
+    // hook after init finished
+    Hooks.Events.run("afterCoreInit", this);
+
+    Logger.info("ReactionCore.init() has run");
+
+    return true;
+  }
+
+  /**
+   * hasPermission - server
+   * server permissions checks
+   * hasPermission exists on both the server and the client.
+   * @param {String | Array} checkPermissions -String or Array of permissions if empty, defaults to "admin, owner"
+   * @param {String} checkUserId - userId, defaults to Meteor.userId()
+   * @param {String} checkGroup group - default to shopId
+   * @return {Boolean} Boolean - true if has permission
+   */
+  hasPermission(checkPermissions, userId = Meteor.userId(), checkGroup = this.getShopId()) {
+    // check(checkPermissions, Match.OneOf(String, Array));
+    // check(userId, String);
+    // check(checkGroup, Match.Optional(String));
+
+    let permissions;
+    // default group to the shop or global if shop
+    // isn't defined for some reason.
+    if (checkGroup !== undefined && typeof checkGroup === "string") {
+      group = checkGroup;
+    } else {
+      group = ReactionCore.getShopId() || Roles.GLOBAL_GROUP;
+    }
+
+    // permissions can be either a string or an array
+    // we'll force it into an array and use that
+    if (checkPermissions === undefined) {
+      permissions = ["owner"];
+    } else if (typeof checkPermissions === "string") {
+      permissions = [checkPermissions];
+    } else {
+      permissions = checkPermissions;
+    }
+
+    // if the user has admin, owner permissions we'll always check if those roles are enough
+    permissions.push("owner");
+    permissions = _.uniq(permissions);
+
+    // return if user has permissions in the group
+    if (Roles.userIsInRole(userId, permissions, group)) {
+      return true;
+    }
+
+    // global roles check
+    let sellerShopPermissions = Roles.getGroupsForUser(userId, "admin");
+
+    // we're looking for seller permissions.
+    if (sellerShopPermissions) {
+      // loop through shops roles and check permissions
+      for (let key in sellerShopPermissions) {
+        if (key) {
+          let shop = sellerShopPermissions[key];
+          if (Roles.userIsInRole(userId, permissions, shop)) {
+            return true;
+          }
+        }
+      }
+    }
+    // no specific permissions found returning false
+    return false;
+  }
+
+  hasOwnerAccess() {
+    let ownerPermissions = ["owner"];
+    return this.hasPermission(ownerPermissions);
+  }
+
+  hasAdminAccess() {
+    let adminPermissions = ["owner", "admin"];
+    return this.hasPermission(adminPermissions);
+  }
+
+  hasDashboardAccess() {
+    let dashboardPermissions = ["owner", "admin", "dashboard"];
+    return this.hasPermission(dashboardPermissions);
+  }
+
+  getSellerShopId() {
+    return Roles.getGroupsForUser(this.userId, "admin");
+  }
+
+  configureMailUrl(user, password, host, port) {
+    let shopSettings = Packages.findOne({
+      shopId: this.getShopId(),
+      name: "core"
+    });
+    let shopMail = {};
+
+    if (shopSettings) {
+      shopMail = shopSettings.settings.mail || {};
+    }
+
+    let processUrl = process.env.MAIL_URL;
+    let settingsUrl = Meteor.settings.MAIL_URL;
+
+    if (user && password && host && port) {
+      let mailString = `smtp://${user}:${password}@${host}:${port}/`;
+      const mailUrl = processUrl = settingsUrl = mailString;
+      process.env.MAIL_URL = mailUrl;
+      return mailUrl;
+    } else if (shopMail.user && shopMail.password && shopMail.host &&
+      shopMail.port) {
+      let mailString =
+        `smtp://${shopMail.user}:${shopMail.password}@${shopMail.host}:${shopMail.port}/`;
+      const mailUrl = processUrl = settingsUrl = mailString;
+      process.env.MAIL_URL = mailUrl;
+
+      Logger.info(`setting default mail url to: ${shopMail.host}`);
+      return mailUrl;
+    } else if (settingsUrl && !processUrl) {
+      const mailUrl = processUrl = settingsUrl;
+      process.env.MAIL_URL = mailUrl;
+      return mailUrl;
+    }
+    // return reasonable warning that we're not configured correctly
+    if (!process.env.MAIL_URL) {
+      Logger.warn(
+        "Mail server not configured. Unable to send email.");
+      return false;
+    }
+  }
+
+  getCurrentShopCursor() {
     let domain = this.getDomain();
     let cursor = Shops.find({
       domains: domain
@@ -17,32 +160,39 @@ _.extend(ReactionCore, {
         domain);
     }
     return cursor;
-  },
-  getCurrentShop: function () {
+  }
+
+  getCurrentShop() {
     const currentShopCursor = this.getCurrentShopCursor();
     // also, we could check in such a way: `currentShopCursor instanceof Object`
     // but not instanceof something.Cursor
     if (typeof currentShopCursor === "object") {
       return currentShopCursor.fetch()[0];
     }
-  },
-  getShopId: function () {
+    return null;
+  }
+
+  getShopId() {
     const currentShop = this.getCurrentShop();
     if (typeof currentShop === "object") {
       return currentShop._id;
     }
-  },
-  getDomain: function () {
+    return null;
+  }
+
+  getDomain() {
     let absoluteUrl = Meteor.absoluteUrl();
     if (typeof absoluteUrl === "string") {
       return absoluteUrl.split("/")[2].split(":")[0];
     }
     return "localhost";
-  },
-  getShopName: function () {
+  }
+
+  getShopName() {
     const currentShop = this.getCurrentShop();
     if (typeof currentShop === "object") {
       return currentShop.name;
     }
+    return null;
   }
-});
+}
