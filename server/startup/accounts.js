86339755b5d4d5ea2f87af43753652fe34f9691e
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-04-28 18:54:41 -0400

    move accounts config to startup

--- server/startup/accounts.js
+++ server/startup/accounts.js
@@ -4,6 +4,45 @@ import { Hooks, Logger, Reaction } from "/server/api";
 
 export default function () {
   /**
+   * Make sure initial admin user has verified their
+   * email before allowing them to login.
+   *
+   * http://docs.meteor.com/#/full/accounts_validateloginattempt
+   */
+
+  Accounts.validateLoginAttempt(function (attempt) {
+    if (!attempt.allowed) {
+      return false;
+    }
+
+    // confirm this is the accounts-password login method
+    if (attempt.type !== "password" || attempt.methodName !== "login") {
+      return attempt.allowed;
+    }
+
+    if (!attempt.user) {
+      return attempt.allowed;
+    }
+
+    let loginEmail = attempt.methodArguments[0].user.email;
+    let adminEmail = process.env.REACTION_EMAIL;
+
+    if (loginEmail && loginEmail === adminEmail) {
+      // filter out the matching login email from any existing emails
+      let userEmail = _.filter(attempt.user.emails, function (email) {
+        return email.address === loginEmail;
+      });
+
+      // check if the email is verified
+      if (!userEmail.length || !userEmail[0].verified) {
+        throw new Meteor.Error("403", "Oops! Please validate your email first.");
+      }
+    }
+
+    return attempt.allowed;
+  });
+
+  /**
    * Reaction Accounts handlers
    * creates a login type "anonymous"
    * default for all unauthenticated visitors
