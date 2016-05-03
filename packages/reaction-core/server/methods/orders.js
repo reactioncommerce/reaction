08963dc52c164e37ee83a0c048f79cec4569c417
newsiberian <newsiberian@users.noreply.github.com>
2016-05-03 22:44:29 +0700

    - renamed results in `orders/shipmentShipped`; (#995)

--- packages/reaction-core/server/methods/orders.js
+++ packages/reaction-core/server/methods/orders.js
@@ -192,8 +192,8 @@ Meteor.methods({
     }
 
     return {
-      notify: notifyResult,
-      workflow: workflowResult,
+      notifyResult: notifyResult,
+      workflowResult: workflowResult,
       completedItems: completedItemsResult,
       completedOrder: completedOrderResult
     };
