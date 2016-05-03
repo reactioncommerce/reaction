a1b4b9a426746194379a95a014340a4b41aac2b3
Mike Murray <hellomikemurray@gmail.com>
2016-05-03 12:33:05 -0700

    Update product grid loading
    Avoiding using find().fetch() to get products as it seems to cause slow
    loading behavior. Reduced unnecessary duplicate fetches / template
    redraws which should help with performance.

--- packages/reaction-product-variant/client/templates/products/productGrid/productGrid.js
+++ packages/reaction-product-variant/client/templates/products/productGrid/productGrid.js
@@ -36,21 +36,50 @@ function loadMoreProducts() {
 
 Template.productGrid.onCreated(function () {
   Session.set("productGrid/selectedProducts", []);
+
+  this.products = ReactiveVar();
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    canLoadMoreProducts: false
+  });
+
   // Update product subscription
   this.autorun(() => {
     const slug = ReactionRouter.getParam("slug");
     const { Tags } = ReactionCore.Collections;
     const tag = Tags.findOne({ slug: slug }) || Tags.findOne(slug);
+    const scrollLimit = Session.get("productScrollLimit");
     let tags = {}; // this could be shop default implementation needed
+
     if (tag) {
       tags = {tags: [tag._id]};
     }
+
     // if we get an invalid slug, don't return all products
     if (!tag && slug) {
       return;
     }
+
     const queryParams = Object.assign({}, tags, ReactionRouter.current().queryParams);
-    Meteor.subscribe("Products", Session.get("productScrollLimit"), queryParams);
+    this.subscribe("Products", scrollLimit, queryParams);
+
+    // we are caching `currentTag` or if we are not inside tag route, we will
+    // use shop name as `base` name for `positions` object
+    const currentTag = ReactionProduct.getTag();
+    const products = ReactionCore.Collections.Products.find({
+      ancestors: []
+      // keep this, as an example
+      // type: { $in: ["simple"] }
+    }, {
+      sort: {
+        [`positions.${currentTag}.position`]: 1,
+        [`positions.${currentTag}.createdAt`]: 1,
+        createdAt: 1
+      }
+    });
+
+    this.state.set("canLoadMoreProducts", products.count() >= Session.get("productScrollLimit"));
+    this.products.set(products);
   });
 
   this.autorun(() => {
@@ -83,85 +112,32 @@ Template.productGrid.events({
 
     Session.set("productGrid/selectedProducts", _.uniq(selectedProducts));
 
-    let products = Template.instance().products;
-    let filteredProducts = _.filter(products, (product) => {
-      return _.contains(selectedProducts, product._id);
-    });
+    let productCursor = Template.instance().products.get();
 
-    ReactionCore.showActionView({
-      label: i18next.t("productDetailEdit.productSettings"),
-      template: "productSettings",
-      type: "product",
-      data: {
-        products: filteredProducts
-      }
-    });
+    if (productCursor) {
+      const products = productCursor.fetch();
+
+      let filteredProducts = _.filter(products, (product) => {
+        return _.contains(selectedProducts, product._id);
+      });
+
+      ReactionCore.showActionView({
+        label: i18next.t("productDetailEdit.productSettings"),
+        template: "productSettings",
+        type: "product",
+        data: {
+          products: filteredProducts
+        }
+      });
+    }
   }
 });
 
 Template.productGrid.helpers({
-  loadMoreProducts: function () {
-    return ReactionCore.Collections.Products.find().count() >= Session.get("productScrollLimit");
+  loadMoreProducts() {
+    return Template.instance().state.equals("canLoadMoreProducts", true);
   },
-  products: function () {
-    /*
-     * take natural sort, sorting by updatedAt
-     * then resort using positions[currentTag].position for this tag
-     * retaining natural sort of untouched items
-     */
-
-    // we are caching `currentTag` or if we are not inside tag route, we will
-    // use shop name as `base` name for `positions` object
-    const currentTag = ReactionProduct.getTag();
-
-    // function to compare and sort position
-    function compare(a, b) {
-      // we need to check that fields exists
-      // todo we could remove part of this checks of `positions` and `base`
-      // settings will be required fields
-      if (a.positions && a.positions[currentTag] &&
-        b.positions && b.positions[currentTag]) {
-        if (a.positions[currentTag].position === b.positions[currentTag].position) {
-          const x = a.positions[currentTag].createdAt;
-          const y = b.positions[currentTag].createdAt;
-
-          if (x > y) {
-            return -1;
-          } else if (x < y) {
-            return 1;
-          }
-
-          return 0;
-        }
-        return a.positions[currentTag].position - b.positions[currentTag].position;
-      } // if some of them not exist, we need to comprare products `updatedAt`
-      const x = a.createdAt;
-      const y = b.createdAt;
-
-      if (x > y) {
-        return -1;
-      } else if (x < y) {
-        return 1;
-      }
-      return 0;
-    }
-
-    // we are passing `ancestors: []`, because in case when we turn back from PDP
-    // for a moment we still subscribed to variants too, and we will get an error
-    // because of it, because our `productGrid` component can't work with variants
-    // objects.
-    //
-    // Also, we it is possible to change this selector to the following:
-    // `type: { $in: ["simple"] }`, but I found this way is not kind to package
-    // creators, because to specify they new product type, they will need to change
-    // this file, which broke another piece of compatibility with `reaction`
-    let gridProducts = ReactionCore.Collections.Products.find({
-      ancestors: []
-      // keep this, as an example
-      // type: { $in: ["simple"] }
-    }).fetch();
-    const products = gridProducts.sort(compare);
-    Template.instance().products = products;
-    return products;
+  products() {
+    return Template.instance().products.get();
   }
 });
