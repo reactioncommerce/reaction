dee0ac4166829e03edb7eb6eb64d7780aa05a772
Mike Murray <hellomikemurray@gmail.com>
2016-05-06 12:33:57 -0700

    Updated product grid
    Moved subscription and fetch to parent component.
    Added a loading spinner.
    Product now tries to wait for all products to load before showing the
    grid. This works 80% of the time.

--- packages/reaction-product-variant/client/templates/products/productGrid/productGrid.js
+++ packages/reaction-product-variant/client/templates/products/productGrid/productGrid.js
@@ -2,98 +2,8 @@
  * productGrid helpers
  */
 
-/**
- * loadMoreProducts
- * @summary whenever #productScrollLimitLoader becomes visible, retrieve more results
- * this basically runs this:
- * Session.set('productScrollLimit', Session.get('productScrollLimit') + ITEMS_INCREMENT);
- * @return {undefined}
- */
-function loadMoreProducts() {
-  let threshold;
-  let target = $("#productScrollLimitLoader");
-  let scrollContainer = $("#reactionAppContainer");
-
-  if (scrollContainer.length === 0) {
-    scrollContainer = $(window);
-  }
-
-  if (target.length) {
-    threshold = scrollContainer.scrollTop() + scrollContainer.height() - target.height();
-
-    if (target.offset().top < threshold) {
-      if (!target.data("visible")) {
-        target.data("productScrollLimit", true);
-        Session.set("productScrollLimit", Session.get("productScrollLimit") + ITEMS_INCREMENT || 24);
-      }
-    } else {
-      if (target.data("visible")) {
-        target.data("visible", false);
-      }
-    }
-  }
-}
-
 Template.productGrid.onCreated(function () {
   Session.set("productGrid/selectedProducts", []);
-
-  this.products = ReactiveVar();
-  this.state = new ReactiveDict();
-  this.state.setDefault({
-    canLoadMoreProducts: false
-  });
-
-  // Update product subscription
-  this.autorun(() => {
-    const slug = ReactionRouter.getParam("slug");
-    const { Tags } = ReactionCore.Collections;
-    const tag = Tags.findOne({ slug: slug }) || Tags.findOne(slug);
-    const scrollLimit = Session.get("productScrollLimit");
-    let tags = {}; // this could be shop default implementation needed
-
-    if (tag) {
-      tags = {tags: [tag._id]};
-    }
-
-    // if we get an invalid slug, don't return all products
-    if (!tag && slug) {
-      return;
-    }
-
-    const queryParams = Object.assign({}, tags, ReactionRouter.current().queryParams);
-    this.subscribe("Products", scrollLimit, queryParams);
-
-    // we are caching `currentTag` or if we are not inside tag route, we will
-    // use shop name as `base` name for `positions` object
-    const currentTag = ReactionProduct.getTag();
-    const products = ReactionCore.Collections.Products.find({
-      ancestors: []
-      // keep this, as an example
-      // type: { $in: ["simple"] }
-    }, {
-      sort: {
-        [`positions.${currentTag}.position`]: 1,
-        [`positions.${currentTag}.createdAt`]: 1,
-        createdAt: 1
-      }
-    });
-
-    this.state.set("canLoadMoreProducts", products.count() >= Session.get("productScrollLimit"));
-    this.products.set(products);
-  });
-
-  this.autorun(() => {
-    const isActionViewOpen = ReactionCore.isActionViewOpen();
-    if (isActionViewOpen === false) {
-      Session.set("productGrid/selectedProducts", []);
-    }
-  });
-});
-
-Template.productGrid.onRendered(() => {
-  // run the above func every time the user scrolls
-  $("#reactionAppContainer").on("scroll", loadMoreProducts);
-  $(window).on("scroll", loadMoreProducts);
 });
 
 Template.productGrid.events({
@@ -112,7 +22,7 @@ Template.productGrid.events({
 
     Session.set("productGrid/selectedProducts", _.uniq(selectedProducts));
 
-    let productCursor = Template.instance().products.get();
+    let productCursor = Template.currentData().products;
 
     if (productCursor) {
       const products = productCursor.fetch();
@@ -138,6 +48,6 @@ Template.productGrid.helpers({
     return Template.instance().state.equals("canLoadMoreProducts", true);
   },
   products() {
-    return Template.instance().products.get();
+    return Template.currentData().products;
   }
 });
