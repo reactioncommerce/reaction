3e0e93427014914af16122bf815d584fb4bece0d
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-05-16 13:25:21 -0400

    fix default data loading

---
+++ server/startup/load-data.js
@@ -0,0 +1,19 @@
+import { Shops } from "/lib/collections";
+import { Hooks, Logger, Reaction } from "/server/api";
+
+export default function () {
+  /**
+   * Hook to setup core additional imports during ReactionCore init (shops process first)
+   */
+
+  Hooks.Events.add("onCoreInit", () => {
+    Logger.info("Initialize using reaction-sample-data");
+    Reaction.Import.fixture().process(Assets.getText("data/Shops.json"), ["name"], Reaction.Import.shop);
+    // ensure Shops are loaded first.
+    Reaction.Import.flush(Shops);
+    // these will flush/import with the rest of the imports from core init.
+    Reaction.Import.fixture().process(Assets.getText("data/Products.json"), ["title"], Reaction.Import.load);
+    Reaction.Import.fixture().process(Assets.getText("data/Tags.json"), ["name"], Reaction.Import.load);
+    Reaction.Import.flush();
+  });
+}
