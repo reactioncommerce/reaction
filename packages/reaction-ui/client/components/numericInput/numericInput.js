4d485edf877f6e15268dee6d0460beb20455ae5b
Mike Murray <hellomikemurray@gmail.com>
2016-05-23 18:25:06 -0700

    Dashboard totals 992 (#1023)
    * Update numeric input component
    
    Convert numeric input to a React component. Rely on the shops currency
    instead of the currency of the current locale.
    
    * Updated instances of price formatting in dashboard
    
    Dashboard orders now uses the numeric input for all instances where
    price must be formatted. This applies to both editable and non editable
    instances.
    
    * adjust price format for 0 decimal currencies
    
    * Modernize and resolve console erros.
    
    Resolves a 'node cannot` be removed error in console.
    Modernized code to be inline with current practices.
    
    * Fixed references to order object
    
    * combine duplicate items in order item list
    
    * fixing eslint issues
    
    * Updated refund field
    
    Refund field now uses the new react NumericInput component.

--- packages/reaction-ui/client/components/numericInput/numericInput.js
+++ packages/reaction-ui/client/components/numericInput/numericInput.js
@@ -1,52 +1,189 @@
+import React from "react";
+import classnames from "classnames";
 const accounting = require("accounting-js");
-import $ from "jquery";
-require("autonumeric");
 
-/**
- * numericInput onCreated
- */
-Template.numericInput.onCreated(() => {
-  const template = Template.instance();
+function setCaretPosition(ctrl, pos) {
+  if (ctrl.setSelectionRange) {
+    ctrl.focus();
+    ctrl.setSelectionRange(pos, pos);
+  } else if (ctrl.createTextRange) {
+    const range = ctrl.createTextRange();
 
-  // Initial method for autoNumeric field
-  template.autoNumericFieldState = "init";
-});
+    range.collapse(true);
+    range.moveEnd("character", pos);
+    range.moveStart("character", pos);
+    range.select();
+  }
+}
 
-/**
- * numericInput onRendered
- * @summary attaches "autoNumeric" to the input element of this template
- */
-Template.numericInput.onRendered(() => {
-  const template = Template.instance();
+class NumericInput extends React.Component {
+  constructor(props) {
+    super(props);
 
-  template.autorun(() => {
-    const data = Template.currentData();
+    // Set default state
+    this.state = {
+      value: this.props.value
+    };
 
-    Meteor.call("shop/getLocale", (error, result) => {
-      if (_.isObject(result)) {
-        // Set options
-        const options = Object.assign({},
-          result.currency,
-          {
-            minValue: "0.00",
-            maxValue: "999999999.99"
-          },
-          data
-        );
+    // Bind event handlers
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        $(template.find("input")).autoNumeric(template.autoNumericFieldState, {
-          aSep: options.thousand,
-          dGroup: options.grouping,
-          aSign: options.symbol,
-          aDec: options.decimal,
-          vMax: accounting.toFixed(options.maxValue, 2),
-          vMin: accounting.toFixed(options.minValue, 2),
-          wEmpty: "sign"
-        });
+  /**
+   * update state when component receives props
+   * @param  {Object} nextProps new props
+   * @return {undefined}
+   */
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      value: nextProps.value
+    });
+  }
 
-        // Subsquent calls runs will update autoNumeric on our field, instead of init
-        template.autoNumericFieldState = "update";
+  get moneyFormat() {
+    const moneyFormat = this.props.format || {};
+    // precision is mis-represented in accounting.js. Precision in this case is actually scale
+    // so we add the property for precision based on scale.
+    moneyFormat.precision = moneyFormat.scale !== undefined ? moneyFormat.scale : 2;
+
+    return moneyFormat;
+  }
+
+  get displayValue() {
+    const value = this.state.value;
+
+    if (typeof value === "number") {
+      if (this.props.format && this.props.format.scale === 0) {
+        return this.format(value * 100);
+      }
+      return this.format(value);
+    }
+
+    return 0;
+  }
+
+  get scale() {
+    const parts = this.state.value.split(".");
+
+    if (parts.length === 2) {
+      return parts[1].length;
+    }
+
+    return 0;
+  }
+
+  /**
+   * format a numeric string
+   * @param  {String} value Value to format
+   * @param  {Object} format Object containing settings for formatting value
+   * @return {String} Foramtted numeric string
+   */
+  format(value, format) {
+    const moneyFormat = format || this.moneyFormat;
+
+
+    // value * (10 ^ (2 - moneyFormat.scale))
+
+    // console.log(moneyFormat, value, value * Math.pow(10, 2 - moneyFormat.precision));
+    const decimal = moneyFormat.decimal || undefined;
+    const unformatedValue = this.unformat(value, decimal);
+
+    return accounting.formatMoney(unformatedValue, moneyFormat);
+  }
+
+  /**
+   * unformat numeric string
+   * @param  {String} value String value to unformat
+   * @param  {String} decimal String representing the decimal place
+   * @return {String} unformatted numeric string
+   */
+  unformat(value, decimal) {
+    return accounting.unformat(value, decimal);
+  }
+
+  /**
+   * Handle change event from text input
+   * @param  {SytheticEvent} event Change event
+   * @return {undefined}
+   */
+  handleChange(event) {
+    const input = event.currentTarget;
+    const value = event.currentTarget.value;
+    let numberValue = this.unformat(value);
+
+    if (this.props.format.scale === 0) {
+      numberValue = numberValue / 100;
+    }
+
+    this.setState({
+      value: numberValue,
+      caretPosition: input.selectionStart
+    }, () => {
+      setCaretPosition(input, Math.max(this.state.caretPosition, 0));
+
+      if (this.props.onChange) {
+        this.props.onChange(event, { value, numberValue });
       }
     });
-  });
-});
+  }
+
+  /**
+   * render
+   * @return {ReactElement} markup
+   */
+  render() {
+    const { classNames } = this.props;
+
+
+    if (this.props.isEditing === false) {
+      const textValueClassName = classnames({
+        rui: true,
+        text: true,
+        ...(classNames.text || {})
+      });
+
+      return (
+        <span className={textValueClassName}>
+          {this.displayValue}
+        </span>
+      );
+    }
+
+    const fieldClassName = classnames({
+      "form-control": true, // eslint-disable-line: quote-props
+      ...(classNames.input || {})
+    });
+
+    return (
+      <div className="rui control numeric-input">
+        <input
+          className={fieldClassName}
+          disabled={this.props.disabled}
+          onChange={this.handleChange}
+          value={this.displayValue}
+        />
+      </div>
+    );
+  }
+}
+
+NumericInput.displayName = "Numeric Input";
+
+NumericInput.defaultProps = {
+  disabled: false,
+  isEditing: true,
+  classNames: {}
+};
+
+NumericInput.propTypes = {
+  classNames: React.PropTypes.shape({}),
+  disabled: React.PropTypes.bool,
+  format: React.PropTypes.shape({
+    scale: React.PropTypes.number
+  }),
+  isEditing: React.PropTypes.bool,
+  onChange: React.PropTypes.func,
+  value: React.PropTypes.number
+};
+
+export default NumericInput;
