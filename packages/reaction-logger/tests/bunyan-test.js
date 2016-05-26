5cade1f6d0e9976a3119c45104d74bd6412e4e1f
Aaron Judd <aaron@reactioncommerce.com>
2016-05-25 19:16:32 -0700

    Velocity ci patches (#1046)
    * add bin/ci.sh
    
    wraps velocity test and watch for tests passed output.  ci will timeout
    if this doesn’t exit in 10 minutes.
    
    * add npm.depends for bunyan testing
    
    kludge fix, add npm.depends specifically for testing
    
    * clean up extra param
    
    remove extra VELOCITY_TEST_PACKAGES=1
    
    * add export
    
    the cleanup that became dirty.. add export

--- packages/reaction-logger/tests/bunyan-test.js
+++ packages/reaction-logger/tests/bunyan-test.js
@@ -1,4 +1,7 @@
-Tinytest.add('bunyan', function(test) {
-  test.isNotNull(logger, 'logger should be available');
-  test.isNotNull(bunyan, 'bunyan should be available');
+bunyan = Npm.require("bunyan");
+bunyanFormat = Npm.require("bunyan-format");
+
+Tinytest.add("bunyan", function (test) {
+  test.isNotNull(logger, "logger should be available");
+  test.isNotNull(bunyan, "bunyan should be available");
 });
