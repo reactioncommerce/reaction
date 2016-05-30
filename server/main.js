bf0572741887d5369e7ebcebe5aa6dc9be0e0f60
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-05-30 13:05:41 -0400

    fix media methods

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 // import Fixtures from "./fixtures";  // TODO: get this working!
-// import Methods from "./methods"; // TODO: wrap each file in a closure
+import "./methods"; // TODO: refactor all of the methods to use import/export
 // import Publications from "./publications";  // TODO: wrap each file in a closure
 import Security from "./security";
 import Startup from "./startup";
