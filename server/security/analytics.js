ddb0b6084c1e70f2c418a54242b18bc985e9834b
aaronjudd <aaron@reactioncommerce.com>
2016-06-02 22:11:49 -0700

    add analytics i18n messages
    - updated permissions

--- server/security/analytics.js
+++ server/security/analytics.js
@@ -2,11 +2,7 @@ import { AnalyticsEvents } from "/lib/collections";
 import { Reaction } from "/server/api";
 
 export default function () {
-  AnalyticsEvents.permit(["insert"]).ifHasRole({
-    role: ["guest", "anonymous"],
-    group: Reaction.getShopId()
-  }).allowInClientCode();
-
+  AnalyticsEvents.permit("insert").ifLoggedIn().allowInClientCode();
   AnalyticsEvents.permit(["update", "remove"]).ifHasRole({
     role: ["admin", "owner"],
     group: Reaction.getShopId()
