e6b1e1bcb8f234bed726656c87c21b1e2c576980
Brent Hoover <brent@thebuddhalodge.com>
2016-06-17 07:57:04 +0800

    Working prototype test

---
+++ server/methods/core/cart-patch.app-test.js
@@ -0,0 +1,78 @@
+/* eslint dot-notation: 0 */
+
+import { Meteor } from "meteor/meteor";
+import { Factory } from "meteor/dburles:factory";
+import { Reaction } from "/server/api";
+import { Cart, Products, Accounts } from "/lib/collections";
+import { expect } from "meteor/practicalmeteor:chai";
+import { sinon, stubs, spies } from "meteor/practicalmeteor:sinon";
+import { addProduct } from "/server/imports/fixtures/products";
+import Fixtures from "/server/imports/fixtures";
+
+Fixtures();
+
+describe("Monkey-patching methods", function () {
+  let user = Factory.create("user");
+  let originals = {};
+
+  before(function () {
+    originals["mergeCart"] = Meteor.server.method_handlers["cart/mergeCart"];
+    originals["copyCartToOrder"] = Meteor.server.method_handlers["cart/copyCartToOrder"];
+    originals["addToCart"] = Meteor.server.method_handlers["cart/addToCart"];
+    originals["setShipmentAddress"] = Meteor.server.method_handlers["cart/setShipmentAddress"];
+    originals["setPaymentAddress"] = Meteor.server.method_handlers["cart/setPaymentAddress"];
+  });
+
+
+  function spyOnMethod(method, id) {
+    return sinon.stub(Meteor.server.method_handlers, `cart/${method}`, function () {
+      check(arguments, [Match.Any]); // to prevent audit_arguments from complaining
+      this.userId = id;
+      return originals[method].apply(this, arguments);
+    });
+  }
+
+  after(function() {
+    spies.restoreAll();
+    stubs.restoreAll();
+  });
+
+  describe("cart/addToCart", function () {
+    const quantity = 1;
+    let product;
+    let productId;
+    let variantId;
+
+    before(function () {
+      sinon.stub(Meteor.server.method_handlers, "cart/resetShipmentMethod", function () {
+        check(arguments, [Match.Any]); // to prevent audit_arguments from complaining
+        return true;
+      });
+
+      product = addProduct();
+      productId = product._id;
+      variantId = Products.findOne({
+        ancestors: [productId]
+      })._id;
+    });
+
+
+    beforeEach(function () {
+      Cart.remove({});
+    });
+
+    it("should add item to cart", function (done) {
+      let cart = Factory.create("cart");
+      let items = cart.items.length;
+      console.log("created Cart with ID: " + cart.userId);
+      spyOnMethod("addToCart", cart.userId);
+      Meteor.call("cart/addToCart", productId, variantId, quantity);
+      Meteor._sleepForMs(500);
+      cart = Cart.findOne(cart._id);
+      expect(cart.items.length).to.equal(items + 1);
+      expect(cart.items[cart.items.length - 1].productId).to.equal(productId);
+      done();
+    });
+  });
+});
+
