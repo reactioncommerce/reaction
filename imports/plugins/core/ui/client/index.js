84e9ac01a45ffeb5958328764fa58d3a86bfc5d3
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-06-22 14:30:34 -0400

    move core modules to plugins dirs

---
+++ imports/plugins/core/ui/client/index.js
@@ -0,0 +1,10 @@
+export * from "./helpers/helpers";
+import "./helpers/react-template-helper";
+
+import "./templates/dashboard/dashboard.html";
+import "./templates/dashboard/dashboard.js";
+import "./templates/themeDetails/themeDetails.html";
+import "./templates/themeDetails/themeDetails.js";
+import "./templates/themeEditor/themeEditor.html";
+import "./templates/themeEditor/themeEditor.js";
+import "./templates/themeEditor/themeEditor.less";
