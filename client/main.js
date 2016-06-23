15f289240339134c766c6a024a7e5d38bb181a7e
Mike Murray <hellomikemurray@gmail.com>
2016-06-22 19:32:25 -0700

    Import various UI components into client main

--- client/main.js
+++ client/main.js
@@ -10,3 +10,6 @@ import "/imports/plugins/included/stripe/client";
 import "/imports/plugins/core/ui-tagnav/client";
 import "/imports/plugins/core/ui-navbar/client";
 import "/imports/plugins/core/ui/client";
+import "/imports/plugins/core/checkout/client";
+import "/imports/plugins/core/dashboard/client";
+import "/imports/plugins/core/layout/client";
