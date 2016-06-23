53f32ac7edc2d6182fa1b4b018a2b0a7bea9d55e
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-06-22 22:20:35 -0400

    more import/exports for plugins

--- imports/plugins/included/inventory/register.js
+++ imports/plugins/included/inventory/register.js
@@ -1,33 +1,31 @@
 import { Reaction } from "/server/api";
 
-export default function () {
-  Reaction.registerPackage({
+Reaction.registerPackage({
+  label: "Inventory",
+  name: "reaction-inventory",
+  icon: "fa fa-building",
+  autoEnable: true,
+  settings: {
+    name: "Inventory"
+  },
+  registry: [{
+    provides: "dashboard",
+    template: "inventoryDashboard",
     label: "Inventory",
-    name: "reaction-inventory",
+    description: "Inventory utilities",
     icon: "fa fa-building",
-    autoEnable: true,
-    settings: {
-      name: "Inventory"
-    },
-    registry: [{
-      provides: "dashboard",
-      template: "inventoryDashboard",
+    priority: 1,
+    container: "utilities",
+    permissions: [{
       label: "Inventory",
-      description: "Inventory utilities",
-      icon: "fa fa-building",
-      priority: 1,
-      container: "utilities",
-      permissions: [{
-        label: "Inventory",
-        permission: "dashboard/inventory"
-      }]
-    }],
-    layout: [{
-      layout: "coreLayout",
-      workflow: "coreInventoryWorkflow",
-      collection: "Inventory",
-      theme: "default",
-      enabled: true
+      permission: "dashboard/inventory"
     }]
-  });
-}
+  }],
+  layout: [{
+    layout: "coreLayout",
+    workflow: "coreInventoryWorkflow",
+    collection: "Inventory",
+    theme: "default",
+    enabled: true
+  }]
+});
---
+++ imports/plugins/included/social/client/index.js
@@ -0,0 +1,14 @@
+import "./templates/apps/facebook.html";
+import "./templates/apps/facebook.js";
+import "./templates/apps/googleplus.html";
+import "./templates/apps/googleplus.js";
+import "./templates/apps/pinterest.html";
+import "./templates/apps/pinterest.js";
+import "./templates/apps/twitter.html";
+import "./templates/apps/twitter.js";
+
+import "./templates/dashboard/social.html";
+import "./templates/dashboard/social.js";
+
+import "./templates/social.html";
+import "./templates/social.js";
