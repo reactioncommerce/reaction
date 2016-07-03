aa6b3d60d9de2b2b7596afe1bec153775fde940d
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-07-03 15:40:57 -0400

    wrap Locale setup in a closure and add to init

--- server/startup/locale.js
+++ server/startup/locale.js
@@ -1,4 +1,6 @@
 import { Reaction } from "/server/api";
 
-// set server side locale
-Reaction.Locale = Meteor.call("shop/getLocale");
+export default function () {
+  // set server side locale
+  Reaction.Locale = Meteor.call("shop/getLocale");
+}
