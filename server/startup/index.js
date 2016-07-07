7111f812adea079973fe3a25d7c565538292988b
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-07-07 00:21:18 -0400

    remove local set on startup

--- server/startup/index.js
+++ server/startup/index.js
@@ -2,7 +2,6 @@ import Accounts from "./accounts";
 import i18n from "./i18n";
 import Jobs from "./jobs";
 import Load from "./load-data";
-import Locale from "./locale";
 import Packages from "./packages";
 import Registry from "./registry";
 import Init from "./init";
@@ -12,7 +11,6 @@ export default function () {
   i18n();
   Jobs();
   Load();
-  Locale();
   Packages();
   Registry();
   Init();
