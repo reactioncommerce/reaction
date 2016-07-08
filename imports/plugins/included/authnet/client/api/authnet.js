e1f722d55d90fb604b3e0b1123de38fb3c55dcc8
Brent Hoover <brent@thebuddhalodge.com>
2016-07-08 10:34:39 +0800

    Removed unused code

--- imports/plugins/included/authnet/client/api/authnet.js
+++ imports/plugins/included/authnet/client/api/authnet.js
@@ -2,27 +2,6 @@
 import { Meteor } from "meteor/meteor";
 
 export const AuthNet = {
-  // accountOptions() {
-  //   let settings = Packages.findOne({
-  //     name: "reaction-auth-net",
-  //     shopId: Reaction.getShopId(),
-  //     enabled: true
-  //   }).settings;
-  //   let ref = Meteor.settings.authnet;
-  //   let options;
-  //
-  //   options = {
-  //     login: getSettings(settings, ref, "api_id"),
-  //     tran_key: getSettings(settings, ref, "transaction_key")
-  //   };
-  //
-  //   if (!options.login) {
-  //     throw new Meteor.Error("invalid-credentials", "Invalid Authnet Credentials");
-  //   }
-  //
-  //   return options;
-  // },
-
   authorize(cardInfo, paymentInfo, callback) {
     Meteor.call("authnetSubmit", "authorizeTransaction",
       cardInfo,
@@ -30,28 +9,4 @@ export const AuthNet = {
       callback
     );
   }
-
-  // capture(paymentMethod, callback) {
-  //   Meteor.call("authnet/payment/capture",
-  //     paymentMethod,
-  //     callback
-  //   );
-  // },
-
-  // refund(cardInfo, paymentMethod, callback) {
-  //   Meteor.call("authnet/refund/create",
-  //     cardInfo,
-  //     paymentMethod,
-  //     callback
-  //   );
-  // },
-
-  // TODO
-  // refunds(paymentMethod, callback) {
-  //   Meteor.call("authnet/refund/list",
-  //     paymentMethod,
-  //     callback
-  //   );
-  // }
 };
-
