6ab0e6bf4b041211a350e7b3c23128e863eb9768
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-07-19 15:38:57 -0700

    Plugin loader (#1162)
    * convert register.js to json for plugins
    
    * build base of plugin/registry loader
    
    * add tag and navbar ui register.json
    
    * fix missing import for theme registerTheme
    
    * write imports to plugins.js on client and server
    
    * stop saving imports to database
    
    * clarify variable naming
    
    * update gitignore
    
    * automatically import any plugins that exist
    
    * add warning message to plugin imports files
    
    * switch registry files back to js
    
    * add bash version of plugin loader for Docker build
    
    * refactor development plugin loader to use JS registry files
    
    * remove manual plugin imports
    
    * only use JS plugin loader in development
    
    * setup plugin imports in CircleCI
    
    * make Docker scripts executable
    
    * less specific NODE_ENV for plugin loader
    
    * remove unnecessary plugin loader script from CI
    
    * put plugin loader script back in CI

---
+++ docker/scripts/plugin-loader.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# set the plugin import file paths
+CLIENT_PLUGINS_FILE=./client/plugins.js
+SERVER_PLUGINS_FILE=./server/plugins.js
+
+# clear any contents in the import files
+> ./client/plugins.js
+> ./server/plugins.js
+
+# define plugin subdirectories
+PLUGIN_SUB_DIRS=("core" "custom" "included")
+
+# generate plugin import files on client and server
+for plugins in "${PLUGIN_SUB_DIRS[@]}"; do
+  for dir in $(find ./imports/plugins/$plugins -mindepth 1 -maxdepth 1 -type d); do
+    if [[ -f "$dir/client/index.js" ]]; then
+      IMPORT_PATH=$(echo "$dir/client/index.js" | cut -c2-)
+      echo "import \"$IMPORT_PATH\"" >> $CLIENT_PLUGINS_FILE
+    fi
+    if [[ -f "$dir/server/index.js" ]]; then
+      IMPORT_PATH=$(echo "$dir/server/index.js" | cut -c2-)
+      echo "import \"$IMPORT_PATH\"" >> $SERVER_PLUGINS_FILE
+    fi
+    if [[ -f "$dir/register.js" ]]; then
+      IMPORT_PATH=$(echo "$dir/register.js" | cut -c2-)
+      echo "import \"$IMPORT_PATH\"" >> $SERVER_PLUGINS_FILE
+    fi
+  done
+done
