d64610ae40b85f942ad3b54745a88ff88b4a7cd2
Erik Kieckhafer <kieckhafer@users.noreply.github.com>
2016-07-27 15:50:06 -0700

    Updates to the way discount amounts are processed (#1210)
    * add check to make sure discounts are not greater than cart total
    
    Reaction was allowing discounts greater than the cart total to be
    applied, meaning orders could end up displaying an infinite amount
    refunds to be paid out. This change adds client and server side checks
    to make sure this doesn’t happen.
    
    * updated surrounding div to remove extra spacing when alert is not active
    
    There was extra spacing inside the shippingInvoice.html showing because
    we were using a class on the div that had padding attached to it. This
    change fixes that by using a different div class.
    
    * updated server-side error: added Logger, changed error message
    
    * updated error message for invalidDiscount
    
    * updated error message for invalidDiscount
    
    * added lodash import to shippingInvoice JS
    
    Order details in the shippingInvoice panel were not displaying
    correctly because of a missing _.lodash import. This change adds loads
    back in, fixing the issue.
    
    * fixed typo: applu -> apply
    
    * updating err to error
    
    * removed un-needed comment
    
    * updating terminology
    
    Replaced “cart” with “order” to better reflect what’s actually happening
    
    * jquery-ui pinned to 1.10.5
    
    - this is pinned for a reason, the structure has changed in the npm
    module in new releases. if we update (which we should) we need to
    refactor jquery-ui implementation.
    
    * added missing lodash imports

--- client/modules/accounts/templates/dashboard/dashboard.js
+++ client/modules/accounts/templates/dashboard/dashboard.js
@@ -1,5 +1,5 @@
-import { Meteor } from "meteor/meteor";
 import _ from "lodash";
+import { Meteor } from "meteor/meteor";
 import { Reaction, i18next } from "/client/api";
 import { ServiceConfigHelper } from "../../helpers/util";
 import { Template } from "meteor/templating";
