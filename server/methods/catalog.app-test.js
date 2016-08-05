78a846c90ec4fcfc16c711791fd6c126f8d13e67
Brent Hoover <brent@thebuddhalodge.com>
2016-08-06 05:38:24 +0800

    Add a wait before pulling record

--- server/methods/catalog.app-test.js
+++ server/methods/catalog.app-test.js
@@ -149,6 +149,7 @@ describe("core product methods", function () {
       expect(variants.length).to.equal(1);
 
       Meteor.call("products/createVariant", product._id, newVariant);
+      Meteor._sleepForMs(500);
       variants = Products.find({ ancestors: [product._id] }).fetch();
       const createdVariant = variants.filter(v => v._id !== firstVariantId);
       expect(variants.length).to.equal(2);
