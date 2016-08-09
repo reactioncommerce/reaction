a80fccb462eb497182d726a15f5c184ec9fdc0a4
Brent Hoover <brent@thebuddhalodge.com>
2016-08-09 13:40:57 +0800

    Add failing test for "decrease below zero"

--- server/methods/core/cart-remove.app-test.js
+++ server/methods/core/cart-remove.app-test.js
@@ -73,6 +73,25 @@ describe("cart methods", function () {
       expect(updatedCart.items[0].quantity).to.equal(originalQty - 1);
     });
 
+    it("when quantity is decresed to zero, remove cart item", function () {
+      sandbox.stub(Meteor.server.method_handlers, "cart/resetShipmentMethod", function () {
+        check(arguments, [Match.Any]);
+      });
+      sandbox.stub(Meteor.server.method_handlers, "shipping/updateShipmentQuotes", function () {
+        check(arguments, [Match.Any]);
+      });
+      let cart = Factory.create("cart");
+      const cartUserId = cart.userId;
+      sandbox.stub(Reaction, "getShopId", () => shop._id);
+      sandbox.stub(Meteor, "userId", () => cartUserId);
+      let cartFromCollection = Collections.Cart.findOne(cart._id);
+      const cartItemId = cartFromCollection.items[0]._id;
+      const originalQty = cartFromCollection.items[0].quantity;
+      Meteor.call("cart/removeFromCart", cartItemId, originalQty);
+      let updatedCart = Collections.Cart.findOne(cart._id);
+      expect(updatedCart.items.length).to.equal(1);
+    });
+
     it("should throw an exception when attempting to remove item from cart of another user", function (done) {
       const cart = Factory.create("cart");
       const cartItemId = "testId123";
