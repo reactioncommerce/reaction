b20c9bb4cb821ee0ad8ed22824c64ac04bda8f56
Josh Ewing <josh.tewing@gmail.com>
2016-08-12 16:14:46 -0700

    More progress on functional test setup

--- tests/acceptance-tests/test-runner.js
+++
@@ -1,8 +0,0 @@
-// let webdriverio = require("webdriverio");
-
-const options = {
-  desiredCapabilities: {
-    // pull browser type from settings.yml
-    browserName: "firefox"
-  }
-};
--- tests/acceptance-tests/test/specs/simple-login.app-test.js
+++ tests/acceptance-tests/test/specs/simple-login.app-test.js
@@ -1,23 +1,27 @@
 "use strict";
 let yaml = require("js-yaml");
 let fs   = require("fs");
-import snooze from "../../lib/snooze";
-//let assert = require("assert");
-// let expect = require("chai").expect;
+let assert = require("assert");
+let expect = require("chai").expect;
 
+beforeEach(function () {
+  let browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
+  const baseUrl = browserConfig["base_url"].toString();
+  browser.url(baseUrl);
+});
 
 describe("simple login test", function () {
   it("verify user is able to login", function () {
     let eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
     let usrData = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/user-data.yml", "utf8"));
-    browser.url("http://localhost:3000");
-      // var elem = browser.element('span.product-image');
-    snooze.snooze(5000, function () {
-      browser.click(eleMap["login_dropdown_btn"]);
-      browser.setValue(eleMap["login_email_fld"], usrData["admin_email"]);
-      browser.setValue(eleMap["login_pw_fld"], usrData["admin_pw"]);
-      browser.click(eleMap["login_btn"]);
-      browser.pause("5000");
+    browser.pause("5000");
+    browser.click(eleMap["login_dropdown_btn"]);
+    browser.setValue(eleMap["login_email_fld"], usrData["admin_email"]);
+    browser.setValue(eleMap["login_pw_fld"], usrData["admin_pw"]);
+    browser.click(eleMap["login_btn"]);
+    browser.pause("5000");
+    browser.getText("#logged-in-display-name").then(function (text) {
+      expect(assert(text === "Admin")).to.be.true;
     });
   });
 });
