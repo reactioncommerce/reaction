cefb0128428e12916813549c11bfcaf0edd5ffa4
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-08-12 00:41:43 -0700

    move braintree methods into new file

---
+++ imports/plugins/included/braintree/server/methods/braintreeMethods.js
@@ -0,0 +1,106 @@
+import * as BraintreeApi from "./braintreeApi";
+import { Logger } from "/server/api";
+import { PaymentMethod } from "/lib/collections/schemas";
+
+
+/**
+ * paymentCapture
+ * Capture payments from Braintree
+ * https://developers.braintreepayments.com/reference/request/transaction/submit-for-settlement/node
+ * @param {Object} paymentMethod - Object containing everything about the transaction to be settled
+ * @return {Object} results - Object containing the results of the transaction
+ */
+export function paymentCapture(paymentMethod) {
+  check(paymentMethod, PaymentMethod);
+
+  const paymentCaptureDetails = {
+    transactionId: paymentMethod.transactionId,
+    amount: paymentMethod.amount
+  };
+
+  let result;
+
+  try {
+    let refundResult = BraintreeApi.captureCharge(paymentCaptureDetails);
+    Logger.info(refundResult);
+    result = refundResult;
+  } catch (error) {
+    Logger.error(error);
+    result = {
+      saved: false,
+      error: `Cannot Capture Payment: ${error.message}`
+    };
+    Logger.fatal("Braintree call failed, refund was not issued");
+  }
+
+  return result;
+}
+
+
+/**
+ * createRefund
+ * Refund BrainTree payment
+ * https://developers.braintreepayments.com/reference/request/transaction/refund/node
+ * @param {Object} paymentMethod - Object containing everything about the transaction to be settled
+ * @param {Number} amount - Amount to be refunded if not the entire amount
+ * @return {Object} results - Object containing the results of the transaction
+ */
+export function createRefund(paymentMethod, amount) {
+  check(paymentMethod, PaymentMethod);
+  check(amount, Number);
+
+  const refundDetails = {
+    transactionId: paymentMethod.transactionId,
+    amount: amount
+  };
+
+  let result;
+
+  try {
+    let refundResult = BraintreeApi.createRefund(refundDetails);
+    Logger.info(refundResult);
+    result = refundResult;
+  } catch (error) {
+    Logger.error(error);
+    result = {
+      saved: false,
+      error: `Cannot issue refund: ${error.message}`
+    };
+    Logger.fatal("Braintree call failed, refund was not issued");
+  }
+
+  return result;
+}
+
+
+/**
+ * listRefunds
+ * List all refunds for a transaction
+ * https://developers.braintreepayments.com/reference/request/transaction/find/node
+ * @param {Object} paymentMethod - Object containing everything about the transaction to be settled
+ * @return {Array} results - An array of refund objects for display in admin
+ */
+export function listRefunds(paymentMethod) {
+  check(paymentMethod, Object);
+
+  const refundListDetails = {
+    transactionId: paymentMethod.transactionId
+  };
+
+  let result;
+
+  try {
+    let refundListResult = BraintreeApi.listRefunds(refundListDetails);
+    Logger.info(refundListResult);
+    result = refundListResult;
+  } catch (error) {
+    Logger.error(error);
+    result = {
+      saved: false,
+      error: `Cannot issue refund: ${error.message}`
+    };
+    Logger.fatal("Braintree call failed, refund was not issued");
+  }
+
+  return result;
+}
