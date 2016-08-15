a185191e9692c3a9932b665d4eaec36852d3bb21
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-08-15 01:04:17 -0700

    wrap PayPal PayFlow in wrapper for easier testing

--- imports/plugins/included/paypal/server/methods/payflow-new.js
+++ imports/plugins/included/paypal/server/methods/payflow-new.js
@@ -2,9 +2,8 @@ import * as PayflowproMethods from "./payflowproMethods";
 import { Meteor } from "meteor/meteor";
 
 Meteor.methods({
-  "payflowpro/payment/submit": PayflowproMethods.paymentSubmit,
+  "payflowproSubmit": PayflowproMethods.paymentSubmit,
   "payflowpro/payment/capture": PayflowproMethods.paymentCapture,
   "payflowpro/refund/create": PayflowproMethods.createRefund,
-  "payflowpro/refund/list": PayflowproMethods.listRefunds,
-  "payflowpro/settings": PayflowproMethods.getSettings
+  "payflowpro/refund/list": PayflowproMethods.listRefunds
 });
