adecaaeb60e32d94a794da0ec567aecac58cfab1
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-08-15 10:33:06 -0700

    removed temporary file

--- imports/plugins/included/paypal/server/methods/payflow-new.js
+++
@@ -1,9 +0,0 @@
-import * as PayflowproMethods from "./payflowproMethods";
-import { Meteor } from "meteor/meteor";
-
-Meteor.methods({
-  "payflowproSubmit": PayflowproMethods.paymentSubmit,
-  "payflowpro/payment/capture": PayflowproMethods.paymentCapture,
-  "payflowpro/refund/create": PayflowproMethods.createRefund,
-  "payflowpro/refund/list": PayflowproMethods.listRefunds
-});
