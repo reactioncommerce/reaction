c78a35b99c2311d09de0657ddcbfca3fd278c0a4
Brent Hoover <brent@thebuddhalodge.com>
2016-08-17 11:20:27 +0800

    Trying to get these tests to run reliably by trying to normalize the inventory beforehand

--- imports/plugins/included/inventory/server/hooks/inventory-hooks.app-test.js
+++ imports/plugins/included/inventory/server/hooks/inventory-hooks.app-test.js
@@ -1,13 +1,30 @@
 /* eslint dot-notation: 0 */
 import { Meteor } from "meteor/meteor";
-import { Inventory, Orders }  from "/lib/collections";
-import { Reaction } from "/server/api";
+import { Inventory, Orders, Products }  from "/lib/collections";
+import { Reaction, Logger } from "/server/api";
 import { expect } from "meteor/practicalmeteor:chai";
 import { sinon } from "meteor/practicalmeteor:sinon";
 import Fixtures from "/server/imports/fixtures";
 import { getShop } from "/server/imports/fixtures/shops";
+import { addProduct } from "/server/imports/fixtures/products";
+import { createCart } from "/server/imports/fixtures/cart";
+import { registerInventory } from "../methods/inventory";
 
 Fixtures();
+
+function resetInventory() {
+  Inventory.remove({});
+  const inventoryCount = Inventory.find().count();
+  Logger.warn(`Found ${inventoryCount} inventory records`);
+  const products = Products.find().fetch();
+  Logger.warn(`There are ${products.length} products now`);
+  for (let product of products) {
+    Logger.warn(`Registering inventory for ${product.title}`);
+    registerInventory(product);
+  }
+  const updatedInventoryCount = Inventory.find().count();
+  Logger.warn(`Found ${updatedInventoryCount} inventory records after register`);
+}
 
 describe("Inventory Hooks", function () {
   let originals;
@@ -21,6 +38,7 @@ describe("Inventory Hooks", function () {
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
+    resetInventory();
   });
 
   afterEach(function () {
@@ -36,6 +54,7 @@ describe("Inventory Hooks", function () {
   }
 
   it("should move allocated inventory to 'sold' when an order is created", function () {
+    this.timeout(50000);
     sandbox.stub(Meteor.server.method_handlers, "orders/sendNotification", function () {
       check(arguments, [Match.Any]);
       return true;
@@ -53,7 +72,7 @@ describe("Inventory Hooks", function () {
       shopId: shop._id
     });
     expect(inventoryItem).to.not.be.undefined;
-    // because the cart fixture does not trigger hooks we need to allocate inventory manuall
+    // because the cart fixture does not trigger hooks we need to allocate inventory manually
     Inventory.update(inventoryItem._id,
       {
         $set: {
@@ -73,20 +92,22 @@ describe("Inventory Hooks", function () {
   });
 
   it("should move allocated inventory to 'shipped' when an order is shipped", function (done) {
+    this.timeout(50000);
     sandbox.stub(Meteor.server.method_handlers, "orders/sendNotification", function () {
       check(arguments, [Match.Any]);
       return true;
     });
     sandbox.stub(Reaction, "hasPermission", () => true);
-    const cart = Factory.create("cartOne");
+    const product = Products.findOne();
+    const cart = createCart(product);
     sandbox.stub(Reaction, "getShopId", function () {
       return cart.shopId;
     });
     let shop = getShop();
-    let product = cart.items[0];
+    let cartProduct = cart.items[0];
     const inventoryItem = Inventory.findOne({
-      productId: product.productId,
-      variantId: product.variants._id,
+      productId: cartProduct.productId,
+      variantId: cartProduct.variants._id,
       shopId: shop._id
     });
     expect(inventoryItem).to.not.be.undefined;
@@ -95,7 +116,7 @@ describe("Inventory Hooks", function () {
       {
         $set: {
           "workflow.status": "reserved",
-          "orderItemId": product._id
+          "orderItemId": cartProduct._id
         }
       });
     spyOnMethod("copyCartToOrder", cart.userId);
