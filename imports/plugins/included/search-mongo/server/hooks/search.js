2864be1a21d73a3af60347289024f4a3c7758a7c
Brent Hoover <brent@thebuddhalodge.com>
2016-08-25 15:38:18 +0800

    Add in hooks to keep PS collection updated

---
+++ imports/plugins/included/search-mongo/server/hooks/search.js
@@ -0,0 +1,38 @@
+import _ from "lodash";
+import { Products } from "/lib/collections";
+import { ProductSearch, getProductSearchParameters, buildProuctSearchCollectionRecord } from "../collections/searchcollections";
+import { Logger } from "/server/api";
+
+
+/**
+ * if product is removed, remove product search record
+ */
+Products.after.remove((userId, doc) => {
+  const productId = doc._id;
+  ProductSearch.remove(productId);
+});
+
+//
+// after product update rebuild product search record
+//
+Products.after.update((userId, doc, fieldNames) => {
+  const productId = doc._id;
+  const { fieldSet } = getProductSearchParameters();
+  const modifiedFields = _.intersection(fieldSet, fieldNames);
+  if (modifiedFields.length) {
+    Logger.info(`Rewriting search record for ${productId}`);
+    ProductSearch.remove(productId);
+    buildProuctSearchCollectionRecord(productId);
+  } else {
+    Logger.info("No watched fields modified, skipping");
+  }
+});
+
+/**
+ * after insert
+ * @summary should fires on create new variants, on clones products/variants
+ */
+Products.after.insert((userId, doc) => {
+  const productId = doc._id;
+  buildProuctSearchCollectionRecord(productId);
+});
