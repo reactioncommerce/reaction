7365ac8077e5b8f0452e7844219c9f0bb46fe54a
Brent Hoover <brent@thebuddhalodge.com>
2016-08-25 15:41:22 +0800

    Build PS collection dynamically based on configuration

--- imports/plugins/included/search-mongo/server/collections/searchcollections.js
+++ imports/plugins/included/search-mongo/server/collections/searchcollections.js
@@ -1,5 +1,76 @@
+import _ from "lodash";
 import { Mongo } from "meteor/mongo";
+import { Packages, Products } from "/lib/collections";
+import { Reaction, Logger } from "/server/api";
 
 export const ProductSearch = new Mongo.Collection("ProductSearch");
 export const OrderSearch = new Mongo.Collection("OrderSearch");
 export const AccountSearch = new Mongo.Collection("AccountSearch");
+
+let requiredFields = ["_id", "hashtags"];
+
+function getPackageSettings() {
+  const searchPackage = Packages.findOne({
+    shopId: Reaction.getShopId(),
+    name: "reaction-search"
+  });
+  return searchPackage.settings;
+}
+
+function filterFields(customFields) {
+  const fieldNames = [];
+  const fieldKeys = _.keys(customFields);
+  for (const fieldKey of fieldKeys) {
+    if (customFields[fieldKey]) {
+      fieldNames.push(fieldKey);
+    }
+  }
+  return fieldNames;
+}
+
+// get the weights for all enabled fields
+function getScores(customFields, settings) {
+  const weightObject = {};
+  for (const weight of _.keys(settings.weights)) {
+    if (_.includes(customFields, weight)) {
+      weightObject[weight] = settings.weights[weight];
+    }
+  }
+  return weightObject;
+}
+
+export function getProductSearchParameters() {
+  const settings = getPackageSettings();
+  const customFields = filterFields(settings.includes);
+  const fieldSet = requiredFields.concat(customFields);
+  const weightObject = getScores(customFields, settings);
+  return { fieldSet: fieldSet, weightObject: weightObject };
+}
+
+export function buildProuctSearchCollectionRecord(productId) {
+  const product = Products.findOne(productId);
+  const { fieldSet } = getProductSearchParameters();
+  const productRecord = {};
+  for (const field of fieldSet) {
+    productRecord[field] = product[field];
+  }
+  const productSearchRecord = ProductSearch.insert(productRecord);
+  return productSearchRecord;
+}
+
+export function buildProductSearchCollection() {
+  Logger.info("(re)Building ProductSearch Collection");
+  ProductSearch.remove({});
+  const { fieldSet, weightObject } = getProductSearchParameters();
+  const products = Products.find().fetch();
+  for (const product of products) {
+    const productRecord = {};
+    for (const field of fieldSet) {
+      productRecord[field] = product[field];
+    }
+    ProductSearch.insert(productRecord);
+  }
+  const rawProductSearchCollection = ProductSearch.rawCollection();
+  rawProductSearchCollection.dropIndexes("*");
+  rawProductSearchCollection.createIndex({"$**": "text"}, weightObject);
+}
