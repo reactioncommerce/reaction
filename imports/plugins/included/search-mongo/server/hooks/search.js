e0043b1f64516cb40752af274dd2d8b4e770e18a
Brent Hoover <brent@thebuddhalodge.com>
2016-08-30 06:45:44 +0800

    Don't run search hooks when ProductSearch collection has not been defined

--- imports/plugins/included/search-mongo/server/hooks/search.js
+++ imports/plugins/included/search-mongo/server/hooks/search.js
@@ -10,24 +10,28 @@ import { Logger } from "/server/api";
  * if product is removed, remove product search record
  */
 Products.after.remove((userId, doc) => {
-  const productId = doc._id;
-  ProductSearch.remove(productId);
-  Logger.info(`Removed product ${productId} from ProductSearch collection`);
+  if (ProductSearch) {
+    const productId = doc._id;
+    ProductSearch.remove(productId);
+    Logger.info(`Removed product ${productId} from ProductSearch collection`);
+  }
 });
 
 //
 // after product update rebuild product search record
 //
 Products.after.update((userId, doc, fieldNames) => {
-  const productId = doc._id;
-  const { fieldSet } = getProductSearchParameters();
-  const modifiedFields = _.intersection(fieldSet, fieldNames);
-  if (modifiedFields.length) {
-    Logger.info(`Rewriting search record for ${doc.title}`);
-    ProductSearch.remove(productId);
-    buildProductSearchCollectionRecord(productId);
-  } else {
-    Logger.info("No watched fields modified, skipping");
+  if (ProductSearch) {
+    const productId = doc._id;
+    const { fieldSet } = getProductSearchParameters();
+    const modifiedFields = _.intersection(fieldSet, fieldNames);
+    if (modifiedFields.length) {
+      Logger.info(`Rewriting search record for ${doc.title}`);
+      ProductSearch.remove(productId);
+      buildProductSearchCollectionRecord(productId);
+    } else {
+      Logger.info("No watched fields modified, skipping");
+    }
   }
 });
 
@@ -36,6 +40,8 @@ Products.after.update((userId, doc, fieldNames) => {
  * @summary should fires on create new variants, on clones products/variants
  */
 Products.after.insert((userId, doc) => {
-  const productId = doc._id;
-  buildProductSearchCollectionRecord(productId);
+  if (ProductSearch) {
+    const productId = doc._id;
+    buildProductSearchCollectionRecord(productId);
+  }
 });
