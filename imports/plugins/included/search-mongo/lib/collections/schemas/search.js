c1344d37f591efc0571872e1e265eb78336a2543
Brent Hoover <brent@thebuddhalodge.com>
2016-08-30 10:59:29 +0800

    Namespace product settings

--- imports/plugins/included/search-mongo/lib/collections/schemas/search.js
+++ imports/plugins/included/search-mongo/lib/collections/schemas/search.js
@@ -3,60 +3,60 @@ import { PackageConfig } from "/lib/collections/schemas/registry";
 
 export const SearchPackageConfig = new SimpleSchema([
   PackageConfig, {
-    "settings.includes.title": {
+    "settings.products.includes.title": {
       type: Boolean,
       label: "Include product title in search",
       defaultValue: true
     },
-    "settings.weights.title": {
+    "settings.products.weights.title": {
       type: Number,
       label: "Weight for product title matches",
       defaultValue: 10,
       max: 10,
       min: 1
     },
-    "settings.includes.description": {
+    "settings.products.includes.description": {
       type: Boolean,
       label: "Include product description in search",
       defaultValue: true
     },
-    "settings.weights.description": {
+    "settings.products.weights.description": {
       type: Number,
       label: "Weight for product description matches",
       defaultValue: 6,
       max: 10,
       min: 1
     },
-    "settings.includes.pageTitle": {
+    "settings.products.includes.pageTitle": {
       type: Boolean,
       label: "Include page title in search",
       defaultValue: false
     },
-    "settings.weights.pageTitle": {
+    "settings.products.weights.pageTitle": {
       type: Number,
       label: "Weight for page title matches",
       defaultValue: 2,
       max: 10,
       min: 1
     },
-    "settings.includes.metafields": {
+    "settings.products.includes.metafields": {
       type: Boolean,
       label: "Include metafields in search",
       defaultValue: true
     },
-    "settings.weights.metafields": {
+    "settings.products.weights.metafields": {
       type: Number,
       label: "Weight for metafields matches",
       defaultValue: 6,
       max: 10,
       min: 1
     },
-    "settings.includes.vendor": {
+    "settings.products.includes.vendor": {
       type: Boolean,
       label: "Include vendor in search",
       defaultValue: true
     },
-    "settings.weights.vendor": {
+    "settings.products.weights.vendor": {
       type: Number,
       label: "Weight for vendor matches",
       defaultValue: 6,
