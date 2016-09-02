240746d4cae1967fe5e8fca2894996ea367d8d79
Mike Murray <hellomikemurray@gmail.com>
2016-09-01 21:28:57 -0700

    Schema updates and render render functions
    Added handlebars render functions, as well as the ability to register a
    custom template renderer.
    Added caching for handlebars templates.
    Updated templates schema with more properties.

--- server/api/core/templates.js
+++ server/api/core/templates.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import Handlebars from "handlebars";
+import { Reaction } from "./core";
 
+
+export const TemplateCache = {};
+export const TemplateParsers = {};
 
 export function processTemplateInfo(templateInfo) {
   const templateData = {
@@ -19,6 +24,45 @@ export function processTemplateInfo(templateInfo) {
   return templateData;
 }
 
+
+export function registerTemplateParser(name, renderFunction) {
+  TemplateParsers[name] = renderFunction;
+}
+
+export function renderTemplate(templateInfo, data) {
+  if (templateInfo.parser === "react") {
+    return null;
+  } else if (templateInfo.parser === "handlebars") {
+    return renderHandlebarsTemplate(templateInfo, data);
+  }
+
+  if (typeof TemplateParsers[name] === "function") {
+    return TemplateParsers[name](templateInfo, data);
+  }
+
+  return false;
+}
+
+/**
+ * Compile and cache Handlebars template
+ * @param {String} name Name of template to register amd save to cache
+ * @param {String} template markup
+ * @return {Function} Compiled handlebars template.
+ */
+export function compileHandlebarsTemplate(name, template) {
+  const compiledTemplate = Handlebars.compile(template);
+  TemplateCache[name] = compiledTemplate;
+  return compiledTemplate;
+}
+
+export function renderHandlebarsTemplate(templateInfo, data) {
+  if (TemplateCache[templateInfo.name] === undefined) {
+    compileHandlebarsTemplate(templateInfo.name, templateInfo.template);
+  }
+
+  const compiledTemplate = Handlebars.compile(TemplateCache[templateInfo.name]);
+  return compiledTemplate(data);
+}
 
 export function renderTemplateToStaticMarkup(template, props) {
   return ReactDOMServer.renderToStaticMarkup(
