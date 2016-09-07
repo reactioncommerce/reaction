1e9cf953e2abf61c7a69158baff2e8b1de84e9be
Capt-Slow <josh.tewing@gmail.com>
2016-09-07 13:58:33 -0700

    Josh test speed increase #1347 (#1355)
    * turning verbosity off when running tests by default
    
    * Removing sleeps/pauses when I can
    
    * flaky test fix
    
    * #1347 cut runtime down by nearly 50%
    
    This is still just running synchronously. As tests grows we will need
    to look into running tests in parallel, however this might require some
    infrastructure in place, i.e. a hosted QA environment. An environment
    that can handle the load of 5-10 tests hitting it at a time.

--- tests/acceptance-tests/lib/admin-order-actions.js
+++ tests/acceptance-tests/lib/admin-order-actions.js
@@ -6,12 +6,14 @@ const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/
 
 module.exports = {
   refundAmount: function () {
+    browser.waitForVisible(eleMap.captured_total, 2000);
     const captTotal = browser.getText(eleMap.captured_total);
-    browser.pause("10000");
+    browser.waitForEnabled(eleMap.refund_amnt_txt_box, 20000);
     browser.setValue(eleMap.refund_amnt_txt_box, captTotal);
     browser.pause("7000");
   },
   voidAmount: function () {
+    browser.waitForVisible(eleMap.order_total_w_tax, 20000);
     const captTotalWTax = browser.getText(eleMap.order_total_w_tax);
     // const orderTotal = browser.getText(eleMap.order_total);
     browser.pause("2000");
--- tests/acceptance-tests/test/specs/braintree-refund.app-test.js
+++ tests/acceptance-tests/test/specs/braintree-refund.app-test.js
@@ -21,35 +21,33 @@ describe("braintree refund test", function () {
     browser.pause("5000");
     userDo.UserActions.refreshShop();
     browser.click(eleMap.product);
-    browser.pause("5000");
+    browser.waitForEnabled(eleMap.red_option, 5000);
     browser.click(eleMap.red_option);
-    browser.pause("2000");
+    browser.waitForEnabled(eleMap.add_to_cart_logged_in, 5000);
     browser.click(eleMap.add_to_cart_logged_in);
-    browser.pause("2000");
+    browser.waitForEnabled(eleMap.checkout_btn, 5000);
     browser.click(eleMap.checkout_btn);
-    browser.pause("5000");
     shopUser.checkForAddress();
     // free shipping option
     browser.click(eleMap.free_shipping);
-    browser.pause("3000");
+    browser.waitForEnabled(eleMap.braintree, 5000);
     browser.click(eleMap.braintree);
-    browser.pause("6000");
     shopUser.braintreePaymentInfo();
-    browser.pause("5000");
+    browser.waitForEnabled(eleMap.braintree_complete_order_btn, 5000);
     browser.click(eleMap.braintree_complete_order_btn);
-    browser.pause("6000");
+    browser.pause(5000);
     browser.click(eleMap.orders_page_btn);
-    browser.pause("5000");
+    browser.pause("15000");
     browser.click(eleMap.first_order_new_btn);
-    browser.pause("10000");
+    browser.waitForEnabled(eleMap.approve_btn, 10000);
     browser.click(eleMap.approve_btn);
-    browser.pause("2000");
+    browser.waitForEnabled(eleMap.capture_payment_btn, 5000);
     browser.click(eleMap.capture_payment_btn);
     adminUser.refundAmount();
     browser.click(eleMap.apply_refund_btn);
-    browser.pause("5000");
+    browser.pause(5000);
     browser.click(eleMap.apply_refund_pop_up_btn);
-    browser.pause("2000");
+    browser.pause(5000);
     expect(browser.getText("h2"))
     .to.equal("Braintree does not allow refunds until transactions are settled. This can take up to 24 hours. Please try again later.");
     // error pop up ok btn
