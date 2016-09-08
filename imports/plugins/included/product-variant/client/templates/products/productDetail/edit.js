f86e2a506c8e97c954da801129de2bf502677935
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-09-08 15:35:55 +0100

    use editors in productDetail edit mode

--- imports/plugins/included/product-variant/client/templates/products/productDetail/edit.js
+++ imports/plugins/included/product-variant/client/templates/products/productDetail/edit.js
@@ -1,78 +1,119 @@
-import autosize from "autosize";
 import { Reaction, i18next, Logger } from "/client/api";
 import { ReactionProduct } from "/lib/api";
 import { Meteor } from "meteor/meteor";
 import { Session } from "meteor/session";
 import { Template } from "meteor/templating";
+import { Product } from "/lib/collections/schemas";
+import { TextArea, TextBox, Select } from "./editors";
+
+function i18nPlaceholder(field) {
+  const i18nKey = `productDetailEdit.${field}`;
+  if (i18next.t(i18nKey) === i18nKey) {
+    Logger.info(`returning empty placeholder productDetailEdit: ${i18nKey} no i18n key found.`);
+  } else {
+    return i18next.t(i18nKey);
+  }
+
+  return "";
+}
+
+function processChange({ name, value }) {
+  const productId = ReactionProduct.selectedProductId();
+
+  Meteor.call("products/updateProductField", productId, name, value,
+    (error, result) => {
+      if (error) {
+        return Alerts.toast(error.reason, "error", {
+          i18nKey: "productDetail.errorMsg"
+        });
+      }
+
+      if (result) {
+        // redirect to new url on title change
+        if (name === "title") {
+          Meteor.call("products/setHandle", productId,
+            (err, res) => {
+              if (err) {
+                Alerts.toast(err.reason, "error", {
+                  i18nKey: "productDetail.errorMsg"
+                });
+              }
+              if (res) {
+                Reaction.Router.go("product", {
+                  handle: res
+                });
+              }
+            }
+          );
+        }
+      }});
+  return Session.set("editing-" + name, false);
+}
+
+function setupEditor({ field, value, templateInstance, component, ...additionalProps }) {
+  return {
+    className: `form-control ${field}-edit-input`,
+    name: field,
+    onChange: processChange.bind(templateInstance),
+    placeholder: i18nPlaceholder(field),
+    value,
+    component,
+    ...additionalProps
+  };
+}
+
+Template.productDetailEdit.onCreated(() => {
+  const t = Template.instance();
+
+  if (t.data.type) {
+    // respect explicit field type
+    return;
+  }
+
+  const fieldDefinition = Product.getDefinition(t.data.field);
+
+  if (fieldDefinition && fieldDefinition.allowedValues) {
+    t.type = "select";
+    t.allowedValues = fieldDefinition.allowedValues;
+  }
+});
 
 /**
  * productDetailEdit helpers
  */
 
 Template.productDetailEdit.helpers({
-  i18nPlaceholder: function () {
-    const i18nKey = `productDetailEdit.${this.field}`;
-    if (i18next.t(i18nKey) === i18nKey) {
-      Logger.info(`returning empty placeholder productDetailEdit: ${i18nKey} no i18n key found.`);
-    } else {
-      return i18next.t(i18nKey);
-    }
-  }
-});
+  TextArea() {
+    return setupEditor({
+      field: this.field,
+      value: this.value,
+      templateInstance: Template.instance(),
+      component: TextArea
+    });
+  },
 
-/**
- * productDetailEdit events
- */
+  TextBox() {
+    return setupEditor({
+      field: this.field,
+      value: this.value,
+      templateInstance: Template.instance(),
+      component: TextBox
+    });
+  },
 
-Template.productDetailEdit.events({
-  "change input,textarea": function (event) {
-    const self = this;
-    const productId = ReactionProduct.selectedProductId();
-    Meteor.call("products/updateProductField", productId, self.field,
-      $(event.currentTarget).val(),
-      (error, result) => {
-        if (error) {
-          return Alerts.inline(error.reason, "error", {
-            placement: "productManagement",
-            i18nKey: "productDetail.errorMsg",
-            id: self._id
-          });
-        }
-        if (result) {
-          // redirect to new url on title change
-          if (self.field === "title") {
-            Meteor.call("products/setHandle", productId,
-              (err, res) => {
-                if (err) {
-                  Alerts.inline(err.reason, "error", {
-                    placement: "productManagement",
-                    i18nKey: "productDetail.errorMsg",
-                    id: self._id
-                  });
-                }
-                if (res) {
-                  Reaction.Router.go("product", {
-                    handle: res
-                  });
-                }
-              }
-            );
-          }
-          // animate updated field
-          // TODO this needs to be moved into a component
-          return $(event.currentTarget).animate({
-            backgroundColor: "#e2f2e2"
-          }).animate({
-            backgroundColor: "#fff"
-          });
-        }
-      });
+  Select() {
+    return setupEditor({
+      field: this.field,
+      value: this.value,
+      templateInstance: Template.instance(),
+      component: Select,
+      options: Template.instance().allowedValues
+    });
+  },
 
-    if (this.type === "textarea") {
-      autosize($(event.currentTarget));
-    }
-
-    return Session.set("editing-" + this.field, false);
+  type() {
+    const t = Template.instance();
+    return t.type || t.data.type;
   }
 });
 
@@ -89,12 +130,4 @@ Template.productDetailField.events({
       return $(`.${this.field}-edit-input`).focus();
     }
   }
-});
-
-/**
- * productDetailEdit onRendered
- */
-
-Template.productDetailEdit.onRendered(function () {
-  return autosize($("textarea"));
 });
