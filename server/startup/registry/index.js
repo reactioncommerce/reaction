f978b320e1b4840598dbfcc7df1ce0c31023219f
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-09-11 11:27:53 -0400

    remove old email api

--- server/startup/registry/index.js
+++ server/startup/registry/index.js
@@ -1,7 +1,6 @@
 import Accounts from "./accounts";
 import Catalog from "./catalog";
 import Core from "./core";
-import Email from "./email";
 import i18n from "./i18n";
 import Router from "./router";
 
@@ -9,7 +8,6 @@ export default function () {
   Accounts();
   Catalog();
   Core();
-  Email();
   i18n();
   Router();
 }
