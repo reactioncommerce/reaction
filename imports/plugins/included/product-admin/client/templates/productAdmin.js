bd63e234e9fc497d2045479beb47501213c5915b
Mike Murray <hellomikemurray@gmail.com>
2016-09-12 12:51:26 -0700

    Fixed exception caused by undefined data

--- imports/plugins/included/product-admin/client/templates/productAdmin.js
+++ imports/plugins/included/product-admin/client/templates/productAdmin.js
@@ -3,8 +3,13 @@ import { ProductAdminContainer } from "../containers";
 Template.ProductAdmin.helpers({
   component() {
     const currentData = Template.currentData();
+    let data;
 
-    return Object.assign({}, currentData.data, {
+    if (currentData && currentData.data) {
+      data = currentData.data;
+    }
+
+    return Object.assign({}, data, {
       component: ProductAdminContainer
     });
   }
