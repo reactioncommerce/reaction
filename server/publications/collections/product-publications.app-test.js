38be17794e2f70a0184d78eb7690c6268ac57f10
Mike Murray <hellomikemurray@gmail.com>
2016-09-16 16:18:00 -0700

    clean up product publication tests

--- server/publications/collections/product-publications.app-test.js
+++ server/publications/collections/product-publications.app-test.js
@@ -1,5 +1,4 @@
 /* eslint dot-notation: 0 */
-import { Meteor } from "meteor/meteor";
 import { expect } from "meteor/practicalmeteor:chai";
 import { sinon } from "meteor/practicalmeteor:sinon";
 import { Roles } from "meteor/alanning:roles";
@@ -14,8 +13,6 @@ Fixtures();
 describe("Publication", function () {
   const shop = getShop();
   let sandbox;
-  let productRemoveStub;
-  let productInsertStub;
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
@@ -23,28 +20,6 @@ describe("Publication", function () {
 
   afterEach(function () {
     sandbox.restore();
-  });
-
-  before(function () {
-    // We are mocking inventory hooks, because we don't need them here, but
-    // if you want to do a real stress test, you could try to comment out
-    // this spyOn lines. This is needed only for ./reaction test. In one
-    // package test this is ignoring.
-    if (Array.isArray(Collections.Products._hookAspects.remove.after) && Collections.Products._hookAspects.remove.after.length) {
-      productRemoveStub = sinon.stub(Collections.Products._hookAspects.remove.after[0], "aspect");
-      productInsertStub = sinon.stub(Collections.Products._hookAspects.insert.after[0], "aspect");
-    }
-    Collections.Products.direct.remove({});
-    // really strange to see this, but without this `remove` finishes in
-    // async way (somewhere in a middle of testing process)
-    Meteor.setTimeout(function () {
-      Collections.Orders.remove({});
-    }, 500);
-  });
-
-  after(function () {
-    productRemoveStub.restore();
-    productInsertStub.restore();
   });
 
   describe("with products", function () {
@@ -61,6 +36,8 @@ describe("Publication", function () {
     };
 
     before(function () {
+      Collections.Products.direct.remove({});
+
       // a product with price range A, and not visible
       Collections.Products.insert({
         ancestors: [],
@@ -217,10 +194,6 @@ describe("Publication", function () {
 
           done();
         });
-        // const productsPub = Meteor.server.publish_handlers["Products"];
-        // const cursor = productsPub(productScrollLimit, filters);
-        // // verify
-        // expect(cursor.fetch().length).to.equal(2);
       });
 
       it("should return products in price.min - price.max range query", function (done) {
