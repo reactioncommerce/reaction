ca4465205ef7acbef1dd8544c6a7c406b7ba62b8
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-09-20 14:07:34 -0700

    importing new templates

--- imports/plugins/included/ui-search/client/index.js
+++ imports/plugins/included/ui-search/client/index.js
@@ -1,6 +1,10 @@
 // Search Modal
 import "./templates/searchModal/searchModal.html";
 import "./templates/searchModal/searchModal.js";
+import "./templates/searchModal/searchInput.html";
+import "./templates/searchModal/searchTypeToggle.html";
+import "./templates/searchModal/productSearchTags.html";
+import "./templates/searchModal/searchResults.html";
 
 // Product Search
 import "./templates/productSearch/searchGrid/searchGrid.html";
