7c3eb488dc9219887bed60de0a8948ea64b86893
Josh Ewing <josh.tewing@gmail.com>
2016-09-23 12:20:21 -0700

    This allows the user to use dynamic ids in tests

--- tests/acceptance-tests/test/specs/smoke-tests/simple-login.app-test.js
+++ tests/acceptance-tests/test/specs/smoke-tests/simple-login.app-test.js
@@ -2,21 +2,27 @@
 const yaml = require("js-yaml");
 const fs   = require("fs");
 const expect = require("chai").expect;
+const getId = require("../../../lib/get-elements.js");
 
 beforeEach(function () {
   const browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
   const baseUrl = browserConfig.base_url.toString();
   browser.url(baseUrl);
+  browser.getSession().then(function (sessionid) {
+    browser.sessionID = sessionid.id_;
+  });
 });
 
 describe("simple login test", function () {
   it("verify user is able to login - and verifies user name in dropdown", function () {
     const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
+    const eleIds = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-ids.yml", "utf8"));
     const usrData = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/user-data.yml", "utf8"));
     browser.pause("5000");
-    browser.click(eleMap.login_dropdown_btn);
-    browser.setValue(eleMap.login_email_fld, usrData.admin_email);
-    browser.setValue(eleMap.login_pw_fld, usrData.admin_pw);
+    // browser.click(eleMap.login_dropdown_btn);
+    browser.pause(5000);
+    browser.setValue(getId.retId(eleIds.login_email_fld_id), usrData.admin_email);
+    browser.setValue(getId.retId(eleIds.login_pw_fld_id), usrData.admin_pw);
     browser.click(eleMap.login_btn);
     browser.pause("5000");
     expect(browser.getText("#logged-in-display-name")).to.equal(usrData.admin_user);
