3ec9bc39d0f2787ac2498f13ee9ce4ce010a5054
Mike Murray <hellomikemurray@gmail.com>
2016-10-16 18:26:39 -0700

    clean up

--- imports/plugins/included/product-detail-simple/client/containers/productDetailContainer.js
+++ imports/plugins/included/product-detail-simple/client/containers/productDetailContainer.js
@@ -1,18 +1,12 @@
-import { Meteor } from "meteor/meteor";
 import React, { Component } from "react";
 import { composeWithTracker } from "react-komposer";
-// import { DragDropContext } from "react-dnd";
-// import HTML5Backend from "react-dnd-html5-backend";
+import { Meteor } from "meteor/meteor";
 import { ReactionProduct } from "/lib/api";
-
 import { Reaction, i18next, Logger } from "/client/api";
 import { Tags, Media } from "/lib/collections";
 import { Loading } from "/imports/plugins/core/ui/client/components";
 import { ProductDetail } from "../components";
-import {
-  SocialContainer,
-  VariantListContainer
-} from "./";
+import { SocialContainer, VariantListContainer } from "./";
 import { MediaGalleryContainer } from "/imports/plugins/core/ui/client/containers";
 import { DragDropProvider, TranslationProvider } from "/imports/plugins/core/ui/client/providers";
 
@@ -33,7 +27,6 @@ class ProductDetailContainer extends Component {
 
   handleAddToCart = () => {
     let productId;
-    let qtyField;
     let quantity;
     const currentVariant = ReactionProduct.selectedVariant();
     const currentProduct = ReactionProduct.selectedProduct();
@@ -61,7 +54,6 @@ class ProductDetailContainer extends Component {
         return [];
       }
 
-      // qtyField =  //, template.$('input[name="addToCartQty"]'); //****
       quantity = parseInt(this.state.cartQuantity, 10);
 
       if (quantity < 1) {
@@ -69,7 +61,6 @@ class ProductDetailContainer extends Component {
       }
 
       if (!currentProduct.isVisible) {
-        console.log("cant add to cart");
         Alerts.inline("Publish product before adding to cart.", "error", {
           placement: "productDetail",
           i18nKey: "productDetail.publishFirst",
@@ -80,16 +71,15 @@ class ProductDetailContainer extends Component {
 
         if (productId) {
           Meteor.call("cart/addToCart", productId, currentVariant._id, quantity, (error) => {
-              if (error) {
-                Logger.error("Failed to add to cart.", error);
-                return error;
-              }
-              // Reset cart quantity on success
-              this.handleCartQuantityChange(null, 1);
-
-              return true;
+            if (error) {
+              Logger.error("Failed to add to cart.", error);
+              return error;
             }
-          );
+            // Reset cart quantity on success
+            this.handleCartQuantityChange(null, 1);
+
+            return true;
+          });
         }
 
         // template.$(".variant-select-option").removeClass("active");
@@ -142,6 +132,10 @@ class ProductDetailContainer extends Component {
     return null;
   }
 
+  handleProductField = (productId, fieldName, value) => {
+    Meteor.call("products/updateProductField", productId, fieldName, value);
+  }
+
   handleViewContextChange = (event, value) => {
     Reaction.Router.setQueryParams({as: value});
   }
@@ -163,6 +157,7 @@ class ProductDetailContainer extends Component {
             socialComponent={<SocialContainer />}
             topVariantComponent={<VariantListContainer />}
             onDeleteProduct={this.handleDeleteProduct}
+            onProductFieldChange={this.handleProductFieldChange}
             {...this.props}
           />
         </DragDropProvider>
@@ -171,10 +166,10 @@ class ProductDetailContainer extends Component {
   }
 }
 
-function changeProductField(productId, fieldName, value) {
-  console.log("productId", productId, fieldName, value);
-  Meteor.call("products/updateProductField", productId, fieldName, value);
-}
+ProductDetailContainer.propTypes = {
+  media: PropTypes.arrayOf(PropTypes.object),
+  product: PropTypes.object
+};
 
 function composer(props, onData) {
   const tagSub = Meteor.subscribe("Tags");
@@ -264,9 +259,4 @@ function composer(props, onData) {
 }
 
 // Decorate component and export
-let decoratedComponent = ProductDetailContainer;
-// decoratedComponent = DragDropContext(HTML5Backend)(decoratedComponent);
-// decoratedComponent = DragDropable(decoratedComponent);
-decoratedComponent = composeWithTracker(composer, Loading)(decoratedComponent);
-
-export default decoratedComponent;
+export default composeWithTracker(composer, Loading)(ProductDetailContainer);
