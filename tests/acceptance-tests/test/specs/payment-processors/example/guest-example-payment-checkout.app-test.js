62713b61e09e23919da1f04f6d0a96adb561b2ea
Capt-Slow <josh.tewing@gmail.com>
2016-10-17 11:53:00 -0700

    browserstack example (#1409)
    * beginning of getting browserstack working
    
    * More configs for browserstack
    
    * runner has been moved
    
    * creating test suite helper
    
    * fixes and moving some stuff around
    
    * More fixes. Allow the user to pick and choose what test suites to run.
    
    This will make it easier for the user if they are switching test
    runners frequently. I didn’t like adding all the tests in one array per
    configuration. This fixes that.
    
    Also allows the user to pick and choose what test suites apply to them.
    
    * lint fix
    
    * Adding reporter for tests, changed admin email, adding npm scripts for running tests
    
    * Allow Allure reporting to be optional
    
    * Primer for switching login/checkout tests to register/checkout
    
    * Login tests, are now register tests
    
    This also removes the dependency to having to create a user prior to
    running
    
    * Adding reporting to browserstack conf
    
    Also, added npm script for opening report. Another thing I noticed,
    browserstack tunnel sometimes hangs. So added process killer.
    
    * Updating test descriptions
    
    * lint fix
    
    * updating npm package
    
    * Addresses login test with hardcoded user assertion
    
    * This allows the user to use dynamic ids in tests
    
    * updated login test to pass local
    
    * updated test packages
    
    * updated from development
    
    - use REACTION_USER/AUTH if available.
    
    * updated versions
    
    * updated user data

--- tests/acceptance-tests/test/specs/payment-processors/example/guest-example-payment-checkout.app-test.js
+++ tests/acceptance-tests/test/specs/payment-processors/example/guest-example-payment-checkout.app-test.js
@@ -2,9 +2,8 @@
 const yaml = require("js-yaml");
 const fs   = require("fs");
 const expect = require("chai").expect;
-const shopUser = require("../../lib/user-shop-actions.js");
-const userDo = require("../../lib/basic-user-actions.js");
-
+const shopUser = require("../../../../lib/user-shop-actions.js");
+const userDo = require("../../../../lib/basic-user-actions.js");
 
 beforeEach(function () {
   const browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
@@ -13,10 +12,9 @@ beforeEach(function () {
 });
 
 
-describe("paypal logged in checkout test", function () {
+describe("example payment guest checkout test", function () {
   const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
-  it("verify logged in user can checkout with paypal", function () {
-    userDo.UserActions.userLogin("guest");
+  it("verify guest can checkout with example payment", function () {
     browser.pause("5000");
     userDo.UserActions.refreshShop();
     browser.click(eleMap.product);
@@ -24,18 +22,19 @@ describe("paypal logged in checkout test", function () {
     browser.click(eleMap.red_option);
     browser.waitForEnabled(eleMap.add_to_cart, 5000);
     browser.click(eleMap.add_to_cart);
-    browser.waitForEnabled(eleMap.checkout_btn, 5000);
+    browser.waitForEnabled(eleMap.checkout_btn, 2000);
     browser.click(eleMap.checkout_btn);
-    shopUser.checkForAddress();
+    browser.waitForEnabled(eleMap.continue_as_guest, 5000);
+    browser.click(eleMap.continue_as_guest);
+    shopUser.userAddress();
     // free shipping option
     browser.click(eleMap.free_shipping);
-    browser.waitForEnabled(eleMap.paypal, 5000);
-    browser.click(eleMap.paypal);
-    shopUser.paypalPaymentInfo();
-    browser.waitForEnabled(eleMap.paypal_complete_order_btn, 5000);
-    browser.click(eleMap.paypal_complete_order_btn);
-    // paypal takes forever 45sec wait
-    browser.waitForVisible("#order-status", 45000);
+    browser.waitForEnabled(eleMap.example_payment, 5000);
+    browser.click(eleMap.example_payment);
+    shopUser.examplePaymentInfo();
+    browser.waitForEnabled(eleMap.example_payment_complete_order_btn, 5000);
+    browser.click(eleMap.example_payment_complete_order_btn);
+    browser.waitForVisible("#order-status", 10000);
     expect(browser.getText("#order-status")).to.equal("Your order is now submitted.");
   });
 });
--- tests/acceptance-tests/test/specs/permissions/shop-settings-permissions.app-test.js
+++ tests/acceptance-tests/test/specs/permissions/shop-settings-permissions.app-test.js
@@ -2,8 +2,7 @@
 const yaml = require("js-yaml");
 const fs   = require("fs");
 const expect = require("chai").expect;
-const adminUser = require("../../lib/basic-user-actions.js");
-const resetUser = require("../../lib/user-permissions-reset.js");
+const adminUser = require("../../../lib/basic-user-actions.js");
 
 beforeEach(function () {
   const browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
@@ -12,31 +11,28 @@ beforeEach(function () {
 });
 
 afterEach(function () {
-  resetUser.resetUserPermissions();
+
 });
 
-describe("dashboard permissions test", function () {
+describe("core permissions test", function () {
   const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
-  it("verify admin adding dashboard permissions to guest user", function () {
+  it("verify admin adding dashboard permissions to guest", function () {
     adminUser.UserActions.userLogin("admin");
     // navigate to dashboard
     browser.click(eleMap.dashboard_btn);
     browser.pause("5000");
-    // select guest user from user page
     browser.click(eleMap.db_accounts);
     browser.pause("5000");
     browser.click(eleMap.account_manage_btn);
     browser.pause("5000");
     browser.click(eleMap.perm_dashboard_chkb);
-    browser.pause("5000");
+    browser.click(eleMap.perm_shop_settings_chkb);
     adminUser.UserActions.userLogout();
-    browser.pause("5000");
-    // login as guest user
     adminUser.UserActions.userLogin("guest");
-    // user should be able to enter the dashboard now
     browser.click(eleMap.dashboard_btn);
     browser.pause("5000");
-    expect(browser.getText(eleMap.dashboard_hdr_txt)).to.equal("Dashboard");
+    // verify Core settings option/button is present
+    expect(browser.getText(eleMap.core_settings_btn_txt)).to.equal("Core");
     adminUser.UserActions.userLogout();
   });
 });
