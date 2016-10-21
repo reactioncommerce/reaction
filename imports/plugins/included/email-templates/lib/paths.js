6ca737fa859d98717c41fb21eb056168e3a2b5a5
Mike Murray <hellomikemurray@gmail.com>
2016-10-21 11:47:06 -0700

    Register all email templates

---
+++ imports/plugins/included/email-templates/lib/paths.js
@@ -0,0 +1,16 @@
+// Tempalte paths relative to private/email/templates
+export const coreDefaultTemplate = "coreDefaultTemplate";
+
+export const resetPaswordTemplate = "accounts/reset_password";
+export const inviteShopMemberTemplate = "accounts/inviteShopMember";
+export const welcomeEmailTemplate = "accounts/sendWelcomeEmail";
+export const verifyEmailTemplate = "accounts/verifyEmail";
+
+export const checkoutLoginTemplate = "checkout/checkoutLogin";
+
+export const coreOrderCompletedTemplate = "checkout/coreOrderCompleted";
+export const coreOrderCreatedTemplate = "checkout/coreOrderCreated";
+export const coreOrderShippingInvoiceTemplate = "checkout/coreOrderShippingInvoice";
+export const coreOrderShippingSummaryTemplate = "checkout/coreOrderShippingSummary";
+export const coreOrderShippingTrackingTemplate = "checkout/coreOrderShippingTracking";
+export const coreOrderNewTemplate = "checkout/coreOrderNew";
--- imports/plugins/included/email-templates/server/index.js
+++ imports/plugins/included/email-templates/server/index.js
@@ -1,9 +1,90 @@
 import { Reaction } from "/server/api";
-import StandardTemplate from "../lib/templates/standard";
+import * as TemplatePaths from "../lib/paths.js";
+
+// Default
+Reaction.registerTemplate({
+  title: "Default",
+  name: TemplatePaths.coreDefaultTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreDefaultTemplate)
+});
+
+// Accounts
+Reaction.registerTemplate({
+  title: "Reset Password",
+  name: TemplatePaths.resetPaswordTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.resetPaswordTemplate)
+});
 
 Reaction.registerTemplate({
-  title: "Standard Email",
-  name: "standard-email",
+  title: "Invite Shop Member",
+  name: TemplatePaths.inviteShopMemberTemplate,
   type: "email",
-  template: StandardTemplate
+  template: Reaction.Email.getTemplate(TemplatePaths.inviteShopMemberTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "Welcome Email",
+  name: TemplatePaths.welcomeEmailTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.welcomeEmailTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "Verify Account",
+  name: TemplatePaths.verifyEmailTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.verifyEmailTemplate)
+});
+
+// Checkout
+Reaction.registerTemplate({
+  title: "Checkout Login",
+  name: TemplatePaths.checkoutLoginTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.checkoutLoginTemplate)
+});
+
+// Order workflow
+Reaction.registerTemplate({
+  title: "Order Completed",
+  name: TemplatePaths.coreOrderCompletedTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreOrderCompletedTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "Order Created",
+  name: TemplatePaths.coreOrderCreatedTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreOrderCreatedTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "Shipping Invoice",
+  name: TemplatePaths.coreOrderShippingInvoiceTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreOrderShippingInvoiceTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "Shipping Summary",
+  name: TemplatePaths.coreOrderShippingSummaryTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreOrderShippingSummaryTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "Shipping Tracking",
+  name: TemplatePaths.coreOrderShippingTrackingTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreOrderShippingTrackingTemplate)
+});
+
+Reaction.registerTemplate({
+  title: "New Order Started Processing",
+  name: TemplatePaths.coreOrderNewTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplate(TemplatePaths.coreOrderNewTemplate)
 });
