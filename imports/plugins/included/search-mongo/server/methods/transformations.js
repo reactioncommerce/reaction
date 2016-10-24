50a913207ad74a58fab9ea5b4582c9f9d990d885
Spencer Norman <spencern@gmail.com>
2016-10-24 15:40:30 -0600

    Fixes #1524 Error was caused by `fieldData.addressBook` existing as an empty array, this checks to make sure that `addressBook[0]` exists before looking for the `fullName` prop.

--- imports/plugins/included/search-mongo/server/methods/transformations.js
+++ imports/plugins/included/search-mongo/server/methods/transformations.js
@@ -28,7 +28,7 @@ transformations.accounts.emails = function (fieldData) {
 
 transformations.accounts.profile = function (fieldData) {
   let profileObject;
-  if (fieldData && fieldData.addressBook && fieldData.addressBook[0].fullName) {
+  if (fieldData && fieldData.addressBook && fieldData.addressbook[0] && fieldData.addressBook[0].fullName) {
     profileObject = {
       firstName: _.split(fieldData.addressBook[0].fullName, " ")[0],
       lastName: _.split(fieldData.addressBook[0].fullName, " ")[1],
@@ -37,4 +37,3 @@ transformations.accounts.profile = function (fieldData) {
   }
   return profileObject;
 };
-
