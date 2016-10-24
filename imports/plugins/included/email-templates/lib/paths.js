f55ec61c1dd6eb1dd54f32f3aca43f8f57c7358a
Mike Murray <hellomikemurray@gmail.com>
2016-10-24 11:47:39 -0700

    Fix template paths

--- imports/plugins/included/email-templates/lib/paths.js
+++ imports/plugins/included/email-templates/lib/paths.js
@@ -1,16 +1,16 @@
 // Tempalte paths relative to private/email/templates
-export const coreDefaultTemplate = "coreDefaultTemplate";
+export const coreDefaultTemplate = "coreDefault";
 
 export const resetPaswordTemplate = "accounts/reset_password";
 export const inviteShopMemberTemplate = "accounts/inviteShopMember";
 export const welcomeEmailTemplate = "accounts/sendWelcomeEmail";
-export const verifyEmailTemplate = "accounts/verifyEmail";
+export const verifyEmailTemplate = "accounts/verify_email";
 
 export const checkoutLoginTemplate = "checkout/checkoutLogin";
 
-export const coreOrderCompletedTemplate = "checkout/coreOrderCompleted";
-export const coreOrderCreatedTemplate = "checkout/coreOrderCreated";
-export const coreOrderShippingInvoiceTemplate = "checkout/coreOrderShippingInvoice";
-export const coreOrderShippingSummaryTemplate = "checkout/coreOrderShippingSummary";
-export const coreOrderShippingTrackingTemplate = "checkout/coreOrderShippingTracking";
-export const coreOrderNewTemplate = "checkout/coreOrderNew";
+export const coreOrderCompletedTemplate = "orders/coreOrderCompleted";
+export const coreOrderCreatedTemplate = "orders/coreOrderCreated";
+export const coreOrderShippingInvoiceTemplate = "orders/coreOrderShippingInvoice";
+export const coreOrderShippingSummaryTemplate = "orders/coreOrderShippingSummary";
+export const coreOrderShippingTrackingTemplate = "orders/coreOrderShippingTracking";
+export const coreOrderNewTemplate = "orders/coreOrderNew";
