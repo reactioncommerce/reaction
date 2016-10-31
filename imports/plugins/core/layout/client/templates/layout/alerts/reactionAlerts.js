c36c1bdaa094b600294e119ddf86cb14a93300ec
Spencer Norman <spencern@gmail.com>
2016-10-31 12:39:47 -0600

    Don't blind catch sweet alert errors.

--- imports/plugins/core/layout/client/templates/layout/alerts/reactionAlerts.js
+++ imports/plugins/core/layout/client/templates/layout/alerts/reactionAlerts.js
@@ -70,7 +70,12 @@ Object.assign(Alerts, {
         if (isConfirm === true && typeof messageOrCallback === "function") {
           messageOrCallback(isConfirm);
         }
-      }).catch(_.noop);
+      }).catch(function (err) {
+        if (err === "cancel" || err === "overlay" || err === "timer") {
+          return undefined; // Silence error
+        }
+        throw err;
+      });
     }
 
     const title = titleOrOptions;
@@ -85,7 +90,12 @@ Object.assign(Alerts, {
       if (isConfirm === true && typeof callback === "function") {
         callback(isConfirm);
       }
-    }).catch(_.noop);
+    }).catch(function (err) {
+      if (err === "cancel" || err === "overlay" || err === "timer") {
+        return undefined; // Silence error
+      }
+      throw err;
+    });
   },
 
   toast(message, type, options) {
