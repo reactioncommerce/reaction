f9cbc0f99cef7e2233b5ef80f524f6d128c276ae
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-11-02 00:17:10 -0700

    added check for original template

--- server/publications/collections/templates.js
+++ server/publications/collections/templates.js
@@ -4,19 +4,23 @@ import { Reaction } from "/server/api";
  /**
  * Templates
  * @param {string} type - Type of template to look for (i.e. 'email').
+ * @param {string} isOriginalTemplate - determines if this is the original or a copy.
  * @param {string} shopId - shopId for the shop template is used with.
  * @return {Array} templates
  */
 
-Meteor.publish("Templates", function (type) {
+Meteor.publish("Templates", function (type, isOriginalTemplate) {
   const shopId = Reaction.getShopId();
   check(type, Match.OneOf(String, null));
+  check(isOriginalTemplate, Match.OneOf(Boolean, undefined, null));
 
   if (!shopId) {
     return this.ready();
   }
   return Templates.find({
     shopId: shopId,
-    type: type
+    type: type,
+    // TODO: make this optional
+    isOriginalTemplate: isOriginalTemplate
   });
 });
