0f116324f4b8570b49f04acabe8ebf71ac686f01
Abisoye Oke-lawal <abisoye.oke-lawal@andela.com>
2016-11-02 18:03:33 +0100

    feature(sort-and-filter): Setup filter data

--- imports/plugins/included/ui-search/client/templates/productSearch/productResults.js
+++ imports/plugins/included/ui-search/client/templates/productSearch/productResults.js
@@ -11,13 +11,78 @@ import { ProductSearch } from "/lib/collections";
 /**
  * productGrid helpers
  */
+
+ // filterItems = [{vendor: []}, {min: 0}, {max: 9999999}, {score: false}];
+function filterResult() {
+  const result = [];
+  getProducts().filter((product) => {
+    console.log(product);
+    let match = false;
+    filterItems.forEach(item => {
+      match = false;
+      const key = Object.keys(item).toString();
+      switch (key) {
+        case "vendor":
+          match = vendorMatch(item[key], product[key]);
+          break;
+        case "min":
+          match = priceMatch("min", product.price, item[key]);
+          break;
+        case "max":
+          match = priceMatch("max", product.price, item[key]);
+          break;
+        case "score":
+          match = scoreMatch(item[key], product[key]);
+          break;
+        default:
+          match = false;
+      }
+      match ? result.push(product) : "";
+    });
+    // console.log("match", match);
+    // return match ? product : "";
+  });
+  return result;
+}
+
+// Match for score
+function scoreMatch(itemKey, productKey) {
+  return productKey >= itemKey ? true : false;
+}
+
+// Match for price
+function priceMatch(type, value, condition) {
+  if (!value) {
+    return false;
+  }
+
+  if (type === "min" && value.min >= condition) {
+    return true;
+  } else if (value.max <= condition) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Match for vendors
+function vendorMatch(itemKey, productKey) {
+  return (itemKey.includes(productKey.toString())) ?
+    true : false;
+}
+
+// format of items {location: item} e.g {vendor: china}
+function getProducts() {
+  return ProductSearch.find().fetch();
+}
+
 Template.productResults.onCreated(function () {
   Session.set("productGrid/selectedProducts", []);
 });
 
 Template.productResults.onRendered(function () {
   const instance = this;
-  filterItems = [{vendor: []}, {min: 0}, {max: 9999999}, {score: false}];
+  filterItems = [{vendor: []}, {min: undefined}, {max: undefined}, {score: undefined}];
 
   if (Reaction.hasPermission("createProduct")) {
     const productSort = $(".product-grid-list")[0];
@@ -61,7 +126,7 @@ Template.productResults.events({
     // {vendor: [China, ireland]}
     const key = event.target.parentNode.id;
     const value = event.target.value;
-
+    console.log("object length", filterItems.length);
     this.filterItems.forEach((item) => {
       let itemKey = Object.keys(item).toString();
       if (itemKey === key) {
@@ -76,8 +141,8 @@ Template.productResults.events({
         }
       }
     });
-    console.log(this.filterItems);
-    console.log(this.getProducts());
+    //console.log(this.filterItems);
+    console.log(filterResult());
     // console.log(ProductSearch.find().fetch());
     // console.log(event.target.value);
     // console.log(event.target.parentNode.id);
@@ -121,24 +186,5 @@ Template.productResults.helpers({
   },
   products() {
     return Template.currentData().products;
-  },
-  // format of items {location: item} e.g {vendor: china}
-  getProducts() {
-    return ProductSearch.find().fetch();
-  },
-  filterResult() {
-    if (this.filterItems.length > 0) {
-      return this.getProducts().filter((product) => {
-        let match = false;
-        this.filterItems.forEach(item => {
-          match = false;
-          const key = Object.keys(item).toString();
-          if (product[key] === item[key]) match = true;
-        });
-        return match ? product : "";
-      });
-    } else {
-      return this.getProducts();
-    }
   }
 });
