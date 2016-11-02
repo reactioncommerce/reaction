a7e3a27f2afe37cd9002857049d9569c1c1b8e37
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-11-01 23:51:17 -0700

    server methods for updating email templates

--- imports/plugins/core/emails/server/methods.js
+++ imports/plugins/core/emails/server/methods.js
@@ -3,21 +3,45 @@ import { Reaction } from "/server/api";
 
 export const methods = {
 
-  /*
-   * remove template
+  /**
+   * templates/email/remove
+   * @summary delete Template method for a email templates
+   * @param {String} templateId - id of template to remove
+   * @return {Number} remove template
+   * TODO: change return type? is number correct? copied from above
    */
-  "templates/remove": function (templateId) {
+  "templates/email/remove": function (templateId) {
     check(templateId, String);
-    // TODO: add permissions here - do we have template permissions?
+    // TODO: add permissions
     // if (!Reaction.hasPermission("shipping")) {
     //   throw new Meteor.Error(403, "Access Denied");
     // }
     return Templates.remove(templateId);
+  },
+
+  /**
+   * templates/email/update
+   * @summary update Template methods for a email templates
+   * @param {String} templateId - id of template to remove
+   * @param {String} doc - data to update doc with
+   * @return {Number} remove template
+   * TODO: change return type? is number correct? copied from above
+   */
+  "templates/email/update": function (templateId, doc) {
+    check(templateId, String);
+    check(doc, Object);
+    // TODO: add permissions
+    // if (!Reaction.hasPermission("shipping")) {
+    //   throw new Meteor.Error(403, "Access Denied");
+    // }
+    return Templates.update({
+      _id: templateId
+    }, {
+      $set: {
+        title: doc.title
+      }
+    });
   }
-
-
-
-
 }
 
 Meteor.methods(methods);
