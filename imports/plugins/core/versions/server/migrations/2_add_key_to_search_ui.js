ac2e3139f5a467cea776c0977c226a04396cc797
Brent Hoover <brent@thebuddhalodge.com>
2016-11-10 09:56:14 +0800

    Add migration to update existing registries

---
+++ imports/plugins/core/versions/server/migrations/2_add_key_to_search_ui.js
@@ -0,0 +1,24 @@
+import { Migrations } from "/imports/plugins/core/versions";
+import { Packages } from "/lib/collections";
+
+
+// Add keys to search so that stock search is enabled by default
+Migrations.add({
+  version: 2,
+  up() {
+    while (!Packages.find({Name: "reaction-ui-search"})) {
+      Packages.update({name: "reaction-ui-search"},
+        {
+          $set: {
+            registry: [{
+              provides: "ui-search",
+              template: "searchModal"
+            }
+
+            ]
+          }
+        }
+      );
+    }
+  }
+});
