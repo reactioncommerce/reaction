c8d183bb5aa003432f8cf6ffc45c0e6b8fe6cb38
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-11-09 16:56:09 -0800

    update comments

--- server/publications/collections/templates.js
+++ server/publications/collections/templates.js
@@ -2,10 +2,7 @@ import { Templates } from "/lib/collections";
 import { Reaction } from "/server/api";
 
  /**
- * Templates
- * @param {string} type - Type of template to look for (i.e. 'email').
- * @param {string} isOriginalTemplate - determines if this is the original or a copy.
- * @param {string} shopId - shopId for the shop template is used with.
+ * Publish Templates
  * @return {Array} templates
  */
 
