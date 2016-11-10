0473ffd83d2d15248ba2dfef35db29283743f072
Brent Hoover <brent@thebuddhalodge.com>
2016-11-10 11:40:31 +0800

    Remove logging

--- imports/plugins/core/versions/server/migrations/2_add_key_to_search_ui.js
+++ imports/plugins/core/versions/server/migrations/2_add_key_to_search_ui.js
@@ -1,12 +1,10 @@
 import { Migrations } from "/imports/plugins/core/versions";
 import { Packages } from "/lib/collections";
-import { Logger } from "/server/api";
 
 // Add keys to search so that stock search is enabled by default
 Migrations.add({
   version: 2,
   up() {
-    Logger.info("Running search package update");
     Packages.update({name: "reaction-ui-search"},
       {
         $set: {
