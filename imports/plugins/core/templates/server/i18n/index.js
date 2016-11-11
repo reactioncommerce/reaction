f3c82f59523c12e21092a72dce0519f1cd6041e8
Erik Kieckhafer <kieckhafer@users.noreply.github.com>
2016-11-11 14:56:40 -0800

    Update index.js

--- imports/plugins/core/templates/server/i18n/index.js
+++ imports/plugins/core/templates/server/i18n/index.js
@@ -1,14 +1,5 @@
 import { loadTranslations } from "/server/startup/i18n";
-// import ar from "./ar.json";
-// import de from "./de.json";
 import en from "./en.json";
-// import es from "./es.json";
-// import fr from "./fr.json";
-// import he from "./he.json";
-// import pt from "./pt.json";
-// import ro from "./ro.json";
-// import ru from "./ru.json";
-// import zh from "./zh.json";
 
 //
 // we want all the files in individual
@@ -16,5 +7,4 @@ import en from "./en.json";
 // automated translation software
 //
 
-// loadTranslations([ar, de, en, es, fr, he, pt, ro, ru, zh]);
 loadTranslations([en]);
