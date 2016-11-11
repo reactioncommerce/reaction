4e2c690a8d02bbfd3d6b78ac1c3fa8a45e7f771a
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-11-11 14:24:29 -0800

    move i18n to new modular style

---
+++ imports/plugins/core/templates/server/i18n/index.js
@@ -0,0 +1,20 @@
+import { loadTranslations } from "/server/startup/i18n";
+// import ar from "./ar.json";
+// import de from "./de.json";
+import en from "./en.json";
+// import es from "./es.json";
+// import fr from "./fr.json";
+// import he from "./he.json";
+// import pt from "./pt.json";
+// import ro from "./ro.json";
+// import ru from "./ru.json";
+// import zh from "./zh.json";
+
+//
+// we want all the files in individual
+// imports for easier handling by
+// automated translation software
+//
+
+// loadTranslations([ar, de, en, es, fr, he, pt, ro, ru, zh]);
+loadTranslations([en]);
