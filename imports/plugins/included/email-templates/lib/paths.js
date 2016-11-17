cd44c7cbaa2eadb8c499dcb2f9b7f7534785af1b
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-11-17 13:51:05 -0800

    finalizing email templates - recovery commit

--- imports/plugins/included/email-templates/lib/paths.js
+++ imports/plugins/included/email-templates/lib/paths.js
@@ -14,29 +14,67 @@ export const coreDefaultTemplate = "coreDefault";
 export const inviteShopMemberTemplate = "accounts/inviteShopMember";
 export const resetPaswordTemplate = "accounts/resetPassword";
 export const welcomeEmailTemplate = "accounts/sendWelcomeEmail";
+// TODO: Used by launchdock, and I haven't been able to test efficiently
 export const verifyEmailTemplate = "accounts/verify_email";
-
-
-/*
- * Checkout related email templates
- */
-export const checkoutLoginTemplate = "checkout/checkoutLogin";
 
 
 /*
  * Order (coreOrder) related email templates
  */
+export const coreOrderNewTemplate = "orders/new";
 export const orderShipped = "orders/shipped";
-export const coreOrderCompletedTemplate1 = "orders/completed";
-export const coreOrderCompletedTemplate2 = "orders/coreOrderCompleted";
-export const ordersCoreOrderWorkflowProcessing = "orders/coreOrderWorkflow/processing";
-export const ordersCoreOrderWorkflowCompleted = "orders/coreOrderWorkflow/completed";
-export const coreOrderCreatedTemplate = "orders/coreOrderCreated";
-export const coreOrderShippingInvoiceTemplate = "orders/coreOrderShippingInvoice";
-export const coreOrderShippingSummaryTemplate = "orders/coreOrderShippingSummary";
-export const coreOrderShippingTrackingTemplate = "orders/coreOrderShippingTracking";
+export const orderRefunded = "orders/refunded";
+
+
+/*
+ * Checkout related email templates
+ */
+// export const checkoutLoginTemplate = "checkout/checkoutLogin";
+
+
+
+
+
+
+
+
+
+
+
+//
+// export const ordersCoreOrderWorkflowProcessing = "orders/coreOrderWorkflow/processing";
+// export const ordersCoreOrderWorkflowCompleted = "orders/coreOrderWorkflow/completed";
+// export const coreOrderCreatedTemplate = "orders/coreOrderCreated";
+// export const coreOrderShippingInvoiceTemplate = "orders/coreOrderShippingInvoice";
+// export const coreOrderShippingSummaryTemplate = "orders/coreOrderShippingSummary";
+// export const coreOrderShippingTrackingTemplate = "orders/coreOrderShippingTracking";
 
 /*
  * Email sent when new order is created
  */
-export const coreOrderNewTemplate = "orders/new";
+
+
+
+
+
+
+
+
+
+
+/*
+ * TODO
+ * Emails that are never sent out.
+ * Should we keep these for legacy users who might be using them? Should we delete these?
+ */
+// Accounts
+
+
+
+/*
+ * TODO
+ * Emails that are no longer used by this version of Reaction
+ * Should we keep these for legacy users who might be using them? Or delete?
+ */
+// This is no longer used, as we use "orders/shipped" to send the email
+// export const coreOrderCompletedTemplate2 = "orders/coreOrderCompleted";
--- imports/plugins/included/email-templates/server/index.js
+++ imports/plugins/included/email-templates/server/index.js
@@ -18,7 +18,10 @@ Reaction.registerTemplate({
  * Account related email templates
  */
 
- // Invite new shop member
+/*
+ * Accounts - Invite Shop member
+ * When: Admin invites new member to shop
+ */
 Reaction.registerTemplate({
   title: "Accounts - Invite Shop Member",
   name: TemplatePaths.inviteShopMemberTemplate,
@@ -27,13 +30,10 @@ Reaction.registerTemplate({
   subject: "You have been invited to join {{shop.name}}"
 });
 
-
-
-
-
-
-
-// Reset Password
+/*
+ * Accounts - Reset Password
+ * When: User requests to reset their password
+ */
 Reaction.registerTemplate({
   title: "Accounts - Reset Password",
   name: TemplatePaths.resetPaswordTemplate,
@@ -42,7 +42,10 @@ Reaction.registerTemplate({
   subject: "{{shop.name}}: Here's your password reset link"
 });
 
-// Welcome email for new account
+/*
+ * Accounts - Welcome Email
+ * When: New user signs up for an account
+ */
 Reaction.registerTemplate({
   title: "Accounts - Welcome Email",
   name: TemplatePaths.welcomeEmailTemplate,
@@ -50,6 +53,84 @@ Reaction.registerTemplate({
   template: Reaction.Email.getTemplateFile(TemplatePaths.welcomeEmailTemplate),
   subject: "You're In. Welcome to {{shop.name}}!"
 });
+
+/*
+ * Accounts - Verify Email
+ * When: A user signs up through launchdock
+ * TODO: Used by launchdock, I haven't been able to test efficiently
+ */
+ Reaction.registerTemplate({
+   title: "Verify Account",
+   name: TemplatePaths.verifyEmailTemplate,
+   type: "email",
+   template: Reaction.Email.getTemplateFile(TemplatePaths.verifyEmailTemplate),
+   subject: "This is the subject"
+ });
+
+
+/*
+ * Order (coreOrder) related email templates
+ */
+
+/*
+ * Orders - New Order Place
+ * When: A user completes the cart checkout flow and a new order is placed
+ */
+Reaction.registerTemplate({
+  title: "Orders - New Order Placed",
+  name: TemplatePaths.coreOrderNewTemplate,
+  type: "email",
+  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderNewTemplate),
+  subject: "Your order is confirmed - {{order._id}}"
+});
+
+/*
+ * Orders - Order Shipped
+ * When: Admin completes the order flow and item is shipped
+ * When: Admin resends shipment notification
+ */
+Reaction.registerTemplate({
+  title: "Orders - Order Shipped",
+  name: TemplatePaths.orderShipped,
+  type: "email",
+  template: Reaction.Email.getTemplateFile(TemplatePaths.orderShipped),
+  subject: "{{shop.name}}: Your order has shipped - {{order._id}}"
+});
+
+/*
+ * Orders - Order Refunded
+ * When: Admin completes the order flow and item is shipped
+ * When: Admin resends shipment notification
+ */
+Reaction.registerTemplate({
+  title: "Orders - Order Refunded",
+  name: TemplatePaths.orderRefunded,
+  type: "email",
+  template: Reaction.Email.getTemplateFile(TemplatePaths.orderRefunded),
+  subject: "{{shop.name}}: Confirmation of refund for {{order._id}}"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -61,123 +142,112 @@ Reaction.registerTemplate({
  * Checkout related email templates
  */
 
-// checkout login
-Reaction.registerTemplate({
-  title: "Checkout Login",
-  name: TemplatePaths.checkoutLoginTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.checkoutLoginTemplate),
-  subject: "This is the subject"
-});
+// // checkout login
+// Reaction.registerTemplate({
+//   title: "Checkout Login",
+//   name: TemplatePaths.checkoutLoginTemplate,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.checkoutLoginTemplate),
+//   subject: "This is the subject"
+// });
 
 
 /*
  * Order (coreOrder) related email templates
  */
 
-/*
- * Order Shipped
- * Sent on: data-event-action="shipmentShipped"
- * Sent on: data-event-action="resendNotification"
- */
-Reaction.registerTemplate({
-  title: "Order Shipped",
-  name: TemplatePaths.orderShipped,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.orderShipped),
-  subject: "{{shop.name}}: Your order has shipped - {{order._id}}"
-});
 
-
-// Order workflow completed - old version
-Reaction.registerTemplate({
-  title: "Order Completed 1",
-  name: TemplatePaths.coreOrderCompletedTemplate1,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderCompletedTemplate1),
-  subject: "This is the subject"
-});
-
-// Order workflow completed - old version #2
-Reaction.registerTemplate({
-  title: "Order Completed 2",
-  name: TemplatePaths.coreOrderCompletedTemplate2,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderCompletedTemplate2),
-  subject: "This is the subject"
-});
-
-// Order workflow completed
-Reaction.registerTemplate({
-  title: "Order Completed New",
-  name: TemplatePaths.ordersCoreOrderWorkflowCompleted,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.ordersCoreOrderWorkflowCompleted),
-  subject: "This is the subject"
-});
-
-// Order workflow processing
-Reaction.registerTemplate({
-  title: "Order Processing New",
-  name: TemplatePaths.ordersCoreOrderWorkflowProcessing,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.ordersCoreOrderWorkflowProcessing),
-  subject: "This is the subject"
-});
-
-// Order workflow new order created
-Reaction.registerTemplate({
-  title: "Order Created",
-  name: TemplatePaths.coreOrderCreatedTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderCreatedTemplate),
-  subject: "{{shop.name}}: Your order has shipped - {{order._id}}"
-});
-
-
-// Still working through
-
-Reaction.registerTemplate({
-  title: "Shipping Invoice",
-  name: TemplatePaths.coreOrderShippingInvoiceTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderShippingInvoiceTemplate),
-  subject: "This is the subject"
-});
-
-Reaction.registerTemplate({
-  title: "Shipping Summary",
-  name: TemplatePaths.coreOrderShippingSummaryTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderShippingSummaryTemplate),
-  subject: "This is the subject"
-});
-
-Reaction.registerTemplate({
-  title: "Shipping Tracking",
-  name: TemplatePaths.coreOrderShippingTrackingTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderShippingTrackingTemplate),
-  subject: "This is the subject"
-});
-
-Reaction.registerTemplate({
-  title: "New Order Started Processing",
-  name: TemplatePaths.coreOrderNewTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderNewTemplate),
-  subject: "This is the subject"
-});
-
-
-/*
- * Account related email templates
- */
+//
+//
+//
+// // Order workflow completed
+// Reaction.registerTemplate({
+//   title: "Order Completed New",
+//   name: TemplatePaths.ordersCoreOrderWorkflowCompleted,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.ordersCoreOrderWorkflowCompleted),
+//   subject: "This is the subject"
+// });
+//
+// // Order workflow processing
+// Reaction.registerTemplate({
+//   title: "Order Processing New",
+//   name: TemplatePaths.ordersCoreOrderWorkflowProcessing,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.ordersCoreOrderWorkflowProcessing),
+//   subject: "This is the subject"
+// });
+//
+// // Order workflow new order created
+// Reaction.registerTemplate({
+//   title: "Order Created",
+//   name: TemplatePaths.coreOrderCreatedTemplate,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderCreatedTemplate),
+//   subject: "{{shop.name}}: Your order has shipped - {{order._id}}"
+// });
+//
+//
+// // Still working through
+//
+// Reaction.registerTemplate({
+//   title: "Shipping Invoice",
+//   name: TemplatePaths.coreOrderShippingInvoiceTemplate,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderShippingInvoiceTemplate),
+//   subject: "This is the subject"
+// });
+//
+// Reaction.registerTemplate({
+//   title: "Shipping Summary",
+//   name: TemplatePaths.coreOrderShippingSummaryTemplate,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderShippingSummaryTemplate),
+//   subject: "This is the subject"
+// });
+//
+// Reaction.registerTemplate({
+//   title: "Shipping Tracking",
+//   name: TemplatePaths.coreOrderShippingTrackingTemplate,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderShippingTrackingTemplate),
+//   subject: "This is the subject"
+// });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// /*
+//  * TODO
+//  * Emails that are never sent out.
+//  * Should we keep these for legacy users who might be using them? Should we delete these?
+//  */
+// TODO: Unused always?????
+// Accounts
 // Account verification email
-Reaction.registerTemplate({
-  title: "Verify Account",
-  name: TemplatePaths.verifyEmailTemplate,
-  type: "email",
-  template: Reaction.Email.getTemplateFile(TemplatePaths.verifyEmailTemplate),
-  subject: "This is the subject"
-});
+
+//
+//
+// /*
+//  * TODO
+//  * Emails that are no longer used by this version of Reaction
+//  * Should we keep these for legacy users who might be using them? Or delete?
+//  */
+//
+//
+//
+// // This is no longer used, as we use "orders/shipped" to send the email
+// // Order workflow completed - old version #2
+// Reaction.registerTemplate({
+//   title: "Order Completed 2",
+//   name: TemplatePaths.coreOrderCompletedTemplate2,
+//   type: "email",
+//   template: Reaction.Email.getTemplateFile(TemplatePaths.coreOrderCompletedTemplate2),
+//   subject: "This is the subject"
+// });
