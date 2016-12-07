307df8cea4ff33f3250f1e5bbb99a82338a78910
aaronjudd <aaron@reactioncommerce.com>
2016-12-07 12:50:20 -0800

    Updated payments method
    - Replaces `payments/paymentMethod` with `payments/cart/apply`
    - add discounts/codes/remove
    - add discounts/codes/apply

--- imports/plugins/included/discount-codes/server/methods/methods.js
+++ imports/plugins/included/discount-codes/server/methods/methods.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { Match, check } from "meteor/check";
 import { Reaction } from "/server/api";
+import { Cart } from "/lib/collections";
 import { Discounts } from  "/imports/plugins/core/discounts/lib/collections";
 import { DiscountCodes as DiscountSchema } from "../../lib/collections/schemas";
 
@@ -32,6 +33,85 @@ export const methods = {
     }
     // else update and return
     return Discounts.update(docId, modifier);
+  },
+  /**
+   * discounts/codes/remove
+   * removes discounts that have been previously applied
+   * to a cart.
+   * @param  {String} cartId cart id of which to remove a code
+   * @param  {String} codeId discount Id from cart.billing
+   * @return {String} returns update/insert result
+   */
+  "discounts/codes/remove": function (cartId, codeId) {
+    check(cartId, String);
+    check(codeId, String);
+
+    //
+    // delete code from cart
+    // TODO update a history record of transaction
+    //
+    return Cart.update(
+      { _id: cartId },
+      { $pull: { billing: { _id: codeId } } },
+      { multi: true }
+    );
+  },
+  /**
+   * discounts/codes/apply
+   * checks validity of code conditions and then
+   * applies a discount as a paymentMethod to cart
+   * @param  {String} cartId cart id of which to remove a code
+   * @param  {String} code valid discount code
+   * @return {Boolean} returns true if successfully applied
+   */
+  "discounts/codes/apply": function (cartId, code) {
+    check(cartId, String);
+    check(code, String);
+
+    // get current cart conditions from cartId
+    const currentCart = Cart.findOne(cartId);
+
+    // todo further expand to meet all condition rules
+    const conditions = {
+      enabled: true
+    };
+
+    // add  conditions: conditions
+    const discount = Discounts.findOne({ code: code });
+
+    // check usage limit
+    // don't apply if cart has exceeded usage limit
+    // will also need to check all time usage.
+    // which means storing the use data with the Discounts
+    // or searching all user's order history
+    // and if a user cancels an order,
+    // is the discount now re-activated
+
+
+    if (discount) {
+      // save to payment methods
+      // and update status in Discounts
+      const paymentMethod = {
+        processor: "discount-code",
+        method: "discounts/codes/apply",
+        code: discount.code,
+        transactionId: Random.id(),
+        amount: discount.discount, // pre-process to amount.
+        status: "created"
+      };
+      // apply to cart
+      Meteor.call("payments/cart/apply", cartId, paymentMethod, (error, result) => {
+        if (result) {
+          console.log("TODO: discount applied.")
+          // we need transaction records of the discount status
+          // ie: has the user used this before, in other carts?
+          // increment the discount use counter, etc.
+        }
+      });
+      // and update Discounts status.
+      return true;
+    }
+    return false;
   }
 };
 
--- server/methods/core/payments.js
+++ server/methods/core/payments.js
@@ -2,33 +2,34 @@ import { Meteor } from "meteor/meteor";
 import { check } from "meteor/check";
 import { Cart } from "/lib/collections";
 
-Meteor.methods({
+export const methods = {
   /**
-   * payments/paymentMethod
+   * payments/cart/apply
    * @summary adds payment to order
    * @param {String} cartId - cartId
    * @param {Object} paymentMethod - formatted payment method object
    * @returns {String} return cart update result
    */
-  "payments/paymentMethod": function (cartId, paymentMethod) {
+  "payments/cart/apply": function (cartId, paymentMethod) {
     check(cartId, String);
     check(paymentMethod, Object);
 
     // temp hack until we build out multiple payment handlers
-    const cart = Cart.findOne(cartId);
-    let paymentId = "";
-    if (cart.billing) {
-      paymentId = cart.billing[0]._id;
-    }
+    // const cart = Cart.findOne(cartId);
+    // let paymentId = "";
+    // if (cart.billing) {
+    //   paymentId = cart.billing[0]._id;
+    // }
 
-    return Cart.upsert({
-      "_id": cartId,
-      "billing._id": paymentId
+    return Cart.update({
+      _id: cartId
     }, {
       $addToSet: {
-        "billing.paymentMethod": paymentMethod
+        billing: { paymentMethod: paymentMethod }
       }
     });
   }
+};
 
-});
+// export methods to Meteor
+Meteor.methods(methods);
