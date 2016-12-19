f172d319918b4741f5e5a341178f31074d49c646
Spencer Norman <spencern@gmail.com>
2016-12-19 13:04:54 -0700

    Set prerender-status-code to 404 when rendering notFound template

--- imports/plugins/core/layout/client/templates/layout/notFound/notFound.js
+++ imports/plugins/core/layout/client/templates/layout/notFound/notFound.js
@@ -1,3 +1,4 @@
 Template.notFound.onCreated(function () {
+  document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<meta name='prerender-status-code' content='404'>");
   // todo report not found source
 });
