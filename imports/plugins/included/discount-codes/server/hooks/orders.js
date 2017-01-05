12c8568b937402833d6ed761eb59cc65deab3ef5
Aaron Judd <aaron@reactioncommerce.com>
2017-01-04 17:37:09 -0800

    Discount limit handling (#1667)
    * Updated icon-list items in orders summary
    
    * Add transaction tracking to discounts
    
    - adds transaction set to Discounts collection
    - copyCartToOrder hook updates transaction
    - tracks userId, orderId, appliedAt.
    
    * Implement expired code validation in DiscountForm
    
    * Handle "enter" input event
    
    * Method call typo
    
    * cleanly log service error
    
    * check that condition exists before limit validation
    
    * Update docker build, node version
    
     — try using docker build rm=false as suggested by
    https://circleci.com/docs/docker-btrfs-error/
    
    * Validate cart.billing for discount calculations hooks
    
    * Updated transaction counting
    
    * Updated Meteor, npm dependencies
    
    * Prevent done() from being called multiple times

---
+++ imports/plugins/included/discount-codes/server/hooks/orders.js
@@ -0,0 +1,19 @@
+import { Cart } from "/lib/collections";
+import { MethodHooks } from "/server/api";
+
+// this hook runs before a cart is converted to an order
+// it looks at any discounts that have been applied to the cart
+// and updates the discount document with a transaction history
+MethodHooks.before("cart/copyCartToOrder", function (options) {
+  const cartId = options.arguments[0];
+  const cart = Cart.findOne(cartId);
+  // record that discounts have been applied to order
+  if (cart && cart.billing) {
+    for (const billing of cart.billing) {
+      // TODO should we enable transactions for rates as well?
+      if (billing.paymentMethod && billing.paymentMethod.processor === "code") {
+        Meteor.call("discounts/transaction", cartId, billing.paymentMethod.id);
+      }
+    }
+  }
+});
