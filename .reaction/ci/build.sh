724dd955bed2c88aab3e44aa66c5e980d35b7a03
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-01-13 13:24:13 -0500

    start using external Docker base and reaction-cli for build

--- .reaction/ci/build.sh
+++ .reaction/ci/build.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
+set -e
+
+DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-"reactioncommerce/reaction"}
+
 # if we're not on a deployment branch, skip the Docker build/test
 if [[ "$CIRCLE_BRANCH" != "master" && "$CIRCLE_BRANCH" != "development" ]]; then
   echo "Not running a deployment branch. Skipping the Docker build test."
   exit 0
 fi
 
-# load up cached image if there is one
-if [[ -e ~/docker/image.tar ]]; then
-  docker load -i ~/docker/image.tar
-fi
-
 # build new base and app images
-.reaction/docker/build.sh
-
-# if successful, save in cache
-mkdir -p ~/docker
-docker save reactioncommerce/reaction:latest > ~/docker/image.tar
+reaction build $DOCKER_NAMESPACE:latest
 
 # run the container and wait for it to boot
-docker-compose -f .reaction/docker/docker-compose.test.yml up -d
-sleep 30
+docker run -p 80:3000 -d $DOCKER_NAMESPACE:latest
+sleep 20
 
 # use curl to ensure the app returns 200's
 curl --retry 10 --retry-delay 10 -v http://localhost
--- .reaction/docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# build the base container and then the app container
-docker build --rm=false -f .reaction/docker/base.dockerfile -t reactioncommerce/base:latest .
-docker build --rm=false -t reactioncommerce/reaction:latest .
