b139d1c4c0d89506ed6c90a2df7760e9f4d99031
Brent Hoover <brent@thebuddhalodge.com>
2017-01-18 14:18:13 +0800

    Use TaxRate API for cart calcularions

--- imports/plugins/included/taxes-avalara/server/hooks/hooks.js
+++ imports/plugins/included/taxes-avalara/server/hooks/hooks.js
@@ -1,95 +1,35 @@
 import { Meteor } from "meteor/meteor";
 import { Logger, MethodHooks } from "/server/api";
 import { Cart, Packages } from "/lib/collections";
-import avalaraapi from "../methods/rateLookupApi";
+import taxCalc from "../methods/taxCalc";
 
-//
-// this entire method will run after the core/taxes
-// plugin runs the taxes/calculate method
-// it overrwites any previous tax calculation
-// tax methods precendence is determined by
-// load order of plugins
-//
-// also note that we should address the issue
-// of the alpha-3 requirement for avalara,
-// and also weither we need the npm package or
-// should we just use HTTP.
-//
+
+function calcTaxable(cart) {
+  let subTotal = 0;
+  for (const item of cart.items) {
+    if (item.variants.taxable) {
+      subTotal += (item.variants.price * item.quantity);
+    }
+  }
+  return subTotal;
+}
+
 MethodHooks.after("taxes/calculate", function (options) {
-  const result = options.result || {};
   const cartId = options.arguments[0];
   const cartToCalc = Cart.findOne(cartId);
   const shopId = cartToCalc.shopId;
-
   const pkg = Packages.findOne({
     name: "taxes-avalara",
     shopId: shopId,
     enabled: true
   });
 
-  // check if package is configured
+  Logger.info("Avalara triggered on taxes/calculate for cartId:", cartId);
   if (pkg && pkg.settings.avalara) {
-    const apiKey = pkg.settings.avalara.apiLoginId;
-
-    // process rate callback object
-    const processTaxes = Meteor.bindEnvironment(function processTaxes(res) {
-      if (!res.error) {
-        // calculate line item taxes
-        // maybe refactor to a core calculation
-        let totalTax = 0;
-        let taxRate = 0;
-        for (const items of cartToCalc.items) {
-          // only processs taxable products
-          if (items.variants.taxable === true) {
-            const subTotal = items.variants.price * items.quantity;
-            const tax = subTotal * (res.totalRate / 100);
-            totalTax += tax;
-          }
-        }
-        // calc overall cart tax rate
-        if (totalTax > 0) {
-          taxRate = (totalTax / cartToCalc.cartSubTotal());
-        }
-        // save taxRate
-        Meteor.call("taxes/setRate", cartId, taxRate, res.rates);
-      } else {
-        Logger.warn("Error fetching tax rate from Avalara", res.code, res.message);
-      }
+    taxCalc.estimateCart(cartToCalc, function (result) {
+      const taxAmount = parseFloat(result.totalTax);
+      const taxRate = taxAmount / calcTaxable(cartToCalc);
+      Meteor.call("taxes/setRate", cartId, taxRate);
     });
-
-    // check if plugin is enabled and this calculation method is enabled
-    if (pkg && pkg.enabled === true && pkg.settings.avalara.enabled === true) {
-      if (!apiKey) {
-        Logger.warn("Avalara API Key is required.");
-      }
-      if (typeof cartToCalc.shipping !== "undefined") {
-        const shippingAddress = cartToCalc.shipping[0].address;
-
-        if (shippingAddress) {
-          // TODO evaluate country-data
-          // either replace our current countries data source
-          // or integrate the alpha-3 codes into our dataset.
-          // const countries = require("country-data").countries;
-          const lookup = require("country-data").lookup;
-          // converting iso alpha 2 country to ISO 3166-1 alpha-3
-          const country = lookup.countries({ alpha2: shippingAddress.country })[0];
-
-          // get tax rate by street address
-          avalaraapi.taxByAddress(apiKey,
-            shippingAddress.address1,
-            shippingAddress.city,
-            shippingAddress.region,
-            country.alpha3,
-            shippingAddress.postal,
-            processTaxes
-          );
-          // tax call made
-          Logger.info("Avalara triggered on taxes/calculate for cartId:", cartId);
-        }
-      }
-    }
   }
-  // Default return value is the return value of previous call in method chain
-  // or an empty object if there's no result yet.
-  return result;
 });
