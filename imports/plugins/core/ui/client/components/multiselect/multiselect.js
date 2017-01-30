47590d8a6656fbe6356a1ccc5e27cdbe3e39e105
haris_vitoros <harris@artlimes.com>
2017-01-30 10:47:47 +0200

    clean multiselect

--- imports/plugins/core/ui/client/components/multiselect/multiselect.js
+++ imports/plugins/core/ui/client/components/multiselect/multiselect.js
@@ -2,31 +2,15 @@ import React, { Component, PropTypes } from "react";
 import Select from "react-select";
 
 class MultiSelect extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: props.value || []
-    };
-  }
-
-  handleOnChange(value) {
-    this.setState({
-      value
-    });
-
-    this.props.onChange(value);
-  }
-
   render() {
     return (
       <Select
         multi
         simpleValue
-        value={this.state.value}
+        value={this.props.value}
         placeholder={this.props.placeholder}
         options={this.props.options}
-        onChange={this.handleOnChange.bind(this)}
+        onChange={this.props.onChange}
       />
     );
   }
