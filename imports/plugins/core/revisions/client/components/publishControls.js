3eb06dc15cec2c0ee2658d1934f064ea5c6643c1
lorenzo <lorenzo@artlimes.com>
2017-02-07 19:04:44 +0200

    fixed i18n typo

--- imports/plugins/core/revisions/client/components/publishControls.js
+++ imports/plugins/core/revisions/client/components/publishControls.js
@@ -337,7 +337,7 @@ class PublishControls extends Component {
       <FlatButton
         onClick={() => {
           Reaction.showActionView({
-            i18nKeyTite: "dashboard.coreTitle",
+            i18nKeyTitle: "dashboard.coreTitle",
             title: "Dashboard",
             template: "dashboardPackages"
           });
