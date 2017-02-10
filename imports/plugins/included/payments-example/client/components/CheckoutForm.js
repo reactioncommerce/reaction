0b8a199b20c9a0cd52ed6f100eff3f8a84414986
andela-jwarugu <joy.warugu@andela.com>
2017-02-10 22:05:41 +0300

    Replecate presentation of the checkout form

--- imports/plugins/included/payments-example/client/components/CheckoutForm.js
+++ imports/plugins/included/payments-example/client/components/CheckoutForm.js
@@ -4,7 +4,56 @@ class CheckoutForm extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <div className="row">
+          <div className="form-group">
+            <label className="control-label">Cardholder name</label>
+            <input
+              type="text"
+            />
+          </div>
+
+          <div className="form-group">
+            <label className="control-label">Card number</label>
+            <input
+              type="text"
+              placeholder="XXXX XXXX XXXX XXXX"
+            />
+          </div>
+          <div className="row">
+            <div className="col-sm-12 col-lg-6 form-group">
+              <label className="control-label">Expiration month</label>
+              <input
+                type="text"
+                placeholder="Exp. Month"
+              />
+            </div>
+
+            <div className="col-sm-12 col-lg-6 form-group">
+              <label className="control-label">Expiration year</label>
+              <input
+                type="text"
+                placeholder="Exp. Year"
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-sm-12 col-lg-3 form-group">
+              <label className="control-label">CVV</label>
+              <input
+                type="text"
+                placeholder="CVV"
+              />
+            </div>
+          </div>
+
+          <div className="alert alert-danger hidden">Oh! Snap!</div>
+          <button type="submit" className="rui btn btn-lg btn-cta btn-block btn-complete-order">
+            <span id="btn-complete-order">Complete your order</span>
+            <i className="fa fa-spinner fa-spin hidden" id="btn-processing"/>
+          </button>
+
+        </div>
       </div>
     );
   }
