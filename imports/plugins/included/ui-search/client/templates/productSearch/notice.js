87593fc11ab7fbb8c2d6541388ca07531758730f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2017-02-13 23:33:19 -0800

    add notice for search

---
+++ imports/plugins/included/ui-search/client/templates/productSearch/notice.js
@@ -0,0 +1,12 @@
+
+Template.searchGridNotice.helpers({
+  isLowQuantity: function () {
+    return this.product.isLowQuantity;
+  },
+  isSoldOut: function () {
+    return this.product.isSoldOut;
+  },
+  isBackorder: function () {
+    return this.product.isBackorder;
+  }
+});
