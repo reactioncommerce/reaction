b8660954bf311bfe1f439315860dbcdfafa96902
Seun Martins <seundotme@gmail.com>
2017-02-22 18:45:29 +0100

    Update account find for taxsettings to subscribe

--- client/modules/accounts/templates/tax-settings/tax-settings.js
+++ client/modules/accounts/templates/tax-settings/tax-settings.js
@@ -1,9 +1,20 @@
 import { Accounts } from "/lib/collections";
 import { Template } from "meteor/templating";
-
+import { Reaction } from "/client/api";
 
 Template.taxSettingsPanel.helpers({
   account() {
-    return Accounts.findOne();
+    if (Reaction.Subscriptions.Account.ready()) {
+      return Accounts.findOne({
+        userId: Meteor.userId()
+      });
+    }
+    return null;
   }
+});
+
+Template.taxSettingsPanel.onCreated(function () {
+  this.autorun(() => {
+    this.subscribe("Account");
+  });
 });
