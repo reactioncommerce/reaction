328b7dc8911cfa0c1e8f2264d456f9ffe0ff0d3b
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2017-02-22 11:13:34 -0800

    lint fixes

--- imports/plugins/included/product-variant/client/templates/products/productSettings/productSettings.js
+++ imports/plugins/included/product-variant/client/templates/products/productSettings/productSettings.js
@@ -189,7 +189,7 @@ Template.productSettings.events({
       // TODO review Template.productSettings events for no-loop-func
       //
       Meteor.call("products/updateProductPosition", product._id, positions, tag,
-        (error) => {
+        (error) => { // eslint-disable-line no-loop-func
           if (error) {
             Logger.warn(error);
             throw new Meteor.Error(403, error);
