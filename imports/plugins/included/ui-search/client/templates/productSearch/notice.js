942b936011a659685ace767d908ab0383b044fcb
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2017-02-22 20:31:31 -0800

    use ReactionProduct

--- imports/plugins/included/ui-search/client/templates/productSearch/notice.js
+++ imports/plugins/included/ui-search/client/templates/productSearch/notice.js
@@ -1,12 +1,51 @@
+// /**
+//  * searchGridNotice helpers
+//  */
+// Template.searchGridNotice.helpers({
+//   isLowQuantity: function () {
+//     return this.isLowQuantity;
+//   },
+//   isSoldOut: function () {
+//     return this.isSoldOut;
+//   },
+//   isBackorder: function () {
+//     return this.isBackorder;
+//   }
+// });
+
+
+
+import { ReactionProduct } from "/lib/api";
+import { Catalog } from "/lib/api";
+
 /**
  * searchGridNotice helpers
  */
 Template.searchGridNotice.helpers({
   isLowQuantity: function () {
-    return this.isLowQuantity;
+    const topVariants = ReactionProduct.getTopVariants(this._id);
+
+    for (const topVariant of topVariants) {
+      const inventoryThreshold = topVariant.lowInventoryWarningThreshold;
+      const inventoryQuantity = ReactionProduct.getVariantQuantity(topVariant);
+
+      if (inventoryQuantity !== 0 && inventoryThreshold >= inventoryQuantity) {
+        return true;
+      }
+    }
+    return false;
   },
   isSoldOut: function () {
-    return this.isSoldOut;
+    const topVariants = ReactionProduct.getTopVariants(this._id);
+
+    for (const topVariant of topVariants) {
+      const inventoryQuantity = ReactionProduct.getVariantQuantity(topVariant);
+
+      if (inventoryQuantity > 0) {
+        return false;
+      }
+    }
+    return true;
   },
   isBackorder: function () {
     return this.isBackorder;
