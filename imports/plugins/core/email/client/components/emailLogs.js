9dfaef0c17e3bb95b3637a5e54380cb8ffc301a6
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2017-03-01 15:38:42 -0800

    customize griddle columns for data display

--- imports/plugins/core/email/client/components/emailLogs.js
+++ imports/plugins/core/email/client/components/emailLogs.js
@@ -1,17 +1,17 @@
 import React, { Component, PropTypes } from "react";
 import moment from "moment";
-import { Card, CardHeader, CardBody, CardGroup, Loading } from "/imports/plugins/core/ui/client/components";
+import { Card, CardHeader, CardBody, CardGroup, Icon, Loading } from "/imports/plugins/core/ui/client/components";
 import MeteorGriddle from "/imports/plugins/core/ui-grid/client/griddle";
 import { Jobs } from "/lib/collections";
 import { i18next } from "/client/api";
+import "./emailConfig.css";
 
 
 class EmailLogs extends Component {
 
   renderEmailsTable() {
-    const filteredFields = ["data.to", "data.subject"];
-    const filteredFieldsColumns = ["data.to", "data.subject", "resend"];
-    // const filteredFields = ["data.to", "data.subject", "status"];
+    const filteredFields = ["data.to", "updated", "data.subject", "status"];
+    const filteredFieldsColumns = ["data.to", "updated", "data.subject", "status"];
     const noDataMessage = i18next.t("admin.logs.noEmails");
 
 
@@ -23,15 +23,61 @@ class EmailLogs extends Component {
     };
 
 
+    const ColumnData = React.createClass({
+
+      handleAction() {
+        const emailId = this.props.rowData._id;
+        const emailAddress = this.props.rowData.data.to;
+
+        Meteor.call("emails/retryFailed", emailId, (err) => {
+          if (err) {
+            return Alerts.toast(i18next.t("app.error", { error: err.reason }), "error");
+          }
+          return Alerts.toast(i18next.t("mail.alerts.resendSuccess", { email: emailAddress }), "success");
+        });
+      },
+      render() {
+        const renderColumn = this.props.metadata.columnName;
+
+        if (renderColumn === "status") {
+          if (this.props.data === "completed") {
+            return (
+              <span>
+                <Icon icon="fa fa-circle" className="pull-left valid" />
+              </span>
+            );
+          }
+          return (
+            <span>
+              <Icon icon="fa fa-circle" className="pull-left error" />
+              <span onClick={this.handleAction} title={this.props.data}><Icon icon="fa fa-share" className="pull-right" /></span>
+            </span>
+          );
+        }
+        if (renderColumn === "updated") {
+          const createdDate = moment(this.props.data).format("LLL");
+          return (
+            <span>{createdDate}</span>
+          );
+        }
+        return (
+          <span>{this.props.data}</span>
+        );
+      }
+    });
+
+
     // add i18n handling to headers
     const customColumnMetadata = [];
     filteredFields.forEach(function (field) {
       const columnMeta = {
         columnName: field,
-        displayName: i18next.t(`admin.logs.headers.${field}`)
+        displayName: i18next.t(`admin.logs.headers.${field}`),
+        customComponent: ColumnData
       };
       customColumnMetadata.push(columnMeta);
     });
+
 
     return (
       <MeteorGriddle
@@ -44,6 +90,7 @@ class EmailLogs extends Component {
         filteredFields={filteredFields}
         columns={filteredFieldsColumns}
         noDataMessage={noDataMessage}
+        columnMetadata={customColumnMetadata}
         externalLoadingComponent={Loading}
       />
     );
