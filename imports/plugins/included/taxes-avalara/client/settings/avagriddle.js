b7f9c7d0f0fc274978f5dc19d2864262027b1155
Brent Hoover <brent@thebuddhalodge.com>
2017-03-03 11:35:29 +0800

    Adjust log details for when there is no document type

--- imports/plugins/included/taxes-avalara/client/settings/avagriddle.js
+++ imports/plugins/included/taxes-avalara/client/settings/avagriddle.js
@@ -1,4 +1,5 @@
 /* eslint react/prop-types:0, react/jsx-sort-props:0, react/forbid-prop-types: 0, "react/prefer-es6-class": [1, "never"] */
+import _ from "lodash";
 import React from "react";
 import moment from "moment";
 import Griddle from "griddle-react";
@@ -21,29 +22,31 @@ const LogGriddle = React.createClass({
       externalResultsPerPage: this.props.externalResultsPerPage,
       externalSortColumn: this.props.externalSortColumn,
       externalSortAscending: this.props.externalSortAscending
-    }
+    };
   },
 
   getMeteorData() {
     const matchingResults = Counts.get(this.props.matchingResultsCount);
     const pubHandle = Meteor.subscribe(this.props.publication, this.props.subscriptionParams);
     const rawResults = this.props.collection.find({}).fetch();
-    const results = rawResults.map((o) => {
-      return {
-        date: moment(o.data).format("MM/DD/YYYY HH:mm:ss"),
-        docType: o.data.request.data.type,
-        request: JSON.stringify(o.data.request),
-        result: JSON.stringify(o.data.result),
-        _id: o._id
-      }
-    });
-
+    let results;
+    if (rawResults) {
+      results = rawResults.map((o) => {
+        return {
+          date: moment(o.data).format("MM/DD/YYYY HH:mm:ss"),
+          docType: _.get(o, "data.request.data.type", ""),
+          request: JSON.stringify(o.data.request),
+          result: JSON.stringify(o.data.result),
+          _id: o._id
+        };
+      });
+    }
 
     return {
       loading: !pubHandle.ready(),
       results,
       matchingResults
-    }
+    };
   },
 
   setPage(index) {
