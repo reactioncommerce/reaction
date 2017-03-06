dea6a03f5ab3b96eff5a9d6649534df04b846d50
joykare <joy.warugu@andela.com>
2017-03-06 21:45:36 +0300

    Add single invoice render

--- imports/plugins/core/orders/client/components/lineItems.js
+++ imports/plugins/core/orders/client/components/lineItems.js
@@ -1,14 +1,16 @@
 import React, { Component, PropTypes } from "react";
+import { Translation } from "/imports/plugins/core/ui/client/components";
 
 class LineItems extends Component {
   render() {
     const items = this.props.items;
-
+    console.log(this.props);
     return (
       <div>
-        <div className="order-items" onClick={this.props.handleClick}>
+        <div className="order-items">
           {items.map((item) => (
-            <div className="order-item form-group order-summary-form-group" key={item._id}>
+            <div>
+            <div className="invoice order-item form-group order-summary-form-group" key={item._id} onClick={this.props.handleClick}>
               <div className="order-item-media">
                 { !this.props.displayMedia(item.variants) &&
                   <img src= "/resources/placeholder.gif" />
@@ -26,6 +28,38 @@ class LineItems extends Component {
                 </div>
               </div>
             </div>
+              {this.props.isExpanded &&
+                <div>
+                  <div className="order-summary-form-group">
+                    <strong><Translation defaultValue="Subtotal" i18nKey="cartSubTotals.subtotal"/></strong>
+                    <div className="invoice-details">
+                      0
+                    </div>
+                  </div>
+
+                  <div className="order-summary-form-group">
+                    <strong><Translation defaultValue="Shipping" i18nKey="cartSubTotals.shipping"/></strong>
+                    <div className="invoice-details">
+                      0
+                    </div>
+                  </div>
+
+                  <div className="order-summary-form-group">
+                    <strong><Translation defaultValue="Tax" i18nKey="cartSubTotals.tax"/></strong>
+                    <div className="invoice-details">
+                      0
+                    </div>
+                  </div>
+
+                  <div className="order-summary-form-group">
+                    <strong><Translation defaultValue="Tax codes"/></strong>
+                    <div className="invoice-details">
+                      0
+                    </div>
+                  </div>
+                </div>
+              }
+            </div>
           ))}
         </div>
       </div>
@@ -36,6 +70,7 @@ class LineItems extends Component {
 LineItems.propTypes = {
   displayMedia: PropTypes.func,
   handleClick: PropTypes.func,
+  isExpanded: PropTypes.bool,
   items: PropTypes.array
 };
 
