338d3066f33021ab8baeee9d675bad28f8fd23ba
joykare <joy.warugu@andela.com>
2017-03-06 19:05:58 +0300

    Add roll over list capability

---
+++ imports/plugins/core/orders/client/containers/lineItemsContainer.js
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from "react";
+import { composeWithTracker } from "/lib/api/compose";
+// import { Reaction } from "/client/api";
+import { Loading } from "/imports/plugins/core/ui/client/components";
+import { TranslationProvider } from "/imports/plugins/core/ui/client/providers";
+import LineItems from "../components/lineItems.js";
+import { Media } from "/lib/collections";
+
+class LineItemsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDisplayMedia = this.handleDisplayMedia.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    console.log("Hey there");
+  }
+
+  handleDisplayMedia(variantObject) {
+    const defaultImage = Media.findOne({
+      "metadata.variantId": variantObject._id,
+      "metadata.priority": 0
+    });
+
+    if (defaultImage) {
+      return defaultImage;
+    }
+    return false;
+  }
+
+  render() {
+    return (
+      <TranslationProvider>
+        <LineItems
+          displayMedia={this.handleDisplayMedia}
+          handleClick={this.handleClick}
+          items={this.props.items}
+        />
+      </TranslationProvider>
+    );
+  }
+}
+
+LineItemsContainer.propTypes = {
+  items: PropTypes.array
+};
+
+const composer = (props, onData) => {
+  onData(null, {
+    items: props.items
+  });
+};
+
+export default composeWithTracker(composer, Loading)(LineItemsContainer);
