99ba702a7023d49d9edd95270b9c76c4428344d4
joykare <joy.warugu@andela.com>
2017-03-06 08:16:06 +0300

    Add state to the invoice view

--- imports/plugins/core/orders/client/templates/workflow/shippingInvoice.js
+++ imports/plugins/core/orders/client/templates/workflow/shippingInvoice.js
@@ -7,7 +7,7 @@ import { i18next, Logger, formatNumber, Reaction } from "/client/api";
 import { NumericInput } from "/imports/plugins/core/ui/client/components";
 import { Media, Orders, Shops } from "/lib/collections";
 import DiscountList from "/imports/plugins/core/discounts/client/components/list";
-import Invoice from "../../components/invoice.js";
+import InvoiceContainer from "../../containers/invoiceContainer.js";
 
 // helper to return the order payment object
 // the first credit paymentMethod on the order
@@ -46,8 +46,8 @@ Template.coreOrderShippingInvoice.helpers({
   DiscountList() {
     return DiscountList;
   },
-  Invoice() {
-    return Invoice;
+  InvoiceContainer() {
+    return InvoiceContainer;
   },
   orderId() {
     const instance = Template.instance();
