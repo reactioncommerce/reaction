e43c425dc1af3fc5ce10ba2b7ef0706c323dce82
joykare <joy.warugu@andela.com>
2017-03-06 08:16:06 +0300

    Add state to the invoice view

--- imports/plugins/core/orders/client/containers/invoiceContainer.js
+++ imports/plugins/core/orders/client/containers/invoiceContainer.js
@@ -1,21 +1,28 @@
-// import React from "react";
-// import { composeWithTracker } from "/lib/api/compose";
+import React, { Component } from "react";
+import { composeWithTracker } from "/lib/api/compose";
 // import { Reaction } from "/client/api";
-// import { Loading } from "/imports/plugins/core/ui/client/components";
-// import { TranslationProvider } from "/imports/plugins/core/ui/client/providers";
-//
-// /**
-//  * handleShowPackage - Push package into action view navigation stack
-//  * @param  {SyntheticEvent} event Original event
-//  * @param  {Object} app Package data
-//  * @return {undefined} No return value
-//  */
-// function handleShowPackage(event, app) {
-//   Reaction.pushActionView(app);
-// }
-//
-// function composer(props, onData) {
-//
-// }
-//
-// return composeWithTracker(composer, Loading)(CompositeComponent);
+import { Loading } from "/imports/plugins/core/ui/client/components";
+import { TranslationProvider } from "/imports/plugins/core/ui/client/providers";
+import { Orders } from "/lib/collections";
+import Invoice from "../components/invoice.js";
+
+class InvoiceContainer extends Component {
+  render() {
+    return (
+      <TranslationProvider>
+        <Invoice />
+      </TranslationProvider>
+    );
+  }
+}
+
+const composer = ({}, onData) => {
+  const subscription = Meteor.subscribe("Orders");
+
+  if (subscription.ready()) {
+    const order = Orders.find();
+    onData(null, { order });
+  }
+};
+
+export default composeWithTracker(composer, Loading)(InvoiceContainer);
