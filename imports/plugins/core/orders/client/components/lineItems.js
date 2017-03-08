536c7193cf53eb764e06a19c7d21e75b9d0e8225
joykare <joy.warugu@andela.com>
2017-03-08 16:02:53 +0300

    Seperate components for roll up lists and line items

--- imports/plugins/core/orders/client/components/lineItems.js
+++ imports/plugins/core/orders/client/components/lineItems.js
@@ -1,18 +1,61 @@
 import React, { Component, PropTypes } from "react";
 import { Translation } from "/imports/plugins/core/ui/client/components";
-import _ from 'lodash';
+import _ from "lodash";
 
 class LineItems extends Component {
   render() {
-    const props = this.props;
-    // console.log("Line items", this.props);
     return (
       <div>
-         <div className="order-items">
-            {props.uniqueItems.map((uniqueItem) => (
+        {this.props.uniqueItems.map((uniqueItem) => {
+          if (!this.props.isExpanded(uniqueItem._id)) {
+            return (
+              <LineItem
+                key={uniqueItem._id}
+                uniqueItem={uniqueItem}
+                quantity={uniqueItem.length}
+                displayMedia={this.props.displayMedia}
+                handleClick={this.props.handleClick}
+              />
+            );
+          }
+          return (
+            <div>
+              <div>
+                <button className="rui btn btn-default flat icon-only" onClick={this.props.onClose}>
+                  <i
+                    className="rui font-icon fa-lg fa fa-times"
+                  />
+                </button>
+              </div>
+              {_.times(uniqueItem.length, () => (
+                <div>
+                  <LineItem
+                    uniqueItem={uniqueItem}
+                    displayMedia={this.props.displayMedia}
+                    handleClick={this.props.handleClick}
+                  />
+                  <InvoiceForm
+                    uniqueItemDetails={uniqueItem.variants}
+                  />
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+class LineItem extends Component {
+  render() {
+    const uniqueItem = this.props.uniqueItem;
+    const quantity = this.props.quantity || 1;
+    return (
+      <div>
+        <div className="order-items">
               <div
                 className="invoice order-item form-group order-summary-form-group"
-                key={uniqueItem._id}
                 onClick={() => this.props.handleClick(uniqueItem._id)}
               >
                 <div className="order-item-media">
@@ -26,7 +69,7 @@ class LineItems extends Component {
                   </div>
                 </div>
                 <div className="order-detail-quantity">
-                  {uniqueItem.length}
+                  {quantity}
                 </div>
                 <div className="order-detail-price">
                   <div className="amount">
@@ -34,48 +77,47 @@ class LineItems extends Component {
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-
-        {/* {props.isExpanded(uniqueItem._id) &&
-              _.times(uniqueItem.length, () => (
-                <div>
-                  <div className="order-summary-form-group">
-                    <strong><Translation defaultValue="Subtotal" i18nKey="cartSubTotals.subtotal"/></strong>
-                    <div className="invoice-details">
-                      0
-                    </div>
-                  </div>
-
-                  <div className="order-summary-form-group">
-                    <strong><Translation defaultValue="Shipping" i18nKey="cartSubTotals.shipping"/></strong>
-                    <div className="invoice-details">
-                      0
-                    </div>
-                  </div>
-
-                  <div className="order-summary-form-group">
-                    <strong><Translation defaultValue="Tax" i18nKey="cartSubTotals.tax"/></strong>
-                    <div className="invoice-details">
-                      0
-                    </div>
-                  </div>
-
-                  <div className="order-summary-form-group">
-                    <strong><Translation defaultValue="Tax codes"/></strong>
-                    <div className="invoice-details">
-                      0
-                    </div>
-                  </div>
-                </div>
-              ))
-            }*/}
+            </div>
       </div>
     );
   }
 }
 
-LineItems.propTypes = {
+class InvoiceForm extends Component {
+  render() {
+    return (
+      <div>
+        <div className="order-summary-form-group">
+          <strong><Translation defaultValue="Subtotal" i18nKey="cartSubTotals.subtotal"/></strong>
+          <div className="invoice-details">
+            0
+          </div>
+        </div>
+        <div className="order-summary-form-group">
+          <strong><Translation defaultValue="Shipping" i18nKey="cartSubTotals.shipping"/></strong>
+          <div className="invoice-details">
+            0
+          </div>
+        </div>
+        <div className="order-summary-form-group">
+          <strong><Translation defaultValue="Tax" i18nKey="cartSubTotals.tax"/></strong>
+          <div className="invoice-details">
+            0
+          </div>
+        </div>
+        <div className="order-summary-form-group">
+          <strong><Translation defaultValue="Tax codes"/></strong>
+          <div className="invoice-details">
+            0
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+LineItem.propTypes = {
+  uniqueItem: PropTypes.object,
   displayMedia: PropTypes.func,
   handleClick: PropTypes.func,
   isExpanded: PropTypes.func,
