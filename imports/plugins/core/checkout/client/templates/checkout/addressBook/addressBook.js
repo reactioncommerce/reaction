a3ec67c49f7d82f58cba7f84d1992acadc0a7771
Seun Martins <seundotme@gmail.com>
2017-03-10 12:19:24 +0100

    Show sidebar only if shipping is not configured

--- imports/plugins/core/checkout/client/templates/checkout/addressBook/addressBook.js
+++ imports/plugins/core/checkout/client/templates/checkout/addressBook/addressBook.js
@@ -1,10 +1,18 @@
 import { Template } from "meteor/templating";
+import { Packages } from "/lib/collections";
 import { Reaction } from "/client/api";
 
 Template.checkoutAddressBook.onCreated(function () {
-  Reaction.showActionView({
-    provides: "settings",
-    name: "settings/shipping",
-    template: "shippingSettings"
+  const shipping = Packages.findOne({
+    name: "reaction-shipping"
   });
+
+  // on address step of checkout show sidebar when shipping is not configured
+  if (!shipping || !shipping.enabled) {
+    Reaction.showActionView({
+      provides: "settings",
+      name: "settings/shipping",
+      template: "shippingSettings"
+    });
+  }
 });
