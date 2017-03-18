2248c8bb69ab651fd7bc96f0b56d1309c3a4f0a2
Abdulsemiu Atanda <semiu@live.com>
2017-03-18 13:11:54 +0100

    refactor cart cleanup job

--- imports/plugins/included/jobcontrol/server/jobs/cart.js
+++ imports/plugins/included/jobcontrol/server/jobs/cart.js
@@ -3,16 +3,22 @@ import moment from "moment";
 import { Accounts, Cart, Jobs } from "/lib/collections";
 import { Hooks, Logger, Reaction } from "/server/api";
 
+const olderThan = moment().subtract(3, "days")._d;
+
 Hooks.Events.add("onJobServerStart", () => {
   Logger.debug("Adding Job removeStaleCart to jobControl");
-  const existingCart = Cart.find({});
-  if (existingCart) {
+  const staleCarts = Cart.find({
+    updatedAt: {
+      $lte: olderThan
+    }
+  }).fetch();
+  if (staleCarts) {
     new Job(Jobs, "cart/removeFromCart", {})
       .priority("normal")
       .retry({
         retries: 5,
         wait: 60000,
-        backoff: "exponential" // delay by twice as long for each subsequent retry rt
+        backoff: "exponential" // delay by twice as long for each subsequent retry
       })
       .repeat({
         schedule: later.parse.text("every day")
@@ -21,7 +27,7 @@ Hooks.Events.add("onJobServerStart", () => {
         cancelRepeats: true
       });
   } else {
-    Logger.warn("Cart does not currently exist");
+    Logger.info("No stale carts at the moment");
   }
 });
 
@@ -30,8 +36,6 @@ export default () => {
     pollInterval: 60 * 60 * 1000, // backup polling, see observer below
     workTimeout: 180 * 1000
   }, (job, callback) => {
-    // TODO: set this interval in the admin UI
-    const olderThan = moment().subtract(3, "days")._d;
     Logger.debug("Processing cart/removeFromCart");
     const cartDetails = Cart.find({
       updatedAt: {
@@ -40,40 +44,38 @@ export default () => {
     }).fetch();
     cartDetails.map(cart => {
       if (cart.items) {
-        const findAcct = Accounts.find({ userId: cart.userId }).fetch();
-        findAcct.map(user => {
-          if (user.emails.length === 0) {
-            const result = Cart.update(
-              { userId: user._id },
-              { $unset: { items: [] } }
-            );
-            if (result === 1) {
-              const success = "Stale anonymous user cart successfully removed";
-              Logger.info(success);
-              job.done(success, { repeatId: true });
-            }
+        const user = Accounts.findOne({ userId: cart.userId });
+        if (!user.emails.length) {
+          const result = Cart.update(
+            { userId: user._id },
+            { $unset: { items: [] } }
+          );
+          if (result === 1) {
+            const success = "Stale anonymous user cart successfully cleaned";
+            Logger.debug(success);
+            job.done(success, { repeatId: true });
+          }
+        } else {
+          const settings = Reaction.getShopSettings();
+          const schedule = Number(settings.cart.cleanup); // configurable in shop settings
+          if (!schedule) {
+            Logger.warn("Cleanup Schedule not configured");
           } else {
-            const settings = Reaction.getShopSettings();
-            const schedule = Number(settings.cart.cleanup); // configurable in shop settings
-            if (!schedule) {
-              Logger.warn("Cleanup Schedule not configured");
-            } else {
-              if (cart.updatedAt <= moment().subtract(schedule, "days")._d) {
-                const userCleanup = Cart.update(
-                  { userId: user._id },
-                  { $unset: { items: [] } }
-                );
-                if (userCleanup === 1) {
-                  const success = "Stale user cart successfully removed";
-                  Logger.info(success);
-                  job.done(success, { repeatId: true });
-                }
+            if (cart.updatedAt <= moment().subtract(schedule, "mins")._d) {
+              const userCleanup = Cart.update(
+                { userId: user._id },
+                { $unset: { items: [] } }
+              );
+              if (userCleanup === 1) {
+                const success = "Stale user cart successfully cleaned";
+                Logger.debug(success);
+                job.done(success, { repeatId: true });
               }
             }
           }
-        });
+        }
       } else {
-        Logger.info("Cart is currently empty");
+        Logger.debug("Cart is currently empty");
       }
     });
     callback();
