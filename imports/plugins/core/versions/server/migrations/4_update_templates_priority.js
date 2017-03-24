0ff8442b01af050f1af8a31d98751a26fca5cd88
Seun Martins <seundotme@gmail.com>
2017-03-24 19:56:32 +0100

    Check before updating during migration

--- imports/plugins/core/versions/server/migrations/4_update_templates_priority.js
+++ imports/plugins/core/versions/server/migrations/4_update_templates_priority.js
@@ -1,6 +1,5 @@
 import { Migrations } from "/imports/plugins/core/versions";
 import { Packages } from "/lib/collections";
-import { Reaction } from "/server/api/";
 
 Migrations.add({
   version: 4,
@@ -9,21 +8,21 @@ Migrations.add({
     const packages = Packages.find({ layout: { $type: 3 } }).fetch();
 
     packages.forEach((pkg) => {
+      let changed = false; // to track if updating is needed
+
       pkg.layout.forEach((layout) => {
         if (layout.priority === 1) {
           layout.priority = 999;
+          changed = true;
         }
       });
 
-      if (pkg.layout.length < 6) {
+      if (changed) {
         Packages.update(pkg._id, {
           $set: { layout: pkg.layout }
         });
       }
     });
-
-    Reaction.loadPackages();
-    Reaction.Import.flush();
   }
 });
 
