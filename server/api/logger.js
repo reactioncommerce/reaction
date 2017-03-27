165a8d5a9617282cf673a72b18195b74fb7b1a98
Seun Martins <seundotme@gmail.com>
2017-03-27 18:40:03 +0100

    Use JSON format for trace log level

--- server/api/logger.js
+++ server/api/logger.js
@@ -6,16 +6,10 @@ import { includes } from "lodash";
 // configure bunyan logging module for reaction server
 // See: https://github.com/trentm/node-bunyan#levels
 const levels = ["FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"];
-let logFormat = "short";
-
-// default console config (stdout)
-const defaultStream = {
-  level: "INFO",
-  stream: bunyanFormat({ outputMode: "short" })
-};
 
 // set stdout log level
 let level = process.env.REACTION_LOG_LEVEL || Meteor.settings.REACTION_LOG_LEVEL || "INFO";
+let outputMode = "short";
 
 level = level.toUpperCase();
 
@@ -24,12 +18,13 @@ if (!includes(levels, level)) {
 }
 
 if (level === "TRACE") {
-  logFormat = "json";
+  outputMode = "json";
 }
 
-const streams = [defaultStream, {
+// default console config (stdout)
+const streams = [{
   level,
-  stream: bunyanFormat({ outputMode: logFormat })
+  stream: bunyanFormat({ outputMode })
 }];
 
 // Loggly config (only used if configured)
