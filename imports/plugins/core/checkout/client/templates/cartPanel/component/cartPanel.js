b9edeba772b5a54342fe91dd0dbb73a933666bc5
Abdulsemiu Atanda <semiu@live.com>
2017-04-05 21:42:37 +0100

    fix linting issue

--- imports/plugins/core/checkout/client/templates/cartPanel/component/cartPanel.js
+++ imports/plugins/core/checkout/client/templates/cartPanel/component/cartPanel.js
@@ -4,7 +4,7 @@ class CartPanel extends Component {
   render() {
     return (
       <div>
-        <div className="cart-alert-text"></div>
+        <div className="cart-alert-text">{}</div>
         <div className="cart-alert-checkout">
           <span className="btn btn-success btn-lg btn-block" id="btn-checkout" data-i18n="cartDrawer.checkout"
             onClick={this.props.onClick || this.props.checkout}
