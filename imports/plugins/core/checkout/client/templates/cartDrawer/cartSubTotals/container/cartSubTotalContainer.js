acb614aabfd3aca65668fe4a48ff25f324ddec21
Abdulsemiu Atanda <semiu@live.com>
2017-04-05 19:44:07 +0100

    remove commented code

--- imports/plugins/core/checkout/client/templates/cartDrawer/cartSubTotals/container/cartSubTotalContainer.js
+++ imports/plugins/core/checkout/client/templates/cartDrawer/cartSubTotals/container/cartSubTotalContainer.js
@@ -5,31 +5,6 @@ import { Loading } from "/imports/plugins/core/ui/client/components";
 import CartSubTotal from "../component/cartSubTotal";
 
 class CartSubTotalContainer extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     cartSubTotal: "0",
-  //     cartCount: 0,
-  //     cartShipping: "0",
-  //     cartDiscount: "0",
-  //     cartTaxes: "0",
-  //     cartTotal: "0"
-  //   };
-  // }
-  // componentWillMount() {
-  //   const cart = Cart.findOne();
-  //   if (cart) {
-  //     this.setState({
-  //       cartSubTotal: cart.cartSubTotal(),
-  //       cartCount: cart.cartCount(),
-  //       cartShipping: cart.cartShipping(),
-  //       cartDiscount: cart.cartDiscounts(),
-  //       cartTaxes: cart.cartTaxes(),
-  //       cartTotal: cart.cartTotal()
-  //     });
-  //   }
-  // }
-
   render() {
     return (
       <CartSubTotal
