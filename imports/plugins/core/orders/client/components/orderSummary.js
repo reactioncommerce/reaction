bc9fd4b5e1d771b3e4c0c8a77e8912bb785c0d29
joykare <joy.warugu@andela.com>
2017-04-12 17:52:08 +0300

    Remove gap at the top

--- imports/plugins/core/orders/client/components/orderSummary.js
+++ imports/plugins/core/orders/client/components/orderSummary.js
@@ -14,7 +14,7 @@ class OrderSummary extends Component {
 
     return (
       <div>
-        <div className="order-summary-form-group bg-info" style={{ lineHeight: 3, marginRight: -15, marginLeft: -15 }}>
+        <div className="order-summary-form-group bg-info" style={{ lineHeight: 3, marginTop: -15, marginRight: -15, marginLeft: -15 }}>
           <strong style={{ marginLeft: 15 }}>{profile.fullName}</strong> , {profile.country}
           <div className="invoice-details" style={{ marginRight: 15 }}>
             <strong>ID </strong>{order._id}
