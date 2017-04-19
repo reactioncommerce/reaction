328ec7ecad45f9105391d0fe7bd282f56c76df35
Abdulsemiu Atanda <semiu@live.com>
2017-04-19 15:25:18 +0100

    fix spacing and swiper issue

--- imports/plugins/core/checkout/client/components/cartItems.js
+++ imports/plugins/core/checkout/client/components/cartItems.js
@@ -1,47 +1,41 @@
 import React, { PropTypes } from "react";
 
-const CartItems = ({ handleLowInventory, pdpPath, handleImage, handleRemoveItem, items }) => {
+const CartItems = ({ handleLowInventory, pdpPath, handleImage, handleRemoveItem, item }) => {
   return (
-    <div>
-      {items.map((item) => {
-        return (
-          <div className="cart-items" key={item._id} style={{ display: "inline-block" }}>
-            <i className="remove-cart-item fa fa-times fa-lg"
-              id={item._id}
-              onClick={handleRemoveItem}
-            />
-            <a href={pdpPath(item)}
-              data-event-action="product-click"
-              data-event-value={item.productId}
-            >
-              {handleImage(item) ?
-                <div className="center-cropped" style={{ backgroundImage: `url(${handleImage(item).url({ store: "small" })})` }}>
-                  <img src={handleImage(item).url({ store: "small" })} className="product-grid-item-images img-responsive" />
-                </div> :
-                <div className="center-cropped" style={{ backgroundImage: "url('/resources/placeholder.gif')" }}>
-                  <img src="/resources/placeholder.gif" className="product-grid-item-images img-responsive" />
-                </div>
-              }
-            </a>
-            <div className="cart-labels">
-              {handleLowInventory(item) ?
-              <div className="badge badge-low-inv-warning"
-                title={item.variants.inventoryQuantity}
-                data-i18n="cartDrawerItems.left"
-              >!</div> :
-            <div>
-            <span className="badge">{item.quantity}</span>
+    <div className="cart-items" key={item._id} style={{ display: "inline-block" }}>
+      <i className="remove-cart-item fa fa-times fa-lg"
+        id={item._id}
+        onClick={handleRemoveItem}
+      />
+      <a href={pdpPath(item)}
+        data-event-action="product-click"
+        data-event-value={item.productId}
+      >
+        {handleImage(item) ?
+          <div className="center-cropped" style={{ backgroundImage: `url(${handleImage(item).url({ store: "small" })})` }}>
+            <img src={handleImage(item).url({ store: "small" })} className="product-grid-item-images img-responsive" />
+          </div> :
+          <div className="center-cropped" style={{ backgroundImage: "url('/resources/placeholder.gif')" }}>
+            <img src="/resources/placeholder.gif" className="product-grid-item-images img-responsive" />
+          </div>
+        }
+      </a>
+      <div className="cart-labels">
+        {handleLowInventory(item) ?
+          <div className="badge badge-low-inv-warning"
+            title={item.variants.inventoryQuantity}
+            data-i18n="cartDrawerItems.left"
+          >!</div> :
+          <div>
+            <span className="badge" style={{ marginRight: "3px" }}>{item.quantity}</span>
             <span className="cart-item-title">
               {item.title}
               <br />
               <small>{item.variants.title}</small>
             </span>
-            </div>
-             }
-            </div>
           </div>
-        );
-      })}
+        }
+      </div>
     </div>
   );
 };
@@ -50,7 +44,7 @@ CartItems.propTypes = {
   handleImage: PropTypes.func,
   handleLowInventory: PropTypes.func,
   handleRemoveItem: PropTypes.func,
-  items: PropTypes.array,
+  item: PropTypes.object,
   pdpPath: PropTypes.func
 };
 
--- imports/plugins/core/checkout/client/container/cartDrawerContainer.js
+++ imports/plugins/core/checkout/client/container/cartDrawerContainer.js
@@ -1,37 +1,98 @@
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
 import { Session } from "meteor/session";
+import { Cart, Media } from "/lib/collections";
 import { Reaction } from "/client/api";
-import CartSubTotals from "../container/cartSubTotalContainer";
-import CartItems from "../container/cartItemContainer";
+import { composeWithTracker } from "/lib/api/compose";
+import { Loading } from "/imports/plugins/core/ui/client/components";
+import CartDrawer from "../components/cartDrawer";
 
 class CartDrawerContainer extends Component {
+  static propTypes = {
+    defaultImage: PropTypes.object,
+    lowInventory: PropTypes.bool,
+    productItems: PropTypes.array
+  }
+
+  handleImage(item) {
+    const { defaultImage } = item;
+    if (defaultImage && defaultImage.url({ store: "small" })) {
+      return defaultImage;
+    }
+    return false;
+  }
+  /**
+  * showLowInventoryWarning
+  * @param {Object} productItem - product item object
+  * @return {Boolean} return true if low inventory on variant
+  */
+  showItemLowInventoryWarning(productItem) {
+    const { variants } = productItem;
+    if (variants && variants.inventoryPolicy &&
+      variants.lowInventoryWarningThreshold) {
+      return variants.inventoryQuantity <=
+        variants.lowInventoryWarningThreshold;
+    }
+    return false;
+  }
+
+  handleLowInventory = (productItem) => {
+    return this.showItemLowInventoryWarning(productItem);
+  }
+
+  pdpPath(productItem) {
+    if (productItem) {
+      const handle = productItem.productId;
+      return Reaction.Router.pathFor("product", {
+        hash: {
+          handle,
+          variantId: productItem.variants._id
+        }
+      });
+    }
+  }
+
+  handleRemoveItem(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const currentCartItemId = event.target.getAttribute("id");
+    $(`#${currentCartItemId}`).fadeOut(500, () => {
+      return Meteor.call("cart/removeFromCart", currentCartItemId);
+    });
+  }
   handleCheckout() {
     $("#cart-drawer-container").fadeOut();
     Session.set("displayCart", false);
     return Reaction.Router.go("cart/checkout");
   }
   render() {
+    const { productItems } = this.props;
     return (
-      <div>
-        <div className="cart-drawer-swiper-container">
-          <div className="cart-drawer-swiper-wrapper">
-            <div className="cart-drawer-swiper-slide">
-              <CartSubTotals />
-            </div>
-            <div>
-              <CartItems />
-            </div>
-          </div>
-        </div>
-        <div className="cart-drawer-pagination">{}</div>
-        <div className="row">
-          <span className="rui btn btn-cta btn-lg btn-block" id="btn-checkout" data-i18n="cartDrawer.checkout" onClick={this.handleCheckout}>
-            Checkout now
-          </span>
-        </div>
-      </div>
+      <CartDrawer
+        productItems={productItems}
+        pdpPath={this.pdpPath}
+        handleLowInventory={this.handleLowInventory}
+        handleImage={this.handleImage}
+        handleRemoveItem={this.handleRemoveItem}
+        handleCheckout={this.handleCheckout}
+      />
     );
   }
 }
 
-export default CartDrawerContainer;
+function composer(props, onData) {
+  const userId = Meteor.userId();
+  const shopId = Reaction.getShopId();
+  let productItems = Cart.findOne({ userId, shopId }).items;
+
+  productItems = productItems.map((item) => {
+    const defaultImage = Media.findOne({
+      "metadata.variantId": item.variants._id
+    });
+    return Object.assign({}, item, { defaultImage });
+  });
+  onData(null, {
+    productItems
+  });
+}
+
+export default composeWithTracker(composer, Loading)(CartDrawerContainer);
