5e3a9c3502d31c3fa346066138c6ec43668f9c10
Mike Murray <hellomikemurray@gmail.com>
2017-04-27 12:05:35 -0700

    Fix broken import

--- imports/plugins/core/layout/client/components/index.js
+++ imports/plugins/core/layout/client/components/index.js
@@ -1,6 +1,5 @@
-export { default as Admin } from "./admin";
+export { default as CoreLayout } from "./coreLayout";
 export { default as QuickMemu } from "./quickMenu";
 export { default as Header } from "./header";
 export { default as CartDrawer } from "./cartDrawer";
-// export { default as Toolbar } from "./toolbar";
 export { default as Content } from "./content";
