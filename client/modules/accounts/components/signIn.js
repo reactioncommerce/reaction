11df14195f42096733a4acfb3b8c0a50e0fc8f97
Njeri Kieha <sylvia.kieha@andela.com>
2017-05-04 21:27:30 +0300

    Replicate look of sign in form

--- client/modules/accounts/components/signIn.js
+++ client/modules/accounts/components/signIn.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import {
+  Button,
+  TextField
+} from "/imports/plugins/core/ui/client/components";
 
 class SignIn extends Component {
 
@@ -9,7 +13,53 @@ class SignIn extends Component {
   render() {
     return (
       <div>
-				Hello
+        <div className="loginForm-title">
+          <h2 data-i18n="accountsUI.signIn">Sign In</h2>
+        </div>
+
+        <form className="login-form" noValidate>
+
+          <div className="form-group form-group-email {{hasError messages.errors.email}}">
+            <TextField
+              i18nKeyLabel="accountsUI.emailAddress"
+              label="Email"
+              className="form-control login-input-email"
+              type="email"
+              tabIndex="1"
+              id="email-{{uniqueId}}"
+            />
+          </div>
+
+          <div className="form-group {{hasError messages.errors.password }}">
+            <TextField
+              i18nKeyLabel="accountsUI.password"
+              label="Password"
+              className="form-control login-input-password"
+              type="password"
+              tabIndex="2"
+              id="password-{{uniqueId}}"
+            />
+          </div>
+
+          <div className="form-group">
+            <Button
+              className="btn-block"
+              primary={true}
+              bezelStyle="solid"
+              i18nKeyLabel="accountsUI.signIn"
+              label="Sign In"
+              type="submit"
+              tabIndex="3"
+              eventAction="submitSignInForm"
+            />
+          </div>
+
+          <div className="form-group flex flex-justify-spaceBetween">
+            <a data-i18n="accountsUI.forgotPassword" href tabIndex="4" data-event-action="forgotPassword">Reset Password</a>
+            <a data-i18n="accountsUI.signUp" href tabIndex="5" data-event-action="signUp">Register</a>
+          </div>
+
+        </form>
       </div>
     );
   }
