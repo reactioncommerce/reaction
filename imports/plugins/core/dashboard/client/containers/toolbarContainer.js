e8151a1230217adc0a2e4fb4baf50a66412e1896
Seun Martins <seundotme@gmail.com>
2017-05-09 19:59:10 +0100

    Fix permissions for items shown in admin dashboard (#2145)
    * Initial test of filtering pkgs
    
    * Filter showing packages by permissions
    
    * Resolve issue with hasAccess check on registry items
    
    * Comment
    
    * Comment edit
    
    * Call packages coll without filter
    
    * Move audience addition to ReactionApps
    
    * Better comment
    
    * Check not needed again.Apps come based on access level
    
    * space fix
    
    * Test: Permissions on toolbar component
    
    * hasProduct access on toolbar
    
    * Fix data
    
    * Use createProduct as filter
    
    * Refactor toolbar permission check
    
    * Improve Comment
    
    * Add missing perms in pub
    
    * remove comment
    
    * Add comment on filter line

--- imports/plugins/core/dashboard/client/containers/toolbarContainer.js
+++ imports/plugins/core/dashboard/client/containers/toolbarContainer.js
@@ -88,6 +88,7 @@ function composer(props, onData) {
     isEnabled: isRevisionControlEnabled(),
     isActionViewAtRootView: Reaction.isActionViewAtRootView(),
     actionViewIsOpen: Reaction.isActionViewOpen(),
+    hasCreateProductAccess: Reaction.hasPermission("createProduct", Meteor.userId(), Reaction.shopId),
 
     // Callbacks
     onAddProduct: handleAddProduct,
