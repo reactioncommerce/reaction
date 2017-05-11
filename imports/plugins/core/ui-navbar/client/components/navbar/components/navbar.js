054a08c2172f874d76833081991ea6d699bbf348
joykare <joy.warugu@andela.com>
2017-05-11 12:34:46 +0300

    add component in navbar

--- imports/plugins/core/ui-navbar/client/components/navbar/components/navbar.js
+++ imports/plugins/core/ui-navbar/client/components/navbar/components/navbar.js
@@ -1,27 +1,22 @@
 import React, { Component } from "react";
+import BrandContainer from "../containers/brandContainer";
 
 class NavBar extends Component {
   render() {
     return (
       <div>
-         <div class="showmenu">{{> button icon="bars" onClick=onMenuButtonClick}}</div>
-    {{> coreNavigationBrand}}
-    <div class="menu">
-      {{> tagNav tagNavProps}}
-    </div>
+        <BrandContainer />
 
-    {{#if isSearchEnabled}}
-      <div class="search">{{> React IconButtonComponent}}</div>
-    {{/if}}
-    {{#if hasPermission 'account/profile'}}
-      <div>{{> React notificationButtonComponent }}</div>
-    {{/if}}
-    <div class="languages hidden-xs">{{> i18nChooser}}</div>
-    <div class="currencies hidden-xs">{{> currencySelect}}</div>
-    <div class="accounts">{{> accounts tpl="loginDropdown"}}</div>
-    <div class="cart">{{> cartIcon}}</div>
-    <div class="cart-alert">{{> React component=cartPanel}}</div>
-  </div>
+        {{#if isSearchEnabled}}
+          <div class="search"><IconButtonComponent /></div>
+        {{/if}}
+        {{#if hasPermission 'account/profile'}}
+          <div><notificationButtonComponent /></div>
+        {{/if}}
+
+        <div class="accounts">{{> accounts tpl="loginDropdown"}}</div>
+        <div class="cart">{{> cartIcon}}</div>
+        <div class="cart-alert"><cartPanel/></div>
       </div>
     );
   }
