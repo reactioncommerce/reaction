10a8d8285d0a0be71a38ed2b7c720fdc5130248f
Seun Martins <seundotme@gmail.com>
2017-05-11 02:51:01 +0100

    Show Base tagList

--- imports/plugins/core/ui-tagnav/client/components/tagTree.js
+++ imports/plugins/core/ui-tagnav/client/components/tagTree.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from "react";
 import { TagItem } from "/imports/plugins/core/ui/client/components/tags/";
+import { TagHelpers } from "/imports/plugins/core/ui-tagnav/client/helpers";
 
 class TagTree extends Component {
   static propTypes = {
@@ -10,18 +11,81 @@ class TagTree extends Component {
     subTagGroups: PropTypes.arrayOf(PropTypes.object)
   }
 
+  get className() {
+    if (this.props.blank) {
+      return "create";
+    }
+  }
+
   suggestions = [{}]
 
   newTag = {
     name: ""
   }
 
+  tagListProps(groupTag) {
+    return {
+      parentTag: groupTag,
+      tags: TagHelpers.subTags(groupTag),
+      editable: this.props.editable,
+      onTagCreate(tagName) {
+        if (this.props.onTagCreate) {
+          this.props.onTagCreate(tagName, groupTag);
+        }
+      },
+      onTagRemove(tag) {
+        this.props.onTagRemove(tag, groupTag);
+      },
+      onTagSort(newTagsOrder) {
+        this.props.onTagSort(newTagsOrder, groupTag);
+      },
+      onTagDragAdd: this.props.onTagDragAdd,
+      onTagUpdate: this.props.onTagUpdate
+    };
+  }
+
+  tagsGen(tags) {
+    if (_.isArray(tags)) {
+      return tags.map((tag, index) => {
+        return (
+          <TagItem
+            data-id={tag._id}
+            editable={this.props.editable}
+            index={index}
+            isSelected={this.isSelected}
+            key={index}
+            draggable={true}
+            onClearSuggestions={this.handleClearSuggestions}
+            onGetSuggestions={this.handleGetSuggestions}
+            onMove={this.handleMoveTag}
+            onTagInputBlur={this.handleTagSave}
+            onTagMouseOut={this.handleTagMouseOut}
+            onTagMouseOver={this.handleTagMouseOver}
+            onTagRemove={this.handleTagRemove}
+            onTagSelect={this.onTagSelect}
+            selectable={true}
+            onTagUpdate={this.handleTagUpdate}
+            suggestions={this.suggestions}
+            tag={tag}
+          />
+        );
+      });
+    }
+  }
+
+  renderTagList(props) {
+    return (
+      <div className="rui tags" data-id={props.parentTag._id}>
+        {this.tagsGen(props.tags)}
+      </div>
+    );
+  }
+
   renderSubTagGroups(subTagGroups) {
     if (_.isArray(subTagGroups)) {
-      console.log({ text: subTagGroups[0] });
       return subTagGroups.map((groupTag, index) => (
-        <div className="rui grouptag {{className}}" data-id={groupTag._id} key={groupTag._id}>
-          <div className="content">
+        <div className={`rui grouptag ${this.className}`} data-id={groupTag._id} key={groupTag._id}>
+          <div className="header">
             <TagItem
               className="js-tagNav-item"
               editable={this.props.editable}
@@ -36,6 +100,9 @@ class TagTree extends Component {
               suggestions={this.suggestions}
               tag={groupTag}
             />
+          </div>
+          <div className="content">
+            {this.renderTagList(this.tagListProps(groupTag))}
           </div>
           { this.props.editable &&
             <div className="rui grouptag create">
