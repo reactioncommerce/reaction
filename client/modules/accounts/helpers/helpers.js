c6612e624b08107546c47227b5c5e222ef0b5cb3
Njeri Kieha <sylvia.kieha@andela.com>
2017-05-11 17:29:19 +0300

    Put back helper methods needed by other templates

--- client/modules/accounts/helpers/helpers.js
+++ client/modules/accounts/helpers/helpers.js
@@ -1,3 +1,4 @@
+import { Template } from "meteor/templating";
 import {
   ForgotContainer
 } from "../containers";
@@ -5,5 +6,17 @@ import {
 export const LoginFormSharedHelpers = {
   forgotComponent() {
     return ForgotContainer;
+  },
+
+  messages: function () {
+    return Template.instance().formMessages.get();
+  },
+
+  hasError(error) {
+    // True here means the field is valid
+    // We're checking if theres some other message to display
+    if (error !== true && typeof error !== "undefined") {
+      return "has-error has-feedback";
+    }
   }
 };
