de4ee46f7ea17ce645fa5b8352dd909e96473aed
joykare <joy.warugu@andela.com>
2017-05-12 12:49:54 +0300

    Change acoounts dropdown to react

---
+++ client/modules/accounts/components/dropdown/loginDropdown.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginDropDown extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default LoginDropDown;
---
+++ client/modules/accounts/containers/dropdown/loginDropdownContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginDropDownContainer extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default LoginDropDownContainer;
