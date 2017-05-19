88ddd74310bcb6a52fbcd7ff54db43ec3b94cd46
michael-alade <aladekolawole15@yahoo.com>
2017-05-19 13:07:05 +0100

    make use of reaction addRolesToDefaultRoleSet function to add new roles to the shop

--- imports/plugins/core/accounts/server/init.js
+++ imports/plugins/core/accounts/server/init.js
@@ -1,15 +1,9 @@
-import { Reaction, Hooks, Logger } from "/server/api";
-import { Shops } from "/lib/collections";
-
-function addRolesToVisitors() {
-    // Add the about permission to all default roles since it's available to all
-  Logger.debug("Adding account/verify route permissions to default roles");
-  const shop = Shops.findOne(Reaction.getShopId());
-  Shops.update(shop._id, {
-    $addToSet: { defaultRoles: "account/verify" }
-  });
-}
+import { Reaction, Hooks } from "/server/api";
 
 Hooks.Events.add("afterCoreInit", () => {
-  addRolesToVisitors();
+  Reaction.addRolesToDefaultRoleSet({
+    allShops: true,
+    roleSets: ["defaultRoles", "defaultVisitorRole"],
+    roles: ["account/verify"]
+  });
 });
