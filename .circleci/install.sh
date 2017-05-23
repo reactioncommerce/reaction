ef85a1ef36a469ad8a23f6f539fb1fdfe70068e2
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-05-23 11:00:31 -0400

    convert CI config to use CircleCI 2.0

---
+++ .circleci/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+# install OS dependencies
+apt-get update
+apt-get install -y locales
+
+
+# fix Meteor/Mongo locale issue on Debian
+# https://github.com/meteor/meteor/issues/4019
+locale-gen en_US.UTF-8
+localedef -i en_GB -f UTF-8 en_US.UTF-8
+
+
+# install Docker client
+DOCKER_VERSION="17.03.1-ce"
+curl -L -o /tmp/docker-$DOCKER_VERSION.tgz https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz
+tar -xz -C /tmp -f /tmp/docker-$DOCKER_VERSION.tgz
+mv /tmp/docker/* /usr/bin
+docker -v
+
+
+
+# install Meteor if it's not already
+hash meteor 2>/dev/null || curl https://install.meteor.com | /bin/sh
+
+
+# install Reaction CLI
+npm i -g reaction-cli
