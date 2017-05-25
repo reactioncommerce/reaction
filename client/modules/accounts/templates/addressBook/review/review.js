31c0349ece11d5c5b020d63b62ad73493f88d7eb
Brent Hoover <brent@thebuddhalodge.com>
2017-05-25 13:07:24 +0800

    Create proper behaviors

--- client/modules/accounts/templates/addressBook/review/review.js
+++ client/modules/accounts/templates/addressBook/review/review.js
@@ -10,6 +10,7 @@ Template.addressBookReview.onCreated(function () {
   const addressState = Session.get("addressState");
   this.state = new ReactiveDict();
   this.state.set("address", addressState.address);
+  this.state.set("originalAddress", addressState.address);  // use this to "revert" address changes
   this.state.set("validatedAddress", addressState.validatedAddress);
   this.state.set("formErrors", addressState.formErrors);
   this.state.set("fieldErrors", addressState.fieldErrors);
@@ -40,22 +41,39 @@ Template.addressBookReview.helpers({
 });
 
 Template.addressBookReview.events({
+  "mouseover .address-ignore": function (event) {
+    event.stopPropagation();
+  },
   "mouseover .address-invalid-line": function (event) {
+    // show checkmark when mousing over
     const addressKey = event.target.getAttribute("data-key");
     $(`div .address-invalid-line [data-key=${addressKey}] .address-line-copyover`).show();
   },
   "mouseout .address-invalid-line": function (event) {
+    // hide checkmark when mousing out
     const addressKey = event.target.getAttribute("data-key");
     $(`div .address-invalid-line [data-key=${addressKey}] .address-line-copyover`).hide();
   },
+  "click .address-ignore": function (event) {
+    // Ignore line but mark it "valid"
+    event.stopPropagation();
+    const addressKey = event.target.getAttribute("data-key");
+    const addressLine = $(`div.${addressKey}-display`);
+    $(`div .address-invalid-line [data-key=${addressKey}] .address-line-copyover`).hide();
+    $(`div .address-invalid-line [data-key=${addressKey}] .address-ignore`).hide();
+    addressLine.removeClass("address-invalid-line");
+  },
   "click .address-invalid-line": function (event) {
-    // set address value to be value from validatedAddress
+    // set address value to be value from validatedAddress and mark it valid
     const addressKey = event.target.getAttribute("data-key");
     const address = Template.instance().state.get("address");
     const validatedAddress = Template.instance().state.get("validatedAddress");
     address[addressKey] = validatedAddress[addressKey];
     Template.instance().state.set("address", address);
-    $(`div.${addressKey}-display`).removeClass("address-invalid-line");
+    $(`div .address-invalid-line [data-key=${addressKey}] .address-ignore`).hide();
+    const addressLine = $(`div.${addressKey}-display`);
+    addressLine.removeClass("address-invalid-line");
+    addressLine.addClass("address-valid-line");
   },
   "click [data-event-action=saveAddress]": function () {
     const instance = Template.instance();
@@ -80,10 +98,11 @@ Template.addressBookReview.events({
     });
   },
   "click [data-event-action=cancelAddressEdit]": function () {
-    const address = Template.instance().state.get("address");
+    // set address back to original value before edits
+    const originalAddress = Template.instance().state.get("originalAddress");
     const addressState = {
       requiresReview: false,
-      address: address,
+      address: originalAddress,
       formErrors: [],
       fieldErrors: {}
     };
