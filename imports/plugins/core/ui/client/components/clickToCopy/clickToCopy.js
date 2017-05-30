1c1a99415c810cf69744fd959f67c90d89586acc
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2017-05-30 15:55:39 -0700

    clickToCopy component

---
+++ imports/plugins/core/ui/client/components/clickToCopy/clickToCopy.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { Tooltip, Translation } from "/imports/plugins/core/ui/client/components";
+
+class ClickToCopy extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tooltipOpen: false
+    };
+
+    this.handleCtcMouseOver = this.handleCtcMouseOver.bind(this);
+    this.handleCtcMouseOut = this.handleCtcMouseOut.bind(this);
+  }
+
+  get isTooltipOpen() {
+    return this.state.tooltipOpen;
+  }
+
+  handleCtcMouseOver() {
+    this.setState({
+      tooltipOpen: this.props.tooltip ? true : false
+    });
+  }
+
+  handleCtcMouseOut() {
+    this.setState({
+      tooltipOpen: false
+    });
+  }
+
+  renderTooltipContent() {
+    if (this.isTooltipOpen) {
+      if (typeof this.props.tooltip === "string") {
+        return (
+          <Translation defaultValue={this.props.tooltip} i18nKey={this.props.i18nKeyTooltip} />
+        );
+      }
+
+      return (
+        <div>
+          {this.props.tooltip}
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+  render() {
+    return (
+      <span
+        className="rui"
+        onClick={this.handleClick}
+        onMouseOver={this.handleCtcMouseOver}
+        onMouseOut={this.handleCtcMouseOut}
+        style={{ display: "inline-flex" }}
+      >
+        <Tooltip tooltipContent={this.renderTooltipContent()} attachement={this.props.tooltipPosition}>
+          <CopyToClipboard
+            text={this.props.copyToClipboard}
+            onCopy={() => this.setState({ copied: true })}
+          >
+            <span>{this.props.displayText}</span>
+          </CopyToClipboard>
+        </Tooltip>
+      </span>
+    );
+  }
+}
+
+ClickToCopy.propTypes = {
+  copyToClipboard: PropTypes.string,
+  displayText: PropTypes.string,
+  i18nKeyTooltip: PropTypes.string,
+  tooltip: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.node]),
+  tooltipPosition: PropTypes.string
+};
+
+ClickToCopy.defaultProps = {
+  tooltip: "Copy to Clipboard",
+  tooltipPosition: "middle left"
+};
+
+export default ClickToCopy;
