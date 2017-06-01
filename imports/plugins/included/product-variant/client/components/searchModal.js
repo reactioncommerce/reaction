adb35a36c2722c09b44c96fa23e510cf1c18f316
joykare <joy.warugu@andela.com>
2017-06-01 06:54:45 +0300

    Add in functionality for modal

--- imports/plugins/included/product-variant/client/components/searchModal.js
+++ imports/plugins/included/product-variant/client/components/searchModal.js
@@ -1,41 +1,106 @@
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
+// import classnames from "classnames/dedupe";
 import { TextField, Button } from "/imports/plugins/core/ui/client/components";
 
 class SearchModal extends Component {
-  state = {
-    value: ""
+  static propTypes = {
+    handleChange: PropTypes.func,
+    handleClick: PropTypes.func,
+    siteName: PropTypes.string
   }
 
-  onChange = (event, value) => {
-    this.setState({
-      value: value
-    });
-  }
+  // state = {
+  //   value: ""
+  //   className: "search-type-option",
+  //   active: false
+  // }
 
-  onClick = () => {
-    this.setState({
-      value: ""
-    });
-  }
+  // onChange = (event, value) => {
+  //   this.setState({
+  //     value: value
+  //   });
+  // }
+
+  // onClick = () => {
+  //   this.setState({
+  //     value: ""
+  //   });
+  // }
+
+  // handleToggle = () => {
+  //   const classNames = classnames({
+  //     "search-type-active": true
+  //   }, this.state.className);
+
+  //   console.log("message", classNames);
+
+  //   this.setState({
+  //     className: classNames
+  //   });
+  // }
 
   renderSearchInput() {
     return (
       <div className="rui search-modal-input">
-        <label data-i18n="search.searchInputLabel">Search Reaction</label>
-        <i className="fa fa-search search-icon"/>
+        <label data-i18n="search.searchInputLabel">Search {this.props.siteName}</label>
+        <i className="fa fa-search search-icon" />
         <TextField
           className="search-input"
           textFieldStyle={{ marginBottom: 0 }}
-          onChange={this.onChange}
-          value={this.state.value}
+          onChange={this.props.handleChange}
+          value={this.props.value}
         />
         <Button
           className="search-clear"
           i18nKeyLabel="search.clearSearch"
           label="Clear"
           containerStyle={{ fontWeight: "normal" }}
-          onClick={this.onClick}
+          onClick={this.props.handleClick}
         />
+      </div>
+    );
+  }
+
+  renderSearchTypeToggle() {
+    return (
+      <div className="rui search-type-toggle">
+        <div
+          className="search-type-option search-type-active"
+          data-i18n="search.searchTypeProducts"
+          data-event-action="searchCollection"
+          data-event-value="products"
+        >
+          Products
+        </div>
+        <div
+          className="search-type-option"
+          data-i18n="search.searchTypeAccounts"
+          data-event-action="searchCollection"
+          data-event-value="accounts"
+        >
+          Accounts
+        </div>
+        <div
+          className="search-type-option"
+          data-i18n="search.searchTypeOrders"
+          data-event-action="searchCollection"
+          data-event-value="orders"
+        >
+          Orders
+        </div>
+      </div>
+    );
+  }
+
+  renderProductSearchTags() {
+    return (
+      <div className="rui search-modal-tags-container">
+        <p className="rui suggested-tags" data-i18n="search.suggestedTags">Suggested tags</p>
+        <div className="rui search-tags">
+          {this.props.tags().map((tag) => (
+            <span className="rui search-tag">{tag.name}</span>
+          ))}
+        </div>
       </div>
     );
   }
@@ -44,6 +109,11 @@ class SearchModal extends Component {
     return (
       <div className="rui search-modal-header">
         {this.renderSearchInput()}
+        {this.renderSearchTypeToggle()}
+        {this.props.tags() && this.renderProductSearchTags()}
+        <pre>
+          {JSON.stringify(this.props.tags(), null, 2)}
+        </pre>
       </div>
     );
   }
--- imports/plugins/included/product-variant/client/containers/searchModalContainer.js
+++ imports/plugins/included/product-variant/client/containers/searchModalContainer.js
@@ -1,19 +1,101 @@
 import React, { Component } from "react";
+import { Meteor } from "meteor/meteor";
+import _ from "lodash";
 import { composeWithTracker } from "/lib/api/compose";
+import * as Collections from "/lib/collections";
 import SearchModal from "../components/searchModal";
 
+
 class SearchModalContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getTags = this.getTags.bind(this);
+  }
+
+  handleChange = (event, value) => {
+    this.setState({
+      value: value
+    });
+  }
+
+  handleClick = () => {
+    this.setState({
+      value: ""
+    });
+  }
+
+  getTags() {
+    const searchQuery = this.state.value;
+    console.log("searchQuery", searchQuery);
+    console.log("subscription.ready", getQuery(searchQuery).ready());
+    if (searchQuery && getQuery(searchQuery).ready()) {
+      const productHashtags = getProducts();
+      const tagSearchResults = Collections.Tags.find({
+        _id: { $in: productHashtags }
+      }).fetch();
+
+      console.log("tagSearchResults", tagSearchResults);
+      return tagSearchResults;
+    }
+    return false;
+  }
+
   render() {
     return (
       <div>
-        <SearchModal />
+        <SearchModal
+          {...this.props}
+          handleChange={this.handleChange}
+          handleClick={this.handleClick}
+          tags={this.getTags}
+          value={this.state.value}
+        />
       </div>
     );
   }
 }
 
+function getSiteName() {
+  const shop = Collections.Shops.findOne();
+  return typeof shop === "object" && shop.name ? shop.name : "";
+}
+
+function getQuery(searchQuery) {
+  console.log("meteor", Meteor);
+  console.log("meteor.subscribe", Meteor.subscribe());
+  return Meteor.subscribe("SearchResults", "products", searchQuery, []);
+}
+
+function getProducts() {
+  const productResults = Collections.ProductSearch.find().fetch();
+  // const productResultsCount = productResults.length;
+  // this.state.set("productSearchResults", productResults);
+  // this.state.set("productSearchCount", productResultsCount);
+
+  const hashtags = [];
+  for (const product of productResults) {
+    if (product.hashtags) {
+      for (const hashtag of product.hashtags) {
+        if (!_.includes(hashtags, hashtag)) {
+          hashtags.push(hashtag);
+        }
+      }
+    }
+  }
+  console.log("hashtags", hashtags);
+  return hashtags;
+}
+
 function composer(props, onData) {
+  const siteName = getSiteName();
+
   onData(null, {
+    siteName
   });
 }
 
