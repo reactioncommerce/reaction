2357d8a1a64e9621c5615293408107ab3eea0cea
Seun Martins <seundotme@gmail.com>
2017-06-07 12:04:03 +0100

    make method call after drag state change

--- imports/plugins/included/product-variant/client/containers/productGridContainer.js
+++ imports/plugins/included/product-variant/client/containers/productGridContainer.js
@@ -1,8 +1,11 @@
 import _ from "lodash";
+import { Meteor } from "meteor/meteor";
 import { Session } from "meteor/session";
 import React, { Component, PropTypes } from "react";
 import update from "react/lib/update";
 import { Reaction } from "/client/api";
+import Logger from "/client/modules/logger";
+import { ReactionProduct } from "/lib/api";
 import { composeWithTracker } from "/lib/api/compose";
 import ProductGrid from "../components/productGrid";
 import { DragDropProvider } from "/imports/plugins/core/ui/client/providers";
@@ -29,7 +32,7 @@ class ProductGridContainer extends Component {
 
   componentWillMount() {
     const selectedProducts = Reaction.getUserPreferences("reaction-product-variant", "selectedGridItems");
-    const products = this.state.products;
+    const products = this.products;
 
     if (_.isEmpty(selectedProducts)) {
       return Reaction.hideActionView();
@@ -69,7 +72,7 @@ class ProductGridContainer extends Component {
     // Save the selected items to the Session
     Session.set("productGrid/selectedProducts", _.uniq(selectedProducts));
 
-    const products = this.state.products;
+    const products = this.products;
 
     if (products) {
       const filteredProducts = _.filter(products, (product) => {
@@ -81,19 +84,20 @@ class ProductGridContainer extends Component {
         i18nKeyLabel: "gridSettingsPanel.title",
         template: "productSettings",
         type: "product",
-        data: {
-          products: filteredProducts
-        }
+        data: { products: filteredProducts }
       });
     }
   }
 
-  onMove = (dragIndex, hoverIndex) => {
+  handleProductDrag = (dragIndex, hoverIndex) => {
+    const newState = this.changeProductOrderOnState(dragIndex, hoverIndex);
+    this.setState(newState, this.callUpdateMethod);
+  }
+
+  changeProductOrderOnState(dragIndex, hoverIndex) {
     const product = this.state.productIds[dragIndex];
 
-    console.log('dragging...', { dragIndex, hoverIndex, productIdonthatIndex: product });
-
-    const newState = update(this.state, {
+    return update(this.state, {
       productIds: {
         $splice: [
           [dragIndex, 1],
@@ -101,36 +105,20 @@ class ProductGridContainer extends Component {
         ]
       }
     });
-
-    console.log(JSON.stringify({ newStatebyId: newState.productIds }, null, 4));
-
-    this.setState(newState);
   }
 
-  changeStateOnMove(dragStartIndex) {
-  }
+  callUpdateMethod() {
+    const tag = ReactionProduct.getTag();
 
-  updatecall() {
-    this.state.products.map((product, index) => {
+    this.state.productIds.map((productId, index) => {
+      const position = { position: index, updatedAt: new Date() };
 
-    });
-    const productId = element.getAttribute("id");
-    const position = {
-      position: index,
-      updatedAt: new Date()
-    };
-
-    Meteor.call("products/updateProductPosition", productId, position, tag, error => {
-      if (error) {
-        Logger.warn(error);
-        throw new Meteor.Error(403, error);
-      }
-    });
-
-    // Set local state so the component does't have to wait for a round-trip
-    // to the server to get the updated list of variants
-    this.setState(newState, () => {
-      debounce(() => TagNavHelpers.onTagSort(this.state.tagIds), 500)(); // Save the updated positions
+      Meteor.call("products/updateProductPosition", productId, position, tag, error => {
+        if (error) {
+          Logger.warn(error);
+          throw new Meteor.Error(403, error);
+        }
+      });
     });
   }
 
@@ -143,8 +131,7 @@ class ProductGridContainer extends Component {
       <DragDropProvider>
         <ProductGrid
           products={this.products}
-          onMove={this.onMove}
-          onDrag={this.onMove}
+          onMove={this.handleProductDrag}
           itemSelectHandler={this.handleSelectProductItem}
         />
       </DragDropProvider>
