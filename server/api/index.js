698713136a6df83f60c1e13744d97be019c2dfb7
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-06-12 18:55:25 -0400

    fix Logger imports

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ import * as Accounts from "./core/accounts";
 import Router from "./router";
 import GeoCoder from "./geocoder";
 import Hooks from "./hooks";
-import Logger from "./logger";
+import { Logger } from "./logger";
 import { MethodHooks } from "./method-hooks";
 
 export {
