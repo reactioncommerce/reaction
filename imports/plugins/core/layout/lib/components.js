efcac14ffb54ae0840425a483c9ef0ce21e7fd76
Mike Murray <hellomikemurray@gmail.com>
2017-06-15 15:11:51 -0700

    Reorganize navbar. reduce blaze usage
    - Remove Blaze navbar templates
    - Add dynamic import for search modal
    - Add module alias for reaction layout

--- imports/plugins/core/layout/lib/components.js
+++ imports/plugins/core/layout/lib/components.js
@@ -1,11 +1,32 @@
+/* eslint-disable react/no-multi-comp */
+
+import React from "react";
+import PropTypes from "prop-types";
 import Immutable from "immutable";
 
 let registeredComponents = Immutable.Map();
 
 export function registerComponent(componentInfo) {
+  const info = {
+    ...componentInfo,
+    default: false
+  };
+
   registeredComponents = registeredComponents.set(
-    componentInfo.name,
-    componentInfo
+    info.name,
+    info
+  );
+}
+
+export function replaceComponent(componentInfo) {
+  const info = {
+    ...componentInfo,
+    default: false
+  };
+
+  registeredComponents = registeredComponents.set(
+    info.name,
+    info
   );
 }
 
@@ -14,6 +35,38 @@ export function getComponent(name) {
   return componentInfo && componentInfo.component || null;
 }
 
+export function getComponentDefinition(name) {
+  const componentInfo = registeredComponents.get(name);
+  return componentInfo || null;
+}
+
+export function createElement(name, props) {
+  const { component } = getComponentDefinition(name);
+
+  if (component) {
+    return React.createElement(component, props);
+  }
+
+  return null;
+}
+
 export function getAllComponents() {
   return registeredComponents.toObject();
 }
+
+export function Component(props) {
+  return createElement(props.name, props.componentProps);
+}
+
+Component.propTypes = {
+  componentProps: PropTypes.object,
+  name: PropTypes.string.isRequired
+};
+
+export default {
+  Component,
+  registerComponent,
+  getComponent,
+  createElement,
+  getAllComponents
+};
