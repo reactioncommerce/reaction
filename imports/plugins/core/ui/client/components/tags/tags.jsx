d7765578b15bdd08fef7a8f07536bcf63ce40456
Brent Hoover <brent@thebuddhalodge.com>
2017-06-23 15:27:09 +0800

    Fix missing lodash import

--- imports/plugins/core/ui/client/components/tags/tags.jsx
+++ imports/plugins/core/ui/client/components/tags/tags.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import _ from "lodash";
 import { PropTypes as ReactionPropTypes } from "/lib/api";
 import { TagItem } from "./";
 import classnames from "classnames";
