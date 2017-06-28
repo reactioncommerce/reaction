ae7b09be8cc42430875f5deb2351d88ef7229c37
rowland henshaw <rowland.henshaw@andela.com>
2017-06-28 09:25:22 +0100

    created component for rendering dynamic data

--- imports/plugins/core/accounts/client/components/accounts.js
+++ imports/plugins/core/accounts/client/components/accounts.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import AccountsTable from "./accountsTable";
+import _ from "lodash";
+import { Shops } from "/lib/collections";
 
 class AccountsComponent extends Component {
   static propTypes = {
@@ -11,35 +13,115 @@ class AccountsComponent extends Component {
     super(props);
 
     this.state = {
-      accounts: props.accounts
+      accounts: props.accounts,
+      shopManagers: [],
+      customerServicers: [],
+      merchandisers: [],
+      fufillment: []
     };
   }
 
-  // renderAccountList() {
-  //   if (Array.isArray(this.state.accounts)) {
-  //     return this.state.accounts.map((account, index) => (
-  //         <ListItem
-  //           key={index}
-  //           actionType="arrow"
-  //           label="My List Item 2"
-  //         />
-  //     ));
-  //   }
-  //   return null;
-  // }
+  componentDidMount() {
+    this.setUserGroups();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ accounts: nextProps.accounts });
+    this.setUserGroups();
+  }
+
+  setUserGroups() {
+    const shopManagers = [];
+    const fufillment = [];
+    const customerServicers = [];
+    const merchandisers = [];
+    this.state.accounts.forEach((account) => {
+      const userGroup = _.get(Shops.findOne(
+        {
+          _id: account.shopId
+        }
+      ), "groups").filter(group => group._id === account.groups[0].groupId[0]);
+      if (userGroup[0]) {
+        const groupName = userGroup[0].name;
+        if (groupName === "Shop Manager") {
+          shopManagers.push(account);
+        }
+        if (groupName === "Merchandiser") {
+          merchandisers.push(account);
+        }
+        if (groupName === "Fufillment") {
+          fufillment.push(account);
+        }
+        if (groupName === "Customer Service") {
+          customerServicers.push(account);
+        }
+      }
+    });
+    this.setState(
+      { shopManagers,
+        customerServicers,
+        fufillment,
+        merchandisers
+      }
+    );
+  }
+
+  renderShopManagers() {
+    if (this.state.shopManagers.length > 0) {
+      return (
+        <AccountsTable
+          users={this.state.shopManagers}
+          headerLabel="Shop Manager"
+        />
+      );
+    }
+    return null;
+  }
+
+  renderCustomerServicers() {
+    if (this.state.customerServicers.length > 0) {
+      return (
+        <AccountsTable
+          users={this.state.customerServicers}
+          headerLabel="Customer Service"
+        />
+      );
+    }
+    return null;
+  }
+
+  renderMerchandisers() {
+    if (this.state.merchandisers.length > 0) {
+      return (
+        <AccountsTable
+          users={this.state.merchandisers}
+          headerLabel="Merchandisers"
+        />
+      );
+    }
+    return null;
+  }
+
+  renderFufillment() {
+    if (this.state.fufillment.length > 0) {
+      return (
+        <AccountsTable
+          users={this.state.fufillment}
+          headerLabel="Merchandisers"
+        />
+      );
+    }
+    return null;
+  }
 
 
   render() {
     return (
       <div className="list-group">
-        <AccountsTable
-          users={this.state.accounts}
-          headerLabel="Shop Manager"
-        />
-        <AccountsTable
-          users={this.state.accounts}
-          headerLabel="Guest"
-        />
+        {this.renderShopManagers()}
+        {this.renderCustomerServicers()}
+        {this.renderMerchandisers()}
+        {this.renderFufillment()}
       </div>
 
     );
