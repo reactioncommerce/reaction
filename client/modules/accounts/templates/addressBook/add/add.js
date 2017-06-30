fce71d5b9ee3855f3ad9af89e7ff1abe906e760a
Spencer Norman <spencern@gmail.com>
2017-06-30 17:55:51 -0600

    For new address default to using shop’s country code if it exists.

--- client/modules/accounts/templates/addressBook/add/add.js
+++ client/modules/accounts/templates/addressBook/add/add.js
@@ -3,12 +3,13 @@ import { Session } from "meteor/session";
 import { Meteor } from "meteor/meteor";
 import { AutoForm } from "meteor/aldeed:autoform";
 import { Template } from "meteor/templating";
-import { i18next } from "/client/api";
+import { Reaction, i18next } from "/client/api";
 import * as Collections from "/lib/collections";
 
 Template.addressBookAdd.helpers({
   thisAddress: function () {
     const thisAddress = {};
+    const shop = Collections.Shops.findOne({ _id: Reaction.getShopId() });
     // admin should receive his account
     const account = Collections.Accounts.findOne({
       userId: Meteor.userId()
@@ -26,6 +27,15 @@ Template.addressBookAdd.helpers({
             thisAddress.isBillingDefault = true;
           }
         }
+      }
+    }
+
+    // Set default country code based on shop's shipping address
+    if (shop && Array.isArray(shop.addressBook) && shop.addressBook.length > 0) {
+      const defaultAddress = shop.addressBook.find(address => address.isShippingDefault);
+      const defaultCountryCode = defaultAddress.country;
+      if (defaultCountryCode) {
+        thisAddress.country = defaultCountryCode;
       }
     }
 
