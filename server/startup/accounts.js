f93c76d7e3e7d399c656ecc7bc95268fc886441d
Seun Martins <seundotme@gmail.com>
2017-07-05 15:31:00 +0100

    Set default group on user when signup

--- server/startup/accounts.js
+++ server/startup/accounts.js
@@ -109,11 +109,14 @@ export default function () {
 
       // if we don't have user.services we're an anonymous user
       if (!user.services) {
-        const defaultGuestRoles = Collections.Groups.findOne({ slug: "guest", shopId }).permissions;
+        const group = Collections.Groups.findOne({ slug: "guest", shopId });
+        const defaultGuestRoles = group.permissions;
         roles[shopId] = defaultGuestRoles || defaultVisitorRole;
+        additionals.groups = [group._id];
       } else {
-        const defaultCustomerRoles = Collections.Groups.findOne({ slug: "customer", shopId }).permissions;
-        roles[shopId] = defaultCustomerRoles || defaultRoles;
+        const group = Collections.Groups.findOne({ slug: "customer", shopId });
+        roles[shopId] = group.permissions || defaultRoles;
+        additionals.groups = [group._id];
         // also add services with email defined to user.emails[]
         for (const service in user.services) {
           if (user.services[service].email) {
