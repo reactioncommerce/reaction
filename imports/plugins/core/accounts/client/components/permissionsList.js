948d2903fef67dec8c0669b4bb065e38bbd4e5de
Seun Martins <seundotme@gmail.com>
2017-07-14 18:59:27 +0100

    Group packages

--- imports/plugins/core/accounts/client/components/permissionsList.js
+++ imports/plugins/core/accounts/client/components/permissionsList.js
@@ -15,30 +15,24 @@ class PermissionsList extends Component {
   render() {
     return (
       <CardGroup>
-        <SettingsCard
-          i18nKeyTitle="admin.i18nSettings.enabledLanguages"
-          name="languages"
-          padded={false}
-          packageName={"PACKAGE_NAME"}
-          saveOpenStateToPreferences={true}
-          showSwitch={false}
-          title="Languages"
-        >
-          <List>
-            {this.props.packages.map((language, key) => {
-              return (
-                <ListItem
-                  actionType={"switch"}
-                  key={key}
-                  label={language.label}
-                  switchOn={language.enabled}
-                  switchName={language.value}
-                  onSwitchChange={() => {}}
-                />
-              );
-            })}
-          </List>
-        </SettingsCard>
+        <List>
+          {this.props.packages.map((pkg, key) => {
+            if (!pkg) {
+              return null;
+            }
+            console.log({ pkg });
+            return (
+              <ListItem
+                actionType="switch"
+                key={key}
+                label={pkg.label}
+                switchOn={pkg.enabled}
+                switchName={pkg.value}
+                onSwitchChange={() => {}}
+              />
+            );
+          })}
+        </List>
       </CardGroup>
     );
   }
--- imports/plugins/core/accounts/client/containers/editGroupContainer.js
+++ imports/plugins/core/accounts/client/containers/editGroupContainer.js
@@ -1,4 +1,5 @@
 import { Meteor } from "meteor/meteor";
+import _ from "lodash";
 import { Reaction } from "/client/api";
 import React, { Component } from "react";
 import { Packages } from "/lib/collections";
@@ -15,7 +16,6 @@ class EditGroupContainer extends Component {
   }
 
   constructor(props) {
-    console.log({ props });
     super(props);
 
     this.state = {
@@ -47,7 +47,7 @@ class EditGroupContainer extends Component {
         <CardBody expandable={true}>
           <div className="settings">
             {this.renderGroups()}
-            <PermissionsList packages={this.props.packages}/>
+            <PermissionsList packages={groupPackages(this.props.packages)}/>
           </div>
         </CardBody>
       </Card>
@@ -64,3 +64,57 @@ const composer = (props, onData) => {
 };
 
 export default composeWithTracker(composer)(EditGroupContainer);
+
+function groupPackages(packages) {
+  return packages.map(pkg => {
+    const permissions = [];
+    if (pkg.registry && pkg.enabled) {
+      for (const registryItem of pkg.registry) {
+        // Skip entires with missing routes
+        if (!registryItem.route) {
+          continue;
+        }
+
+        // Get all permissions, add them to an array
+        if (registryItem.permissions) {
+          for (const permission of registryItem.permissions) {
+            // check needed because of non-object perms in the permissions array (e.g "admin", "owner")
+            if (typeof permission === "object") {
+              permission.shopId = Reaction.getShopId();
+              permissions.push(permission);
+            }
+          }
+        }
+
+        // Also create an object map of those same permissions as above
+        const permissionMap = getPermissionMap(permissions);
+        if (!permissionMap[registryItem.route]) {
+          permissions.push({
+            shopId: pkg.shopId,
+            permission: registryItem.name || pkg.name + "/" + registryItem.template, // launchdock-connect/connectDashboard
+            icon: registryItem.icon,
+            label: registryItem.label || registryItem.provides || registryItem.route
+          });
+        }
+      }
+      // TODO review this, hardcoded WIP
+      const label = pkg.name.replace("reaction", "").replace(/(-.)/g, x => " " + x[1].toUpperCase());
+
+      return {
+        shopId: pkg.shopId,
+        icon: pkg.icon,
+        name: pkg.name,
+        label: label,
+        permissions: _.uniq(permissions)
+      };
+    }
+  });
+}
+
+function getPermissionMap(permissions) {
+  const permissionMap = {};
+  _.each(permissions, function(existing) {
+    permissionMap[existing.permission] = existing.label;
+  });
+  return permissionMap;
+}
