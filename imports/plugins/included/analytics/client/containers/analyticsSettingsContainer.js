76c6bc738303b4d483c5af03887e75fb4b2d1994
rowland henshaw <rowland.henshaw@andela.com>
2017-07-14 20:00:43 +0100

    * added switch handler for updates when analytics is configured an enabled * added validation for handling empty values on analytics api keys

--- imports/plugins/included/analytics/client/containers/analyticsSettingsContainer.js
+++ imports/plugins/included/analytics/client/containers/analyticsSettingsContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Meteor } from "meteor/meteor";
 import { Reaction, i18next } from "/client/api";
 import { Packages } from "/lib/collections";
@@ -6,22 +7,48 @@ import { composeWithTracker } from "/lib/api/compose";
 import AnalyticsSettings from "../components/settings";
 
 class AnalyticsSettingsContainer extends Component {
+  static propTypes = {
+    enabled: PropTypes.object,
+    packageData: PropTypes.object
+  }
+
+  handleToggle = (event, isChecked, name) => {
+    const { enabled } = this.props;
+    if (enabled[name]) {
+      Meteor.call("reaction-analytics/updateStatus", name, "enabled", isChecked);
+    } else {
+      Alerts.toast(
+        i18next.t(`admin.settings.${name}NotConfigured`, { defaultValue: `Analytics not configured for ${name}` }),
+        "error"
+      );
+    }
+  }
+
   handleSave = (settingName, values) => {
-    Meteor.call("reaction-analytics/updateAnalyticsSettings", values.settings, (error) => {
-      if (!error) {
-        Alerts.toast(
-          i18next.t("admin.settings.analyticsSettingsSaved", { defaultValue: "Analytics settings saved" }),
-          "success"
-        );
-      }
-    });
+    const data = values.settings.public[settingName].api_key;
+
+    if (data) {
+      Meteor.call("reaction-analytics/updateAnalyticsSettings", data, settingName, (error) => {
+        if (!error) {
+          Alerts.toast(
+            i18next.t("admin.settings.analyticsSettingsSaved", { defaultValue: "Analytics settings saved" }),
+            "success"
+          );
+        }
+      });
+    } else {
+      Alerts.toast(
+        i18next.t("admin.settings.analyticsSettingsNotSaved", { defaultValue: `No value provided for ${settingName} settings` }),
+        "error"
+      );
+    }
   }
 
   render() {
     return (
       <AnalyticsSettings
         onSave={this.handleSave}
-        onToggle={this.handleToggle}
+        handleToggle={this.handleToggle}
         {...this.props}
       />
     );
