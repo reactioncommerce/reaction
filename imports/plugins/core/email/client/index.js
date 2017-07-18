70b018080413b2598f2e9df8a846e965f8fff8a7
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-07-18 16:40:40 -0400

    fix export typos

--- imports/plugins/core/email/client/index.js
+++ imports/plugins/core/email/client/index.js
@@ -2,5 +2,5 @@ import "./templates/email.html";
 import "./templates/email.js";
 
 export { default as EmailConfig } from "./containers/emailConfig";
-export { default as EmailLo } from "./containers/emailLogs";
-export { default as EmailSettins } from "./containers/emailSettings";
+export { default as EmailLogs } from "./containers/emailLogs";
+export { default as EmailSettings } from "./containers/emailSettings";
