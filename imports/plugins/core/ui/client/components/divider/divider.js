639aa5fa1c81f34cf8ccf1e05f9c7d7088216a0c
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-07-19 12:12:19 -0400

    refactor Divider component

--- imports/plugins/core/ui/client/components/divider/divider.js
+++ imports/plugins/core/ui/client/components/divider/divider.js
@@ -1,43 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
+import { pure } from "recompose";
 import { Components, registerComponent } from "@reactioncommerce/reaction-components";
 
-class Divider extends Component {
-  renderLabel() {
+const Divider = (props) => {
+  const { label, i18nKeyLabel } = props;
+
+  const classes = classnames({
+    rui: true,
+    separator: true,
+    divider: true,
+    labeled: label || i18nKeyLabel
+  });
+
+  if (label) {
     return (
-      <Components.Translation defaultValue={this.props.label} />
-    );
-  }
-
-  render() {
-    const { label, i18nKeyLabel } = this.props;
-    const classes = classnames({
-      rui: true,
-      separator: true,
-      divider: true,
-      labeled: label || i18nKeyLabel
-    });
-
-    if (label) {
-      return (
-        <div className={classes} id={this.props.id}>
-          <hr />
-          <span className="label">
-            <Components.Translation defaultValue={label} i18nKey={i18nKeyLabel} />
-          </span>
-          <hr />
-        </div>
-      );
-    }
-
-    return (
-      <div className={classes}>
+      <div className={classes} id={props.id}>
+        <hr />
+        <span className="label">
+          <Components.Translation defaultValue={label} i18nKey={i18nKeyLabel} />
+        </span>
         <hr />
       </div>
     );
   }
-}
+
+  return (
+    <div className={classes}>
+      <hr />
+    </div>
+  );
+};
 
 Divider.propTypes = {
   i18nKeyLabel: PropTypes.string,
@@ -45,6 +39,6 @@ Divider.propTypes = {
   label: PropTypes.string
 };
 
-registerComponent("Divider", Divider);
+registerComponent("Divider", Divider, pure);
 
 export default Divider;
