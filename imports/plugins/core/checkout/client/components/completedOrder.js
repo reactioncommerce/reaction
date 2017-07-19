026311b487e8f55f7ed4b82b6f678d8ba0e6834f
Brent Hoover <brent@thebuddhalodge.com>
2017-07-19 13:57:39 +0800

    Change name for clarity

--- imports/plugins/core/checkout/client/components/completedOrder.js
+++ imports/plugins/core/checkout/client/components/completedOrder.js
@@ -4,7 +4,87 @@ import PropTypes from "prop-types";
 
 const CompletedOrder = ({ order }) => {
   return (
-    <div>Completed Order Stuff Goes right here</div>
+    <div>
+      <div className="panel panel-default order-list-detail">
+        <div className="panel-heading">
+          <h3 className="panel-title">
+            <span data-i18n="order.placed">Placed</span>
+            <span>orderAsge</span>
+            <span data-i18n="order.on">on</span>
+            <span>order Date</span>
+            <span className="pull-right"><b>Order ID</b>: {order._id}</span>
+          </h3>
+        </div>
+      </div>
+
+      <div className="panel-body">
+        <div className="row">
+          <div className="col-xs-6 col-sm-2">
+            <strong>
+              <span data-i18n="order.status">Status</span>
+            </strong>
+          </div>
+          <div className="col-xs-6 col-sm-10">
+            <span data-i18n="order.completed">Completed</span>
+          </div>
+        </div>
+      </div>
+      <div className="row order-address-info">
+        <div className="col-xs-6 col-sm-2">
+          <strong>
+            <span data-i18n="order.destination">Destination</span>
+          </strong>
+        </div>
+        <div className="col-xs-10">
+          <span>
+            <address>
+              Address Here
+            </address>
+          </span>
+        </div>
+      </div>
+
+      <div className="row order-shipment-info">
+        <div className="col-xs-6 col-sm-2">
+          <strong>
+            <span data-i18n="order.shipment">Shipment</span>
+          </strong>
+        </div>
+        <div className="col-xs-10">
+          shippingmethod.carrier - shippment.label
+        </div>
+      </div>
+
+      <div className="row order-payment-info">
+        <div className="col-xs-6 col-sm-2">
+          <strong>
+            <span data-i18n="order.payment">Payment</span>
+          </strong>
+        </div>
+        <div className="col-xs-10">
+          <span id="order-payment-method">paymentMethod.storedCard</span>
+          <span data-id="order.reference">Ref:</span>
+          <span id="order-payment-transaction">paymentMethod.transactionId</span>
+        </div>
+      </div>
+
+      <div className="row order-group-title">
+        <div className="col-xs-12">
+          <strong>
+            <span id="order-item-count">itemCount</span>
+            <span data-i18n="order.itemsFrom">item(s) from</span>
+            <span id="order-group-name">shopName</span>
+          </strong>
+        </div>
+      </div>
+
+      <div className="row order-list-items">
+        <div className="col-xs-12">
+          Order List Items Go Here
+        </div>
+      </div>
+
+    </div>
   );
 };
 
--- imports/plugins/core/checkout/client/containers/completedOrderContainer.js
+++ imports/plugins/core/checkout/client/containers/completedOrderContainer.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Meteor } from "meteor/meteor";
+import { Orders } from "/lib/collections";
+import { composeWithTracker } from "/lib/api/compose";
+import { Reaction } from "/client/api";
+
 import CompletedOrder from "../components/completedOrder";
 
 class CompletedOrderContainer extends Component {
 
+
   render() {
     return (
       <div>
-        <CompletedOrder />
+        <CompletedOrder order={this.props.order} />
       </div>
     );
   }
 }
 
-export default CompletedOrderContainer;
+function composer(props, onData) {
+  const order = Orders.findOne({
+    userId: Meteor.userId(),
+    cartId: Reaction.Router.getQueryParam("_id")
+  });
+
+  onData(null, {
+    order
+  });
+}
+
+CompletedOrder.propTypes  = {
+  order: PropTypes.object
+};
+
+export default composeWithTracker(composer)(CompletedOrderContainer);
