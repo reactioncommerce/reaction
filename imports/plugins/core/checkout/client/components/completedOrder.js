23f3ff49c715496a8a76a2480a87c6647b0397c6
Brent Hoover <brent@thebuddhalodge.com>
2017-07-26 08:17:48 +0800

    Redoing HTML to be responsive

--- imports/plugins/core/checkout/client/components/completedOrder.js
+++ imports/plugins/core/checkout/client/components/completedOrder.js
@@ -2,85 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-const CompletedOrder = ({ order }) => {
+const CompletedOrder = (order) => {
+
   return (
-    <div>
-      <div className="panel panel-default order-list-detail">
-        <div className="panel-heading">
-          <h3 className="panel-title">
-            <span data-i18n="order.placed">Placed</span>
-            <span>orderAsge</span>
-            <span data-i18n="order.on">on</span>
-            <span>order Date</span>
-            <span className="pull-right"><b>Order ID</b>: {order._id}</span>
-          </h3>
-        </div>
+    <div className="container order-completed">
+      <div>
+        <h3><span data-i18n="cartCompleted.thankYou">Thank You!</span></h3>
       </div>
-
-      <div className="panel-body">
-        <div className="row">
-          <div className="col-xs-6 col-sm-2">
-            <strong>
-              <span data-i18n="order.status">Status</span>
-            </strong>
-          </div>
-          <div className="col-xs-6 col-sm-10">
-            <span data-i18n="order.completed">Completed</span>
-          </div>
-        </div>
+      <div>
+        Order updates will be sent to ryan@reactioncommerce.com
       </div>
-      <div className="row order-address-info">
-        <div className="col-xs-6 col-sm-2">
-          <strong>
-            <span data-i18n="order.destination">Destination</span>
-          </strong>
-        </div>
-        <div className="col-xs-10">
-          <span>
-            <address>
-              Address Here
-            </address>
-          </span>
-        </div>
+      <div><h4>Your Items</h4></div>
+      <div className="order-details-shop-status-box">
+        <h4>Shoe Box</h4>
+        <div>Free Shipping - estimated delivery 06/17/16</div>
       </div>
-
-      <div className="row order-shipment-info">
-        <div className="col-xs-6 col-sm-2">
-          <strong>
-            <span data-i18n="order.shipment">Shipment</span>
-          </strong>
-        </div>
-        <div className="col-xs-10">
-          shippingmethod.carrier - shippment.label
-        </div>
-      </div>
-
-      <div className="row order-payment-info">
-        <div className="col-xs-6 col-sm-2">
-          <strong>
-            <span data-i18n="order.payment">Payment</span>
-          </strong>
-        </div>
-        <div className="col-xs-10">
-          <span id="order-payment-method">paymentMethod.storedCard</span>
-          <span data-id="order.reference">Ref:</span>
-          <span id="order-payment-transaction">paymentMethod.transactionId</span>
-        </div>
-      </div>
-
-      <div className="row order-group-title">
-        <div className="col-xs-12">
-          <strong>
-            <span id="order-item-count">itemCount</span>
-            <span data-i18n="order.itemsFrom">item(s) from</span>
-            <span id="order-group-name">shopName</span>
-          </strong>
-        </div>
-      </div>
-
-      <div className="row order-list-items">
-        <div className="col-xs-12">
-          Order List Items Go Here
+      <div className="row order-completed-item-box">
+        <div className="order-completed-line-item">
+          <div className="order-item-media"><img src="/resources/placeholder.gif" /></div>
+          <div className="order-details-title">Air Max</div>
+          <div className="order-details-quantity">2</div>
+          <div className="order-details-price">$250.00</div>
         </div>
       </div>
 
