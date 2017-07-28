4415585e0e6599b94f30f931b6c948ddab53cd53
Seun Martins <seundotme@gmail.com>
2017-07-29 00:00:41 +0100

    Add form to call invite user form marketplace settings dashgs

--- imports/plugins/included/marketplace/client/templates/dashboard/settings.js
+++ imports/plugins/included/marketplace/client/templates/dashboard/settings.js
@@ -1,3 +1,4 @@
+import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { AutoForm } from "meteor/aldeed:autoform";
 import { Reaction, i18next } from "/client/api";
@@ -26,6 +27,41 @@ Template.marketplaceSettings.helpers({
  * marketplace Catalog settings
  */
 Template.marketplaceShopSettings.inheritsHelpersFrom("marketplaceSettings");
+
+Template.inviteUser.events({
+  "click [data-event-action=inviteUser]": function (e) {
+    e.preventDefault();
+    const name = $("#owner-name").val();
+    const email = $("#owner-email").val();
+
+    Meteor.call("accounts/inviteShopOwner", { name, email }, (error, result) => {
+      if (error) {
+        let message;
+        if (error.reason === "Unable to send invitation email.") {
+          message = i18next.t("accountsUI.error.unableToSendInvitationEmail");
+        } else if (error.reason === "A user with this email address already exists") {
+          message = i18next.t("accountsUI.error.userWithEmailAlreadyExists");
+        } else if (error.reason !== "") {
+          message = error;
+        } else {
+          message = `${i18next.t("accountsUI.error.errorSendingEmail")} ${error}`;
+        }
+
+        Alerts.inline(message, "error", { placement: "memberform" });
+        $("#owner-name").val("");
+        $("#owner-email").val("");
+        return false;
+      }
+
+      if (result) {
+        Alerts.toast(i18next.t("accountsUI.info.invitationSent", "Invitation sent."), "success");
+        $("#owner-name").val("");
+        $("#owner-email").val("");
+        return true;
+      }
+    });
+  }
+});
 
 /**
  * marketplaceSettings autoform alerts
