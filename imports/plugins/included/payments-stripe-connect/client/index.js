6a583180f5c782697011b6630fcf4aa23b6c65f1
Spencer Norman <spencern@gmail.com>
2017-07-30 18:16:04 -0600

    Init merchant stripe connect settings

--- imports/plugins/included/payments-stripe-connect/client/index.js
+++ imports/plugins/included/payments-stripe-connect/client/index.js
@@ -1,1 +1,5 @@
-import "./settings/settings";
+import "./settings/settings.html";
+import "./settings/settings.js";
+
+import "./settings/merchant-settings.html";
+import "./settings/merchant-settings.js";
