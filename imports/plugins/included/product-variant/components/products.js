a9b59aa471d77e13867ab4b13622272e6e644209
Mike Murray <hellomikemurray@gmail.com>
2017-08-10 18:03:25 -0700

    Added a not found view to product grid (#2656)
    * Added a not found view to product grid
    
    - Added new component NotFound that lets you do blank views
    - Added not found view to product grid if tag was not found
    
    * Add comment to component
    
    * Fixed typos and propTypes
    
    * Show not found if no products are avalable when tag is valid
    
    - Adds jsdoc to all members of Products class
    
    * Fixed typos in comments
    
    * Remove unused props
    
    The classnames transformation for FlatButton icons was unnecessary.
    
    * explicitly add protypes for icon and onIcon to appease eslint
    
    * Disable eslint warning for direct state mutation
    
    * fix comment style
    
    * format React components PropType in a JSDoc style that works
    
    * spelling + lint fix
    
    * Add proptype definition for contentClassName

--- imports/plugins/included/product-variant/components/products.js
+++ imports/plugins/included/product-variant/components/products.js
@@ -4,21 +4,51 @@ import { Components } from "@reactioncommerce/reaction-components";
 import { Reaction } from "/client/api";
 import { getTagIds as getIds } from "/lib/selectors/tags";
 
+/** Class representing the Products React component
+ * @summary PropTypes for Product React component
+ * @property {Function} loadMoreProducts - load more products callback
+ * @property {Function} loadProducts - Load products callback
+ * @property {Array} products - Array of products
+ * @property {Object} products - Products subscription
+ * @property {Function} ready - Ready state check helper
+ * @property {Boolean} showNotFound - Force show not-found view
+*/
+
 class Products extends Component {
   static propTypes = {
     loadMoreProducts: PropTypes.func,
     loadProducts: PropTypes.func,
     products: PropTypes.array,
     productsSubscription: PropTypes.object,
-    ready: PropTypes.func
+    ready: PropTypes.func,
+    showNotFound: PropTypes.bool
   };
 
+  /**
+   * Checks and returns a Boolean if the `products` array from props is not empty.
+   * @return {Boolean} Boolean value `true` if products are available, `false` otherwise.
+   */
+  get hasProducts() {
+    return Array.isArray(this.props.products) && this.props.products.length > 0;
+  }
+
+  /**
+   * Handle load more button click
+   * @access protected
+   * @param  {SyntheticEvent} event Synthetic event object
+   * @return {undefined}
+   */
   handleClick = (event) => {
     if (this.props.loadProducts) {
       this.props.loadProducts(event);
     }
   }
 
+  /**
+   * Render product grid
+   * @access protected
+   * @return {Node} React node containing the `ProductGrid` component.
+   */
   renderProductGrid() {
     const products = this.props.products;
 
@@ -40,16 +70,24 @@ class Products extends Component {
     );
   }
 
+  /**
+   * Render loading component
+   * @access protected
+   * @return {Node} React node containing the `Loading` component.
+   */
   renderSpinner() {
     if (this.props.productsSubscription.ready() === false) {
       return (
-        <div className="spinner-container">
-          <div className="spinner" />
-        </div>
+        <Components.Loading />
       );
     }
   }
 
+  /**
+   * Render load more button
+   * @access protected
+   * @return {Node|undefined} React node contianing a `laod more` button or undefined.
+   */
   renderLoadMoreProductsButton() {
     if (this.props.loadMoreProducts()) {
       return (
@@ -65,21 +103,48 @@ class Products extends Component {
     }
   }
 
-  render() {
-    if (this.props.ready()) {
-      return (
-        <div id="container-main">
-          {this.renderProductGrid()}
-          {this.renderLoadMoreProductsButton()}
-          {this.renderSpinner()}
-        </div>
-      );
-    }
+  /**
+   * Render the not found component
+   * @access protected
+   * @return {Node} React node contianing the `NotFound` component.
+   */
+  renderNotFound() {
     return (
-      <div className="spinner-container spinner-container-lg">
-        <div className="spinner" />
-      </div>
+      <Components.NotFound
+        i18nKeyTitle="productGrid.noProductsFound"
+        icon="fa fa-barcode"
+        title="No Products Found"
+      />
     );
+  }
+
+  /**
+   * Render component
+   * @access protected
+   * @return {Node} React node containing elements that make up the `Products` component.
+   */
+  render() {
+    // Force show the not-found view.
+    if (this.props.showNotFound) {
+      return this.renderNotFound();
+    } else if (this.props.ready()) {
+      // Render products grid if products are available after subscription ready.
+      if (this.hasProducts) {
+        return (
+          <div id="container-main">
+            {this.renderProductGrid()}
+            {this.renderLoadMoreProductsButton()}
+            {this.renderSpinner()}
+          </div>
+        );
+      }
+
+      // Render not-found view if no products are available.
+      return this.renderNotFound();
+    }
+
+    // Render loading component by default if no condition above matches.
+    return this.renderSpinner();
   }
 }
 
