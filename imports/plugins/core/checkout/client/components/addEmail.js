edf2f13dd026c3c5597456771bc5bc0c9b5088c4
Brent Hoover <brent@thebuddhalodge.com>
2017-08-11 11:14:45 +0800

    Make submit email functionality (mostly) work

--- imports/plugins/core/checkout/client/components/addEmail.js
+++ imports/plugins/core/checkout/client/components/addEmail.js
@@ -2,18 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
 
+
 class AddEmail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       hasEmail: !!this.props.orderEmail
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFieldChange = this.handleFieldChange.bind(this);
   }
+
+  handleFieldChange = (event, value, field) => {
+    this.setState({
+      [field]: value
+    });
+  };
 
   handleSubmit(event) {
     event.preventDefault();
     if (this.props.handleEmailSubmit) {
-      this.props.handleEmailSubmit(event);
+      this.props.handleEmailSubmit(this.state.email);
     }
   }
 
@@ -29,8 +39,13 @@ class AddEmail extends React.Component {
     return (
       <form onSubmit={this.handleSubmit} className="add-email-input">
         <div className="input-group">
-          <input type="email" name="email" className="form-control" placeholder="email"
-            id="update-order-email" aria-describedby="update-order-email"
+          <Components.TextField
+            label="Email"
+            name="email"
+            type="email"
+            tabIndex="1"
+            value={this.state.email}
+            onChange={this.handleFieldChange}
           />
           <input type="submit" className="input-group-addon" id="update-order" data-i18n="app.submit" />
         </div>
