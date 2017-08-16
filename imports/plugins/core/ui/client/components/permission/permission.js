604f8daf53a155e39d85f4be76161413ac2d5641
Seun Martins <seundotme@gmail.com>
2017-08-16 20:47:13 +0100

    check that userPermissions includes all elements from groupPermissions

--- imports/plugins/core/ui/client/components/permission/permission.js
+++ imports/plugins/core/ui/client/components/permission/permission.js
@@ -1,8 +1,9 @@
 import { Component, Children } from "react";
 import { Meteor } from "meteor/meteor";
+import _ from "lodash";
 import PropTypes from "prop-types";
 import { composeWithTracker, registerComponent } from "@reactioncommerce/reaction-components";
-import { Accounts, Groups } from "/lib/collections";
+import { Groups } from "/lib/collections";
 import { Reaction } from "/client/api";
 
 class Permission extends Component {
@@ -25,21 +26,24 @@ class Permission extends Component {
  * withPermissions(["guest"])(MyComponent)
  * @param  {Array|String} roles String or array of strings of permissions to check. default: roles=["guest", "anonymous"]
  * @param  {String} group Slug title of a group to check against. Group option supercedes roles option. default: group="customer".
+ * Group access is given to users at that group level and above
  * @return {function} Returns a compser higher order component
  */
 export function withPermissions(roles = ["guest", "anonymous"], group = "customer") {
   return composeWithTracker((props, onData) => {
     const grpSub = Meteor.subscribe("Groups");
-    const userSub = Meteor.subscribe("Accounts", Meteor.userId());
-    let hasPermission = Reaction.hasPermission(props.roles || roles);
 
-    if (grpSub.ready() && userSub.ready()) {
+    if (grpSub.ready()) {
+      let hasPermission = Reaction.hasPermission(props.roles || roles);
+
       if (group) {
-        const grp = Groups.find({ slug: group }).fetch();
-        const account = Accounts.find({ _id: Meteor.userId() }).fetch();
-        if (grp && grp._id) {
-          console.log(account.groups, grp._id);
-          hasPermission = account.groups.indexOf(grp._id) > -1;
+        hasPermission = false;
+        const grp = Groups.findOne({ slug: props.group || group });
+        const user = Meteor.user();
+        const permissions = user.roles[Reaction.getShopId()] || [];
+        if (grp && grp.permissions) {
+          // checks that userPermissions includes all elements from groupPermissions
+          hasPermission = _.difference(grp.permissions, permissions).length === 0;
         }
       }
 
