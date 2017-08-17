770329b74437ac038017fb2c881c219b7a252a86
Brent Hoover <brent@thebuddhalodge.com>
2017-08-17 10:48:43 +0800

    Add "Your Cart" heading

--- imports/plugins/core/checkout/client/components/completedOrderSummary.js
+++ imports/plugins/core/checkout/client/components/completedOrderSummary.js
@@ -4,49 +4,56 @@ import { Components, registerComponent } from "@reactioncommerce/reaction-compon
 
 
 const CompletedOrderSummary = ({ orderSummary }) => {
-  return <div className="order-details-info-box">
-    <div className="order-details-info-box-content">
-      <div className="order-summary-line">
-        <div className="order-summary-title">
-          <Components.Translation defaultValue="Quantity Total" i18nKey={"cartCompleted.quantityTotal"}/>
-        </div>
-        <div className="order-summary-value">{orderSummary.quantityTotal}</div>
+  return (
+    <div>
+      <div className="order-details-content-title">
+        <Components.Translation defaultValue="Your Cart" i18nKey={"cartCompleted.yourCart"} />
       </div>
-      <div className="order-summary-line">
-        <div className="order-summary-title">
-          <Components.Translation defaultValue="Subtotal" i18nKey={"cartCompleted.orderSubtotal"} />
-        </div>
-        <div className="order-summary-value">
-          <Components.Currency amount={orderSummary.subtotal} />
-        </div>
-      </div>
-      <div className="order-summary-line">
-        <div className="order-summary-title">
-          <Components.Translation defaultValue="Shipping" i18nKey={"cartCompleted.orderShipping"} />
-        </div>
-        <div className="order-summary-value">
-          <Components.Currency amount={orderSummary.shipping} />
-        </div>
-      </div>
-      <div className="order-summary-line">
-        <div className="order-summary-title">
-          <Components.Translation defaultValue="Tax" i18nKey={"cartCompleted.orderTax"} />
-        </div>
-        <div className="order-summary-value">
-          <Components.Currency amount={orderSummary.tax} />
-        </div>
-      </div>
-      <hr/>
-      <div className="order-summary-line">
-        <div className="order-summary-total-title">
-          <Components.Translation defaultValue="Captured Total" i18nKey={"cartCompleted.orderTotal"} />
-        </div>
-        <div className="order-summary-total-value">
-          <Components.Currency amount={orderSummary.total} />
+      <div className="order-details-info-box">
+        <div className="order-details-info-box-content">
+          <div className="order-summary-line">
+            <div className="order-summary-title">
+              <Components.Translation defaultValue="Quantity Total" i18nKey={"cartCompleted.quantityTotal"}/>
+            </div>
+            <div className="order-summary-value">{orderSummary.quantityTotal}</div>
+          </div>
+          <div className="order-summary-line">
+            <div className="order-summary-title">
+              <Components.Translation defaultValue="Subtotal" i18nKey={"cartCompleted.orderSubtotal"} />
+            </div>
+            <div className="order-summary-value">
+              <Components.Currency amount={orderSummary.subtotal} />
+            </div>
+          </div>
+          <div className="order-summary-line">
+            <div className="order-summary-title">
+              <Components.Translation defaultValue="Shipping" i18nKey={"cartCompleted.orderShipping"} />
+            </div>
+            <div className="order-summary-value">
+              <Components.Currency amount={orderSummary.shipping} />
+            </div>
+          </div>
+          <div className="order-summary-line">
+            <div className="order-summary-title">
+              <Components.Translation defaultValue="Tax" i18nKey={"cartCompleted.orderTax"} />
+            </div>
+            <div className="order-summary-value">
+              <Components.Currency amount={orderSummary.tax} />
+            </div>
+          </div>
+          <hr/>
+          <div className="order-summary-line">
+            <div className="order-summary-total-title">
+              <Components.Translation defaultValue="Captured Total" i18nKey={"cartCompleted.orderTotal"} />
+            </div>
+            <div className="order-summary-total-value">
+              <Components.Currency amount={orderSummary.total} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 CompletedOrderSummary.propTypes = {
