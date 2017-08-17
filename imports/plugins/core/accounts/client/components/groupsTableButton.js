90a4c09bc647e007e84590cc225dbc2de22551ee
Seun Martins <seundotme@gmail.com>
2017-08-17 22:08:35 +0100

    Add comment

--- imports/plugins/core/accounts/client/components/groupsTableButton.js
+++ imports/plugins/core/accounts/client/components/groupsTableButton.js
@@ -2,6 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Components, registerComponent, withPermissions } from "@reactioncommerce/reaction-components";
 
+/**
+ * @summary React stateless component for "remove from group" button for groupTable
+ * @param {Object} props - React PropTypes
+ * @property {Object} account - User account object
+ * @property {Object} group - Group data
+ * @property {Function} handleRemoveUserFromGroup - function to call on button click
+ * @property {Boolean} hasPermissions - true or false depending on if user is granted access
+ * @return {Node} React node containing wrapped button
+ */
 const GroupsTableButton = ({ account, group, handleRemoveUserFromGroup, hasPermissions }) => {
   if (group.slug === "owner") {
     return null;
