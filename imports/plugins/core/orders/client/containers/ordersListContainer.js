58794e7fd5b55772bf0cd7d86e8f26f6ecc12b25
joykare <warugujoy@gmail.com>
2017-08-21 12:24:16 +0300

    Use ordersListContainer

--- imports/plugins/core/orders/client/containers/ordersListContainer.js
+++ imports/plugins/core/orders/client/containers/ordersListContainer.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { composeWithTracker } from "@reactioncommerce/reaction-components";
 import { Meteor } from "meteor/meteor";
-import { Tracker } from "meteor/tracker";
 import { Media, Orders } from "/lib/collections";
 import { Reaction } from "/client/api";
 import { Loading } from "/imports/plugins/core/ui/client/components";
 import OrdersList from "../components/orderList.js";
 import {
   PACKAGE_NAME,
-  // ORDER_LIST_FILTERS_PREFERENCE_NAME,
   ORDER_LIST_SELECTED_ORDER_PREFERENCE_NAME
 } from "../../lib/constants";
 
@@ -83,11 +81,9 @@ class OrdersListContainer extends Component {
 
     this.state = {
       selectedItems: [],
-      // orders: props.orders,
+      orders: props.orders,
       multipleSelect: false,
-      orders: [],
       ready: false,
-      filter: "",
       query: {}
     };
 
@@ -95,37 +91,13 @@ class OrdersListContainer extends Component {
     this.handleDisplayMedia = this.handleDisplayMedia.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
     this.selectAllOrders = this.selectAllOrders.bind(this);
-    this.dep = new Tracker.Dependency;
-  }
-
-  componentDidMount() {
-    Tracker.autorun(() => {
-      this.dep.depend();
-
-      const filter = this.state.filter;
-      const query = OrderHelper.makeQuery(filter);
-      this.subscription = Meteor.subscribe("CustomPaginatedOrders");
-
-      if (this.subscription.ready()) {
-        const orders = Orders.find(query).fetch();
-        this.setState({
-          orders,
-          query,
-          ready: true
-        });
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    this.subscription.stop();
   }
 
   handleMenuClick = (event, value) => {
+    const query = OrderHelper.makeQuery(value);
+
     this.setState({
-      filter: value
-    }, () => {
-      this.dep.changed();
+      query
     });
   }
 
@@ -200,8 +172,6 @@ class OrdersListContainer extends Component {
         _id: order._id
       });
     }
-
-    this.dep.changed();
   }
 
   /**
@@ -234,30 +204,31 @@ class OrdersListContainer extends Component {
   }
 
   render() {
-    if (this.state.ready) {
-      return (
-        <OrdersList
-          handleSelect={this.handleSelect}
-          orders={this.state.orders}
-          query={this.state.query}
-          handleClick={this.handleClick}
-          displayMedia={this.handleDisplayMedia}
-          selectedItems={this.state.selectedItems}
-          selectAllOrders={this.selectAllOrders}
-          multipleSelect={this.state.multipleSelect}
-          handleMenuClick={this.handleMenuClick}
-        />
-      );
-    }
-    return <Loading />;
+    return (
+      <OrdersList
+        handleSelect={this.handleSelect}
+        orders={this.state.orders}
+        query={this.state.query}
+        handleClick={this.handleClick}
+        displayMedia={this.handleDisplayMedia}
+        selectedItems={this.state.selectedItems}
+        selectAllOrders={this.selectAllOrders}
+        multipleSelect={this.state.multipleSelect}
+        handleMenuClick={this.handleMenuClick}
+      />
+    );
   }
 }
 
 const composer = (props, onData) => {
-  const subscription = Meteor.subscribe("Media");
-  if (subscription.ready()) {
+  const mediaSubscription = Meteor.subscribe("Media");
+  const ordersSubscription = Meteor.subscribe("CustomPaginatedOrders");
+
+  if (mediaSubscription.ready() && ordersSubscription.ready()) {
+    const orders = Orders.find().fetch();
+
     onData(null, {
-      ...props
+      orders
     });
   }
 };
