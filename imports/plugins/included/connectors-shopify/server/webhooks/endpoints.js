808c041fbd12db2349e2300897b487630088f815
Spencer Norman <spencern@gmail.com>
2017-08-31 22:47:06 -0600

    Update webhook endpoints for shopify sync

--- imports/plugins/included/connectors-shopify/server/webhooks/endpoints.js
+++ imports/plugins/included/connectors-shopify/server/webhooks/endpoints.js
@@ -3,16 +3,8 @@ import crypto from "crypto";
 import { Meteor } from "meteor/meteor";
 
 import { Reaction } from "/server/api";
-import { Products } from "/lib/collections";
 
-// Shopify Shared Signature verification middleware
-Reaction.Endpoints.add("post", "/webhooks/shopify/orders-create", function (req, res) {
-  // We'll move the code that's in the orders-updated hook into here once it's functional
-  // easier to iterate in that hook for now.
-  Reaction.Endpoints.sendResponse(res);
-});
-
-Reaction.Endpoints.add("post", "/webhooks/shopify/orders-updated", function (req, res) {
+function verifyWebhook(req) {
   // ensure we have a shopId in the request query string
   if (!req || !req.query || !req.query.shopId) {
     throw new Meteor.Error("access-denied", "webhook requests must include a shopId");
@@ -34,31 +26,21 @@ Reaction.Endpoints.add("post", "/webhooks/shopify/orders-updated", function (req
     throw new Meteor.Error("server-error", `No shopify package found for shop ${Reaction.getShopId()}`);
   }
 
-  // Send response immediately
-  Reaction.Endpoints.sendResponse(res);
-
   const hmac = crypto.createHmac("sha256", shopifyPkg.settings.sharedSecret).update(req.rawBody).digest("base64");
   if (hmac !== req.headers["x-shopify-hmac-sha256"]) {
     throw new Meteor.Error("access-denied", "Could not verify webhook request was from Shopify");
   }
-  req.body.line_items.forEach((lineItem) => {
-    const variantsWithShopifyId = Products.find({ shopifyId: lineItem.variant_id }).fetch();
 
-    // iterate through the variants that match this shopifyId
-    // return the one with the longest list of ancestors
-    const productVariant = variantsWithShopifyId.reduce((shopifyVariant, variant) => {
-      if (!shopifyVariant.ancestors || !Array.isArray(shopifyVariant.ancestors)) {
-        return variant;
-      }
-      if (Array.isArray(variant.ancestors)) {
-        if (variant.ancestors.length > shopifyVariant.ancestors.length) {
-          return variant;
-        }
-      }
-      return shopifyVariant;
-    });
+  return true;
+}
 
-    // productVariant is the reaction variant we need to adjust inventory for.
-    console.log("productVariant", productVariant);
-  });
+Reaction.Endpoints.add("post", "/webhooks/shopify/orders-create", function (req, res) {
+  // We'll move the code that's in the orders-updated hook into here once it's functional
+  // easier to iterate in that hook for now.
+  Reaction.Endpoints.sendResponse(res);
+
+  // If we can verify that this request is legitimate, call our shopify/sync/orders/created
+  if (verifyWebhook(req)) {
+    Meteor.call("shopify/sync/orders/created", req.body.line_items);
+  }
 });
