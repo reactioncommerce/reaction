8916529bdf817764d4b7e9903826e29002818e75
Brent Hoover <brent@thebuddhalodge.com>
2017-09-05 13:20:27 +0800

    only show shipping methods from primaryShopId

--- imports/plugins/core/shipping/client/templates/checkout/shipping.js
+++ imports/plugins/core/shipping/client/templates/checkout/shipping.js
@@ -9,13 +9,15 @@ import { Cart } from "/lib/collections";
 function cartShippingQuotes(currentCart) {
   const cart = currentCart || Cart.findOne();
   const shipmentQuotes = [];
-
+  const primaryShopId = Reaction.getPrimaryShopId();
   if (cart) {
     if (cart.shipping) {
       for (const shipping of cart.shipping) {
         if (shipping.shipmentQuotes) {
           for (const quote of shipping.shipmentQuotes) {
-            shipmentQuotes.push(quote);
+            if (shipping.shopId === primaryShopId) {
+              shipmentQuotes.push(quote);
+            }
           }
         }
       }
