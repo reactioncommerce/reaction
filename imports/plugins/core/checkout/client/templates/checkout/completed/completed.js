7ead8c0aa54c1f30c60f553060c135d6dc1c8f88
Brent Hoover <brent@thebuddhalodge.com>
2017-09-07 02:17:31 +0800

    Completed orders should break items out by Shop (#2645)
    * Use Jeremy's new syntax and add recompose.
    
    * Redoing HTML to be responsive
    
    * Correct style names
    
    * flex-boxify the completed order page
    
    * add border to boxes
    
    * Add rest of HTML elements to preliminary mock page
    
    * Basic layout coming together. Still just static data.
    
    * Re-create this branch again
    
    * Start filling with dynamic data
    
    * Render individual items
    
    * Delete file left over from merge
    
    * Styling fixes (mostly alignment)
    
    * Dynamic values + translations
    
    * Show images
    
    * handle no image available by using placeholder
    
    * Break completed orders down by Shop
    
    * Style changes
    
    * Continuing styling tweaks
    
    * Styling tweaks plus moving variables up to container
    
    * Move data munging to order transform
    
    * Final style tweaks hopefully
    
    * Normalize order payment methods via transform
    
    * Move payments to component and show CC logos for credit card payments
    
    * Fix non-responsive order details
    
    * Move summary to component. Add "add-email" handler
    
    * Remove unneeded blaze stuff
    
    * Add submit email functionality
    
    * Make submit email functionality (mostly) work
    
    * Change the structure of the addEmail code a little bit
    
    * Fix lint errors
    
    * Replace input tag with Component. Handle add order errors.
    
    * Handle order not found
    
    * Fix display when showing multi-shop orders
    
    * Fix lint errors
    
    * Register all components
    
    * LingoHub Update :rocket:
    
    Manual push by LingoHub User: Brent Hoover.
    Project: reaction
    
    Made with :heart: by https://lingohub.com
    
    * Change per CR comment
    
    * Convert lodash function to native.
    
    * Normalize card type to uppercase
    
    * Add "Your Cart" heading
    
    * Fix alignment issues
    
    * Add overall margin so not so squished at more narrow layouts
    
    * Simplify implementation and sort objects by shop name
    
    * Build object of byShop/subtotals
    
    * Break out totals by shop
    
    * Add missing translation keys
    
    * Use billing.invoice to get shipping and taxes
    
    * Fix tax and shipping and style
    
    * Add shop name line
    
    * Pass in plain object and pass in key
    
    * Styling adjustments per Ryan
    
    * Eliminate reference to billing[0]
    
    * Add method comments
    
    * Update host and port with service config on select
    
    When changing email services the host and port were not changing, kept
    original. This update assigns the host/port from the service info.
    
    Additional fix for #2630
    
    * Fix i18nKey typo
    
    * If no stored card, show payment processor
    
    Allow for PaypalExpress or any other payment method processor to be
    made pretty/translated by i18n.
    
    * Migrate new methods over to cartOrder
    
    * Removed unused import
    
    * Update self-references
    
    * Show discounts
    
    * Don't show zero values
    
    * Change method names to new values
    
    * Compute and display discounts
    
    * Add translations
    
    * Begone ye hard-coded zero indices
    
    * More translations
    
    * Styling changes
    
    * Only show one payment method when we use the same method multiple times
    
    * Fix broken shipping method display
    
    * Center page within window
    
    * Make syntax less verbose
    
    * Add component documentation
    
    * Return all of shipmentMethod so we can show carrier and label
    
    * Add component docs
    
    * Combine header and detail and remove top line
    
    * Fix discounts (again)
    
    * More translations
    
    * Float email box to align with left details
    
    * Style the email input box
    
    * Normalize discounts as strings
    
    * Put space between shop blocks
    
    * Lint issues
    
    * Correct JSDoc
    
    * Lint fix
    
    * Remove border from "add email" button
    
    * Improve comments
    
    * Fix for "dead" cart subscription after Cart record is deleted
    
    * Add comment explaining why we need to re-init the Cart subscription
    
    * Add name to shipping address on completed order screen

--- imports/plugins/core/checkout/client/templates/checkout/completed/completed.js
+++ imports/plugins/core/checkout/client/templates/checkout/completed/completed.js
@@ -1,10 +1,5 @@
-import { Meteor } from "meteor/meteor";
-import { Session } from "meteor/session";
 import { Template } from "meteor/templating";
-import { check } from "meteor/check";
-import { Reaction, i18next } from "/client/api";
-import { Orders } from "/lib/collections";
-
+import CompletedOrderContainer from "/imports/plugins/core/checkout/client/containers/completedOrderContainer";
 
 /**
  * cartCompleted helpers
@@ -12,81 +7,7 @@ import { Orders } from "/lib/collections";
  * if order status = new translate submitted message
  */
 Template.cartCompleted.helpers({
-  orderCompleted: function () {
-    const id = Reaction.Router.getQueryParam("_id");
-    if (id) {
-      const ccoSub = Meteor.subscribe("CompletedCartOrder", Meteor.userId(), id);
-      if (ccoSub.ready()) {
-        return true;
-      }
-    }
-    return false;
-  },
-  order: function () {
-    return Orders.findOne({
-      userId: Meteor.userId(),
-      cartId: Reaction.Router.getQueryParam("_id")
-    });
-  },
-  orderStatus: function () {
-    switch (this.workflow.status) {
-      case "new":
-        return i18next.t("cartCompleted.submitted");
-      case "coreOrderWorkflow/processing":
-        return i18next.t("cartCompleted.processing");
-      case "coreOrderWorkflow/completed":
-        return i18next.t("cartCompleted.completed");
-      default:
-        return this.workflow.status;
-    }
-  },
-  userOrders: function () {
-    if (Meteor.user()) {
-      return Orders.find({
-        userId: Meteor.userId(),
-        cartId: this._id
-      });
-    }
-    return {};
+  completedOrder: function () {
+    return { component: CompletedOrderContainer };
   }
-});
-
-/**
- * cartCompleted events
- *
- * adds email to order
- */
-Template.cartCompleted.events({
-  "click #update-order": function () {
-    const templateInstance = Template.instance();
-    const email = templateInstance.find("input[name=email]").value;
-    check(email, String);
-    const cartId = Reaction.Router.getQueryParam("_id");
-
-    return Meteor.call("orders/addOrderEmail", cartId, email, (err) => {
-      if (err) {
-        Alerts.toast(i18next.t("mail.alerts.cantSendEmail"), "error");
-      } else {
-        const order = Orders.findOne({
-          userId: Meteor.userId(),
-          cartId: Reaction.Router.getQueryParam("_id")
-        });
-        Meteor.call("orders/sendNotification", order);
-      }
-    });
-  }
-});
-
-/**
- * cartCompleted onCreated
- *
- * when the order is completed we need to destroy and recreate
- * the subscription to get the new cart
- */
-Template.cartCompleted.onCreated(function () {
-  const sessionId = Session.get("sessionId");
-  const userId = Meteor.userId();
-  const cartSub = Reaction.Subscriptions.Cart = Meteor.subscribe("Cart", sessionId, userId);
-  cartSub.stop();
-  Reaction.Subscriptions.Cart = Meteor.subscribe("Cart", sessionId, userId);
 });
