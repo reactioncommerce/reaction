5ddd6eff325bb0a6d50fa66489e192e2cfd57416
Erik Kieckhafer <kieckhafer@users.noreply.github.com>
2017-09-15 16:32:44 -0700

    commit to trigger bithound

--- imports/plugins/core/ui/client/components/table/sortableTable.js
+++ imports/plugins/core/ui/client/components/table/sortableTable.js
@@ -9,7 +9,7 @@ import { registerComponent } from "@reactioncommerce/reaction-components";
 
 // SortableTable is a wrapper around ReactTable.
 // Anything that works in ReactTable should work in SortableTable OOTB (although it may not be styled).
-// ReactTable docs are available here: https://react-table.js.org/#/story/readme
+// ReactTable docs are available at: https://react-table.js.org/#/story/readme
 
 class SortableTable extends Component {
   constructor(props) {
