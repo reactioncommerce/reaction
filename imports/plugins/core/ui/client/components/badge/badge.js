add32ea6449d6ff5e54556d82a88075534f8d5df
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2017-09-19 18:37:59 -0700

    add tooltips to badge component

--- imports/plugins/core/ui/client/components/badge/badge.js
+++ imports/plugins/core/ui/client/components/badge/badge.js
@@ -4,6 +4,51 @@ import classnames from "classnames/dedupe";
 import { Components, registerComponent } from "@reactioncommerce/reaction-components";
 
 class Badge extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tooltipOpen: false
+    };
+
+    this.handleButtonMouseOver = this.handleButtonMouseOver.bind(this);
+    this.handleButtonMouseOut = this.handleButtonMouseOut.bind(this);
+  }
+
+  get isTooltipOpen() {
+    return this.state.tooltipOpen;
+  }
+
+  handleButtonMouseOver() {
+    this.setState({
+      tooltipOpen: this.props.tooltip ? true : false
+    });
+  }
+
+  handleButtonMouseOut() {
+    this.setState({
+      tooltipOpen: false
+    });
+  }
+
+  renderTooltipContent() {
+    if (this.isTooltipOpen) {
+      if (typeof this.props.tooltip === "string") {
+        return (
+          <Components.Translation defaultValue={this.props.tooltip} i18nKey={this.props.i18nKeyTooltip} />
+        );
+      }
+
+      return (
+        <div>
+          {this.props.tooltip}
+        </div>
+      );
+    }
+
+    return null;
+  }
+
   renderLabel() {
     if (this.props.label) {
       if (typeof this.props.label === "string") {
@@ -25,7 +70,7 @@ class Badge extends Component {
   }
 
   render() {
-    const { status, badgeSize, className } = this.props;
+    const { badgeSize, className, indicator, status, tooltip, tooltipAttachment } = this.props;
 
     const classes = classnames({
       "rui": true,
@@ -39,8 +84,24 @@ class Badge extends Component {
       "badge-info": status === "info",
       "badge-primary": status === "primary",
       "badge-success": status === "success",
-      "badge-warning": status ===  "warning"
+      "badge-warning": status ===  "warning",
+      "badge-indicator": indicator
     }, className);
+
+    if (tooltip) {
+      return (
+        <span
+          onMouseOut={this.handleButtonMouseOut}
+          onMouseOver={this.handleButtonMouseOver}
+        >
+          <Components.Tooltip attachment={tooltipAttachment} tooltipContent={this.renderTooltipContent()}>
+            <span className={classes} style={{ display: "inline-flex" }}>
+              {this.renderLabel()}
+            </span>
+          </Components.Tooltip>
+        </span>
+      );
+    }
 
     return (
       <span className={classes} style={{ display: "inline-flex" }}>
@@ -54,13 +115,19 @@ Badge.propTypes = {
   badgeSize: PropTypes.string,
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   i18nKeyLabel: PropTypes.string,
+  i18nKeyTooltip: PropTypes.string,
+  indicator: PropTypes.bool,
   label: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  status: PropTypes.string
+  status: PropTypes.string,
+  tooltip: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.node]),
+  tooltipAttachment: PropTypes.string
 };
 
 Badge.defaultProps = {
   badgeSize: "small",
-  status: "default"
+  indicator: false,
+  status: "default",
+  tooltipAttachment: "bottom center"
 };
 
 registerComponent("Badge", Badge);
