766b0d4706380253272a2919bdc1120e110f9765
haris vitoros <harris@artlimes.com>
2017-09-25 12:53:55 +0300

    added uol

--- imports/plugins/included/marketplace/server/methods.js
+++ imports/plugins/included/marketplace/server/methods.js
@@ -1,26 +1,23 @@
-import { Meteor } from "meteor/meteor";
-import { Reaction } from "/lib/api";
-import { Shops } from "/lib/collections";
-
-Meteor.methods({
-  "marketplace/updateShopDetails": function (doc, _id) {
-    check(_id, String);
-    check(doc, Object);
-
-    const currentShopId = Reaction.getShopId();
-    // in case the method is called though console and another's shop id is passed
-    if (_id !== currentShopId) {
-      throw new Meteor.Error(403, "Access Denied");
-    }
-
-    if (!Reaction.hasPermission("admin", this.userId, _id)) {
-      return;
-    }
-
-    Shops.update(_id, doc, function (error) {
-      if (error) {
-        throw new Meteor.Error(500, error.message);
-      }
-    });
-  }
-});
+// import { Meteor } from "meteor/meteor";
+// // import { check } from "meteor/check";
+// // import { Reaction } from "/lib/api";
+// // import { Shops } from "/lib/collections";
+//
+//
+// Meteor.methods({
+//   // "marketplace/updateShopDetails": function (doc, _id) {
+//   //   check(_id, String);
+//   //   check(doc, Object);
+//   //
+//   //   if (!Reaction.hasPermission("admin", this.userId, Reaction.getSellerShopId(this.userId))) {
+//   //     return;
+//   //   }
+//   //
+//   //   Shops.update(_id, doc, function (error) {
+//   //     if (error) {
+//   //       throw new Meteor.Error(500, error.message);
+//   //     }
+//   //   });
+//   // }
+//   //
+// });
