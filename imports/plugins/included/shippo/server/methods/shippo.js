a9d5d3f028293577791c5d0e9dc2161a1025e2c2
haris vitoros <harris@artlimes.com>
2017-09-25 11:10:52 +0300

    uol added

--- imports/plugins/included/shippo/server/methods/shippo.js
+++ imports/plugins/included/shippo/server/methods/shippo.js
@@ -409,10 +409,9 @@ export const methods = {
       const shippoAddressFrom = createShippoAddress(shop.addressBook[0], shop.emails[0].address, purpose);
       // product in the cart has to have parcel property with the dimensions
       if (cart.items && cart.items[0] && cart.items[0].parcel) {
-        const unitOfMeasure = shop && shop.unitsOfMeasure && shop.unitsOfMeasure[0].uom || "kg";
-        // at the moment shops don't have a kind of unitOfMeasure for distance
-        // so we put cm...
-        shippoParcel = createShippoParcel(cart.items[0].parcel, unitOfMeasure, "cm");
+        const unitOfMeasure = shop && shop.baseUOM || "kg";
+        const unitOfLength = shop && shop.baseUOL || "cm";
+        shippoParcel = createShippoParcel(cart.items[0].parcel, unitOfMeasure, unitOfLength);
       } else {
         errorDetails.message = "This cart has no items, or the first item has no 'parcel' property.";
         return [[errorDetails], []];
