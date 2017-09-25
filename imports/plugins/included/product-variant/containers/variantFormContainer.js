f89e69d84ee1c60f37c15373c18020d2d313fb2e
joykare <warugujoy@gmail.com>
2017-09-25 23:14:12 +0300

    Fix linting errors

--- imports/plugins/included/product-variant/containers/variantFormContainer.js
+++ imports/plugins/included/product-variant/containers/variantFormContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import _ from "lodash";
 import { compose } from "recompose";
 import { registerComponent, composeWithTracker } from "@reactioncommerce/reaction-components";
 import { Meteor } from "meteor/meteor";
