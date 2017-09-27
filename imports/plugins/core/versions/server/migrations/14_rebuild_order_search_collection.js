7094c44b3301a4a8ba9e218a2c107448ca739874
Seun Martins <seundotme@gmail.com>
2017-09-27 18:04:53 +0100

    Reset changes on 1_rebuild_account_and_order_search_collections

--- imports/plugins/core/versions/server/migrations/14_rebuild_order_search_collection.js
+++ imports/plugins/core/versions/server/migrations/14_rebuild_order_search_collection.js
@@ -3,8 +3,7 @@ import { OrderSearch } from "/lib/collections";
 import { buildOrderSearch } from "/imports/plugins/included/search-mongo/server/methods/searchcollections";
 
 Migrations.add({
-  // Migrations 12 and 13 introduced changes on Orders, so we need to rebuild the search collections.
-  // Building OrderSearch here then covers the rebuild in migration 1 (so it was moved from migration 1 to here)
+  // Migrations 12 and 13 introduced changes on Orders, so we need to rebuild the search collection
   version: 14,
   up: function () {
     OrderSearch.remove({});
--- imports/plugins/core/versions/server/migrations/1_rebuild_account_and_order_search_collections.js
+++ imports/plugins/core/versions/server/migrations/1_rebuild_account_and_order_search_collections.js
@@ -1,17 +1,22 @@
 import { Migrations } from "meteor/percolate:migrations";
-import { AccountSearch } from "/lib/collections";
-import { buildAccountSearch } from "/imports/plugins/included/search-mongo/server/methods/searchcollections";
+import { OrderSearch, AccountSearch } from "/lib/collections";
+import { buildOrderSearch,
+  buildAccountSearch } from "/imports/plugins/included/search-mongo/server/methods/searchcollections";
 
 Migrations.add({
   version: 1,
   up: function () {
+    OrderSearch.remove({});
     AccountSearch.remove();
+    buildOrderSearch();
     buildAccountSearch();
   },
   down: function () {
     // whether we are going up or down we just want to update the search collections
     // to match whatever the current code in the build methods are.
+    OrderSearch.remove({});
     AccountSearch.remove();
+    buildOrderSearch();
     buildAccountSearch();
   }
 });
