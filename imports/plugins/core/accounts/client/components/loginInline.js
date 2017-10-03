276164e6315843a4cdb9bd7b8397deba188e541c
Njeri Kieha <sylvia.kieha@andela.com>
2017-10-03 13:33:03 +0300

    render email entry form if Stripe is enabled

--- imports/plugins/core/accounts/client/components/loginInline.js
+++ imports/plugins/core/accounts/client/components/loginInline.js
@@ -1,44 +1,100 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Reaction } from "/client/api";
 import { Components } from "@reactioncommerce/reaction-components";
 
-const LoginInline = (props) => {
-  return (
-    <div className="accounts-dialog accounts-inline">
-      {Reaction.allowGuestCheckout() &&
-        <div className="checkout-guest">
-          <div className="guest-checkout">
-            <p className="text-justify">
-              <Components.Translation
-                defaultValue="Continue as a guest, and you can create an account later."
-                i18nKey="checkoutLogin.guestMessage"
+class LoginInline extends Component {
+  static propTypes = {
+    continueAsGuest: PropTypes.func,
+    renderEmailForm: PropTypes.bool
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ""
+    };
+  }
+
+  /**
+   * @summary handle setting state whenever the field on the form change
+   * @param {Event} event - the event that fired
+   * @param {String} value - the new value for the field
+   * @param {String} field - which field to modify it's value
+   * @return {null} null
+   */
+  handleFieldChange = (event, value, field) => {
+    this.setState({
+      [field]: value
+    });
+  };
+
+  /**
+   * @summary Handle submitting the email form
+   * @param {Event} event - the event that fired
+   * @returns {null} null
+   */
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("HERE", this.state);
+  }
+
+  render() {
+    if (this.props.renderEmailForm) {
+      return (
+        <div className="accounts-dialog accounts-inline">
+          <form onSubmit={this.handleSubmit} className="add-email-input">
+            <Components.Translation defaultValue="Hello! Add an email and receive order updates" i18nKey="{cartCompleted.registerGuest}" />
+            <div>
+              <Components.TextField
+                name="email"
+                type="email"
+                tabIndex="1"
+                value={this.state.email}
+                onChange={this.handleFieldChange}
               />
-            </p>
-            <Components.Button
-              status="primary"
-              buttonType="submit"
-              bezelStyle="solid"
-              className="btn-block login-button single-login-button continue-guest"
-              i18nKeyLabel="checkoutLogin.continueAsGuest"
-              label="Continue as Guest"
-              onClick={props.continueAsGuest}
-            />
-          </div>
+              <Components.Button
+                type="submit"
+                label="Add Email"
+                bezelStyle={"solid"}
+              />
+            </div>
+          </form>
         </div>
-      }
-      <div className="checkout-login">
-        <Components.Login
-          loginFormCurrentView="loginFormSignUpView"
-        />
+      );
+    }
+    return (
+      <div className="accounts-dialog accounts-inline">
+        {Reaction.allowGuestCheckout() &&
+          <div className="checkout-guest">
+            <div className="guest-checkout">
+              <p className="text-justify">
+                <Components.Translation
+                  defaultValue="Continue as a guest, and you can create an account later."
+                  i18nKey="checkoutLogin.guestMessage"
+                />
+              </p>
+              <Components.Button
+                status="primary"
+                buttonType="submit"
+                bezelStyle="solid"
+                className="btn-block login-button single-login-button continue-guest"
+                i18nKeyLabel="checkoutLogin.continueAsGuest"
+                label="Continue as Guest"
+                onClick={this.props.continueAsGuest}
+              />
+            </div>
+          </div>
+        }
+        <div className="checkout-login">
+          <Components.Login
+            loginFormCurrentView="loginFormSignUpView"
+          />
+        </div>
       </div>
-    </div>
-  );
-};
-
-LoginInline.propTypes = {
-  continueAsGuest: PropTypes.func
-};
-
+    );
+  }
+}
 
 export default LoginInline;
--- imports/plugins/core/accounts/client/containers/loginInline.js
+++ imports/plugins/core/accounts/client/containers/loginInline.js
@@ -1,23 +1,73 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Meteor } from "meteor/meteor";
-import { registerComponent } from "@reactioncommerce/reaction-components";
+import { Reaction } from "/client/api";
+import { registerComponent, composeWithTracker } from "@reactioncommerce/reaction-components";
 import LoginInline from "../components/loginInline";
 
 class LoginInlineContainer extends Component {
+  static propTypes = {
+    isStripeEnabled: PropTypes.bool
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isStripeEnabled: props.isStripeEnabled,
+      renderEmailForm: false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      isStripeEnabled: nextProps.isStripeEnabled
+    });
+  }
+
   continueAsGuest = (event) => {
     event.preventDefault();
-    Meteor.call("workflow/pushCartWorkflow", "coreCartWorkflow", "checkoutLogin");
+    if (this.state.isStripeEnabled) {
+      this.setState({
+        renderEmailForm: true
+      });
+    } else {
+      Meteor.call("workflow/pushCartWorkflow", "coreCartWorkflow", "checkoutLogin");
+    }
   }
 
   render() {
     return (
       <LoginInline
         continueAsGuest={this.continueAsGuest}
+        renderEmailForm={this.state.renderEmailForm}
       />
     );
   }
 }
 
-registerComponent("LoginInline", LoginInlineContainer);
+function composer(props, onData) {
+  let isStripeEnabled = false;
+  const subscription = Reaction.Subscriptions.Packages;
+  const primaryShopId = Reaction.getPrimaryShopId();
 
-export default LoginInlineContainer;
+  const stripePkg = Reaction.getPackageSettingsWithOptions({
+    shopId: primaryShopId,
+    name: "reaction-stripe",
+    enabled: true
+  });
+
+  if (subscription.ready()) {
+    if (stripePkg) {
+      isStripeEnabled = true;
+    }
+
+    onData(null, {
+      isStripeEnabled
+    });
+  }
+}
+
+registerComponent("LoginInline", LoginInlineContainer, composeWithTracker(composer));
+
+export default composeWithTracker(composer)(LoginInlineContainer);
