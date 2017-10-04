303d4b653142c58a20fe1e2c5bdfb07ceb4c90b1
Brent Hoover <brent@thebuddhalodge.com>
2017-10-04 08:28:20 +0800

    Revert "Show products that fail validation when publishing. (#2795)"
    This reverts commit e1298b07c88b9a6d0cd03c0bc0f6d7481e588ba7.

--- imports/plugins/included/product-variant/containers/gridItemControlsContainer.js
+++ imports/plugins/included/product-variant/containers/gridItemControlsContainer.js
@@ -4,36 +4,21 @@ import { compose } from "recompose";
 import { registerComponent, composeWithTracker } from "@reactioncommerce/reaction-components";
 import { Session } from "meteor/session";
 import { Reaction } from "/client/api";
-import { ReactionProduct } from "/lib/api";
 import GridItemControls from "../components/gridItemControls";
-import { ProductVariant } from "/lib/collections/schemas/products";
-import { Validation } from "@reactioncommerce/reaction-collections";
-
 
 const wrapComponent = (Comp) => (
   class GridItemControlsContainer extends Component {
     static propTypes = {
       isSelected: PropTypes.bool,
-      product: PropTypes.object,
-      variant: PropTypes.object
+      product: PropTypes.object
     }
 
     constructor() {
       super();
 
-      this.validation = new Validation(ProductVariant);
-
-      this.state = {
-        invalidVariant: []
-      };
-
       this.hasCreateProductPermission = this.hasCreateProductPermission.bind(this);
       this.hasChanges = this.hasChanges.bind(this);
       this.checked = this.checked.bind(this);
-    }
-
-    componentWillMount() {
-      this.variantValidation();
     }
 
     hasCreateProductPermission = () => {
@@ -48,23 +33,6 @@ const wrapComponent = (Comp) => (
       return this.props.isSelected === true;
     }
 
-    // checks whether the product variant is valid
-    variantValidation = () => {
-      const variants = ReactionProduct.getVariants(this.props.product._id);
-      let validationStatus;
-      let invalidVariant;
-      if (variants) {
-        validationStatus = variants.map((variant) => {
-          return this.validation.validate(variant);
-        });
-
-        invalidVariant = validationStatus.filter(status => status.isValid === false);
-      }
-      this.setState({
-        invalidVariant
-      });
-    }
-
     render() {
       return (
         <Comp
@@ -72,7 +40,6 @@ const wrapComponent = (Comp) => (
           hasCreateProductPermission={this.hasCreateProductPermission}
           hasChanges={this.hasChanges}
           checked={this.checked}
-          invalidVariant={this.state.invalidVariant}
         />
       );
     }
