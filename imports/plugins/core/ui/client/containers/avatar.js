304314bd54aa823cbb15c6f3c614be0023214787
Folusho Oladipo <folusho.oladipo@andela.com>
2017-10-04 14:08:14 +0100

    show the avatar of any user, not just the one who is currently logged in

--- imports/plugins/core/ui/client/containers/avatar.js
+++ imports/plugins/core/ui/client/containers/avatar.js
@@ -1,11 +1,21 @@
 import { registerComponent, composeWithTracker } from "@reactioncommerce/reaction-components";
 import { Meteor } from "meteor/meteor";
 import { Accounts } from "/lib/collections";
+import { Reaction } from "/client/api";
 import { ReactionAvatar } from "../components/avatar";
 
 const composer = (props, onData) => {
-  const user = Accounts.findOne(Meteor.userId());
-  const email = user.emails[0].address;
+  const targetUserId = Reaction.Router.getQueryParam("userId");
+  let user = Accounts.findOne(targetUserId);
+
+  if (!user) {
+    user = Accounts.findOne(Meteor.userId());
+  }
+
+  let email;
+  if (user.email && user.emails[0]) {
+    email = user.emails[0].address;
+  }
   onData(null, { email });
 };
 
