360370b6cec301bb54b96c18acdee1f60e0842c2
Mike Murray <hellomikemurray@gmail.com>
2017-10-06 09:04:57 -0700

    Filter out the primary shop out of the list of marketplace shops

--- imports/plugins/included/marketplace/client/containers/marketplaceShops.js
+++ imports/plugins/included/marketplace/client/containers/marketplaceShops.js
@@ -1,15 +1,20 @@
 import { composeWithTracker, registerComponent } from  "@reactioncommerce/reaction-components";
 import { Meteor } from "meteor/meteor";
 import { Shops } from "/lib/collections";
+import { Reaction } from "/client/api";
 import MarketplaceShops from "../components/marketplaceShops";
-
 
 const onWorkflowChange = (shopId, value) => {
   Meteor.call("marketplace/updateShopWorkflow", shopId, value);
 };
 
 const composer = (props, onData) => {
-  const shops = Shops.find({}).fetch();
+  // Get all shops, excluding the primary shop
+  const shops = Shops.find({
+    _id: {
+      $nin: [Reaction.getPrimaryShopId()]
+    }
+  }).fetch();
 
   onData(null, {
     shops,
