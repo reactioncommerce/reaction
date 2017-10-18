95022d8c1b578420c8de26958aa34f4018ba8098
Folusho Oladipo <folusho.oladipo@andela.com>
2017-10-18 13:06:32 +0100

    remove obsolete autoform code

--- imports/plugins/included/taxes-taxcloud/client/settings/taxcloud.js
+++ imports/plugins/included/taxes-taxcloud/client/settings/taxcloud.js
@@ -1,8 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
-import { AutoForm } from "meteor/aldeed:autoform";
 import { Packages } from "/lib/collections";
-import { TaxCodes } from "/imports/plugins/core/taxes/lib/collections";
 import { Reaction, i18next } from "/client/api";
 import { TaxCloudPackageConfig } from "../../lib/collections/schemas";
 import { TaxCloudSettingsForm } from "../components";
@@ -77,33 +75,5 @@ Template.taxCloudSettings.helpers({
         });
       }
     };
-  }
-});
-
-
-AutoForm.hooks({
-  "taxcloud-update-form": {
-    onSuccess: function () {
-      if (!TaxCodes.findOne({ taxCodeProvider: "taxes-taxcloud" })) {
-        Meteor.call("taxcloud/getTaxCodes", (err, res) => {
-          if (res && Array.isArray(res)) {
-            Alerts.toast(i18next.t("admin.taxSettings.shopTaxMethodsSaved"),
-              "success");
-            res.forEach((code) => {
-              Meteor.call("taxes/insertTaxCodes", Reaction.getShopId(), code,
-                "taxes-taxcloud");
-            });
-          }
-        });
-      } else {
-        Alerts.toast(i18next.t("admin.taxSettings.shopTaxMethodsAlreadySaved"),
-          "success");
-      }
-    },
-    onError: function (operation, error) {
-      return Alerts.toast(
-        `${i18next.t("admin.taxSettings.shopTaxMethodsFailed")} ${error}`,
-        "error");
-    }
   }
 });
