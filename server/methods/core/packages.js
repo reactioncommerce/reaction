f8968629ba2e181f04fc72a8e0cc0e1b09db62ba
Folusho Oladipo <folusho.oladipo@andela.com>
2017-10-18 13:04:51 +0100

    add jsdocs

--- server/methods/core/packages.js
+++ server/methods/core/packages.js
@@ -3,25 +3,39 @@ import { check } from "meteor/check";
 import { Packages } from "/lib/collections";
 import { Reaction } from "/server/api";
 
-export const methods = {
-  "package/update": function (packageName, field, value) {
-    check(packageName, String);
-    check(field, String);
-    check(value, Object);
+/**
+ * @method updatePackage
+ * @summary updates the data stored for a certain Package.
+ * @param {String} packageName - the name of the Package to update.
+ * @param {String} field - the part of the Package's data that is to
+ * be updated.
+ * @param {Object} value - the new data that's to be stored for the said
+ * Package.
+ * @since 1.5.1
+ * @return {Object} - returns an object with info about the update operation.
+ */
+export function updatePackage(packageName, field, value) {
+  check(packageName, String);
+  check(field, String);
+  check(value, Object);
 
-    if (!Reaction.hasPermission([packageName])) {
-      throw new Meteor.Error(403, `Access Denied. You don't have permissions for the ${packageName} package.`);
-    }
-
-    return Packages.update({
-      name: packageName,
-      shopId: Reaction.getShopId()
-    }, {
-      $set: {
-        [field]: value
-      }
-    });
+  if (!Reaction.hasPermission([packageName])) {
+    throw new Meteor.Error(403, `Access Denied. You don't have permissions for the ${packageName} package.`);
   }
-};
 
-Meteor.methods(methods);
+  // TODO: What if the said Package doesn't exist? Should we create it? Or
+  // throw an error?
+
+  return Packages.update({
+    name: packageName,
+    shopId: Reaction.getShopId()
+  }, {
+    $set: {
+      [field]: value
+    }
+  });
+}
+
+Meteor.methods({
+  "package/update": updatePackage
+});
