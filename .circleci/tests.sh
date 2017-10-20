1a6331dcd373c261bed467cc5a01e09f3f7ca3e6
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-10-20 10:23:44 -0400

    simplify backup test on CircleCI

--- .circleci/tests.sh
+++ .circleci/tests.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# run reaction test the first time with the +e flag so we can
+# Allow reaction tests to potentially fail the first time because
+# tests sometimes fail due to a SIGKILL during Meteor package npm install.
+# Since the install doesn't need to happen on second run, the error is
+# often avoided and the false positive doesn't fail the CircleCI build.
+#
 # This will update npm deps which takes a while
 # cfs:tempstore: updating npm dependencies -- combined-stream...
 # cfs:gridfs: updating npm dependencies -- mongodb, gridfs-stream...
-# sometimes the tests fail the first time because of a SIGKILL because of this
-set +e
-reaction test |& tee testoutput
 
-set -e
+set +e
+
+reaction test
+
+if [ $? -eq 0 ]; then
+  echo "Reaction tests have passed!"
+else
+  echo "Reaction tests failed. Trying one more time..."
+  set -e
+  reaction test
+fi
