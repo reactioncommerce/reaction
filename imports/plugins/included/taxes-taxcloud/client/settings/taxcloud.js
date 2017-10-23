a75cdb6ac35d82a6be82e629a59bdb5edde36152
Folusho Oladipo <folusho.oladipo@andela.com>
2017-10-23 20:41:31 +0100

    replace some code with the use of TaxCloudSettingsFormContainer

--- imports/plugins/included/taxes-taxcloud/client/settings/taxcloud.js
+++ imports/plugins/included/taxes-taxcloud/client/settings/taxcloud.js
@@ -1,65 +1,15 @@
-import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
-import { Reaction, i18next } from "/client/api";
-import { TaxCloudPackageConfig } from "../../lib/collections/schemas";
-import { TaxCloudSettingsForm } from "../components";
+import { TaxCloudSettingsFormContainer } from "../containers";
 
 Template.taxCloudSettings.helpers({
-  /**
-   * @method taxCloudPackageData
-   * @summary returns the data for the taxes-taxcloud Package.
-   * @since 1.5.1
-   * @return {Object} - returns data for the said Package.
-   */
-  taxCloudPackageData() {
-    return Reaction.getPackageSettings("taxes-taxcloud");
-  },
   /**
    * @method taxCloudCard
    * @summary returns a component for updating the TaxCloud settings for
    * this app.
-   * @since 1.5.1
-   * @return {Object} - returns an object that contains the component
-   * to render and some data to use as props for it.
+   * @since 1.5.2
+   * @return {Object} - an object containing the component to render.
    */
   taxCloudCard() {
-    const providerName = "taxcloud";
-    const packageName = "taxes-taxcloud";
-    const packageData = Reaction.getPackageSettings(packageName);
-    return {
-      component: TaxCloudSettingsForm,
-      schema: TaxCloudPackageConfig,
-      doc: { settings: { ...packageData.settings } },
-      docPath: `settings.${providerName}`,
-      name: `settings.${providerName}`,
-      fields: {
-        [`settings.${providerName}.apiKey`]: TaxCloudPackageConfig._schema[`settings.${providerName}.apiKey`],
-        [`settings.${providerName}.apiLoginId`]: TaxCloudPackageConfig._schema[`settings.${providerName}.apiLoginId`]
-      },
-      hideFields: [
-        `settings.${providerName}.enabled`,
-        `settings.${providerName}.refreshPeriod`,
-        `settings.${providerName}.taxCodeUrl`
-
-      ],
-      handleSubmit: (event, changedInfo, targetField) => {
-        if (!changedInfo.isValid) {
-          return;
-        }
-        Meteor.call("package/update", packageName, targetField, changedInfo.doc.settings.taxcloud, (error) => {
-          if (error) {
-            Alerts.toast(
-              i18next.t("admin.update.updateFailed", { defaultValue: "Failed to update TaxCloud settings." }),
-              "error"
-            );
-            return;
-          }
-          Alerts.toast(
-            i18next.t("admin.update.updateSucceeded", { defaultValue: "TaxCloud settings updated." }),
-            "success"
-          );
-        });
-      }
-    };
+    return { component: TaxCloudSettingsFormContainer };
   }
 });
