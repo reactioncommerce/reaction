31a313fda7e0c700592290dde36d1685816596a7
Folusho Oladipo <folusho.oladipo@andela.com>
2017-10-26 15:00:41 +0100

    remove JSX code from TaxCloudSettingsFormContainer thus limiting its concerns to data, not presentation

--- imports/plugins/included/taxes-taxcloud/client/components/taxCloudSettingsForm.js
+++ imports/plugins/included/taxes-taxcloud/client/components/taxCloudSettingsForm.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Form } from "/imports/plugins/core/ui/client/components";
+import { TaxCloudPackageConfig } from "../../lib/collections/schemas";
 
 /**
- * @file TaxCloudSettingsForm is a React Component for changing TaxCloud
+ * @file TaxCloudSettingsForm is a React Component used to change TaxCloud
  * settings.
  * @module TaxCloudSettingsForm
  */
@@ -12,27 +13,33 @@ import { Form } from "/imports/plugins/core/ui/client/components";
  * @method TaxCloudSettingsForm
  * @summary renders a form for updating TaxCloud settings.
  * @param {Object} props - some data for use by this component.
- * @property {Object} doc - current TaxCloud settings.
- * @property {String} docPath - the field in the TaxCloud Package where settings are saved.
- * @property {Object} fields - info about the fields the form is to show.
  * @property {Function} handleSubmit - a function for saving new TaxCloud settings.
- * @property {Array} hideFields - the fields (from the TaxCloud Package) to hide from the form.
- * @property {String} name - the name of this provider in lowercase (i.e taxcloud).
- * @property {Object} schema - info about the structure of the TaxCloud Package.
+ * @property {Array} hiddenFields - the fields (of the TaxCloud Package) to hide from the form.
+ * @property {Object} settings - the value of the "settings" field in the TaxCloud Package.
+ * @property {Object} shownFields - info about the fields the form is to show.
  * @since 1.5.2
  * @return {Node} - a React node containing the TaxCloud settings form.
  */
 const TaxCloudSettingsForm = (props) => {
+  const { handleSubmit, hiddenFields, settings, shownFields } = props;
   return (
-    <Form
-      schema={props.schema}
-      doc={props.doc}
-      docPath={props.docPath}
-      fields={props.fields}
-      hideFields={props.hideFields}
-      name={props.name}
-      onSubmit={props.handleSubmit}
-    />
+    <div>
+      {!settings.taxcloud.apiLoginId &&
+        <div className="alert alert-info">
+          <span data-i18n="admin.taxSettings.taxcloudCredentials">Add API Login ID to enable</span>
+          <a href="https://www.taxcloud.com/" target="_blank">TaxCloud</a>
+        </div>
+      }
+      <Form
+        schema={TaxCloudPackageConfig}
+        doc={{ settings }}
+        docPath={"settings.taxcloud"}
+        name={"settings.taxcloud"}
+        fields={shownFields}
+        hideFields={hiddenFields}
+        onSubmit={handleSubmit}
+      />
+    </div>
   );
 };
 
@@ -40,25 +47,19 @@ const TaxCloudSettingsForm = (props) => {
   * @name TaxCloudSettingsForm propTypes
   * @type {propTypes}
   * @param {Object} props - React PropTypes
-  * @property {Object} doc - data about the current TaxCloud settings.
-  * @property {String} docPath - the field in the TaxCloud Package where settings are saved.
-  * @property {Object} fields - info about the fields of the TaxCloud Package
-  * that the settings form will allow users to change.
   * @property {Function} handleSubmit - a function that saves new TaxCloud settings.
-  * @property {Array} hideFields - an array of the TaxCloud Package's fields
+  * @property {Array} hiddenFields - an array of the TaxCloud Package's fields
   * to hide from the settings form.
-  * @property {String} name - the name of this taxes provider in lowercase.
-  * @property {Object} schema - info about the structure of the TaxCloud Package.
+  * @property {Object} settings - the value of the "settings" field in the TaxCloud Package.
+  * @property {Object} shownFields - info about the fields of the TaxCloud Package
+  * that the settings form will allow users to change.
   * @return {Array} React propTypes
   */
 TaxCloudSettingsForm.propTypes = {
-  doc: PropTypes.object,
-  docPath: PropTypes.string,
-  fields: PropTypes.object,
   handleSubmit: PropTypes.func,
-  hideFields: PropTypes.arrayOf(PropTypes.string),
-  name: PropTypes.string,
-  schema: PropTypes.object
+  hiddenFields: PropTypes.arrayOf(PropTypes.string),
+  settings: PropTypes.object,
+  shownFields: PropTypes.object
 };
 
 export default TaxCloudSettingsForm;
--- imports/plugins/included/taxes-taxcloud/client/containers/taxCloudSettingsFormContainer.js
+++ imports/plugins/included/taxes-taxcloud/client/containers/taxCloudSettingsFormContainer.js
@@ -1,26 +1,16 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import { compose, withProps } from "recompose";
 import { composeWithTracker, registerComponent } from  "@reactioncommerce/reaction-components";
 import { Meteor } from "meteor/meteor";
-import { Packages } from "/lib/collections";
 import { Reaction, i18next } from "/client/api";
 import { TaxCloudPackageConfig } from "../../lib/collections/schemas";
 import { TaxCloudSettingsForm } from "../components";
 
 /**
- * @file TaxCloudSettingsFormContainer is a React Component that serves
- * as a container for TaxCloudSettingsForm.
- * @module TaxCloudSettingsFormContainer
- * @extends Component
+ * @file This is a container for TaxCloudSettingsForm.
+ * @module taxCloudSettingsFormContainer
  */
 
-class TaxCloudSettingsFormContainer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
+const handlers = {
   /**
     * handleSubmit
     * @method
@@ -28,7 +18,6 @@ class TaxCloudSettingsFormContainer extends Component {
     * @param {Object} event - event info.
     * @param {Object} changedInfo - info about the new TaxCloud settings.
     * @param {String} targetField - where to save the new settings in the TaxCloud Package.
-    * @property {String} packageName - the name of this tax provider in lowercase.
     * @since 1.5.2
     * @return {null} - returns nothing
     */
@@ -36,7 +25,7 @@ class TaxCloudSettingsFormContainer extends Component {
     if (!changedInfo.isValid) {
       return;
     }
-    Meteor.call("package/update", this.props.packageName, targetField, changedInfo.doc.settings.taxcloud, (error) => {
+    Meteor.call("package/update", "taxes-taxcloud", targetField, changedInfo.doc.settings.taxcloud, (error) => {
       if (error) {
         Alerts.toast(
           i18next.t("admin.update.updateFailed", { defaultValue: "Failed to update TaxCloud settings." }),
@@ -50,68 +39,30 @@ class TaxCloudSettingsFormContainer extends Component {
       );
     });
   }
-
-  render() {
-    const providerName = this.props.providerName;
-    const packageSettings = this.props.settings;
-    const shownFields = {
-      [`settings.${providerName}.apiKey`]: TaxCloudPackageConfig._schema[`settings.${providerName}.apiKey`],
-      [`settings.${providerName}.apiLoginId`]: TaxCloudPackageConfig._schema[`settings.${providerName}.apiLoginId`]
-    };
-    const hiddenFields = [
-      `settings.${providerName}.enabled`,
-      `settings.${providerName}.refreshPeriod`,
-      `settings.${providerName}.taxCodeUrl`
-    ];
-
-    return (
-      <div>
-        {!packageSettings.taxcloud.apiLoginId &&
-          <div className="alert alert-info">
-            <span data-i18n="admin.taxSettings.taxcloudCredentials">Add API Login ID to enable</span>
-            <a href="https://www.taxcloud.com/" target="_blank">TaxCloud</a>
-          </div>
-        }
-        <TaxCloudSettingsForm
-          schema={TaxCloudPackageConfig}
-          doc={{ settings: packageSettings }}
-          docPath={`settings.${providerName}`}
-          name={`settings.${providerName}`}
-          fields={shownFields}
-          hideFields={hiddenFields}
-          handleSubmit={this.handleSubmit}
-        />
-      </div>
-    );
-  }
-}
-
-/**
-  * @name TaxCloudSettingsFormContainer propTypes
-  * @type {propTypes}
-  * @param {Object} props - React PropTypes
-  * @property {String} packageName - the value of the "name" field in the TaxCloud Package.
-  * @property {String} providerName - the name of this provider in lowercase i.e "taxcloud".
-  * @property {Object} settings - the value of the "settings" field in the TaxCloud Package.
-  * @return {Array} React propTypes
-  */
-TaxCloudSettingsFormContainer.propTypes = {
-  packageName: PropTypes.string,
-  providerName: PropTypes.string,
-  settings: PropTypes.object
 };
 
 const composer = (props, onData) => {
+  const providerName = "taxcloud";
+  const shownFields = {
+    [`settings.${providerName}.apiKey`]: TaxCloudPackageConfig._schema[`settings.${providerName}.apiKey`],
+    [`settings.${providerName}.apiLoginId`]: TaxCloudPackageConfig._schema[`settings.${providerName}.apiLoginId`]
+  };
+  const hiddenFields = [
+    `settings.${providerName}.enabled`,
+    `settings.${providerName}.refreshPeriod`,
+    `settings.${providerName}.taxCodeUrl`
+  ];
+
   const shopId = Reaction.getShopId();
   const packageSub = Meteor.subscribe("Packages", shopId);
   if (packageSub.ready()) {
-    const providerName = "taxcloud";
-    const packageName = "taxes-taxcloud";
-    const packageData = Reaction.getPackageSettings(packageName);
-    onData(null, { ...packageData, packageName, providerName });
+    const packageData = Reaction.getPackageSettings("taxes-taxcloud");
+    onData(null, { settings: packageData.settings, shownFields, hiddenFields });
   }
 };
 
-registerComponent("TaxCloudSettingsFormContainer", TaxCloudSettingsFormContainer, composeWithTracker(composer));
+registerComponent("TaxCloudSettingsForm", TaxCloudSettingsForm, [
+  withProps(handlers), composeWithTracker(composer)
+]);
 
-export default composeWithTracker(composer)(TaxCloudSettingsFormContainer);
+export default compose(withProps(handlers), composeWithTracker(composer))(TaxCloudSettingsForm);
