cd1322c77c5f77fe998c3924d4bc8e18296554d0
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-11-07 10:19:59 -0500

    handle unhandled promise rejections

--- server/main.js
+++ server/main.js
@@ -2,6 +2,11 @@ import { Meteor } from "meteor/meteor";
 import "./methods";
 import Startup from "./startup";
 import Security from "./security";
+import { Logger } from "/server/api";
+
+process.on("unhandledRejection", (err) => {
+  Logger.error(err);
+});
 
 Meteor.startup(() => {
   Startup();
