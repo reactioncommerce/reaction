715105e7eb1b2f721dd042e1a5652c6ebec46b76
Machiko Yasuda <machiko@reactioncommerce.com>
2017-11-08 13:34:26 -0800

    jsdoc: add stubbed method

--- imports/plugins/core/shipping/server/methods/methods.js
+++ imports/plugins/core/shipping/server/methods/methods.js
@@ -9,8 +9,17 @@ import { shippingRoles } from "../lib/roles";
  * @namespace Methods/Shipping
  */
 
-
 export const methods = {
+  /**
+   * @name shipping/status/refresh
+   * @method
+   * @memberof Methods/Shipping
+   * @todo This is a stub for future core processing
+   * @summary Blank method. Serves as a place for Method Hooks,
+   * in other shipping packages, like Shippo
+   * @param  {String} orderId order ID
+   * @return {String}         order ID
+   */
   "shipping/status/refresh": function (orderId) {
     check(orderId, String);
     // this is a stub for future core processing
@@ -18,15 +27,16 @@ export const methods = {
     // in other shipping packages, like Shippo
     return orderId;
   },
+
   /**
    * @name shipping/provider/toggle
    * @method
    * @memberof Methods/Shipping
    * @example Meteor.call("shipping/provider/toggle", packageId, settingsKey)
    * @summary Toggle enabled provider
-   * @param { String } packageId - packageId
-   * @param { String } provider - provider name
-   * @return { Number } update - result
+   * @param { String } packageId packageId
+   * @param { String } provider provider name
+   * @return { Number } update result
    */
   "shipping/provider/toggle": function (packageId, provider) {
     check(packageId, String);
