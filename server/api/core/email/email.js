d9708d2c98c57f2e42487d9da8d48ce5006619f9
Machiko Yasuda <machiko@reactioncommerce.com>
2017-11-09 17:47:12 -0800

    doc: add methods from Email into its own Email namespace

--- server/api/core/email/email.js
+++ server/api/core/email/email.js
@@ -3,10 +3,27 @@ import { Job } from "meteor/vsivsi:job-collection";
 import { Jobs, Templates } from "/lib/collections";
 import { Reaction, Logger } from "/server/api";
 
+/**
+ * @file `Reaction.Email` methods for creating e-mail jobs, fetching e-mail templates and e-mail subject lines
+ *
+ * @see https://github.com/reactioncommerce/reaction/pull/1282
+ * @namespace Email
+ */
 
 /**
- * Reaction.Email.send()
- * (Job API doc) https://github.com/vsivsi/meteor-job-collection/#user-content-job-api
+ * @method send
+ * @memberof Email
+ * @summary Add send e-mail job to job queue.
+ * The worker will then process it immediately (in batches of up to 10) and will retry failures up to 5 times
+ * (waiting 3 mins between each try) before failing completely.
+ * All email sending attempts are logged in the job collection.
+ * @see (Job API doc) https://github.com/vsivsi/meteor-job-collection/#user-content-job-api
+ * @example Reaction.Email.send({
+    from: 'me@example.com',
+    to: 'you@example.com',
+    subject: 'RE: new email API',
+    html: SSR.render('some-name', { shopUrl: Meteor.absoluteUrl() })
+  });
  * @param  {Object} options - object containing to/from/subject/html String keys
  * @return {Boolean} returns job object
  */
@@ -18,10 +35,12 @@ export function send(options) {
     }).save();
 }
 
-
 /**
- * Reaction.Email.getSubject() - Returns a subject source for SSR consumption
+ * @method getSubject
+ * @memberof Email
+ * @summary Returns a subject source for SSR consumption
  * layout must be defined + template
+ * @example SSR.compileTemplate(subject, Reaction.Email.getSubject(tpl));
  * @param {String} template name of the template in either Layouts or fs
  * @returns {Object} returns source
  */
@@ -49,8 +68,10 @@ export function getSubject(template) {
 }
 
 /**
- * Reaction.Email.getTemplate() - Returns a template source for SSR consumption
- * layout must be defined + template
+ * @method getTemplate
+ * @memberof Email
+ * @summary Returns a template source for SSR consumption. layout must be defined + template
+ * @example Reaction.Email.getTemplate('path/of/template');
  * @param {String} template name of the template in either Layouts or fs
  * @returns {Object} returns source
  */
@@ -80,7 +101,8 @@ export function getTemplate(template) {
 }
 
 /**
- * Reaction.Email.getTemplateFile
+ * @method getTemplateFile
+ * @memberof Email
  * @param  {String} file name of the template on file system
  * @return {String} returns source
  */
