c8c2a9df0d3069a8eda1a51c44c7702407fa5fa3
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-11-10 09:14:01 -0500

    debug log MongoError if driver attempts to remove collection that doesn’t exist yet

--- server/main.js
+++ server/main.js
@@ -4,8 +4,16 @@ import Startup from "./startup";
 import Security from "./security";
 import { Logger } from "/server/api";
 
+// handle any unhandled Promise rejections
 process.on("unhandledRejection", (err) => {
-  Logger.error(err);
+  // If we get an error from the Mongo driver because something tried to drop a
+  // collection before it existed, log it out as debug info.
+  // Otherwise, log whatever happened as an error.
+  if (err.name === "MongoError" && err.message === "ns not found") {
+    Logger.debug(err);
+  } else {
+    Logger.error(err);
+  }
 });
 
 Meteor.startup(() => {
