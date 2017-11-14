79fc355a9b11ae7958156e7f7fda0c5d90e7c703
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-11-14 08:50:20 -0500

    add back logging handler for unhandled Promise rejections

--- server/main.js
+++ server/main.js
@@ -2,6 +2,13 @@ import { Meteor } from "meteor/meteor";
 import "./methods";
 import Startup from "./startup";
 import Security from "./security";
+import { Logger } from "/server/api";
+
+// handle any unhandled Promise rejections because
+// Node 8 no longer swallows them
+process.on("unhandledRejection", (err) => {
+  Logger.error(err);
+});
 
 Meteor.startup(() => {
   Startup();
