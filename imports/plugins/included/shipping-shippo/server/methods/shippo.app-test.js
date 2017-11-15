929f94f5335a3b18c14ea0db1c14374dc6d08976
Esther Falayi <estherfalayi@Esther-Falayi.local>
2017-11-15 17:12:26 +0100

    - Create stub function for test

--- imports/plugins/included/shipping-shippo/server/methods/shippo.app-test.js
+++ imports/plugins/included/shipping-shippo/server/methods/shippo.app-test.js
@@ -1,37 +1,29 @@
+/* eslint dot-notation: 0 */
 import { Meteor } from "meteor/meteor";
-// import { Roles } from "meteor/alanning:roles";
-// import { expect } from "meteor/practicalmeteor:chai";
-import { sinon } from "meteor/practicalmeteor:sinon";
+import { check, Match } from "meteor/check";
 import { Factory } from "meteor/dburles:factory";
 import { Reaction } from "/server/api";
-import { Shops } from "/lib/collections";
+import { Cart } from "/lib/collections";
+// import { expect } from "meteor/practicalmeteor:chai";
+import { sinon } from "meteor/practicalmeteor:sinon";
+import { getCartItem } from "/server/imports/fixtures/cart";
+import Fixtures from "/server/imports/fixtures";
 
-// const parcelSize = {
-//   weight: 10,
-//   length: 10,
-//   width: 10,
-//   height: 10
-// };
+Fixtures();
+
 const shippoDocs = {};
 const retrialTargets = [];
 
-let cart;
-let shop;
-// let initialDefaultParcelSize;
-before(function () {
-  this.timeout(10000);
-  Meteor._sleepForMs(7000);
-  cart = Factory.create("cart");
-  shop = Factory.create("shop");
-  // initialDefaultParcelSize = shop.defaultParcelSize;
-});
-
-after(function () {
-  Shops.direct.remove();
-});
-
-describe.only("shippo method", function () {
+describe.only("Shippo methods", function () {
   let sandbox;
+  let originals;
+
+  before(function () {
+    originals = {
+      addToCart: Meteor.server.method_handlers["cart/addToCart"],
+      getShippingRatesForCart: Meteor.server.method_handlers["shippo/getShippingRatesForCart"]
+    };
+  });
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
@@ -41,19 +33,73 @@ describe.only("shippo method", function () {
     sandbox.restore();
   });
 
+  after(() => {
+    Meteor.users.remove({});
+  });
+
+  afterEach(function () {
+    Meteor.users.remove({});
+  });
+
+
+  function spyOnMethod(method, id) {
+    return sandbox.stub(Meteor.server.method_handlers, `cart/${method}`, function () {
+      check(arguments, [Match.Any]);
+      this.userId = id;
+      return originals[method].apply(this, arguments);
+    });
+  }
+
+  function spyOnShippo(method, id) {
+    return sandbox.stub(Meteor.server.method_handlers, `shippo/${method}`, function () {
+      check(arguments, [Match.Any]); // to prevent audit_arguments from complaining
+      this.userId = id;
+      return originals[method].apply(this, arguments);
+    });
+  }
 
   describe("shippo/getShippingRatesForCart", function () {
-    it("should set shipping rates for cart", function (done) {
-      sandbox.stub(Reaction, "hasPermission", () => true);
-      sandbox.stub(Meteor, "userId", function () {
-        return cart.userId;
+    const quantity = 1;
+    let permissionStub;
+    let resetShipmentStub;
+    let updateShipmentQuoteStub;
+
+    before(function () {
+      permissionStub = sinon.stub(Reaction, "hasPermission", function () {
+        return true;
       });
-      sandbox.stub(Reaction, "getShopId", () => {
-        return shop._id;
+
+      resetShipmentStub = sinon.stub(Meteor.server.method_handlers, "cart/resetShipmentMethod", function () {
+        check(arguments, [Match.Any]);
+        return true;
       });
-      // console.log("cart: items: ", cart.items[0]);
-      Meteor.call("shippo/getShippingRatesForCart", cart._id, shippoDocs, retrialTargets);
-      return done();
+      updateShipmentQuoteStub = sinon.stub(Meteor.server.method_handlers, "shipping/updateShipmentQuotes", function () {
+        check(arguments, [Match.Any]);
+        return true;
+      });
+    });
+
+    after(function () {
+      permissionStub.restore();
+      resetShipmentStub.restore();
+      updateShipmentQuoteStub.restore();
+    });
+
+    beforeEach(function () {
+      Cart.remove({});
+    });
+
+    it("should add item to cart", function (done) {
+      let cart = Factory.create("cart");
+      const cartId = cart._id;
+      const { productId, variants } = getCartItem();
+      spyOnMethod("addToCart", cart.userId);
+      Meteor.call("cart/addToCart", productId, variants._id, quantity);
+      Meteor._sleepForMs(1000);
+      cart = Cart.findOne(cartId);
+      spyOnShippo("getShippingRatesForCart", cart.userId);
+      Meteor.call("shippo/getShippingRatesForCart", cartId, shippoDocs, retrialTargets);
+      done();
     });
   });
 });
