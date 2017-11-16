b97b0757d7465e17166aaf7142fc0a2e829829e2
Folusho Oladipo <folusho.oladipo@andela.com>
2017-11-16 14:02:16 +0100

    fix failing tests

--- server/api/core/hasPermission.app-test.js
+++ server/api/core/hasPermission.app-test.js
@@ -4,7 +4,7 @@ import { sinon } from "meteor/practicalmeteor:sinon";
 import { Factory } from "meteor/dburles:factory";
 import Core from "./core";
 
-describe.only("hasPermission", function () {
+describe("hasPermission", function () {
   let sandbox;
 
   beforeEach(function () {
@@ -15,42 +15,54 @@ describe.only("hasPermission", function () {
     sandbox.restore();
   });
 
-  it.only("should return false when not passed any permissions", function () {
+  it("should return false when not passed any permissions", function () {
     sandbox.stub(Meteor, "userId", () => "random-user-id");
     const hasPermissions = Core.hasPermission();
     expect(hasPermissions).to.be.false;
   });
 
-  it.only("should return false when `permissions` is an empty string", function () {
+  it("should return false when `permissions` is an empty string", function () {
     sandbox.stub(Meteor, "userId", () => "random-user-id");
     const hasPermissions = Core.hasPermission("");
     expect(hasPermissions).to.be.false;
   });
 
-  it.only("should return false when `permissions` is an empty array", function () {
+  it("should return false when `permissions` is an empty array", function () {
     sandbox.stub(Meteor, "userId", () => "random-user-id");
     const hasPermissions = Core.hasPermission([]);
     expect(hasPermissions).to.be.false;
   });
 
-  it.only("should return false for users that don't have the `owner` permission", function () {
+  it("should return false for users that don't have the `owner` permission", function () {
     const registeredUser = Factory.create("registeredUser");
     sandbox.stub(Meteor, "userId", () => registeredUser._id);
     const hasPermissions = Core.hasPermission(["owner"]);
     expect(hasPermissions).to.be.false;
   });
 
-  it.only("should return true for anonymous users when `permissions === ['anonymous', 'guest']`", function () {
+  it("should return true for anonymous users when `permissions === ['anonymous', 'guest']`", function () {
     const user = Factory.create("anonymous");
     sandbox.stub(Meteor, "userId", () => user._id);
-    const hasPermissions = Core.hasPermission(["anonymous", "guest"]);
+
+    // It is necessary to explicity supply shopId to the Core.hasPermission call
+    // below. If we don't, the said function defaults to using Core.getShopId
+    // which, when this test is run, will give a shop ID that will be different
+    // from the one used by Factory.create("anonymous").
+    const shopId = Object.keys(user.roles)[0];
+    const hasPermissions = Core.hasPermission(["anonymous", "guest"], user._id, shopId);
     expect(hasPermissions).to.be.true;
   });
 
-  it.only("should return true for registered users when `permissions === ['account/profile', 'guest']`", function () {
+  it("should return true for registered users when `permissions === ['account/profile', 'guest']`", function () {
     const user = Factory.create("registeredUser");
     sandbox.stub(Meteor, "userId", () => user._id);
-    const hasPermissions = Core.hasPermission(["account/profile", "guest"]);
+
+    // It is necessary to explicity supply shopId to the Core.hasPermission call
+    // below. If we don't, the said function defaults to using Core.getShopId
+    // which, when this test is run, will give a shop ID that will be different
+    // from the one used by Factory.create("registeredUser").
+    const shopId = Object.keys(user.roles)[0];
+    const hasPermissions = Core.hasPermission(["account/profile", "guest"], user._id, shopId);
     expect(hasPermissions).to.be.true;
   });
 });
