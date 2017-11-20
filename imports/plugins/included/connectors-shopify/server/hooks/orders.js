0a27210a72f70537c3020b2dfb69f8b6b05c8d05
Brent Hoover <brent@thebuddhalodge.com>
2017-11-20 10:07:57 +0800

    Converting lineitems

--- imports/plugins/included/connectors-shopify/server/hooks/orders.js
+++ imports/plugins/included/connectors-shopify/server/hooks/orders.js
@@ -1,7 +1,8 @@
 /* eslint camelcase: 0 */
+import accounting from "accounting-js";
 import Shopify from "shopify-api-node";
 import { Reaction, Logger } from "/server/api";
-import { Orders } from "/lib/collections";
+import { Orders, Shops } from "/lib/collections";
 import { getApiInfo } from "../methods/api";
 
 export function exportToShopify(doc) {
@@ -11,12 +12,13 @@ export function exportToShopify(doc) {
   const numShopOrders = doc.billing.length; // if we have multiple billing, we have multiple shops
   for (let i = 0; i < numShopOrders; i++) {
     // send a shopify order once for each merchant order
-    const shopifyOrder = convertRcOrderToShopifyOrder(doc, i);
+    const shopId = doc.billing[i].shopId;
+    const shopifyOrder = convertRcOrderToShopifyOrder(doc, i, shopId);
     shopify.order.create(shopifyOrder);
   }
 }
 
-function convertRcOrderToShopifyOrder(order, index) {
+function convertRcOrderToShopifyOrder(order, index, shopId) {
   const shopifyOrder = {};
   const billingAddress = convertAddress(order.billing[index]);
   shopifyOrder.billing_address = billingAddress;
@@ -31,18 +33,82 @@ function convertRcOrderToShopifyOrder(order, index) {
     shopifyOrder.financial_status = "paid";
   }
   shopifyOrder.id = order._id;
-  shopifyOrder.line_items = convertLineItems(order.items);
+  const itemsForShop = order.getItemsByShop()[shopId];
+  shopifyOrder.line_items = convertLineItems(itemsForShop, order);
   shopifyOrder.transactions = convertTransactions(order.billing[index].paymentMethod);
   return shopifyOrder;
 }
 
-function convertLineItems(items) {
-  // does not actually convert items yet
-  return items;
+function normalizeWeight(weight, shopId) {
+  // if weight is not grams, convert to grams if is grams just return
+  const shop = Shops.findOne(shopId);
+  const { baseUOM } = shop;
+  // Could've used switch, this was easier to read
+  if (baseUOM === "g") {
+    return weight;
+  }
+  if (baseUOM === "lb") {
+    const grams = weight * 453.592;
+    return grams;
+  }
+
+  if (baseUOM === "oz") {
+    const grams = weight * 28.35;
+    return grams;
+  }
+
+  if (baseUOM === "kg") {
+    const grams = weight / 1000;
+    return grams;
+  }
+}
+
+function convertLineItems(items, order) {
+  const lineItems = items.map((item) => {
+    const lineItem = {};
+    lineItem.fulfillable_quantity = item.quantity;
+    lineItem.fulfillment_service = "manual";
+    lineItem.fullfillment_status = null;
+    lineItem.grams = normalizeWeight(item.parcel.weight, item.shopId);
+    lineItem.id = item._id;
+    lineItem.product_id = item.productId;
+    lineItem.quantity = item.quantity;
+    lineItem.requires_shipping = item.product.requiresShipping;
+    // lineItem.sku = ??? Not sure what should be the SKU here
+    lineItem.title = item.product.title;
+    lineItem.variant_id = item.variants._id;
+    lineItem.variant_title = item.variants.title;
+    lineItem.vendor = item.vendor;
+    lineItem.taxable = item.variants.taxable;
+    if (order.taxes) {
+      lineItem.tax_lines = [];
+      // when using Avalara we get tax detail
+      // get the tax iten for this particular line
+      const taxItem = order.taxes.filter((tax) => tax.lineNumber === item._id);
+      taxItem.details.forEach((detail) => {
+        const taxLine = {
+          title: detail.taxName,
+          price: accounting.toFixed(detail.taxCalculated, 2),
+          rate: detail.rate
+        };
+        lineItem.tax_lines.push(taxLine);
+      });
+    }
+    // for custom tax codes we get this one data point
+    if (item.taxData) {
+      lineItem.tax_lines = [{
+        title: item.taxData.taxCode,
+        price: accounting.toFixed((item.taxData.rate / 100) * item.variants.price, 2),
+        rate: item.taxData.rate / 100
+      }];
+    }
+    // lineItem.total_discount = ????;
+    return lineItem;
+  });
+  return lineItems;
 }
 
 function convertTransactions(paymentMethod) {
-  // does not actually convert paymentmethod to transactions yet
   return paymentMethod;
 }
 
