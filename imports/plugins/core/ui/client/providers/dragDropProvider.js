6a72c3d42884aaabe3b3109338552b2b9c397672
Mike Murray <hellomikemurray@gmail.com>
2017-12-18 15:10:28 -0800

    (fix): Remove commented code

--- imports/plugins/core/ui/client/providers/dragDropProvider.js
+++ imports/plugins/core/ui/client/providers/dragDropProvider.js
@@ -6,23 +6,6 @@ import { registerComponent } from "@reactioncommerce/reaction-components";
 
 const defaultManager = new DragDropManager(HTML5Backend);
 
-// /**
-//  * This is singleton used to initialize only once dnd in our app.
-//  * If you initialized dnd and then try to initialize another dnd
-//  * context the app will break.
-//  * Here is more info: https://github.com/gaearon/react-dnd/issues/186
-//  *
-//  * The solution is to call Dnd context from this singleton this way
-//  * all dnd contexts in the app are the same.
-//  */
-// export default function getDndContext() {
-//   if (defaultManager) return defaultManager;
-//
-//   defaultManager = new DragDropManager(HTML5Backend);
-//
-//   return defaultManager;
-// }
-
 class DragDropProvider extends Component {
   getChildContext() {
     return {
