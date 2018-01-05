87c6ac57716e0b0ba2c4d77cee1932a3d3485a34
Akhator Itua <iakhator@Iakhator.local>
2018-01-05 15:58:40 +0100

    (refactor): convert array to object write down() migration comment code

--- imports/plugins/core/versions/server/migrations/19_add_object_of_boolean_to_payments_schema.js
+++ imports/plugins/core/versions/server/migrations/19_add_object_of_boolean_to_payments_schema.js
@@ -16,34 +16,105 @@ const PaypalSupport = {
 Migrations.add({
   version: 19,
   up() {
+    // looping through paymentMethodName to get each paymentMethod
     Object.keys(paymentMethodName).forEach((key) => {
       let packages;
       let support;
       if (paymentMethodName[key] === "reaction-paypal") {
         packages = Packages.find({ name: paymentMethodName[key] });
+        // Loop through all packages and update support to be an object
         packages.forEach((pkg) => {
+          // Lopp through all paypalsupport of payment to get each paymentmethod
           Object.keys(PaypalSupport).forEach((paypal) => {
             support = pkg.settings[PaypalSupport[paypal]].support;
-            Packages.update({ _id: pkg._id }, {
-              $set: {
-                [`settings.${PaypalSupport[paypal]}.support`]: support
-              }
-            });
+            // Checking to make sure support is an array
+            if (Array.isArray(support)) {
+              const supportObject = {};
+              support.forEach((method) => {
+                supportObject[method.toLowerCase()] = true;
+              });
+              // Update the package  settings support document with the new support
+              Packages.update({ _id: pkg._id }, {
+                $set: {
+                  [`settings.${PaypalSupport[paypal]}.support`]: supportObject
+                }
+              });
+            }
           });
         });
       } else {
         packages = Packages.find({ name: paymentMethodName[key] });
+        // Loop through all packages and update support to be an object
         packages.forEach((pkg) => {
           support = pkg.settings[paymentMethodName[key]].support;
-          Packages.update({ _id: pkg._id }, {
-            $set: {
-              [`settings.${paymentMethodName[key]}.support`]: support
-            }
-          });
+          // Checking to make sure support is an array
+          if (Array.isArray(support)) {
+            const supportObject = {};
+            // loop through support array
+            support.forEach((method) => {
+              supportObject[method.toLowerCase()] = true;
+            });
+            // Update the package  settings support document with the new support
+            Packages.update({ _id: pkg._id }, {
+              $set: {
+                [`settings.${paymentMethodName[key]}.support`]: supportObject
+              }
+            });
+          }
         });
       }
     });
   },
   down() {
+    // looping through paymentMethodName to get each paymentMethod
+    Object.keys(paymentMethodName).forEach((key) => {
+      let packages;
+      let support;
+      // reaction-paypal has paymentMethods of payflow and paypal
+      // checking if paymentMethodName equals reaction-paypal
+      if (paymentMethodName[key] === "reaction-paypal") {
+        packages = Packages.find({ name: paymentMethodName[key] });
+        // Loop through all packages and update support to be an array
+        packages.forEach((pkg) => {
+          Object.keys(PaypalSupport).forEach((paypal) => {
+            support = pkg.settings[PaypalSupport[paypal]].support;
+            // checking if support is an object
+            if (typeof support === "object") {
+              const supportArray = [];
+              // loop through support object and push into supportArray;
+              Object.keys(support).forEach((method) => {
+                supportArray.push(method.charAt(0).toUpperCase() + method.slice(1));
+              });
+              // Update the package  settings support document with the new support
+              Packages.update({ _id: pkg._id }, {
+                $set: {
+                  [`settings.${PaypalSupport[paypal]}.support`]: supportArray
+                }
+              });
+            }
+          });
+        });
+      } else {
+        packages = Packages.find({ name: paymentMethodName[key] });
+        // Loop through all packages and update support to be an array
+        packages.forEach((pkg) => {
+          support = pkg.settings[paymentMethodName[key]].support;
+          if (typeof support === "object") {
+            // intiliaze an empty array
+            const supportArray = [];
+            // loop through support object and push into supportArray;
+            Object.keys(support).forEach((method) => {
+              supportArray.push(method.charAt(0).toUpperCase() + method.slice(1));
+            });
+            // Update the package  settings support document with the new support
+            Packages.update({ _id: pkg._id }, {
+              $set: {
+                [`settings.${paymentMethodName[key]}.support`]: supportArray
+              }
+            });
+          }
+        });
+      }
+    });
   }
 });
