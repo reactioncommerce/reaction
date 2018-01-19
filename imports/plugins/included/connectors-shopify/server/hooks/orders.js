63130ad66981d2b80eed7bad8647aae43c0899c5
Spencer Norman <spencern@gmail.com>
2018-01-19 13:39:47 -0700

    Export orders from merchant shops in addition to primary shop

--- imports/plugins/included/connectors-shopify/server/hooks/orders.js
+++ imports/plugins/included/connectors-shopify/server/hooks/orders.js
@@ -15,16 +15,31 @@ function addExportJob(orderId) {
 
 
 Orders.after.insert((userId, doc) => {
-  const pkgData = Reaction.getPackageSettings("reaction-connectors-shopify");
-  if (pkgData) {
-    const { settings } = pkgData;
-    const { synchooks } = settings;
-    if (synchooks) {
-      synchooks.forEach((hook) => {
-        if (hook.topic === "orders" && hook.event === "orders/create" && hook.syncType === "exportToShopify") {
-          addExportJob(doc._id);
-        }
+  // Ensure we have items
+  if (doc && doc.items && Array.isArray(doc.items)) {
+    // Get all shopIds represented in this cart
+    const shopIdsInCart = doc.items.map((item) => item.shopId);
+
+    // get the unique set of shopIds in this cart
+    const uniqueShopIds = [... new Set(shopIdsInCart)];
+
+    // For each shopid check to see if there are synchooks are respond appropriately
+    uniqueShopIds.forEach((shopId) => {
+      const shopifyPkg = Reaction.getPackageSettingsWithOptions({
+        shopId: shopId,
+        name: "reaction-connectors-shopify"
       });
-    }
+      if (shopifyPkg) {
+        const { settings } = shopifyPkg;
+        const { synchooks } = settings;
+        if (synchooks) {
+          synchooks.forEach((hook) => {
+            if (hook.topic === "orders" && hook.event === "orders/create" && hook.syncType === "exportToShopify") {
+              addExportJob(doc._id);
+            }
+          });
+        }
+      }
+    });
   }
 });
