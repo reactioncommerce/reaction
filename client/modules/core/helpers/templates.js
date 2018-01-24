2e7b1d0e7b434f33cd3b9811167b7673ab20edde
Nat Hamilton <info@nathamilton.com>
2018-01-24 13:43:46 -0600

    updated unary operators on lines 112 & 117 from ++ to += 1 in client/modules/core/helpers/templates.js

--- client/modules/core/helpers/templates.js
+++ client/modules/core/helpers/templates.js
@@ -109,12 +109,12 @@ Template.registerHelper("yearOptions", function (showDefaultOption = true) {
   }
 
   let year = new Date().getFullYear();
-  for (let i = 1; i < 9; i++) {
+  for (let i = 1; i < 9; i += 1) {
     yearOptions.push({
       value: year,
       label: year
     });
-    year++;
+    year += 1;
   }
   return yearOptions;
 });
