635ce2cd369507ff9d941b4e24643835acb3321a
Mike Murray <hellomikemurray@gmail.com>
2018-01-29 11:02:50 -0800

    (feat): supply an ID for checkbox and label
    Add an `id` prop for the label and checkbox control. Supply a default id if one is not defined. An `id` and `htmlFor` are required for form controls and labels.

--- imports/plugins/core/ui/client/components/checkbox/checkbox.js
+++ imports/plugins/core/ui/client/components/checkbox/checkbox.js
@@ -3,6 +3,16 @@ import PropTypes from "prop-types";
 import { Components, registerComponent } from "@reactioncommerce/reaction-components";
 
 class Checkbox extends Component {
+  // Create a semi-random ID
+  //
+  // ID's are required for form controls and their labels for ARIA compliance.
+  // This will be the default ID for this component unless once is passed in through props.
+  _id = `checkbox-${Math.floor(Math.random() * 100000)}`
+
+  get id() {
+    return this.props.id || this._id;
+  }
+
   handleChange = (event) => {
     if (this.props.onChange) {
       const isInputChecked = !this.props.checked;
@@ -20,8 +30,9 @@ class Checkbox extends Component {
 
   render() {
     return (
-      <label>
+      <label htmlFor={this.id}>
         <input
+          id={this.id}
           checked={this.props.checked}
           onChange={this.handleChange}
           className={this.props.className}
@@ -43,6 +54,7 @@ Checkbox.propTypes = {
   checked: PropTypes.bool,
   className: PropTypes.string,
   i18nKeyLabel: PropTypes.string,
+  id: PropTypes.string,
   label: PropTypes.string,
   name: PropTypes.string,
   onChange: PropTypes.func,
