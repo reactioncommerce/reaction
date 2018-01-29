31b86e48ee2e50238a279c482735318645427043
Nat Hamilton <info@nathamilton.com>
2018-01-29 10:46:35 -0600

    fix: returned the .includes to .indexOf on line 238 and updated the conditional on line 240 of imports/plugins/core/checkout/server/methods/workflow.js

--- imports/plugins/core/checkout/server/methods/workflow.js
+++ imports/plugins/core/checkout/server/methods/workflow.js
@@ -235,9 +235,9 @@ Meteor.methods({
 
     const { workflow } = cart.workflow;
     // get index of `newWorkflowStatus`
-    const resetToIndex = workflow.includes(newWorkflowStatus);
+    const resetToIndex = workflow.indexOf(newWorkflowStatus);
     // exit if no such step in workflow
-    if (!resetToIndex) return false;
+    if (resetToIndex < 0) return false;
     // remove all steps that further `newWorkflowStatus` and itself
     const resetedWorkflow = workflow.slice(0, resetToIndex);
 
