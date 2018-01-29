478dcc3a2fcbe0775b2225a3075aaace2701e6d0
Spencer Norman <spencern@gmail.com>
2018-01-29 15:11:43 -0700

    properly tag docker image with SHA1, version, and branch

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,16 +5,24 @@ set -e
 DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-"reactioncommerce/reaction"}
 
 # if we're not on a deployment branch or a Docker related PR branch, skip the Docker build/test
-if [[ "$CIRCLE_BRANCH" != "master" && "$CIRCLE_BRANCH" != *"docker"* ]]; then
-  echo "Not running a build branch. Skipping the Docker build test."
-  exit 0
-fi
 
-# build new image
+BRANCH=$CIRCLE_BRANCH
+SHA1=$(git rev-parse --verify "${CIRCLE_SHA1}")
+
+# Ensure that we build the docker image and tag with the git SHA1 ref.
+echo "Building the Docker image."
 docker build \
   --build-arg TOOL_NODE_FLAGS="--max-old-space-size=4096" \
   --build-arg INSTALL_MONGO=true \
-  -t reactioncommerce/reaction:latest .
+  -t "${DOCKER_NAMESPACE}:${SHA1}" .
+
+echo "Applying git branch tags to Docker image ${DOCKER_NAMESPACE}:${SHA1}"
+docker tag "${DOCKER_NAMESPACE}:${SHA1}" "${DOCKER_NAMESPACE}:${BRANCH}"
+
+echo "Applying git tags to Docker image ${DOCKER_NAMESPACE}:${SHA1}"
+git show-ref --tags -d | grep "^${SHA1}" | sed -e 's,.* refs/tags/,,' -e 's/\^{}//' 2> /dev/null \
+  | xargs -t -I % \
+  echo docker tag "${DOCKER_NAMESPACE}:${SHA1} ${DOCKER_NAMESPACE}:%"
 
 # run the container and wait for it to boot
 docker-compose -f .circleci/docker-compose.yml up -d
@@ -23,6 +31,3 @@ sleep 30
 # use curl to ensure the app returns 200's
 docker exec reaction bash -c "apt-get update && apt-get install -y curl && \
   curl --retry 10 --retry-delay 10 -v http://localhost:3000"
-
-# now change the image tag to the configured name
-docker tag reactioncommerce/reaction:latest $DOCKER_NAMESPACE:latest
