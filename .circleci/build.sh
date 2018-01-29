c5d57115aec1971b117a9380e499e7469c70da9f
Spencer Norman <spencern@gmail.com>
2018-01-29 15:51:09 -0700

    special logic for tagging docker latest

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
+# Build and tag docker image with SHA1, branch name, git tag, and latest if necessary
 set -e
 
 DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-"reactioncommerce/reaction"}
-
-# if we're not on a deployment branch or a Docker related PR branch, skip the Docker build/test
-
 BRANCH=$CIRCLE_BRANCH
 SHA1=$(git rev-parse --verify "${CIRCLE_SHA1}")
 
@@ -22,7 +19,32 @@ docker tag "${DOCKER_NAMESPACE}:${SHA1}" "${DOCKER_NAMESPACE}:${BRANCH}"
 echo "Applying git tags to Docker image ${DOCKER_NAMESPACE}:${SHA1}"
 git show-ref --tags -d | grep "^${SHA1}" | sed -e 's,.* refs/tags/,,' -e 's/\^{}//' 2> /dev/null \
   | xargs -t -I % \
-  echo docker tag "${DOCKER_NAMESPACE}:${SHA1} ${DOCKER_NAMESPACE}:%"
+  echo docker tag "${DOCKER_NAMESPACE}:${SHA1}" "${DOCKER_NAMESPACE}:%"
+
+# Special logic for applying latest tag
+# Check to see if we're on the master branch
+if [[ "$BRANCH" == "master" ]]; then
+  # Check to see if we have a valid `vX.X.X` tag and assign to CURRENT_TAG
+  CURRENT_TAG=$( \
+    git show-ref --tags -d \
+      | grep "^${SHA1}" \
+      | sed -e 's,.* refs/tags/,,' -e 's/\^{}//' \
+      | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+$" \
+      | sort \
+  )
+
+  # Find the highest tagged version number
+  HIGHEST_TAG=$(git --no-pager tag | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+$" | sort -r | head -n 1)
+
+  # We tag :latest only if
+  # 1. We have a current tag
+  # 2. The current tag is equal to the highest tag, OR the highest tag does not exist
+  if [[ -n "$CURRENT_TAG" ]]; then
+    if [[ "$CURRENT_TAG" == "$HIGHEST_TAG" ]] || [[ -z "$HIGHEST_TAG" ]]; then
+      docker tag "${DOCKER_NAMESPACE}:${SHA1}" "${DOCKER_NAMESPACE}:latest"
+    fi
+  fi
+fi
 
 # run the container and wait for it to boot
 docker-compose -f .circleci/docker-compose.yml up -d
