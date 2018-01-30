c85294caf5d05a1d9c1318907de5a14bb0077234
Spencer Norman <spencern@gmail.com>
2018-01-29 17:25:36 -0700

    build all branches

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -11,27 +11,9 @@
 #
 # $DOCKER_NAMESPACE     - the image name for production deployments [Default]: reactioncommerce/reaction
 
-if [[ "$CIRCLE_BRANCH" != "master" ]]; then
-  echo "Not running a deployment branch."
-  exit 0
-fi
+set -e
+DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-"reactioncommerce/reaction"}
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 
-# Master branch deployment (only runs when a version git tag exists - syntax: "v1.2.3")
-if [[ "$CIRCLE_BRANCH" == "master" ]]; then
-  VERSION=$(git describe --tags | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+$")
-
-  if [[ "$VERSION" ]]; then
-    set -e
-
-    DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-"reactioncommerce/reaction"}
-
-    docker tag $DOCKER_NAMESPACE:latest $DOCKER_NAMESPACE:$VERSION
-
-    docker login -u $DOCKER_USER -p $DOCKER_PASS
-
-    docker push $DOCKER_NAMESPACE:$VERSION
-    docker push $DOCKER_NAMESPACE:latest
-  else
-    echo "On the master branch, but no version tag was found. Skipping image deployment."
-  fi
-fi
+# Push image and all tags
+docker push "${DOCKER_NAMESPACE}"
