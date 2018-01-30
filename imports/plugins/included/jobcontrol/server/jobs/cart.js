4a5ef0046f3da9fc5be2d502b2ae3db7d61a329f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-01-30 14:41:18 -0800

    refactor: use withMoment to pass moment into cart.js

--- imports/plugins/included/jobcontrol/server/jobs/cart.js
+++ imports/plugins/included/jobcontrol/server/jobs/cart.js
@@ -5,6 +5,13 @@ import { Accounts, Cart, Jobs } from "/lib/collections";
 import { Hooks, Logger, Reaction } from "/server/api";
 import { ServerSessions } from "/server/publications/collections/sessions";
 
+let moment;
+async function lazyLoadMoment() {
+  if (moment) return;
+  const mod = await import("moment");
+  moment = mod.default;
+}
+
 
 /**
  * {Function} that fetches stale carts
@@ -40,55 +47,51 @@ export function setupStaleCartHook() {
   });
 }
 
-export async function cartCleanupJob() {
-  try {
-    const moment = await import("moment");
-    const removeStaleCart = Jobs.processJobs("cart/removeFromCart", {
-      pollInterval: 60 * 60 * 1000, // backup polling, see observer below
-      workTimeout: 180 * 1000
-    }, (job, callback) => {
-      Logger.debug("Processing cart/removeFromCart");
-      const settings = Reaction.getShopSettings();
-      if (settings.cart) {
-        const schedule = (settings.cart.cleanupDurationDays).match(/\d/);// configurable in shop settings
-        const olderThan = moment().subtract(Number(schedule[0]), "days")._d;
-        const carts = getstaleCarts(olderThan);
-        carts.forEach(cart => {
-          const user = Accounts.findOne({ _id: cart.userId });
-          if (!user.emails.length) {
-            const removeCart = Cart.remove({ userId: user._id });
-            const removeAccount = Accounts.remove({
-              _id: cart.userId,
-              emails: []
-            });
-            const destroySession = ServerSessions.remove({ _id: cart.sessionId });
-            Meteor.users.remove({ _id: user._id, emails: [] }); // clears out anonymous user
-            if (removeCart && removeAccount && destroySession) {
-              const success = "Stale anonymous user cart and account successfully cleaned";
-              Logger.debug(success);
-              job.done(success, { repeatId: true });
-            }
-          } else {
-            Cart.remove({ userId: user._id });
-            const success = "Stale user cart successfully cleaned";
+export function cartCleanupJob() {
+  const removeStaleCart = Jobs.processJobs("cart/removeFromCart", {
+    pollInterval: 60 * 60 * 1000, // backup polling, see observer below
+    workTimeout: 180 * 1000
+  }, (job, callback) => {
+    Logger.debug("Processing cart/removeFromCart");
+    const settings = Reaction.getShopSettings();
+    if (settings.cart) {
+      Promise.await(lazyLoadMoment());
+      const schedule = (settings.cart.cleanupDurationDays).match(/\d/);// configurable in shop settings
+      const olderThan = moment().subtract(Number(schedule[0]), "days")._d;
+      const carts = getstaleCarts(olderThan);
+      carts.forEach(cart => {
+        const user = Accounts.findOne({ _id: cart.userId });
+        if (!user.emails.length) {
+          const removeCart = Cart.remove({ userId: user._id });
+          const removeAccount = Accounts.remove({
+            _id: cart.userId,
+            emails: []
+          });
+          const destroySession = ServerSessions.remove({ _id: cart.sessionId });
+          Meteor.users.remove({ _id: user._id, emails: [] }); // clears out anonymous user
+          if (removeCart && removeAccount && destroySession) {
+            const success = "Stale anonymous user cart and account successfully cleaned";
             Logger.debug(success);
             job.done(success, { repeatId: true });
           }
-        });
-      } else {
-        Logger.debug("No cart cleanup schedule");
-      }
-      callback();
-    });
-    Jobs.find({
-      type: "cart/removeFromCart",
-      status: "ready"
-    }).observe({
-      added() {
-        return removeStaleCart.trigger();
-      }
-    });
-  } catch (error) {
-    Logger.debug(error, "moment.js async import error");
-  }
+        } else {
+          Cart.remove({ userId: user._id });
+          const success = "Stale user cart successfully cleaned";
+          Logger.debug(success);
+          job.done(success, { repeatId: true });
+        }
+      });
+    } else {
+      Logger.debug("No cart cleanup schedule");
+    }
+    callback();
+  });
+  Jobs.find({
+    type: "cart/removeFromCart",
+    status: "ready"
+  }).observe({
+    added() {
+      return removeStaleCart.trigger();
+    }
+  });
 }
