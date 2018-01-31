2263f357e6fa18542d14a38a0c7d14b5405ae34c
Seun Martins <seundotme@gmail.com>
2018-01-31 16:18:11 +0100

    Add sudo to ln command

--- .circleci/install_meteor.sh
+++ .circleci/install_meteor.sh
@@ -5,7 +5,7 @@ set -x
 # install Meteor if it's not already
 if [[ -f ~/.meteor/meteor ]]; then
   printf "\nMeteor already installed. Creating symlink.\n"
-  ln -s ~/.meteor/meteor /usr/local/bin/meteor;
+  sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor;
 else
   printf "\Installing Meteor\n"
   curl https://install.meteor.com | /bin/sh
