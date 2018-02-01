51063b9c2cde05100fb4722e7990730a0c80efdb
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-02-01 11:31:08 -0800

    fix: make moment import default instead of full object

--- imports/plugins/core/orders/client/templates/list/pdf.js
+++ imports/plugins/core/orders/client/templates/list/pdf.js
@@ -49,7 +49,7 @@ Template.completedPDFLayout.helpers({
     return null;
   },
   dateFormat(context, block) {
-    const moment = Template.instance().moment;
+    const moment = Template.instance().moment.default;
     const f = block.hash.format || "MMM DD, YYYY hh:mm:ss A";
     return moment(context).format(f);
   },
