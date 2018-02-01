753fce6eb3963277bda18f8d0e0f54fb3ee55fa3
joykare <warugujoy@gmail.com>
2018-02-01 20:01:44 +0300

    fix lint error

--- imports/plugins/core/layout/client/templates/layout/alerts/reactionAlerts.js
+++ imports/plugins/core/layout/client/templates/layout/alerts/reactionAlerts.js
@@ -24,21 +24,23 @@ const getAlertWrapper = () => {
 };
 
 const initAlertWrapper = () => {
-  ReactDOM.render((
-    <Alert
-      effect="stackslide"
-      position="bottom-left"
-      timeout={5000}
-      html={false}
-      onRouteClose={true}
-      stack={true}
-      offset={0} // in px - will be added to first alert (bottom or top - depends of the position in config)
-      beep={false}
-    />
-  ), getAlertWrapper());
+  ReactDOM.render(
+    (
+      <Alert
+        effect="stackslide"
+        position="bottom-left"
+        timeout={5000}
+        html={false}
+        onRouteClose={true}
+        stack={true}
+        offset={0} // in px - will be added to first alert (bottom or top - depends of the position in config)
+        beep={false}
+      />
+    ), getAlertWrapper()
+  );
 };
 
-Meteor.startup(function () {
+Meteor.startup(() => {
   initAlertWrapper();
 });
 
