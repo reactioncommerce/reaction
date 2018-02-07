02a86f586e8adbd26a71a5b32c4f5d423d3c6f33
Akarshit Wal <akarshitwal@gmail.com>
2018-02-07 16:22:06 +0530

    publishing shops of user

--- client/modules/core/main.js
+++ client/modules/core/main.js
@@ -299,39 +299,6 @@ export default {
   },
 
   /**
-   * getShopsForUser -
-   * @summary gets shopIds of shops where user has provided permissions
-   * @param {Array} roles - roles to check if user has
-   * @param {Object} userId - userId to check permissions for (defaults to current user)
-   * @return {Array} - shopIds user has provided permissions for
-   */
-  getShopsForUser(roles, userId = Meteor.userId()) {
-    // Get full user object, and get shopIds of all shops they are attached to
-    const user = Meteor.user(userId);
-    const shopIds = Object.keys(user.roles);
-    // Remove "__global_roles__" from the list of shopIds, as this function will always return true for
-    // marketplace admins if that "id" is left in the check
-    const filteredShopIds = shopIds.filter(shopId => shopId !== "__global_roles__");
-
-    // Reduce shopIds to shopsWithPermission, using the roles passed in to this function
-    const shopIdsWithRoles = filteredShopIds.reduce((shopsWithPermission, shopId) => {
-      // Get list of roles user has for this shop
-      const rolesUserHas = user.roles[shopId];
-
-      // Find first role that is included in the passed in roles array, otherwise hasRole is undefined
-      const hasRole = rolesUserHas.find((roleUserHas) => roles.includes(roleUserHas));
-
-      // if we found the role, then the user has permission for this shop. Add shopId to shopsWithPermission array
-      if (hasRole) {
-        shopsWithPermission.push(shopId);
-      }
-      return shopsWithPermission;
-    }, []);
-
-    return shopIdsWithRoles;
-  },
-
-  /**
    * hasDashboardAccessForAnyShop - client
    * @summary - client permission check for any "owner", "admin", or "dashboard" permissions for more than one shop.
    * @return {Boolean} Boolean - true if has dashboard access for more than one shop
