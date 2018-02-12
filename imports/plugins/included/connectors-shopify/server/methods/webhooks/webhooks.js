4680af7f7ecb67a19e68043196dbf52c229cc4c6
Akarshit Wal <akarshitwal@gmail.com>
2018-02-12 14:08:27 +0530

    form name correction

--- imports/plugins/included/connectors-shopify/server/methods/webhooks/webhooks.js
+++ imports/plugins/included/connectors-shopify/server/methods/webhooks/webhooks.js
@@ -75,6 +75,7 @@ export const methods = {
         }
       });
     } catch (error) {
+      Logger.error("server-error", `Shopify API Error creating new webhook: ${error.message}`, error);
       throw new Meteor.Error("server-error", `Shopify API Error creating new webhook: ${error.message}`);
     }
   },
