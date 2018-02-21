d2160cb89fd05d08196c5b989aae84dcc3d99c3e
Loan Laux <contact@loanlaux.fr>
2018-02-21 16:46:33 +0100

    Initial work

--- imports/plugins/core/versions/server/migrations/1_rebuild_account_and_order_search_collections.js
+++ imports/plugins/core/versions/server/migrations/1_rebuild_account_and_order_search_collections.js
@@ -1,22 +1,53 @@
 import { Migrations } from "meteor/percolate:migrations";
-import { OrderSearch, AccountSearch } from "/lib/collections";
-import { buildOrderSearch,
-  buildAccountSearch } from "/imports/plugins/included/search-mongo/server/methods/searchcollections";
+import { OrderSearch, AccountSearch, Packages } from "/lib/collections";
+import { Reaction, Logger } from "/server/api";
 
-Migrations.add({
+const searchPackage = Reaction.getPackageSettingsWithOptions("reaction-search");
+
+Logger.info("searchPackage:", searchPackage);
+Logger.info("WHERE U AT, PACKAGES?!?!?!", Packages.find().fetch());
+
+let buildOrderSearch;
+let buildAccountSearch;
+
+async function loadSearchRecordBuilderIfItExists() {
+  if (typeof searchPackage === "object") {
+    ({
+      buildOrderSearch,
+      buildAccountSearch
+    } = await import("/imports/plugins/included/search-mongo/server/methods/searchcollections"));
+  } else {
+    Logger.warn("Failed to load reaction-search plugin. Skipping building order and account search records " +
+                "on version migration step 1.");
+  }
+}
+
+loadSearchRecordBuilderIfItExists().then(() => Migrations.add({
   version: 1,
   up() {
     OrderSearch.remove({});
     AccountSearch.remove({});
-    buildOrderSearch();
-    buildAccountSearch();
+
+    if (buildOrderSearch) {
+      buildOrderSearch();
+    }
+
+    if (buildAccountSearch) {
+      buildAccountSearch();
+    }
   },
   down() {
     // whether we are going up or down we just want to update the search collections
     // to match whatever the current code in the build methods are.
     OrderSearch.remove({});
     AccountSearch.remove({});
-    buildOrderSearch();
-    buildAccountSearch();
+
+    if (buildOrderSearch) {
+      buildOrderSearch();
+    }
+
+    if (buildAccountSearch) {
+      buildAccountSearch();
+    }
   }
-});
+}), (err) => Logger.warn(`Failed to run version migration step 1. Received error: ${err}.`));
