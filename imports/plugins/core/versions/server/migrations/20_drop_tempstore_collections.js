3a02eba7228000c39f8ba0fb6f8579248bead80f
Eric Dobbertin <eric@dairystatedesigns.com>
2018-02-24 22:46:37 -0600

    fix: Ignore dropCollection errors in Media migration

--- imports/plugins/core/versions/server/migrations/20_drop_tempstore_collections.js
+++ imports/plugins/core/versions/server/migrations/20_drop_tempstore_collections.js
@@ -6,8 +6,13 @@ const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
 Migrations.add({
   version: 20,
   up() {
-    db.dropCollection("cfs._tempstore.chunks");
-    db.dropCollection("cfs_gridfs._tempstore.chunks");
-    db.dropCollection("cfs_gridfs._tempstore.files");
+    try {
+      db.dropCollection("cfs._tempstore.chunks");
+      db.dropCollection("cfs_gridfs._tempstore.chunks");
+      db.dropCollection("cfs_gridfs._tempstore.files");
+    } catch (error) {
+      // These seem to throw an error from mongo NPM pkg, but only after
+      // they succeed in dropping the collections, so we'll just ignore
+    }
   }
 });
