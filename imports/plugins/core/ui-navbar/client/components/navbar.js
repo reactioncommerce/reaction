6498b4ec840442f4ffe45daa45d964b5de737d8c
Mike Murray <hellomikemurray@gmail.com>
2018-02-27 13:26:47 -0800

    refactor: remove unnecessary meteor import

--- imports/plugins/core/ui-navbar/client/components/navbar.js
+++ imports/plugins/core/ui-navbar/client/components/navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
-import { Meteor } from "meteor/meteor";
 
 class NavBar extends Component {
   static propTypes = {
