b87fab8ec8e837c90640f882dafa49e8223c30fd
Mike Murray <hellomikemurray@gmail.com>
2018-02-27 13:20:03 -0800

    breaking: search modal blaze wrapper

--- imports/plugins/included/ui-search/client/index.js
+++ imports/plugins/included/ui-search/client/index.js
@@ -1,5 +1,2 @@
 // Search Modal
-import "./templates/searchModal/searchModal.html";
-import "./templates/searchModal/searchModal.js";
-
 export { default as SearchModalContainer } from "../lib/containers/searchModalContainer";
