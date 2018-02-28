c38266cda57b7a594c8042d58d3571279e59d5b4
Mike Murray <hellomikemurray@gmail.com>
2018-02-28 12:47:03 -0800

    feat: add customer product grid component

---
+++ imports/plugins/included/product-variant/components/customer/productGrid.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Components } from "@reactioncommerce/reaction-components";
+import ProductGridItem from "./productGridItem";
+import { ReactionProduct } from "/lib/api";
+
+class ProductGrid extends Component {
+  static propTypes = {
+    productMediaById: PropTypes.object,
+    products: PropTypes.array
+  }
+
+  renderProductGridItems() {
+    const { products } = this.props;
+    const currentTag = ReactionProduct.getTag();
+
+    if (Array.isArray(products)) {
+      return products.map((product, index) => (
+        <ProductGridItem
+          {...this.props}
+          product={product}
+          position={product.positions && product.positions[currentTag]}
+          // productMedia={this.props.productMediaById[product._id]}
+          key={index}
+          index={index}
+        />
+      ));
+    }
+
+    return (
+      <div className="row">
+        <div className="text-center">
+          <h3>
+            <Components.Translation defaultValue="No Products Found" i18nKey="app.noProductsFound" />
+          </h3>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    console.log("this.props", this.props);
+
+    return (
+      <div className="container-main">
+        <div className="product-grid">
+          <Components.DragDropProvider>
+            <ul className="product-grid-list list-unstyled" id="product-grid-list">
+              {this.renderProductGridItems()}
+            </ul>
+          </Components.DragDropProvider>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductGrid;
