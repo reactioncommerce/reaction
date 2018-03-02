4889a21edcafc9803832d13deb75dc7ee5c73f8e
Ticean Bennett <ticean@gmail.com>
2018-03-01 16:02:18 -0800

    GraphQL server running within the Meteor process
    This commit colocates the GraphQL server in the Meteor server using
    `WebApp.connectHandlers.use`.

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,24 +1,17 @@
+import cors from "cors";
 import { Meteor } from "meteor/meteor";
-import { WebApp } from "meteor/webapp";
-import { server } from "./server";
+import { createApolloServer } from "./server";
+import { makeExecutableSchema } from "graphql-tools";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./schemas";
 
-// TODO: Get these settings from the application config.
-const port = 4000;
-const serverOptions = {
-  endpoint: "/",
-  subscriptions: "/",
-  playground: "/",
-  port,
-  uploads: null
-};
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-// NOTE: By starting the server here rather than adding it to Meteor via
-// connect handlers we're running two separate webservers on different ports.
-// That won't be as efficient as adding middleware to the existing Meteor app.
-// Initial attempts to inject with connectHandlers didn't work in prototyping,
-// but it would be best to use that method.
-//
-// Bind the specified paths to the Express server running Apollo + GraphiQL
-// WebApp.connectHandlers.use(server.express);
-//
-server.start(serverOptions);
+server = createApolloServer(
+  { schema },
+  {
+    configServer(express) {
+      express.use(cors());
+    }
+  }
+);
