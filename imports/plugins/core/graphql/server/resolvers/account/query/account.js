b55662f69afae0ef07984b4c4b4aec9c7c8a43d5
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-13 13:56:54 -0700

    docs: add jsdoc

--- imports/plugins/core/graphql/server/resolvers/account/query/account.js
+++ imports/plugins/core/graphql/server/resolvers/account/query/account.js
@@ -4,6 +4,17 @@ import { userAccountQuery } from "/imports/accounts/server/methods/userAccountQu
 export default function account(_, { id }) {
   // search for user from the Accounts collection via provided Account ID
 
+/**
+ * @name account
+ * @method
+ * @summary query the Accounts collection and return user account data
+ * @param {Object} _ - an object containing the result returned from the resolver
+ * @param {Object} args - an object containing Meteor context
+ * @param {String} args.id - id of user to query
+ * @param {Object} context - an object containing the per-request state
+ * @return {Object} user account object
+ */
+export default function account(_, { id }, context) {
   // Trasform ID from base64
   // Returns an object. Use `.id` to get ID
   const idFromBase64 = transformIdFromBase64(id);
