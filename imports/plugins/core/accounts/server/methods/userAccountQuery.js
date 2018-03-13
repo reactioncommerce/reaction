e017aba80501e5c0df355917618d66f7cf81149c
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-13 11:25:18 -0700

    add permissions check

--- imports/plugins/core/accounts/server/methods/userAccountQuery.js
+++ imports/plugins/core/accounts/server/methods/userAccountQuery.js
@@ -2,6 +2,8 @@ import { Accounts } from "/lib/collections";
 import { Reaction } from "/lib/api";
 
 export default function userAccountQuery(id) {
+  // Check to make sure current user has permissions to view queried user
+  if (!Reaction.hasPermission("reaction-accounts")) throw new Error("User does not have permission");
 
   // Query the accounts collection to find user by ID
   const userAccount = Accounts.findOne({
