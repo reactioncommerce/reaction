54fe8f86deedc15953436e27713b6526c113a35e
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-14 11:29:25 -0700

    move new files into folder xforms

---
+++ imports/plugins/core/graphql/server/resolvers/xforms/ramda-ext.js
@@ -0,0 +1,5 @@
+import { assoc, curry, keys, reduce } from "ramda";
+
+export const renameKeys = curry((keysMap, obj) =>
+  reduce((acc, key) => assoc(keysMap[key] || key, obj[key], acc), {}, keys(obj))
+);
