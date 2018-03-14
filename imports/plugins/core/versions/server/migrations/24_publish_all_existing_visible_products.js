42146590df8886353b1b6cce4daf740ad88c56e7
Brent Hoover <brent@thebuddhalodge.com>
2018-03-15 07:45:24 +0800

    (fix) Break up lines to make it easier to read

--- imports/plugins/core/versions/server/migrations/24_publish_all_existing_visible_products.js
+++ imports/plugins/core/versions/server/migrations/24_publish_all_existing_visible_products.js
@@ -5,7 +5,10 @@ import { publishProductsToCatalog } from "/imports/plugins/core/catalog/server/m
 Migrations.add({
   version: 24,
   up() {
-    const visiblePublishedProducts = Products.find({ isVisible: true, isDeleted: false, type: "simple" }, { _id: 1 }).map((product) => product._id);
+    const visiblePublishedProducts = Products.find(
+      { isVisible: true, isDeleted: false, type: "simple" },
+      { _id: 1 }
+    ).map((product) => product._id);
     publishProductsToCatalog(visiblePublishedProducts);
   }
 });
