83ca33acea80af9568a4feadd6a2190e286394a5
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-14 18:18:36 -0700

    formatting

--- imports/plugins/core/graphql/server/resolvers/account/shop.test.js
+++ imports/plugins/core/graphql/server/resolvers/account/shop.test.js
@@ -3,10 +3,9 @@ import shop from "./shop";
 const fakeUser = {
   _id: "123",
   name: "Bob Builder",
-  shopId: "W64ZQe9RUMuAoKrli"
+  shopId: "W64ZQe9RUMuAoKrli" // encoded shopId is: cmVhY3Rpb24vc2hvcDpXNjRaUWU5UlVNdUFvS3JsaQ==
 };
 
-// encoded shopId is: cmVhY3Rpb24vc2hvcDpXNjRaUWU5UlVNdUFvS3JsaQ==
 
 test("returns the shop object from the context", () => {
   const shopObject = shop(fakeUser);
