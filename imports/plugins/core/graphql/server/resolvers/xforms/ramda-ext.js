02a984e7559da73d423cf0a597184b772b647394
Mike Murray <hellomikemurray@gmail.com>
2018-03-16 09:55:28 -0700

    refactor: fix lint issues

--- imports/plugins/core/graphql/server/resolvers/xforms/ramda-ext.js
+++ imports/plugins/core/graphql/server/resolvers/xforms/ramda-ext.js
@@ -1,5 +1,4 @@
 import { assoc, curry, keys, reduce } from "ramda";
 
 export const renameKeys = curry((keysMap, obj) =>
-  reduce((acc, key) => assoc(keysMap[key] || key, obj[key], acc), {}, keys(obj))
-);
+  reduce((acc, key) => assoc(keysMap[key] || key, obj[key], acc), {}, keys(obj)));
