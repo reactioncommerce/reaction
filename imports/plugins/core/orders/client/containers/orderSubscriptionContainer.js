afc01dac40f1711757276f19899040ce1c33192d
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2018-03-20 16:50:21 -0300

    Update orderSubscriptionContainer.js
    fix trailing space

--- imports/plugins/core/orders/client/containers/orderSubscriptionContainer.js
+++ imports/plugins/core/orders/client/containers/orderSubscriptionContainer.js
@@ -28,7 +28,6 @@ function composer(props, onData) {
       // being here means no search text is inputed or search was cleared, so reset any previous match
       delete query._id;
     }
-    
     const options = { limit: 100 };
     const ordersSubscription = Meteor.subscribe("CustomPaginatedOrders", query, options);
 
