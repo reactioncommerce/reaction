c7d5db868565daa43f9ce3d2e753cd2ce7e8b75f
Eric Dobbertin <eric@dairystatedesigns.com>
2018-03-21 06:51:01 -0500

    fix: Missing exports

--- imports/plugins/core/graphql/server/resolvers/util/index.js
+++ imports/plugins/core/graphql/server/resolvers/util/index.js
@@ -1,2 +1,4 @@
+export { default as applyPaginationToMongoQuery } from "./applyPaginationToMongoQuery";
+export { default as getPaginatedResponse } from "./getPaginatedResponse";
 export { default as namespaces } from "./namespaces";
 export { default as optimizeIdOnly } from "./optimizeIdOnly";
