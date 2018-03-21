8d9242777db83a48d62b97b9d42e3541fe148609
Mike Murray <hellomikemurray@gmail.com>
2018-03-21 15:18:06 -0700

    feat: add group and groups methods

--- imports/plugins/core/graphql/server/queries.js
+++ imports/plugins/core/graphql/server/queries.js
@@ -1,7 +1,10 @@
 import { shopAdministratorsQuery } from "/imports/plugins/core/accounts/server/methods/shopAdministratorsQuery";
 import { userAccountQuery } from "/imports/plugins/core/accounts/server/methods/userAccountQuery";
+import { groupQuery, groupsQuery } from "/imports/plugins/core/accounts/server/methods/groupQuery";
 
 export default {
+  group: groupQuery,
+  groups: groupsQuery,
   shopAdministrators: shopAdministratorsQuery,
   shopById(context, _id) {
     return {
