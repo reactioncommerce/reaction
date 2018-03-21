d1f7d3607d0916c9cfde1aaa8f437afdc7dd53d7
Mike Murray <hellomikemurray@gmail.com>
2018-03-21 14:11:39 -0700

    feat: import group query

--- imports/plugins/core/graphql/server/resolvers/account/query/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/query/index.js
@@ -1,9 +1,11 @@
 import account from "./account";
 import administrators from "./administrators";
+import groups from "./groups";
 import viewer from "./viewer";
 
 export default {
   account,
   administrators,
+  groups,
   viewer
 };
