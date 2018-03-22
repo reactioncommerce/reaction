d284c24deedc6c97822d2e6d025fba39ac6bfd96
Mike Murray <hellomikemurray@gmail.com>
2018-03-22 11:06:22 -0700

    docs: update jsdoc

--- imports/plugins/core/graphql/server/resolvers/shop/Shop/groups.js
+++ imports/plugins/core/graphql/server/resolvers/shop/Shop/groups.js
@@ -4,13 +4,14 @@ import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/sh
 /**
  * @name groups
  * @method
- * @summary find and return the groups for an aaccount
- * @param {String} args._id - id of group to query
- * @param {Object} args - an object of all arguments that were sent by the client
- * @param {String} args.after - Connection argument
- * @param {String} args.before - Connection argument
- * @param {Number} args.first - Connection argument
- * @param {Number} args.last - Connection argument
+ * @summary find and return the groups for a shop
+ * @param {Object} resolverArgs - an object containing the result returned from the resolver
+ * @param {String} resolverArgs._id - id of group to query
+ * @param {Object} connectionArgs - an object of all arguments that were sent by the client
+ * @param {String} connectionArgs.after - Connection argument
+ * @param {String} connectionArgs.before - Connection argument
+ * @param {Number} connectionArgs.first - Connection argument
+ * @param {Number} connectionArgs.last - Connection argument
  * @param {Object} context - an object containing the per-request state
  * @return {Object[]} Promise that resolves with array of user Group objects
  */
