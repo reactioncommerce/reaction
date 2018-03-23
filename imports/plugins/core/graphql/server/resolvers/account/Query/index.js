3194594a3c50d6c0e41e22e0aea39f1bc0a3d6d7
Mike Murray <hellomikemurray@gmail.com>
2018-03-23 14:27:31 -0700

    fix: imports after merge

--- imports/plugins/core/graphql/server/resolvers/account/Query/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/Query/index.js
@@ -1,9 +1,13 @@
 import account from "./account";
 import administrators from "./administrators";
+import group from "./group";
+import groups from "./groups";
 import viewer from "./viewer";
 
 export default {
   account,
   administrators,
+  group,
+  groups,
   viewer
 };
