83042f5586670ba52612d8bc0e8bd25dfdc29ca9
MichaelJenny <michael.jenny@projekt-und-partner.de>
2018-03-26 11:29:47 +0200

    Fix for "TaxCloud service not calculating tax"
    Resolves issue #3973
    
    Changes:
      - use Reaction.getPackageSetting to retrieve the package settings
      - Don't check the state of the pkgSettings.enabled which formerly
      indicated if a plugin is "installed" (e.g. enabled is a misnomer).
      If so, it would have been displayed in the dashboard. This seems no
      longer be true for any of the plugins, i.e. they appear in dashboard
      regardlesss of the state of that boolean. Also it will be obsolete
      anyway as soon as we're transitioning to plugins as NPM packages.
      Because then the plugin is installed not through toggling a checkbox,
      but rather through a dedicated `npm install`. I'm going to create a separate
      issue for this TODO.

--- imports/plugins/included/taxes-taxcloud/server/hooks/hooks.js
+++ imports/plugins/included/taxes-taxcloud/server/hooks/hooks.js
@@ -1,7 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { HTTP } from "meteor/http";
-import { Logger, MethodHooks } from "/server/api";
-import { Shops, Cart, Packages } from "/lib/collections";
+import { Logger, MethodHooks, Reaction } from "/server/api";
+import { Shops, Cart } from "/lib/collections";
 
 //
 // this entire method will run after the core/taxes
@@ -19,15 +19,10 @@ MethodHooks.after("taxes/calculate", (options) => {
   if (cartToCalc) {
     const { shopId } = cartToCalc;
     const shop = Shops.findOne(shopId);
-    const pkg = Packages.findOne({
-      name: "taxes-taxcloud",
-      shopId,
-      enabled: true
-    });
+    const pkgSettings = Reaction.getPackageSettings("taxes-taxcloud");
 
-    // check if package is configured
-    if (shop && pkg && pkg.settings.taxcloud) {
-      const { apiKey, apiLoginId } = pkg.settings.taxcloud;
+    if (pkgSettings && pkgSettings.settings.taxcloud.enabled === true) {
+      const { apiKey, apiLoginId } = pkgSettings.settings.taxcloud;
 
       // get shop address
       // this will need some refactoring
@@ -42,85 +37,82 @@ MethodHooks.after("taxes/calculate", (options) => {
         };
       }
 
-      // check if plugin is enabled and this calculation method is enabled
-      if (pkg && pkg.enabled === true && pkg.settings.taxcloud.enabled === true) {
-        if (!apiKey || !apiLoginId) {
-          Logger.warn("TaxCloud API Key is required.");
-        }
-        if (Array.isArray(cartToCalc.shipping) && cartToCalc.shipping.length > 0 && cartToCalc.items) {
-          const shippingAddress = cartToCalc.shipping[0].address;
+      if (!apiKey || !apiLoginId) {
+        Logger.warn("TaxCloud API Key is required.");
+      }
+      if (Array.isArray(cartToCalc.shipping) && cartToCalc.shipping.length > 0 && cartToCalc.items) {
+        const shippingAddress = cartToCalc.shipping[0].address;
 
-          if (shippingAddress) {
-            Logger.debug("TaxCloud triggered on taxes/calculate for cartId:", cartId);
-            const url = "https://api.taxcloud.net/1.0/TaxCloud/Lookup";
-            const cartItems = [];
-            const destination = {
-              Address1: shippingAddress.address1,
-              City: shippingAddress.city,
-              State: shippingAddress.region,
-              Zip5: shippingAddress.postal
-            };
+        if (shippingAddress) {
+          Logger.debug("TaxCloud triggered on taxes/calculate for cartId:", cartId);
+          const url = "https://api.taxcloud.net/1.0/TaxCloud/Lookup";
+          const cartItems = [];
+          const destination = {
+            Address1: shippingAddress.address1,
+            City: shippingAddress.city,
+            State: shippingAddress.region,
+            Zip5: shippingAddress.postal
+          };
 
-            // format cart items to TaxCloud structure
-            let index = 0;
-            for (const items of cartToCalc.items) {
-              // only processs taxable products
-              if (items.variants.taxable === true) {
-                const item = {
-                  Index: index,
-                  ItemID: items.variants._id,
-                  TIC: "00000",
-                  Price: items.variants.price,
-                  Qty: items.quantity
-                };
-                index += 1;
-                cartItems.push(item);
-              }
+          // format cart items to TaxCloud structure
+          let index = 0;
+          for (const items of cartToCalc.items) {
+            // only processs taxable products
+            if (items.variants.taxable === true) {
+              const item = {
+                Index: index,
+                ItemID: items.variants._id,
+                TIC: "00000",
+                Price: items.variants.price,
+                Qty: items.quantity
+              };
+              index += 1;
+              cartItems.push(item);
             }
-
-            // request object
-            const request = {
-              headers: {
-                "accept": "application/json",
-                "content-type": "application/json"
-              },
-              data: {
-                apiKey,
-                apiLoginId,
-                customerID: cartToCalc.userId,
-                cartItems,
-                origin,
-                destination,
-                cartID: cartId,
-                deliveredBySeller: false
-              }
-            };
-
-            HTTP.post(url, request, (error, response) => {
-              let taxRate = 0;
-              // ResponseType 3 is a successful call.
-              if (!error && response.data.ResponseType === 3) {
-                let totalTax = 0;
-                for (const item of response.data.CartItemsResponse) {
-                  totalTax += item.TaxAmount;
-                }
-                // don't run this calculation if there isn't tax.
-                if (totalTax > 0) {
-                  taxRate = (totalTax / cartToCalc.getSubTotal());
-                }
-                // we should consider if we want percentage and dollar
-                // as this is assuming that subTotal actually contains everything
-                // taxable
-                Meteor.call("taxes/setRate", cartId, taxRate, response.CartItemsResponse);
-              } else {
-                let errMsg = "Unable to access service. Check credentials.";
-                if (response && response.data.Messages[0].Message) {
-                  errMsg = response.data.Messages[0].Message;
-                }
-                Logger.warn("Error fetching tax rate from TaxCloud:", errMsg);
-              }
-            });
           }
+
+          // request object
+          const request = {
+            headers: {
+              "accept": "application/json",
+              "content-type": "application/json"
+            },
+            data: {
+              apiKey,
+              apiLoginId,
+              customerID: cartToCalc.userId,
+              cartItems,
+              origin,
+              destination,
+              cartID: cartId,
+              deliveredBySeller: false
+            }
+          };
+
+          HTTP.post(url, request, (error, response) => {
+            let taxRate = 0;
+            // ResponseType 3 is a successful call.
+            if (!error && response.data.ResponseType === 3) {
+              let totalTax = 0;
+              for (const item of response.data.CartItemsResponse) {
+                totalTax += item.TaxAmount;
+              }
+              // don't run this calculation if there isn't tax.
+              if (totalTax > 0) {
+                taxRate = (totalTax / cartToCalc.getSubTotal());
+              }
+              // we should consider if we want percentage and dollar
+              // as this is assuming that subTotal actually contains everything
+              // taxable
+              Meteor.call("taxes/setRate", cartId, taxRate, response.CartItemsResponse);
+            } else {
+              let errMsg = "Unable to access service. Check credentials.";
+              if (response && response.data.Messages[0].Message) {
+                errMsg = response.data.Messages[0].Message;
+              }
+              Logger.warn("Error fetching tax rate from TaxCloud:", errMsg);
+            }
+          });
         }
       }
     }
