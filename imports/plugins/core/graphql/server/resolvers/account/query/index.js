f53a086e4f28d522dfd1b4e362e3a08d4f74a2be
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-26 15:33:52 -0700

    export roles accoutn query

--- imports/plugins/core/graphql/server/resolvers/account/query/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/query/index.js
@@ -1,9 +1,11 @@
 import account from "./account";
 import administrators from "./administrators";
+import roles from "./roles";
 import viewer from "./viewer";
 
 export default {
   account,
   administrators,
+  roles,
   viewer
 };
