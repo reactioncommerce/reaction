caf6bbe4a7fcd9b3f95135289cff5e038fa3b31e
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-26 14:10:53 -0700

    add rolesQuery to graphql queries

--- imports/plugins/core/graphql/server/queries.js
+++ imports/plugins/core/graphql/server/queries.js
@@ -1,7 +1,9 @@
 import { shopAdministratorsQuery } from "/imports/plugins/core/accounts/server/methods/shopAdministratorsQuery";
 import { userAccountQuery } from "/imports/plugins/core/accounts/server/methods/userAccountQuery";
+import { rolesQuery } from "/imports/plugins/core/accounts/server/methods/rolesQuery";
 
 export default {
+  roles: rolesQuery,
   shopAdministrators: shopAdministratorsQuery,
   shopById(context, _id) {
     return {
