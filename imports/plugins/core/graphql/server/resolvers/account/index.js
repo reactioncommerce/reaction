e1f42f00a6637fc2e62f575b3da0a43356c5c0ee
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-26 15:13:17 -0700

    add RoleConnection and RoleEdge

--- imports/plugins/core/graphql/server/resolvers/account/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/index.js
@@ -2,16 +2,18 @@ import Account from "./Account";
 import AccountConnection from "./AccountConnection";
 import AccountEdge from "./AccountEdge";
 import AddAccountAddressBookEntryPayload from "./AddAccountAddressBookEntryPayload";
-import AddressEdge from "./AddressEdge";
 import Mutation from "./Mutation";
 import Query from "./Query";
+import RoleConnection from "./RoleConnection";
+import RoleEdge from "./RoleEdge";
 
 export default {
   Account,
   AccountConnection,
   AccountEdge,
   AddAccountAddressBookEntryPayload,
-  AddressEdge,
   Mutation,
-  Query
+  Query,
+  RoleConnection,
+  RoleEdge
 };
