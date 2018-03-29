119a13cf9f54335c6359cb94f11e5375d5114d06
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-29 15:45:26 -0700

    add accounts/setProfileCurrency to GraphQL methods

--- imports/plugins/core/graphql/server/resolvers/account/Mutation/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/Mutation/index.js
@@ -1,7 +1,9 @@
 import addAccountAddressBookEntry from "./addAccountAddressBookEntry";
+import setAccountProfileCurrency from "./setAccountProfileCurrency";
 import updateAccountAddressBookEntry from "./updateAccountAddressBookEntry";
 
 export default {
   addAccountAddressBookEntry,
+  setAccountProfileCurrency,
   updateAccountAddressBookEntry
 };
