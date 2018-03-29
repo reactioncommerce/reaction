a7864b768e328e15e00bc7d7e3e84899ae21df22
Mike Murray <hellomikemurray@gmail.com>
2018-03-29 16:05:25 -0700

    feat: import addAccountToGroup mutation

--- imports/plugins/core/graphql/server/resolvers/account/Mutation/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/Mutation/index.js
@@ -1,7 +1,9 @@
 import addAccountAddressBookEntry from "./addAccountAddressBookEntry";
+import addAccountToGroup from "./addAccountToGroup";
 import updateAccountAddressBookEntry from "./updateAccountAddressBookEntry";
 
 export default {
   addAccountAddressBookEntry,
+  addAccountToGroup,
   updateAccountAddressBookEntry
 };
