1d87faa6da52ddf03a181decc2e404f287e6204a
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-30 10:51:02 -0700

    code comments

--- imports/plugins/core/graphql/server/resolvers/xforms/currency.js
+++ imports/plugins/core/graphql/server/resolvers/xforms/currency.js
@@ -1,20 +1,24 @@
 import { assoc, compose, map, toPairs } from "ramda";
 import { Reaction } from "/server/api";
 
-// Trasform currency object.
+// add `code` and `_id` keys to each currency object
 const xformCurrencyEntry = ([k, v]) => compose(
   assoc("code", k),
   assoc("_id", k)
 )(v);
 
+// map over all provided currencies, provided in the format stored in our Shops collection,
+// and conver them to the format that GraphQL needs
 const xformLegacyCurrencies = compose(map(xformCurrencyEntry), toPairs);
 
+// retrive all currencies from the Shops collection and xform them
 function getXformedCurrenciesByShop() {
   const shopCurrencies = Reaction.getShopCurrencies();
 
   return xformLegacyCurrencies(shopCurrencies);
 }
 
+// Find an individual xformed currency
 export function getXformedCurrencyByCode(code) {
   const allCurrencies = getXformedCurrenciesByShop();
   return allCurrencies.find((currency) => currency.code === code);
