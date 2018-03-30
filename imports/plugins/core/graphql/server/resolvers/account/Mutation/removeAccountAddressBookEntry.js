03e85d53cda279f2d0ad756ccc0c053cbad8dd76
Mike Murray <hellomikemurray@gmail.com>
2018-03-30 09:25:52 -0700

    docs: update type for param `args.input.addressId` to String

--- imports/plugins/core/graphql/server/resolvers/account/Mutation/removeAccountAddressBookEntry.js
+++ imports/plugins/core/graphql/server/resolvers/account/Mutation/removeAccountAddressBookEntry.js
@@ -8,7 +8,7 @@ import { xformAddressResponse, decodeAddressOpaqueId } from "@reactioncommerce/r
  * @param {Object} _ - unused
  * @param {Object} args.input - an object of all mutation arguments that were sent by the client
  * @param {String} args.input.accoundId - The account ID
- * @param {AddressInput} args.input.addressId - The ID of the address to remove
+ * @param {String} args.input.addressId - The ID of the address to remove
  * @param {String} [args.input.clientMutationId] - An optional string identifying the mutation call
  * @param {Object} context - an object containing the per-request state
  * @return {Object} removeAccountAddressBookEntryPayload
