904e9f8d8abcd6726d23919619b0b625e468770a
Mike Murray <hellomikemurray@gmail.com>
2018-03-30 09:38:51 -0700

    feat: import `removeAccountFromGroup` mutation

--- imports/plugins/core/graphql/server/resolvers/account/Mutation/index.js
+++ imports/plugins/core/graphql/server/resolvers/account/Mutation/index.js
@@ -1,9 +1,11 @@
 import addAccountAddressBookEntry from "./addAccountAddressBookEntry";
 import addAccountToGroup from "./addAccountToGroup";
 import updateAccountAddressBookEntry from "./updateAccountAddressBookEntry";
+import removeAccountFromGroup from "./removeAccountFromGroup";
 
 export default {
   addAccountAddressBookEntry,
   addAccountToGroup,
-  updateAccountAddressBookEntry
+  updateAccountAddressBookEntry,
+  removeAccountFromGroup
 };
