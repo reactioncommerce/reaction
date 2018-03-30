c44e6b286e30b55cfd9f910b604f27e67df5ff1a
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-03-30 10:28:19 -0700

    move transforms into correct file

--- imports/plugins/core/graphql/server/resolvers/xforms/currency.js
+++ imports/plugins/core/graphql/server/resolvers/xforms/currency.js
@@ -1,8 +1,21 @@
 import { assoc, compose, map, toPairs } from "ramda";
+import { Reaction } from "/server/api";
 
+// Trasform currency object.
 const xformCurrencyEntry = ([k, v]) => compose(
   assoc("code", k),
   assoc("_id", k)
 )(v);
 
-export const xformLegacyCurrencies = compose(map(xformCurrencyEntry), toPairs);
+const xformLegacyCurrencies = compose(map(xformCurrencyEntry), toPairs);
+
+function getXformedCurrenciesByShop() {
+  const shopCurrencies = Reaction.getShopCurrencies();
+
+  return xformLegacyCurrencies(shopCurrencies);
+}
+
+export function getXformedCurrencyByCode(code) {
+  const allCurrencies = getXformedCurrenciesByShop();
+  return allCurrencies.find((currency) => currency.code === code);
+}
