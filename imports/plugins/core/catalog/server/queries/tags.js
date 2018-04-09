948576d255e200e17dcd4e4873e5ee16f7b24033
Eric Dobbertin <eric@dairystatedesigns.com>
2018-04-09 11:32:36 -0500

    fix(GraphQL): no auth necessary for tags query

--- imports/plugins/core/catalog/server/queries/tags.js
+++ imports/plugins/core/catalog/server/queries/tags.js
@@ -1,20 +1,16 @@
-import { Meteor } from "meteor/meteor";
-
 /**
  * @name tags
  * @method
  * @summary query the Tags collection by shop ID and optionally by isTopLevel
  * @param {Object} context - an object containing the per-request state
  * @param {String} shopId - ID of shop to query
- * @param {Boolean} [isTopLevel] - If set, look for `isTopLevel` matching this value
+ * @param {Object} [params] - Additional options for the query
+ * @param {Boolean} [params.isTopLevel] - If set, look for `isTopLevel` matching this value
+ * @param {Boolean} [params.includeDeleted] - Whether or not to include `isDeleted=true` tags. Default is `false`
  * @return {Promise<MongoCursor>} - A MongoDB cursor for the proper query
  */
 export default async function tags(context, shopId, { includeDeleted = false, isTopLevel } = {}) {
-  const { collections, userHasPermission } = context;
-
-  if (!userHasPermission(["owner", "admin"], shopId)) {
-    throw new Meteor.Error("access-denied", "User does not have permissions to view tags");
-  }
+  const { collections } = context;
 
   const { Tags } = collections;
   const query = { shopId };
--- imports/plugins/core/catalog/server/queries/tagsByIds.js
+++ imports/plugins/core/catalog/server/queries/tagsByIds.js
@@ -1,15 +1,15 @@
 /**
  * @name tagsByIds
  * @method
- * @summary query the Tags collection by shop ID and optionally by isTopLevel
+ * @summary query the Tags collection by a list of IDs
  * @param {Object} context - an object containing the per-request state
  * @param {String[]} tagIds - tag IDs to get
+ * @param {Object} [params] - Additional options for the query
+ * @param {Boolean} [params.includeDeleted] - Whether or not to include `isDeleted=true` tags. Default is `false`
  * @return {Promise<MongoCursor>} - A MongoDB cursor for the proper query
  */
 export default async function tagsByIds(context, tagIds, { includeDeleted = false } = {}) {
   const { collections } = context;
-
-  // Note that we currently do not need a permission check here because we've already checked by the time we use this query
 
   const { Tags } = collections;
   const query = { _id: { $in: tagIds } };
