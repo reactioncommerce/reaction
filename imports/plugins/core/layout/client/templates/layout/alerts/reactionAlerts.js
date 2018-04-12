05e232c2225815deb2cf7ed441fc8b9f04982130
MichaelJenny <michael.jenny@projekt-und-partner.de>
2018-04-12 11:05:07 +0200

    Fix for "document.getElementBuId("react-root") may fail"
    This PR resolves issue #4171
    
    Changes:
    Use the return value of getRootNode() which always contains the correct node, no matter how it is named.

--- imports/plugins/core/layout/client/templates/layout/alerts/reactionAlerts.js
+++ imports/plugins/core/layout/client/templates/layout/alerts/reactionAlerts.js
@@ -16,9 +16,8 @@ import { getRootNode } from "/imports/plugins/core/router/client/browserRouter.j
  */
 
 const getAlertWrapper = () => {
-  getRootNode();
-  const rootNode = document.getElementById("react-root");
-
+  const rootNode = getRootNode();
+  
   rootNode.insertAdjacentHTML("beforebegin", "<div id='s-alert-wrapper'></div>");
   return document.getElementById("s-alert-wrapper");
 };
