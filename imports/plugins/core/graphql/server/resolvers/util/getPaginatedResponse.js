a778066fd078f0ebbf14213a06c041382d7f5dbd
Nat Hamilton <info@nathamilton.com>
2018-04-20 17:41:37 -0500

    fix: updated getPaginatedResponse to get the totalCount form the query and to pass it as a param to applyPaginationToMongoCursor, updated applyPaginationToMongoCursor to set a default first to 50 if none is provided and to take the totalCount as a param, updated all related test

--- imports/plugins/core/graphql/server/resolvers/util/getPaginatedResponse.js
+++ imports/plugins/core/graphql/server/resolvers/util/getPaginatedResponse.js
@@ -4,17 +4,19 @@ import getCollectionFromCursor from "./getCollectionFromCursor";
 import getMongoSort from "./getMongoSort";
 
 const getPaginatedResponse = async (query, args) => {
-  const { totalCount, pageInfo } = await applyPaginationToMongoCursor(query, args);
-
+  const totalCount = await query.clone().count();
   const collection = getCollectionFromCursor(query);
-  const baseFilter = query.cmd.query;
 
+  const baseFilter = query.cmd.query;
   const updatedFilter = await applyBeforeAfterToFilter({ collection, baseFilter, ...args });
   const sort = getMongoSort(args);
 
-  const nodes = await query.filter(updatedFilter).sort(sort).toArray();
+  query.filter(updatedFilter).sort(sort);
 
+  const { pageInfo } = await applyPaginationToMongoCursor(query, args, totalCount);
+  const nodes = await query.toArray();
   const count = nodes.length;
+
   if (count) {
     pageInfo.startCursor = nodes[0]._id;
     pageInfo.endCursor = nodes[count - 1]._id;
