9e5d5dd5e13db9f32c09467e0e401e4dc60675cc
Nat Hamilton <info@nathamilton.com>
2018-04-24 16:11:26 -0500

    refactor: added a few more test cases to isSoldOut.test.js

--- imports/plugins/core/catalog/server/methods/isSoldOut.test.js
+++ imports/plugins/core/catalog/server/methods/isSoldOut.test.js
@@ -18,18 +18,53 @@ afterAll(restore$ProductRevision);
 const mockCollections = {};
 
 // mock variant
-const mockVariant = {
+const mockVariantWithInventoryManagment = {
   inventoryManagement: true
 };
 
-test("expect true when product variant is sold out", async () => {
+const mockVariantWithOutInventoryManagment = {
+  inventoryManagement: false
+};
+
+test("expect true when a single product variant is sold out and inventory management is enabled", async () => {
   mockProductRevision.getVariantQuantity.mockReturnValueOnce(Promise.resolve(0));
-  const spec = await isSoldOut([mockVariant], mockCollections);
+  const spec = await isSoldOut([mockVariantWithInventoryManagment], mockCollections);
   expect(spec).toBe(true);
 });
 
-test("expect false when product variant is not sold out", async () => {
+test("expect false when a single product variant is not sold out and inventory management is enabled", async () => {
   mockProductRevision.getVariantQuantity.mockReturnValueOnce(Promise.resolve(1));
-  const spec = await isSoldOut([mockVariant], mockCollections);
+  const spec = await isSoldOut([mockVariantWithInventoryManagment], mockCollections);
+  expect(spec).toBe(false);
+});
+
+test("expect true when an array of product variants are sold out and inventory management is enabled", async () => {
+  mockProductRevision.getVariantQuantity
+    .mockReturnValueOnce(Promise.resolve(0))
+    .mockReturnValueOnce(Promise.resolve(0));
+  const spec = await isSoldOut([mockVariantWithInventoryManagment, mockVariantWithInventoryManagment], mockCollections);
+  expect(spec).toBe(true);
+});
+
+test("expect false when an array of product variants are not sold out and inventory management is enabled", async () => {
+  mockProductRevision.getVariantQuantity
+    .mockReturnValueOnce(Promise.resolve(1))
+    .mockReturnValueOnce(Promise.resolve(1));
+  const spec = await isSoldOut([mockVariantWithInventoryManagment, mockVariantWithInventoryManagment], mockCollections);
+  expect(spec).toBe(false);
+});
+
+test("expect false when a single product variant is sold out and inventory management is disabled", async () => {
+  mockProductRevision.getVariantQuantity.mockReturnValueOnce(Promise.resolve(0));
+  const spec = await isSoldOut([mockVariantWithOutInventoryManagment], mockCollections);
+  expect(spec).toBe(false);
+});
+
+test("expect false when one product variant has inventory management is disabled", async () => {
+  mockProductRevision.getVariantQuantity.mockReturnValueOnce(Promise.resolve(0));
+  const spec = await isSoldOut(
+    [mockVariantWithOutInventoryManagment, mockVariantWithInventoryManagment],
+    mockCollections
+  );
   expect(spec).toBe(false);
 });
