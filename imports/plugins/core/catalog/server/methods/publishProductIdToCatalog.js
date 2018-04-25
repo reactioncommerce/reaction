73341456531d39f9af582bc249f7ee1579d618bd
Nat Hamilton <info@nathamilton.com>
2018-04-25 12:53:15 -0500

    refactor: moved publishProductIdToCatalog to it's own file, created a test file still needs test cases

---
+++ imports/plugins/core/catalog/server/methods/publishProductIdToCatalog.js
@@ -0,0 +1,32 @@
+import Logger from "@reactioncommerce/logger";
+
+import publishProductToCatalog from "./publishProductToCatalog";
+
+/**
+ * @method publishProductIdToCatalog
+ * @summary Publish a product to the Catalog by ID
+ * @memberof Catalog
+ * @param {string} productId - A string product id
+ * @return {boolean} true on successful publish, false if publish was unsuccessful
+ * @todo revisit this function's name, maybe should be `publishProductToCatalogById`?
+ */
+export default async function publishProductIdToCatalog(productId, collections) {
+  const { Products } = collections;
+  // Find the product by id
+  let product = Products.findOne({ _id: productId });
+
+  // Stop if a product could not be found
+  if (!product) {
+    Logger.info("Cannot publish product to catalog");
+    return false;
+  }
+
+  // If the product has ancestors, then find the top product document
+  if (Array.isArray(product.ancestors) && product.ancestors.length) {
+    product = Products.findOne({
+      _id: product.ancestors[0]
+    });
+  }
+
+  return publishProductToCatalog(product, collections);
+}
