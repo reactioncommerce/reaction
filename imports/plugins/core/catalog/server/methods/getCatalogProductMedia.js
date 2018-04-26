e24b1068f11f0078da9a1caef26e545dc994bbdd
Nat Hamilton <info@nathamilton.com>
2018-04-26 15:19:30 -0500

    refactor: moved productMedia array creation into it's own file and created a test file

---
+++ imports/plugins/core/catalog/server/methods/getCatalogProductMedia.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * @method getCatalogProductMedia
+ * @summary TODO:
+ * @param {String} productId - TODO
+ * @param {Object} collections - TODO
+ * @return {Array}
+ */
+export default async function getCatalogProductMedia(productId, collections) {
+  const { Media } = collections;
+  const mediaArray = await Media.find(
+    {
+      "metadata.productId": productId,
+      "metadata.toGrid": 1,
+      "metadata.workflow": { $nin: ["archived", "unpublished"] }
+    },
+    {
+      sort: { "metadata.priority": 1, uploadedAt: 1 }
+    }
+  );
+
+  // Denormalize media
+  const catalogProductMedia = mediaArray
+    .map((media) => {
+      const { metadata } = media;
+      const { toGrid, priority, productId, variantId } = metadata || {};
+
+      return {
+        priority,
+        toGrid,
+        productId,
+        variantId,
+        URLs: {
+          large: `${media.url({ store: "large" })}`,
+          medium: `${media.url({ store: "medium" })}`,
+          original: `${media.url({ store: "image" })}`,
+          small: `${media.url({ store: "small" })}`,
+          thumbnail: `${media.url({ store: "thumbnail" })}`
+        }
+      };
+    })
+    .sort((a, b) => a.priority - b.priority);
+
+  return catalogProductMedia;
+}
