c5dde219cfa96ca1f28352325a4973d73e664c04
Mike Murray <hellomikemurray@gmail.com>
2018-05-16 15:19:09 -0700

    docs: update comment

--- imports/plugins/core/versions/server/migrations/25_remove_revision_control.js
+++ imports/plugins/core/versions/server/migrations/25_remove_revision_control.js
@@ -1,7 +1,7 @@
 import { Migrations } from "meteor/percolate:migrations";
 import { Packages } from "/lib/collections";
 
-// Migration file created for removing revision control package registry entry from all shops
+// Migration file created for removing the revision control package registry entry from all shops
 Migrations.add({
   version: 25,
   up() {
