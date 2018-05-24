565291600d586d6a53ed938521315a546d386e66
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-05-24 08:17:30 -0700

    change tagId to slugOrId

--- imports/plugins/core/catalog/server/no-meteor/queries/tag.js
+++ imports/plugins/core/catalog/server/no-meteor/queries/tag.js
@@ -4,17 +4,17 @@
  * @memberof Catalog/NoMeteorQueries
  * @summary query the Tags collection and return a tag by tagId
  * @param {Object} context - an object containing the per-request state
- * @param {String} tagId - ID of tag to query
+ * @param {String} slugOrId - ID or Slug of tag to query
  * @return {Promise<MongoCursor>} - A MongoDB cursor for the proper query
  */
-export default async function tag(context, tagId) {
+export default async function tag(context, slugOrId) {
   const { collections } = context;
 
   const { Tags } = collections;
   const query = {
     $or: [
-      { _id: tagId },
-      { slug: tagId }
+      { _id: slugOrId },
+      { slug: slugOrId }
     ]
   };
 
