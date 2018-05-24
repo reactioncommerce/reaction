691bd9a29d993f5f23669a6ab5fafaf3be0b9ca6
Eric Dobbertin <eric@dairystatedesigns.com>
2018-05-24 09:23:33 -0500

    feat: Catalog schema migration

---
+++ imports/plugins/core/versions/server/migrations/25_update_catalog_schema.js
@@ -0,0 +1,46 @@
+import { Migrations } from "meteor/percolate:migrations";
+import _ from "lodash";
+import { Catalog, Shops } from "/lib/collections";
+import convertCatalogItem from "../util/convertCatalogItem";
+
+// Do this migration in batches of 200 to avoid memory issues
+const LIMIT = 200;
+
+function getShopsForItems(items) {
+  const uniqueShopIds = _.uniq(items.map(({ shopId }) => shopId));
+
+  return Shops.find(
+    {
+      _id: { $in: uniqueShopIds }
+    },
+    {
+      fields: {
+        currencies: 1,
+        currency: 1
+      }
+    }
+  ).fetch();
+}
+
+Migrations.add({
+  version: 25,
+  up() {
+    // We moved everything to a `product` object, so checking for the existence of that
+    // seems like a good way to find everything we haven't converted yet.
+    let items;
+
+    do {
+      items = Catalog.find({ product: { $exists: false } }, { limit: LIMIT }).fetch();
+
+      if (items.length) {
+        const shops = getShopsForItems(items);
+
+        items.forEach((item) => {
+          const shop = shops.find(({ _id }) => _id === item.shopId);
+          const doc = convertCatalogItem(item, shop);
+          Catalog.update({ _id: item._id }, doc, { bypassCollection2: true });
+        });
+      }
+    } while (items.length);
+  }
+});
