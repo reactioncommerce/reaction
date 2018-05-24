70b5c74b259219798c92c67acc59a643c34b2a68
Eric Dobbertin <aldeed@gmail.com>
2018-05-24 11:51:14 -0500

    docs: fix tag query jsdoc

--- imports/plugins/core/catalog/server/no-meteor/queries/tag.js
+++ imports/plugins/core/catalog/server/no-meteor/queries/tag.js
@@ -2,10 +2,10 @@
  * @name tag
  * @method
  * @memberof Catalog/NoMeteorQueries
- * @summary query the Tags collection and return a tag by tagId
+ * @summary query the Tags collection and return a tag by tag ID or slug
  * @param {Object} context - an object containing the per-request state
- * @param {String} slugOrId - ID or Slug of tag to query
- * @return {Promise<MongoCursor>} - A MongoDB cursor for the proper query
+ * @param {String} slugOrId - ID or slug of tag to query
+ * @return {Object} - A Tag document if one was found
  */
 export default async function tag(context, slugOrId) {
   const { collections } = context;
