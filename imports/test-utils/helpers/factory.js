50b0197665dc852c5e0b16c26f4601bb6338b520
Nat Hamilton <info@nathamilton.com>
2018-06-01 17:10:34 -0500

    chore: removed temp data-factory code, added @reactioncommerce/data-factory package

--- imports/test-utils/helpers/factory.js
+++ imports/test-utils/helpers/factory.js
@@ -1,5 +1,5 @@
+import { createFactoryForSchema, Factory } from "@reactioncommerce/data-factory";
 import * as schemas from "imports/collections/schemas";
-import { createFactoryForSchema, Factory } from "./dataFactory";
 
 Object.keys(schemas).forEach((key) => {
   createFactoryForSchema(key, schemas[key]);
