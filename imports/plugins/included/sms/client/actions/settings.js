cfc1c5c69c5c8fa55ab32cca55ff324b3b87b0a3
Eric Dobbertin <eric@dairystatedesigns.com>
2018-06-11 19:40:12 -0500

    feat: Use new standard components for SMS settings form

--- imports/plugins/included/sms/client/actions/settings.js
+++ imports/plugins/included/sms/client/actions/settings.js
@@ -7,47 +7,29 @@ export default {
   /**
    * Save email settings
    * @param {Object} settings - object of mail provider settings
-   * @param {Function} callback - optional callback
-   * @return {Boolean} returns true if all fields provided and update method called
+   * @return {Promise<undefined>}
    */
-  saveSettings(settings, callback) {
-    const { apiKey, apiToken, smsProvider, smsPhone } = settings;
-
-    if (!apiKey) {
-      Alert(i18next.t("app.error"), i18next.t("admin.alerts.noApiKey"), "error");
-      return callback();
-    }
-    if (!apiToken) {
-      Alert(i18next.t("app.error"), i18next.t("admin.alerts.noApiToken"), "error");
-      return callback();
-    }
-    if (!smsProvider) {
-      Alert(i18next.t("app.error"), i18next.t("admin.alerts.noSmsProvider"), "error");
-      return callback();
-    }
-    if (!smsPhone) {
-      Alert(i18next.t("app.error"), i18next.t("admin.alerts.noSmsPhone"), "error");
-      return callback();
-    }
-
-    const save = () => {
+  saveSettings(settings) {
+    return new Promise((resolve) => {
       Meteor.call("sms/saveSettings", settings, (err) => {
+        resolve();
+
         if (err) {
-          return Alert(
+          Alert(
             i18next.t("app.error"),
             "Your API credentials could not be saved",
             "error"
           );
+          return;
         }
-        return Alert({
+
+        Alert({
           title: i18next.t("app.success"),
           text: i18next.t("admin.alerts.saveSuccess"),
           type: "success",
           timer: 1700
-        }).catch(() => null);
+        });
       });
-    };
-    save();
-    return callback();
+    });
   }
 };
