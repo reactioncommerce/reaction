5adc88abe9ccc56d340f407a27a5d28870db0e08
James Porlante <james@reactioncommerce.com>
2018-06-15 13:10:22 +0700

    (fix): update prerender.js

--- server/startup/prerender.js
+++ server/startup/prerender.js
@@ -3,9 +3,12 @@ import { WebApp } from "meteor/webapp";
 import { Logger } from "/server/api";
 
 export default function preRender() {
-  if (process.env.PRERENDER_TOKEN && process.env.PRERENDER_HOST) {
+  if (process.env.PRERENDER_TOKEN && process.env.PRERENDER_SERVICE_URL) {
     prerender.set("prerenderToken", process.env.PRERENDER_TOKEN);
-    prerender.set("host", process.env.PRERENDER_HOST);
+    prerender.set(“prerenderServiceUrl”, process.env.PRERENDER_SERVICE_URL);
+    if (process.env.PRERENDER_HOST) {
+      prerender.set("host", process.env.PRERENDER_HOST);
+    }
     prerender.set("protocol", "https");
     WebApp.rawConnectHandlers.use(prerender);
     Logger.info("Prerender Initialization finished.");
