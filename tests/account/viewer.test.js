e9f0870131c1e1af97e9d22b3f8054b4e7ecab3f
Eric Dobbertin <eric@dairystatedesigns.com>
2018-06-19 18:31:03 -0500

    tests: improved viewer query tests

--- tests/account/viewer.test.js
+++ tests/account/viewer.test.js
@@ -1,4 +1,6 @@
 import TestApp from "../TestApp";
+import ViewerFullQuery from "./ViewerFullQuery.graphql";
+import Factory from "/imports/test-utils/helpers/factory";
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 
@@ -8,11 +10,7 @@ beforeAll(async () => {
   testApp = new TestApp();
   await testApp.start();
 
-  viewerQuery = testApp.query(`{
-    viewer {
-      _id
-    }
-  }`);
+  viewerQuery = testApp.query(ViewerFullQuery);
 });
 
 afterAll(() => testApp.stop());
@@ -25,14 +23,51 @@ test("unauthenticated", async () => {
 });
 
 test("authenticated", async () => {
-  await testApp.setLoggedInUser({
+  const mockAccount = Factory.Accounts.makeOne({
     _id: "123"
   });
+
+  await testApp.setLoggedInUser(mockAccount);
 
   const result = await viewerQuery();
   expect(result).toEqual({
     viewer: {
-      _id: "cmVhY3Rpb24vYWNjb3VudDoxMjM="
+      _id: "cmVhY3Rpb24vYWNjb3VudDoxMjM=",
+      addressBook: {
+        nodes: [
+          { address1: "mockAddress1" }
+        ]
+      },
+      createdAt: mockAccount.createdAt.toISOString(),
+      currency: null,
+      emailRecords: [
+        {
+          address: mockAccount.emails[0].address,
+          verified: mockAccount.emails[0].verified
+        }
+      ],
+      groups: {
+        nodes: null
+      },
+      metafields: [
+        {
+          description: "mockDescription",
+          key: "mockKey",
+          namespace: "mockNamespace",
+          scope: "mockScope",
+          value: "mockValue",
+          valueType: "mockValueType"
+        }
+      ],
+      name: "mockName",
+      note: "mockNote",
+      preferences: {},
+      shop: null,
+      taxSettings: {
+        customerUsageType: "mockCustomerUsageType",
+        exemptionNo: "mockExemptionNo"
+      },
+      updatedAt: mockAccount.updatedAt.toISOString()
     }
   });
 
