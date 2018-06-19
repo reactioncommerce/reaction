ad4c9715d0bd84b03eefd1855f5e31ddc06a4468
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-19 16:19:26 -0700

    remove extra migration

--- imports/plugins/core/versions/server/migrations/index.js
+++ imports/plugins/core/versions/server/migrations/index.js
@@ -24,5 +24,4 @@ import "./23_drop_tempstore_collections";
 import "./24_publish_all_existing_visible_products";
 import "./25_update_catalog_schema.js";
 import "./26_remove_revision_control";
-import "./27_add_product_hash";
-import "./28_publish_all_existing_visible_products_rerun";
+// import "./27_add_product_hash";
