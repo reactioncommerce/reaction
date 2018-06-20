34a18ba2486c51ad062b7ca4f51586800c072da7
Nat Hamilton <info@nathamilton.com>
2018-06-20 17:10:06 -0500

    test: updated catalog find query

--- server/publications/collections/product.js
+++ server/publications/collections/product.js
@@ -115,8 +115,8 @@ Meteor.publish("Product", function (productIdOrHandle, shopIdOrSlug) {
       "product._id": productIdOrHandle
     }, {
       "product.slug": productIdOrHandle
-    }]
-    // "product.type": "product-simple",
+    }],
+    "product.type": "product-simple"
     // "product.shopId": selector.shopId,
     // "product.isVisible": true,
     // "product.isDeleted": { $in: [null, false] }
