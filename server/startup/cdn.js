ea08fdf04f85cc04e9d2a5a7fcee2f7e1d776c73
Akarshit Wal <akarshitwal@gmail.com>
2018-06-20 10:58:42 +0530

    jsdoc fix

--- server/startup/cdn.js
+++ server/startup/cdn.js
@@ -1,10 +1,11 @@
 import { Meteor } from "meteor/meteor";
 
 /**
- * Sets prefix for the user who wants to serve bundled js and css
+ * @name setupCdn
+ * @summary sets prefix for the user who wants to serve bundled js and css
  * files from different URL
  *
- * @return {null} null
+ * @return {undefined} undefined
  */
 export default function setupCdn() {
   if (Meteor.settings.cdnPrefix) {
