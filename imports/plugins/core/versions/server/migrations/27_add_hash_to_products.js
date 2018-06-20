2f40f91ab9b2458ede674b58c62d9149642d98d4
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-19 23:29:00 -0700

    remove extraneous migration code

--- imports/plugins/core/versions/server/migrations/27_add_hash_to_products.js
+++ imports/plugins/core/versions/server/migrations/27_add_hash_to_products.js
@@ -3,27 +3,13 @@ import { Catalog } from "/lib/collections";
 import collections from "/imports/collections/rawCollections";
 import hashProduct from "/imports/plugins/core/catalog/server/no-meteor/utils/hashProduct";
 
-// Do this migration in batches of 200 to avoid memory issues
-const LIMIT = 200;
-
 Migrations.add({
   version: 27,
   up() {
-    let products;
+    const products = Catalog.find({
+      "product.type": "product-simple"
+    }).fetch();
 
-    do {
-      products = Catalog.find({
-        "product.type": "product-simple"
-      }, {
-        limit: LIMIT,
-        sort: {
-          createdAt: 1
-        }
-      }).fetch();
-
-      if (products.length) {
-        products.forEach((product) => Promise.await(hashProduct(product.product._id, collections)));
-      }
-    } while (products.length);
+    products.forEach((product) => Promise.await(hashProduct(product.product._id, collections)));
   }
 });
