c1d5be31c54854c6b4f4831f48baf6ca15578126
Nat Hamilton <info@nathamilton.com>
2018-06-20 15:58:01 -0500

    test: updated products publication test

--- server/publications/collections/product-publications.app-test.js
+++ server/publications/collections/product-publications.app-test.js
@@ -383,6 +383,10 @@ describe("Publication", function () {
     });
 
     describe("Product", function () {
+      beforeEach(function () {
+        Collections.Catalog.remove({});
+      });
+
       it("should return a product based on an id", function (done) {
         this.timeout(25000);
         const product = Collections.Products.findOne({
