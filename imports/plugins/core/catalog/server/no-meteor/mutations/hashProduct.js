e1cb72a7b87b2c6a9a02456b2ee22afee11133ad
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-21 11:17:34 -0700

    add more fields to hashable product

--- imports/plugins/core/catalog/server/no-meteor/mutations/hashProduct.js
+++ imports/plugins/core/catalog/server/no-meteor/mutations/hashProduct.js
@@ -1,14 +1,17 @@
 import hash from "object-hash";
-import Random from "@reactioncommerce/random";
+import createCatalogProduct from "../utils/createCatalogProduct";
 
 /**
  * @method createProductHash
  * @summary Create a hash of a product to compare for updates
  * @memberof Catalog
- * @param {String} product - A product object
+ * @param {String} productToConvert - A product object
+ * @param {Object} collections - Raw mongo collections
  * @return {String} product hash
  */
-function createProductHash(product) {
+async function createProductHash(productToConvert, collections) {
+  const product = await createCatalogProduct(productToConvert, collections);
+
   const hashableFields = {
     _id: product._id,
     ancestors: product.ancestors,
@@ -17,18 +20,15 @@ function createProductHash(product) {
     googleplusMsg: product.googleplusMsg,
     handle: product.handle,
     hashtags: product.hashtags,
-    isBackorder: product.isBackorder,
     isDeleted: product.isDeleted,
-    isLowQuantity: product.isLowQuantity,
-    isSoldOut: product.isSoldOut,
     isVisible: product.isVisible,
+    media: product.media,
     metaDescription: product.metaDescription,
     metafields: product.metafields,
     originCountry: product.originCountry,
     pageTitle: product.pageTitle,
     parcel: product.parcel,
     pinterestMsg: product.pinterestMsg,
-    positions: product.positions,
     productType: product.productType,
     price: {
       range: product.price.range,
@@ -42,10 +42,8 @@ function createProductHash(product) {
     title: product.title,
     twitterMsg: product.twitterMsg,
     type: product.type,
-    vendor: product.vendor,
-    workflow: {
-      status: product.workflow.status
-    }
+    variants: product.variants,
+    vendor: product.vendor
   };
 
   return hash(hashableFields);
@@ -64,7 +62,7 @@ export default async function hashProduct(productId, collections) {
 
   const product = await Products.findOne({ _id: productId });
 
-  const productHash = createProductHash(product, collections);
+  const productHash = await createProductHash(product, collections);
 
   // Insert/update product document with hash field
   const result = await Products.updateOne(
