bac25b3e9f4c38a31fc02fff885577b08abf36ea
Eric Dobbertin <eric@dairystatedesigns.com>
2018-06-25 18:04:25 -0500

    fix: incorrect tag query

--- imports/plugins/core/catalog/server/no-meteor/queries/index.js
+++ imports/plugins/core/catalog/server/no-meteor/queries/index.js
@@ -1,11 +1,13 @@
 import catalogItems from "./catalogItems";
 import catalogItemProduct from "./catalogItemProduct";
+import tag from "./tag";
 import tags from "./tags";
 import tagsByIds from "./tagsByIds";
 
 export default {
   catalogItems,
   catalogItemProduct,
+  tag,
   tags,
   tagsByIds
 };
