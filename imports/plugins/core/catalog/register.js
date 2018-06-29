223db543d31f3c2ca8aa24cd97a8d508f634bf41
Will Lopez <will.lopez77@gmail.com>
2018-06-29 14:00:05 -0700

    Only remove menu item registration, and leave Catalog package registered.

---
+++ imports/plugins/core/catalog/register.js
@@ -0,0 +1,12 @@
+
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "Catalog",
+  name: "reaction-catalog",
+  icon: "fa fa-book",
+  autoEnable: true,
+  settings: {
+    name: "Catalog"
+  }
+});
