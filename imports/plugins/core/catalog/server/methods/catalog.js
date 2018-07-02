815b2369a3bd34ba376d3b3381d08078cb17949e
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-02 14:37:38 -0700

    change $in: [false, undefined] to $ne: true to account for null

--- imports/plugins/core/catalog/server/methods/catalog.js
+++ imports/plugins/core/catalog/server/methods/catalog.js
@@ -626,7 +626,7 @@ Meteor.methods({
     const selector = {
       // Don't "archive" variants that are already marked deleted.
       isDeleted: {
-        $in: [false, undefined]
+        $ne: true
       },
       $or: [
         {
@@ -874,7 +874,7 @@ Meteor.methods({
     const productsWithVariants = Products.find({
       // Don't "archive" products that are already marked deleted.
       isDeleted: {
-        $in: [false, undefined]
+        $ne: true
       },
       $or: [
         {
