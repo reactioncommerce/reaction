4ea11c82be8f302da107689c4123e82a87f4733b
Daniel Castellon <dan.a.castellon@gmail.com>
2018-07-11 11:25:18 -0400

    (feat): recurring job that regenerates sitemaps

--- imports/plugins/included/sitemap-generator/server/index.js
+++ imports/plugins/included/sitemap-generator/server/index.js
@@ -1,8 +1,12 @@
 import { Meteor } from "meteor/meteor";
 import { WebApp } from "meteor/webapp";
+import generateSitemapsJob from "./jobs/generateSitemapsJob";
 import handleSitemapRoutes from "./middleware/handleSitemapRoutes";
 import methods from "./methods";
 import "./i18n";
 
+generateSitemapsJob();
+
 WebApp.connectHandlers.use(handleSitemapRoutes);
+
 Meteor.methods(methods);
