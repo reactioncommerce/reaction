3ffe3e763e6b3256274ad41a11178ae707b32333
Eric Dobbertin <eric@dairystatedesigns.com>
2018-07-12 16:27:44 -0500

    tests: fix test failures

--- imports/plugins/core/graphql/server/no-meteor/resolvers/cart/Query/accountCartByAccountId.test.js
+++ imports/plugins/core/graphql/server/no-meteor/resolvers/cart/Query/accountCartByAccountId.test.js
@@ -9,13 +9,15 @@ test("calls queries.cart.accountCartByAccountId and returns the result", async (
   const mockResponse = "MOCK_RESPONSE";
   const mockQuery = jest.fn().mockName("queries.cart.accountCartByAccountId").mockReturnValueOnce(Promise.resolve(mockResponse));
 
+  const context = {
+    queries: { cart: { accountCartByAccountId: mockQuery } },
+    userId: "123"
+  };
+
   const result = await accountCartByAccountId(null, {
     accountId: opaqueAccountId,
     shopId: opaqueShopId
-  }, {
-    queries: { cart: { accountCartByAccountId: mockQuery } },
-    userId: "123"
-  });
+  }, context);
 
   expect(result).toEqual(mockResponse);
   expect(mockQuery).toHaveBeenCalledWith(context, {
