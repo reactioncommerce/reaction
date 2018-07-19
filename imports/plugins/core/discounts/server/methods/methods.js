f6377201ff98adc3a4c008241bd81bc7ef2d086a
Brent Hoover <brent@thebuddhalodge.com>
2018-07-19 15:34:21 +0800

    (refactor) Fixed tests to not use method that's not used in code

--- imports/plugins/core/discounts/server/methods/methods.js
+++ imports/plugins/core/discounts/server/methods/methods.js
@@ -52,26 +52,6 @@ export const methods = {
   },
 
   /**
-   * @name discounts/addRate
-   * @method
-   * @memberof Discounts/Rates/Methods
-   * @param  {Object} doc A Discounts document to be inserted
-   * @param  {String} [docId] DEPRECATED. Existing ID to trigger an update. Use discounts/editCode method instead.
-   * @return {String} Insert result
-   */
-  "discounts/addRate"(doc, docId) {
-    check(doc, Object); // actual schema validation happens during insert below
-
-    // Backward compatibility
-    check(docId, Match.Optional(String));
-    if (docId) return Meteor.call("discounts/editRate", { _id: docId, modifier: doc });
-
-    if (!Reaction.hasPermission("discount-rates")) throw new Meteor.Error("access-denied", "Access Denied");
-    doc.shopId = Reaction.getShopId();
-    return Discounts.insert(doc);
-  },
-
-  /**
    * @name discounts/editRate
    * @method
    * @memberof Discounts/Rates/Methods
