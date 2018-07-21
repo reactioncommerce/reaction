8cc870353dc3079dd94be6baf6b7f7588c29a38e
Eric Dobbertin <eric@dairystatedesigns.com>
2018-07-21 06:27:17 -0500

    feat: cart/mergeCart Meteor method now defers to reconcileCarts mutation

--- imports/plugins/core/cart/server/methods/mergeCart.js
+++ imports/plugins/core/cart/server/methods/mergeCart.js
@@ -6,6 +6,8 @@ import { Accounts, Cart } from "/lib/collections";
 import Reaction from "/imports/plugins/core/core/server/Reaction";
 import getCart from "/imports/plugins/core/cart/both/util/getCart";
 import hashLoginToken from "/imports/plugins/core/accounts/server/no-meteor/util/hashLoginToken";
+import getGraphQLContextInMeteorMethod from "/imports/plugins/core/graphql/server/getGraphQLContextInMeteorMethod";
+import reconcileCarts from "../no-meteor/mutations/reconcileCarts";
 
 /**
  * @method cart/mergeCart
@@ -17,12 +19,11 @@ import hashLoginToken from "/imports/plugins/core/accounts/server/no-meteor/util
  * @memberof Cart/Methods
  * @param {String} cartId - cartId of the cart to merge matching session carts into.
  * @param {String} [currentSessionId] - current client session id
- * @todo I think this method should be moved out from methods to a Function Declaration to keep it more secure
  * @return {Object|Boolean} cartId - cartId on success or false
  */
 export default function mergeCart(cartId, currentSessionId) {
   check(cartId, String);
-  // TODO: Review this. currentSessionId sometimes come in as false. e.g from Accounts.onLogin
+  // currentSessionId sometimes come in as false. e.g from Accounts.onLogin
   check(currentSessionId, Match.Optional(String));
 
   // we don't process current cart, but merge into it.
@@ -39,74 +40,45 @@ export default function mergeCart(cartId, currentSessionId) {
   const shopId = Reaction.getCartShopId();
 
   // If things are working properly, there should be only one.
-  const sessionCarts = Cart.find({
+  const sessionCart = Cart.findOne({
     accountId: { $ne: accountCart._id },
     anonymousAccessToken: hashLoginToken(sessionId),
     shopId
-  }).fetch();
-
-  Logger.debug(`merge cart: begin merge processing of session carts into account cart with ID ${accountCart._id}`);
-  // loop through session carts and merge into user cart
-  sessionCarts.forEach((sessionCart) => {
-    Logger.debug(`merge cart: merge user account ID: ${account._id}, sessionCart ID: ${sessionCart._id}`);
-
-    // really if we have no items, there's nothing to merge
-    if (Array.isArray(sessionCart.items) && sessionCart.items.length) {
-      // if currentCart already have a cartWorkflow, we don't need to clean it
-      // up completely, just to `coreCheckoutShipping` stage. Also, we will
-      // need to recalculate shipping rates
-      if (accountCart.workflow && accountCart.workflow.workflow) {
-        if (accountCart.workflow.workflow.length > 2) {
-          Meteor.call("workflow/revertCartWorkflow", "coreCheckoutShipping");
-          // refresh shipping quotes
-          Meteor.call("shipping/updateShipmentQuotes", cartId);
-        }
-      } else {
-        // if user logged in he doesn't need to show `checkoutLogin` step
-        Meteor.call("workflow/revertCartWorkflow", "checkoutAddressBook");
-      }
-
-      const cartSum = sessionCart.items.concat(accountCart.items);
-      const mergedItems = cartSum.reduce((newItems, item) => {
-        if (item) {
-          const existingItem = newItems.find((cartItem) => cartItem.variantId === item.variantId);
-          if (existingItem) {
-            existingItem.quantity += item.quantity;
-          } else {
-            newItems.push(item);
-          }
-        }
-        return newItems;
-      }, []);
-
-      Cart.update(accountCart._id, {
-        $push: {
-          items: { $each: mergedItems, $slice: -(mergedItems.length) }
-        }
-      });
-
-      // Calculate discounts
-      Hooks.Events.run("afterCartUpdateCalculateDiscount", accountCart._id);
-    }
-
-    // Destroy the anonymous cart
-    Cart.remove({ _id: sessionCart._id });
-
-    // Destroy the anonymous user and account
-    const sessionAccount = Accounts.findOne({ _id: sessionCart.accountId });
-    if (sessionAccount) {
-      Accounts.remove({ _id: sessionAccount._id });
-      Hooks.Events.run("afterAccountsRemove", this.userId, sessionAccount._id);
-      Meteor.users.remove({ _id: sessionAccount.userId });
-      Logger.debug(`merge cart: delete cart ${sessionCart._id} and anonymous account: ${sessionAccount._id}`);
-    }
-
-    Logger.debug(`merge cart: processed merge for anonymous cart ID ${sessionCart._id}`);
   });
 
-  // `checkoutLogin` should be used for anonymous only. Registered users
-  // no need see this.
-  if (accountCart.workflow && accountCart.workflow.status === "new") {
+  if (!sessionCart) {
+    throw new Meteor.Error("not-found", `No cart found with token ${sessionId}`);
+  }
+
+  // Pass through to the new mutation function at this point
+  const context = Promise.await(getGraphQLContextInMeteorMethod(account.userId));
+  Promise.await(reconcileCarts(context, {
+    anonymousCartId: sessionCart._id,
+    anonymousCartToken: sessionId,
+    mode: "merge",
+    shopId
+  }));
+
+  // Destroy the anonymous user and account
+  const sessionAccount = Accounts.findOne({ _id: sessionCart.accountId });
+  if (sessionAccount) {
+    Accounts.remove({ _id: sessionAccount._id });
+    Hooks.Events.run("afterAccountsRemove", this.userId, sessionAccount._id);
+    Meteor.users.remove({ _id: sessionAccount.userId });
+    Logger.debug(`merge cart: delete cart ${sessionCart._id} and anonymous account: ${sessionAccount._id}`);
+  }
+
+  // Calculate discounts
+  Hooks.Events.run("afterCartUpdateCalculateDiscount", accountCart._id);
+
+  const accountCartWorkflow = accountCart.workflow;
+  if (accountCartWorkflow && accountCartWorkflow.workflow) {
+    if (accountCartWorkflow.workflow.length > 2) {
+      Meteor.call("workflow/revertCartWorkflow", "coreCheckoutShipping");
+      // refresh shipping quotes
+      Meteor.call("shipping/updateShipmentQuotes", cartId);
+    }
+  } else if (accountCartWorkflow && accountCartWorkflow.status === "new") {
     // to call `workflow/pushCartWorkflow` two times is the only way to move
     // from status "new" to "checkoutAddressBook" which I found without
     // refactoring of `workflow/pushCartWorkflow`
@@ -121,6 +93,9 @@ export default function mergeCart(cartId, currentSessionId) {
       "workflow/pushCartWorkflow", "coreCartWorkflow",
       "checkoutAddressBook", cartId
     );
+  } else {
+    // if user logged in he doesn't need to show `checkoutLogin` step
+    Meteor.call("workflow/revertCartWorkflow", "checkoutAddressBook");
   }
 
   return accountCart._id;
