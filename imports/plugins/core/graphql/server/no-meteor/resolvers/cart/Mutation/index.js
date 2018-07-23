0643dcea74a9bbd9ee6db20e0f5cd38107f27f28
Eric Dobbertin <eric@dairystatedesigns.com>
2018-07-23 09:28:10 -0500

    feat(GraphQL): add resolver for removeCartItems

--- imports/plugins/core/graphql/server/no-meteor/resolvers/cart/Mutation/index.js
+++ imports/plugins/core/graphql/server/no-meteor/resolvers/cart/Mutation/index.js
@@ -1,11 +1,13 @@
 import addCartItems from "./addCartItems";
 import createCart from "./createCart";
 import reconcileCarts from "./reconcileCarts";
+import removeCartItems from "./removeCartItems";
 import updateCartItemsQuantity from "./updateCartItemsQuantity";
 
 export default {
   addCartItems,
   createCart,
   reconcileCarts,
+  removeCartItems,
   updateCartItemsQuantity
 };
