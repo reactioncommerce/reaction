f8a10c01b948f4bd81ca5ad2b812c737005d075c
Daniel Castellon <dan.a.castellon@gmail.com>
2018-07-25 16:37:42 -0400

    (feat): migration to new Products React component

---
+++ imports/plugins/core/versions/server/migrations/33_change_products_template_name.js
@@ -0,0 +1,40 @@
+import { Migrations } from "meteor/percolate:migrations";
+import { Packages } from "/lib/collections";
+
+Migrations.add({
+  version: 33,
+  up() {
+    Packages.update({
+      "layout.structure.template": "products"
+    }, {
+      $set: {
+        "layout.$.structure.template": "Products"
+      }
+    }, { bypassCollection2: true });
+
+    Packages.update({
+      "registry.template": "products"
+    }, {
+      $set: {
+        "registry.$.template": "Products"
+      }
+    }, { bypassCollection2: true });
+  },
+  down() {
+    Packages.update({
+      "layout.structure.template": "Products"
+    }, {
+      $set: {
+        "layout.$.structure.template": "products"
+      }
+    }, { bypassCollection2: true });
+
+    Packages.update({
+      "registry.template": "Products"
+    }, {
+      $set: {
+        "registry.$.template": "products"
+      }
+    }, { bypassCollection2: true });
+  }
+});
--- imports/plugins/core/versions/server/migrations/index.js
+++ imports/plugins/core/versions/server/migrations/index.js
@@ -30,3 +30,4 @@ import "./29_default_parcel_size";
 import "./30_cart_schema_changes";
 import "./31_order_schema_changes";
 import "./32_publish_all_existing_visible_products";
+import "./33_change_products_template_name";
