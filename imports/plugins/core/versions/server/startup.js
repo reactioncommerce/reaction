340e1057c390a44cbb8a368f48bd98da6e43d6c8
Seun Martins <seundotme@gmail.com>
2018-07-27 13:15:38 +0100

    Update logger messages and no setcontrol

--- imports/plugins/core/versions/server/startup.js
+++ imports/plugins/core/versions/server/startup.js
@@ -20,9 +20,13 @@ Hooks.Events.add("afterCoreInit", () => {
   const currentMigrationVersion = Migrations._getControl().version;
   const highestAvailableVersion = Migrations._list[Migrations._list.length - 1].version;
 
+  // Checks to ensure the app is running against a DB at the right migration state. Running the app
+  // with a wrong DB state will cause the app to malfunction
   if (currentMigrationVersion > highestAvailableVersion) {
-    Logger.warn(`You are running a Reaction install with migration version (${highestAvailableVersion}) below your current DB migration state (${currentMigrationVersion})`);
-    Migrations._setControl({ locked: false, version: highestAvailableVersion });
+    Logger.fatal(`You are running a Reaction install with migration version (${highestAvailableVersion}) below your current DB migration state (${currentMigrationVersion})`);
+    Logger.fatal(`Upgrade to a version of Reaction containing migration ${currentMigrationVersion} or higher.`);
+    Logger.fatal("If you really want to downgrade to this version, you should restore your DB to a previous state from your backup.");
+    process.exit(0);
   } else if (!Meteor.isTest) {
     Migrations.migrateTo("latest");
   }
