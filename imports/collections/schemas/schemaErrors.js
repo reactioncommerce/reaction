6ec85520a6c767ce5a1afe268d02e3e0caa2c84d
Nat Hamilton <info@nathamilton.com>
2018-07-27 16:47:30 -0500

    refactor: replaced Meteor.Error with ReactionError in import/collection methods

--- imports/collections/schemas/schemaErrors.js
+++ imports/collections/schemas/schemaErrors.js
@@ -1,11 +1,11 @@
 import SimpleSchema from "simpl-schema";
-import { Meteor } from "meteor/meteor";
+import ReactionError from "/imports/plugins/core/graphql/server/no-meteor/ReactionError";
 
-// This is needed so that it throws a Meteor.Error as `check()` would do
+// This is needed so that it throws a ReactionError as `check()` would do
 // when we call schema.validate() in a Meteor method.
 // https://github.com/aldeed/node-simple-schema/#customize-the-error-that-is-thrown
 SimpleSchema.defineValidationErrorTransform((error) => {
-  const ddpError = new Meteor.Error(error.message);
+  const ddpError = new ReactionError(error.message);
   ddpError.error = "validation-error";
   ddpError.details = error.details;
   return ddpError;
