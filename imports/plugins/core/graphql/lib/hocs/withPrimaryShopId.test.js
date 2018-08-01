b0c4c05556b11d0b6113fc82dcf6a099dda7d14f
Daniel Castellon <dan.a.castellon@gmail.com>
2018-08-01 15:05:51 -0400

    (test): wait until GraphQL data is loaded before expecting

--- imports/plugins/core/graphql/lib/hocs/withPrimaryShopId.test.js
+++ imports/plugins/core/graphql/lib/hocs/withPrimaryShopId.test.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { compose } from "recompose";
 import { mount } from "enzyme";
 import { MockedProvider } from "react-apollo/test-utils";
+import waitUntilDataLoaded from "/imports/test-utils/helpers/waitUntilLoaded";
 import getPrimaryShopId from "../queries/getPrimaryShopId";
 import withPrimaryShopId from "./withPrimaryShopId";
 
 const fakeOpaqueShopId = "cmVhY3Rpb24vc2hvcDpKOEJocTN1VHRkZ3daeDNyeg==";
 const MockComponent = () => <div>Mock</div>;
-const TestComponent = compose(withPrimaryShopId)(MockComponent);
+const TestComponent = withPrimaryShopId(MockComponent);
 
 test("renders child component with correct shop id", async () => {
   const mocks = [{
@@ -25,8 +25,7 @@ test("renders child component with correct shop id", async () => {
     </MockedProvider>
   );
 
-  // Wait for next tick to get past loading state
-  await new Promise(resolve => setTimeout(resolve));
+  await waitUntilDataLoaded(wrapper, "MockComponent", "isLoadingPrimaryShopId");
 
-  expect(wrapper.update().find("MockComponent").prop("shopId")).toBe(fakeOpaqueShopId);
+  expect(wrapper.find("MockComponent").prop("shopId")).toBe(fakeOpaqueShopId);
 });
---
+++ imports/test-utils/helpers/waitUntilLoaded.js
@@ -0,0 +1,22 @@
+/**
+ * @name waitUntilDataLoaded
+ * @summary Promise function that resolves when an enzyme-mounted React component's prop in question is truthy.
+ *  Useful for waiting until a GraphQL HOC has finished loading data (via Apollo's MockedProvider) in unit tests.
+ * @namespace TestHelpers
+ * @param {ReactWrapper} reactWrapper - result of calling enzyme's mount()
+ * @param {String} componentName - name of component to check
+ * @param {String} propName - name of prop on component to check for truthiness
+ * @returns {Promise} that resolves when prop is truthy
+ */
+export default function waitUntilDataLoaded(reactWrapper, componentName, propName) {
+  return new Promise((resolve) => {
+    // Use timeout to prevent blocking event loop, to allow MockedProvider to update loading status asynchronously
+    setTimeout(() => {
+      if (reactWrapper.update().find(componentName).prop(propName)) {
+        resolve(waitUntilDataLoaded());
+      } else {
+        resolve();
+      }
+    });
+  });
+}
