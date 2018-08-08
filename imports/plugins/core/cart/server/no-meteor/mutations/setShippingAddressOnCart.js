50188d2eaed2e105735acabc42958d4ee28ef4ab
Eric Dobbertin <eric@dairystatedesigns.com>
2018-08-08 15:05:27 -0500

    chore: generate setShippingAddressOnCart files

---
+++ imports/plugins/core/cart/server/no-meteor/mutations/setShippingAddressOnCart.js
@@ -0,0 +1,12 @@
+import Logger from "@reactioncommerce/logger";
+
+/**
+ * @method setShippingAddressOnCart
+ * @summary TODO
+ * @param {Object} context -  an object containing the per-request state
+ * @return {Promise<Object>} TODO
+ */
+export default async function setShippingAddressOnCart(context) {
+  Logger.info("setShippingAddressOnCart mutation is not yet implemented");
+  return null;
+}
