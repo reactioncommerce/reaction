ed457a6aca9a50c551b18aeaeb0b6d88b10f9652
Daniel Castellon <dan.a.castellon@gmail.com>
2018-08-23 17:07:08 -0400

    chore: fix eslint issue

--- imports/plugins/core/graphql/server/no-meteor/resolvers/catalog/CatalogProduct/index.js
+++ imports/plugins/core/graphql/server/no-meteor/resolvers/catalog/CatalogProduct/index.js
@@ -15,11 +15,9 @@ export default {
   tags,
   media: (node, args, context) => node.media.map((mediaItem) => xformProductMedia(mediaItem, context)),
   primaryImage: (node, args, context) => xformProductMedia(node.primaryImage, context),
-  variants: (node, args, context) => {
-    return node.variants.map((variant) => {
-      variant.media = variant.media.map((mediaItem) => xformProductMedia(mediaItem, context));
-      variant.primaryImage = xformProductMedia(variant.primaryImage, context);
-      return variant;
-    });
-  }
+  variants: (node, args, context) => node.variants.map((variant) => {
+    variant.media = variant.media.map((mediaItem) => xformProductMedia(mediaItem, context));
+    variant.primaryImage = xformProductMedia(variant.primaryImage, context);
+    return variant;
+  })
 };
