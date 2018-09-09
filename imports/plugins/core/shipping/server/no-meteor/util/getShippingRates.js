3a728539059dd608bd28955ddac2cef31f5869ff
Eric Dobbertin <eric@dairystatedesigns.com>
2018-09-09 08:03:04 -0500

    refactor: update getShippingRates function to work on a group

--- imports/plugins/core/shipping/server/no-meteor/util/getShippingRates.js
+++ imports/plugins/core/shipping/server/no-meteor/util/getShippingRates.js
@@ -5,27 +5,27 @@ import { getRegisteredFunctionsForType } from "/imports/core-server";
  * @name getShippingRates
  * @method
  * @summary Just gets rates, without updating anything
- * @param {Object} cart - cart object
+ * @param {Object} fulfillmentGroup - fulfillmentGroup object
  * @param {Object} context - Context
  * @return {Array} return updated rates in cart
  * @private
  */
-export default async function getShippingRates(cart, context) {
+export default async function getShippingRates(fulfillmentGroup, context) {
   const rates = [];
   const retrialTargets = [];
   // must have items to calculate shipping
-  if (!cart.items || !cart.items.length) {
+  if (!fulfillmentGroup.items || !fulfillmentGroup.items.length) {
     return rates;
   }
 
   const rateFunctions = getRegisteredFunctionsForType("getShippingPrices");
 
-  let promises = rateFunctions.map((rateFunction) => rateFunction(context, cart, [rates, retrialTargets]));
+  let promises = rateFunctions.map((rateFunction) => rateFunction(context, fulfillmentGroup, [rates, retrialTargets]));
   await Promise.all(promises);
 
   // Try once more.
   if (retrialTargets.length > 0) {
-    promises = rateFunctions.map((rateFunction) => rateFunction(context, cart, [rates, retrialTargets]));
+    promises = rateFunctions.map((rateFunction) => rateFunction(context, fulfillmentGroup, [rates, retrialTargets]));
     await Promise.all(promises);
 
     if (retrialTargets.length > 0) {
