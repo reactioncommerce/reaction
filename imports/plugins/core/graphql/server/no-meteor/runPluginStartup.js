0a8fc7d3b968dffc0fdeb88698cf9e0b7aae1bc0
Eric Dobbertin <eric@dairystatedesigns.com>
2018-09-09 07:53:18 -0500

    feat: hook to delete cart after order is created from it

--- imports/plugins/core/graphql/server/no-meteor/runPluginStartup.js
+++ imports/plugins/core/graphql/server/no-meteor/runPluginStartup.js
@@ -1,8 +1,10 @@
 import { registerFunction } from "/imports/core-server";
+import cartStartup from "/imports/plugins/core/cart/server/no-meteor/startup";
 import discountsStartup from "/imports/plugins/core/discounts/server/no-meteor/startup";
 import shippingStartup from "/imports/plugins/core/shipping/server/no-meteor/startup";
 import notificationsStartup from "/imports/plugins/included/notifications/server/no-meteor/startup";
 import searchStartup from "/imports/plugins/included/search-mongo/server/no-meteor/startup";
+import shippingRatesStartup from "/imports/plugins/included/shipping-rates/server/no-meteor/startup";
 
 /**
  * @param {Object} context Context object with appEvents and collections
@@ -13,6 +15,8 @@ export default function runPluginStartup(context) {
     ...context,
     registerFunction
   };
+
+  cartStartup(expandedContext);
   discountsStartup(expandedContext);
   notificationsStartup(expandedContext);
   searchStartup(expandedContext);
