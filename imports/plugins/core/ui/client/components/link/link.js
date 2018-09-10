1c49d5232a3987d84d271697565e0efbb21c1a2a
Daniel Castellon <dan.a.castellon@gmail.com>
2018-09-10 09:51:44 -0400

    chore: jsdoc for Link component

--- imports/plugins/core/ui/client/components/link/link.js
+++ imports/plugins/core/ui/client/components/link/link.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+/**
+ * @file
+ * Link is a drop-in replacement for the HTML <a> tag. It uses Reaction's router to navigate to the specified href,
+ * preventing the entire client app from reloading. It also scrolls to the top of the next page.
+ *
+ * @module Link
+ * @extends Component
+ */
+
 export default class Link extends Component {
   static propTypes = {
     href: PropTypes.string.isRequired
