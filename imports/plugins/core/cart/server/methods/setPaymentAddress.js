e77280999d722f4a02dd2bcb0d66d2840961401c
Eric Dobbertin <eric@dairystatedesigns.com>
2018-09-09 20:02:15 -0500

    refactor: store billing address on cart.billingAddress

--- imports/plugins/core/cart/server/methods/setPaymentAddress.js
+++ imports/plugins/core/cart/server/methods/setPaymentAddress.js
@@ -19,36 +19,15 @@ export default function setPaymentAddress(cartId, cartToken, address) {
   check(cartToken, Match.Maybe(String));
   Reaction.Schemas.Address.validate(address);
 
-  const { cart } = getCart(cartId, { cartToken, throwIfNotFound: true });
+  getCart(cartId, { cartToken, throwIfNotFound: true });
 
-  let selector;
-  let update;
-  // temp hack until we build out multiple billing handlers
-  // if we have an existing item update it, otherwise add to set.
-  if (Array.isArray(cart.billing) && cart.billing.length > 0) {
-    selector = {
-      "_id": cartId,
-      "billing._id": cart.billing[0]._id
-    };
-    update = {
-      $set: {
-        "billing.$.address": address
-      }
-    };
-  } else {
-    selector = {
-      _id: cartId
-    };
-    update = {
-      $addToSet: {
-        billing: {
-          address
-        }
-      }
-    };
-  }
-
-  const result = Cart.update(selector, update);
+  const result = Cart.update({
+    _id: cartId
+  }, {
+    $set: {
+      billingAddress: address
+    }
+  });
 
   const updatedCart = Cart.findOne({ _id: cartId });
 
