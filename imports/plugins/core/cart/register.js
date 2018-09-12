fbdf52bccbdeb8dc6a59f46471d5bc4608257129
Eric Dobbertin <eric@dairystatedesigns.com>
2018-09-12 14:43:28 -0500

    refactor: move cart resolvers into cart plugin

--- imports/plugins/core/cart/register.js
+++ imports/plugins/core/cart/register.js
@@ -1,12 +1,16 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
 import mutations from "./server/no-meteor/mutations";
 import queries from "./server/no-meteor/queries";
+import resolvers from "./server/no-meteor/resolvers";
 import startup from "./server/no-meteor/startup";
 
 Reaction.registerPackage({
   label: "Cart",
   name: "reaction-cart",
   autoEnable: true,
+  graphQL: {
+    resolvers
+  },
   mutations,
   queries,
   startupFunctions: [startup]
