329c4adc064b5ac49eae097ad2fcd52892820568
Eric Dobbertin <eric@dairystatedesigns.com>
2018-09-12 21:55:48 -0500

    refactor: import resolvers into node app

--- imports/node-app/core/resolvers.js
+++ imports/node-app/core/resolvers.js
@@ -1,3 +1,27 @@
-import core from "/imports/plugins/core/graphql/server/no-meteor/resolvers";
+import { merge } from "lodash";
+import accounts from "/imports/plugins/accounts/graphql/server/no-meteor/resolvers";
+import cart from "/imports/plugins/cart/graphql/server/no-meteor/resolvers";
+import catalog from "/imports/plugins/catalog/graphql/server/no-meteor/resolvers";
+import orders from "/imports/plugins/orders/graphql/server/no-meteor/resolvers";
+import payments from "/imports/plugins/payments/graphql/server/no-meteor/resolvers";
+import product from "/imports/plugins/product/graphql/server/no-meteor/resolvers";
+import shipping from "/imports/plugins/shipping/graphql/server/no-meteor/resolvers";
+import marketplace from "/imports/plugins/marketplace/graphql/server/no-meteor/resolvers";
+import paymentsExample from "/imports/plugins/payments-example/graphql/server/no-meteor/resolvers";
+import paymentsStripe from "/imports/plugins/payments-stripe/graphql/server/no-meteor/resolvers";
+import shippingRates from "/imports/plugins/shipping-rates/graphql/server/no-meteor/resolvers";
 
-export default core;
+export default merge(
+  {},
+  accounts,
+  cart,
+  catalog,
+  orders,
+  payments,
+  product,
+  shipping,
+  marketplace,
+  paymentsExample,
+  paymentsStripe,
+  shippingRates
+);
