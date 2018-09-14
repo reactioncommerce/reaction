2e58a627168f02566b8655c35b95fbb5dbfb1f79
Daniel Castellon <dan.a.castellon@gmail.com>
2018-09-14 12:18:05 -0400

    feat: getDisplayPrice util function

---
+++ imports/plugins/core/catalog/server/no-meteor/utils/getDisplayPrice.js
@@ -0,0 +1,40 @@
+import { formatMoney, toFixed } from "accounting-js";
+
+/**
+ * @name getDisplayPrice
+ * @method
+ * @summary Returns a price for front-end display in the given currency
+ * @param {Number} minPrice Minimum price
+ * @param {Number} maxPrice Maximum price
+ * @param {Object} currencyInfo Currency object from Reaction shop schema
+ * @returns {String} Display price with currency symbol(s)
+ */
+export default function getDisplayPrice(minPrice, maxPrice, currencyInfo = { symbol: "" }) {
+  console.log("currencyInfo:", currencyInfo);
+  let displayPrice;
+
+  if (minPrice === maxPrice) {
+    // Display 1 price (min = max)
+    displayPrice = formatMoney(minPrice, currencyInfo);
+  } else {
+    // Display range
+    let minFormatted;
+
+    // Account for currencies where only one currency symbol should be displayed. Ex: 680,18 - 1 359,68 руб.
+    if (currencyInfo.where === "right") {
+      const modifiedCurrencyInfo = Object.assign({}, currencyInfo, {
+        symbol: ""
+      });
+      minFormatted = formatMoney(minPrice, modifiedCurrencyInfo).trim();
+    } else {
+      minFormatted = formatMoney(minPrice, currencyInfo);
+    }
+
+    const maxFormatted = formatMoney(maxPrice, currencyInfo);
+    displayPrice = `${minFormatted} - ${maxFormatted}`;
+  }
+
+  console.log("displayPrice:", displayPrice);
+
+  return displayPrice;
+}
