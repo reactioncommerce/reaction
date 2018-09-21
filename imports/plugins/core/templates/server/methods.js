99df0cbb23925534f6971d6a4774647e70a4e5f6
Eric Dobbertin <eric@dairystatedesigns.com>
2018-09-21 07:19:03 -0500

    fix: submitting the template edit form now works

--- imports/plugins/core/templates/server/methods.js
+++ imports/plugins/core/templates/server/methods.js
@@ -16,15 +16,13 @@ export const methods = {
    * @name templates/email/update
    * @method
    * @memberof Templates/Methods
-   * @todo Add permissions
    * @summary Updates email template in Templates collection
-   * @param {String} templateId - id of template to remove
-   * @param {Object} doc - data to update
-   * @return {Number} remove template
+   * @param {String} details._id - id of template to update
+   * @param {Object} details.modifier - data to update
+   * @return {Number} update template
    */
-  "templates/email/update"(templateId, doc) {
-    check(templateId, String);
-    check(doc, Object);
+  "templates/email/update"(details) {
+    check(details, Object);
 
     const shopId = Reaction.getShopId();
     const userId = Reaction.getUserId();
@@ -35,19 +33,10 @@ export const methods = {
     }
 
     return Templates.update({
-      _id: templateId,
+      _id: details._id,
       type: "email",
       shopId // Ensure that the template we're attempting to update is owned by the active shop.
-    }, {
-      $set: {
-        title: doc.title,
-        name: doc.name,
-        language: doc.language,
-        template: doc.template,
-        subject: doc.subject,
-        enabled: doc.enabled
-      }
-    });
+    }, details.modifier);
   }
 };
 
