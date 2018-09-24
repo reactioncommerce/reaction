8efe62b0623e7b16598abd423cd5d0c2ac5b2b76
Seun Martins <seundotme@gmail.com>
2018-09-24 12:43:10 +0000

    fix: Get hydra session working
    When a user who already logged in (and is sill within the session lifespan), tries to login again, we won't show the login form again

--- imports/plugins/core/hydra-oauth/server/oauthEndpoints.js
+++ imports/plugins/core/hydra-oauth/server/oauthEndpoints.js
@@ -3,7 +3,7 @@ import Logger from "@reactioncommerce/logger";
 import { WebApp } from "meteor/webapp";
 import hydra from "./util/hydra";
 
-const { HYDRA_OAUTH2_ERROR_URL } = process.env;
+const { HYDRA_OAUTH2_ERROR_URL, HYDRA_SESSION_LIFESPAN } = process.env;
 const errorHandler = (errorMessage, res) => {
   Logger.error(`Error while performing Hydra login request - ${errorMessage}`);
   if (HYDRA_OAUTH2_ERROR_URL) {
@@ -23,13 +23,14 @@ WebApp.connectHandlers.use("/login", (req, res) => {
       const requestUrl = url.parse(getLoginRequestRes.request_url, true);
       const { loginAction } = requestUrl.query;
       // If Hydra was already able to authenticate the user, skip will be true
-      // and there will be no need to present a login form to the user.
+      // we do not need to re-authenticate the user.
       if (getLoginRequestRes.skip) {
         const acceptLoginResponse = await hydra.acceptLoginRequest(challenge, {
           subject: getLoginRequestRes.subject
         });
         Logger.debug(`Auth status confirmed from Hydra. Redirecting to: ${acceptLoginResponse.redirect_to}`);
-        res.redirect(acceptLoginResponse.redirect_to);
+        res.writeHead(301, { Location: acceptLoginResponse.redirect_to });
+        return res.end();
       }
 
       res.writeHead(301, { Location: `/account/login?action=${loginAction}&login_challenge=${challenge}` });
@@ -41,9 +42,16 @@ WebApp.connectHandlers.use("/login", (req, res) => {
 
 WebApp.connectHandlers.use("/consent", (req, res) => {
   const challenge = req.query.consent_challenge;
-  // Call acceptConsent without presenting a consent form to the user
+  // Here, we accept consent directly without presenting a consent form to the user
+  // because this was built for a trusted Consumer client.
+  // For non-trusted Consumer clients, this should be updated to present a Consent UI to
+  // the user grant or deny specific scopes
   hydra
-    .acceptConsentRequest(challenge, {})
+    .acceptConsentRequest(challenge, {
+      remember: true,
+      remember_for: HYDRA_SESSION_LIFESPAN || 3600, // eslint-disable-line camelcase
+      session: {} // we are not adding any extra user, we use only the sub value already present
+    })
     .then((consentResponse) => {
       Logger.debug(`Consent call complete. Redirecting to: ${consentResponse.redirect_to}`);
       res.writeHead(301, { Location: consentResponse.redirect_to });
