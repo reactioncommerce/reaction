975f540188ceaa97dea97dc35c98cdc232149230
Akarshit Wal <akarshitwal@gmail.com>
2018-09-27 16:52:16 +0530

    jsdoc

--- imports/plugins/core/catalog/server/no-meteor/utils/isSoldOut.js
+++ imports/plugins/core/catalog/server/no-meteor/utils/isSoldOut.js
@@ -2,9 +2,9 @@ import getProductQuantity from "./getProductQuantity";
 
 /**
  * @method isSoldOut
- * @summary If all the product variants have a quantity of 0 return `true`.
+ * @summary If the type and all its descendant have a quantity of 0 return `true`.
  * @memberof Catalog
- * @param {Object[]} variants - Array with top-level variants
+ * @param {Object[]} variants - Array with top-level variants/options/option
  * @return {Boolean} true if quantity is zero.
  */
 export default function isSoldOut(variants) {
@@ -19,15 +19,36 @@ export default function isSoldOut(variants) {
   return isOptionSoldOut(variants);
 }
 
+/**
+ * @method isVariantSoldOut
+ * @summary If all the product option have a quantity of 0 return `true`.
+ * @memberof Catalog
+ * @param {Object[]} options - Array of options
+ * @return {Boolean} true if quantity is zero.
+ */
 function isVariantSoldOut(options) {
   const results = options.map(isOptionSoldOut);
   return results.every((result) => result);
 }
 
+/**
+ * @method isOptionSoldOut
+ * @summary If the option has quantity 0 return `true`.
+ * @memberof Catalog
+ * @param {Object} option - option
+ * @return {Boolean} true if quantity is zero.
+ */
 function isOptionSoldOut(option) {
   return option.inventoryManagement && getProductQuantity(option) <= 0;
 }
 
+/**
+ * @method isProductSoldOut
+ * @summary If all the product variants have a quantity of 0 return `true`.
+ * @memberof Catalog
+ * @param {Object[]} variants - Array with top-level variants
+ * @return {Boolean} true if quantity is zero.
+ */
 function isProductSoldOut(variants) {
   const results = variants.map((variant) => {
     const quantity = getProductQuantity(variant, variants);
