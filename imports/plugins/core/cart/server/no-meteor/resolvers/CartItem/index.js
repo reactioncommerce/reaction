1edff81ab2a1b04129ccb14a36ae648a2a6a1915
Will Lopez <will.lopez77@gmail.com>
2018-10-08 09:33:25 -0700

    fix: lint error

--- imports/plugins/core/cart/server/no-meteor/resolvers/CartItem/index.js
+++ imports/plugins/core/cart/server/no-meteor/resolvers/CartItem/index.js
@@ -1,6 +1,6 @@
 import { encodeCartItemOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/cart";
 import { resolveShopFromShopId } from "@reactioncommerce/reaction-graphql-utils";
-import productTags from "./productTags"; 
+import productTags from "./productTags";
 
 export default {
   _id: (node) => encodeCartItemOpaqueId(node._id),
