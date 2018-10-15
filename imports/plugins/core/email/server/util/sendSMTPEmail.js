40a8da1e965558b44b773412bd02e1f6d9ade20c
Daniel Castellon <dan.a.castellon@gmail.com>
2018-10-15 13:22:06 -0400

    feat: skip sendSMTPEmail if selected email provider is not SMTP

--- imports/plugins/core/email/server/util/sendSMTPEmail.js
+++ imports/plugins/core/email/server/util/sendSMTPEmail.js
@@ -1,17 +1,42 @@
 import Logger from "@reactioncommerce/logger";
 import nodemailer from "@reactioncommerce/nodemailer";
 import { Meteor } from "meteor/meteor";
+import { Packages } from "/lib/collections";
+import Reaction from "/imports/plugins/core/core/server/Reaction";
 import Email from "/imports/plugins/core/core/server/Reaction/Email";
 
+/**
+ * @name sendSMTPEmail
+ * @summary Responds to the "sendEmail" app event to send an email via SMTP
+ * @param {Object} job Current sendEmail job being processed
+ * @param {Object} callbacks Functions to call on success or failure
+ * @param {Function} callbacks.sendEmailCompleted Call when email was successfully sent
+ * @param {Function} callbacks.sendEmailFailed Call on error
+ * @return {undefined} Call one of the callbacks with a return statement
+ */
 export default function sendSMTPEmail(job, { sendEmailCompleted, sendEmailFailed }) {
-  // TODO determine if selected email provider is SMTP, if not, return
-  const config = Email.getMailConfig();
+  // Determine if email provider is SMTP
+  const smtpProviders = require("nodemailer-wellknown/services.json");
+  const smtpProviderNames = Object.keys(smtpProviders);
+  const settings = Reaction.getShopSettings();
+  const mailSettings = settings.mail || {};
+  const mailServiceName = mailSettings.service || "";
 
-  if (config.direct) {
+  if (mailServiceName === "") {
     return sendEmailFailed(job, "Mail not configured");
   }
 
-  Logger.debug(config, "Sending email with config");
+  if (smtpProviderNames.includes(mailServiceName) === false) {
+    // Using non-SMTP email provider, skip, assuming another plugin will handle job
+    return;
+  }
+
+  const config = Email.getMailConfig();
+  if (config.direct) {
+    return sendEmailFailed(job, "SMTP mail settings not configured");
+  }
+
+  Logger.debug(config, "Sending SMTP email with config");
 
   const { from, to, subject, html, ...optionalEmailFields } = job.data;
   const transport = nodemailer.createTransport(config);
