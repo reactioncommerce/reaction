4fd2c650197aec5b899f538ab9c3cd56b20bfdba
Will Lopez <will.lopez77@gmail.com>
2018-10-15 16:12:40 -0700

    docs: correct orderItem param name.

--- imports/plugins/core/orders/server/no-meteor/resolvers/OrderItem/productTags.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/OrderItem/productTags.js
@@ -6,13 +6,13 @@ import { xformArrayToConnection } from "@reactioncommerce/reaction-graphql-xform
  * @method
  * @memberof Catalog/GraphQL
  * @summary Returns the tags for an OrderItem
- * @param {Object} order - OrderItem from parent resolver
+ * @param {Object} orderItem - OrderItem from parent resolver
  * @param {TagConnectionArgs} args - arguments sent by the client {@link ConnectionArgs|See default connection arguments}
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Object[]>} Promise that resolves with array of Tag objects
  */
-export default async function tags(OrderItem, connectionArgs, context) {
-  const { productTagIds } = OrderItem;
+export default async function productTags(orderItem, connectionArgs, context) {
+  const { productTagIds } = orderItem;
   if (!productTagIds || productTagIds.length === 0) return xformArrayToConnection(connectionArgs, []);
 
   const query = await context.queries.tagsByIds(context, productTagIds, connectionArgs);
