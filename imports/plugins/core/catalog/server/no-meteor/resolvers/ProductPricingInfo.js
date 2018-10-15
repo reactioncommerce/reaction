38853ac454f15712a085d37e0a04cfbb035d476e
Mike Murray <hellomikemurray@gmail.com>
2018-10-15 15:54:55 -0700

    fix: return null for compareAtPrice if its not a number greater than 0

--- imports/plugins/core/catalog/server/no-meteor/resolvers/ProductPricingInfo.js
+++ imports/plugins/core/catalog/server/no-meteor/resolvers/ProductPricingInfo.js
@@ -3,5 +3,5 @@ import { getXformedCurrencyByCode, xformCurrencyExchangePricing } from "@reactio
 export default {
   currency: (node) => getXformedCurrencyByCode(node.currencyCode),
   currencyExchangePricing: (node, { currencyCode }, context) => xformCurrencyExchangePricing(node, currencyCode, context),
-  compareAtPrice: (node) => ({ amount: node.compareAtPrice || 0, currencyCode: node.currencyCode })
+  compareAtPrice: ({ compareAtPrice: amount, currencyCode }) => (typeof amount === "number" && amount > 0 ? { amount, currencyCode } : null)
 };
