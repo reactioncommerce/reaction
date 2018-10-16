ff6c6264946e7b8622493160e699e96d438ae646
Ross Hadden <rosshadden@gmail.com>
2018-10-16 10:39:55 -0400

    Better handled checks for payment methods being enabled, and made a separate check for marketplace.

--- imports/plugins/core/versions/server/migrations/41_payment_methods.js
+++ imports/plugins/core/versions/server/migrations/41_payment_methods.js
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 import { Migrations } from "meteor/percolate:migrations";
 import { Packages, Shops } from "/lib/collections";
 
@@ -14,13 +15,23 @@ Migrations.add({
         "registry.provides": "paymentMethod"
       })
         .forEach((pkg) => {
-          if (pkg.name === "example-paymentmethod") {
+          if (pkg.name === "example-paymentmethod" && _.get(pkg, "settings.example-paymentmethod.enabled")) {
             methods.add("iou_example");
-          } else if (pkg.name === "reaction-stripe") {
+          } else if (pkg.name === "reaction-stripe" && _.get(pkg, "settings.reaction-stripe.enabled")) {
             methods.add("stripe_card");
-            methods.add("marketplace_stripe_card");
           }
         });
+
+      const marketplacePkg = Packages.findOne({
+        shopId: shop._id,
+        enabled: true,
+        name: "reaction-marketplace"
+      });
+      if (marketplacePkg && marketplacePkg.enabled) {
+        // We would never want both stripe card methods enabled, and marketplace should take precedence
+        methods.delete("stripe_card");
+        methods.add("marketplace_stripe_card");
+      }
 
       Shops.update(
         { _id: shop._id },
