bc14102897efcc681ed048c1b289a74d2e76d5c5
Ross Hadden <rosshadden@gmail.com>
2018-10-16 10:30:26 -0400

    Better use of mongo cursors.

--- imports/plugins/core/versions/server/migrations/41_payment_methods.js
+++ imports/plugins/core/versions/server/migrations/41_payment_methods.js
@@ -5,32 +5,29 @@ Migrations.add({
   version: 41,
 
   up() {
-    const shops = Shops.find();
-
-    for (const shop of shops) {
+    Shops.find().forEach((shop) => {
       const methods = new Set(shop.availablePaymentMethods);
 
-      const packages = Packages.find({
+      Packages.find({
         "shopId": shop._id,
         "enabled": true,
         "registry.provides": "paymentMethod"
-      });
-
-      for (const pkg of packages) {
-        if (pkg.name === "example-paymentmethod") {
-          methods.add("iou_example");
-        } else if (pkg.name === "reaction-stripe") {
-          methods.add("stripe_card");
-          methods.add("marketplace_stripe_card");
-        }
-      }
+      })
+        .forEach((pkg) => {
+          if (pkg.name === "example-paymentmethod") {
+            methods.add("iou_example");
+          } else if (pkg.name === "reaction-stripe") {
+            methods.add("stripe_card");
+            methods.add("marketplace_stripe_card");
+          }
+        });
 
       Shops.update(
         { _id: shop._id },
         { $set: { availablePaymentMethods: Array.from(methods) } },
         { bypassCollection2: true }
       );
-    }
+    });
   },
 
   down() {
