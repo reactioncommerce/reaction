6b3d3a480eb5d501919419e17b268b7578d16887
Daniel Castellon <dan.a.castellon@gmail.com>
2018-10-17 12:58:08 -0400

    fix: query function for primaryShop

--- imports/plugins/core/core/server/no-meteor/resolvers/Query/primaryShop.js
+++ imports/plugins/core/core/server/no-meteor/resolvers/Query/primaryShop.js
@@ -6,10 +6,9 @@
  * @param {Object} parentObject - unused
  * @param {Object} args - unused
  * @param {Object} context - an object containing the per-request state
- * @return {Promise<o>} The shop, based on the domain in ROOT_URL
+ * @return {Promise<Object>} The shop, based on the domain in ROOT_URL
  */
-export default async function primaryShopId(_, __, context) {
-  const shopId = await context.queries.primaryShopId(context.collections);
-  const shop = await context.queries.shopById(context, shopId);
+export default async function primaryShop(_, __, context) {
+  const shop = await context.queries.primaryShop(context);
   return shop;
 }
