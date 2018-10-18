8c503a060333443144e2931efc99532cbfd67fca
griggheo <grig@gheorghiu.net>
2018-10-18 14:57:08 -0700

    use a single propel.yaml file

--- .circleci/bin/ecs-deploy.sh
+++ .circleci/bin/ecs-deploy.sh
@@ -1,64 +1,48 @@
 #!/bin/bash
 
-sudo apt-get -y install python3-pip wget jq
+sudo apt-get -y install python3-pip wget
 sudo pip3 install awscli
-wget https://github.com/mikefarah/yq/releases/download/2.0.1/yq_linux_amd64 -O /tmp/yq
-sudo mv /tmp/yq /usr/local/bin/yq
-sudo chmod +x /usr/local/bin/yq
 
 if [ -z "${AWS_REGION}" ]; then
         AWS_REGION=us-west-2
 fi
 
 ENVIRONMENT=staging
-SERVICE_DIR_NAME=.reaction/devops/aws/services
-SERVICES=$(ls ${SERVICE_DIR_NAME})
+CLUSTER=core
+export storefront_CIRCLE_SHA1=$CIRCLE_SHA1
 
-for SERVICE in $SERVICES; do
-        DISABLED=$(echo $SERVICE | grep disabled)
-        if [ "${DISABLED}" == "${SERVICE}" ]; then
-            continue
-        fi
-	echo "START PROCESSING SERVICE ${SERVICE}"
+PROPEL_CONFIG_FILE="propel.yaml"
+if [ ! -f ${PROPEL_CONFIG_FILE} ]; then
+	echo "Propel configuration file not found!"
+	exit 1
+fi
 
-	cd ${SERVICE_DIR_NAME}/${SERVICE}
+ENV_NAME_UPPERCASE=$(echo $ENVIRONMENT | awk '{print toupper($0)}')
+AWS_ACCESS_KEY_ID_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_SECRET_ACCESS_KEY
 
-	PROPEL_CONFIG_FILE="propel-${ENVIRONMENT}.yaml"
-	if [ ! -f ${PROPEL_CONFIG_FILE} ]; then
-	    echo "Propel configuration file not found!"
-	    exit 1
-	fi
+if [ "${!AWS_ACCESS_KEY_ID_VAR_NAME}" ]; then
+	AWS_ACCESS_KEY_ID=${!AWS_ACCESS_KEY_ID_VAR_NAME}
+fi
 
-	ENV_NAME_UPPERCASE=$(echo $ENVIRONMENT | awk '{print toupper($0)}')
-	AWS_ACCESS_KEY_ID_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_ACCESS_KEY_ID
-	AWS_SECRET_ACCESS_KEY_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_SECRET_ACCESS_KEY
+if [ "${!AWS_SECRET_ACCESS_KEY_VAR_NAME}" ]; then
+	AWS_SECRET_ACCESS_KEY=${!AWS_SECRET_ACCESS_KEY_VAR_NAME}
+fi
 
-	if [ "${!AWS_ACCESS_KEY_ID_VAR_NAME}" ]; then
-		AWS_ACCESS_KEY_ID=${!AWS_ACCESS_KEY_ID_VAR_NAME}
-	fi
+mkdir -p ~/.aws
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
 
-	if [ "${!AWS_SECRET_ACCESS_KEY_VAR_NAME}" ]; then
-		AWS_SECRET_ACCESS_KEY=${!AWS_SECRET_ACCESS_KEY_VAR_NAME}
-	fi
+echo "[default]" > ~/.aws/config
+echo "region = ${AWS_REGION}" >> ~/.aws/config
 
-	mkdir -p ~/.aws
-	echo "[default]" > ~/.aws/credentials
-	echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
-	echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
+echo Running aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
+aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
 
-	echo "[default]" > ~/.aws/config
-	echo "region = ${AWS_REGION}" >> ~/.aws/config
+sudo mv propel /usr/local/bin/propel
+sudo chmod +x /usr/local/bin/propel
 
-	echo Running aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
-	aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
-
-	sudo mv propel /usr/local/bin/propel
-	sudo chmod +x /usr/local/bin/propel
-
-	RELEASE_DESCRIPTION="CircleCI build URL: ${CIRCLE_BUILD_URL}"
-        propel release create --deploy --descr "${RELEASE_DESCRIPTION}" -f ${PROPEL_CONFIG_FILE}
-	
-	echo "END PROCESSING SERVICE ${SERVICE}"
-	
-	cd -
-done
+RELEASE_DESCRIPTION="CircleCI build URL: ${CIRCLE_BUILD_URL}"
+echo storefront_CIRCLE_SHA1=$storefront_CIRCLE_SHA1
+propel release create --deploy --descr "${RELEASE_DESCRIPTION}" -f ${PROPEL_CONFIG_FILE} --env $ENVIRONMENT --cluster $CLUSTER
