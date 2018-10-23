100bdd1e5802d1198d298005a6efa9219f8d429b
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-10-22 22:10:52 -0700

    add white and blacklist filters

--- imports/plugins/core/shipping/server/no-meteor/util/filterShippingAttributes.js
+++ imports/plugins/core/shipping/server/no-meteor/util/filterShippingAttributes.js
@@ -9,6 +9,8 @@
 export default function filterShippingAttributes(rates, shippingAttributes) {
 
   const activeShippingRates = rates.reduce((validShippingRates, rate) => {
+    console.log("------------------------------ shippingAttributes", shippingAttributes);
+
     // If rate has no restriction. add it to validShippingRates
     if (!rate.method.restrictions) {
       validShippingRates.push(rate);
@@ -22,13 +24,29 @@ export default function filterShippingAttributes(rates, shippingAttributes) {
     const destinationRestriction = rate.method.restrictions.destination;
     if (destinationRestriction) {
       console.log("---------- There is a destination restriction on this item", destinationRestriction);
+      // Whitelist - We add it to the list if attributes match
+      if (destinationRestriction.type === "whitelist") {
+        console.log("---------- This is a whitelist match, meaning we add the shipping method if it matches");
+        if (destinationRestriction.regions.includes(shippingAttributes.address.region)) {
+          console.log("---------- IT WORKSSSSSSSSSSSSSSSSS");
+          validShippingRates.push(rate);
+        };
+      }
+      // Whitelist - We don't add it to the list if attributes match
+      if (destinationRestriction.type === "blacklist") {
+        console.log("---------- This is a blacklist match, meaning we add the shipping method if it does not match");
+        if (!destinationRestriction.regions.includes(shippingAttributes.address.region)) {
+          console.log("---------- IT WORKSSSSSSSSSSSSSSSSS for blacklists toooooooooooo");
+          validShippingRates.push(rate);
+        };
+      }
     }
 
-    // Hazard restrictions
-    const hazardRestriction = rate.method.restrictions.hazard;
-    if (hazardRestriction) {
-      console.log("---------- There is a hazard restriction on this item", hazardRestriction);
-    }
+    // // Hazard restrictions
+    // const hazardRestriction = rate.method.restrictions.hazard;
+    // if (hazardRestriction) {
+    //   console.log("---------- There is a hazard restriction on this item", hazardRestriction);
+    // }
 
     // Return validShippingRates reduced array
     return validShippingRates;
@@ -38,3 +56,40 @@ export default function filterShippingAttributes(rates, shippingAttributes) {
   console.log("-------------------- activeShippingRates --------------------", activeShippingRates); // TODO: Remove, for testing only
   return activeShippingRates;
 }
+
+
+
+
+
+
+// const activeMethods = [];
+  // const inactiveMethods = [];
+
+
+  // if (shippingAttributes.address.region === "AK") {
+  //   console.log("---------- WE ARE SHIPPING TO ALASKA");
+  //   activeMethods.push("Alaska");
+  // }
+  // if (shippingAttributes.address.region === "HI") {
+  //   console.log("---------- WE ARE SHIPPING TO HAWAII");
+  //   activeMethods.push("Hawaii");
+  // }
+  // if (shippingAttributes.address.region !== "AK" && shippingAttributes.address.region !== "HI") {
+  //   console.log("---------- WE ARE SHIPPING TO THE CONTIGUOUS UNITED STATES");
+  //   inactiveMethods.push("Alaska");
+  //   inactiveMethods.push("Hawaii");
+  // }
+
+  // const allMethods = rates.map((item) => item.method.name);
+
+  // // const newRates = rates;
+  // let newRates = rates.filter((item) => allMethods.includes(item.method.name));
+  // if (activeMethods && activeMethods.length) {
+  //   newRates = rates.filter((item) => activeMethods.includes(item.method.name));
+  // }
+
+  // if (inactiveMethods && !activeMethods.length) {
+  //   newRates = rates.filter((item) => !inactiveMethods.includes(item.method.name));
+  // }
+
+  // return newRates;
