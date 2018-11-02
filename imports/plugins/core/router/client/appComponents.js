f79c55b216602171df925ad6e577938f2aca384e
Eric Dobbertin <eric@dairystatedesigns.com>
2018-11-02 13:57:55 -0500

    feat: update to @reactioncommerce/components 0.54.0

--- imports/plugins/core/router/client/appComponents.js
+++ imports/plugins/core/router/client/appComponents.js
@@ -6,6 +6,7 @@ import iconPlus from "@reactioncommerce/components/svg/iconPlus";
 import iconLock from "@reactioncommerce/components/svg/iconLock";
 import spinner from "@reactioncommerce/components/svg/spinner";
 import Accordion from "@reactioncommerce/components/Accordion/v1";
+import AccordionFormList from "@reactioncommerce/components/AccordionFormList/v1";
 import AddressBook from "@reactioncommerce/components/AddressBook/v1";
 import AddressForm from "@reactioncommerce/components/AddressForm/v1";
 import AddressReview from "@reactioncommerce/components/AddressReview/v1";
@@ -16,11 +17,14 @@ import CatalogGridItem from "@reactioncommerce/components/CatalogGridItem/v1";
 import Checkbox from "@reactioncommerce/components/Checkbox/v1";
 import ErrorsBlock from "@reactioncommerce/components/ErrorsBlock/v1";
 import Field from "@reactioncommerce/components/Field/v1";
+import InlineAlert from "@reactioncommerce/components/InlineAlert/v1";
 import { Link } from "/imports/plugins/core/ui/client/components/link";
+import MultiSelect from "@reactioncommerce/components/MultiSelect/v1";
 import PhoneNumberInput from "@reactioncommerce/components/PhoneNumberInput/v1";
 import Price from "@reactioncommerce/components/Price/v1";
 import ProgressiveImage from "@reactioncommerce/components/ProgressiveImage/v1";
 import QuantityInput from "@reactioncommerce/components/QuantityInput/v1";
+import RegionInput from "@reactioncommerce/components/RegionInput/v1";
 import Select from "@reactioncommerce/components/Select/v1";
 import SelectableItem from "@reactioncommerce/components/SelectableItem/v1";
 import SelectableList from "@reactioncommerce/components/SelectableList/v1";
@@ -30,6 +34,7 @@ export default {
   Accordion,
   AddressBook,
   AddressForm,
+  AccordionFormList,
   AddressReview,
   BadgeOverlay,
   Button,
@@ -44,11 +49,14 @@ export default {
   iconPlus,
   iconValid,
   iconLock,
+  InlineAlert,
   Link,
+  MultiSelect,
   PhoneNumberInput,
   Price,
   ProgressiveImage,
   QuantityInput,
+  RegionInput,
   Select,
   spinner,
   SelectableItem,
