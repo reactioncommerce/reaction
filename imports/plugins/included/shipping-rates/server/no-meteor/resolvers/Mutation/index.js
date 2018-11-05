7c2a28c2d42f716efe3f5a127b282ab9c565cb7f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-11-05 15:37:49 -0800

    graphql: add imports for new files

--- imports/plugins/included/shipping-rates/server/no-meteor/resolvers/Mutation/index.js
+++ imports/plugins/included/shipping-rates/server/no-meteor/resolvers/Mutation/index.js
@@ -1,9 +1,11 @@
 import createFlatRateFulfillmentMethod from "./createFlatRateFulfillmentMethod";
 import deleteFlatRateFulfillmentMethod from "./deleteFlatRateFulfillmentMethod";
 import updateFlatRateFulfillmentMethod from "./updateFlatRateFulfillmentMethod";
+import deleteFlatRateFulfillmentRestriction from "./deleteFlatRateFulfillmentRestriction";
 
 export default {
   createFlatRateFulfillmentMethod,
   deleteFlatRateFulfillmentMethod,
-  updateFlatRateFulfillmentMethod
+  updateFlatRateFulfillmentMethod,
+  deleteFlatRateFulfillmentRestriction
 };
