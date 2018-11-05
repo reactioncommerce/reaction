ca1276d52efd40c75e181ef8a06f2b5ed6245f4e
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-11-05 15:32:19 -0800

    graphql: deleteFlatRateFulfillmentRestriction mutation

---
+++ imports/plugins/included/shipping-rates/server/no-meteor/mutations/deleteFlatRateFulfillmentRestriction.js
@@ -0,0 +1,34 @@
+import SimpleSchema from "simpl-schema";
+import ReactionError from "@reactioncommerce/reaction-error";
+
+const inputSchema = new SimpleSchema({
+  restrictionId: String,
+  shopId: String
+});
+
+/**
+ * @method deleteFlatRateFulfillmentRestriction
+ * @summary deletes a flat rate fulfillment restriction
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} input - Input (see SimpleSchema)
+ * @return {Promise<Object>} An object with a `restriction` property containing the deleted restriction
+ */
+export default async function deleteFlatRateFulfillmentRestriction(context, input) {
+  inputSchema.validate(input);
+
+  const { restrictionId, shopId } = input;
+  const { collections, userHasPermission } = context;
+  const { FlatRateFulfillmentRestrictions } = collections;
+
+  if (!userHasPermission(["admin", "owner", "shipping"], shopId)) {
+    throw new ReactionError("access-denied", "Access Denied");
+  }
+
+  const { ok, value } = await FlatRateFulfillmentRestrictions.findOneAndDelete({
+    _id: restrictionId,
+    shopId
+  });
+  if (ok !== 1) throw new ReactionError("not-found", "Not found");
+
+  return { restriction: value };
+}
