0be626901ee3b78d6260af1a7480959c3d93e322
Seun Martins <seundotme@gmail.com>
2018-11-07 11:59:59 +0100

    Fix identity provider login page not found due to missing permission

--- imports/plugins/core/hydra-oauth/server/index.js
+++ imports/plugins/core/hydra-oauth/server/index.js
@@ -1,8 +1,20 @@
 import { Meteor } from "meteor/meteor";
+import Hooks from "@reactioncommerce/hooks";
 import { oauthLogin } from "./oauthMethods";
+import Reaction from "/imports/plugins/core/core/server/Reaction";
 
 Meteor.methods({
   "oauth/login": oauthLogin
+});
+
+// Allow the login forms to be shown to all visitors.
+// Without this, the route for the login page will not be published because of permission checks
+Hooks.Events.add("afterCoreInit", () => {
+  Reaction.addRolesToGroups({
+    allShops: true,
+    groups: ["guest", "customer"],
+    roles: ["reaction-hydra-oauth"]
+  });
 });
 
 // Init endpoints
