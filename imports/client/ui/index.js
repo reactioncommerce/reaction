180ea3d6ffe0119b709b08174b769d1315e97c78
Will Lopez <will.lopez77@gmail.com>
2018-11-07 16:45:56 -0800

    docs: add jsdoc

--- imports/client/ui/index.js
+++ imports/client/ui/index.js
@@ -2,14 +2,26 @@ import { getReactComponentOrBlazeTemplate } from "/imports/plugins/core/componen
 
 export const operatorRoutes = [];
 
-export function registerOperatorRoute(routeInfo) {
-  const { mainComponent } = routeInfo;
+/**
+ * @name registerOperatorRoute
+ * @summary Registers new route in the operator UI.
+ * @param {Object} route - The route
+ * @param {String} route.path - The URL path for this route
+ * @param {Node|String} route.mainComponent - A react component to render in
+ * the main content area or the name of a Blaze template that has been registered
+ * by a package.
+ * @param {Node} route.sidebarIconComponent - The menu icon for this route
+ * @param {String} route.sidebarI18nLabel - The i18n key for this route, i.e. "admin.dashboard.ordersLabel"
+ * @returns {undefined}
+ */
+export function registerOperatorRoute(route) {
+  const { mainComponent } = route;
   let component = mainComponent;
 
   if (typeof mainComponent === "string") {
     component = () => getReactComponentOrBlazeTemplate(mainComponent);
   }
 
-  operatorRoutes.push({ ...routeInfo, mainComponent: component });
+  operatorRoutes.push({ ...route, mainComponent: component });
 }
 
