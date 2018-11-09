8128a61839e187cdaa24ea9e55ffedfcc40e10af
Will Lopez <will.lopez77@gmail.com>
2018-11-09 13:30:55 -0800

    refactor: render settigns menu items after main navigation menu items

--- imports/client/ui/components/Sidebar/Sidebar.js
+++ imports/client/ui/components/Sidebar/Sidebar.js
@@ -3,10 +3,12 @@ import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import styledMUI from "styled-components-mui";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
+import MUIDivider from "@material-ui/core/Divider";
 import MUIListItemText from "@material-ui/core/ListItemText";
 import MUIDrawer from "@material-ui/core/Drawer";
 import MUIIconButton from "@material-ui/core/IconButton";
@@ -26,6 +28,11 @@ const Drawer = styledMUI(MUIDrawer, { paper: "Paper" })`
     background-color: ${applyTheme("Sidebar.menuBarBackgroundColor")};
     width: ${applyTheme("Sidebar.drawerWidth")};
   }
+`;
+
+const Divider = styledMUI(MUIDivider)`
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 const ListItemText = styledMUI(MUIListItemText)`
@@ -81,13 +88,16 @@ export default class Sidebar extends Component {
     this.props.handleDrawerClose();
   };
 
-  renderMenuItems = () => {
+  renderNavigationMenuItems = (settings = false) => {
     const { routes } = this.props;
+    const filteredRoutes = settings ?
+      routes.filter(({ isNavigationLink, isSetting }) => isNavigationLink && isSetting) :
+      routes.filter(({ isNavigationLink, isSetting }) => isNavigationLink && !isSetting);
 
     return (
-      <List>
+      <Fragment>
         {
-          routes.filter(({ isNavigationLink }) => isNavigationLink).map((route) => (
+          filteredRoutes.map((route) => (
             <Link to={`/operator${route.path}`} activeClassName={activeClassName} key={route.path}>
               <ListItem button>
                 <ListItemIcon>
@@ -100,7 +110,7 @@ export default class Sidebar extends Component {
             </Link>
           ))
         }
-      </List>
+      </Fragment>
     );
   }
 
@@ -111,11 +121,15 @@ export default class Sidebar extends Component {
       <nav>
         <DrawerHeader>
           <IconButton onClick={this.handleDrawerClose}>
-            <ChevronLeftIcon />
+            <FontAwesomeIcon icon={faChevronLeft} />
           </IconButton>
         </DrawerHeader>
         <CompanyName variant="h5">Reaction</CompanyName>
-        {this.renderMenuItems()}
+        <List>
+          {this.renderNavigationMenuItems()}
+          <Divider component="li" />
+          {this.renderNavigationMenuItems(true)}
+        </List>
       </nav>
     );
 
