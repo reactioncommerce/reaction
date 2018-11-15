e75f2cbc118bd850487ae7032441a42422ee82be
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-11-15 15:45:13 -0800

    load appEvents on startup

--- imports/plugins/included/surcharges/server/no-meteor/startup.js
+++ imports/plugins/included/surcharges/server/no-meteor/startup.js
@@ -1,8 +1,52 @@
+import getSurcharges from "./getSurcharges";
+
+const EMITTED_BY_NAME = "SURCHARGES_PLUGIN";
+
 /**
  * @summary Called on startup
  * @param {Object} context Startup context
  * @returns {undefined}
  */
 export default function startup(context) {
-  context.collections.Surcharges = context.app.db.collection("Surcharges");
+  const { appEvents, collections } = context;
+  const { Cart, Orders } = collections;
+
+  // Adds Surcharges collection to context
+  collections.Surcharges = context.app.db.collection("Surcharges");
+
+  // Update the cart to include surcharges, if applicable
+  appEvents.on("afterCartUpdate", async (cart, { emittedBy } = {}) => {
+    if (emittedBy === EMITTED_BY_NAME) return; // short circuit infinite loops
+
+    const cartSurcharges = await getSurcharges(context, cart);
+
+    const { value: updatedCart } = await Cart.findOneAndUpdate({ _id: cart._id }, {
+      $set: {
+        surcharges: cartSurcharges
+      }
+    }, {
+      // Default behavior is to return the original. We want the updated.
+      returnOriginal: false
+    });
+
+    appEvents.emit("afterCartUpdate", updatedCart, { emittedBy: EMITTED_BY_NAME });
+  });
+
+  // Update the order to include surcharges, if applicable
+  appEvents.on("afterOrderCreate", async (order, { emittedBy } = {}) => {
+    if (emittedBy === EMITTED_BY_NAME) return;
+
+    const orderSurcharges = await getSurcharges(context, order);
+
+    const { value: updatedOrder } = await Orders.findOneAndUpdate({ _id: order._id }, {
+      $set: {
+        surcharges: orderSurcharges
+      }
+    }, {
+      // Default behavior is to return the original. We want the updated.
+      returnOriginal: false
+    });
+
+    appEvents.emit("afterOrderUpdate", updatedOrder, { emittedBy: EMITTED_BY_NAME });
+  });
 }
