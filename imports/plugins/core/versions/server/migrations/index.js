c4b7b08f4afbe224dd9939ad3a24be5d48e3ecae
Spencer Norman <spencern@gmail.com>
2018-11-26 14:46:57 -0700

    fix: fb appsecret vulnerability
    A security vulnerability was discovered which would permit an unauthenticated user to access the App Secret for the Facebook app associated with the Reaction Commerce social plugin.
    This fix includes a migration to remove existing app secret data from the database.
    
    Additionally you should do the following:
    **Invalidate Existing Secrets**
    If you found a Facebook App Secret listed in your operator panel, you should invalidate it immediately from the Facebook App settings page.
    
    **Generate New Secrets**
    If you used this App Secret in any other applications or for Facebook oAuth login, you should generate and use a new secrets to continue to provide services to your customers.
    
    You should not add a new App Secret to the social plugin dashboard. It is unnecessary for application operation. This version reomves the App Secret input field from the social plugin.
    
    If you have any questions about this advisory or about the patches, please contact us at: security@reactioncommerce.com

--- imports/plugins/core/versions/server/migrations/index.js
+++ imports/plugins/core/versions/server/migrations/index.js
@@ -24,3 +24,4 @@ import "./23_drop_tempstore_collections";
 import "./24_publish_all_existing_visible_products";
 import "./25_update_catalog_schema.js";
 import "./26_remove_revision_control";
+import "./26-1_remove_social_app_secret";
