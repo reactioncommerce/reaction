467de7c7863376cb137f20efa259fbd1a6de6e1a
Daniel Castellon <dan.a.castellon@gmail.com>
2018-11-27 15:24:50 -0500

    chore: fix eslint issues

--- imports/plugins/core/hydra-oauth/server/init.js
+++ imports/plugins/core/hydra-oauth/server/init.js
@@ -4,7 +4,6 @@ import Reaction from "/imports/plugins/core/core/server/Reaction";
 // Allow the login forms to be shown to all visitors.
 // Without this, the route for the login page will not be published because of permission checks
 Hooks.Events.add("afterCoreInit", () => {
-  console.log("Add account/login...");
   Reaction.addRolesToGroups({
     allShops: true,
     groups: ["guest", "customer"],
--- imports/plugins/core/versions/server/migrations/5_update_defaultRoles_to_groups.js
+++ imports/plugins/core/versions/server/migrations/5_update_defaultRoles_to_groups.js
@@ -1,10 +1,4 @@
-import _ from "lodash";
-import Logger from "@reactioncommerce/logger";
-import { Meteor } from "meteor/meteor";
-import { Roles } from "meteor/alanning:roles";
 import { Migrations } from "meteor/percolate:migrations";
-import Reaction from "/imports/plugins/core/core/server/Reaction";
-import { Accounts, Groups, Shops } from "/lib/collections";
 
 Migrations.add({
   version: 5,
