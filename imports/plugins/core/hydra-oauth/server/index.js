c8f77bad1a9cf51ba3122ceef3439a6b2ca2ccac
Daniel Castellon <dan.a.castellon@gmail.com>
2018-11-27 14:28:35 -0500

    fix: deprecate Migration 5 in favor of createGroups() + add account/login permission to fix login 404

--- imports/plugins/core/hydra-oauth/server/index.js
+++ imports/plugins/core/hydra-oauth/server/index.js
@@ -1,20 +1,9 @@
 import { Meteor } from "meteor/meteor";
-import Hooks from "@reactioncommerce/hooks";
 import { oauthLogin } from "./oauthMethods";
-import Reaction from "/imports/plugins/core/core/server/Reaction";
+import "./init";
 
 Meteor.methods({
   "oauth/login": oauthLogin
-});
-
-// Allow the login forms to be shown to all visitors.
-// Without this, the route for the login page will not be published because of permission checks
-Hooks.Events.add("afterCoreInit", () => {
-  Reaction.addRolesToGroups({
-    allShops: true,
-    groups: ["guest", "customer"],
-    roles: ["reaction-hydra-oauth"]
-  });
 });
 
 // Init endpoints
---
+++ imports/plugins/core/hydra-oauth/server/init.js
@@ -0,0 +1,13 @@
+import Hooks from "@reactioncommerce/hooks";
+import Reaction from "/imports/plugins/core/core/server/Reaction";
+
+// Allow the login forms to be shown to all visitors.
+// Without this, the route for the login page will not be published because of permission checks
+Hooks.Events.add("afterCoreInit", () => {
+  console.log("Add account/login...");
+  Reaction.addRolesToGroups({
+    allShops: true,
+    groups: ["guest", "customer"],
+    roles: ["account/login"]
+  });
+});
