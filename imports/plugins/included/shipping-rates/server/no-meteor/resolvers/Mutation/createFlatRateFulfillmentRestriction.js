ba7f7ec803c69bf5483813ce4b8ce6e5ba475e89
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-11-29 11:25:00 -0800

    update docs

--- imports/plugins/included/shipping-rates/server/no-meteor/resolvers/Mutation/createFlatRateFulfillmentRestriction.js
+++ imports/plugins/included/shipping-rates/server/no-meteor/resolvers/Mutation/createFlatRateFulfillmentRestriction.js
@@ -2,13 +2,13 @@ import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/sh
 import createFlatRateFulfillmentRestrictionMutation from "../../mutations/createFlatRateFulfillmentRestriction";
 
 /**
- * @name "Mutation.createFlatRateFulfillmentMethod"
+ * @name "Mutation/createFlatRateFulfillmentMethod"
  * @method
  * @memberof Fulfillment/GraphQL
  * @summary resolver for the createFlatRateFulfillmentMethod GraphQL mutation
  * @param {Object} parentResult - unused
  * @param {Object} args.input - an object of all mutation arguments that were sent by the client
- * @param {Object} args.input.method - The method object
+ * @param {Object} args.input.restriction - The restriction object
  * @param {String} args.input.shopId - The shop to create this flat rate fulfillment method for
  * @param {String} [args.input.clientMutationId] - An optional string identifying the mutation call
  * @param {Object} context - an object containing the per-request state
