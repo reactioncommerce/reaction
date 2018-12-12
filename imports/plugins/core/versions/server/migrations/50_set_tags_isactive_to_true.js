3c79a53b59094a7439855ba6ff3230b6e7de2b80
Machiko Yasuda <machikoyasuda@gmail.com>
2018-12-12 15:11:51 -0800

    chore(migration): add migration to set isActive to true for all legacy Tags

---
+++ imports/plugins/core/versions/server/migrations/50_set_tags_isactive_to_true.js
@@ -0,0 +1,30 @@
+import { Migrations } from "meteor/percolate:migrations";
+import { Tags } from "/lib/collections";
+
+/**
+ * @file
+ * Sets isActive on all existing Tags to true
+ */
+
+Migrations.add({
+  version: 50,
+  up() {
+    Tags.rawCollection().update({}, {
+      $set: {
+        isActive: true
+      }
+    }, {
+      multi: true 
+    });
+  },
+  down() {
+    Tags.rawCollection().update({}, {
+      $set: {
+        isActive: ""
+      }
+    }, {
+      multi: true 
+    });
+  }
+});
+
