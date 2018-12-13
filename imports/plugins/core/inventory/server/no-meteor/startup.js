eb2ef3082ca90e06d63478eced389f84fe9f503f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-12-12 23:49:58 -0800

    fix typo: varaint -> variant

--- imports/plugins/core/inventory/server/no-meteor/startup.js
+++ imports/plugins/core/inventory/server/no-meteor/startup.js
@@ -2,8 +2,9 @@
 import rawCollections from "/imports/collections/rawCollections";
 import updateCatalogProductInventoryStatus from "/imports/plugins/core/catalog/server/no-meteor/utils/updateCatalogProductInventoryStatus";
 import appEvents from "/imports/node-app/core/util/appEvents";
-import getTopLevelVariant from "/imports/plugins/core/catalog/server/no-meteor/utils/getTopLevelVariant.js";
-import getVariantInventoryAvailableToSellQuantity from "/imports/plugins/core/catalog/server/no-meteor/utils/getVariantInventoryAvailableToSellQuantity.js";
+import getTopLevelVariant from "/imports/plugins/core/catalog/server/no-meteor/utils/getTopLevelVariant";
+import getProductInventoryAvailableToSellQuantity from "/imports/plugins/core/catalog/server/no-meteor/utils/getProductInventoryAvailableToSellQuantity";
+import getVariantInventoryAvailableToSellQuantity from "/imports/plugins/core/catalog/server/no-meteor/utils/getVariantInventoryAvailableToSellQuantity";
 
 /**
 
@@ -19,7 +20,7 @@ export default function startup(context) {
     const orderItems = order.shipping.reduce((list, group) => [...list, ...group.items], []);
 
     orderItems.forEach(async (item) => {
-      // Update supplied items inventory
+      // Update supplied item inventory
       await collections.Products.updateOne(
         {
           _id: item.variantId
@@ -37,16 +38,16 @@ export default function startup(context) {
         }
       );
 
-      // Check to see if this is the top level variant, or an option
-      const topLevelVaraint = await getTopLevelVariant(item.variantId, collections);
+      // Check to see if this item is the top level variant, or an option
+      const topLevelVariant = await getTopLevelVariant(item.variantId, collections);
 
-      // If it was an option, update the quantity on it's parent too
-      if (topLevelVaraint._id !== item.varaintId) {
-        const variantInventoryAvailableToSellQuantity = await getVariantInventoryAvailableToSellQuantity(topLevelVaraint, context.collections);
+      // If item is an option, update the quantity on its parent variant too
+      if (topLevelVariant._id !== item.variantId) {
+        const variantInventoryAvailableToSellQuantity = await getVariantInventoryAvailableToSellQuantity(topLevelVariant, context.collections);
 
         await collections.Products.updateOne(
           {
-            _id: topLevelVaraint._id
+            _id: topLevelVariant._id
           },
           {
             $set: {
@@ -61,6 +62,25 @@ export default function startup(context) {
           }
         );
       }
+
+      // Update the top level product to be the sum of all variant inventory numbers
+      const productInventoryAvailableToSellQuantity = await getProductInventoryAvailableToSellQuantity(item.productId, context.collections);
+      await collections.Products.updateOne(
+        {
+          _id: item.productId
+        },
+        {
+          $set: {
+            inventoryAvailableToSell: productInventoryAvailableToSellQuantity
+          }
+        },
+        {
+          publish: true,
+          selector: {
+            type: "variant"
+          }
+        }
+      );
 
       // Publish inventory updates to the Catalog
       Promise.await(updateCatalogProductInventoryStatus(item.productId, rawCollections));
