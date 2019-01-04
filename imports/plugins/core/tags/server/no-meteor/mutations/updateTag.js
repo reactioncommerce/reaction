0b5e694cfd2184242ef4470c1b5855adf7415ccc
Mike Murray <hellomikemurray@gmail.com>
2019-01-04 10:44:18 -0800

    feat: add updateTag mutation

---
+++ imports/plugins/core/tags/server/no-meteor/mutations/updateTag.js
@@ -0,0 +1,56 @@
+import ReactionError from "@reactioncommerce/reaction-error";
+import SimpleSchema from "simpl-schema";
+
+const inputSchema = new SimpleSchema({
+  name: String,
+  displayTitle: String,
+  isVisible: Boolean
+}, { requiredByDefault: false });
+
+/**
+ * @name Mutation.updateTag
+ * @method
+ * @memberof Routes/GraphQL
+ * @summary Add a tag
+ * @param {Object} context -  an object containing the per-request state
+ * @param {Object} input - mutation input
+ * @return {Promise<Object>} UpdateTagPayload
+ */
+export default async function updateTag(context, input) {
+  const { collections, userHasPermission } = context;
+  const { Tags } = collections;
+  const { clientMutationId, shopId, tagId } = input;
+
+  // Check for owner or admin permissions from the user before allowing the mutation
+  if (!userHasPermission(["owner", "admin"], shopId)) {
+    throw new ReactionError("access-denied", "User does not have permission");
+  }
+
+  const params = {
+    from: input.from,
+    to: input.to,
+    enabled: input.enabled,
+    type: input.type,
+    status: input.status
+  };
+
+  if (params.type === "rewrite") params.status = null;
+  inputSchema.validate(params);
+  params.updatedAt = new Date();
+
+  const { result } = await Tags.updateOne(
+    { _id: tagId, shopId },
+    { $set: params }
+  );
+
+  if (result.n === 0) {
+    throw new ReactionError("not-found", "Redirect rule not found");
+  }
+
+  const tag = await Tags.findOne({ _id: tagId, shopId });
+
+  return {
+    clientMutationId,
+    tag
+  };
+}
