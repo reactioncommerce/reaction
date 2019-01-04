1b5514c93c3ccff934a592ac69402c393d7d989d
Mike Murray <hellomikemurray@gmail.com>
2019-01-04 10:47:05 -0800

    feat: add mutation resolver index

---
+++ imports/plugins/core/tags/server/no-meteor/resolvers/Mutation/index.js
@@ -0,0 +1,9 @@
+import addRedirectRule from "./addRedirectRule";
+import removeRedirectRule from "./removeRedirectRule";
+import updateRedirectRule from "./updateRedirectRule";
+
+export default {
+  addRedirectRule,
+  removeRedirectRule,
+  updateRedirectRule
+};
