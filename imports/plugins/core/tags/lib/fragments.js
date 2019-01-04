079e286bc7bb999f9a29eb0eea9c11b7b0f397f4
Mike Murray <hellomikemurray@gmail.com>
2019-01-04 13:54:41 -0800

    feat: add fragments file with common tag fields

---
+++ imports/plugins/core/tags/lib/fragments.js
@@ -0,0 +1,16 @@
+export const Tag = `
+  _id
+  displayTitle
+  position
+  name
+  slug
+  isVisible
+  metafields {
+    description
+    key
+    namespace
+    scope
+    value
+    valueType
+  }
+`;
