d2ad3127534996b291eb1418972fbdd14b636235
Daniel Castellon <dan.a.castellon@gmail.com>
2019-01-07 13:30:22 -0500

    fix: make Accounts.createdAt optional in schema

--- imports/collections/schemas/accounts.js
+++ imports/collections/schemas/accounts.js
@@ -205,7 +205,8 @@ export const Accounts = new SimpleSchema({
   },
   "createdAt": {
     type: Date,
-    autoValue: createdAtAutoValue
+    autoValue: createdAtAutoValue,
+    optional: true
   },
   "updatedAt": {
     type: Date,
