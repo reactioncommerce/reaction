3cc07ec42cf85932510884c576d82b222436fa8d
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-10 11:20:41 -0600

    chore: fix lint

--- imports/plugins/included/payments-example/client/settings/components/exampleSettingsForm.js
+++ imports/plugins/included/payments-example/client/settings/components/exampleSettingsForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import _ from "lodash";
 import { TextField, Translation } from "/imports/plugins/core/ui/client/components";
 
 class ExampleSettingsForm extends Component {
