99356b059a7371582078b7957fd70b2fea33a6c1
Mike Murray <hellomikemurray@gmail.com>
2019-01-10 13:27:11 -0800

    feat: add products by tag query resolver

---
+++ imports/plugins/core/tags/server/no-meteor/resolvers/Query/productsByTagId.js
@@ -0,0 +1,33 @@
+import { getPaginatedResponse } from "@reactioncommerce/reaction-graphql-utils";
+import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/shop";
+import { decodeTagOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/tag";
+
+/**
+ * @name Query.productsByTagId
+ * @method
+ * @memberof Tags/GraphQL
+ * @summary get a list of products by tag id
+ * @param {Object} _ - unused
+ * @param {Object} [params] - an object of all arguments that were sent by the client
+ * @param {String} [params.shopId] - Shop id
+ * @param {String} [params.tagId] - Tag id
+ * @param {Object} context - an object containing the per-request state
+ * @return {Promise<Array<Object>>} TagProducts Connection
+ */
+export default async function productsByTagId(_, params, context) {
+  const shopId = decodeShopOpaqueId(params.shopId);
+  const tagId = decodeTagOpaqueId(params.tagId);
+
+  const query = await context.queries.productsByTagId(context, {
+    shopId,
+    tagId
+  });
+
+  const connectionArgs = {
+    after: params.after,
+    before: params.before,
+    first: params.first,
+    last: params.last
+  };
+  return getPaginatedResponse(query, connectionArgs);
+}
