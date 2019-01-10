1db2192ecfd997cacd926ab21fc36256b1d60e1f
Mike Murray <hellomikemurray@gmail.com>
2019-01-10 09:52:48 -0800

    refactor: remove unused Fragment

--- imports/client/ui/layouts/Dashboard.js
+++ imports/client/ui/layouts/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled, { css, injectGlobal } from "styled-components";
 import styledMUI from "styled-components-mui";
