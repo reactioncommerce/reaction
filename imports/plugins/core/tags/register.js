d3449a3bde899c82c6e020ac21f2c0f3fadb9a8c
Mike Murray <hellomikemurray@gmail.com>
2019-01-10 13:27:34 -0800

    feat: register queries

--- imports/plugins/core/tags/register.js
+++ imports/plugins/core/tags/register.js
@@ -1,8 +1,8 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
 import mutations from "./server/no-meteor/mutations";
+import queries from "./server/no-meteor/queries";
 import resolvers from "./server/no-meteor/resolvers";
 import schemas from "./server/no-meteor/schemas";
-// import startup from "./server/no-meteor/startup";
 
 Reaction.registerPackage({
   label: "Tags",
@@ -14,9 +14,7 @@ Reaction.registerPackage({
     schemas
   },
   mutations,
-  // functionsByType: {
-  //   startup: [startup]
-  // },
+  queries,
   registry: [{
     label: "Tags",
     description: "Tag Management",
