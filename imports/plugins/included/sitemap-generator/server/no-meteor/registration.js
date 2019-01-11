a7c3e1faae612d1ea2d1d10f6ddfe6a414bb9c31
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-11 13:58:18 -0600

    refactor: replace "onGenerateSitemap" hook
    with "getPageSitemapItems" functions

---
+++ imports/plugins/included/sitemap-generator/server/no-meteor/registration.js
@@ -0,0 +1,8 @@
+/**
+ * @summary Will be called for every plugin
+ * @param {Object} options The options object that the plugin passed to registerPackage
+ * @returns {undefined}
+ */
+export function registerPluginHandler({  }) {
+
+}
