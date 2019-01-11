6b7f27c8116ccd4acab086f8ca52c060a47113fe
Mike Murray <hellomikemurray@gmail.com>
2019-01-10 20:37:25 -0800

    feat: create the start of a mongo aggregation

--- imports/plugins/core/tags/server/no-meteor/queries/productsByTagId.js
+++ imports/plugins/core/tags/server/no-meteor/queries/productsByTagId.js
@@ -14,15 +14,48 @@ import ReactionError from "@reactioncommerce/reaction-error";
 export default async function productsByTagId(context, params) {
   const { shopId, tagId } = params;
   const { collections, userHasPermission } = context;
-  const { Products } = collections;
+  const { Products, Tags } = collections;
 
   // Check for owner or admin permissions from the user before allowing the query
   if (!userHasPermission(["owner", "admin"], shopId)) {
     throw new ReactionError("access-denied", "User does not have permission");
   }
 
-  return Products.find({
-    shopId,
-    hashtags: { $in: [tagId] }
+  const tag = await Tags.findOne({
+    _id: tagId
   });
+
+  // Products from catalog sample data
+  const positions = tag.featuredProductIds;
+
+  // Aggregation Pipeline
+  // Find the products in the "order" array
+  const match = {
+    $match: {
+      shopId,
+      hashtags: { $in: [tagId] }
+    }
+  };
+
+  // Add a new field "__order" to each product with the order they are in the array
+  const addFields = {
+    $addFields: {
+      position: {
+        $indexOfArray: [positions, "$_id"]
+      }
+    }
+  };
+
+  // Sort the results by "__order"
+  const sort = {
+    $sort: {
+      position: 1
+    }
+  };
+
+  // Profit
+  return {
+    collection: Products,
+    pipeline: [match, addFields, sort]
+  };
 }
