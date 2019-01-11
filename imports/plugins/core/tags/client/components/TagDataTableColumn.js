17eb93b9d05077a457a3b2ae2f936251bfca7d28
Mike Murray <hellomikemurray@gmail.com>
2019-01-11 09:58:24 -0800

    feat: add custom status column

--- imports/plugins/core/tags/client/components/TagDataTableColumn.js
+++ imports/plugins/core/tags/client/components/TagDataTableColumn.js
@@ -3,21 +3,9 @@ import PropTypes from "prop-types";
 import { i18next } from "/client/api";
 import { Components, registerComponent, withMoment } from "@reactioncommerce/reaction-components";
 import styled from "styled-components";
-import Tooltip from "@material-ui/core/Tooltip";
 
 const EnabledLabel = styled.span`
   padding-left: 8px;
-`;
-
-const TooltipLabel = styled.div`
-  word-break: break-all;
-  font-size: 12px;
-  padding: 3px;
-  line-height: 18px
-`;
-
-const StatusLabel = styled.span`
-  text-transform: capitalize
 `;
 
 class TagDataTableColumn extends Component {
@@ -32,11 +20,11 @@ class TagDataTableColumn extends Component {
 
     const renderColumn = row.column.id;
 
-    if (renderColumn === "enabled") {
+    if (renderColumn === "isVisible") {
       return (
         <span>
           <Components.Icon icon="fa fa-circle" className={row.value ? "valid" : "error"} />
-          <EnabledLabel>{i18next.t(row.value ? "admin.routing.enabled" : "admin.routing.disabled")}</EnabledLabel>
+          <EnabledLabel>{i18next.t(row.value ? "admin.tags.visible" : "admin.tags.hidden")}</EnabledLabel>
         </span>
       );
     } else if (renderColumn === "edit") {
@@ -44,23 +32,6 @@ class TagDataTableColumn extends Component {
         <span>
           <Components.Icon icon="fa fa-pencil" />
         </span>
-      );
-    } else if (renderColumn === "status") {
-      const status = row.original.type === "rewrite" ? row.original.type : row.value;
-      return <StatusLabel>{status}</StatusLabel>;
-    } else if (renderColumn === "updated") {
-      const { moment } = this.props;
-      const createdDate = (moment && moment(row.value).format("LLL")) || row.value.toLocaleString();
-      return (
-        <span>{createdDate}</span>
-      );
-    }
-
-    if (renderColumn === "from" || renderColumn === "to") {
-      return (
-        <Tooltip title={<TooltipLabel>{row.value}</TooltipLabel>}>
-          <span>{row.value}</span>
-        </Tooltip>
       );
     }
 
