84efd7cfd4afc039b733254bc846a36bd1d1f1d4
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-11 14:02:33 -0600

    chore: remove never-used file

--- imports/plugins/included/sitemap-generator/server/no-meteor/registration.js
+++
@@ -1,8 +0,0 @@
-/**
- * @summary Will be called for every plugin
- * @param {Object} options The options object that the plugin passed to registerPackage
- * @returns {undefined}
- */
-export function registerPluginHandler({  }) {
-
-}
