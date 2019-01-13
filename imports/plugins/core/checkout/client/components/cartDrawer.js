544f66bc2277884ca5b978a84ab2beafde345715
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-12 20:16:47 -0600

    feat: remove checkout button from cart drawer
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/checkout/client/components/cartDrawer.js
+++ imports/plugins/core/checkout/client/components/cartDrawer.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
 
-const CartDrawer = ({ productItems, pdpPath, handleRemoveItem, handleCheckout, handleImage, handleShowProduct }) => (
+const CartDrawer = ({ productItems, pdpPath, handleRemoveItem, handleImage, handleShowProduct }) => (
   <div>
     <div className="cart-drawer-swiper-container">
       <div className="cart-drawer-swiper-wrapper">
@@ -23,25 +23,10 @@ const CartDrawer = ({ productItems, pdpPath, handleRemoveItem, handleCheckout, h
       </div>
     </div>
     <div className="cart-drawer-pagination" />
-    <div className="row">
-      <Components.Button
-        bezelStyle="solid"
-        className={{
-          "btn-lg": true,
-          "btn-block": true
-        }}
-        status="cta"
-        id="btn-checkout"
-        label="Checkout now"
-        i18nKeyLabel="cartDrawer.checkout"
-        onClick={handleCheckout}
-      />
-    </div>
   </div>
 );
 
 CartDrawer.propTypes = {
-  handleCheckout: PropTypes.func,
   handleImage: PropTypes.func,
   handleRemoveItem: PropTypes.func,
   handleShowProduct: PropTypes.func,
