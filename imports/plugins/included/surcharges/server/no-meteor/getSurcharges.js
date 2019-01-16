0ffecfbe40ca5f52b12ce5b27b5a669c85f40dde
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2019-01-16 11:03:17 -0800

    remove comments and old code

--- imports/plugins/included/surcharges/server/no-meteor/getSurcharges.js
+++ imports/plugins/included/surcharges/server/no-meteor/getSurcharges.js
@@ -5,20 +5,19 @@ import { surchargeCheck } from "./util/surchargeCheck";
 
 
 /**
- * @summary Returns a list of surcharges based on the cart.
+ * @summary Returns a list of surcharges to apply based on the cart.
  * @param {Object} context - Context
  * @param {Object} cart - the user's cart
  * @return {Array} - an array that surcharges to apply to cart / order
  * @private
  */
-export default async function getSurcharges(context, cart) {
+export default async function getSurcharges(context, { cart }) {
   const { Surcharges } = context.collections;
   const surcharges = await Surcharges.find({}).toArray();
 
   // Create an extended common order to check surcharges against
-  const commonOrder = await xformCartGroupToCommonOrder(cart, cart.shipping[0], context);
+  const commonOrder = await xformCartGroupToCommonOrder(cart, cart.shipping[0], context); // TODO: EK - pass correct shipping object
   const extendedCommonOrder = await extendCommonOrder(context, commonOrder);
-
 
   const allAppliedSurcharges = await surcharges.reduce(async (appliedSurcharges, surcharge) => {
     const awaitedAppliedSurcharges = await appliedSurcharges;
@@ -41,7 +40,6 @@ export default async function getSurcharges(context, cart) {
       surchargeId: surcharge._id,
       amount: surcharge.amount,
       message: surcharge.message,
-      reason: surcharge.reason,
       cartId: cart._id
     };
   });
--- imports/plugins/included/surcharges/server/no-meteor/startup.js
+++ imports/plugins/included/surcharges/server/no-meteor/startup.js
@@ -9,7 +9,7 @@ const EMITTED_BY_NAME = "SURCHARGES_PLUGIN";
  */
 export default function startup(context) {
   const { appEvents, collections } = context;
-  const { Cart, Orders } = collections;
+  const { Cart } = collections;
 
   // Adds Surcharges collection to context
   collections.Surcharges = context.app.db.collection("Surcharges");
@@ -18,7 +18,7 @@ export default function startup(context) {
   appEvents.on("afterCartUpdate", async (cart, { emittedBy } = {}) => {
     if (emittedBy === EMITTED_BY_NAME) return; // short circuit infinite loops
 
-    const cartSurcharges = await getSurcharges(context, cart);
+    const cartSurcharges = await getSurcharges(context, { cart });
 
     const { value: updatedCart } = await Cart.findOneAndUpdate({ _id: cart._id }, {
       $set: {
@@ -30,23 +30,5 @@ export default function startup(context) {
     });
 
     appEvents.emit("afterCartUpdate", updatedCart, { emittedBy: EMITTED_BY_NAME });
-  });
-
-  // Update the order to include surcharges, if applicable
-  appEvents.on("afterOrderCreate", async (order, { emittedBy } = {}) => {
-    if (emittedBy === EMITTED_BY_NAME) return;
-
-    const orderSurcharges = await getSurcharges(context, order);
-
-    const { value: updatedOrder } = await Orders.findOneAndUpdate({ _id: order._id }, {
-      $set: {
-        surcharges: orderSurcharges
-      }
-    }, {
-      // Default behavior is to return the original. We want the updated.
-      returnOriginal: false
-    });
-
-    appEvents.emit("afterOrderUpdate", updatedOrder, { emittedBy: EMITTED_BY_NAME });
   });
 }
