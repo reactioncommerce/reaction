4cb1349090ff9d9fb7e37f87f172cb4e1b373908
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2019-01-15 16:51:15 -0800

    move common order hydration into core package

---
+++ imports/plugins/included/surcharges/server/no-meteor/util/helpers.js
@@ -0,0 +1,21 @@
+/**
+ * @constant
+ * @type {Object}
+*/
+export const operators = {
+  eq(varA, varB) { return varA === varB; },
+  gt(varA, varB) { return varA > varB; },
+  lt(varA, varB) { return varA < varB; },
+  ne(varA, varB) { return varA !== varB; }
+};
+
+/**
+ * @constant
+ * @type {Object}
+*/
+export const propertyTypes = {
+  bool(varA) { return varA.trim().toLowerCase() === "true"; },
+  float(varA) { return parseFloat(varA); },
+  int(varA) { return parseInt(varA, 10); },
+  string(varA) { return varA; }
+};
