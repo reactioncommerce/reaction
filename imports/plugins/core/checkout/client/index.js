3e215b84a61385c95cd02377007499ade1f4b3b3
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-17 14:33:06 -0600

    feat: remove cart drawer components
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/checkout/client/index.js
+++ imports/plugins/core/checkout/client/index.js
@@ -1,13 +1,1 @@
 import "./helpers/cart";
-
-import "./templates/cartDrawer/cartDrawer.html";
-import "./templates/cartDrawer/cartDrawer.js";
-
-export { default as CartDrawer } from "./components/cartDrawer";
-export { default as CartItems } from "./components/cartItems";
-export { default as CartPanel } from "./components/cartPanel";
-export { default as CartSubTotal } from "./components/cartSubTotal";
-export { default as EmptyCartDrawer } from "./components/emptyCartDrawer";
-
-export { default as CartDrawerContainer } from "./containers/cartDrawerContainer";
-export { default as CartSubTotalContainer } from "./containers/cartSubTotalContainer";
