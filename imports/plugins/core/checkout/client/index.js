773d784caa019ee0873870a1b88b923c0202bf4d
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-17 14:26:13 -0600

    feat: remove cart icon from nav bar
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/checkout/client/index.js
+++ imports/plugins/core/checkout/client/index.js
@@ -3,16 +3,11 @@ import "./helpers/cart";
 import "./templates/cartDrawer/cartDrawer.html";
 import "./templates/cartDrawer/cartDrawer.js";
 
-import "./templates/cartIcon/cartIcon.html";
-import "./templates/cartIcon/cartIcon.js";
-
 export { default as CartDrawer } from "./components/cartDrawer";
-export { default as CartIcon } from "./components/cartIcon";
 export { default as CartItems } from "./components/cartItems";
 export { default as CartPanel } from "./components/cartPanel";
 export { default as CartSubTotal } from "./components/cartSubTotal";
 export { default as EmptyCartDrawer } from "./components/emptyCartDrawer";
 
 export { default as CartDrawerContainer } from "./containers/cartDrawerContainer";
-export { default as CartIconContainer } from "./containers/cartIconContainer";
 export { default as CartSubTotalContainer } from "./containers/cartSubTotalContainer";
