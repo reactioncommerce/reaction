8bbf75e43d5432672fe5e668f0db2e19e9640747
Mike Murray <hellomikemurray@gmail.com>
2019-01-16 20:20:44 -0800

    feat: add setTagHeroMedia mutation
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/tags/server/no-meteor/mutations/index.js
+++ imports/plugins/core/tags/server/no-meteor/mutations/index.js
@@ -1,9 +1,11 @@
 import addTag from "./addTag";
+import setTagHeroMedia from "./setTagHeroMedia";
 import removeTag from "./removeTag";
 import updateTag from "./updateTag";
 
 export default {
   addTag,
+  setTagHeroMedia,
   removeTag,
   updateTag
 };
---
+++ imports/plugins/core/tags/server/no-meteor/mutations/setTagHeroMedia.js
@@ -0,0 +1,57 @@
+import appEvents from "/imports/node-app/core/util/appEvents";
+import ReactionError from "@reactioncommerce/reaction-error";
+import { Media } from "/imports/plugins/core/files/server";
+import { FileRecord } from "@reactioncommerce/file-collections";
+import { MediaRecords } from "/lib/collections";
+
+/**
+ * @name tag/setTagHeroMedia
+ * @method
+ * @memberof Tag/Methods
+ * @summary Insert a new hero media record and attach it to a tag.
+ * @param {Object} context -  an object containing the per-request state
+ * @param {Object} input - mutation input
+ * @return {Promise<Object>} SetTagHeroMediaPayload
+ */
+export default async function setTagHeroMedia(context, input) {
+  const { collections, userHasPermission } = context;
+  const { Tags } = collections;
+  const { shopId, tagId, fileRecord } = input;
+
+  // Check for owner or admin permissions from the user before allowing the mutation
+  if (!userHasPermission(["owner", "admin"], shopId)) {
+    throw new ReactionError("access-denied", "User does not have permission");
+  }
+
+  const doc = {
+    ...fileRecord,
+    metadata: {
+      ...fileRecord.metadata,
+      workflow: "published"
+    }
+  };
+
+  const mediaRecordId = await MediaRecords.insert(doc);
+
+  // Because we don't have access to the URL of the file, we have to take
+  // do our best to get he URL as it will be once the file is finished being processed.
+  const heroMediaUrl = `${FileRecord.downloadEndpointPrefix}/${Media.name}/${mediaRecordId}/large/${fileRecord.original.name}`;
+
+  const { result } = await Tags.updateOne({
+    _id: tagId
+  }, {
+    $set: {
+      heroMediaUrl
+    }
+  });
+
+  if (result.n === 0) {
+    throw new ReactionError("not-found", "Redirect rule not found");
+  }
+
+  const tag = await Tags.findOne({ _id: tagId, shopId });
+
+  appEvents.emit("afterSetTagHeroMedia", doc);
+
+  return tag;
+}
