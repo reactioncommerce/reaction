de26908c248c53647e7d9b38f33676f7e5f36f57
Erik Kieckhafer <ek@ato.la>
2019-01-18 10:27:07 -0800

    refactor: move language arguement to top level of surcharge query
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/surcharges/server/no-meteor/queries/getSurcharge.js
+++ imports/plugins/included/surcharges/server/no-meteor/queries/getSurcharge.js
@@ -5,16 +5,24 @@
  * @summary Query the Surcharges collection for a surcharge with the provided shopId and surchargeId
  * @param {Object} context - an object containing the per-request state
  * @param {Object} params - request parameters
+ * @param {String} params.language - Language to retrieve surcharge message in
  * @param {String} params.shopId - Shop ID for the shop that owns the surcharge
  * @param {String} params.surchargeId - Surcharge ID of the surcharge we are requesting
  * @return {Promise<Object>|undefined} - A surcharge document, if one is found
  */
-export default async function getSurcharge(context, { surchargeId, shopId } = {}) {
+export default async function getSurcharge(context, { language, surchargeId, shopId } = {}) {
   const { collections } = context;
   const { Surcharges } = collections;
 
-  return Surcharges.findOne({
+  const surcharge = await Surcharges.findOne({
     _id: surchargeId,
     shopId
   });
+
+  if (surcharge) {
+    // Add language from args so that we can use it to get translated message
+    surcharge.language = language;
+  }
+
+  return surcharge;
 }
--- imports/plugins/included/surcharges/server/no-meteor/resolvers/Query/getSurcharge.js
+++ imports/plugins/included/surcharges/server/no-meteor/resolvers/Query/getSurcharge.js
@@ -8,15 +8,17 @@ import { decodeSurchargeOpaqueId } from "../../xforms/surcharge";
  * @summary resolver for the getSurcharge GraphQL mutation
  * @param {Object} parentResult - unused
  * @param {Object} args - an object of all arguments that were sent by the client
+ * @param {String} args.language - Language to retrieve surcharge message in
+ * @param {String} args.surchargeId - Surcharge ID of the surcharge we are requesting
  * @param {String} args.shopId - The shop that owns this surcharge
- * @param {ConnectionArgs} args - an object of all arguments that were sent by the client
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Object>|undefined} A Surcharge object
  */
 export default async function getSurcharge(parentResult, args, context) {
-  const { surchargeId, shopId } = args;
+  const { language, surchargeId, shopId } = args;
 
   return context.queries.getSurcharge(context, {
+    language,
     surchargeId: decodeSurchargeOpaqueId(surchargeId),
     shopId: decodeShopOpaqueId(shopId)
   });
