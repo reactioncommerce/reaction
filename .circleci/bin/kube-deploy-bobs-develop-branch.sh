cee5956ba400c0d02a0ffb47e5f03714cabcae0a
griggheo <grig@gheorghiu.net>
2019-01-19 15:12:08 -0800

    circleci modifications for deploying to kube via helm

---
+++ .circleci/bin/kube-deploy-bobs-develop-branch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt-get -y install python3-pip wget
+sudo pip3 install awscli
+
+if [ -z "${AWS_REGION}" ]; then
+        AWS_REGION=us-east-1
+fi
+
+AWS_ACCESS_KEY_ID_VAR_NAME=KUBECTL_CONFIG_STAGING_AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY_VAR_NAME=KUBECTL_CONFIG_STAGING_SECRET_AWS_ACCESS_KEY
+
+if [ "${!AWS_ACCESS_KEY_ID_VAR_NAME}" ]; then
+        export AWS_ACCESS_KEY_ID=${!AWS_ACCESS_KEY_ID_VAR_NAME}
+fi
+
+if [ "${!AWS_SECRET_ACCESS_KEY_VAR_NAME}" ]; then
+        export AWS_SECRET_ACCESS_KEY=${!AWS_SECRET_ACCESS_KEY_VAR_NAME}
+fi
+
+mkdir -p ~/.kube
+echo Running aws s3 cp s3://${KUBECTL_CONFIG_STAGING_S3_BUCKET}/${KUBECTL_CONFIG_STAGING_FILENAME} ~/.kube/config
+aws s3 cp s3://${KUBECTL_CONFIG_STAGING_S3_BUCKET}/${KUBECTL_CONFIG_STAGING_FILENAME} ~/.kube/config
+
+# download kubectl and helm
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl
+sudo mv kubectl /usr/local/bin
+sudo chmod +x /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/kubernetes-helm/helm-v2.12.2-linux-amd64.tar.gz
+tar xvfz helm-v2.12.2-linux-amd64.tar.gz
+sudo cp linux-amd64/helm /usr/local/bin
+sudo chmod +x /usr/local/bin/helm
+
+./kubectl config use-context circleci-context
+helm upgrade reaction-core helm-charts/reaction-core
+
