4588d5abcbc33d5dcf1215c6e10a348681a6702f
Erik Kieckhafer <ek@ato.la>
2019-01-23 10:34:07 -0800

    chore: remove unused file
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/surcharges/server/no-meteor/collections.js
+++
@@ -1,9 +0,0 @@
-/**
- * @name getSurchargesCollection
- * @summary Get surcharges collection from Mongo
- * @param {Object} context -  an object containing the per-request state
- * @returns {Object|null} surcharges collection
- */
-export function getSurchargesCollection(context) {
-  return context.app.db.collection("Surcharges");
-}
