9c2b52dd34f10eab5b9806b52819b8871ff5306a
Erik Kieckhafer <kieckhafer@users.noreply.github.com>
2019-01-22 22:53:23 -0800

    fix: formatting fix
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/sitemap-generator/server/no-meteor/resolvers/Query/sitemap.js
+++ imports/plugins/included/sitemap-generator/server/no-meteor/resolvers/Query/sitemap.js
@@ -7,7 +7,7 @@ import url from "url";
  * @param {Object} params - an object of all arguments that were sent by the client
  * @param {String} params.handle - Sitemap's handle, as set in Sitemaps collection
  * @param {String} params.shopUrl - URL of the shop the sitemap belongs to. The URL is used to find the shop with the domain of the URL
- * @param {Object} context -  an object containing the per-request state
+ * @param {Object} context - an object containing the per-request state
  * @returns {String} - Sitemap object containing XML with placeholders replaced (BASE_URL, LAST_MOD)
  */
 export default async function sitemapQuery(_, params, context) {
