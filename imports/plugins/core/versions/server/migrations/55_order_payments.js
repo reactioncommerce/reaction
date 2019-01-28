ffa962462156714d309131660fe248e9597bfb26
Erik Kieckhafer <kieckhafer@users.noreply.github.com>
2019-01-27 21:57:12 -0800

    fix: wrong version in migration 55 file
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/versions/server/migrations/55_order_payments.js
+++ imports/plugins/core/versions/server/migrations/55_order_payments.js
@@ -3,7 +3,7 @@ import { Orders } from "/lib/collections";
 import findAndConvertInBatches from "../util/findAndConvertInBatches";
 
 Migrations.add({
-  version: 54,
+  version: 55,
   up() {
     findAndConvertInBatches({
       collection: Orders,
