6e0823522e724f4d4091b763673a4529e006fd06
Mike Murray <hellomikemurray@gmail.com>
2019-01-28 15:28:58 -0800

    fix: import template directly
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/navigation/client/index.js
+++ imports/plugins/core/navigation/client/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { registerOperatorRoute } from "/imports/client/ui";
+import NavigationDashboard from "./containers/navigationDashboardContainer";
 
 import "./containers";
 import "./templates";
@@ -11,7 +12,7 @@ import "./styles.less";
 registerOperatorRoute({
   isNavigationLink: true,
   isSetting: true,
-  mainComponent: "navigationDashboard",
+  mainComponent: NavigationDashboard,
   path: "/navigation",
   // eslint-disable-next-line react/display-name
   SidebarIconComponent: (props) => <FontAwesomeIcon icon={faLink} {...props} />,
