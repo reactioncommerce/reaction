4b4c4d72ffd7d6642583b0859166ea9d41bf1954
Mike Murray <hellomikemurray@gmail.com>
2019-01-28 15:23:40 -0800

    feat: add navigation ui to operator 2.0 experience
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/navigation/client/index.js
+++ imports/plugins/core/navigation/client/index.js
@@ -1,3 +1,19 @@
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { registerOperatorRoute } from "/imports/client/ui";
+
 import "./containers";
 import "./templates";
 import "./styles.less";
+
+registerOperatorRoute({
+  isNavigationLink: true,
+  isSetting: true,
+  mainComponent: "navigationDashboard",
+  path: "/navigation",
+  // eslint-disable-next-line react/display-name
+  SidebarIconComponent: (props) => <FontAwesomeIcon icon={faLink} {...props} />,
+  sidebarI18nLabel: "admin.navigation.navigation"
+});
