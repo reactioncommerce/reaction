a375ebe803d5a75812d22ee794a56fecdf7beb2f
Eric Dobbertin <eric@dairystatedesigns.com>
2019-01-31 10:40:47 -0600

    feat: update collectionIndex to support passing options
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/utils/collectionIndex.js
+++ imports/utils/collectionIndex.js
@@ -1,15 +1,14 @@
 /**
  * @name collectionIndex
- * @private
+ * @summary Sets up necessary indexes on a collection. A wrapper around `createIndex`
  * @param {RawMongoCollection} collection the collection
- * @param {Object} index field to index
-* @returns {undefined}
- *
- * Sets up necessary indexes on the Catalog collection
+ * @param {Object} index field or fields to index
+ * @param {Object} [options] createIndex options. `background: true` is added automatically
+ * @returns {Promise<undefined>} Promise that resolves with undefined
  */
-export default async function collectionIndex(collection, index) {
+export default async function collectionIndex(collection, index, options = {}) {
   try {
-    await collection.createIndex(index, { background: true });
+    await collection.createIndex(index, { background: true, ...options });
   } catch (error) {
     console.error(error); // eslint-disable-line no-console
   }
