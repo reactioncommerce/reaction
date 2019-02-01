b4f3871178090474b67eb7de8b36139cd7bdd09f
Erik Kieckhafer <ek@ato.la>
2019-01-31 16:27:17 -0800

    refactor: move surcharges from cart and order to surcharge package
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ imports/plugins/included/surcharges/server/no-meteor/resolvers/AppliedSurcharge/index.js
@@ -0,0 +1,10 @@
+import { encodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/shop";
+import { encodeSurchargeOpaqueId } from "../../xforms/surcharge";
+import xformSurchargeAmount from "../../xforms/xformSurchargeAmount";
+import xformSurchargeMessage from "../../xforms/xformSurchargeMessage";
+
+export default {
+  _id: (node) => encodeSurchargeOpaqueId(node._id),
+  amount: (node, _, context) => node.amount && xformSurchargeAmount(context, node.shopId, node.amount),
+  message: (node, { language }) => node.messagesByLanguage && node.messagesByLanguage.length && xformSurchargeMessage(language, node.messagesByLanguage)
+};
