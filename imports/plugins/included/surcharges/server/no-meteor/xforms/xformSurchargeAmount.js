15c17c9eff5eab1155099ba04867ffda111e1d87
Erik Kieckhafer <ek@ato.la>
2019-01-31 16:25:21 -0800

    refactor: add currencyCode to surcharge xform
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/surcharges/server/no-meteor/xforms/xformSurchargeAmount.js
+++ imports/plugins/included/surcharges/server/no-meteor/xforms/xformSurchargeAmount.js
@@ -4,8 +4,12 @@
  * @param {String} importedAmount amount to convert to Money type
  * @return {Object} Transformed amount
  */
-export default async function xformSurchargeAmount(importedAmount) {
+export default async function xformSurchargeAmount(context, shopId, importedAmount) {
+  const shop = await context.collections.Shops.findOne({ _id: shopId });
+  const { currency: currencyCode } = shop;
+
   return {
-    amount: importedAmount
+    amount: importedAmount,
+    currencyCode
   };
 }
