98e4bf4251de177c302561625489d32f51485bd5
Erik Kieckhafer <ek@ato.la>
2019-02-06 12:00:59 -0800

    feat: move shipping operator into 2.0
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/shipping/client/components/Shipping.js
+++ imports/plugins/core/shipping/client/components/Shipping.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Blaze from "meteor/gadicc:blaze-react-component";
 
 export default class Shipping extends Component {
   render() {
     return (
       <div>
         <h4>Shipping</h4>
+        <Blaze template="shippingSettings" {...this.props} />
       </div>
     );
   }
