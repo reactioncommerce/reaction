7aac515ec505f27b1a2f2121558f9b12ef4a2b65
Erik Kieckhafer <ek@ato.la>
2019-02-08 09:50:07 -0800

    remove redundant function
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/surcharges/server/no-meteor/resolvers/Surcharge/index.js
+++ imports/plugins/included/surcharges/server/no-meteor/resolvers/Surcharge/index.js
@@ -1,11 +1,11 @@
 import { encodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/shop";
 import { encodeSurchargeOpaqueId } from "../../xforms/surcharge";
 import xformSurchargeAmount from "../../xforms/xformSurchargeAmount";
-import xformSurchargeMessage from "../../xforms/xformSurchargeMessage";
+import getSurchargeMessageForLanguage from "../../util/getSurchargeMessageForLanguage";
 
 export default {
   _id: (node) => encodeSurchargeOpaqueId(node._id),
   shopId: (node) => encodeShopOpaqueId(node.shopId),
   amount: (node, _, context) => node.amount && xformSurchargeAmount(context, node.shopId, node.amount),
-  message: (node, { language }) => node.messagesByLanguage && node.messagesByLanguage.length && xformSurchargeMessage(language, node.messagesByLanguage)
+  message: (node, { language }) => node.messagesByLanguage && node.messagesByLanguage.length && getSurchargeMessageForLanguage(language, node.messagesByLanguage)
 };
