99eee84b0ad62e6b3a6e403060c3f657b6f64cc8
Mike Murray <hellomikemurray@gmail.com>
2019-02-21 14:21:12 -0800

    feat: register operator routes for product list and detail
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/index.js
+++ imports/plugins/included/product-admin/client/index.js
@@ -2,3 +2,30 @@ import "./templates/productAdmin.html";
 import "./templates/productAdmin.js";
 
 export { default as ProductAdmin } from "./containers/productAdmin";
+
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBox } from "@fortawesome/free-solid-svg-icons";
+import { registerOperatorRoute } from "/imports/client/ui";
+
+import ProductList from "./components/ProductList";
+import ProductDetail from "./components/ProductDetail";
+
+
+registerOperatorRoute({
+  isNavigationLink: false,
+  isSetting: false,
+  path: "/products/:handle/:variantId?",
+  mainComponent: ProductDetail
+});
+
+registerOperatorRoute({
+  isNavigationLink: true,
+  isSetting: false,
+  path: "/products",
+  mainComponent: ProductList,
+  // eslint-disable-next-line react/display-name, react/no-multi-comp
+  SidebarIconComponent: (props) => <FontAwesomeIcon icon={faBox} {...props} />,
+  sidebarI18nLabel: "admin.products.Products"
+});
