70e0ca03651f03e49fa4e146f53a763c6c7d8dde
Mike Murray <hellomikemurray@gmail.com>
2019-02-21 14:20:37 -0800

    feat: add product detail component which wraps the original product detail page for operator 2.0
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/plugins/included/product-admin/client/components/ProductDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Components } from "@reactioncommerce/reaction-components";
+
+/**
+ * ProductDetail component
+ * @return {Node} React node
+ */
+function ProductDetail() {
+  return (
+    <Components.ProductDetail />
+  );
+}
+
+export default ProductDetail;
