3e81f659879cf5f1bc409e63ec1b1c4d54b62a5f
Erik Kieckhafer <ek@ato.la>
2019-02-21 11:44:44 -0800

    add orderStatus variable to query to limit search resutls to certain workflow statuses
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/queries/ordersByAccountId.js
+++ imports/plugins/core/orders/server/no-meteor/queries/ordersByAccountId.js
@@ -8,10 +8,11 @@ import ReactionError from "@reactioncommerce/reaction-error";
  * @param {Object} context - an object containing the per-request state
  * @param {Object} params - request parameters
  * @param {String} params.accountId - Account ID to search orders for
+ * @param {String} params.orderStatus - Workflow status to limit search results
  * @param {String} params.shopIds - Shop IDs for the shops that owns the orders
  * @return {Promise<Object>|undefined} - An Array of Order documents, if found
  */
-export default async function ordersByAccountId(context, { accountId, shopIds } = {}) {
+export default async function ordersByAccountId(context, { accountId, orderStatus, shopIds } = {}) {
   const { accountId: contextAccountId, collections, shopsUserHasPermissionFor, userHasPermission } = context;
   const { Orders } = collections;
 
@@ -19,9 +20,28 @@ export default async function ordersByAccountId(context, { accountId, shopIds } 
     throw new ReactionError("invalid-param", "You must provide accountId arguments");
   }
 
-  const query = {
+  let query = {
     accountId
   };
+
+  // Limit which orders are returned: `all`, `open`, or `completed`
+  // `open` is anything that does not have the `workflow.status` of `coreOrderWorkflow/completed`
+  if (orderStatus !== "all") {
+    if (orderStatus === "open") {
+      query = {
+        "workflow.status": { $ne: "coreOrderWorkflow/completed" },
+        ...query
+      };
+    }
+
+    if (orderStatus === "completed") {
+      query = {
+        "workflow.status": { $eq: "coreOrderWorkflow/completed" },
+        ...query
+      };
+    }
+  }
+
 
   if (shopIds) query.shopId = { $in: shopIds };
 
--- imports/plugins/core/orders/server/no-meteor/resolvers/Query/ordersByAccountId.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/Query/ordersByAccountId.js
@@ -10,17 +10,19 @@ import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/sh
  * @param {Object} parentResult - unused
  * @param {ConnectionArgs} args - An object of all arguments that were sent by the client
  * @param {String} args.accountId - accountId of owner of the orders
+ * @param {String} params.orderStatus - workflow status to limit search results
  * @param {String} args.shopIds - shop IDs to check for orders from
  * @param {Object} context - An object containing the per-request state
  * @return {Promise<Object>|undefined} An Order object
  */
 export default async function ordersByAccountId(parentResult, args, context) {
-  const { accountId, shopIds: opaqueShopIds, ...connectionArgs } = args;
+  const { accountId, orderStatus, shopIds: opaqueShopIds, ...connectionArgs } = args;
 
   const shopIds = opaqueShopIds && opaqueShopIds.map(decodeShopOpaqueId);
 
   const query = await context.queries.ordersByAccountId(context, {
     accountId: decodeAccountOpaqueId(accountId),
+    orderStatus,
     shopIds
   });
 
