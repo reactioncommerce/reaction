7f29d634b4cc171f6ca69a59a8994a91c9a927c6
Erik Kieckhafer <ek@ato.la>
2019-02-22 10:19:48 -0800

    add orderSummary to order resolver
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/resolvers/Order/index.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/Order/index.js
@@ -2,6 +2,7 @@ import { encodeCartOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/ca
 import { encodeOrderOpaqueId, xformOrderPayment } from "@reactioncommerce/reaction-graphql-xforms/order";
 import { resolveAccountFromAccountId, resolveShopFromShopId } from "@reactioncommerce/reaction-graphql-utils";
 import orderStatus from "./orderStatus";
+import orderSummary from "./orderSummary";
 import totalItemQuantity from "./totalItemQuantity";
 
 export default {
@@ -13,5 +14,6 @@ export default {
   payments: (node) => (Array.isArray(node.payments) ? node.payments.map(xformOrderPayment) : null),
   shop: resolveShopFromShopId,
   status: (node, { language }, context) => orderStatus(context, node, language),
+  summary: (node, _, context) => orderSummary(context, node),
   totalItemQuantity
 };
