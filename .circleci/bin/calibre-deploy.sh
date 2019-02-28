5679c7b0f28a8b6b92bc7ceb76fbaafbc930417e
Jeffrey Corpuz <jeffcorpuz@icloud.com>
2019-02-28 11:13:40 -0800

    One directory up
    Signed-off-by: Jeffrey Corpuz <jeffcorpuz@icloud.com>

--- .circleci/bin/calibre-deploy.sh
+++ .circleci/bin/calibre-deploy.sh
@@ -6,7 +6,7 @@ URL=$1
 LOCATION=$2
 
 # Run One Off Test
-./node_modules/calibre/bin/linux/calibre test create $URL --location=$LOCATION
+./../node_modules/calibre/bin/linux/calibre test create $URL --location=$LOCATION
 
 # Run Screenshot
 # ./node_modules/calibre/bin/linux/calibre site create-snapshot --site reaction-core-"$(echo $LOCATION | tr '[A-Z]' '[a-z]')"
