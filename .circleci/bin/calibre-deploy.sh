cb40e409a9e0b7d8eedcb48e7c1ac449ab97750c
Jeffrey Corpuz <jeffcorpuz@icloud.com>
2019-02-28 11:20:14 -0800

    Turn on Snapshot feature
    Signed-off-by: Jeffrey Corpuz <jeffcorpuz@icloud.com>

--- .circleci/bin/calibre-deploy.sh
+++ .circleci/bin/calibre-deploy.sh
@@ -9,4 +9,4 @@ LOCATION=$2
 ./../node_modules/calibre/bin/linux/calibre test create $URL --location=$LOCATION
 
 # Run Screenshot
-# ./node_modules/calibre/bin/linux/calibre site create-snapshot --site reaction-core-"$(echo $LOCATION | tr '[A-Z]' '[a-z]')"
+./../node_modules/calibre/bin/linux/calibre site create-snapshot --site reaction-core-"$(echo $LOCATION | tr '[A-Z]' '[a-z]')"
