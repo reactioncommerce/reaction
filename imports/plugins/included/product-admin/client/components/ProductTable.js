240d815de4fe2dea30dcd4faf93af83ad3cc40c3
Mike Murray <hellomikemurray@gmail.com>
2019-03-04 21:53:58 -0800

    feat: add product table component
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/plugins/included/product-admin/client/components/ProductTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Components } from "@reactioncommerce/reaction-components";
+
+/**
+ * ProductList component
+ * @return {Node} React node
+ */
+function ProductList() {
+  return (
+    <Components.ProductsAdmin />
+  );
+}
+
+export default ProductList;
