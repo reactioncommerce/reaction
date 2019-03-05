ebdc5b67728f9eb462a501d5279d05be5e9c4904
Mike Murray <hellomikemurray@gmail.com>
2019-03-04 22:08:09 -0800

    feat: update layout with the publish toolbar, admin form, header and product list components
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/components/ProductDetail.js
+++ imports/plugins/included/product-admin/client/components/ProductDetail.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
+import Grid from "@material-ui/core/Grid";
+import withProduct from "../hocs/withProduct";
+import ProductList from "./ProductList";
+import ProductAdminForm from "./productAdmin";
+import ProductHeader from "./ProductHeader";
 
 /**
  * ProductDetail component
+ * @param {Object} props Component props
  * @return {Node} React node
  */
-function ProductDetail() {
+function ProductDetail(props) {
   return (
-    <Components.ProductDetail />
+    <Fragment>
+      <Components.ProductPublish />
+      <Grid container spacing={24}>
+        <Grid item sm={12}>
+          <ProductHeader
+            product={props.product}
+            title="Variants"
+            onCreate={props.onCreateVariant}
+          />
+        </Grid>
+        <Grid item sm={4}>
+          <ProductList
+            items={props.variants}
+          />
+        </Grid>
+        <Grid item sm={8}>
+          <ProductAdminForm {...props} />
+        </Grid>
+      </Grid>
+    </Fragment>
   );
 }
 
-export default ProductDetail;
+ProductDetail.propTypes = {
+  onCreateVariant: PropTypes.func,
+  product: PropTypes.object,
+  variants: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default withProduct(ProductDetail);
