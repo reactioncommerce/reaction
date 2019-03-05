1b69d9141e88a832f23f488b1e83929f9c819675
Mike Murray <hellomikemurray@gmail.com>
2019-03-04 22:09:28 -0800

    feat: add additional routes for variant and options
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/index.js
+++ imports/plugins/included/product-admin/client/index.js
@@ -9,15 +9,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBox } from "@fortawesome/free-solid-svg-icons";
 import { registerOperatorRoute } from "/imports/client/ui";
 
-import ProductList from "./components/ProductList";
+import ProductTable from "./components/ProductTable";
 import ProductDetail from "./components/ProductDetail";
+import VariantDetail from "./components/VariantDetail";
 import ContentViewExtraWideLayout from "/imports/client/ui/layouts/ContentViewExtraWideLayout";
-
 
 registerOperatorRoute({
   isNavigationLink: false,
   isSetting: false,
-  path: "/products/:handle/:variantId?",
+  path: "/products/:handle/:variantId/:optionId",
+  mainComponent: VariantDetail
+});
+
+registerOperatorRoute({
+  isNavigationLink: false,
+  isSetting: false,
+  path: "/products/:handle/:variantId",
+  mainComponent: VariantDetail
+});
+
+registerOperatorRoute({
+  isNavigationLink: false,
+  isSetting: false,
+  path: "/products/:handle",
   mainComponent: ProductDetail
 });
 
@@ -26,8 +40,8 @@ registerOperatorRoute({
   isSetting: false,
   layoutComponent: ContentViewExtraWideLayout,
   path: "/products",
-  mainComponent: ProductList,
+  mainComponent: ProductTable,
   // eslint-disable-next-line react/display-name, react/no-multi-comp
   SidebarIconComponent: (props) => <FontAwesomeIcon icon={faBox} {...props} />,
-  sidebarI18nLabel: "admin.products.Products"
+  sidebarI18nLabel: "admin.products"
 });
