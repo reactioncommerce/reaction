08b55a6fdd339b3755d8c4141b7f5b495176b210
Mike Murray <hellomikemurray@gmail.com>
2019-03-06 13:18:22 -0800

    feat: add product list
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/components/ProductTable.js
+++ imports/plugins/included/product-admin/client/components/ProductTable.js
@@ -1,14 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { i18next } from "/client/api";
+import withCreateProduct from "../hocs/withCreateProduct";
 
 /**
  * ProductList component
+ * @param {Object} props Component props
  * @return {Node} React node
  */
-function ProductList() {
+function ProductList({ onCreateProduct }) {
   return (
-    <Components.ProductsAdmin />
+    <Grid container spacing={24}>
+      <Grid item sm={12}>
+        <Button
+          color="primary"
+          onClick={onCreateProduct}
+          variant="contained"
+        >
+          {i18next.t("admin.createProduct")}
+        </Button>
+      </Grid>
+      <Grid item sm={12}>
+        <Components.ProductsAdmin />
+      </Grid>
+    </Grid>
   );
 }
 
-export default ProductList;
+ProductList.propTypes = {
+  onCreateProduct: PropTypes.func
+};
+
+export default withCreateProduct(ProductList);
