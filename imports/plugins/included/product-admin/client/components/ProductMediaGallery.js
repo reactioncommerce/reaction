750d160ebe5584941a8ed7002de878f536951e97
Mike Murray <hellomikemurray@gmail.com>
2019-03-06 23:04:00 -0800

    feat: change layout of media gallery to a table
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/components/ProductMediaGallery.js
+++ imports/plugins/included/product-admin/client/components/ProductMediaGallery.js
@@ -1,127 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Dropzone from "react-dropzone";
-import { Components } from "@reactioncommerce/reaction-components";
+import { i18next } from "/client/api";
+import Button from "@material-ui/core/Button";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
 import withProductMedia from "../hocs/withProductMedia";
+import ProductMediaItem from "./ProductMediaItem";
 
-class ProductMediaGallery extends Component {
-  static propTypes = {
-    editable: PropTypes.bool, // eslint-disable-line react/boolean-prop-naming
-    media: PropTypes.arrayOf(PropTypes.object),
-    onDrop: PropTypes.func,
-    onMouseEnterMedia: PropTypes.func,
-    onMouseLeaveMedia: PropTypes.func,
-    onMoveMedia: PropTypes.func,
-    onRemoveMedia: PropTypes.func,
-    uploadProgress: PropTypes.shape({
-      bytesUploaded: PropTypes.number.isRequired,
-      bytesTotal: PropTypes.number.isRequired,
-      percentage: PropTypes.number.isRequired
-    })
-  };
+/**
+ * ProductMediaGallery
+ * @param {Object} props Component props
+ * @returns {Node} React component
+ */
+function ProductMediaGallery(props) {
+  const { editable, media, onSetMediaPriority, onRemoveMedia, uploadProgress } = props;
 
-  static defaultProps = {
-    onDrop() { },
-    onMouseEnterMedia() { },
-    onMouseLeaveMedia() { },
-    onMoveMedia() { },
-    onRemoveMedia() { }
-  };
+  if (editable) {
+    const hasMedia = Array.isArray(media) && media.length > 0;
 
-  constructor() {
-    super();
-
-    this.state = {
-      dimensions: {
-        width: -1,
-        height: -1
-      }
-    };
-  }
-
-  get hasMedia() {
-    const { media } = this.props;
-
-    return Array.isArray(media) && media.length > 0;
-  }
-
-  handleDropClick = () => {
-    this.dropzone && this.dropzone.open();
-  };
-
-  handleDrop = (files) => {
-    if (files.length === 0) return;
-    this.props.onDrop(files);
-  };
-
-  renderAddItem() {
-    if (this.props.editable) {
-      return (
-        <Components.Button
-          className={{
-            // Disable default button class names
-            "flat": false,
-            "btn": false,
-            "btn-default": false,
-
-            // Media gallery class names
-            "gallery-image": true,
-            "add": true
-          }}
-          tagName="div"
-          onClick={this.handleDropClick}
-        >
-          <img
-            alt=""
-            className="img-responsive"
-            src={"/resources/placeholder.gif"}
-          />
-          <div className="rui badge-container">
-            <i className="fa fa-plus fa-2x" />
-          </div>
-        </Components.Button>
-      );
-    }
-
-    return null;
-  }
-
-  renderProgressItem() {
-    // For now, we are not showing actual progress, but we could
-    return (
-      <div className="gallery-image add">
-        <img
-          alt=""
-          className="img-responsive"
-          src={"/resources/placeholder.gif"}
-        />
-        <div className="rui badge-container">
-          <Components.CircularProgress indeterminate={true} />
-        </div>
-      </div>
-    );
-  }
-
-  renderMediaThumbnails() {
-    const { editable, media: mediaList, onMouseEnterMedia, onMouseLeaveMedia, onMoveMedia, onRemoveMedia } = this.props;
-
-    return (mediaList || []).map((media, index) => (
-      <Components.MediaItem
-        editable={editable}
-        key={media._id}
-        index={index} // index prop is required for SortableItem HOC to work
-        onMouseEnter={onMouseEnterMedia}
-        onMouseLeave={onMouseLeaveMedia}
-        onMove={onMoveMedia}
-        onRemoveMedia={onRemoveMedia}
-        size="small"
-        source={media}
-      />
-    ));
-  }
-
-  renderMediaGalleryUploader() {
-    const { uploadProgress } = this.props;
     return (
       <div className="rui media-gallery">
         <Dropzone
@@ -129,52 +30,94 @@ class ProductMediaGallery extends Component {
           disableClick
           multiple
           disablePreview
-          onDrop={this.handleDrop}
+          onDrop={(files) => {
+            if (files.length === 0) return;
+            props.onDrop(files);
+          }}
           ref={(inst) => { this.dropzone = inst; }}
           accept="image/jpg, image/png, image/jpeg"
         >
-          <div className="rui gallery">
-            <div className="rui gallery-thumbnails">
-              {!!this.hasMedia && this.renderMediaThumbnails()}
-              {!!uploadProgress && this.renderProgressItem()}
-              {this.renderAddItem()}
-            </div>
-          </div>
+          <Table padding="dense">
+            <TableHead>
+              <TableRow>
+                <TableCell>{i18next.t("admin.productTable.header.order")}</TableCell>
+                <TableCell>{"Media"}</TableCell>
+                <TableCell />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!!hasMedia && (
+                (media || []).map((mediaItem) => (
+                  <ProductMediaItem
+                    editable={editable}
+                    key={mediaItem._id}
+                    onSetMediaPriority={onSetMediaPriority}
+                    onRemoveMedia={onRemoveMedia}
+                    size="small"
+                    source={mediaItem}
+                  />
+                ))
+              )}
+              {!!uploadProgress && (
+                <TableRow>
+                  <TableCell colSpan={3}>
+                    <LinearProgress indeterminate={true} />
+                  </TableCell>
+                </TableRow>
+              )}
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <Button
+                    fullWidth
+                    onClick={() => {
+                      this.dropzone && this.dropzone.open();
+                    }}
+                    size="large"
+                  >
+                    {"Drag image or click to upload"}
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </Dropzone>
       </div>
     );
   }
 
-  renderMediaGallery() {
-    return (
-      <div className="rui media-gallery">
-        <div className="rui gallery">
-          <div className="rui gallery-thumbnails">
-            {this.renderMediaThumbnails()}
-          </div>
-        </div>
-      </div>
-    );
-  }
 
-  render() {
-    const { editable } = this.props;
-
-    let gallery;
-    if (editable) {
-      gallery = this.renderMediaGalleryUploader();
-    } else {
-      gallery = this.renderMediaGallery();
-    }
-
-    // Note that only editable mode actually uses drag-drop, but since both views render
-    // MediaItems, which are SortableItems, there is an error if it isn't in the ancestor tree
-    return (
-      <div>
-        {gallery}
-      </div>
-    );
-  }
+  return (
+    <Table padding="dense">
+      <TableHead>
+        <TableRow>
+          <TableCell>{i18next.t("admin.productTable.header.order")}</TableCell>
+          <TableCell>{"Media"}</TableCell>
+          <TableCell />
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {this.renderRows()}
+      </TableBody>
+    </Table>
+  );
 }
+
+ProductMediaGallery.propTypes = {
+  editable: PropTypes.bool, // eslint-disable-line react/boolean-prop-naming
+  media: PropTypes.arrayOf(PropTypes.object),
+  onRemoveMedia: PropTypes.func,
+  onSetMediaPriority: PropTypes.func,
+  uploadProgress: PropTypes.shape({
+    bytesUploaded: PropTypes.number.isRequired,
+    bytesTotal: PropTypes.number.isRequired,
+    percentage: PropTypes.number.isRequired
+  })
+};
+
+ProductMediaGallery.defaultProps = {
+  onDrop() {},
+  onRemoveMedia() {}
+};
+
 
 export default withProductMedia(ProductMediaGallery);
