295e4672339b121f066b450da4504f43778083d0
Machiko Yasuda <machikoyasuda@gmail.com>
2019-03-06 23:31:16 -0800

    testing tests

---
+++ tests/catalog/catalogItemsByTag.test.js
@@ -0,0 +1,54 @@
+import TestApp from "../TestApp";
+// temp mocks
+import { mockMoreCatalogItems } from "../mocks/mockMoreCatalogItems";
+// temp mocks
+import CatalogProductItemsFullQuery from "./CatalogProductItemsFullQuery.graphql";
+
+const internalTagIds = [
+  "sjrKzMBz7HkX3ZSM7", // 8
+  "QGxZeKJtfubC4m3ck", // 3
+  "4jevtBGXLz2BF8Lf8" // 12
+];
+
+const opaqueTagIds = [
+  "cmVhY3Rpb24vdGFnOnNqckt6TUJ6N0hrWDNaU003", // T-Shirts
+  "cmVhY3Rpb24vdGFnOlFHeFplS0p0ZnViQzRtM2Nr", // Womens
+  "cmVhY3Rpb24vdGFnOjRqZXZ0QkdYTHoyQkY4TGY4" // Shirts
+];
+
+export const internalShopId = "123";
+export const opaqueShopId = "cmVhY3Rpb24vc2hvcDoxMjM="; // reaction/shop:123
+export const shopName = "Test Shop";
+
+jest.setTimeout(300000);
+
+let testApp;
+let query;
+
+beforeAll(async () => {
+  testApp = new TestApp();
+  await testApp.start();
+  query = testApp.query(CatalogProductItemsFullQuery);
+  await testApp.insertPrimaryShop({ _id: internalShopId, name: shopName });
+  await Promise.all(internalTagIds.map((_id) => testApp.collections.Tags.insert({ _id, shopId: internalShopId })));
+  await Promise.all(mockMoreCatalogItems.map((mockCatalogItem) => testApp.collections.Catalog.insert(mockCatalogItem)));
+});
+
+afterAll(async () => {
+  await testApp.collections.Shops.remove({ _id: internalShopId });
+  await Promise.all(internalTagIds.map((_id) => testApp.collections.Tags.remove({ _id })));
+  await Promise.all(mockMoreCatalogItems.map((mockCatalogItem) => testApp.collections.Catalog.remove({ _id: mockCatalogItem._id })));
+  testApp.stop();
+});
+
+test("get all items for shop", async () => {
+  let result;
+  try {
+    result = await query({ shopIds: [opaqueShopId], tagIds: [opaqueTagIds[0]] });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+  console.log(result);
+  expect(result.catalogItems.nodes.count).toEqual(8);
+});
---
+++ tests/mocks/mockMoreCatalogItems.js
@@ -0,0 +1,164 @@
+import { internalShopId } from "./mockShop";
+
+const createdAt = new Date("2018-04-16T15:34:28.043Z");
+const updatedAt = new Date("2018-04-17T15:34:28.043Z");
+
+export const mockMoreCatalogItems = [{
+  _id: "883SNvS4Q9Lkg8FyY",
+  product: {
+    tagIds: [
+      "sjrKzMBz7HkX3ZSM7",
+      "QGxZeKJtfubC4m3ck",
+      "4jevtBGXLz2BF8Lf8"
+    ],
+    title: "Ticket to Anywhere T-Shirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "8Zn7vLqMhXvdncsQh",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "sjrKzMBz7HkX3ZSM7",
+      "QGxZeKJtfubC4m3ck"
+    ],
+    title: "Skeletal Peace T-Shirt (Women's)"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "LF2Ga4cETQHWuy8gN",
+  product: {
+    tagIds: [
+      "sjrKzMBz7HkX3ZSM7",
+      "QGxZeKJtfubC4m3ck",
+      "4jevtBGXLz2BF8Lf8"
+    ],
+    title: "New York City 1998 T-Shirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "LM6TStCkZvaq3k5Y8",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "sjrKzMBz7HkX3ZSM7"
+    ],
+    title: "Skeletal Peace T-Shirt (Men's)"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "SLHLdsuDC7GC7e7ZF",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "sjrKzMBz7HkX3ZSM7"
+    ],
+    title: "Thankful T-Shirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "TqyppvanAXXoeGaCB",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "sjrKzMBz7HkX3ZSM7"
+    ],
+    title: "Biker Club Patch T-Shirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "ZQfdvwHv6e2s76PWt",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "MdjegJtuvT7YSNBNs"
+    ],
+    title: "Floral Button Up"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "hmNLAW8A5egh8uCd5",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "JopxLLmC8yBWiyPSz"
+    ],
+    title: "Classic Cotton Long Sleeve Shirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "mK5vkYum2XM3AhAxe",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "sjrKzMBz7HkX3ZSM7"
+    ],
+    title: "Light Bulb Logo T-Shirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "s25TkrTw3uGAQq29o",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "mCCv2TQBeyQThJYyC"
+    ],
+    title: "The Red Sweatshirt"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "vWLeAAC9EwqJxbz9b",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "sjrKzMBz7HkX3ZSM7"
+    ],
+    title: "Undefeated Tee"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+},
+{
+  _id: "yFc6HjafY82YNwBwp",
+  product: {
+    tagIds: [
+      "4jevtBGXLz2BF8Lf8",
+      "mk2Zrp8uRQ2ZyHPoG"
+    ],
+    title: "Dream Tank Top"
+  },
+  shopId: internalShopId,
+  createdAt,
+  updatedAt
+}];
