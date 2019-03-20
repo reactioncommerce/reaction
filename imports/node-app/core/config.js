2ce46acaf2cb72edaaadc6808fa69008f520ccc4
Eric Dobbertin <eric@dairystatedesigns.com>
2019-03-20 11:52:00 -0500

    test: explicitly add NODE_ENV to envalid
    Fixes integration test errors
    
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/node-app/core/config.js
+++ imports/node-app/core/config.js
@@ -1,7 +1,13 @@
-import envalid, { bool } from "envalid";
+import envalid, { bool, str } from "envalid";
 
 export default envalid.cleanEnv(process.env, {
   GRAPHQL_INTROSPECTION_ENABLED: bool({ default: false }),
-  GRAPHQL_PLAYGROUND_ENABLED: bool({ default: false })
+  GRAPHQL_PLAYGROUND_ENABLED: bool({ default: false }),
+  // This is necessary to override the envalid default
+  // validation for NODE_ENV, which uses
+  // str({ choices: ['development', 'test', 'production'] })
+  //
+  // We currently need to set NODE_ENV to "jesttest" when
+  // integration tests run.
+  NODE_ENV: str()
 });
-
