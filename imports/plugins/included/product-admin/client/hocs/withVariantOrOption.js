c1a535e475a2b8170235b24542addbba927a7ebe
Mike Murray <hellomikemurray@gmail.com>
2019-03-29 14:22:33 -0700

    docs: update jsdoc
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/hocs/withVariantOrOption.js
+++ imports/plugins/included/product-admin/client/hocs/withVariantOrOption.js
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 
 const wrapComponent = (Comp) => {
   /**
-   * withOptions HOC
+   * The `withVariantOrOptions` HOC redirects the option prop to the variant prop if available.
+   * This useful for some blocks like `VariantDetailForm` which expects `variant` prop.
+   * With Variants and Options being identical in all ways but their name, this is useful for views
+   * that expect a `variant`. Typically used after the `withVariant` HOC to redirect the loaded `option` to `variant`.
+   * @example withVariant(withVariantOrOption(MyComponent))
    * @param {Object} props Component props
    * @returns {React.Component} A React component
    */
