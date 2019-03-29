fe03ff87a162d3219f1930f09ee49d707735bea3
Mike Murray <hellomikemurray@gmail.com>
2019-03-29 14:24:30 -0700

    feat: add variant header block component
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/plugins/included/product-admin/client/blocks/VariantHeader.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ProductHeader from "../components/ProductHeader";
+
+/**
+ * Variant table block component
+ * @param {Object} props Component props
+ * @return {Node} React node
+ */
+function VariantHeader(props) {
+  const {
+    option,
+    product,
+    variant,
+    removeVariant,
+    restoreVariant,
+    cloneVariant,
+    onVisibilityButtonClick
+  } = props;
+
+  return (
+    <ProductHeader
+      option={option}
+      product={product}
+      variant={variant}
+      onCloneVariant={cloneVariant}
+      onArchiveProduct={removeVariant}
+      onRestoreProduct={restoreVariant}
+      onVisibilityChange={onVisibilityButtonClick}
+    />
+  );
+}
+
+VariantHeader.propTypes = {
+  cloneVariant: PropTypes.func,
+  onVisibilityButtonClick: PropTypes.func,
+  option: PropTypes.object,
+  product: PropTypes.object,
+  removeVariant: PropTypes.func,
+  restoreVariant: PropTypes.func,
+  variant: PropTypes.object
+};
+
+export default VariantHeader;
