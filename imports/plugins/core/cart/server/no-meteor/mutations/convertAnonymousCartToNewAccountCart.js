4bb0351e6fd00b5b13568af6810936399d7e6169
James Porlante <james@reactioncommerce.com>
2019-03-29 16:13:07 +0800

    (fix) lint issue
    Signed-off-by: James Porlante <james@reactioncommerce.com>

--- imports/plugins/core/cart/server/no-meteor/mutations/convertAnonymousCartToNewAccountCart.js
+++ imports/plugins/core/cart/server/no-meteor/mutations/convertAnonymousCartToNewAccountCart.js
@@ -60,7 +60,7 @@ export default async function convertAnonymousCartToNewAccountCart({
   const { deletedCount } = await Cart.deleteOne(anonymousCartSelector);
   if (deletedCount === 0) {
     if (referenceId) {
-      await Cart.updateOne({ _id }, {set: { referenceId } });
+      await Cart.updateOne({ _id }, { $set: { referenceId } });
     }
     throw new ReactionError("server-error", "Unable to delete anonymous cart");
   }
