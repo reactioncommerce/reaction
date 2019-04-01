f273b122852bf4e18cacc4e6b51b9601d1387f7b
Chris Potter <chris@reactioncommerce.com>
2019-04-01 14:56:08 -0400

    updates to systeminfo.js to remove user authentication
    Signed-off-by: Chris Potter <chris@reactioncommerce.com>

--- imports/plugins/core/system-info/server/no-meteor/queries/systemInformation.js
+++ imports/plugins/core/system-info/server/no-meteor/queries/systemInformation.js
@@ -4,17 +4,12 @@ import packageJson from "/package.json";
 /**
  * @name queries.systemInformation
  * @method
- * @memberof Tags/GraphQL
+ * @memberof SystemInformation/GraphQL
  * @summary get systemInformations
  * @param {Object} context - an object containing the per-request state
- * @param {String} [params.shopId] - Shop ID
- * @return {Promise<Array<Object>>} array of TagProduct
+ * @return {<Object} System Information
  **/
 export default async function systemInformation(context) {
-  // if (!context.userHasPermission(["owner", "admin"], shopId)) {
-  //   throw new ReactionError("access-denied", "Access denied");
-  // }
-
   return {
     apiVersion: packageJson['version']
   };
