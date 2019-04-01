8231c3746aa9f31e6f8f4c4a440fa5d06293d1b9
Eric Dobbertin <eric@dairystatedesigns.com>
2019-04-01 15:23:13 -0500

    refactor: use collectionIndex for Emails
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/email/server/no-meteor/startup.js
+++ imports/plugins/core/email/server/no-meteor/startup.js
@@ -8,10 +8,11 @@ import collectionIndex from "/imports/utils/collectionIndex";
  */
 export default function startup(context) {
   const { collections } = context;
-  const { Templates } = collections;
+  const { Emails, Templates } = collections;
 
   // Create indexes. We set specific names for backwards compatibility
   // with indexes created by the aldeed:schema-index Meteor package.
+  collectionIndex(Emails, { jobId: 1 }, { name: "c2_jobId" });
   collectionIndex(Templates, { shopId: 1 }, { name: "c2_shopId" });
 }
 
