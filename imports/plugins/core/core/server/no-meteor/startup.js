2eb20dd228e060d2fbaf414568bedbe0feaaf0e3
Eric Dobbertin <eric@dairystatedesigns.com>
2019-04-01 14:43:06 -0500

    refactor: use collectionIndex for Shops
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/core/server/no-meteor/startup.js
+++ imports/plugins/core/core/server/no-meteor/startup.js
@@ -8,7 +8,7 @@ import collectionIndex from "/imports/utils/collectionIndex";
  */
 export default function startup(context) {
   const { collections } = context;
-  const { Packages } = collections;
+  const { Packages, Shops } = collections;
 
   // Create indexes. We set specific names for backwards compatibility
   // with indexes created by the aldeed:schema-index Meteor package.
@@ -26,4 +26,20 @@ export default function startup(context) {
   collectionIndex(Packages, { "registry.provides": 1 }, { name: "c2_registry.$.provides" });
   collectionIndex(Packages, { "registry.route": 1 }, { name: "c2_registry.$.route" });
   collectionIndex(Packages, { shopId: 1 }, { name: "c2_shopId" });
+
+  collectionIndex(Shops, { active: 1 }, { name: "c2_active" });
+  collectionIndex(Shops, { domains: 1 }, { name: "c2_domains" });
+  collectionIndex(Shops, { "layout.layout": 1 }, { name: "c2_layout.$.layout" });
+  collectionIndex(Shops, { "layout.structure.adminControlsFooter": 1 }, { name: "c2_layout.$.structure.adminControlsFooter" });
+  collectionIndex(Shops, { "layout.structure.dashboardControls": 1 }, { name: "c2_layout.$.structure.dashboardControls" });
+  collectionIndex(Shops, { "layout.structure.dashboardHeader": 1 }, { name: "c2_layout.$.structure.dashboardHeader" });
+  collectionIndex(Shops, { "layout.structure.dashboardHeaderControls": 1 }, { name: "c2_layout.$.structure.dashboardHeaderControls" });
+  collectionIndex(Shops, { "layout.structure.layoutFooter": 1 }, { name: "c2_layout.$.structure.layoutFooter" });
+  collectionIndex(Shops, { "layout.structure.layoutHeader": 1 }, { name: "c2_layout.$.structure.layoutHeader" });
+  collectionIndex(Shops, { "layout.structure.notFound": 1 }, { name: "c2_layout.$.structure.notFound" });
+  collectionIndex(Shops, { "layout.structure.template": 1 }, { name: "c2_layout.$.structure.template" });
+  collectionIndex(Shops, { name: 1 }, { name: "c2_name" });
+  collectionIndex(Shops, { shopType: 1 }, { name: "c2_shopType" });
+  collectionIndex(Shops, { slug: 1 }, { name: "c2_slug", sparse: true, unique: true });
+  collectionIndex(Shops, { "workflow.status": 1 }, { name: "c2_workflow.status" });
 }
