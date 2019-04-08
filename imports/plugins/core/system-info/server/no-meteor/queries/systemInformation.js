0e66bdddba11e3996567693d6d6ee77d1e90d351
Chris Potter <chris@reactioncommerce.com>
2019-04-08 16:54:02 -0400

    updates from code review including Shop ID and array map
    Signed-off-by: Chris Potter <chris@reactioncommerce.com>

--- imports/plugins/core/system-info/server/no-meteor/queries/systemInformation.js
+++ imports/plugins/core/system-info/server/no-meteor/queries/systemInformation.js
@@ -1,16 +1,21 @@
 import packageJson from "/package.json";
+import ReactionError from "@reactioncommerce/reaction-error";
+
 
 /**
  * @name queries.systemInformation
  * @method
  * @memberof SystemInformation/GraphQL
  * @summary get systemInformations
+ * @param {String} args.shopId The ID of the shop to load settings for
  * @param {Object} context - an object containing the per-request state
  * @return {<Object} System Information
  **/
-export default async function systemInformation(context) {
+export default async function systemInformation(id, context) {
+  if (!context.userHasPermission(["owner", "admin"], id)) throw new ReactionError("access-denied", "User does not have permission");
+  const plugins = await context.collections.Packages.find({ shopId: id, version: { $exists: true } }).toArray();
   return {
     apiVersion: packageJson.version,
-    plugins: await context.collections.Packages.find({ version: { $exists: true } }).toArray()
+    plugins: plugins.map(({ name, version }) => ({ name, version }))
   };
 }
--- imports/plugins/core/system-info/server/no-meteor/resolvers/Query/systemInformation.js
+++ imports/plugins/core/system-info/server/no-meteor/resolvers/Query/systemInformation.js
@@ -1,13 +1,15 @@
+import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/shop";
+
 /**
  * @name Query.systemInformation
  * @method
  * @memberof SystemInformation/GraphQL
  * @summary get system information for reaction site
  * @param {ConnectionArgs} args An object of all arguments that were sent by the client
- * @param {String} args.shopId The ID of the shop to load navigation items for
  * @param {Object} context An object containing the per-request state
  * @return {<Object>} System Information Object
  */
 export default async function systemInformation(_, args, context) {
-  return context.queries.systemInformation(context);
+  const dbShopId = decodeShopOpaqueId(args.shopId);
+  return context.queries.systemInformation(dbShopId, context);
 }
