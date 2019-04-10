93c4fce45a8eb4bf52f6fe169b935b4a338436fa
Eric Dobbertin <eric@dairystatedesigns.com>
2019-04-10 11:08:49 -0500

    chore: graphql schema linter config fixes
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

---
+++ .reaction/graphql-linter-rules/type_fields_sorted_alphabetically.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.TypeFieldsSortedAlphabetically = TypeFieldsSortedAlphabetically;
+
+var _validation_error = require('graphql-schema-linter/lib/validation_error');
+
+function TypeFieldsSortedAlphabetically(context) {
+  return {
+    ObjectTypeDefinition: function ObjectTypeDefinition(node) {
+      const fieldList = (node.fields || []).map((field) => field.name.value);
+
+      if (!arraysEqual(fieldList, fieldList.slice().sort())) {
+        context.reportError(
+          new _validation_error.ValidationError(
+            'type-fields-sorted-alphabetically',
+            'The fields of object type `' +
+              node.name.value +
+              '` must be sorted alphabetically.',
+            [node]
+          )
+        );
+      }
+    }
+  };
+}
+
+function arraysEqual(a, b) {
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
