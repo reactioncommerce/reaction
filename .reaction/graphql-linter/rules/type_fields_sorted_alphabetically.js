b8f50215b7e516ac91a18b0fa6b99f3cab603394
Eric Dobbertin <eric@dairystatedesigns.com>
2019-04-10 14:27:59 -0500

    refactor: dry graphql lint custom rules
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

---
+++ .reaction/graphql-linter/rules/type_fields_sorted_alphabetically.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.TypeFieldsSortedAlphabetically = TypeFieldsSortedAlphabetically;
+
+var _validation_error = require("graphql-schema-linter/lib/validation_error");
+const { listIsAlphabetical } = require("../util/listIsAlphabetical");
+
+function TypeFieldsSortedAlphabetically(context) {
+  return {
+    ObjectTypeDefinition: function ObjectTypeDefinition(node) {
+      const fieldList = (node.fields || []).map((field) => field.name.value);
+      const sortedFieldList = listIsAlphabetical(fieldList);
+
+      if (sortedFieldList !== true) {
+        context.reportError(
+          new _validation_error.ValidationError(
+            "type-fields-sorted-alphabetically",
+            "The fields of object type `" +
+              node.name.value +
+              "` must be sorted alphabetically." +
+              " Expected sorting: " +
+              sortedFieldList.join(", "),
+            [node]
+          )
+        );
+      }
+    }
+  };
+}
+
+
---
+++ .reaction/graphql-linter/util/listIsAlphabetical.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.listIsAlphabetical = listIsAlphabetical;
+
+/**
+ * @summary Returns `true` if two arrays have the same item values in the same order.
+ */
+function arraysEqual(a, b) {
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+/**
+ * @summary Returns `true` if the list is in alphabetical order,
+ *   or an alphabetized list if not
+ * @param {String[]} list Array of strings
+ */
+function listIsAlphabetical(list) {
+  const sortedList = list.slice().sort();
+  if (arraysEqual(list, sortedList)) return true;
+  return sortedList;
+}
