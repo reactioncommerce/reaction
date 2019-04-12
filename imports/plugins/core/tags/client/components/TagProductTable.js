6374cc79d3b3f676d35abace30970f7a07335145
Mike Murray <hellomikemurray@gmail.com>
2019-04-12 15:06:46 -0700

    feat: update to support offset (page) based pagination
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/tags/client/components/TagProductTable.js
+++ imports/plugins/core/tags/client/components/TagProductTable.js
@@ -3,17 +3,14 @@ import PropTypes from "prop-types";
 import { Query } from "react-apollo";
 import { i18next } from "/client/api";
 import TextInput from "@reactioncommerce/components/TextInput/v1";
-import Button from "@material-ui/core/Button";
-import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
-import ChevronRightIcon from "mdi-material-ui/ChevronRight";
-import ChevronLeftIcon from "mdi-material-ui/ChevronLeft";
 import { tagProductsQuery } from "../../lib/queries";
-import { pagination } from "./util/pagination";
+import paginationByPageNumber from "/imports/utils/paginationByPageNumber";
 
 class TagProductTable extends Component {
   static propTypes = {
@@ -27,19 +24,25 @@ class TagProductTable extends Component {
     onProductPriorityChange() {}
   }
 
+  state = {
+    page: 0,
+    limit: 10
+  }
+
   handlePriorityChange = (productId, priority) => {
     this.props.onProductPriorityChange(productId, priority);
   }
 
   render() {
     const { tagId, shopId } = this.props;
+    const { limit, page } = this.state;
 
     if (!tagId) {
       return null;
     }
 
     return (
-      <Query query={tagProductsQuery} variables={{ tagId, shopId, first: 20 }} fetchPolicy="network-only">
+      <Query query={tagProductsQuery} variables={{ tagId, shopId, limit }} fetchPolicy="network-only">
         {({ data, fetchMore }) => {
           const productsByTagId = data && data.productsByTagId;
           let products;
@@ -47,14 +50,12 @@ class TagProductTable extends Component {
             products = productsByTagId.nodes;
           }
 
-          const pageInfo = pagination({
+          const { totalCount, loadPage } = paginationByPageNumber({
             fetchMore,
             data,
             queryName: "productsByTagId",
-            limit: 20
+            limit
           });
-
-          const { hasNextPage, hasPreviousPage, loadNextPage, loadPreviousPage } = pageInfo;
 
           return (
             <Fragment>
@@ -87,24 +88,26 @@ class TagProductTable extends Component {
                   })}
                 </TableBody>
               </Table>
-              <ExpansionPanelActions>
-                <Button
-                  color="primary"
-                  disabled={!hasPreviousPage}
-                  onClick={loadPreviousPage}
-                >
-                  <ChevronLeftIcon />
-                  {i18next.t("admin.routing.tableText.previousText")}
-                </Button>
-                <Button
-                  color="primary"
-                  disabled={!hasNextPage}
-                  onClick={loadNextPage}
-                >
-                  {i18next.t("admin.routing.tableText.nextText")}
-                  <ChevronRightIcon />
-                </Button>
-              </ExpansionPanelActions>
+              <TablePagination
+                count={totalCount}
+                component="div"
+                page={page}
+                rowsPerPage={limit}
+                rowsPerPageOptions={[10, 25, 50, 100]}
+                backIconButtonProps={{
+                  "aria-label": "Previous Page"
+                }}
+                nextIconButtonProps={{
+                  "aria-label": "Next Page"
+                }}
+                onChangePage={(event, index) => {
+                  this.setState({ page: index });
+                  loadPage(index);
+                }}
+                onChangeRowsPerPage={(event) => {
+                  this.setState({ limit: event.target.value });
+                }}
+              />
             </Fragment>
           );
         }}
