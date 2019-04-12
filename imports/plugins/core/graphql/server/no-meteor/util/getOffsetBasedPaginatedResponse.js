c8881c61a4470692e6e57a9b52d42db9e46b4c8a
Mike Murray <hellomikemurray@gmail.com>
2019-04-12 12:08:19 -0700

    feat: add offset-based pagination util for mongo cursors
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/plugins/core/graphql/server/no-meteor/util/getOffsetBasedPaginatedResponse.js
@@ -0,0 +1,46 @@
+/**
+ * @name getOffsetBasedPaginatedResponse
+ * @method
+ * @memberof GraphQL/ResolverUtilities
+ * @summary Given a MongoDB cursor, adds skip, limit, sort, and other filters as necessary
+ *   based on GraphQL resolver arguments.
+ * @param {Cursor} mongoCursor Node MongoDB Cursor instance. Will be mutated.
+ * @param {Object} args Connection arguments from GraphQL query
+ * @return {Promise<Object>} `{ nodes, pageInfo, totalCount }`
+ */
+export default async function getOffsetBasedPaginatedResponse(mongoCursor, args) {
+  const { sortOrder } = args;
+  const totalCount = await mongoCursor.clone().count();
+  const nodes = await mongoCursor.toArray();
+  const limit = args.limit || 20;
+  const page = args.page || 0;
+  const skip = page * limit;
+  let sortBy;
+
+  if (sortBy === "minPrice") {
+    if (typeof args.sortByPriceCurrencyCode !== "string") {
+      throw new Error("sortByPriceCurrencyCode is required when sorting by minPrice");
+    }
+    sortBy = `product.pricing.${args.sortByPriceCurrencyCode}.minPrice`;
+  } else {
+    sortBy = args.sortBy; // eslint-disable-line prefer-destructuring
+  }
+
+  mongoCursor
+    .limit(limit)
+    .skip(skip)
+    .sort({
+      [sortBy]: sortOrder === "desc" ? -1 : 1
+    });
+
+  return {
+    nodes,
+    pageInfo: {
+      hasNextPage: (page + 1) * limit < totalCount,
+      hasPreviousPage: page > 0 && totalCount,
+      startCursor: (nodes.length && nodes[0]._id) || null,
+      endCursor: (nodes.length && nodes[nodes.length - 1]._id) || null
+    },
+    totalCount
+  };
+}
