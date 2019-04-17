19964c9bf99522470d97a6549f64c3a5b296c452
Mike Murray <hellomikemurray@gmail.com>
2019-04-16 17:00:48 -0700

    fix: make tags admin only and limit to only the requested tag ids
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/core/server/publications/collections/tags.js
+++ imports/plugins/core/core/server/publications/collections/tags.js
@@ -1,24 +1,39 @@
 import { Meteor } from "meteor/meteor";
+import { check, Match } from "meteor/check";
 import { Tags } from "/lib/collections";
 import Reaction from "/imports/plugins/core/core/server/Reaction";
 
 /**
  * tags
  */
-Meteor.publish("Tags", function () {
-  const selector = {};
+Meteor.publish("Tags", function (tagIds) {
+  check(tagIds, Match.OneOf(undefined, Array));
 
   const shopId = Reaction.getShopId();
+
+  // Only let users what have createProduct permissions see the tags
+  if (!Reaction.hasPermission(["createProduct"], this.userId)) {
+    return this.ready();
+  }
+
+  // Prevent subscribing to all tags
+  if (!Array.isArray(tagIds)) {
+    return this.ready();
+  }
 
   if (!shopId) {
     return this.ready();
   }
 
+  const selector = {
+    _id: {
+      $in: tagIds
+    }
+  };
+
   if (!Reaction.isShopPrimary()) {
     selector.shopId = shopId;
   }
 
-  // TODO: filter tag results based on permissions and isVisible or some other
-  // publication quality
   return Tags.find(selector);
 });
