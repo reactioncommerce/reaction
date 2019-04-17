182b5ea0d30944c580a7ec4672ba1872c79ed228
Erik Kieckhafer <ek@ato.la>
2019-04-17 14:04:02 -0700

    remove old indexes, fix existing ones
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/startup.js
+++ imports/plugins/core/orders/server/no-meteor/startup.js
@@ -18,7 +18,6 @@ export default function startup(context) {
   collectionIndex(Orders, { referenceId: 1 }, { unique: true });
   collectionIndex(Orders, { sessionId: 1 }, { name: "c2_sessionId" });
   collectionIndex(Orders, { shopId: 1 }, { name: "c2_shopId" });
-  collectionIndex(Orders, { "items.productId": 1 }, { name: "c2_items.$.productId" });
   collectionIndex(Orders, { "items.shopId": 1 }, { name: "c2_items.$.shopId" });
   collectionIndex(Orders, { "items.product.ancestors": 1 }, { name: "c2_items.$.product.ancestors" });
   collectionIndex(Orders, { "items.product.shopId": 1 }, { name: "c2_items.$.product.shopId" });
@@ -28,7 +27,6 @@ export default function startup(context) {
   collectionIndex(Orders, { "items.product.isVisible": 1 }, { name: "c2_items.$.product.isVisible" });
   collectionIndex(Orders, { "items.product.createdAt": 1 }, { name: "c2_items.$.product.createdAt" });
   collectionIndex(Orders, { "items.product.workflow.status": 1 }, { name: "c2_items.$.product.workflow.status" });
-  collectionIndex(Orders, { "items.variantId": 1 }, { name: "c2_items.$.variantId" });
   collectionIndex(Orders, { "items.variants.isVisible": 1 }, { name: "c2_items.$.variants.isVisible" });
   collectionIndex(Orders, { "items.variants.isDeleted": 1 }, { name: "c2_items.$.variants.isDeleted" });
   collectionIndex(Orders, { "items.variants.shopId": 1 }, { name: "c2_items.$.variants.shopId" });
@@ -37,9 +35,9 @@ export default function startup(context) {
   collectionIndex(Orders, { "billing.paymentMethod.workflow.status": 1 }, { name: "c2_billing.$.paymentMethod.workflow.status" });
   collectionIndex(Orders, { "billing.paymentMethod.items.productId": 1 }, { name: "c2_billing.$.paymentMethod.items.$.productId" });
   collectionIndex(Orders, { "billing.paymentMethod.items.shopId": 1 }, { name: "c2_billing.$.paymentMethod.items.$.shopId" });
-  collectionIndex(Orders, { "shipping.items.productId": 1 }, { name: "c2_shipping.$.items.$.productId" });
+  collectionIndex(Orders, { "shipping.items.productId": 1 });
   collectionIndex(Orders, { "shipping.items.shopId": 1 }, { name: "c2_shipping.$.items.$.shopId" });
-  collectionIndex(Orders, { "shipping.items.variantId": 1 }, { name: "c2_shipping.$.items.$.variantId" });
+  collectionIndex(Orders, { "shipping.items.variantId": 1 });
   collectionIndex(Orders, { "shipping.items.workflow.status": 1 }, { name: "c2_shipping.$.items.$.workflow.status" });
   collectionIndex(Orders, { "shipping.workflow.status": 1 }, { name: "c2_shipping.$.workflow.status" });
   collectionIndex(Orders, { "workflow.status": 1 }, { name: "c2_workflow.status" });
