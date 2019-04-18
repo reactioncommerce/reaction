803b78330368110b730f6d85b99d015f9669ea66
Will Lopez <will.lopez77@gmail.com>
2019-04-18 14:12:20 -0700

    refactor: covert filters to boolean types
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

--- imports/plugins/core/catalog/server/no-meteor/utils/catalogFilters.js
+++ imports/plugins/core/catalog/server/no-meteor/utils/catalogFilters.js
@@ -1,39 +1,16 @@
-import ReactionError from "@reactioncommerce/reaction-error";
-
-const ALLOWED_FILTERS = [
-  "isDeleted",
-  "isVisible",
-  "isLowQuantity",
-  "isSoldOut",
-  "isBackorder"
-];
-
 /**
  *
  * @method xformCatalogFilters
- * @summary Transforms JSON encoded filters for the Catalog collection into
- * Mongo expressions.
+ * @summary Transforms a boolean filters array into a filters object.
  * @param {Object[]} filters - Array of JSON encoded filters
  * @return {Object[]} Array Mongo filter expressions
  */
 export default function xformCatalogFilters(filters) {
   const mongoFilters = {};
   for (let index = 0; index < filters.length; index += 1) {
-    const { name: filterName, value } = filters[index];
+    const { name, value } = filters[index];
 
-    // if this filter is not allowed, skip.
-    if (!ALLOWED_FILTERS.includes(filterName)) {
-      continue;
-    }
-
-    let filterValue;
-    try {
-      filterValue = JSON.parse(value);
-    } catch (error) {
-      throw new ReactionError("invalid-catalog-filter-value", `The value for Catalog filter: ${filterName} could no be parsed`);
-    }
-
-    mongoFilters[`product.${filterName}`] = filterValue;
+    mongoFilters[`product.${name}`] = value;
   }
 
   return mongoFilters;
