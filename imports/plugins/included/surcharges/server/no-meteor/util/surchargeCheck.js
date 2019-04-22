92f552549c14662f172fbe85680a2ebeb95a1650
Erik Kieckhafer <ek@ato.la>
2019-04-22 15:20:44 -0700

    add pricing engine data to surcharges
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/surcharges/server/no-meteor/util/surchargeCheck.js
+++ imports/plugins/included/surcharges/server/no-meteor/util/surchargeCheck.js
@@ -1,26 +1,38 @@
 import operators from "/imports/utils/operators";
 import propertyTypes from "/imports/utils/propertyTypes";
+import getVariantPrice from "/imports/plugins/custom/reaction-plugin-pricing-engine/server/no-meteor/queries/getVariantPrice";
 
 /**
  * @summary Filter surcharges based on surcharge restriction data
+ * @param {Object} context - Context
  * @param {Object} surcharge - surcharge to check attributes and destination against
  * @param {Object} extendCommonOrder - details about the purchase a user wants to make.
  * @returns {Bool} true / false as to whether method is still valid after this check
  */
-export async function surchargeCheck(surcharge, extendCommonOrder) {
+export async function surchargeCheck(context, surcharge, extendCommonOrder) {
   const { items, shippingAddress } = extendCommonOrder;
   const { attributes, destination } = surcharge;
 
   const validSurcharge = items.some((item) => { // eslint-disable-line
     if (Array.isArray(attributes) && attributes.length) {
       // Item must meet all attributes to be restricted
-      return attributes.every((attribute) => {
-        let attributeFound = operators[attribute.operator](item[attribute.property], propertyTypes[attribute.propertyType](attribute.value));
+      return attributes.every(async (attribute) => {
+        let attributeValue = attribute.value;
+        // If `prices` object exists, this means we are using the pricing engine,
+        // instead of simple `price` field on a Product document.
+        // We need to find the correct price from the engine to check against.
+        if (attribute.property === "price" && item.prices && Array.isArray(item.prices) && item.prices.length) {
+          const variantPrice = await getVariantPrice(context, item);
+          const { price } = variantPrice;
+          attributeValue = price;
+        }
+
+        let attributeFound = operators[attribute.operator](item[attribute.property], propertyTypes[attribute.propertyType](attributeValue));
 
         // If attribute is an array on the item, use `includes` instead of checking for ===
         // This works for tags, tagIds, and any future attribute that might be an array
         if (Array.isArray(item[attribute.property])) {
-          attributeFound = item[attribute.property].includes(attribute.value);
+          attributeFound = item[attribute.property].includes(attributeValue);
         }
 
         if (attributeFound && shippingAddress) {
