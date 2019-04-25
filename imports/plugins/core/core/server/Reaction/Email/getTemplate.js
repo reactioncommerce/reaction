82562cfbca409cbd4afc3ef34852fa11a1c03c8c
Jaka Maver <Jaka.Maver@comtrade.com>
2019-04-25 14:23:29 +0200

    (feat): update getTemplate to accept language as optional second parameter
    Signed-off-by: Jaka Maver <Jaka.Maver@comtrade.com>

--- imports/plugins/core/core/server/Reaction/Email/getTemplate.js
+++ imports/plugins/core/core/server/Reaction/Email/getTemplate.js
@@ -10,17 +10,21 @@ import getTemplateFile from "./getTemplateFile";
  * @summary Returns a template source for SSR consumption. layout must be defined + template
  * @example Reaction.Email.getTemplate('path/of/template');
  * @param {String} template name of the template in either Layouts or fs
+ * @param {String} [language=Reaction.getShopLanguage()] language of a template
  * @returns {Object} returns source
  */
-export default function getTemplate(template) {
+export default function getTemplate(template, language = Reaction.getShopLanguage()) {
   if (typeof template !== "string") {
     const msg = "Reaction.Email.getTemplate() requires a template name";
     Logger.error(msg);
     throw new ReactionError("invalid-parameter", msg);
   }
 
-  // set default
-  const language = Reaction.getShopLanguage();
+  if (typeof language !== "string") {
+    const msg = "Reaction.Email.getTemplate() requires optional language code that is a string";
+    Logger.error(msg);
+    throw new ReactionError("invalid-parameter", msg);
+  }
 
   // check database for a matching template
   const tmpl = Templates.findOne({
