2d38ecdb5eee34536c6af78a133ece8b7bda91c3
Erik Kieckhafer <ek@ato.la>
2019-04-25 13:10:56 -0700

    add query to get pricing from Catalog
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/cart/server/no-meteor/util/xformCartGroupToCommonOrder.js
+++ imports/plugins/core/cart/server/no-meteor/util/xformCartGroupToCommonOrder.js
@@ -13,28 +13,39 @@ export default async function xformCartGroupToCommonOrder(cart, group, context) 
   let items = group.itemIds.map((itemId) => cart.items.find((item) => item._id === itemId));
   items = items.filter((item) => !!item); // remove nulls
 
+  // Get productId's from items, and then use ID's to get full Catalog product
+  // to provide missing pricing information
+  const catalogItemIds = items.map((item) => item.productId);
+  const catalogItemsInGroup = await collections.Catalog.find({ "product._id": { $in: catalogItemIds } }).toArray();
+
   // We also need to add `subtotal` on each item, based on the current price of that item in
   // the catalog. `getFulfillmentGroupTaxes` uses subtotal prop to calculate the tax.
   // ** If you add any data here, be sure to add the same data to the matching xformOrderGroupToCommonOrder xform
-  items = items.map((item) => ({
-    _id: item._id,
-    attributes: item.attributes,
-    isTaxable: item.isTaxable,
-    parcel: item.parcel,
-    price: item.price,
-    productId: item.productId,
-    productVendor: item.productVendor,
-    quantity: item.quantity,
-    shopId: item.shopId,
-    subtotal: {
-      amount: +toFixed(item.price.amount * item.quantity, 3),
-      currencyCode
-    },
-    taxCode: item.taxCode,
-    title: item.title,
-    variantId: item.variantId,
-    variantTitle: item.variantTitle
-  }));
+  items = items.map(async (item) => {
+    // Get the catalog version of the item to get pricing data from it
+    const catalogItem = catalogItemsInGroup.find((catalogItemProduct) => catalogItemProduct.product._id === item.productId);
+    const currentVariantPrice = await context.queries.getVariantPrice(context, catalogItem.product, "USD");
+
+    return {
+      _id: item._id,
+      attributes: item.attributes,
+      isTaxable: item.isTaxable,
+      parcel: item.parcel,
+      price: currentVariantPrice.price,
+      productId: item.productId,
+      productVendor: item.productVendor,
+      quantity: item.quantity,
+      shopId: item.shopId,
+      subtotal: {
+        amount: +toFixed(item.price.amount * item.quantity, 3),
+        currencyCode
+      },
+      taxCode: item.taxCode,
+      title: item.title,
+      variantId: item.variantId,
+      variantTitle: item.variantTitle
+    };
+  });
 
   const { address, shipmentMethod, shopId, type: fulfillmentType } = group;
   const shop = await collections.Shops.findOne({ _id: shopId });
