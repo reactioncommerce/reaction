01577364ed737eb49441b68856def70d599faa9c
Jaka Maver <Jaka.Maver@comtrade.com>
2019-04-26 09:01:49 +0200

    (feat): update functions to properly handle optional language param
    Signed-off-by: Jaka Maver <Jaka.Maver@comtrade.com>

--- imports/plugins/core/core/server/Reaction/Email/getSubject.js
+++ imports/plugins/core/core/server/Reaction/Email/getSubject.js
@@ -10,26 +10,39 @@ import ReactionError from "@reactioncommerce/reaction-error";
  * layout must be defined + template
  * @example SSR.compileTemplate(subject, Reaction.Email.getSubject(tpl));
  * @param {String} template name of the template in either Layouts or fs
- * @param {String} [language=Reaction.getShopLanguage()] language of a template
+ * @param {String} [language] i18n language of a template
  * @returns {Object} returns source
  */
-export default function getSubject(template, language = Reaction.getShopLanguage()) {
+export default function getSubject(template, language) {
   if (typeof template !== "string") {
     const msg = "Reaction.Email.getSubject() requires a template name";
     Logger.error(msg);
     throw new ReactionError("invalid-parameter", msg);
   }
 
-  if (typeof language !== "string") {
+  if (language !== undefined && typeof language !== "string") {
     const msg = "Reaction.Email.getSubject() requires optional language code that is a string";
     Logger.error(msg);
     throw new ReactionError("invalid-parameter", msg);
   }
 
-  // check database for a matching template
+  if (language !== undefined) {
+    // check database for a matching template using language param
+    const tmpl = Templates.findOne({
+      name: template,
+      language: language
+    });
+
+    // use that template if found
+    if (tmpl && tmpl.template) {
+      return tmpl.template;
+    }
+  }
+
+  // check database for a matching template using shop language
   const tmpl = Templates.findOne({
     name: template,
-    language
+    language: Reaction.getShopLanguage()
   });
 
   // use that template if found
--- imports/plugins/core/core/server/Reaction/Email/getTemplate.js
+++ imports/plugins/core/core/server/Reaction/Email/getTemplate.js
@@ -10,26 +10,39 @@ import getTemplateFile from "./getTemplateFile";
  * @summary Returns a template source for SSR consumption. layout must be defined + template
  * @example Reaction.Email.getTemplate('path/of/template');
  * @param {String} template name of the template in either Layouts or fs
- * @param {String} [language=Reaction.getShopLanguage()] language of a template
+ * @param {String} [language] i18n language of a template
  * @returns {Object} returns source
  */
-export default function getTemplate(template, language = Reaction.getShopLanguage()) {
+export default function getTemplate(template, language) {
   if (typeof template !== "string") {
     const msg = "Reaction.Email.getTemplate() requires a template name";
     Logger.error(msg);
     throw new ReactionError("invalid-parameter", msg);
   }
 
-  if (typeof language !== "string") {
+  if (language !== undefined && typeof language !== "string") {
     const msg = "Reaction.Email.getTemplate() requires optional language code that is a string";
     Logger.error(msg);
     throw new ReactionError("invalid-parameter", msg);
   }
 
-  // check database for a matching template
+  if (language !== undefined) {
+    // check database for a matching template using language param
+    const tmpl = Templates.findOne({
+      name: template,
+      language: language
+    });
+
+    // use that template if found
+    if (tmpl && tmpl.template) {
+      return tmpl.template;
+    }
+  }
+
+  // check database for a matching template using shop language
   const tmpl = Templates.findOne({
     name: template,
-    language
+    language: Reaction.getShopLanguage()
   });
 
   // use that template if found
