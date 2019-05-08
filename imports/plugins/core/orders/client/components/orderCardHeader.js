9192c937d4394b1b3b185f967f79388108b217c8
Erik Kieckhafer <ek@ato.la>
2019-05-08 11:52:02 -0700

    add styling to OrderCardHeader
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/orderCardHeader.js
+++ imports/plugins/core/orders/client/components/orderCardHeader.js
@@ -1,58 +1,26 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Cancel from "mdi-material-ui/Cancel";
-import Email from "mdi-material-ui/Email";
-import Account from "mdi-material-ui/Account";
-import Phone from "mdi-material-ui/Phone";
-import { Mutation } from "react-apollo";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Address from "@reactioncommerce/components/Address/v1";
 import { withMoment } from "@reactioncommerce/reaction-components";
 import { ClickToCopy } from "@reactioncommerce/reaction-ui";
 import { i18next, Reaction } from "/client/api";
-import cancelOrderItemMutation from "../graphql/mutations/cancelOrderItem";
 import OrderCardStatusChip from "./orderCardStatusChip";
 
+
 const styles = (theme) => ({
-  orderCardTitleInfo: {
-    borderTop: `solid 1px ${theme.palette.colors.black10}`,
-    marginTop: theme.spacing.unit * 2.5,
-    paddingTop: theme.spacing.unit * 2.5
-  },
-
-
-  displayFlexForIcon: {
-    display: "flex"
-  },
-  leftIcon: {
-    marginRight: theme.spacing.unit
-  },
-  paddingForIcon: {
-    marginRight: theme.spacing.unit
-  },
-  orderCardDetailsHeader: {
-    background: theme.palette.colors.coolGrey
-  },
   orderCardInfoTextBold: {
     fontWeight: theme.typography.fontWeightBold
   },
-  orderCardDivider: {
-    borderTop: `solid 1px ${theme.palette.colors.black10}`,
-    marginTop: theme.spacing.unit * 2.5,
-    paddingTop: theme.spacing.unit * 2.5
-  },
-  orderCardSection: {
-    marginBottom: theme.spacing.unit * 3
-  },
-  orderCardReferenceIdLink: {
-    pointer: "cursor"
+  printButton: {
+    flex: 1,
+    justifyContent: "flex-end",
+    display: "flex"
   }
 });
 
@@ -71,43 +39,19 @@ class OrderCardHeader extends Component {
     })
   };
 
-  handleCancelOrder(mutation) {
-    Alerts.alert({
-      title: i18next.t("order.cancelOrder"),
-      type: "warning",
-      showCancelButton: true
-    }, async (isConfirm) => {
-      if (isConfirm) {
-        const { order } = this.props;
-        const { fulfillmentGroups } = order;
-
-        // We need to loop over every fulfillmentGroup
-        // and then loop over every item inside group
-        fulfillmentGroups.forEach(async (fulfillmentGroup) => {
-          // for (const item of fulfillmentGroup.items.nodes) {
-          fulfillmentGroup.items.nodes.forEach(async (item) => {
-            console.log(" --- item to cancel", item);
-
-            await mutation({
-              variables: {
-                input: {
-                  cancelQuantity: item.quantity,
-                  itemId: item._id,
-                  orderId: order._id,
-                  reason: "Order cancelled inside Catalyst operator UI"
-                }
-              }
-            });
-          });
-        });
-      }
-    });
-  }
-
-
   orderLink() {
     const { order: { referenceId } } = this.props;
     return `${window.location.origin}/operator/orders/${referenceId}`;
+  }
+
+  printLink() {
+    const { order } = this.props;
+
+    return Reaction.Router.pathFor("dashboard/pdf/orders", {
+      hash: {
+        id: order.referenceId
+      }
+    });
   }
 
   renderOrderPayments() {
@@ -132,31 +76,71 @@ class OrderCardHeader extends Component {
     return null;
   }
 
+  renderPaymentStatusChip() {
+    const { order } = this.props;
+    const { payments } = order;
+    const paymentStatuses = payments.map((payment) => payment.status);
+    const uniqueStatuses = [...new Set(paymentStatuses)];
+
+    // If all payment statuses are equal, and also not "new", then show a single badge
+    if (Array.isArray(uniqueStatuses) && uniqueStatuses.length === 1) {
+      const [paymentStatus] = uniqueStatuses;
+
+      // No action has been taken on any payments, no need to show a status badge
+      // since we show the button to take action
+      if (paymentStatus === "created") {
+        return null;
+      }
+
+      // An action has been taken on payment, and all payment statuses are the same
+      // show a single badge with the status of all payments
+      return (
+        <Grid item>
+          <OrderCardStatusChip displayStatus={paymentStatus} status={paymentStatus} type="payment" />
+        </Grid>
+      );
+    }
+
+    // Payment statuses aren't equal, show an error badge here
+    // and show badges next to payments to represent their status
+    return (
+      <Grid item>
+        <OrderCardStatusChip displayStatus="multiple" status="multiple" type="payment" />
+      </Grid>
+    );
+  }
+
   render() {
     const { classes, moment, order } = this.props;
     const { createdAt, displayStatus, email, fulfillmentGroups, payments, referenceId, status } = order;
     const { shippingAddress } = fulfillmentGroups[0].data;
     const orderDate = (moment && moment(createdAt).format("MM/DD/YYYY")) || createdAt.toLocaleString();
 
-    const printOrder = Reaction.Router.pathFor("dashboard/pdf/orders", {
-      hash: {
-        id: order.referenceId
-      }
-    });
-
     return (
-      <Fragment>
+      <Grid container spacing={32}>
         <Grid item xs={12}>
-          <Grid item xs={12} md={8}>
-            <Typography variant="body2" className={classes.orderCardInfoTextBold} inline={true}>Order - {referenceId} | </Typography>
-            <Typography variant="body2" inline={true}>{orderDate}</Typography>
-            <OrderCardStatusChip displayStatus={displayStatus} status={status} />
-            <OrderCardStatusChip displayStatus={displayStatus} status={status} />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <a href={printOrder} target="_blank">
-              <Button variant="outlined">Print invoice</Button>
-            </a>
+          <Grid container alignItems="center" spacing={8}>
+            <Grid item>
+              <Typography variant="body2" className={classes.orderCardInfoTextBold} inline={true}>
+                Order -
+                <ClickToCopy
+                  copyToClipboard={this.orderLink()}
+                  displayText={referenceId}
+                  i18nKeyTooltip="admin.orderWorkflow.summary.copyOrderLink"
+                  tooltip="Copy Order Link"
+                />
+              </Typography>
+              <Typography variant="body2" inline={true}> | {orderDate}</Typography>
+            </Grid>
+            <Grid item>
+              <OrderCardStatusChip displayStatus={displayStatus} status={status} type="order" />
+            </Grid>
+            {this.renderPaymentStatusChip()}
+            <Grid item className={classes.printButton}>
+              <a href={this.printLink()} target="_blank">
+                <Button size="small" variant="outlined">{i18next.t("admin.invoice.printInvoice", "Print invoice")}</Button>
+              </a>
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12}>
@@ -204,7 +188,7 @@ class OrderCardHeader extends Component {
             </Grid>
           </Grid>
         </Grid>
-      </Fragment>
+      </Grid>
     );
   }
 }
