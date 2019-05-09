2c45851203becaf57b7e284996757da9df0da9d8
Erik Kieckhafer <ek@ato.la>
2019-05-09 12:38:51 -0700

    add withPrimaryShop HOC
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ imports/plugins/core/graphql/lib/hocs/withPrimaryShop.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Query } from "react-apollo";
+import getPrimaryShop from "../queries/getPrimaryShop";
+import getPrimaryShopId from "../queries/getPrimaryShopId";
+
+
+export default (Component) => (
+  class PrimaryShop extends React.Component {
+    static propTypes = {
+      shouldSkipGraphql: PropTypes.bool // Whether to skip this HOC's GraphQL query & data
+    };
+
+    render() {
+      const { shouldSkipGraphql } = this.props;
+
+      if (shouldSkipGraphql) {
+        return (
+          <Component {...this.props} />
+        );
+      }
+
+      return (
+        <Query query={getPrimaryShopId}>
+          {({ loading, data }) => {
+            const props = {
+              ...this.props,
+              isLoadingPrimaryShopId: loading
+            };
+
+            if (loading === false) {
+              const { primaryShopId } = data;
+              props.shopId = primaryShopId;
+            }
+
+            return (
+              <Query query={getPrimaryShop} variables={{ id: props.shopId }}>
+                {({ loading: shopLoading, data: shopData }) => {
+                  if (shopLoading) return null;
+
+                  const { shop } = shopData;
+
+                  return (
+                    <Component shop={shop} />
+                  );
+                }}
+              </Query>
+            );
+          }}
+        </Query>
+      );
+    }
+  }
+);
