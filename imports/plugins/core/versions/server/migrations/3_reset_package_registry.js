86b089834c9a8e02399752a28380775b54ad3c17
Eric Dobbertin <eric@dairystatedesigns.com>
2019-05-13 06:55:46 -0700

    fix: disable migration 3
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/versions/server/migrations/3_reset_package_registry.js
+++ imports/plugins/core/versions/server/migrations/3_reset_package_registry.js
@@ -1,16 +1,20 @@
 import { Migrations } from "meteor/percolate:migrations";
-import { Packages } from "/lib/collections";
+// import { Packages } from "/lib/collections";
 
 Migrations.add({
   version: 3,
   up() {
-    Packages.update(
-      {}, {
-        $set: {
-          registry: []
-        }
-      },
-      { bypassCollection2: true, multi: true }
-    );
+    // ED 5-13-2019 This migration is now running after packages load and
+    // wiping out registry on a fresh installation. This may have been
+    // correct at one point but I don't think it is anymore. Commenting out.
+
+    // Packages.update(
+    //   {}, {
+    //     $set: {
+    //       registry: []
+    //     }
+    //   },
+    //   { bypassCollection2: true, multi: true }
+    // );
   }
 });
