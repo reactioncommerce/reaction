a7c330ec2ba6cfc1a8d46fb4c2c096c90e50593c
Eric Dobbertin <eric@dairystatedesigns.com>
2019-05-22 09:56:13 -0500

    feat: UI for canSellVariantWithoutInventory app setting
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

---
+++ imports/plugins/core/inventory/client/index.js
@@ -0,0 +1,15 @@
+import { registerBlock } from "/imports/plugins/core/components/lib";
+import InventorySettings from "./InventorySettings";
+import withInventoryShopSettings from "./withInventoryShopSettings";
+import withUpdateInventoryShopSettings from "./withUpdateInventoryShopSettings";
+
+registerBlock({
+  region: "ShopSettings",
+  name: "InventorySettings",
+  component: InventorySettings,
+  hocs: [
+    withInventoryShopSettings,
+    withUpdateInventoryShopSettings
+  ],
+  priority: 1
+});
