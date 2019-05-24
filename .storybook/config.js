7ef3d2f333cdd5fae457f9100455fe898735c191
Mike Murray <hellomikemurray@gmail.com>
2019-05-24 10:38:46 -0700

    feat: add global theme decorators

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,23 @@
-import { configure } from "@storybook/react";
+import React from "react";
+import { addDecorator, configure } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import theme from "../imports/plugins/core/router/client/theme";
+import muiTheme from "../imports/plugins/core/router/client/theme/muiTheme.js";
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../imports', true, /\.stories\.js$/);
+const req = require.context("../imports", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// You can add decorator globally:
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <MuiThemeProvider theme={muiTheme}>
+      {story()}
+    </MuiThemeProvider>
+  </ThemeProvider>
+));
 
 configure(loadStories, module);
