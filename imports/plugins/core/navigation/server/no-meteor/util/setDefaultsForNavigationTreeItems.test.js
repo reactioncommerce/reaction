f3358f100f5cf5c89c908d5087520aa00ff68169
Mike Murray <hellomikemurray@gmail.com>
2019-05-29 14:10:11 -0700

    test: update tests to use default values for navigation tree items
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/navigation/server/no-meteor/util/setDefaultsForNavigationTreeItems.test.js
+++ imports/plugins/core/navigation/server/no-meteor/util/setDefaultsForNavigationTreeItems.test.js
@@ -40,7 +40,41 @@ const mockNavigationTreeItemsResult = [
   }
 ];
 
-test("filters navigation tree excluding secondary items", async () => {
-  const results = setDefaultsForNavigationTreeItems(mockNavigationTreeItemsInput);
+const mockNavigationTreeItemsResult2 = [
+  {
+    isPrivate: false,
+    isSecondary: false,
+    isVisible: false,
+    navigationItemId: "cmVhY3Rpb24vbmF2aWdhdGlvbkl0ZW06dWFYWGF3YzVveHk5ZVI0aFA=",
+    items: [
+      {
+        isPrivate: true,
+        isSecondary: false,
+        isVisible: false,
+        navigationItemId: "cmVhY3Rpb24vbmF2aWdhdGlvbkl0ZW06dEFLQVRRZXFvRDRBaDVnZzI="
+      }
+    ]
+  },
+  {
+    isPrivate: false,
+    isSecondary: true,
+    isVisible: false,
+    navigationItemId: "cmVhY3Rpb24vbmF2aWdhdGlvbkl0ZW06S0VjbjZOdnJSdXp0bVBNcTg="
+  }
+];
+
+test("filters navigation tree excluding secondary items, with supplied defaults values", async () => {
+  const defaultValues = {
+    shouldNavigationTreeItemsBeAdminOnly: false,
+    shouldNavigationTreeItemsBePublicallyVisible: true,
+    shouldNavigationTreeItemsBeSecondaryNavOnly: false
+  };
+
+  const results = setDefaultsForNavigationTreeItems(mockNavigationTreeItemsInput, defaultValues);
   expect(results).toEqual(mockNavigationTreeItemsResult);
+});
+
+test("filters navigation tree excluding secondary items, without supplied default values", async () => {
+  const results = setDefaultsForNavigationTreeItems(mockNavigationTreeItemsInput);
+  expect(results).toEqual(mockNavigationTreeItemsResult2);
 });
