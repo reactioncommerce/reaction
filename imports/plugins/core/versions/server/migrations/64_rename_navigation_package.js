69b23a29ce90f313377a62ccfd67ecf5d08d8a3f
Mike Murray <hellomikemurray@gmail.com>
2019-05-31 13:17:23 -0700

    refactor: use raw mongo collections
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/versions/server/migrations/64_rename_navigation_package.js
+++ imports/plugins/core/versions/server/migrations/64_rename_navigation_package.js
@@ -1,19 +1,21 @@
 import { Migrations } from "meteor/percolate:migrations";
-import { Packages } from "/lib/collections";
+import rawCollections from "/imports/collections/rawCollections";
+
+const { Packages } = rawCollections;
 
 Migrations.add({
   version: 64,
   up() {
     // Remove the old registry entry, the new one is called "reaction-navigation" and will be added
     // on app startup
-    Packages.remove({
+    Packages.deleteMany({
       name: "navigation"
     });
   },
   down() {
     // Remove the old package. The old package was named "navigation" and will be added
     // on app startup
-    Packages.remove({
+    Packages.deleteMany({
       name: "reaction-navigation"
     });
   }
