0c31dcc65e1fd3b118afa6ec9d2714f23ca4bd7d
Mike Murray <hellomikemurray@gmail.com>
2019-05-31 15:39:27 -0700

    docs: update notes
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/client/ui/components/ConfirmDialog/ConfirmDialog.stories.js
+++ imports/client/ui/components/ConfirmDialog/ConfirmDialog.stories.js
@@ -4,10 +4,26 @@ import { action } from "@storybook/addon-actions";
 import Button from "@material-ui/core/Button";
 import ConfirmDialog from "./ConfirmDialog";
 
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+const markdown = `
+# Confirm Dialog
+
+A component that provides a button guarded by a alert dialog for actions that require user confirmation.
+
+## Usage
+~~~js
+<ConfirmDialog
+  title="Are you sure?"
+  message="Are you sure you want to do this?"
+  onConfirm={() => { }}
+>
+  {({ openDialog }) => (
+    <Button onClick={openDialog}>{"Show dialog"}</Button>
+  )}
+</ConfirmDialog>
+~~~
+`;
 
 storiesOf("ConfirmDialog", module)
-  // .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add("standard dialog", () => (
     <ConfirmDialog
       title="Are you sure?"
@@ -19,22 +35,5 @@ storiesOf("ConfirmDialog", module)
       )}
     </ConfirmDialog>
   ), {
-    info: {
-      text: `
-        ### Notes
-
-        ### Usage
-        ~~~js
-        <ConfirmDialog
-          title="Are you sure?"
-          message="Are you sure you want to do this?"
-          onConfirm={() => { }}
-        >
-          {({ openDialog }) => (
-            <Button onClick={openDialog}>{"Show dialog"}</Button>
-          )}
-        </ConfirmDialog>
-        ~~~
-      `
-    }
+    notes: { markdown }
   });
