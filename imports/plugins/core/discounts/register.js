fcbbb70f714b41eef48060bc8784f2fa4e1de9b2
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-03 10:38:32 -0500

    refactor: no-meteor discounts plugin register
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/discounts/register.js
+++ imports/plugins/core/discounts/register.js
@@ -1,22 +1,4 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
-import startup from "./server/no-meteor/startup";
+import register from "./server/no-meteor/register";
 
-Reaction.registerPackage({
-  label: "Discounts",
-  name: "reaction-discounts",
-  icon: "fa fa-gift",
-  autoEnable: true,
-  collections: {
-    Discounts: {
-      name: "Discounts",
-      indexes: [
-        // Create indexes. We set specific names for backwards compatibility
-        // with indexes created by the aldeed:schema-index Meteor package.
-        [{ shopId: 1 }, { name: "c2_shopId" }]
-      ]
-    }
-  },
-  functionsByType: {
-    startup: [startup]
-  }
-});
+Reaction.whenAppInstanceReady(register);
