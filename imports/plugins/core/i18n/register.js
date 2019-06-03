1ce37826775eb38ff33d2e7059e7fbb7179e75ad
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-03 10:39:23 -0500

    refactor: no-meteor i18n plugin register
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/i18n/register.js
+++ imports/plugins/core/i18n/register.js
@@ -1,35 +1,4 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
+import register from "./server/no-meteor/register";
 
-Reaction.registerPackage({
-  label: "i18n",
-  name: "reaction-i18n",
-  icon: "fa fa-language",
-  autoEnable: true,
-  collections: {
-    Translations: {
-      name: "Translations",
-      indexes: [
-        // Create indexes. We set specific names for backwards compatibility
-        // with indexes created by the aldeed:schema-index Meteor package.
-        [{ shopId: 1, i18n: 1 }]
-      ]
-    }
-  },
-  settings: {
-    name: "i18n"
-  },
-  registry: [{
-    provides: ["dashboard"],
-    label: "i18n",
-    description: "Internationalization utilities",
-    icon: "fa fa-language",
-    priority: 1,
-    container: "utilities"
-  }, {
-    provides: ["settings"],
-    template: "i18nSettings",
-    label: "Localization and i18n",
-    icon: "fa fa-language",
-    container: "reaction-i18n"
-  }]
-});
+Reaction.whenAppInstanceReady(register);
