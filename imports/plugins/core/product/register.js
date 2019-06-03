a135322e746b45a263bea43c6de4e03ccd1dca46
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-03 10:39:51 -0500

    refactor: no-meteor product plugin register
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/product/register.js
+++ imports/plugins/core/product/register.js
@@ -1,28 +1,4 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
-import resolvers from "./server/no-meteor/resolvers";
-import schemas from "./server/no-meteor/schemas";
+import register from "./server/no-meteor/register";
 
-Reaction.registerPackage({
-  label: "Product",
-  name: "reaction-product",
-  autoEnable: true,
-  collections: {
-    Products: {
-      name: "Products",
-      indexes: [
-        // Create indexes. We set specific names for backwards compatibility
-        // with indexes created by the aldeed:schema-index Meteor package.
-        [{ ancestors: 1 }, { name: "c2_ancestors" }],
-        [{ createdAt: 1 }, { name: "c2_createdAt" }],
-        [{ handle: 1 }, { name: "c2_handle" }],
-        [{ hashtags: 1 }, { name: "c2_hashtags" }],
-        [{ shopId: 1 }, { name: "c2_shopId" }],
-        [{ "workflow.status": 1 }, { name: "c2_workflow.status" }]
-      ]
-    }
-  },
-  graphQL: {
-    resolvers,
-    schemas
-  }
-});
+Reaction.whenAppInstanceReady(register);
