2cc20d2ed0b50982db5b3fb3b5cdc5a75d662410
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-03 10:37:42 -0500

    refactor: no-meteor catalog plugin register
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/catalog/register.js
+++ imports/plugins/core/catalog/register.js
@@ -1,41 +1,4 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
-import mutations from "./server/no-meteor/mutations";
-import queries from "./server/no-meteor/queries";
-import resolvers from "./server/no-meteor/resolvers";
-import schemas from "./server/no-meteor/schemas";
-import startup from "./server/no-meteor/startup";
+import register from "./server/no-meteor/register";
 
-Reaction.registerPackage({
-  label: "Catalog",
-  name: "reaction-catalog",
-  icon: "fa fa-book",
-  autoEnable: true,
-  collections: {
-    Catalog: {
-      name: "Catalog",
-      indexes: [
-        // Without _id: 1 on these, they cannot be used for sorting by createdAt
-        // because all sorts include _id: 1 as secondary sort to be fully stable.
-        [{ createdAt: 1, _id: 1 }],
-        [{ updatedAt: 1, _id: 1 }],
-        [{ shopId: 1 }],
-        [{ "product._id": 1 }],
-        [{ "product.productId": 1 }],
-        [{ "product.slug": 1 }],
-        [{ "product.tagIds": 1 }]
-      ]
-    }
-  },
-  functionsByType: {
-    startup: [startup]
-  },
-  graphQL: {
-    resolvers,
-    schemas
-  },
-  mutations,
-  queries,
-  settings: {
-    name: "Catalog"
-  }
-});
+Reaction.whenAppInstanceReady(register);
