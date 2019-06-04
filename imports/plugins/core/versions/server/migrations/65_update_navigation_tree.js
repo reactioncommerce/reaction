b098fd9f807975bf804a95a9acef61e22b767012
Mike Murray <hellomikemurray@gmail.com>
2019-06-04 13:35:02 -0700

    fix: await promise
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/versions/server/migrations/65_update_navigation_tree.js
+++ imports/plugins/core/versions/server/migrations/65_update_navigation_tree.js
@@ -11,33 +11,35 @@ Migrations.add({
     // Find all navigation trees
     const navigationTrees = await NavigationTrees.find({}).toArray();
 
-    // Update default values for each tree
-    Promise.all(navigationTrees.map((tree) => {
-      // Update any undefined values for items
-      const items = setDefaultsForNavigationTreeItems(tree.items, {
-        shouldNavigationTreeItemsBeAdminOnly: false,
-        shouldNavigationTreeItemsBePubliclyVisible: true,
-        shouldNavigationTreeItemsBeSecondaryNavOnly: false
-      });
+    try {
+      // Update default values for each tree
+      Promise.await(Promise.all(navigationTrees.map((tree) => {
+        // Update any undefined values for items
+        const items = setDefaultsForNavigationTreeItems(tree.items, {
+          shouldNavigationTreeItemsBeAdminOnly: false,
+          shouldNavigationTreeItemsBePubliclyVisible: true,
+          shouldNavigationTreeItemsBeSecondaryNavOnly: false
+        });
 
-      // Update any undefined values for draft items
-      const draftItems = setDefaultsForNavigationTreeItems(tree.draftItems, {
-        shouldNavigationTreeItemsBeAdminOnly: false,
-        shouldNavigationTreeItemsBePubliclyVisible: true,
-        shouldNavigationTreeItemsBeSecondaryNavOnly: false
-      });
+        // Update any undefined values for draft items
+        const draftItems = setDefaultsForNavigationTreeItems(tree.draftItems, {
+          shouldNavigationTreeItemsBeAdminOnly: false,
+          shouldNavigationTreeItemsBePubliclyVisible: true,
+          shouldNavigationTreeItemsBeSecondaryNavOnly: false
+        });
 
-      // Update items and draft items
-      return NavigationTrees.updateOne({
-        _id: tree._id
-      }, {
-        $set: {
-          items,
-          draftItems
-        }
-      });
-    })).catch((error) => {
+        // Update items and draft items
+        return NavigationTrees.updateOne({
+          _id: tree._id
+        }, {
+          $set: {
+            items,
+            draftItems
+          }
+        });
+      })));
+    } catch (error) {
       Logger.error(error);
-    });
+    }
   }
 });
