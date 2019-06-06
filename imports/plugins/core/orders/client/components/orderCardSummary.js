ac6004c39ebd39546f54e43ffb3ab19dc403c0f4
Erik Kieckhafer <ek@ato.la>
2019-06-06 16:07:25 -0700

    convert OrderSummary from Class to functional component
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/orderCardSummary.js
+++ imports/plugins/core/orders/client/components/orderCardSummary.js
@@ -1,68 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import CartSummary from "@reactioncommerce/components/CartSummary/v1";
+import { i18next } from "/client/api";
 
 
-class OrderCardSummary extends Component {
-  static propTypes = {
-    order: PropTypes.shape({
-      summary: PropTypes.shape({
-        fulfillmentTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        itemTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        surchargeTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        taxTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        total: PropTypes.shape({
-          displayAmount: PropTypes.string
-        })
+/**
+ * @name OrderCardSummary
+ * @params {Object} props Component props
+ * @returns {React.Component} returns a React component
+ */
+function OrderCardSummary({ order }) {
+  const { summary } = order;
+
+  if (summary) {
+    const {
+      fulfillmentTotal,
+      itemTotal,
+      surchargeTotal,
+      taxTotal,
+      total
+    } = summary;
+
+    return (
+      <Card>
+        <CardHeader
+          title={i18next.t("orderCard.orderSummary.title", "Order Summary")}
+        />
+        <CardContent>
+          <CartSummary
+            isDense
+            displayShipping={fulfillmentTotal && fulfillmentTotal.displayAmount}
+            displaySubtotal={itemTotal && itemTotal.displayAmount}
+            displaySurcharge={surchargeTotal && surchargeTotal.displayAmount}
+            displayTax={taxTotal && taxTotal.displayAmount}
+            displayTotal={total && total.displayAmount}
+          />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return null;
+}
+
+OrderCardSummary.propTypes = {
+  order: PropTypes.shape({
+    summary: PropTypes.shape({
+      fulfillmentTotal: PropTypes.shape({
+        displayAmount: PropTypes.string
+      }),
+      itemTotal: PropTypes.shape({
+        displayAmount: PropTypes.string
+      }),
+      surchargeTotal: PropTypes.shape({
+        displayAmount: PropTypes.string
+      }),
+      taxTotal: PropTypes.shape({
+        displayAmount: PropTypes.string
+      }),
+      total: PropTypes.shape({
+        displayAmount: PropTypes.string
       })
     })
-  }
-
-  render() {
-    const { order } = this.props;
-    const { summary } = order;
-
-    if (summary) {
-      const {
-        fulfillmentTotal,
-        itemTotal,
-        surchargeTotal,
-        taxTotal,
-        total
-      } = summary;
-
-      return (
-        <Card>
-          <CardHeader
-            title="Order Summary"
-          />
-          <CardContent>
-            <CartSummary
-              isDense
-              displayShipping={fulfillmentTotal && fulfillmentTotal.displayAmount}
-              displaySubtotal={itemTotal && itemTotal.displayAmount}
-              displaySurcharge={surchargeTotal && surchargeTotal.displayAmount}
-              displayTax={taxTotal && taxTotal.displayAmount}
-              displayTotal={total && total.displayAmount}
-            />
-          </CardContent>
-        </Card>
-      );
-    }
-
-    return null;
-  }
-}
+  })
+};
 
 export default OrderCardSummary;
