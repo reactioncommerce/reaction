fc0fc5d4ecc466f4514333550938f3c7c68dbda6
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-09 19:44:06 -0500

    fix: OrderCard handle not found properly
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/orders/client/containers/orderCardContainer.js
+++ imports/plugins/core/orders/client/containers/orderCardContainer.js
@@ -33,9 +33,14 @@ class OrderCardContainer extends Component {
 
     return (
       <Query errorPolicy="all" query={orderByReferenceId} variables={variables}>
-        {({ loading: isLoading, data: orderData }) => {
+        {({ data: orderData, loading: isLoading }) => {
           if (isLoading) return null;
+
           const { order } = orderData || {};
+
+          if (!order) {
+            return <div>Order not found</div>;
+          }
 
           return (
             <OrderCard
