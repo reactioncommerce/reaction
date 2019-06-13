bc7dcf0b9c722ad9f510aa48f6589e8e7c1d0069
Mike Murray <hellomikemurray@gmail.com>
2019-06-12 21:30:31 -0700

    feat: update various theme values for a better mobile experience for AppBars
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/router/client/theme/muiTheme.js
+++ imports/plugins/core/router/client/theme/muiTheme.js
@@ -1,18 +1,18 @@
 import { defaultComponentTheme } from "@reactioncommerce/components";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import createBreakpoints from "@material-ui/core/styles/createBreakpoints";
-import { fade } from "@material-ui/core/styles/colorManipulator";
 import colors from "./colors";
 
 const { rui_typography: typography } = defaultComponentTheme;
 const breakpoints = createBreakpoints({});
 const toolbarHeight = 80;
+const toolbarMobileHeight = 54;
 
 export const defaultSpacingUnit = 8;
 
 // Colors
 export const colorPrimaryMain = colors.coolGrey;
-export const colorSecondaryMain = colors.coolGrey;
+export const colorSecondaryMain = colors.darkBlue500;
 
 export const rawMuiTheme = {
   palette: {
@@ -30,7 +30,8 @@ export const rawMuiTheme = {
     divider: colors.black10,
     text: {
       secondary: colors.black15,
-      active: colors.reactionBlue
+      secondaryActive: colors.white,
+      active: "#8acef2"
     }
   },
   typography: {
@@ -95,7 +96,14 @@ export const rawMuiTheme = {
     toolbar: {
       minHeight: toolbarHeight,
       [`${breakpoints.up("xs")} and (orientation: landscape)`]: {
-        minHeight: toolbarHeight
+        minHeight: toolbarMobileHeight,
+        paddingLeft: defaultSpacingUnit,
+        paddingRight: defaultSpacingUnit
+      },
+      [`${breakpoints.up("xs")} and (orientation: portrait)`]: {
+        minHeight: toolbarMobileHeight,
+        paddingLeft: defaultSpacingUnit,
+        paddingRight: defaultSpacingUnit
       },
       [breakpoints.up("sm")]: {
         minHeight: toolbarHeight
@@ -105,7 +113,7 @@ export const rawMuiTheme = {
   // Override default props
   props: {
     MuiAppBar: {
-      elevation: 3
+      elevation: 0
     },
     MuiCardHeader: {
       titleTypographyProps: {
@@ -117,16 +125,27 @@ export const rawMuiTheme = {
   overrides: {
     MuiAppBar: {
       root: {
-        height: toolbarHeight
+        height: toolbarHeight,
+        [`${breakpoints.up("xs")} and (orientation: landscape)`]: {
+          height: toolbarMobileHeight
+        },
+        [`${breakpoints.up("xs")} and (orientation: portrait)`]: {
+          height: toolbarMobileHeight
+        },
+        [breakpoints.up("sm")]: {
+          height: toolbarHeight
+        }
       },
       colorPrimary: {
-        backgroundColor: colors.white
+        backgroundColor: colors.white,
+        borderBottom: `1px solid ${colors.black05}`
       },
       colorSecondary: {
-        backgroundColor: fade(colors.coolGrey, 0.2)
+        backgroundColor: "#3C4950" // colors.coolGrey with 20% opacity, opaque
       },
       colorDefault: {
-        backgroundColor: colors.white
+        backgroundColor: colors.white,
+        borderBottom: `1px solid ${colors.black05}`
       }
     },
     MuiButton: {
@@ -179,6 +198,12 @@ export const rawMuiTheme = {
       },
       paperAnchorDockedLeft: {
         borderRight: "none"
+      }
+    },
+    MuiFab: {
+      sizeSmall: {
+        width: 36,
+        height: 36
       }
     },
     MuiOutlinedInput: {
