fc6f0c92bcd6fb81ccd3c3499519f7d4506e6860
Mike Murray <hellomikemurray@gmail.com>
2019-06-12 21:19:55 -0700

    refactor: move title display to the toolbar
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/tags/client/components/TagToolbar.js
+++ imports/plugins/core/tags/client/components/TagToolbar.js
@@ -1,46 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Button from "@reactioncommerce/components/Button/v1";
 import { i18next } from "/client/api";
 import PrimaryAppBar from "/imports/client/ui/components/PrimaryAppBar/PrimaryAppBar";
 
-class TagToolbar extends Component {
-  static propTypes = {
-    canBeDeleted: PropTypes.bool,
-    isNew: PropTypes.bool,
-    onCancel: PropTypes.func,
-    onDelete: PropTypes.func,
-    onSave: PropTypes.func
-  }
+/**
+ * Tag toolbar component
+ * @param {Object} props Component props
+ * @returns {React.Component} Tag toolbar component
+ */
+function TagToolbar(props) {
+  const { canBeDeleted, onDelete, onCancel, onSave, title } = props;
 
-  static defaultProps = {
-    allowsDeletion: true,
-    isNew: true
-  }
-
-  render() {
-    const { onDelete, onCancel, onSave, canBeDeleted } = this.props;
-
-    return (
-      <PrimaryAppBar title="Main Navigation">
-        {(canBeDeleted) &&
-          <Button
-            actionType="secondary"
-            isTextOnly={true}
-            onClick={onDelete}
-          >
-            {i18next.t("admin.tags.form.delete")}
-          </Button>
-        }
-        <Button actionType="secondary" onClick={onCancel}>
-          {i18next.t("admin.tags.form.cancel")}
+  return (
+    <PrimaryAppBar title={title}>
+      {(canBeDeleted) &&
+        <Button
+          actionType="secondary"
+          isTextOnly={true}
+          onClick={onDelete}
+        >
+          {i18next.t("admin.tags.form.delete")}
         </Button>
-        <Button onClick={onSave}>
-          {i18next.t("admin.tags.form.saveChanges")}
-        </Button>
-      </PrimaryAppBar>
-    );
-  }
+      }
+      <Button actionType="secondary" onClick={onCancel}>
+        {i18next.t("admin.tags.form.cancel")}
+      </Button>
+      <Button onClick={onSave}>
+        {i18next.t("admin.tags.form.saveChanges")}
+      </Button>
+    </PrimaryAppBar>
+  );
 }
+
+TagToolbar.propTypes = {
+  canBeDeleted: PropTypes.bool,
+  isNew: PropTypes.bool,
+  onCancel: PropTypes.func,
+  onDelete: PropTypes.func,
+  onSave: PropTypes.func.props,
+  title: PropTypes.string
+};
+
+TagToolbar.defaultProps = {
+  allowsDeletion: true,
+  isNew: true
+};
 
 export default TagToolbar;
