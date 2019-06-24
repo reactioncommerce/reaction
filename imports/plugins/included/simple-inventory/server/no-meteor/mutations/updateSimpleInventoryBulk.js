bb33031c84d7c54216c1d459efd1cea08d3d268b
Seun Martins <seundotme@gmail.com>
2019-06-24 19:59:23 +0100

    feat: multiple changes to refactor how the bulk operation objects get created. Also sharing of code blocks with the single update version
    Signed-off-by: Seun Martins <seundotme@gmail.com>

--- imports/plugins/included/simple-inventory/server/no-meteor/mutations/updateSimpleInventoryBulk.js
+++ imports/plugins/included/simple-inventory/server/no-meteor/mutations/updateSimpleInventoryBulk.js
@@ -1,91 +1,28 @@
-import SimpleSchema from "simpl-schema";
-import Random from "@reactioncommerce/random";
+import _ from "lodash";
 import Logger from "@reactioncommerce/logger";
-import { ProductConfigurationSchema, SimpleInventoryCollectionSchema } from "../simpleSchemas";
+import { inputSchema } from "../utils/defaults";
+import getModifier from "../utils/getModifier";
 
-const logCtx = { name: "core-inventory" };
-
-const inputSchema = new SimpleSchema({
-  productConfiguration: ProductConfigurationSchema,
-  canBackorder: {
-    type: Boolean,
-    optional: true
-  },
-  inventoryInStock: {
-    type: SimpleSchema.Integer,
-    min: 0,
-    optional: true
-  },
-  isEnabled: {
-    type: Boolean,
-    optional: true
-  },
-  lowInventoryWarningThreshold: {
-    type: SimpleSchema.Integer,
-    min: 0,
-    optional: true
-  },
-  shopId: String
-});
-
-const updateFields = [
-  "canBackorder",
-  "inventoryInStock",
-  "isEnabled",
-  "lowInventoryWarningThreshold"
-];
-
-const defaultValues = {
-  canBackorder: false,
-  inventoryInStock: 0,
-  isEnabled: false,
-  lowInventoryWarningThreshold: 0
-};
+const logCtx = { name: "simple-inventory", file: "updateSimpleInventoryBulk" };
 
 // eslint-disable-next-line require-jsdoc
 export default async function updateSimpleInventoryBulk(context, input) {
-  const { appEvents, collections } = context;
+  const { appEvents, collections, userId } = context;
   const { SimpleInventory } = collections;
 
-  const bulkOperations = input.updates.map((update) => {
+  // generate mongo operation object for each product input. Return null on any invalid input
+  let bulkOperations = input.updates.map((update) => {
+    let modifier = {};
+    try {
+      inputSchema.validate(update);
+      modifier = getModifier(update);
+    } catch (error) {
+      Logger.error({ ...logCtx, error });
+      return null;
+    }
+
+    if (!modifier.$set || !modifier.$setOnInsert) return null;
     const { productConfiguration, shopId } = update;
-    inputSchema.validate(update); // try/catch
-    const $set = { updatedAt: new Date() };
-    const $setOnInsert = {
-      "_id": Random.id(),
-      "createdAt": new Date(),
-      // inventoryReserved is calculated by this plugin rather than being set by
-      // users, but we need to init it to some number since this is required.
-      // Below we update this to the correct number if we inserted.
-      "inventoryReserved": 0,
-      // The upsert query below has only `productVariantId` so we need to ensure both are inserted
-      "productConfiguration.productId": productConfiguration.productId
-    };
-    updateFields.forEach((field) => {
-      const value = update[field];
-      if (value !== undefined && value !== null) {
-        $set[field] = value;
-      } else {
-        // If we are not setting the value here, then we add it to the setOnInsert.
-        // This is necessary because all fields are required by the collection schema.
-        $setOnInsert[field] = defaultValues[field];
-      }
-    });
-
-    // if (Object.getOwnPropertyNames($set).length === 1) {
-    //   throw new ReactionError("invalid-param", "You must provide at least one field to update.");
-    // }
-
-    const modifier = { $set, $setOnInsert };
-
-    SimpleInventoryCollectionSchema.validate({
-      $set,
-      $setOnInsert: {
-        ...$setOnInsert,
-        "productConfiguration.productVariantId": productConfiguration.productVariantId,
-        shopId
-      }
-    }, { modifier: true, upsert: true });
 
     return {
       updateOne: {
@@ -99,22 +36,25 @@ export default async function updateSimpleInventoryBulk(context, input) {
     };
   });
 
+  bulkOperations = bulkOperations.filter((op) => op !== null);
   let res;
   try {
     res = await SimpleInventory.bulkWrite(bulkOperations, { ordered: false });
   } catch (error) {
-    Logger.error({ ...logCtx, error });
-    error.result.result.writeErrors.forEach((writeError) => {
-      Logger.debug({
-        ...logCtx,
-        error,
-        op: writeError.err.op,
-        reason: writeError.err.errmsg
-      });
-    });
+    Logger.error({ ...logCtx, error }, "One or more of the bulk update failed");
+    res = error; // error object has details about failed & successful operations
   }
 
   const upsertedIds = res.result.upserted.map((each) => each._id);
+  const failedOps = bulkOperations.filter((op) => {
+    const match = res.result.writeErrors.find((erroredOp) => {
+      const isFilterEqual = _.isEqual(op.updateOne.filter, erroredOp.op.q);
+      const isUpdateEqual = _.isEqual(op.updateOne.update, erroredOp.op.u);
+      return isFilterEqual && isUpdateEqual;
+    });
+    if (match) return true;
+    return false;
+  });
 
   // If we inserted, set the "reserved" quantity to what it should be. We could have
   // put this in the $setOnInsert but then we'd have to do the Orders lookup for
@@ -131,7 +71,6 @@ export default async function updateSimpleInventoryBulk(context, input) {
         shopId: operationData.updateOne.filter.shopId
       };
       Logger.debug({ ...logCtx, arg }, "Calling recalculateReservedSimpleInventory");
-      // for bulk version: do not await.
       context.mutations.recalculateReservedSimpleInventory(context, arg);
     });
   }
@@ -139,9 +78,11 @@ export default async function updateSimpleInventoryBulk(context, input) {
   if (res.result.nModified > 0) {
     const modifiedDocsOpData = bulkOperations.filter((op) => !upsertedIds.includes(op.updateOne.update.$setOnInsert._id));
     const productConfigurations = modifiedDocsOpData.map((operationData) => ({
-      productId: operationData.updateOne.update.$setOnInsert["productConfiguration.productId"]
+      productId: operationData.updateOne.update.$setOnInsert["productConfiguration.productId"],
+      productVariantId: operationData.updateOne.filter["productConfiguration.productVariantId"]
     }));
-    Logger.debug({ ...logCtx, productConfigurations }, "Product configurations for afterBulkInventoryUpdate");
-    appEvents.emit("afterBulkInventoryUpdate", { productConfigurations });
+    appEvents.emit("afterBulkInventoryUpdate", { productConfigurations, updatedBy: userId });
   }
+
+  return { failedOps };
 }
