bb78f20d01e0f9e16c7ae88c277c55cc75324875
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-23 21:26:12 -0500

    refactor: remove double loaded plugin config
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/node-app/devserver/resolvers.js
+++ imports/node-app/devserver/resolvers.js
@@ -1,37 +1,14 @@
 // CORE
 import { merge } from "lodash";
-import accounts from "/imports/plugins/core/accounts/server/no-meteor/resolvers";
 import address from "/imports/plugins/core/address/server/no-meteor/resolvers";
-import cart from "/imports/plugins/core/cart/server/no-meteor/resolvers";
-import catalog from "/imports/plugins/core/catalog/server/no-meteor/resolvers";
-import core from "/imports/plugins/core/core/server/no-meteor/resolvers";
-import navigation from "/imports/plugins/core/navigation/server/no-meteor/resolvers";
-import orders from "/imports/plugins/core/orders/server/no-meteor/resolvers";
 import payments from "/imports/plugins/core/payments/server/no-meteor/resolvers";
-import product from "/imports/plugins/core/product/server/no-meteor/resolvers";
 import shipping from "/imports/plugins/core/shipping/server/no-meteor/resolvers";
-import tags from "/imports/plugins/core/tags/server/no-meteor/resolvers";
 import taxes from "/imports/plugins/core/taxes/server/no-meteor/resolvers";
-// INCLUDED
-import shippingRates from "/imports/plugins/included/shipping-rates/server/no-meteor/resolvers";
-import simpleInventory from "/imports/plugins/included/simple-inventory/server/no-meteor/resolvers";
-import simplePricing from "/imports/plugins/included/simple-pricing/server/no-meteor/resolvers";
 
 export default merge(
   {},
-  accounts,
   address,
-  cart,
-  catalog,
-  core,
-  navigation,
-  orders,
   payments,
-  product,
   shipping,
-  tags,
-  taxes,
-  shippingRates,
-  simpleInventory,
-  simplePricing
+  taxes
 );
--- imports/node-app/devserver/schemas.js
+++ imports/node-app/devserver/schemas.js
@@ -1,43 +1,19 @@
 // CORE
-import accounts from "/imports/plugins/core/accounts/server/no-meteor/schemas";
 import address from "/imports/plugins/core/address/server/no-meteor/schemas";
-import cart from "/imports/plugins/core/cart/server/no-meteor/schemas";
-import catalog from "/imports/plugins/core/catalog/server/no-meteor/schemas";
-import core from "/imports/plugins/core/core/server/no-meteor/schemas";
-import inventory from "/imports/plugins/core/inventory/server/no-meteor/schemas";
-import navigation from "/imports/plugins/core/navigation/server/no-meteor/schemas";
-import orders from "/imports/plugins/core/orders/server/no-meteor/schemas";
 import payments from "/imports/plugins/core/payments/server/no-meteor/schemas";
-import product from "/imports/plugins/core/product/server/no-meteor/schemas";
 import shipping from "/imports/plugins/core/shipping/server/no-meteor/schemas";
-import tags from "/imports/plugins/core/tags/server/no-meteor/schemas";
 import taxes from "/imports/plugins/core/taxes/server/no-meteor/schemas";
 // INCLUDED
 import marketplace from "/imports/plugins/included/marketplace/server/no-meteor/schemas";
 import paymentsExample from "/imports/plugins/included/payments-example/server/no-meteor/schemas";
 import paymentsStripe from "/imports/plugins/included/payments-stripe/server/no-meteor/schemas";
-import shippingRates from "/imports/plugins/included/shipping-rates/server/no-meteor/schemas";
-import simpleInventory from "/imports/plugins/included/simple-inventory/server/no-meteor/schemas";
-import simplePricing from "/imports/plugins/included/simple-pricing/server/no-meteor/schemas";
 
 export default [
-  ...accounts,
   ...address,
-  ...cart,
-  ...catalog,
-  ...core,
-  ...inventory,
-  ...navigation,
-  ...orders,
   ...payments,
-  ...product,
   ...shipping,
-  ...tags,
   ...taxes,
   ...marketplace,
   ...paymentsExample,
-  ...paymentsStripe,
-  ...shippingRates,
-  ...simpleInventory,
-  ...simplePricing
+  ...paymentsStripe
 ];
