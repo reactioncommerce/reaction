8a865d09a2bd3d46e0d437ddecf3e65d8b4e6b15
Eric Dobbertin <eric@dairystatedesigns.com>
2019-06-23 23:03:18 -0500

    refactor: demeteorize sitemap generator plugin registration
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/node-app/registerPlugins.js
+++ imports/node-app/registerPlugins.js
@@ -25,6 +25,7 @@ import registerNotificationsPlugin from "/imports/plugins/included/notifications
 import registerExamplePaymentsPlugin from "/imports/plugins/included/payments-example/server/no-meteor/register";
 import registerStripePaymentsPlugin from "/imports/plugins/included/payments-stripe/server/no-meteor/register";
 import registerShippingRatesPlugin from "/imports/plugins/included/shipping-rates/server/no-meteor/register";
+import registerSitemapGeneratorPlugin from "/imports/plugins/included/sitemap-generator/server/no-meteor/register";
 import registerSimpleInventoryPlugin from "/imports/plugins/included/simple-inventory/server/no-meteor/register";
 import registerSimplePricingPlugin from "/imports/plugins/included/simple-pricing/server/no-meteor/register";
 import registerSurchargesPlugin from "/imports/plugins/included/surcharges/server/no-meteor/register";
@@ -70,6 +71,7 @@ export default async function registerPlugins(app) {
   await registerShippingRatesPlugin(app);
   await registerSimpleInventoryPlugin(app);
   await registerSimplePricingPlugin(app);
+  await registerSitemapGeneratorPlugin(app);
   await registerSurchargesPlugin(app);
   await registerTaxesRatesPlugin(app);
 
--- imports/plugins/included/sitemap-generator/register.js
+++ imports/plugins/included/sitemap-generator/register.js
@@ -1,19 +1,4 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
-import mutations from "./server/no-meteor/mutations";
-import resolvers from "./server/no-meteor/resolvers";
-import schemas from "./server/no-meteor/schemas";
-import startup from "./server/no-meteor/startup";
+import register from "./server/no-meteor/register";
 
-Reaction.registerPackage({
-  label: "Sitemap Generator",
-  name: "reaction-sitemap-generator",
-  icon: "fa fa-vine",
-  graphQL: {
-    resolvers,
-    schemas
-  },
-  functionsByType: {
-    startup: [startup]
-  },
-  mutations
-});
+Reaction.whenAppInstanceReady(register);
