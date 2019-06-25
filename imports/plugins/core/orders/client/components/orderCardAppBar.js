93a166c0517ac98d6bff830103bd5a8569c2dece
Erik Kieckhafer <ek@ato.la>
2019-06-25 15:28:48 -0700

    remove old functions from orderCardAppBar
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/orderCardAppBar.js
+++ imports/plugins/core/orders/client/components/orderCardAppBar.js
@@ -1,51 +1,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import classnames from "classnames";
 import { Mutation } from "react-apollo";
-import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Typography from "@material-ui/core/Typography";
-import { i18next } from "/client/api";
+import { i18next, Reaction } from "/client/api";
 import ConfirmButton from "/imports/client/ui/components/ConfirmButton";
 import PrimaryAppBar from "/imports/client/ui/components/PrimaryAppBar/PrimaryAppBar";
 import cancelOrderItemMutation from "../graphql/mutations/cancelOrderItem";
-import { approveOrderPayments, captureOrderPayments } from "../graphql";
-import { approvePayment, getOrderRiskBadge } from "../helpers";
-import { getOrderRiskStatus } from "../helpers/graphql";
 
-
-const styles = (theme) => ({
-  toolbarButton: {
-    marginLeft: theme.spacing.unit
-  },
-  leftSidebarOpen: {
-    ...theme.mixins.leadingPaddingWhenPrimaryDrawerIsOpen
-  }
-});
 
 class OrderCardAppBar extends Component {
   static propTypes = {
     classes: PropTypes.object,
     order: PropTypes.shape({
-      summary: PropTypes.shape({
-        fulfillmentTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        itemTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        surchargeTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        taxTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        total: PropTypes.shape({
-          displayAmount: PropTypes.string
-        })
-      })
+      status: PropTypes.string
     })
   }
 
@@ -53,22 +21,10 @@ class OrderCardAppBar extends Component {
     shouldRestock: true
   }
 
-  handleApprovePayment = async (order, paymentIds) => {
-
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
-      if (!order.payments) return Promise.resolve(null);
-
-      // If paymentIds are not provided, capture all payments
-      const paymentIdList = paymentIds || order.payments.map((payment) => payment._id);
-
-      return approveOrderPayments({ orderId: order._id, paymentIds: paymentIdList, shopId: order.shop._id });
-    }
-
-    return null;
-  }
-
   handleCancelOrder = (mutation) => {
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
+    const hasPermission = Reaction.hasPermission("reaction-orders", Reaction.getUserId(), Reaction.getShopId());
+
+    if (hasPermission) {
       const { order } = this.props;
       const { shouldRestock } = this.state;
       const { fulfillmentGroups } = order;
@@ -96,62 +52,10 @@ class OrderCardAppBar extends Component {
     return null;
   }
 
-  handleCapturePayment = async (order, paymentIds) => {
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
-      // TODO: EK - handle capturing payment
-
-      if (!order.payments) return Promise.resolve(null);
-
-      // If paymentIds are not provided, capture all payments
-      const paymentIdList = paymentIds || order.payments.map((payment) => payment._id);
-
-      const approve = (paymentIdsNew) => {
-        // foeach paymentId, approve payment
-        return paymentIdsNew.map((paymentId) => {
-          console.log("paymentId", paymentId);
-
-          // get non opaque ID for each payment
-          return approvePayment(order._id, paymentId);
-        });
-      };
-
-      const capture = () => captureOrderPayments({ orderId: order._id, paymentIds: paymentIdList, shopId: order.shop._id });
-
-      /**
-       * @summary Show alert
-       * @returns {Promise<Boolean>} Resolves if they click Continue
-       */
-      function alertDialog() {
-        let alertType = "warning";
-        const riskBadge = getOrderRiskBadge(getOrderRiskStatus(order));
-        // use red alert color for high risk level
-        if (riskBadge === "danger") {
-          alertType = "error";
-        }
-
-        return Alerts.alert({
-          title: i18next.t("admin.orderRisk.riskCapture"),
-          text: i18next.t("admin.orderRisk.riskCaptureWarn"),
-          type: alertType,
-          showCancelButton: true,
-          cancelButtonText: i18next.t("admin.settings.cancel"),
-          confirmButtonText: i18next.t("admin.settings.continue")
-        });
-      }
-
-      // before capturing, check if there's a payment risk on order; alert admin before capture
-      if (getOrderRiskStatus(order)) {
-        return alertDialog().then(approve(paymentIdList)).then(capture);
-      }
-
-      return approve(paymentIdList).then(capture);
-    }
-
-    return null;
-  }
-
   handleInventoryRestockCheckbox = (name) => (event) => {
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
+    const hasPermission = Reaction.hasPermission("reaction-orders", Reaction.getUserId(), Reaction.getShopId());
+
+    if (hasPermission) {
       this.setState({
         ...this.state,
         [name]: event.target.checked
@@ -160,8 +64,11 @@ class OrderCardAppBar extends Component {
   };
 
   handleInventoryRestock = (item) => {
-    // TODO: EK - handle inventory restock
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
+    const { shouldRestock } = this.state;
+    const hasPermission = Reaction.hasPermission("reaction-orders", Reaction.getUserId(), Reaction.getShopId());
+
+    if (hasPermission && shouldRestock) {
+      return console.log(" ----- ----- ----- handleInventoryRestock for item ", item);
     }
 
     return null;
@@ -171,8 +78,9 @@ class OrderCardAppBar extends Component {
     const { order } = this.props;
     const { shouldRestock } = this.state;
     const canCancelOrder = (order.status !== "coreOrderWorkflow/canceled");
+    const hasPermission = Reaction.hasPermission("reaction-orders", Reaction.getUserId(), Reaction.getShopId());
 
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
+    if (hasPermission) {
       if (canCancelOrder) {
         return (
           <Mutation mutation={cancelOrderItemMutation}>
@@ -180,7 +88,7 @@ class OrderCardAppBar extends Component {
               <ConfirmButton
                 buttonColor="danger"
                 buttonText={i18next.t("order.cancelOrderLabel", "Cancel order")}
-                buttonVariant="outlined"
+                buttonVariant="contained"
                 cancelActionText={i18next.t("app.close", "Close")}
                 confirmActionText={i18next.t("order.cancelOrderLabel", "Cancel order")}
                 title={i18next.t("order.cancelOrderLabel", "Cancel order")}
@@ -207,59 +115,13 @@ class OrderCardAppBar extends Component {
     return null;
   }
 
-  renderCapturePaymentButton = () => {
-    const { classes, order } = this.props;
-    const canCapturePayment = order.payments.every((payment) => payment.mode !== "captured");
-
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
-      if (canCapturePayment) {
-        return (
-          <Button
-            className={classes.toolbarButton}
-            color="primary"
-            variant="contained"
-            onClick={() => this.handleCapturePayment(order)}
-          >
-            {i18next.t("order.capturePayment", "Capture payment")}
-          </Button>
-        );
-      }
-    }
-
-    return null;
-  }
-
-  renderApprovePaymentButton = () => {
-    const { classes, order } = this.props;
-    const canApprovePayment = order.payments.every((payment) => payment.mode === "created");
-
-    if (this.props.hasEditPermission) { // TODO: EK - update to better permission handling
-      if (canApprovePayment) {
-        return (
-          <Button
-            className={classes.toolbarButton}
-            color="primary"
-            variant="contained"
-            onClick={() => this.handleApprovePayment(order)}
-          >
-            Approve them all
-          </Button>
-        );
-      }
-    }
-
-    return null;
-  }
-
   render() {
     return (
       <PrimaryAppBar title={i18next.t("orderDetails", "Order details")}>
         {this.renderCancelOrderButton()}
-        {this.renderCapturePaymentButton()}
-        {this.renderApprovePaymentButton()}
       </PrimaryAppBar>
     );
   }
 }
 
-export default withStyles(styles, { name: "MuiOrderCardAppBar" })(OrderCardAppBar);
+export default OrderCardAppBar;
