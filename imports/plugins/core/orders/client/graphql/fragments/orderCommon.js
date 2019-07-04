0e40815a68657088d2e17b95c338075b14e3dcab
Erik Kieckhafer <ek@ato.la>
2019-07-03 21:50:37 -0700

    update fields required for payments
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/graphql/fragments/orderCommon.js
+++ imports/plugins/core/orders/client/graphql/fragments/orderCommon.js
@@ -1,57 +1,110 @@
 import gql from "graphql-tag";
 
-export const orderCommon = gql`
+export const orderCommonFragment = gql`
   fragment OrderCommon on Order {
-  _id
-  account {
     _id
-  }
-  cartId
-  createdAt
-  displayStatus(language: $language)
-  email
-  fulfillmentGroups {
-    _id
-    data {
-      ... on ShippingOrderFulfillmentGroupData {
-        shippingAddress {
-          _id
-          address1
-          address2
-          city
-          company
-          country
-          fullName
-          isCommercial
-          isShippingDefault
-          phone
-          postal
-          region
+    account {
+      _id
+    }
+    cartId
+    createdAt
+    displayStatus(language: $language)
+    email
+    fulfillmentGroups {
+      _id
+      data {
+        ... on ShippingOrderFulfillmentGroupData {
+          shippingAddress {
+            _id
+            address1
+            address2
+            city
+            company
+            country
+            fullName
+            isCommercial
+            isShippingDefault
+            phone
+            postal
+            region
+          }
         }
       }
-    }
-    items {
-      nodes {
-        _id
-        addedAt
-        createdAt
-        imageURLs {
-          large
-          medium
-          original
-          small
-          thumbnail
+      displayStatus(language: $language)
+      items {
+        nodes {
+          _id
+          addedAt
+          createdAt
+          imageURLs {
+            large
+            medium
+            original
+            small
+            thumbnail
+          }
+          isTaxable
+          optionTitle
+          parcel {
+            containers
+            distanceUnit
+            height
+            length
+            massUnit
+            weight
+            width
+          }
+          price {
+            amount
+            currency {
+              code
+            }
+            displayAmount
+          }
+          productConfiguration {
+            productId
+            productVariantId
+          }
+          productSlug
+          productType
+          productVendor
+          productTags {
+            nodes {
+              name
+            }
+          }
+          quantity
+          shop {
+            _id
+          }
+          subtotal {
+            amount
+            currency {
+              code
+            }
+            displayAmount
+          }
+          taxCode
+          title
+          updatedAt
+          variantTitle
         }
-        isTaxable
-        optionTitle
-        parcel {
-          containers
-          distanceUnit
-          height
-          length
-          massUnit
-          weight
-          width
+      }
+      selectedFulfillmentOption {
+        fulfillmentMethod {
+          _id
+          carrier
+          displayName
+          fulfillmentTypes
+          group
+          name
+        }
+        handlingPrice {
+          amount
+          currency {
+            code
+          }
+          displayAmount
         }
         price {
           amount
@@ -60,62 +113,73 @@ export const orderCommon = gql`
           }
           displayAmount
         }
-        productConfiguration {
-          productId
-          productVariantId
-        }
-        productSlug
-        productType
-        productVendor
-        productTags {
-          nodes {
-            name
-          }
-        }
-        quantity
-        shop {
-          _id
-        }
-        subtotal {
+      }
+      shop {
+        _id
+      }
+      status
+      summary {
+        fulfillmentTotal {
           amount
-          currency {
-            code
-          }
           displayAmount
         }
-        taxCode
-        title
-        updatedAt
-        variantTitle
+        itemTotal {
+          amount
+          displayAmount
+        }
+        surchargeTotal {
+          amount
+          displayAmount
+        }
+        taxTotal {
+          amount
+          displayAmount
+        }
+        total {
+          amount
+          displayAmount
+        }
       }
+      tracking
+      type
     }
-    selectedFulfillmentOption {
-      fulfillmentMethod {
-        _id
-        carrier
+    payments {
+      _id
+      amount {
+        displayAmount
+      }
+      billingAddress {
+        address1
+        address2
+        city
+        company
+        country
+        fullName
+        isCommercial
+        phone
+        postal
+        region
+      }
+      captureErrorMessage
+      displayName
+      method {
         displayName
-        fulfillmentTypes
-        group
         name
       }
-      handlingPrice {
-        amount
-        currency {
-          code
-        }
-        displayAmount
-      }
-      price {
-        amount
-        currency {
-          code
-        }
-        displayAmount
-      }
+      mode
+      processor
+      riskLevel
+      status
+      transactionId
     }
+    referenceId
     shop {
       _id
+      currency {
+        code
+      }
     }
+    status
     summary {
       fulfillmentTotal {
         amount
@@ -138,62 +202,7 @@ export const orderCommon = gql`
         displayAmount
       }
     }
-    tracking
-    type
+    totalItemQuantity
+    updatedAt
   }
-  payments {
-    _id
-    amount {
-      displayAmount
-    }
-    billingAddress {
-      address1
-      address2
-      city
-      company
-      country
-      fullName
-      isCommercial
-      phone
-      postal
-      region
-    }
-    displayName
-    method {
-      name
-    }
-  }
-  referenceId
-  shop {
-    _id
-    currency {
-      code
-    }
-  }
-  status
-  summary {
-    fulfillmentTotal {
-      amount
-      displayAmount
-    }
-    itemTotal {
-      amount
-      displayAmount
-    }
-    surchargeTotal {
-      amount
-      displayAmount
-    }
-    taxTotal {
-      amount
-      displayAmount
-    }
-    total {
-      amount
-      displayAmount
-    }
-  }
-  totalItemQuantity
-  updatedAt
-}
 `;
