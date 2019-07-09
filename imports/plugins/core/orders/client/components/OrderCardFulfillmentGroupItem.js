36e78cbbb21d0ca524a1aa58eed8cfe7293a9f7a
Erik Kieckhafer <ek@ato.la>
2019-07-09 11:18:03 -0700

    move orderpayments to orderpaymentsmeteor
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/OrderCardFulfillmentGroupItem.js
+++ imports/plugins/core/orders/client/components/OrderCardFulfillmentGroupItem.js
@@ -6,8 +6,8 @@ import Typography from "@material-ui/core/Typography";
 
 
 const styles = (theme) => ({
-  fontWeightBold: {
-    fontWeight: theme.typography.fontWeightBold
+  fontWeightSemiBold: {
+    fontWeight: theme.typography.fontWeightSemiBold
   }
 });
 
@@ -56,7 +56,7 @@ class OrderCardFulfillmentGroupItem extends Component {
               {price.displayAmount}
             </Typography>
             <Typography variant="body1" align="right">
-              Total({quantity}):&nbsp; <span className={classes.fontWeightBold}>{subtotal.displayAmount}</span>
+              Total({quantity}):&nbsp; <span className={classes.fontWeightSemiBold}>{subtotal.displayAmount}</span>
             </Typography>
           </Grid>
         </Grid>
