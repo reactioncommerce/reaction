09ba800fdd284d797a7bcb5ff7422c6874a82079
Erik Kieckhafer <ek@ato.la>
2019-07-10 14:15:33 -0700

    convert OrderContainer to functional component
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/containers/OrderContainer.js
+++ imports/plugins/core/orders/client/containers/OrderContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Query } from "react-apollo";
 import { withRouter } from "react-router-dom";
@@ -7,50 +7,52 @@ import withPrimaryShop from "/imports/plugins/core/graphql/lib/hocs/withPrimaryS
 import Order from "../components/Order";
 import orderByReferenceId from "../graphql/queries/orderByReferenceId";
 
-//TODO: EK - convert to functional component
-class OrderContainer extends Component {
-  static propTypes = {
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        _id: PropTypes.string
-      })
-    }),
-    shop: PropTypes.shape({
-      _id: PropTypes.string,
-      language: PropTypes.string
-    })
-  }
+/**
+ * @name OrderContainer
+ * @param {Object} props Component props
+ * @returns {React.Component} returns a React component
+ */
+function OrderContainer(props) {
+  const { match: { params }, shop } = props;
 
-  render() {
-    const { match: { params }, shop } = this.props;
+  const variables = {
+    id: params._id,
+    language: shop.language,
+    shopId: shop._id,
+    token: null
+  };
 
-    const variables = {
-      id: params._id,
-      language: shop.language,
-      shopId: shop._id,
-      token: null
-    };
+  return (
+    <Query errorPolicy="all" query={orderByReferenceId} variables={variables}>
+      {({ data: orderData, loading: isLoading }) => {
+        if (isLoading) return null;
+        const { order } = orderData || {};
 
-    return (
-      <Query errorPolicy="all" query={orderByReferenceId} variables={variables}>
-        {({ data: orderData, loading: isLoading }) => {
-          if (isLoading) return null;
-          const { order } = orderData || {};
+        if (!order) {
+          return <div>Order not found</div>;
+        }
 
-          if (!order) {
-            return <div>Order not found</div>;
-          }
-
-          return (
-            <Order
-              order={order}
-            />
-          );
-        }}
-      </Query>
-    );
-  }
+        return (
+          <Order
+            order={order}
+          />
+        );
+      }}
+    </Query>
+  );
 }
+
+OrderContainer.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      _id: PropTypes.string
+    })
+  }),
+  shop: PropTypes.shape({
+    _id: PropTypes.string,
+    language: PropTypes.string
+  })
+};
 
 export default compose(
   withPrimaryShop,
