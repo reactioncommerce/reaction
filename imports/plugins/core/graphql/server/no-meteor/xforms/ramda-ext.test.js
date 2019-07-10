711718f3fe8510979166c9f165345a6967e683d6
Erik Kieckhafer <ek@ato.la>
2019-07-09 19:36:26 -0700

    fix failing test
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/graphql/server/no-meteor/xforms/ramda-ext.test.js
+++ imports/plugins/core/graphql/server/no-meteor/xforms/ramda-ext.test.js
@@ -2,10 +2,10 @@ import { renameKeys } from "./ramda-ext";
 
 test("renameKeys renames keys in a map", () => {
   const input = { varA: 1, varB: 2 };
-  expect(renameKeys({ varA: "d" }, input)).toEqual({ varD: 1, varB: 2 });
+  expect(renameKeys({ varA: "varD" }, input)).toEqual({ varD: 1, varB: 2 });
 });
 
 test("renameKeys passes over missing keys", () => {
   const input = { varA: 1, varB: 2 };
-  expect(renameKeys({ varF: "d" }, input)).toEqual({ varA: 1, varB: 2 });
+  expect(renameKeys({ varF: "varD" }, input)).toEqual({ varA: 1, varB: 2 });
 });
