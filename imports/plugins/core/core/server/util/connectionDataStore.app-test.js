b17860c8d72214521e47e43e732ca61e41e9d557
Erik Kieckhafer <ek@ato.la>
2019-07-12 10:34:10 -0700

    perf: wrap tests in check to make sure app is ready when they run
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/core/server/util/connectionDataStore.app-test.js
+++ imports/plugins/core/core/server/util/connectionDataStore.app-test.js
@@ -1,94 +1,97 @@
 import { expect } from "meteor/practicalmeteor:chai";
 import { sinon } from "meteor/practicalmeteor:sinon";
 import { DDP } from "meteor/ddp-client";
+import Reaction from "/imports/plugins/core/core/server/Reaction";
 import ConnectionDataStore from "./connectionDataStore";
 
-describe("ConnectionDataStore", () => {
-  describe("used outside of a connection", () => {
-    it("sets/gets cached data", () => {
-      ConnectionDataStore.set("key", "val");
+Reaction.onAppStartupComplete(() => {
+  describe("ConnectionDataStore", () => {
+    describe("used outside of a connection", () => {
+      it("sets/gets cached data", () => {
+        ConnectionDataStore.set("key", "val");
 
-      expect(ConnectionDataStore.get("key")).to.equal("val");
+        expect(ConnectionDataStore.get("key")).to.equal("val");
+      });
+
+      it("clears cached data", () => {
+        ConnectionDataStore.set("key", "val");
+
+        expect(ConnectionDataStore.get("key")).to.equal("val");
+
+        ConnectionDataStore.clear("key");
+
+        expect(ConnectionDataStore.get("key")).to.be.undefined;
+      });
     });
 
-    it("clears cached data", () => {
-      ConnectionDataStore.set("key", "val");
+    describe("within the context of a connection", () => {
+      let mockConnection;
 
-      expect(ConnectionDataStore.get("key")).to.equal("val");
+      beforeEach(() => {
+        mockConnection = {};
 
-      ConnectionDataStore.clear("key");
+        sinon.stub(DDP._CurrentMethodInvocation, "get")
+          .returns({ connection: mockConnection });
 
-      expect(ConnectionDataStore.get("key")).to.be.undefined;
-    });
-  });
+        ConnectionDataStore.set("key", "val");
+      });
 
-  describe("within the context of a connection", () => {
-    let mockConnection;
+      afterEach(() => {
+        mockConnection = undefined;
 
-    beforeEach(() => {
-      mockConnection = {};
+        DDP._CurrentMethodInvocation.get.restore();
+      });
 
-      sinon.stub(DDP._CurrentMethodInvocation, "get")
-        .returns({ connection: mockConnection });
+      it("sets/gets cached data", () => {
+        expect(ConnectionDataStore.get("key")).to.equal("val");
+      });
 
-      ConnectionDataStore.set("key", "val");
-    });
-
-    afterEach(() => {
-      mockConnection = undefined;
-
-      DDP._CurrentMethodInvocation.get.restore();
+      it("stores data on the connection object", () => {
+        expect(mockConnection["connection-data"].key).to.equal("val");
+      });
     });
 
-    it("sets/gets cached data", () => {
-      expect(ConnectionDataStore.get("key")).to.equal("val");
-    });
+    describe("within the context of a multiple connections", () => {
+      let mockConnection;
+      let mockConnection2;
 
-    it("stores data on the connection object", () => {
-      expect(mockConnection["connection-data"].key).to.equal("val");
-    });
-  });
+      beforeEach(() => {
+        mockConnection = {};
+        mockConnection2 = {};
 
-  describe("within the context of a multiple connections", () => {
-    let mockConnection;
-    let mockConnection2;
+        sinon.stub(DDP._CurrentMethodInvocation, "get")
+          .returns({ connection: mockConnection });
 
-    beforeEach(() => {
-      mockConnection = {};
-      mockConnection2 = {};
+        ConnectionDataStore.set("key", "val");
 
-      sinon.stub(DDP._CurrentMethodInvocation, "get")
-        .returns({ connection: mockConnection });
+        DDP._CurrentMethodInvocation.get.restore();
 
-      ConnectionDataStore.set("key", "val");
+        sinon.stub(DDP._CurrentMethodInvocation, "get")
+          .returns({ connection: mockConnection2 });
 
-      DDP._CurrentMethodInvocation.get.restore();
+        ConnectionDataStore.set("key2", "val2");
+      });
 
-      sinon.stub(DDP._CurrentMethodInvocation, "get")
-        .returns({ connection: mockConnection2 });
+      afterEach(() => {
+        mockConnection = undefined;
+        mockConnection2 = undefined;
 
-      ConnectionDataStore.set("key2", "val2");
-    });
+        DDP._CurrentMethodInvocation.get.restore();
+      });
 
-    afterEach(() => {
-      mockConnection = undefined;
-      mockConnection2 = undefined;
+      // we are now in the context of the second connection
+      it("does not access previous connection's data", () => {
+        expect(ConnectionDataStore.get("key")).to.be.undefined;
+      });
 
-      DDP._CurrentMethodInvocation.get.restore();
-    });
+      it("does not modify previous connection's data", () => {
+        expect(mockConnection["connection-data"]).not.to.have.property("key2");
+      });
 
-    // we are now in the context of the second connection
-    it("does not access previous connection's data", () => {
-      expect(ConnectionDataStore.get("key")).to.be.undefined;
-    });
-
-    it("does not modify previous connection's data", () => {
-      expect(mockConnection["connection-data"]).not.to.have.property("key2");
-    });
-
-    it("does modify _this_ connection's data", () => {
-      expect(ConnectionDataStore.get("key2")).to.equal("val2");
-      expect(mockConnection2["connection-data"].key2).to.equal("val2");
+      it("does modify _this_ connection's data", () => {
+        expect(ConnectionDataStore.get("key2")).to.equal("val2");
+        expect(mockConnection2["connection-data"].key2).to.equal("val2");
+      });
     });
   });
 });
