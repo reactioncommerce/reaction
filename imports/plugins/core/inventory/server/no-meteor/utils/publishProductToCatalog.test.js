80329f75869c099306365b259e60caff0f299297
Eric Dobbertin <eric@dairystatedesigns.com>
2019-07-17 17:24:07 -0500

    test: add tests for inventory catalog
    publishProductToCatalog
    
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

---
+++ imports/plugins/core/inventory/server/no-meteor/utils/publishProductToCatalog.test.js
@@ -0,0 +1,182 @@
+import publishProductToCatalog from "./publishProductToCatalog";
+import Factory from "/imports/test-utils/helpers/factory";
+import mockContext from "/imports/test-utils/helpers/mockContext";
+
+const productId = "TOP_PRODUCT_ID";
+
+mockContext.queries.inventoryForProductConfigurations = jest.fn().mockName("inventoryForProductConfigurations");
+
+/**
+ * @summary Returns a mock catalogProduct for these tests
+ * @returns {Object} CatalogProduct
+ */
+function getCatalogProduct() {
+  return Factory.CatalogProduct.makeOne({
+    isDeleted: false,
+    isVisible: true,
+    productId,
+    variants: Factory.CatalogVariantSchema.makeMany(1, {
+      isDeleted: false,
+      isVisible: true,
+      variantId: "TOP_VARIANT_1",
+      options: Factory.VariantBaseSchema.makeMany(2, {
+        isDeleted: false,
+        isVisible: true,
+        variantId: (index) => `OPTION_VARIANT_${index + 1}`
+      })
+    })
+  });
+}
+
+test("publishProductToCatalog adds inventory booleans to catalogProduct", async () => {
+  const catalogProduct = getCatalogProduct();
+
+  // Verify beginning state
+  expect(catalogProduct.isSoldOut).toBe(undefined);
+  expect(catalogProduct.isLowQuantity).toBe(undefined);
+  expect(catalogProduct.isBackorder).toBe(undefined);
+  expect(catalogProduct.variants[0].isSoldOut).toBe(undefined);
+  expect(catalogProduct.variants[0].options[0].isSoldOut).toBe(undefined);
+  expect(catalogProduct.variants[0].options[1].isSoldOut).toBe(undefined);
+
+  mockContext.queries.inventoryForProductConfigurations.mockReturnValueOnce(Promise.resolve([
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "TOP_VARIANT_1"
+      },
+      inventoryInfo: {
+        canBackorder: true,
+        inventoryAvailableToSell: 0,
+        inventoryInStock: 0,
+        inventoryReserved: 0,
+        isBackorder: true,
+        isLowQuantity: true,
+        isSoldOut: true
+      }
+    },
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "OPTION_VARIANT_1"
+      },
+      inventoryInfo: {
+        canBackorder: true,
+        inventoryAvailableToSell: 0,
+        inventoryInStock: 0,
+        inventoryReserved: 0,
+        isBackorder: true,
+        isLowQuantity: false,
+        isSoldOut: true
+      }
+    },
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "OPTION_VARIANT_2"
+      },
+      inventoryInfo: {
+        canBackorder: true,
+        inventoryAvailableToSell: 0,
+        inventoryInStock: 0,
+        inventoryReserved: 0,
+        isBackorder: true,
+        isLowQuantity: true,
+        isSoldOut: true
+      }
+    }
+  ]));
+
+  await publishProductToCatalog(catalogProduct, { context: mockContext });
+
+  expect(catalogProduct.isSoldOut).toBe(true);
+  expect(catalogProduct.isLowQuantity).toBe(true);
+  expect(catalogProduct.isBackorder).toBe(true);
+  expect(catalogProduct.variants[0].isSoldOut).toBe(true);
+  expect(catalogProduct.variants[0].options[0].isSoldOut).toBe(true);
+  expect(catalogProduct.variants[0].options[1].isSoldOut).toBe(true);
+});
+
+test("publishProductToCatalog ignores deleted variants", async () => {
+  const catalogProduct = getCatalogProduct();
+
+  catalogProduct.variants.push(Factory.CatalogVariantSchema.makeOne({
+    isDeleted: true,
+    isVisible: true,
+    variantId: "TOP_VARIANT_2",
+    options: null
+  }));
+
+  mockContext.queries.inventoryForProductConfigurations.mockReturnValueOnce(Promise.resolve([
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "TOP_VARIANT_1"
+      },
+      inventoryInfo: {
+        canBackorder: true,
+        inventoryAvailableToSell: 0,
+        inventoryInStock: 0,
+        inventoryReserved: 0,
+        isBackorder: true,
+        isLowQuantity: true,
+        isSoldOut: true
+      }
+    },
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "OPTION_VARIANT_1"
+      },
+      inventoryInfo: {
+        canBackorder: true,
+        inventoryAvailableToSell: 0,
+        inventoryInStock: 0,
+        inventoryReserved: 0,
+        isBackorder: true,
+        isLowQuantity: true,
+        isSoldOut: true
+      }
+    },
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "OPTION_VARIANT_2"
+      },
+      inventoryInfo: {
+        canBackorder: true,
+        inventoryAvailableToSell: 0,
+        inventoryInStock: 0,
+        inventoryReserved: 0,
+        isBackorder: true,
+        isLowQuantity: true,
+        isSoldOut: true
+      }
+    },
+    {
+      productConfiguration: {
+        productId,
+        productVariantId: "TOP_VARIANT_2"
+      },
+      inventoryInfo: {
+        canBackorder: false,
+        inventoryAvailableToSell: 4,
+        inventoryInStock: 4,
+        inventoryReserved: 0,
+        isBackorder: false,
+        isLowQuantity: false,
+        isSoldOut: false
+      }
+    }
+  ]));
+
+  await publishProductToCatalog(catalogProduct, { context: mockContext });
+
+  expect(catalogProduct.isSoldOut).toBe(true);
+  expect(catalogProduct.isLowQuantity).toBe(true);
+  expect(catalogProduct.isBackorder).toBe(true);
+  expect(catalogProduct.variants[0].isSoldOut).toBe(true);
+  expect(catalogProduct.variants[0].options[0].isSoldOut).toBe(true);
+  expect(catalogProduct.variants[0].options[1].isSoldOut).toBe(true);
+  expect(catalogProduct.variants[1].isSoldOut).toBe(false);
+});
