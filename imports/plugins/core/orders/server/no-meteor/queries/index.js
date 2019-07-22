99956c964cb10f6b1f98ebb4119845edeb847faf
Erik Kieckhafer <ek@ato.la>
2019-07-22 12:15:11 -0700

    feat: add listRefunds GraphQL query
    Signed-off-by: Erik Kieckhafer <ek@ato.la>
    (cherry picked from commit 77d8b7558f4c8b6dc50d25f264a509aadaa8bbb4)

--- imports/plugins/core/orders/server/no-meteor/queries/index.js
+++ imports/plugins/core/orders/server/no-meteor/queries/index.js
@@ -1,8 +1,10 @@
+import listRefunds from "./listRefunds";
 import orderById from "./orderById";
 import orderByReferenceId from "./orderByReferenceId";
 import ordersByAccountId from "./ordersByAccountId";
 
 export default {
+  listRefunds,
   orderById,
   orderByReferenceId,
   ordersByAccountId
