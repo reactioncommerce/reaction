476fc2405e70827cee8b025986babdca4514991a
Erik Kieckhafer <ek@ato.la>
2019-07-29 21:48:38 -0700

    ref: add refunds into each payment
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/resolvers/Order/index.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/Order/index.js
@@ -1,8 +1,9 @@
 import { encodeCartOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/cart";
-import { encodeOrderOpaqueId, xformOrderPayment } from "@reactioncommerce/reaction-graphql-xforms/order";
+import { encodeOrderOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/order";
 import { resolveAccountFromAccountId, resolveShopFromShopId } from "@reactioncommerce/reaction-graphql-utils";
 import orderDisplayStatus from "./orderDisplayStatus";
 import orderSummary from "./orderSummary";
+import payments from "./payments";
 import refunds from "./refunds";
 import totalItemQuantity from "./totalItemQuantity";
 
@@ -13,7 +14,7 @@ export default {
   displayStatus: (node, { language }, context) => orderDisplayStatus(context, node, language),
   fulfillmentGroups: (node) => node.shipping || [],
   notes: (node) => node.notes || [],
-  payments: (node) => (Array.isArray(node.payments) ? node.payments.map(xformOrderPayment) : null),
+  payments: (node, _, context) => payments(context, node),
   refunds: (node, _, context) => refunds(context, node),
   shop: resolveShopFromShopId,
   status: (node) => node.workflow.status,
