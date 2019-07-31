a0d60f40fae43f6bf370135a04cd361731a72b3d
Erik Kieckhafer <ek@ato.la>
2019-07-30 17:00:06 -0700

    ref: remove uneeded resolver data
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/resolvers/Refund/index.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/Refund/index.js
@@ -2,9 +2,6 @@ import amount from "./amount";
 import createdAt from "./createdAt";
 
 export default {
-  _id: (node) => node._id,
   amount,
-  createdAt,
-  paymentDisplayName: (node) => node.paymentDisplayName,
-  reason: (node) => node.reason
+  createdAt
 };
