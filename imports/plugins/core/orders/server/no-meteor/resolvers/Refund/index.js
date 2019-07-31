f6032eb89f23bd251978e2b4557ec92a502b8460
Erik Kieckhafer <ek@ato.la>
2019-07-30 17:09:08 -0700

    ref: add paymentId encoder
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/resolvers/Refund/index.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/Refund/index.js
@@ -1,7 +1,10 @@
+import { encodeRefundOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/refund";
 import amount from "./amount";
 import createdAt from "./createdAt";
 
 export default {
   amount,
   createdAt
+  createdAt,
+  paymentId: (node) => encodePaymentOpaqueId(node.paymentId)
 };
