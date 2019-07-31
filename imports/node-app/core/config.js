5729c3122d6cf73b3983c2ebb122aa6643f0c697
Erik Kieckhafer <ek@ato.la>
2019-07-31 16:35:06 -0700

    ref: change value to number instead of string, add validator
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/node-app/core/config.js
+++ imports/node-app/core/config.js
@@ -1,8 +1,14 @@
-import envalid, { bool, str } from "envalid";
+import envalid, { bool, makeValidator, str } from "envalid";
+
+const bodyParserValidator = makeValidator((value) => {
+  if (typeof value !== "number") throw new Error("Expected type number letters");
+  if (value <= 0) throw new Error("Expected value to be greater than 0");
+  return value;
+});
 
 export default envalid.cleanEnv(process.env, {
-  BODY_PARSER_SIZE_LIMIT: str({
-    default: "5mb"
+  BODY_PARSER_SIZE_LIMIT: bodyParserValidator({
+    default: 5 * 1000000 // value in bytes = 5mb
   }),
   GRAPHQL_INTROSPECTION_ENABLED: bool({ default: false }),
   GRAPHQL_PLAYGROUND_ENABLED: bool({ default: false }),
