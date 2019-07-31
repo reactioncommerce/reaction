5611ecdd8aabd3ab59a098ee6dc93ac4519b855f
Erik Kieckhafer <ek@ato.la>
2019-07-30 17:09:42 -0700

    ref: encode id for refund
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/resolvers/Refund/index.js
+++ imports/plugins/core/orders/server/no-meteor/resolvers/Refund/index.js
@@ -1,10 +1,11 @@
+import { encodePaymentOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/payment";
 import { encodeRefundOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/refund";
 import amount from "./amount";
 import createdAt from "./createdAt";
 
 export default {
+  _id: (node) => encodeRefundOpaqueId(node._id),
   amount,
-  createdAt
   createdAt,
   paymentId: (node) => encodePaymentOpaqueId(node.paymentId)
 };
