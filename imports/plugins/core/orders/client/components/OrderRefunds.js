315fb1a0583fe20317c77db8e8c991c2a98637d1
Erik Kieckhafer <ek@ato.la>
2019-08-01 16:06:58 -0700

    add step
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/OrderRefunds.js
+++ imports/plugins/core/orders/client/components/OrderRefunds.js
@@ -206,6 +206,7 @@ function OrderRefunds(props) {
                                           onChange={handleRefundAmountChange}
                                           onChanging={handleRefundAmountChange}
                                           placeholder="0.00"
+                                          step=".01"
                                           type="number"
                                         />
                                         <ErrorsBlock names={["amounts"]} />
