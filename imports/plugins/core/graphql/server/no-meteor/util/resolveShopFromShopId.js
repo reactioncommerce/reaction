1c3f7a5427dd1461dcd60dbb1b10474e7acbeaf7
Erik Kieckhafer <ek@ato.la>
2019-08-02 10:16:33 -0700

    docs: jsdoc update
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/graphql/server/no-meteor/util/resolveShopFromShopId.js
+++ imports/plugins/core/graphql/server/no-meteor/util/resolveShopFromShopId.js
@@ -4,7 +4,7 @@
  * @memberof GraphQL/ResolverUtilities
  * @summary A generic resolver that gets the shop object for the provided parent result, assuming it has a `shopId` property
  * @param {Object} parent - result of the parent resolver
- * @param {Object} args - an object of all arguments that were sent by the client
+ * @param {Object} _ - an object of all arguments that were sent by the client
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Object>} The shop having ID parent.shopId, in GraphQL schema format
  */
