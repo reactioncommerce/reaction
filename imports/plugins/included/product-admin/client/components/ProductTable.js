0f651c4c91f85d3696cd71e18bca16976a90340a
Machiko Yasuda <machiko@reactioncommerce.com>
2019-08-06 14:08:04 -0700

    feat(producttable): first pass at card for product table with upload button
    Signed-off-by: Machiko Yasuda <machiko@reactioncommerce.com>

--- imports/plugins/included/product-admin/client/components/ProductTable.js
+++ imports/plugins/included/product-admin/client/components/ProductTable.js
@@ -1,10 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
+import { Grid, Button, Card, CardHeader, CardContent, IconButton, Typography, makeStyles } from "@material-ui/core";
+import CloseIcon from "mdi-material-ui/Close";
+import ImportIcon from "mdi-material-ui/Download";
+import Dropzone from "react-dropzone";
 import { i18next } from "/client/api";
 import withCreateProduct from "../hocs/withCreateProduct";
+
+const useStyles = makeStyles(theme => ({
+  leftIcon: {
+    marginRight: theme.spacing(1),
+  },
+  helpText: {
+    marginLeft: "20px",
+    letterSpacing: "0.28px",
+    fontWeight: theme.typography.fontWeightRegular
+  },
+  cardHeaderTitle: {
+    letterSpacing: "0.3px"
+  },
+  cardContainer: {
+    alignItems: "center"
+  },
+  dropzone: {
+    display: "inline-block"
+  }
+}));
 
 /**
  * ProductTable component
@@ -12,8 +34,54 @@ import withCreateProduct from "../hocs/withCreateProduct";
  * @returns {Node} React node
  */
 function ProductTable({ onCreateProduct }) {
+  const classes = useStyles();
+
   return (
     <Grid container spacing={3}>
+      <Grid item sm={12}>
+        <Card raised>
+          <CardHeader
+            className={classes.cardHeaderTitle}
+            action={
+              <IconButton aria-label="close">
+                <CloseIcon />
+              </IconButton>
+            }
+            title="Filter products by file"
+          />
+          <CardContent>
+            <Grid container spacing={1} className={classes.cardContainer}>
+              <Grid item sm={12}>
+                <Dropzone
+                  className={classes.dropzone}
+                  disableClick
+                  multiple
+                  disablePreview
+                  onDrop={(files) => {
+                    console.log(files)
+                  }}
+                  ref={(inst) => { this.dropzone = inst; }}
+                  accept="csv"
+                  >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      this.dropzone && this.dropzone.open();
+                    }}
+                  >
+                    <ImportIcon className={classes.leftIcon}/>
+                    Import
+                  </Button>
+                </Dropzone>
+                <Typography variant="h5" display="inline" className={classes.helpText}>
+                  Import a .csv file with a list of product IDs, separated by commas.
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
       <Grid item sm={12}>
         <Button
           color="primary"
