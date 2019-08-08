ee7a43a10d62b00cce76dbd0beca3d3853349570
Erik Kieckhafer <ek@ato.la>
2019-08-08 13:52:47 -0700

    chore: remove blaze templates of old orders UI
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/index.js
+++ imports/plugins/core/orders/client/index.js
@@ -16,14 +16,6 @@ import "./templates/list/pdf.js";
 // To remove with orders-2.0
 // To remove with orders-2.0
 import "./helpers";
-import "./templates/list/summary.html";
-import "./templates/list/summary.js";
-import "./templates/workflow/shippingTracking.html";
-import "./templates/workflow/shippingTracking.js";
-import "./templates/workflow/workflow.html";
-import "./templates/workflow/workflow.js";
-import "./templates/orders.html";
-import "./templates/orders.js";
 import "./containers/invoiceContainer";
 import "./containers/orderSummaryContainer";
 import "../lib/extendShopSchema";
