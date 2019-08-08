51ba53368a347c361b633ba16bb4583022583901
Erik Kieckhafer <ek@ato.la>
2019-08-08 14:18:18 -0700

    remove unused meteor style graphql methods
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/graphql/index.js
+++ imports/plugins/core/orders/client/graphql/index.js
@@ -3,11 +3,9 @@ import simpleGraphQLClient from "/imports/plugins/core/graphql/lib/helpers/simpl
 import getOpaqueIds from "/imports/plugins/core/core/client/util/getOpaqueIds";
 import approveOrderPaymentsMutation from "./approveOrderPayments.graphql";
 import captureOrderPaymentsMutation from "./captureOrderPayments.graphql";
-import updateOrderFulfillmentGroupMutation from "./updateOrderFulfillmentGroup.graphql";
 
 const approveOrderPaymentsMutate = simpleGraphQLClient.createMutationFunction(approveOrderPaymentsMutation);
 const captureOrderPaymentsMutate = simpleGraphQLClient.createMutationFunction(captureOrderPaymentsMutation);
-const updateOrderFulfillmentGroupMutate = simpleGraphQLClient.createMutationFunction(updateOrderFulfillmentGroupMutation);
 
 export const approveOrderPayments = async ({ orderId, paymentIds, shopId }) => {
   // Convert MongoDB IDs to GraphQL IDs
@@ -58,30 +56,5 @@ export const captureOrderPayments = async ({ orderId, paymentIds, shopId }) => {
       paymentIds: opaquePaymentIds,
       shopId: opaqueShopId
     }
-  });
-};
-
-export const updateOrderFulfillmentGroup = async ({
-  orderId,
-  orderFulfillmentGroupId,
-  status,
-  tracking
-}) => {
-  // Convert MongoDB IDs to GraphQL IDs
-  const conversionRequests = [
-    { namespace: "Order", id: orderId },
-    { namespace: "OrderFulfillmentGroup", id: orderFulfillmentGroupId }
-  ];
-
-  const [
-    opaqueOrderId,
-    opaqueOrderFulfillmentGroupId
-  ] = await getOpaqueIds(conversionRequests);
-
-  return updateOrderFulfillmentGroupMutate({
-    orderFulfillmentGroupId: opaqueOrderFulfillmentGroupId,
-    orderId: opaqueOrderId,
-    status,
-    tracking
   });
 };
