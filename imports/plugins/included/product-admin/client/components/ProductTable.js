f459b660091eb676ad4e21cd2fc71a3202f224f7
Machiko Yasuda <machiko@reactioncommerce.com>
2019-08-12 22:35:34 -0700

    style(card): slide
    Signed-off-by: Machiko Yasuda <machiko@reactioncommerce.com>

--- imports/plugins/included/product-admin/client/components/ProductTable.js
+++ imports/plugins/included/product-admin/client/components/ProductTable.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Components } from "@reactioncommerce/reaction-components";
-import { Grid, Button, Card, CardHeader, CardContent, IconButton, Typography, makeStyles } from "@material-ui/core";
+import { Slide, Grid, Button, Card, CardHeader, CardContent, IconButton, Typography, makeStyles } from "@material-ui/core";
 import CloseIcon from "mdi-material-ui/Close";
 import ImportIcon from "mdi-material-ui/Download";
 import Dropzone from "react-dropzone";
@@ -41,9 +41,11 @@ function ProductTable({ onCreateProduct }) {
   if ( isClosed === true ) {
     displayCard = "none";
     displayButton = "block";
+    fade = false;
   } else {
-    displayCard = "block"
+    displayCard = "block";
     displayButton = "none";
+    fade = true;
   };
 
   const closeCard = () => {
@@ -52,50 +54,52 @@ function ProductTable({ onCreateProduct }) {
 
   return (
     <Grid container spacing={3}>
-      <Grid item sm={12} style={{ display: displayCard }}>
-        <Card raised>
-          <CardHeader
-            className={classes.cardHeaderTitle}
-            action={
-              <IconButton aria-label="close" onClick={() => setClosed(true)}>
-                <CloseIcon/>
-              </IconButton>
-            }
-            title="Filter products by file"
-          />
-          <CardContent>
-            <Grid container spacing={1} className={classes.cardContainer}>
-              <Grid item sm={12}>
-                <Dropzone
-                  className={classes.dropzone}
-                  disableClick
-                  multiple
-                  disablePreview
-                  onDrop={(files) => {
-                    console.log(files)
-                  }}
-                  ref={(inst) => { this.dropzone = inst; }}
-                  accept="csv"
-                  >
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => {
-                      this.dropzone && this.dropzone.open();
+      <Slide direction="down" in={fade} mountOnEnter unmountOnExit>
+        <Grid item sm={12} className={classes.card} style={{ display: displayCard }}>
+          <Card raised>
+            <CardHeader
+              className={classes.cardHeaderTitle}
+              action={
+                <IconButton aria-label="close" onClick={() => setClosed(true)}>
+                  <CloseIcon/>
+                </IconButton>
+              }
+              title="Filter products by file"
+            />
+            <CardContent>
+              <Grid container spacing={1} className={classes.cardContainer}>
+                <Grid item sm={12}>
+                  <Dropzone
+                    className={classes.dropzone}
+                    dzisableClick
+                    multiple
+                    disablePreview
+                    onDrop={(files) => {
+                      console.log(files)
                     }}
-                  >
-                    <ImportIcon className={classes.leftIcon}/>
-                    Import
-                  </Button>
-                </Dropzone>
-                <Typography variant="h5" display="inline" className={classes.helpText}>
-                  Import a .csv file with a list of product IDs, separated by commas.
-                </Typography>
+                    ref={(inst) => { this.dropzone = inst; }}
+                    accept="csv"
+                    >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => {
+                        this.dropzone && this.dropzone.open();
+                      }}
+                    >
+                      <ImportIcon className={classes.leftIcon}/>
+                      Import
+                    </Button>
+                  </Dropzone>
+                  <Typography variant="h5" display="inline" className={classes.helpText}>
+                    Import a .csv file with a list of product IDs, separated by commas.
+                  </Typography>
+                </Grid>
               </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Slide>
       <Grid item sm={12} style={{ display: displayButton }}>
         <Button
           color="primary"
