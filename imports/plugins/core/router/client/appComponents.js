d8292fd49e1e7e4bcd6309825f967617916d40a5
Mike Murray <hellomikemurray@gmail.com>
2019-08-20 16:00:47 -0700

    feat: use core-js 3
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/router/client/appComponents.js
+++ imports/plugins/core/router/client/appComponents.js
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import iconClear from "@reactioncommerce/components/svg/iconClear";
 import iconError from "@reactioncommerce/components/svg/iconError";
 import iconValid from "@reactioncommerce/components/svg/iconValid";
