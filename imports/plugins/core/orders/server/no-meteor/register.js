6381a8dba4615b403ffc6bd30aba188045bf0a07
Erik Kieckhafer <ek@ato.la>
2019-08-21 10:45:06 -0700

    refactor: remove unused layout registery items
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/server/no-meteor/register.js
+++ imports/plugins/core/orders/server/no-meteor/register.js
@@ -46,28 +46,6 @@ export default async function register(app) {
       name: "Orders"
     },
     registry: [{
-      route: "/dashboard/orders",
-      provides: ["dashboard"],
-      workflow: "coreOrderWorkflow",
-      name: "orders",
-      label: "Orders",
-      description: "Fulfill your orders",
-      icon: "fa fa-sun-o",
-      priority: 0,
-      container: "core",
-      template: "orders"
-    }, {
-      route: "/dashboard/orders",
-      name: "dashboard/orders",
-      provides: ["shortcut"],
-      label: "Orders",
-      description: "Fulfill your orders",
-      icon: "fa fa-sun-o",
-      priority: 1,
-      container: "dashboard",
-      template: "orders",
-      audience: ["seller"]
-    }, {
       route: "/dashboard/pdf/orders/:id",
       workflow: "coreOrderPrintWorkflow",
       layout: "printLayout",
@@ -86,22 +64,6 @@ export default async function register(app) {
       name: "order/view"
     }],
     layout: [{
-      layout: "coreLayout",
-      workflow: "coreOrderWorkflow",
-      collection: "Orders",
-      theme: "default",
-      enabled: true,
-      structure: {
-        template: "orders",
-        layoutHeader: "NavBar",
-        layoutFooter: "Footer",
-        notFound: "notFound",
-        dashboardHeader: "dashboardHeader",
-        dashboardHeaderControls: "orderListFilters",
-        dashboardControls: "dashboardControls",
-        adminControlsFooter: "adminControlsFooter"
-      }
-    }, {
       layout: "printLayout",
       workflow: "coreOrderPrintWorkflow",
       collection: "Orders",
@@ -111,49 +73,6 @@ export default async function register(app) {
         layoutHeader: "NavBar",
         layoutFooter: "Footer"
       }
-    }, {
-      layout: "coreLayout",
-      workflow: "coreOrderShipmentWorkflow",
-      collection: "Orders",
-      theme: "default",
-      enabled: true
-    }, {
-      label: "Order Processing",
-      status: "created",
-      workflow: "coreOrderWorkflow",
-      audience: ["dashboard/orders"]
-    }, {
-      template: "coreOrderProcessing",
-      label: "Order Processing",
-      status: "processing",
-      workflow: "coreOrderWorkflow",
-      audience: ["dashboard/orders"]
-    }, {
-      template: "coreOrderCompleted",
-      label: "Order Completed",
-      status: "completed",
-      workflow: "coreOrderWorkflow",
-      audience: ["dashboard/orders"]
-    }, { // Standard Order Item workflow
-      label: "Inventory Adjust",
-      workflow: "coreOrderItemWorkflow",
-      status: "inventoryAdjusted",
-      audience: ["dashboard/orders"]
-    }, {
-      label: "Item Payment Captured",
-      workflow: "coreOrderItemWorkflow",
-      status: "captured",
-      audience: ["dashboard/orders"]
-    }, {
-      label: "Item Ship",
-      workflow: "coreOrderItemWorkflow",
-      status: "shipped",
-      audience: ["dashboard/orders"]
-    }, {
-      label: "Item Delivered",
-      workflow: "coreOrderItemWorkflow",
-      status: "completed",
-      audience: ["dashboard/orders"]
     }]
   });
 }
