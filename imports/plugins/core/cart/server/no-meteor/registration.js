276030b1ff78a08d15239222e3dbb98728a6b3a3
Eric Dobbertin <eric@dairystatedesigns.com>
2019-08-27 11:32:16 -0500

    feat: validate cart transform registration
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/cart/server/no-meteor/registration.js
+++ imports/plugins/core/cart/server/no-meteor/registration.js
@@ -1,3 +1,11 @@
+import SimpleSchema from "simpl-schema";
+
+const transformSchema = new SimpleSchema({
+  name: String,
+  fn: Function,
+  priority: Number
+});
+
 // Objects with `name`, `priority` and `fn` properties
 export const cartTransforms = [];
 
@@ -6,12 +14,14 @@ export const cartTransforms = [];
  * @param {Object} options The options object that the plugin passed to registerPackage
  * @returns {undefined}
  */
-export function registerPluginHandler({ cart }) {
+export function registerPluginHandler({ name, cart }) {
   if (cart) {
     const { transforms } = cart;
-    if (Array.isArray(transforms)) {
-      cartTransforms.push(...transforms);
-      cartTransforms.sort((prev, next) => prev.priority - next.priority);
-    }
+
+    if (!Array.isArray(transforms)) throw new Error(`In ${name} plugin registerPlugin object, cart.transforms must be an array`);
+    transformSchema.validate(transforms);
+
+    cartTransforms.push(...transforms);
+    cartTransforms.sort((prev, next) => prev.priority - next.priority);
   }
 }
