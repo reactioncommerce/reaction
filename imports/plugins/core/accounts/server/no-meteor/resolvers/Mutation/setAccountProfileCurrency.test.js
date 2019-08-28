bdb46e3f46b4d3de49460a99216442ab3c1a5f84
Erik Kieckhafer <ek@ato.la>
2019-08-28 10:50:13 -0700

    test: add test for mutation
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ imports/plugins/core/accounts/server/no-meteor/resolvers/Mutation/setAccountProfileCurrency.test.js
@@ -0,0 +1,32 @@
+import { encodeAccountOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/account";
+import mockContext from "/imports/test-utils/helpers/mockContext";
+import setAccountProfileCurrency from "./setAccountProfileCurrency";
+
+mockContext.mutations.setAccountProfileCurrency = jest.fn().mockName("mutations.setAccountProfileCurrency");
+
+test("correctly passes through to internal mutation function", async () => {
+  const accountId = encodeAccountOpaqueId("1");
+  const currencyCode = "USD";
+
+  const fakeResult = { _id: "1", profile: { currencyCode: "USD" } };
+
+  mockContext.mutations.setAccountProfileCurrency.mockReturnValueOnce(Promise.resolve(fakeResult));
+
+  const result = await setAccountProfileCurrency(null, {
+    input: {
+      accountId,
+      currencyCode,
+      clientMutationId: "clientMutationId"
+    }
+  }, mockContext);
+
+  expect(mockContext.mutations.setAccountProfileCurrency).toHaveBeenCalledWith(
+    mockContext,
+    { decodedAccountId: "1", currencyCode: "USD" }
+  );
+
+  expect(result).toEqual({
+    account: fakeResult,
+    clientMutationId: "clientMutationId"
+  });
+});
