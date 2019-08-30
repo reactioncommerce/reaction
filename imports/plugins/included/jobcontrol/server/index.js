14739768a8ee03533a3e45077970faf87e01db5c
Eric Dobbertin <eric@dairystatedesigns.com>
2019-08-29 20:32:28 -0500

    chore: delete unneeded jobcontrol i18n files
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/included/jobcontrol/server/index.js
+++ imports/plugins/included/jobcontrol/server/index.js
@@ -1,6 +1,5 @@
 import { cleanupJob, addCleanupJobControlHook } from "./jobs/cleanup";
 import { fetchRateJobs, setupFetchFlushCurrencyHooks } from "./jobs/exchangerates";
-import "./i18n";
 
 addCleanupJobControlHook();
 cleanupJob();
