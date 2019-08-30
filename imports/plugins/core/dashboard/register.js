d86ea245aa0134d06db22d57ba127c6c068888fd
Eric Dobbertin <eric@dairystatedesigns.com>
2019-08-29 19:54:19 -0500

    refactor: use registerPlugin for dashboard i18n
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/dashboard/register.js
+++ imports/plugins/core/dashboard/register.js
@@ -1,67 +1,11 @@
+/**
+ * This file is necessary for backwards compatibility while we refactor
+ * the API to remove Meteor. The no-meteor `register.js` file will
+ * eventually become the main entry point of the plugin, but for now
+ * our Meteor tooling loads this file, so we include this here as a
+ * temporary bridge.
+ */
 import Reaction from "/imports/plugins/core/core/server/Reaction";
+import register from "./server/no-meteor/register";
 
-Reaction.registerPackage({
-  label: "Dashboard",
-  name: "reaction-dashboard",
-  icon: "fa fa-th",
-  settings: {
-    name: "Dashboard"
-  },
-  registry: [{
-    provides: ["dashboard"],
-    workflow: "coreDashboardWorkflow",
-    name: "dashboardPackages",
-    label: "Core",
-    description: "Reaction core shop configuration",
-    icon: "fa fa-th",
-    priority: 0,
-    container: "core",
-    permissions: [{
-      label: "Dashboard",
-      permission: "dashboard"
-    }]
-  }, {
-    route: "/dashboard",
-    name: "dashboard",
-    workflow: "coreDashboardWorkflow",
-    provides: ["shortcut"],
-    label: "Dashboard",
-    template: "dashboardPackages",
-    icon: "icon-reaction-logo",
-    priority: 0,
-    permissions: [{
-      label: "Dashboard",
-      permission: "dashboard"
-    }]
-  }, {
-    route: "/dashboard/shop/settings",
-    template: "shopSettings",
-    name: "shopSettings",
-    label: "Shop Settings",
-    icon: "fa fa-th",
-    provides: ["settings"],
-    container: "dashboard"
-  }, {
-    label: "Options",
-    provides: ["shopSettings"],
-    container: "dashboard",
-    template: "optionsShopSettings",
-    showForShopTypes: ["primary"]
-  }],
-  layout: [{
-    layout: "coreLayout",
-    workflow: "coreDashboardWorkflow",
-    theme: "default",
-    enabled: true,
-    structure: {
-      template: "dashboardPackages",
-      layoutHeader: "NavBar",
-      layoutFooter: "",
-      notFound: "notFound",
-      dashboardHeader: "dashboardHeader",
-      dashboardControls: "dashboardControls",
-      dashboardHeaderControls: "dashboardHeaderControls",
-      adminControlsFooter: "adminControlsFooter"
-    }
-  }]
-});
+Reaction.whenAppInstanceReady(register);
