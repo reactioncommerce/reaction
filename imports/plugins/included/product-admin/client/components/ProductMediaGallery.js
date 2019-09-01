f7c57a97a9a547e5414c86a0a9070c24f2c6b105
Eric Dobbertin <eric@dairystatedesigns.com>
2019-09-01 11:46:08 -0500

    feat: use MediaUploader for product media
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/included/product-admin/client/components/ProductMediaGallery.js
+++ imports/plugins/included/product-admin/client/components/ProductMediaGallery.js
@@ -1,9 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useDropzone } from "react-dropzone";
 import { i18next } from "/client/api";
-import Button from "@material-ui/core/Button";
-import LinearProgress from "@material-ui/core/LinearProgress";
+import { Components } from "@reactioncommerce/reaction-components";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
@@ -18,21 +16,36 @@ import ProductMediaItem from "./ProductMediaItem";
  * @returns {Node} React component
  */
 function ProductMediaGallery(props) {
-  const { editable, media, onSetMediaPriority, onRemoveMedia, uploadProgress } = props;
-
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop: (files) => {
-      if (files.length === 0) return;
-      props.onDrop(files);
-    },
-    multiple: true,
-    disablePreview: true,
-    accept: "image/jpg, image/png, image/jpeg",
-    disableClick: true
-  });
+  const {
+    editable,
+    media,
+    onRemoveMedia,
+    onSetMediaPriority,
+    productId,
+    shopId,
+    userId,
+    variantId
+  } = props;
 
   if (editable) {
-    const hasMedia = Array.isArray(media) && media.length > 0;
+    let count = (Array.isArray(media) && media.length) || 0;
+    const hasMedia = count > 0;
+
+    const getFileMetadata = () => {
+      count += 1;
+      return {
+        ownerId: userId,
+        productId,
+        variantId,
+        shopId,
+        priority: count,
+        toGrid: 1 // we need number
+      };
+    };
+
+    const onUploadError = (error) => {
+      Alerts.toast(error.reason || error.message, "error");
+    };
 
     return (
       <div className="rui media-gallery">
@@ -57,22 +70,13 @@ function ProductMediaGallery(props) {
                 />
               ))
             )}
-            {!!uploadProgress && (
-              <TableRow>
-                <TableCell colSpan={3}>
-                  <LinearProgress indeterminate={true} />
-                </TableCell>
-              </TableRow>
-            )}
             <TableRow>
-              <TableCell colSpan={3} {...getRootProps({ className: "dropzone" })}>
-                <Button
-                  fullWidth
-                  size="large"
-                >
-                  <input {...getInputProps()} />
-                  {"Drag image or click to upload"}
-                </Button>
+              <TableCell colSpan={3}>
+                <Components.MediaUploader
+                  canUploadMultiple
+                  metadata={getFileMetadata}
+                  onError={onUploadError}
+                />
               </TableCell>
             </TableRow>
           </TableBody>
@@ -80,7 +84,6 @@ function ProductMediaGallery(props) {
       </div>
     );
   }
-
 
   return (
     <Table>
@@ -101,18 +104,15 @@ function ProductMediaGallery(props) {
 ProductMediaGallery.propTypes = {
   editable: PropTypes.bool, // eslint-disable-line react/boolean-prop-naming
   media: PropTypes.arrayOf(PropTypes.object),
-  onDrop: PropTypes.func,
   onRemoveMedia: PropTypes.func,
   onSetMediaPriority: PropTypes.func,
-  uploadProgress: PropTypes.shape({
-    bytesUploaded: PropTypes.number.isRequired,
-    bytesTotal: PropTypes.number.isRequired,
-    percentage: PropTypes.number.isRequired
-  })
+  productId: PropTypes.string,
+  shopId: PropTypes.string,
+  userId: PropTypes.string,
+  variantId: PropTypes.string
 };
 
 ProductMediaGallery.defaultProps = {
-  onDrop() {},
   onRemoveMedia() {}
 };
 
