da03d98add5bd99ea00b82c70a0a0c1dc8cc1125
Mia Steinkirch <mia.steinkirch@gmail.com>
2019-09-02 10:45:21 -1000

    add unit tests as well
    Signed-off-by: Mia Steinkirch <mia.steinkirch@gmail.com>

--- tests/integration/api/queries/orderById/orderById.test.js
+++ tests/integration/api/queries/orderById/orderById.test.js
@@ -1,8 +1,8 @@
 import Factory from "/imports/test-utils/helpers/factory";
 import TestApp from "/imports/test-utils/helpers/TestApp";
 
-const orderId = "integration-test-order-id"; 
-const fakeShopId = "integration-test-shop-id";
+const orderId = "cmVhY3Rpb24vc2hvcDoxMjM=";
+const fakeShopId = "cmVhY3Rpb24vc2hvcDoxMjM=";
 const shopName = "Test Shop";
 
 
@@ -33,8 +33,15 @@ beforeAll(async () => {
 
 afterAll(() => testApp.stop());
 
+function makeContext() {
+  return { accountId: "unit-test-account-id", userHasPermission: () => true };
+}
 
-test("get order successful", async () => {
-  const result = await query({ id: orderId, shopId: fakeShopId, token: null });
-  expect(result.shop.name).toBe(shopName);
+test("getOrderQuery shop admin", async () => {
+  const shopId = "unit-test-shop-id";
+  const orderId2 = "unit-test-order-id";
+  const context = makeContext();
+  const qq = await query(context, { _id: orderId2 }, shopId, null);
+  expect(qq).toMatchObject({ _id: "unit-test-order-id", shopId });
+  expect(qq.accountId).toBeUndefined();
 });
