1998b084de2b24fb06cea23e20170f4d5a066bbd
Erik Kieckhafer <ek@ato.la>
2019-09-03 13:47:44 -0700

    chore: add migration for cart and shipping fulfillmentTypes
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ imports/plugins/core/versions/server/migrations/72_add_fulfillmentType_to_shipping_methods.js
@@ -0,0 +1,42 @@
+import { Migrations } from "meteor/percolate:migrations";
+import rawCollections from "/imports/collections/rawCollections";
+
+Migrations.add({
+  version: 72,
+  async up() {
+    const { Cart, Shipping } = rawCollections;
+
+    await Shipping.updateMany({
+      "provider.name": "flatRates"
+    }, {
+      $set: {
+        "methods.$[].fulfillmentTypes": ["shipping"]
+      }
+    });
+
+    await Cart.updateMany({}, {
+      $set: {
+        "shipping.$[].shipmentMethod.fulfillmentTypes": ["shipping"],
+        "shipping.$[].shipmentQuotes.$[].method.fulfillmentTypes": ["shipping"],
+      }
+    });
+  },
+  async down() {
+    const { Cart, Shipping } = rawCollections;
+
+    await Shipping.updateMany({
+      "provider.name": "flatRates"
+    }, {
+      $unset: {
+        "methods.$[].fulfillmentTypes": ""
+      }
+    });
+
+    await Cart.updateMany({}, {
+      $unset: {
+        "shipping.$[].shipmentMethod.fulfillmentTypes": "",
+        "shipping.$[].shipmentQuotes.$[].method.fulfillmentTypes": ""
+      }
+    });
+  }
+});
