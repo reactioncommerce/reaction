00dad4d5db254f5f350c0412d90bc426ef8e4286
Erik Kieckhafer <ek@ato.la>
2019-09-03 20:48:08 -0700

    refactor: keep required accountID and add internalCall
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/accounts/server/no-meteor/mutations/removeAccountAddressBookEntry.js
+++ imports/plugins/core/accounts/server/no-meteor/mutations/removeAccountAddressBookEntry.js
@@ -12,7 +12,7 @@ const inputSchema = new SimpleSchema({
  * @summary Remove existing address in user's profile
  * @param {Object} context - GraphQL execution context
  * @param {Object} input - Necessary input for mutation. See SimpleSchema.
- * @param {String} [input.accountId] - optional decoded ID of account on which entry should be updated, for admin
+ * @param {String} input.accountId - optional decoded ID of account on which entry should be updated, for admin
  * @param {String} input.addressId - decoded ID of the address to remove
  * @returns {Promise<Object>} with removed address
  */
@@ -21,16 +21,15 @@ export default async function removeAccountAddressBookEntry(context, input) {
   const { appEvents, collections, userHasPermission, userId: userIdFromContext } = context;
   const { Accounts } = collections;
   const {
-    accountId: providedAccountId,
+    accountId,
     addressId
   } = input;
 
-  const accountId = providedAccountId || userIdFromContext;
   const account = await Accounts.findOne({ _id: accountId });
   if (!account) throw new ReactionError("not-found", "Not Found");
 
-  if (userIdFromContext !== providedAccountId && !userHasPermission(["reaction-accounts"], account.shopId)) {
-    throw new ReactionError("access-denied", "Access denied");
+  if (!context.isInternalCall && userIdFromContext !== accountId) {
+    if (!userHasPermission(["reaction-accounts"], account.shopId)) throw new ReactionError("access-denied", "Access denied");
   }
 
   const addressBeingRemoved = account.profile && Array.isArray(account.profile.addressBook) &&
