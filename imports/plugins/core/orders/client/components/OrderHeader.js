cd212500dcf270182ea4cac0fc75be34e7a1d726
Erik Kieckhafer <ek@ato.la>
2019-09-04 23:27:47 -0700

    refactor: use moments calendar format to display time alongside date on order
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/OrderHeader.js
+++ imports/plugins/core/orders/client/components/OrderHeader.js
@@ -26,7 +26,9 @@ const styles = (theme) => ({
 function OrderHeader(props) {
   const { classes, moment, order } = props;
   const { createdAt, displayStatus, referenceId, status } = order;
-  const orderDate = (moment && moment(createdAt).format("MM/DD/YYYY")) || createdAt.toLocaleString();
+  const orderDate = (moment && moment(createdAt).calendar(null, {
+    sameElse: "DD/MM/YYYY h:mm A"
+  })) || createdAt.toLocaleString();
   const { payments } = order;
   const paymentStatuses = payments.map((payment) => payment.status);
   const uniqueStatuses = [...new Set(paymentStatuses)];
