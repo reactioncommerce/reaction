1d6eb1b02fd786a3ddb6054aa181c765c1b7e262
Erik Kieckhafer <ek@ato.la>
2019-09-04 23:34:07 -0700

    refactor: reformat date
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/orders/client/components/OrderHeader.js
+++ imports/plugins/core/orders/client/components/OrderHeader.js
@@ -27,7 +27,7 @@ function OrderHeader(props) {
   const { classes, moment, order } = props;
   const { createdAt, displayStatus, referenceId, status } = order;
   const orderDate = (moment && moment(createdAt).calendar(null, {
-    sameElse: "DD/MM/YYYY h:mm A"
+    sameElse: "MMMM DD, YYYY h:mm A"
   })) || createdAt.toLocaleString();
   const { payments } = order;
   const paymentStatuses = payments.map((payment) => payment.status);
