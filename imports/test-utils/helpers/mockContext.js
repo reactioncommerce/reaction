ab5075cee78c7ca770b404543531c7c2febd11ab
Will Lopez <will.lopez77@gmail.com>
2019-09-09 12:01:50 -0700

    fix: failing tests
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/test-utils/helpers/mockContext.js
+++ imports/test-utils/helpers/mockContext.js
@@ -1,8 +1,8 @@
 const mockContext = {
   accountId: "FAKE_ACCOUNT_ID",
   appEvents: {
-    emit() {},
-    on() {}
+    emit() { },
+    on() { }
   },
   collections: {},
   getAbsoluteUrl: jest.fn().mockName("getAbsoluteUrl").mockImplementation((path) => {
@@ -33,8 +33,8 @@ export function mockCollection(collectionName) {
       throw new Error("update mongo method is deprecated, use updateOne or updateMany");
     },
     bulkWrite: jest.fn().mockName(`${collectionName}.bulkWrite`).mockReturnValue(Promise.resolve({
-      nMatched: 2,
-      nModified: 2,
+      nMatched: 1,
+      nModified: 1,
       result: {
         writeErrors: []
       }
