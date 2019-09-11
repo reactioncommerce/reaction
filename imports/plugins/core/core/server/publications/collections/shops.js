5d707f224046cc271913ef259605b02955826bae
Eric Dobbertin <eric@dairystatedesigns.com>
2019-09-11 14:17:02 -0500

    chore: remove outdated comment
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/core/core/server/publications/collections/shops.js
+++ imports/plugins/core/core/server/publications/collections/shops.js
@@ -1,10 +1,8 @@
 import { Meteor } from "meteor/meteor";
 import { Shops } from "/lib/collections";
 
-// We should be able to publish just the enabled languages/currencies/
 Meteor.publish("PrimaryShop", () => Shops.find({
   shopType: "primary"
 }, {
-  fields: {},
   limit: 1
 }));
