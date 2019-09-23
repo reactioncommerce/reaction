6485e680bdb2374832d557c7a52d6410e9da019e
Eric Dobbertin <eric@dairystatedesigns.com>
2019-09-23 16:23:48 -0500

    feat: remove exchange rate updater job
    Exchange rate feature isn't currently used. Setting the rates
    some other way should still work if you use it.
    
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- imports/plugins/included/jobcontrol/server/index.js
+++ imports/plugins/included/jobcontrol/server/index.js
@@ -1,8 +1,4 @@
 import { cleanupJob, addCleanupJobControlHook } from "./jobs/cleanup";
-import { fetchRateJobs, setupFetchFlushCurrencyHooks } from "./jobs/exchangerates";
 
 addCleanupJobControlHook();
 cleanupJob();
-
-setupFetchFlushCurrencyHooks();
-fetchRateJobs();
