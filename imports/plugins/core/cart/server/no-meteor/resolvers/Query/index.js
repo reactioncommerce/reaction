377174b2264ca770834177c71a26666861aacdf4
Erik Kieckhafer <ek@ato.la>
2019-10-01 10:39:34 -0700

    refactor: remove query from resolver
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/plugins/core/cart/server/no-meteor/resolvers/Query/index.js
+++ imports/plugins/core/cart/server/no-meteor/resolvers/Query/index.js
@@ -1,9 +1,7 @@
 import accountCartByAccountId from "./accountCartByAccountId";
 import anonymousCartByCartId from "./anonymousCartByCartId";
-import getCommonOrderForCartGroup from "./getCommonOrderForCartGroup";
 
 export default {
   accountCartByAccountId,
-  anonymousCartByCartId,
-  getCommonOrderForCartGroup
+  anonymousCartByCartId
 };
