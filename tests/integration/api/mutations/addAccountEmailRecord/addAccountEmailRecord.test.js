0e7e797a4ebaf6104a47700ef2dea0de199eb7fb
Chris Potter <chris@reactioncommerce.com>
2019-10-02 16:12:29 -0400

    adds test files. returning null
    Signed-off-by: Chris Potter <chris@reactioncommerce.com>

---
+++ tests/integration/api/mutations/addAccountEmailRecord/addAccountEmailRecord.test.js
@@ -0,0 +1,44 @@
+import Factory from "/imports/test-utils/helpers/factory";
+import TestApp from "/imports/test-utils/helpers/TestApp";
+import AddAccountEmailRecordMutation from "./AddAccountEmailRecordMutation.graphql";
+
+jest.setTimeout(300000);
+
+let testApp;
+let addAccountEmailRecord;
+beforeAll(async () => {
+  testApp = new TestApp();
+  await testApp.start();
+  await testApp.insertPrimaryShop();
+  addAccountEmailRecord = testApp.mutate(AddAccountEmailRecordMutation);
+});
+
+afterAll(async () => {
+  await testApp.collections.Shops.deleteMany({});
+  await testApp.stop();
+});
+
+const accountInternalId = "123";
+const accountOpaqueId = "cmVhY3Rpb24vYWNjb3VudDoxMjM=";
+
+test("user can add an email to their own account", async () => {
+  await testApp.setLoggedInUser({ _id: accountInternalId });
+
+  const email = Factory.Email.makeOne();
+
+  // _id is set by the server
+  delete email._id;
+
+  let result;
+  try {
+    result = await addAccountEmailRecord({ accountId: accountOpaqueId, email });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  console.log(email);
+  console.log(result);
+
+  expect(result.addAccountEmailRecord.emailRecord).toEqual(email);
+});
