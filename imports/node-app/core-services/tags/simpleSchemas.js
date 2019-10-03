f22d4963e0ab241da7663eb76dc95396b570c561
Erik Kieckhafer <ek@ato.la>
2019-10-03 14:04:53 -0700

    refactor: move Metafield to top of file
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/node-app/core-services/tags/simpleSchemas.js
+++ imports/node-app/core-services/tags/simpleSchemas.js
@@ -23,6 +23,47 @@ export function createdAtAutoValue() {
 }
 
 /**
+ * @name Metafield
+ * @memberof Schemas
+ * @type {SimpleSchema}
+ * @property {String} key optional
+ * @property {String} namespace optional
+ * @property {String} scope optional
+ * @property {String} value optional
+ * @property {String} valueType optional
+ * @property {String} description optional
+ */
+const Metafield = new SimpleSchema({
+  key: {
+    type: String,
+    max: 30,
+    optional: true
+  },
+  namespace: {
+    type: String,
+    max: 20,
+    optional: true
+  },
+  scope: {
+    type: String,
+    optional: true
+  },
+  value: {
+    type: String,
+    optional: true
+  },
+  valueType: {
+    type: String,
+    optional: true
+  },
+  description: {
+    type: String,
+    optional: true
+  }
+});
+
+
+/**
  * @name Tag
  * @memberof Schemas
  * @type {SimpleSchema}
