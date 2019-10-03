32997e7d26bf89a63303c76e36e140ff83b76ac3
Erik Kieckhafer <ek@ato.la>
2019-10-03 14:09:28 -0700

    fix: move extra metafield const
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- imports/node-app/core-services/tags/simpleSchemas.js
+++ imports/node-app/core-services/tags/simpleSchemas.js
@@ -62,7 +62,6 @@ const Metafield = new SimpleSchema({
   }
 });
 
-
 /**
  * @name Tag
  * @memberof Schemas
@@ -149,45 +148,4 @@ export const Tag = new SimpleSchema({
     optional: true
   },
   "featuredProductIds.$": String
-});
-
-
-/**
- * @name Metafield
- * @memberof Schemas
- * @type {SimpleSchema}
- * @property {String} key optional
- * @property {String} namespace optional
- * @property {String} scope optional
- * @property {String} value optional
- * @property {String} valueType optional
- * @property {String} description optional
- */
-const Metafield = new SimpleSchema({
-  key: {
-    type: String,
-    max: 30,
-    optional: true
-  },
-  namespace: {
-    type: String,
-    max: 20,
-    optional: true
-  },
-  scope: {
-    type: String,
-    optional: true
-  },
-  value: {
-    type: String,
-    optional: true
-  },
-  valueType: {
-    type: String,
-    optional: true
-  },
-  description: {
-    type: String,
-    optional: true
-  }
 });
