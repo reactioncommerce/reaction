377ea251e6a9962f77f23e5a2bfbb4e799fabed4
Machiko Yasuda <machiko@reactioncommerce.com>
2019-10-08 17:39:26 -0700

    tests(ordersByAccountId): query for payments, refunds, fulfillmentGroups
    Signed-off-by: Machiko Yasuda <machiko@reactioncommerce.com>

--- tests/integration/api/queries/ordersByAccountId/ordersByAccountId.test.js
+++ tests/integration/api/queries/ordersByAccountId/ordersByAccountId.test.js
@@ -7,7 +7,6 @@ const shopId = "integ-test-shop-id";
 const accountId = "integ-test-account-id";
 const opaqueAccountId = "cmVhY3Rpb24vYWNjb3VudDppbnRlZy10ZXN0LWFjY291bnQtaWQ=";
 const orderId = "integ-test-order-id";
-
 const mockAccount = Factory.Account.makeOne({
   _id: accountId
 });
@@ -31,12 +30,46 @@ const ordersByAccountIdQuery = `query ($accountId: ID!, $orderStatus: [String]) 
       displayStatus(language: "en")
       email
       fulfillmentGroups {
+        _id
+        displayStatus(language: "en")
         status
+        tracking
+        trackingUrl
+        type
       }
       notes {
         content
       }
+      payments {
+        _id
+        amount {
+          amount
+          displayAmount
+        }
+        billingAddress {
+          _id
+          address1
+          address2
+        }
+        cardBrand
+        displayName
+        isCaptured
+        method {
+          displayName
+          isEnabled
+          name
+          pluginName
+        }
+        mode
+        processor
+        riskLevel
+        status
+        transactionId
+      }
       referenceId
+      refunds {
+        _id
+      }
       shop {
         _id
       }
@@ -48,27 +81,65 @@ const ordersByAccountIdQuery = `query ($accountId: ID!, $orderStatus: [String]) 
       }
       totalItemQuantity
       updatedAt
+      surcharges {
+        _id
+        fulfillmentGroupId
+      }
     }
   }
 }`;
 
 const order = Factory.Order.makeOne({
   _id: orderId,
+  accountId: mockAccount._id,
+  payments: {
+    _id: "Dez6yxGsD5sMYvaCi",
+    data: {
+      fullName: "Reaction Commerce",
+      gqlType: "ExampleIOUPaymentData"
+    },
+    displayName: "IOU from Reaction Commerce",
+    method: "credit",
+    mode: "authorize",
+    name: "iou_example",
+    paymentPluginName: "example-paymentmethod",
+    processor: "Example",
+    riskLevel: "normal",
+    shopId,
+    status: "created",
+    transactionId: "s9atGoLbagKvQ3pJc"
+  },
   shopId,
   workflow: {
     status: "new"
-  },
-  accountId: mockAccount._id
+  }
 });
 
 const orders = Factory.Order.makeMany(10, {
   _id: (iterator) => (iterator + 500).toString(),
+  accountId: mockAccount._id,
   referenceId: (iterator) => (iterator + 123).toString(),
   shopId,
+  payments: {
+    _id: (iterator) => (iterator + 33).toString(),
+    data: {
+      fullName: "Reaction Commerce",
+      gqlType: "ExampleIOUPaymentData"
+    },
+    displayName: "IOU from Reaction Commerce",
+    method: "credit",
+    mode: "authorize",
+    name: "iou_example",
+    paymentPluginName: "example-paymentmethod",
+    processor: "Example",
+    riskLevel: "normal",
+    shopId,
+    status: "created",
+    transactionId: "s9atGoLbagKvQ3pJc"
+  },
   workflow: {
     status: "new"
-  },
-  accountId: mockAccount._id
+  }
 });
 
 let testApp;
