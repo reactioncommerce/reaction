b8e13231ced619b686fac6f9a33ec353d4a46bc3
Eric Dobbertin <eric@dairystatedesigns.com>
2019-10-12 15:28:36 -0500

    refactor: move inviteShopMember gql to account plugin
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/core/schemas/index.js
+++ src/core-services/core/schemas/index.js
@@ -4,12 +4,10 @@ const require = createRequire(import.meta.url);
 
 const email = require("./email.graphql");
 const metafield = require("./metafield.graphql");
-const shop = require("./shop.graphql");
 const tag = require("./tag.graphql");
 
 export default [
   email,
   metafield,
-  shop,
   tag
 ];
