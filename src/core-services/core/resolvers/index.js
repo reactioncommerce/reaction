6a410ce602bc7dcee7470191fc00872d369da510
Eric Dobbertin <eric@dairystatedesigns.com>
2019-10-12 15:22:58 -0500

    refactor: move Address type to address plugin
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/core/resolvers/index.js
+++ src/core-services/core/resolvers/index.js
@@ -1,13 +1,10 @@
 import getConnectionTypeResolvers from "@reactioncommerce/api-utils/graphql/getConnectionTypeResolvers.js";
-import Address from "./Address.js";
 import Query from "./Query/index.js";
 import Tag from "./Tag/index.js";
 
 export default {
-  Address,
   Query,
   Tag,
-  ...getConnectionTypeResolvers("Address"),
   ...getConnectionTypeResolvers("Tag")
 };
 
@@ -21,28 +18,6 @@ export default {
  * @property {Number} args.last - Connection argument
  * @property {Number} args.sortBy - Connection argument. Check schema for allowed values.
  * @property {Number} args.sortOrder - Connection argument
- */
-
-/**
- * Arguments passed by the client a groups query
- * @memberof GraphQL
- * @typedef {Object} AddressInput - Address
- * @property {String} address1 - Address line 1
- * @property {String} [address2] - Address line 2
- * @property {String} city - City
- * @property {String} [company] - Company name
- * @property {String} country - Country
- * @property {Boolean} [failedValidation] - Mark address as failed validation by address validation service
- * @property {String} fullName - Full name
- * @property {String} [firstName] - First name
- * @property {String} [lastName] - Last name
- * @property {Boolean} isBillingDefault - Mark address as default for billing
- * @property {Boolean} isCommercial - Mask address as commercial
- * @property {Boolean} isShippingDefault -  Mark address as default for shipping
- * @property {Array<MetafieldInput>} metafields - Array of metafields
- * @property {String} phone - Phone number
- * @property {String} postal - Postal code
- * @property {String} region - Region of country
  */
 
 /**
--- src/core-services/core/schemas/index.js
+++ src/core-services/core/schemas/index.js
@@ -2,14 +2,12 @@ import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 
-const address = require("./address.graphql");
 const email = require("./email.graphql");
 const metafield = require("./metafield.graphql");
 const shop = require("./shop.graphql");
 const tag = require("./tag.graphql");
 
 export default [
-  address,
   email,
   metafield,
   shop,
