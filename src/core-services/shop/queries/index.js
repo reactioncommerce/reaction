91cfb08387b8c363265c4b49ff67db214edc37d4
Eric Dobbertin <eric@dairystatedesigns.com>
2019-10-15 09:59:12 -0500

    refactor: move primaryShopId query to shop plugin
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/shop/queries/index.js
+++ src/core-services/shop/queries/index.js
@@ -1,5 +1,9 @@
 export default {
   shopById: (context, _id) => context.dataLoaders.Shops.load(_id),
   shopBySlug: (context, slug) => context.collections.Shops.findOne({ slug }),
-  primaryShop: (context) => context.collections.Shops.findOne({ shopType: "primary" })
+  primaryShop: (context) => context.collections.Shops.findOne({ shopType: "primary" }),
+  primaryShopId(context) {
+    const shop = context.collections.Shops.findOne({ shopType: "primary" });
+    return shop ? shop._id : null;
+  }
 };
