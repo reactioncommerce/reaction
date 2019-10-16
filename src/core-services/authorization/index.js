a56699b90f00dbc8173eb0c17e777e4be64c3031
Erik Kieckhafer <ek@ato.la>
2019-10-16 14:36:20 -0700

    refactor: move permission related functions from accounts to authorization package
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/core-services/authorization/index.js
@@ -0,0 +1,20 @@
+import i18n from "./i18n/index.js";
+import { getHasPermissionFunctionForUser } from "./util/hasPermission.js";
+import { getShopsUserHasPermissionForFunctionForUser } from "./util/shopsUserHasPermissionFor.js";
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {ReactionNodeApp} app The ReactionNodeApp instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "Authorization",
+    name: "reaction-authorization",
+    i18n,
+    auth: {
+      getHasPermissionFunctionForUser,
+      getShopsUserHasPermissionForFunctionForUser
+    }
+  });
+}
