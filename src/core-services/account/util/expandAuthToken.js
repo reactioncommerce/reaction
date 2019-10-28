0863c15766a910ff768f0c41773961186ee2b411
Eric Dobbertin <eric@dairystatedesigns.com>
2019-10-28 12:45:29 -0500

    refactor: use envalid to load hydra URL
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/account/util/expandAuthToken.js
+++ src/core-services/account/util/expandAuthToken.js
@@ -1,7 +1,9 @@
 import qs from "querystring";
 import fetch from "node-fetch";
-import Logger from "@reactioncommerce/logger";
 import ReactionError from "@reactioncommerce/reaction-error";
+import config from "../config.js";
+
+const { HYDRA_OAUTH2_INTROSPECT_URL } = config;
 
 /**
  * Given an Authorization Bearer token it returns a JSON object with user
@@ -14,21 +16,15 @@ import ReactionError from "@reactioncommerce/reaction-error";
  * @returns {Object} JSON object
  */
 export default async function expandAuthToken(token) {
-  if (!process.env.HYDRA_OAUTH2_INTROSPECT_URL) {
-    Logger.fatal("HYDRA_OAUTH2_INTROSPECT_URL not set in ENV. Auth introspection failed");
-    throw new ReactionError("server-error", "Hydra token introspection not configured");
-  }
-
-  const res = await fetch(process.env.HYDRA_OAUTH2_INTROSPECT_URL, {
+  const response = await fetch(HYDRA_OAUTH2_INTROSPECT_URL, {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     method: "POST",
     body: qs.stringify({ token })
   });
 
-  if (!res.ok) {
+  if (!response.ok) {
     throw new ReactionError("access-denied", "Error introspecting token");
   }
 
-  const tokenObj = await res.json();
-  return tokenObj;
+  return response.json();
 }
