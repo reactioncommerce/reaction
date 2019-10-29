fe0e633a5520872abfcf4d0762dbc87a5e4b0140
Erik Kieckhafer <ek@ato.la>
2019-10-29 16:02:14 -0700

    refactor: add checkPermissions to non-user buildContext
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/core/util/buildContext.js
+++ src/core/util/buildContext.js
@@ -41,6 +41,9 @@ export default async function buildContext(context, request = {}) {
       context.shopsUserHasPermissionFor = () => [];
     }
   } else {
+    context.checkPermissions = async () => {
+      throw new ReactionError("access-denied", "Access Denied");
+    };
     context.userHasPermission = () => false;
     context.shopsUserHasPermissionFor = () => [];
   }
