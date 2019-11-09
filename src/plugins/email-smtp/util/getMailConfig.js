56a25ae61eb71281021f9d833f785667c6b7e14f
Will Lopez <will.lopez77@gmail.com>
2019-11-08 17:08:26 -0800

    refactor: gracefully handle invalid characters in MAIL_URL's user and password.
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

--- src/plugins/email-smtp/util/getMailConfig.js
+++ src/plugins/email-smtp/util/getMailConfig.js
@@ -2,22 +2,22 @@ import Logger from "@reactioncommerce/logger";
 import ReactionError from "@reactioncommerce/reaction-error";
 import envConfig from "../config.js";
 
-
 /**
  * @summary get email sending config for Nodemailer based on parsing a mail URL
- * @param {Boolean} logger Whether to log debug messages
  * @param {String} mailUrl The mail URL
  * @returns {Object} A mail config object
  */
-function getConfigFromMailUrl({ logger, mailUrl }) {
+function getConfigFromMailUrl({ mailUrl }) {
   const urlSections = mailUrl.split(":");
   // Prevent URL parsing from breaking due to invalid characters in user/password
   // Look for invalid characters in username,
   // ignore the first two // as they are port of the protocol in username section.
   // Also look for invalid character in password, split with @ delimiter to ignore host and only look at password.
   if (urlSections[1].slice(2).indexOf("/") >= 0 || urlSections[2].split("@")[0].indexOf("/") >= 0) {
-    throw new ReactionError(`Invalid character detected in environment variable MAIL_URL, 
-    user or password has invalid characters, please replace "/" with "%2F"`);
+    throw new ReactionError(`
+      Invalid character detected in environment variable MAIL_URL, 
+      user or password has invalid characters, please replace "/" with "%2F"
+    `);
   }
 
   const parsedUrl = new URL(mailUrl);
@@ -28,8 +28,7 @@ function getConfigFromMailUrl({ logger, mailUrl }) {
   const config = {
     host: parsedUrl.hostname,
     port: parsedUrl.port,
-    secure: parseInt(parsedUrl.port, 10) === 465,
-    logger
+    secure: parseInt(parsedUrl.port, 10) === 465
   };
 
   // add user/pass to the config object if they were found
@@ -55,23 +54,27 @@ function getConfigFromMailUrl({ logger, mailUrl }) {
 export default async function getMailConfig() {
   const { MAIL_URL, EMAIL_DEBUG } = envConfig;
   const logger = EMAIL_DEBUG === "true";
+  let config = {};
+  let direct = true;
 
-  /**
-   * Use MAIL_URL if present
-   */
-  if (MAIL_URL) return getConfigFromMailUrl({ logger, mailUrl: MAIL_URL });
+  // If a mail service config URL is provided, use it to build the mailer's config.
+  if (MAIL_URL) {
+    config = getConfigFromMailUrl({ mailUrl: MAIL_URL });
+    direct = false;
+  }
 
-  /**
-   * Try direct sending but warn that this rarely works.
-   */
-  Logger.warn(`
-    Mail service not configured. Attempting to use direct sending option.
-    The mail may send, but messages are far more likely go to the user's spam folder.
-    Please configure an SMTP mail sending provider.
-  `);
+  if (direct) {
+    // Try direct sending but warn that this rarely works.
+    Logger.warn(`
+      Mail service not configured. Attempting to use direct sending option.
+      The mail may send, but messages are far more likely go to the user's spam folder.
+      Please configure an SMTP mail sending provider.
+    `);
+  }
 
   return {
-    direct: true,
+    ...config,
+    direct,
     logger
   };
 }
