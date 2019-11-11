ec0822bee28e08f27805738882d811d433a8755e
Mike Murray <hellomikemurray@gmail.com>
2019-11-11 14:02:01 -0800

    feat: add delete mutation
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ src/plugins/discount-codes/mutations/deleteDiscountCode.js
@@ -0,0 +1,34 @@
+/**
+ * @name Mutation.deleteDiscountCode
+ * @method
+ * @memberof GraphQL/discount-codes
+ * @summary Delete a discount code
+ * @param {Object} context -  an object containing the per-request state
+ * @param {Object} input - mutation input
+ * @returns {Promise<Object>} DeleteDiscountCodePayload
+ */
+export default async function deleteDiscountCode(context, input) {
+  // Check for owner or admin permissions from the user before allowing the mutation
+  const {
+    _id,
+    shopId
+  } = input;
+  const { appEvents, checkPermissions, collections } = context;
+  const { DiscountCodes } = collections;
+
+  await checkPermissions(["admin", "owner"], shopId);
+
+  const discountCodeToRemove = await DiscountCodes.findOne({
+    _id,
+    shopId
+  });
+
+  await DiscountCodes.removeOne({
+    _id,
+    shopId
+  });
+
+  await appEvents.emit("afterDiscountCodeRemove", discountCodeToRemove);
+
+  return discountCodeToRemove;
+}
