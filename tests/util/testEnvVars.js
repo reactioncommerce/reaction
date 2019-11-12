a6ee0e92e0cde11af6627e9fa48321fa0e6eabc7
Will Lopez <will.lopez77@gmail.com>
2019-11-12 11:25:31 -0800

    fix: CI test failure
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

--- tests/util/testEnvVars.js
+++ tests/util/testEnvVars.js
@@ -1,10 +1,10 @@
 import envalid from "envalid";
 
-const { str } = envalid;
+const { str, testOnly } = envalid;
 
 export default envalid.cleanEnv(process.env, {
   MAIL_URL: str({
     desc: "A test mail service configuration",
-    default: "smtp://user:password@email-smtp.us-west-2.amazonaws.com:465"
+    devDefault: testOnly("smtp://user:password@email-smtp.us-west-2.amazonaws.com:465")
   })
 });
