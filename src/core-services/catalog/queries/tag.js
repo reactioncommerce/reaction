1b8993fdc37afc247a02dd651c48e1a45911f0fa
trojanh <rajan.tiwari@kiprosh.com>
2019-11-13 19:24:34 +0530

    refactor: remove context shopId from tag API
    Signed-off-by: trojanh <rajan.tiwari@kiprosh.com>

--- src/core-services/catalog/queries/tag.js
+++ src/core-services/catalog/queries/tag.js
@@ -5,21 +5,22 @@
  * @summary query the Tags collection and return a tag by tag ID or slug
  * @param {Object} context - an object containing the per-request state
  * @param {String} slugOrId - ID or slug of tag to query
+ * @param {String} shopId - shopId of tag
  * @param {Boolean} [params.shouldIncludeInvisible] - Whether or not to include `isVisible=true` tags. Default is `false`
  * @returns {Object} - A Tag document if one was found
  */
-export default async function tag(context, slugOrId, { shouldIncludeInvisible = false } = {}) {
+export default async function tag(context, { slugOrId, shopId }, { shouldIncludeInvisible = false } = {}) {
   const { collections, userHasPermission } = context;
   const { Tags } = collections;
   let query = {
     $and: [
       { isVisible: true },
+      { shopId },
       { $or: [{ _id: slugOrId }, { slug: slugOrId }] }
     ]
   };
 
   if (shouldIncludeInvisible === true) {
-    const shopId = await context.queries.primaryShopId(context);
     if (userHasPermission(["owner", "admin"], shopId)) {
       query = { $or: [{ _id: slugOrId }, { slug: slugOrId }] };
     }
