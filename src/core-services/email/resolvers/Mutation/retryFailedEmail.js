40bfac313b4dbc59a82e5335af5f339b940b84c7
trojanh <rajan.tiwari@kiprosh.com>
2019-11-14 21:42:09 +0530

    refactor: Fix Mutation method name and update description
    Signed-off-by: trojanh <rajan.tiwari@kiprosh.com>

--- src/core-services/email/resolvers/Mutation/retryFailedEmail.js
+++ src/core-services/email/resolvers/Mutation/retryFailedEmail.js
@@ -1,24 +1,24 @@
 import { decodeJobOpaqueId, decodeShopOpaqueId } from "../../xforms/id.js";
 
 /**
- * @name Mutation.resendFailedEmail
+ * @name Mutation.retryFailedEmail
  * @method
  * @memberof Routes/GraphQL
  * @summary Add a tag
  * @param {Object} parentResult - unused
- * @param {Object} args.input - ResendFailedEmailInput
+ * @param {Object} args.input - RetryFailedEmailInput
  * @param {String} args.input.id - Email Job id
  * @param {String} [args.input.clientMutationId] - An optional string identifying the mutation call
  * @param {Object} context - an object containing the per-request state
- * @returns {Promise<Object>} ResendFailedEmailPayload
+ * @returns {Promise<Object>} RetryFailedEmailPayload
  */
-export default async function resendFailedEmail(parentResult, { input }, context) {
+export default async function retryFailedEmail(parentResult, { input }, context) {
   const {
     clientMutationId = null,
     jobId: opaqueJobId,
     shopId: opaqueShopId
   } = input;
-  const resp = await context.mutations.resendFailedEmail(context, {
+  const resp = await context.mutations.retryFailedEmail(context, {
     jobId: decodeJobOpaqueId(opaqueJobId),
     shopId: decodeShopOpaqueId(opaqueShopId)
   });
