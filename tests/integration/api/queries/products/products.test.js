5a7e707f3e07eb55afde02a000750354b66ba044
Mike Murray <hellomikemurray@gmail.com>
2019-11-21 11:27:29 -0800

    test: add integration tests for all product filters
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- tests/integration/api/queries/products/products.test.js
+++ tests/integration/api/queries/products/products.test.js
@@ -1,3 +1,4 @@
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
 import importAsString from "@reactioncommerce/api-utils/importAsString.js";
 import TestApp from "/tests/util/TestApp.js";
 
@@ -18,10 +19,27 @@ for (let index = 100; index < 136; index += 1) {
   const mockProduct = {
     _id: productId,
     ancestors: [],
+    hashtags: [
+      `tag-${index}-0`,
+      `tag-${index}-1`,
+      `tag-${index}-2`
+    ],
     title: `Fake Product ${index}`,
     shopId: internalShopId,
     isDeleted: false,
     isVisible: true,
+    price: {
+      min: index,
+      max: index + 50,
+      range: `${index} - ${index + 50}`
+    },
+    weight: {
+      min: index,
+      max: index + 10
+    },
+    metafields: [
+      { key: "index", value: `${index}` }
+    ],
     supportedFulfillmentTypes: ["shipping"],
     type: "simple",
     createdAt: new Date(),
@@ -128,3 +146,173 @@ test("expect a list of products on the second page", async () => {
   expect(result.products.nodes[9].variants[0].options[0].title).toEqual("Fake Product Option 119");
 });
 
+test("expect a list of products filtered by product ids", async () => {
+  const productId1 = encodeOpaqueId("reaction/product", "product-100");
+  const productId2 = encodeOpaqueId("reaction/product", "product-110");
+  const productId3 = encodeOpaqueId("reaction/product", "product-119");
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      productIds: [productId1, productId2, productId3]
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(3);
+  expect(result.products.nodes[0]._id).toEqual(productId1);
+  expect(result.products.nodes[1]._id).toEqual(productId2);
+  expect(result.products.nodes[2]._id).toEqual(productId3);
+});
+
+test("expect a list of products filtered by a minimum price", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      priceMin: 132
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(4);
+  expect(result.products.nodes[0].price.max).toEqual(182);
+  expect(result.products.nodes[3].price.max).toEqual(185);
+});
+
+test("expect a list of products filtered by a maximum price", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      priceMax: 151
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(2);
+  expect(result.products.nodes[0].price.max).toEqual(150);
+  expect(result.products.nodes[1].price.max).toEqual(151);
+});
+
+test("expect a list of products filtered by a price range", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      priceMin: 110,
+      priceMax: 163
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(4);
+  expect(result.products.nodes[0].price.min).toEqual(110);
+  expect(result.products.nodes[0].price.max).toEqual(160);
+  expect(result.products.nodes[3].price.min).toEqual(113);
+  expect(result.products.nodes[3].price.max).toEqual(163);
+});
+
+test("expect a list of products filtered by tags", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      tagIds: [
+        encodeOpaqueId("reaction/tag", "tag-100-0"),
+        encodeOpaqueId("reaction/tag", "tag-120-2"),
+        encodeOpaqueId("reaction/tag", "tag-130-1"),
+        encodeOpaqueId("reaction/tag", "tag-110-0")
+      ]
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(4);
+  expect(result.products.nodes[0].title).toEqual("Fake Product 100");
+  expect(result.products.nodes[1].title).toEqual("Fake Product 110");
+  expect(result.products.nodes[2].title).toEqual("Fake Product 120");
+  expect(result.products.nodes[3].title).toEqual("Fake Product 130");
+});
+
+test("expect a list of products filtered by a metafield", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      metafieldKey: "index",
+      metafieldValue: "110"
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(1);
+  expect(result.products.nodes[0].title).toEqual("Fake Product 110");
+});
+
+test("expect a single of product filtered by a search query", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      query: "Fake Product 130"
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(1);
+  expect(result.products.nodes[0].title).toEqual("Fake Product 130");
+});
+
+test("expect an empty list of products filtered by a visibility", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      isVisible: false
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(0);
+});
+
+test("expect an empty list of products filtered by a archived", async () => {
+  let result;
+
+  try {
+    result = await queryProducts({
+      shopIds: [opaqueShopId],
+      isArchived: true
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.products.nodes.length).toEqual(0);
+});
