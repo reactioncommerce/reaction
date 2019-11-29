8e41fd96a23c0cff39c780cc359b887417c26f7c
quophyie <quophyie@yahoo.com>
2019-11-29 17:04:22 +0000

    (feat): add removeUserPermissions mutation and resolvers
    Signed-off-by: quophyie <quophyie@yahoo.com>

---
+++ src/core-services/account/resolvers/Mutation/removeUserPermissions.js
@@ -0,0 +1,19 @@
+import AddOrRemoveAccountGroupsOperationType from "../../mutations/AddOrRemoveAccountGroupsOperationType.js";
+
+/**
+ * @name Mutation/setUserPermissions
+ * @method
+ * @memberof Accounts/GraphQL
+ * @summary resolver to add user permissions
+ * @param {Object} _ - unused
+ * @param {Object} input - an object of all mutation arguments that were sent by the client
+ * @param {String} input.groups - The group the user is to be added to
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} context.userId - the userId of user to add to the given group
+ * @param {Object} context.accountId - the userId of user to add to the given group
+ * @returns {Object} - object
+ */
+export default async function setUserPermissions(_, { input }, context) {
+  await context.validatePermissions("reaction:accounts", "create", { });
+  return context.mutations.addOrRemoveAccountGroups(context, input, AddOrRemoveAccountGroupsOperationType.ADD);
+}
---
+++ src/core-services/account/resolvers/Mutation/removeUserPermissions.test.js
@@ -0,0 +1,49 @@
+import mockContext from "@reactioncommerce/api-utils/tests/mockContext.js";
+import AddOrRemoveAccountGroupsOperationType from "../../mutations/AddOrRemoveAccountGroupsOperationType";
+import setUserPermissions from "./setUserPermissions.js";
+
+mockContext.mutations.addOrRemoveAccountGroups = jest.fn().mockName("mutations.addOrRemoveAccountGroups");
+mockContext.validatePermissions = jest.fn().mockName("validatePermissions");
+
+test("correctly passes through to internal mutation function", async () => {
+  const groups = ["test-group-id"];
+
+  const fakeResult = {
+    _id: "3vx5cqBZsymCfHbpf",
+    acceptsMarketing: false,
+    createdAt: "2019-11-05T16:34:49.644Z",
+    emails: [
+      {
+        address: "admin@localhost",
+        verified: true,
+        provides: "default"
+      }
+    ],
+    shopId: "test-shop-id",
+    state: "new",
+    userId: "3vx5cqBZsymCfHbpf",
+    accountId: "3vx5cqBZsymCfHbpf",
+    groups: [
+      "test-group-id"
+    ]
+  };
+
+  mockContext.mutations.addOrRemoveAccountGroups.mockReturnValueOnce(Promise.resolve(fakeResult));
+  mockContext.validatePermissions.mockReturnValueOnce(Promise.resolve({ allow: true }));
+
+  const result = await setUserPermissions(null, {
+    input: {
+      groups
+    }
+  }, mockContext);
+
+  expect(mockContext.mutations.addOrRemoveAccountGroups).toHaveBeenCalledWith(
+    mockContext,
+    { groups: ["test-group-id"] },
+    AddOrRemoveAccountGroupsOperationType.ADD
+  );
+
+  expect(mockContext.validatePermissions).toHaveBeenCalledWith("reaction:accounts", "create", { });
+
+  expect(result).toEqual(fakeResult);
+});
