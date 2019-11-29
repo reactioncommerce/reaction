708dd0264dac12b757b1283dc5dd8a2bbdc4a0b5
quophyie <quophyie@yahoo.com>
2019-11-29 16:12:28 +0000

    (doc): update jsdoc for setUserPermissions
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- src/core-services/account/resolvers/Mutation/setUserPermissions.js
+++ src/core-services/account/resolvers/Mutation/setUserPermissions.js
@@ -6,10 +6,11 @@ import AddOrRemoveAccountGroupsOperationType from "../../mutations/AddOrRemoveAc
  * @memberof Accounts/GraphQL
  * @summary resolver to add user permissions
  * @param {Object} _ - unused
- * @param {Object} args.input - an object of all mutation arguments that were sent by the client
- * @param {String} args.input.group - The group the user is to be added to
+ * @param {Object} input - an object of all mutation arguments that were sent by the client
+ * @param {String} input.groups - The group the user is to be added to
  * @param {Object} context - an object containing the per-request state
- * @param {Object} args.context.userId - the userId of user to add to the given group
+ * @param {Object} context.userId - the userId of user to add to the given group
+ * @param {Object} context.accountId - the userId of user to add to the given group
  * @returns {Object} - object
  */
 export default async function setUserPermissions(_, { input }, context) {
