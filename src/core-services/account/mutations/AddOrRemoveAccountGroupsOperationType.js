bb7602522b9652d9dd32804ee2e6c195c77b2d1b
quophyie <quophyie@yahoo.com>
2019-11-29 12:45:55 +0000

    (feat): add AddOrRemoveAccountGroupsOperationType enum
    Signed-off-by: quophyie <quophyie@yahoo.com>

---
+++ src/core-services/account/mutations/AddOrRemoveAccountGroupsOperationType.js
@@ -0,0 +1,12 @@
+/**
+ * Enum for AddOrRemoveAccountGroupsOperationType values.
+ * This enum represents the operations that can be used in the addOrRemoveAccountGroups method
+ * @readonly
+ * @enum {string}
+ */
+const AddOrRemoveAccountGroupsOperationType = Object.freeze({
+  ADD_OR_SET: "ADD_OR_SET",
+  REMOVE: "REMOVE"
+});
+
+export default AddOrRemoveAccountGroupsOperationType;
