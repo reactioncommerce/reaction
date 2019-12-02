1b4173c676b873e3e9ca6b04a389555edfeab667
Mike Murray <hellomikemurray@gmail.com>
2019-12-02 10:53:48 -0800

    feat: export resolvers
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- src/plugins/shipping-rates/resolvers/index.js
+++ src/plugins/shipping-rates/resolvers/index.js
@@ -1,10 +1,13 @@
 import getConnectionTypeResolvers from "@reactioncommerce/api-utils/graphql/getConnectionTypeResolvers.js";
+import FlatRateFulfillmentMethod from "./FlatRateFulfillmentMethod/index.js";
 import FlatRateFulfillmentRestriction from "./FlatRateFulfillmentRestriction/index.js";
 import Mutation from "./Mutation/index.js";
 import Query from "./Query/index.js";
 
 export default {
   ...getConnectionTypeResolvers("FlatRateFulfillmentRestriction"),
+  ...getConnectionTypeResolvers("FlatRateFulfillmentMethod"),
+  FlatRateFulfillmentMethod,
   FlatRateFulfillmentRestriction,
   Mutation,
   Query
