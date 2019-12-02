d82c641cd9b8d5492f3e46a7da436d73607d72d0
Mike Murray <hellomikemurray@gmail.com>
2019-12-02 14:15:17 -0800

    feat: export fulfillment method query resolver
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- src/plugins/shipping-rates/resolvers/Query/index.js
+++ src/plugins/shipping-rates/resolvers/Query/index.js
@@ -1,8 +1,10 @@
+import flatRateFulfillmentMethod from "./flatRateFulfillmentMethod.js";
 import getFlatRateFulfillmentRestriction from "./getFlatRateFulfillmentRestriction.js";
 import getFlatRateFulfillmentRestrictions from "./getFlatRateFulfillmentRestrictions.js";
 import getFlatRateFulfillmentMethods from "./getFlatRateFulfillmentMethods.js";
 
 export default {
+  flatRateFulfillmentMethod,
   getFlatRateFulfillmentRestriction,
   getFlatRateFulfillmentRestrictions,
   getFlatRateFulfillmentMethods
