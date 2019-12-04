442b0ac57cb3de03ddefd72a1198572d054d2f21
Mike Murray <hellomikemurray@gmail.com>
2019-12-03 16:43:05 -0800

    feat: add accounts query resolver
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ src/core-services/account/resolvers/Query/accounts.js
@@ -0,0 +1,29 @@
+import getPaginatedResponse from "@reactioncommerce/api-utils/graphql/getPaginatedResponse.js";
+import wasFieldRequested from "@reactioncommerce/api-utils/graphql/wasFieldRequested.js";
+import { decodeShopOpaqueId } from "../../xforms/id.js";
+
+/**
+ * @name Query/accounts
+ * @method
+ * @memberof Accounts/GraphQL
+ * @summary query the Accounts collection and return a list of accounts
+ * @param {Object} _ - unused
+ * @param {Object} args - an object of all arguments that were sent by the client
+ * @param {String} args.shopId - Shop Id
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} info Info about the GraphQL request
+ * @returns {Promise<Object>} Promise containing queried accounts
+ */
+export default async function account(_, args, context, info) {
+  const { shopId: opaqueShopId, ...connectionArgs } = args;
+
+  const shopId = decodeShopOpaqueId(opaqueShopId);
+
+  const query = await context.queries.accounts(context, { shopId });
+
+  return getPaginatedResponse(query, connectionArgs, {
+    includeHasNextPage: wasFieldRequested("pageInfo.hasNextPage", info),
+    includeHasPreviousPage: wasFieldRequested("pageInfo.hasPreviousPage", info),
+    includeTotalCount: wasFieldRequested("totalCount", info)
+  });
+}
