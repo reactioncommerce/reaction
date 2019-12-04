f36f020a26585883cbed7b5eef7c62e1e5590e00
quophyie <quophyie@yahoo.com>
2019-12-04 16:41:30 +0000

    (feat): add createGroup resolver functions
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- src/core-services/account/resolvers/Group/index.js
+++ src/core-services/account/resolvers/Group/index.js
@@ -1,9 +1,11 @@
 import resolveShopFromShopId from "@reactioncommerce/api-utils/graphql/resolveShopFromShopId.js";
 import { encodeGroupOpaqueId } from "../../xforms/id.js";
 import createdBy from "./createdBy.js";
+import createGroup from "./createGroup.js";
 
 export default {
   _id: (node) => encodeGroupOpaqueId(node._id),
   createdBy,
-  shop: resolveShopFromShopId
+  shop: resolveShopFromShopId,
+  createGroup
 };
