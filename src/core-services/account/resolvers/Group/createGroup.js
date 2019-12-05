98101eb4984bda14f9acd54b6d633e08431dbf5d
quophyie <quophyie@yahoo.com>
2019-12-05 15:00:15 +0000

    (refactor): change method name from createBy to createGroup and fix documentation
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- src/core-services/account/resolvers/Group/createGroup.js
+++ src/core-services/account/resolvers/Group/createGroup.js
@@ -7,15 +7,15 @@
  * It creates permission group for a given shop with passed in roles
  * @param {object} _ - Not used
  * @param {object} input - The input supplied from GraphQL
- * @param {Object} input.groupData - info about group to create
- * @param {String} input.groupData.name - name of the group to be created
- * @param {String} input.groupData.description - Optional description of the group to be created
- * @param {Array} input.groupData.permissions - permissions to assign to the group being created
- * @param {Array} input.groupData.members - members of the
+ * @param {Object} input.group - info about group to create
+ * @param {String} input.group.name - name of the group to be created
+ * @param {String} [input.group.description] - Optional description of the group to be created
+ * @param {Array} input.group.permissions - permissions to assign to the group being created
+ * @param {Array} [input.group.members] - members of the
  * @param {object} context - The GraphQL context
  * @param {String} context.shopId - id of the shop the group belongs to
  * @returns {Object} - `object.status` of 200 on success or Error object on failure
  */
-export default async function createdBy(_, { input }, context) {
+export default async function createGroup(_, { input }, context) {
   return context.mutations.createGroup(input, context);
 }
