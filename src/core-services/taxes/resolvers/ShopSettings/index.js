ec5efdd05444b3ec715ca60b1c423e5f3a331ac1
Eric Dobbertin <eric@dairystatedesigns.com>
2019-12-05 12:25:41 -0600

    fix: fix shop settings resolvers permission checks
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/taxes/resolvers/ShopSettings/index.js
+++ src/core-services/taxes/resolvers/ShopSettings/index.js
@@ -1,20 +1,15 @@
-import { decodeShopOpaqueId } from "../../xforms/id.js";
-
 // ShopSettings are public by default. Here we add a permission check.
 export default {
   async defaultTaxCode(settings, args, context) {
-    await context.checkPermissions(["admin", "tax-settings/write", "tax-settings/read"], decodeShopOpaqueId(args.shopId));
-
+    await context.checkPermissions(["admin", "tax-settings/write", "tax-settings/read"], settings.shopId);
     return settings.defaultTaxCode;
   },
   async fallbackTaxServiceName(settings, args, context) {
-    await context.checkPermissions(["admin", "tax-settings/write", "tax-settings/read"], decodeShopOpaqueId(args.shopId));
-
+    await context.checkPermissions(["admin", "tax-settings/write", "tax-settings/read"], settings.shopId);
     return settings.fallbackTaxServiceName;
   },
   async primaryTaxServiceName(settings, args, context) {
-    await context.checkPermissions(["admin", "tax-settings/write", "tax-settings/read"], decodeShopOpaqueId(args.shopId));
-
+    await context.checkPermissions(["admin", "tax-settings/write", "tax-settings/read"], settings.shopId);
     return settings.primaryTaxServiceName;
   }
 };
--- src/plugins/navigation/resolvers/ShopSettings/index.js
+++ src/plugins/navigation/resolvers/ShopSettings/index.js
@@ -1,14 +1,14 @@
 export default {
   async shouldNavigationTreeItemsBeAdminOnly(settings, args, context) {
-    await context.checkPermissions(["admin"], args.shopId);
+    await context.checkPermissions(["admin"], settings.shopId);
     return settings.shouldNavigationTreeItemsBeAdminOnly;
   },
   async shouldNavigationTreeItemsBePubliclyVisible(settings, args, context) {
-    await context.checkPermissions(["admin"], args.shopId);
+    await context.checkPermissions(["admin"], settings.shopId);
     return settings.shouldNavigationTreeItemsBePubliclyVisible;
   },
   async shouldNavigationTreeItemsBeSecondaryNavOnly(settings, args, context) {
-    await context.checkPermissions(["admin"], args.shopId);
+    await context.checkPermissions(["admin"], settings.shopId);
     return settings.shouldNavigationTreeItemsBeSecondaryNavOnly;
   }
 };
