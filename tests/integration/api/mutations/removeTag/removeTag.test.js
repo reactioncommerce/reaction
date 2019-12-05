7f0bdb3bc24b6c7d9427559735b78d2c0921a53d
Will Lopez <will.lopez77@gmail.com>
2019-12-05 15:12:00 -0600

    chore: fix lint errors
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

--- tests/integration/api/mutations/removeTag/removeTag.test.js
+++ tests/integration/api/mutations/removeTag/removeTag.test.js
@@ -18,83 +18,83 @@ let fakeTag;
 const accountInternalId = "123";
 
 beforeAll(async () => {
-    testApp = new TestApp();
-    await testApp.start();
-    shopId = await testApp.insertPrimaryShop();
+  testApp = new TestApp();
+  await testApp.start();
+  shopId = await testApp.insertPrimaryShop();
 
-    mockTagsAccount = Factory.Account.makeOne({
-        roles: {
-            [encodeOpaqueId("reaction/shop", shopId)]: ["owner"]
-        }
-    });
-    await testApp.createUserAndAccount(mockTagsAccount);
+  mockTagsAccount = Factory.Account.makeOne({
+    roles: {
+      [encodeOpaqueId("reaction/shop", shopId)]: ["owner"]
+    }
+  });
+  await testApp.createUserAndAccount(mockTagsAccount);
 
-    removeTag = testApp.mutate(RemoveTagMutation);
+  removeTag = testApp.mutate(RemoveTagMutation);
 
-    fakeTag = Factory.Tag.makeOne({
-        _id: encodeOpaqueId("reaction/account", accountInternalId),
-        shopId: encodeOpaqueId("reaction/shop", shopId)
-    });
+  fakeTag = Factory.Tag.makeOne({
+    _id: encodeOpaqueId("reaction/account", accountInternalId),
+    shopId: encodeOpaqueId("reaction/shop", shopId)
+  });
 
-    tagInput = {
-        id: encodeOpaqueId("reaction/tag", fakeTag._id),
-        shopId: encodeOpaqueId("reaction/shop", fakeTag.shopId)
-    };
+  tagInput = {
+    id: encodeOpaqueId("reaction/tag", fakeTag._id),
+    shopId: encodeOpaqueId("reaction/shop", fakeTag.shopId)
+  };
 });
 
 afterAll(async () => {
-    await testApp.collections.Shops.deleteMany({});
-    await testApp.stop();
+  await testApp.collections.Shops.deleteMany({});
+  await testApp.stop();
 });
 beforeEach(async () => {
-    await testApp.collections.Tags.insertOne(fakeTag);
+  await testApp.collections.Tags.insertOne(fakeTag);
 });
 afterEach(async () => {
-    await testApp.collections.Tags.deleteMany({});
+  await testApp.collections.Tags.deleteMany({});
 });
 
 describe("unauthorized user", () => {
-    let logLevel;
-    beforeAll(async () => {
-        await testApp.setLoggedInUser({ _id: accountInternalId });
-        logLevel = Logger.level();
-        Logger.level("FATAL");
-    });
-    afterAll(async () => {
-        await testApp.clearLoggedInUser();
-        Logger.level(logLevel);
-    });
-    test("cannot remove tag", async () => {
-        expect.assertions(1);
-        try {
-            await removeTag(tagInput);
-        } catch (error) {
-            expect(error).toMatchSnapshot();
-        }
-    });
+  let logLevel;
+  beforeAll(async () => {
+    await testApp.setLoggedInUser({ _id: accountInternalId });
+    logLevel = Logger.level();
+    Logger.level("FATAL");
+  });
+  afterAll(async () => {
+    await testApp.clearLoggedInUser();
+    Logger.level(logLevel);
+  });
+  test("cannot remove tag", async () => {
+    expect.assertions(1);
+    try {
+      await removeTag(tagInput);
+    } catch (error) {
+      expect(error).toMatchSnapshot();
+    }
+  });
 });
 
 describe("authorized user", () => {
-    beforeAll(async () => {
-        await testApp.setLoggedInUser(mockTagsAccount);
-    });
-    afterAll(async () => {
-        await testApp.clearLoggedInUser();
-    });
-    test("can remove tag", async () => {
-        try {
-            let removedTag = await testApp.collections.Tags.findOne({ _id: fakeTag._id, shopId: fakeTag.shopId });
-            // tag is returned unencoded diretly from mongoDB but is returned encoded from the mutation
-            removedTag._id = encodeOpaqueId("reaction/tag", removedTag._id);
-            removedTag.heroMediaUrl = `https://shop.fake.site/${removedTag.heroMediaUrl}`;
-            expect(removedTag).not.toBeNull();
-            const result = await removeTag(tagInput);
-            // mongoDB tag returns more fields than removeTag mutation, so objectContaining is to ensure returned fields match from db entry.
-            expect(removedTag).toEqual(expect.objectContaining(result.removeTag.tag));
-            removedTag = await testApp.collections.Tags.findOne({ _id: fakeTag._id, shopId: fakeTag.shopId });
-            expect(removedTag).toBeNull();
-        } catch (error) {
-            expect(error).toBeUndefined();
-        }
-    });
+  beforeAll(async () => {
+    await testApp.setLoggedInUser(mockTagsAccount);
+  });
+  afterAll(async () => {
+    await testApp.clearLoggedInUser();
+  });
+  test("can remove tag", async () => {
+    try {
+      let removedTag = await testApp.collections.Tags.findOne({ _id: fakeTag._id, shopId: fakeTag.shopId });
+      // tag is returned unencoded diretly from mongoDB but is returned encoded from the mutation
+      removedTag._id = encodeOpaqueId("reaction/tag", removedTag._id);
+      removedTag.heroMediaUrl = `https://shop.fake.site/${removedTag.heroMediaUrl}`;
+      expect(removedTag).not.toBeNull();
+      const result = await removeTag(tagInput);
+      // mongoDB tag returns more fields than removeTag mutation, so objectContaining is to ensure returned fields match from db entry.
+      expect(removedTag).toEqual(expect.objectContaining(result.removeTag.tag));
+      removedTag = await testApp.collections.Tags.findOne({ _id: fakeTag._id, shopId: fakeTag.shopId });
+      expect(removedTag).toBeNull();
+    } catch (error) {
+      expect(error).toBeUndefined();
+    }
+  });
 });
