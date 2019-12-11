96a07d40873cfab469321cf98a602ec3e15e5a3c
quophyie <quophyie@yahoo.com>
2019-12-11 15:32:05 +0000

    (feat): add initial setUserPermissions integration tests
    Signed-off-by: quophyie <quophyie@yahoo.com>

---
+++ tests/integration/api/mutations/setUserPermissions/setUserPermissions.test.js
@@ -0,0 +1,9 @@
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+import Factory from "/tests/util/factory.js";
+import TestApp from "/tests/util/TestApp.js";
+
+const SetUserPermissionsMutation = importAsString("./setUserPermissionsMutation.graphql");
+
+jest.setTimeout(300000);
+
