560649e311e48cb4d9783fdcdd12899df417523e
quophyie <quophyie@yahoo.com>
2019-12-16 20:01:53 +0000

    (test): update removeUserPermissions resolver tests to pass an opaque shopId to the resolver function
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- src/core-services/account/resolvers/Mutation/removeUserPermissions.test.js
+++ src/core-services/account/resolvers/Mutation/removeUserPermissions.test.js
@@ -1,4 +1,5 @@
 import mockContext from "@reactioncommerce/api-utils/tests/mockContext.js";
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
 import removeUserPermissions from "./removeUserPermissions.js";
 
 mockContext.mutations.removeUserPermissions = jest.fn().mockName("mutations.removeUserPermissions");
@@ -7,38 +8,49 @@ mockContext.validatePermissions = jest.fn().mockName("validatePermissions");
 test("removeUserPermissions must correctly passes through to internal removeUserPermissions mutation function", async () => {
   const groups = ["test-group-id"];
   const clientMutationId = "SOME_CLIENT_MUTATION_ID";
+  const shopId = "test-shop-id";
+  const shopIdOpaque = encodeOpaqueId("reaction/shop", shopId);
 
   const fakeResult = {
-    _id: "3vx5cqBZsymCfHbpf",
-    acceptsMarketing: false,
-    createdAt: "2019-11-05T16:34:49.644Z",
-    emails: [
-      {
-        address: "admin@localhost",
-        verified: true,
-        provides: "default"
-      }
-    ],
-    shopId: "test-shop-id",
-    state: "new",
-    userId: "3vx5cqBZsymCfHbpf",
-    accountId: "3vx5cqBZsymCfHbpf",
-    groups: [
-      "test-group-id"
-    ]
+    account: {
+      _id: "3vx5cqBZsymCfHbpf",
+      acceptsMarketing: false,
+      createdAt: "2019-11-05T16:34:49.644Z",
+      emails: [
+        {
+          address: "admin@localhost",
+          verified: true,
+          provides: "default"
+        }
+      ],
+      shopId,
+      state: "new",
+      userId: "3vx5cqBZsymCfHbpf",
+      accountId: "3vx5cqBZsymCfHbpf",
+      groups: [
+        "test-group-id"
+      ]
+    },
+    clientMutationId
   };
 
   mockContext.mutations.removeUserPermissions.mockReturnValueOnce(Promise.resolve(fakeResult));
 
   const result = await removeUserPermissions(null, {
     input: {
-      groups, clientMutationId
+      groups,
+      clientMutationId,
+      shopId: shopIdOpaque
     }
   }, mockContext);
 
   expect(mockContext.mutations.removeUserPermissions).toHaveBeenCalledWith(
     mockContext,
-    { groups: ["test-group-id"], clientMutationId }
+    {
+      groups: ["test-group-id"],
+      clientMutationId,
+      shopId
+    }
   );
 
   expect(result).toEqual(fakeResult);
