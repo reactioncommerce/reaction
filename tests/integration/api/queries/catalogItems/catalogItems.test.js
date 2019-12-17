4810617e83eb51115d771a5d64ad1ac66c01ed74
trojanh <rajan.tiwari@kiprosh.com>
2019-12-17 20:56:14 +0530

    tests: revert catalogItems tests
    Signed-off-by: trojanh <rajan.tiwari@kiprosh.com>

--- tests/integration/api/queries/catalogItems/catalogItems.test.js
+++ tests/integration/api/queries/catalogItems/catalogItems.test.js
@@ -1,111 +1,39 @@
-import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
 import importAsString from "@reactioncommerce/api-utils/importAsString.js";
-import Factory from "/tests/util/factory.js";
 import TestApp from "/tests/util/TestApp.js";
+// temp mocks
+import { internalShopId, opaqueShopId, shopName } from "../../../../mocks/mockShop";
+import { internalTagIds, opaqueTagIds } from "../../../../mocks/mockTags";
+import { internalCatalogItemIds } from "../../../../mocks/mockCatalogProducts";
+import {
+  mockCatalogItems,
+  mockUnsortedCatalogItemsResponse,
+  mockSortedByPriceHigh2LowCatalogItemsResponse,
+  mockSortedByPriceLow2HighCatalogItemsResponse
+} from "../../../../mocks/mockCatalogItems";
 
 const CatalogProductItemsFullQuery = importAsString("./CatalogProductItemsFullQuery.graphql");
 const CatalogProductItemsFullQueryPaginated = importAsString("./CatalogProductItemsFullQueryPaginated.graphql");
-
-const internalShopId = "123";
-const opaqueShopId = "cmVhY3Rpb24vc2hvcDoxMjM=";
-const shopName = "Test Shop";
-
-const mockTag = Factory.Tag.makeOne({
-  featuredProductIds: [
-    "501",
-    "503",
-    "505",
-    "506",
-    "504",
-    "500",
-    "501",
-    "502",
-    "507",
-    "508",
-    "509",
-    "512",
-    "511",
-    "513",
-    "510",
-    "506",
-    "514"
-  ],
-  shopId: internalShopId,
-  slug: "2"
-});
-
-const mockCatalogItems = Factory.Catalog.makeMany(15, {
-  _id: (iterator) => (iterator + 500).toString(),
-  product: (iterator) =>
-    Factory.CatalogProduct.makeOne({
-      _id: (iterator + 500).toString(),
-      productId: (iterator + 500).toString(),
-      isDeleted: false,
-      isVisible: true,
-      tagIds: [mockTag._id],
-      shopId: internalShopId,
-      pricing: {
-        USD: {
-          compareAtPrice: 6.0 + (iterator + 10),
-          displayPrice: `${0.99 + iterator} - ${1.99 + iterator}`,
-          maxPrice: 1.99 + iterator,
-          minPrice: 0.99 + iterator,
-          price: null
-        }
-      },
-      media: [
-        {
-          priority: 1,
-          productId: (iterator + 500).toString(),
-          variantId: null,
-          URLs: { thumbnail: "/thumbnail", small: "/small", medium: "/medium", large: "/large", original: "/original" }
-        }
-      ],
-      socialMetadata: [
-        { service: "twitter", message: "twitterMessage" },
-        { service: "facebook", message: "facebookMessage" },
-        { service: "googleplus", message: "googlePlusMessage" },
-        { service: "pinterest", message: "pinterestMessage" }
-      ],
-      variants: Factory.CatalogProductVariant.makeMany(1, {
-        options: null,
-        shopId: internalShopId,
-        pricing: {
-          USD: {
-            compareAtPrice: 6.0 + (iterator + 10),
-            displayPrice: `${0.99 + iterator} - ${1.99 + iterator}`,
-            maxPrice: 1.99 + iterator,
-            minPrice: 0.99 + iterator,
-            price: null
-          }
-        }
-      })
-    }),
-  shopId: internalShopId
-});
-
-const opaqueCatalogItemIds = mockCatalogItems.map((item) => encodeOpaqueId("reaction/catalogProduct", item._id));
 
 jest.setTimeout(300000);
 
 let testApp;
 let query;
 let paginatedQuery;
-
 beforeAll(async () => {
   testApp = new TestApp();
   await testApp.start();
   query = testApp.query(CatalogProductItemsFullQuery);
   paginatedQuery = testApp.query(CatalogProductItemsFullQueryPaginated);
   await testApp.insertPrimaryShop({ _id: internalShopId, name: shopName });
-  await testApp.collections.Tags.insertOne(mockTag);
-  await Promise.all(mockCatalogItems.map((mockItem) => testApp.collections.Catalog.insertOne(mockItem)));
+  await Promise.all(internalTagIds.map((_id) => testApp.collections.Tags.insertOne({ _id, shopId: internalShopId, slug: `slug${_id}` })));
+  await Promise.all(mockCatalogItems.map((mockCatalogItem) => testApp.collections.Catalog.insertOne(mockCatalogItem)));
 });
 
 afterAll(async () => {
   await testApp.collections.Shops.deleteOne({ _id: internalShopId });
-  await testApp.collections.Tags.deleteOne({ _id: mockTag._id });
-  await Promise.all(mockCatalogItems.map((mockItem) => testApp.collections.Catalog.deleteOne({ _id: mockItem._id })));
+  await testApp.collections.Tags.deleteMany({ _id: { $in: internalTagIds } });
+  await testApp.collections.Catalog.deleteOne({ _id: internalCatalogItemIds[0] });
+  await testApp.collections.Catalog.deleteOne({ _id: internalCatalogItemIds[1] });
   await testApp.stop();
 });
 
@@ -118,11 +46,7 @@ test("get all items for shop", async () => {
     return;
   }
 
-  expect(result.catalogItems.nodes.length).toEqual(15);
-
-  result.catalogItems.nodes.forEach(({ product }) => {
-    expect(opaqueCatalogItemIds).toContain(product._id);
-  });
+  expect(result).toEqual(mockUnsortedCatalogItemsResponse);
 });
 
 // expect CatalogItems sorted by minPrice form high to low
@@ -135,25 +59,10 @@ test("expect CatalogItemProducts sorted by minPrice from highest to lowest when 
     return;
   }
 
-  const firstCatalogItem = mockCatalogItems[0];
-  const lastCatalogItem = mockCatalogItems[mockCatalogItems.length - 1];
-
-  const firstResultItem = result.catalogItems.nodes[0];
-  const lastResultItem = result.catalogItems.nodes[result.catalogItems.nodes.length - 1];
-
-  const resultProductIds = result.catalogItems.nodes.map((node) => node.product._id);
-
-  expect(result.catalogItems.nodes.length).toEqual(15);
-
-  expect(firstResultItem.product.pricing[0].minPrice).toEqual(lastCatalogItem.product.pricing.USD.minPrice);
-  expect(firstResultItem.product._id).toEqual(opaqueCatalogItemIds[opaqueCatalogItemIds.length - 1]);
-
-  expect(lastResultItem.product.pricing[0].minPrice).toEqual(firstCatalogItem.product.pricing.USD.minPrice);
-  expect(lastResultItem.product._id).toEqual(opaqueCatalogItemIds[0]);
-  expect(resultProductIds).toEqual(opaqueCatalogItemIds.reverse());
+  expect(result).toEqual(mockSortedByPriceHigh2LowCatalogItemsResponse);
 });
 
-test("expect CatalogItemProducts with offset 0 to return all items", async () => {
+test("expect CatalogItemProducts with offset 0 to return first item", async () => {
   let result;
   try {
     result = await paginatedQuery({
@@ -164,10 +73,10 @@ test("expect CatalogItemProducts with offset 0 to return all items", async () =>
     expect(error).toBeUndefined();
     return;
   }
-  expect(result.catalogItems.pageInfo.hasNextPage).toBe(false);
-  expect(result.catalogItems.pageInfo.hasPreviousPage).toBe(false);
-  expect(result.catalogItems.totalCount).toBe(15);
-  expect(result.catalogItems.nodes.length).toBe(15);
+
+  expect(result.catalogItems.totalCount).toBe(2);
+  expect(result.catalogItems.nodes.length).toBe(2);
+  expect(result.catalogItems.nodes[0].product._id).toEqual(mockUnsortedCatalogItemsResponse.catalogItems.nodes[0].product._id);
 });
 
 // expect CatalogItems with offset to skip items
@@ -185,7 +94,8 @@ test("expect CatalogitemProducts with offset to skip items", async () => {
 
   expect(result.catalogItems.pageInfo.hasNextPage).toBe(false);
   expect(result.catalogItems.pageInfo.hasPreviousPage).toBe(true);
-  expect(result.catalogItems.nodes.length).toBe(14);
+  expect(result.catalogItems.nodes.length).toBe(1);
+  expect(result.catalogItems.nodes[0].product._id).toEqual(mockUnsortedCatalogItemsResponse.catalogItems.nodes[1].product._id);
 });
 
 // expect CatalogItems with feature sortBy and offset to skip items correctly
@@ -196,7 +106,7 @@ test("expect CatalogitemProducts with offset and featured sort to skip items", a
       shopIds: [opaqueShopId],
       offset: 1,
       sortBy: "featured",
-      tagIds: [encodeOpaqueId("reaction/tag", mockTag._id)]
+      tagIds: [opaqueTagIds[0]]
     });
   } catch (error) {
     expect(error).toBeUndefined();
@@ -204,9 +114,9 @@ test("expect CatalogitemProducts with offset and featured sort to skip items", a
   }
 
   expect(result.catalogItems.pageInfo.hasNextPage).toBe(false);
-  // @TODO: debug this
   expect(result.catalogItems.pageInfo.hasPreviousPage).toBe(true);
   expect(result.catalogItems.nodes.length).toBe(1);
+  expect(result.catalogItems.nodes[0].product._id).toEqual(mockUnsortedCatalogItemsResponse.catalogItems.nodes[0].product._id);
 });
 
 // expect CatalogItems sorted by minPrice form high to low when sortOrder is desc
@@ -223,9 +133,8 @@ test("expect CatalogItemProducts sorted by minPrice from highest to lowest when 
     expect(error).toBeUndefined();
     return;
   }
-  const resultProductIds = result.catalogItems.nodes.map((node) => node.product._id);
 
-  expect(resultProductIds).toEqual(opaqueCatalogItemIds);
+  expect(result).toEqual(mockSortedByPriceHigh2LowCatalogItemsResponse);
 });
 
 // expect CatalogItems sorted by minPrice form low to high when sortOrder is asc
@@ -242,9 +151,8 @@ test("expect CatalogItemProducts sorted by minPrice from lowest to highest when 
     expect(error).toBeUndefined();
     return;
   }
-  const resultProductIds = result.catalogItems.nodes.map((node) => node.product._id);
 
-  expect(resultProductIds).toEqual(opaqueCatalogItemIds.reverse());
+  expect(result).toEqual(mockSortedByPriceLow2HighCatalogItemsResponse);
 });
 
 // expect error when invalid currency code is provided
