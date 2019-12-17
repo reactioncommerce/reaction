29293bd7165dc480ad4339f9e746bddb8e5e0b54
quophyie <quophyie@yahoo.com>
2019-12-17 14:04:50 +0000

    (refactor): update addUserPermissions resolver function to decode encoded shopId and accountId before passing to mutation function
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- src/core-services/account/resolvers/Mutation/addUserPermissions.js
+++ src/core-services/account/resolvers/Mutation/addUserPermissions.js
@@ -1,18 +1,25 @@
-import setUserPermissions from "./setUserPermissions.js";
+import { decodeShopOpaqueId, decodeAccountOpaqueId } from "../../xforms/id.js";
 
 /**
  * @name Mutation/addUserPermissions
  * @method
  * @memberof Accounts/GraphQL
- * @summary resolver to add user permissions. This is an alias to {@link setUserPermissions.js}
+ * @summary resolver to add user permissions
  * @param {Object} _ - unused
- * @param {Object} args.input - an object of all mutation arguments that were sent by the client
- * @param {String} args.input.groups - The groups the user is to be added to
+ * @param {Object} input - an object of all mutation arguments that were sent by the client
+ * @param {String} input.groups - The group the user is to be added to
+ * @param {String} input.accountId - the userId of user to add to the given group
+ * @param {String} input.shopId - the shopId of the user to add to the given group
+ * @param {String} input.clientMutationId - the client mutation Id  i.e. string identifying the mutation call,
+ * which will be returned in the response payload
  * @param {Object} context - an object containing the per-request state
- * @param {Object} args.context.userId - the userId of user to add to the given group
- * @param {Object} args.context.accountId - the userId of account to add to the given groups
  * @returns {Object} - object
  */
 export default async function addUserPermissions(_, { input }, context) {
-  return setUserPermissions(_, { input }, context);
+  const { shopId, accountId, clientMutationId } = input;
+  const decodedShopId = decodeShopOpaqueId(shopId);
+  const decodedAccountId = decodeAccountOpaqueId(accountId);
+  const transformedInput = { ...input, shopId: decodedShopId, accountId: decodedAccountId };
+  const result = await context.mutations.addUserPermissions(context, transformedInput);
+  return { account: result, clientMutationId };
 }
