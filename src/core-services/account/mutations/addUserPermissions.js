383af5460cee927f4b5e5cd06991b35a60ecefdb
quophyie <quophyie@yahoo.com>
2019-12-17 13:40:48 +0000

    (refactor): update exception message to be more specific when account is not found during update operation of adding account group
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- src/core-services/account/mutations/addUserPermissions.js
+++ src/core-services/account/mutations/addUserPermissions.js
@@ -54,6 +54,10 @@ export default async function addUserPermissions(context, input) {
     }
   );
 
+  if (!updatedAccount) {
+    throw new ReactionError("server-error", "Unable to update account groups. Account not found");
+  }
+
   // Create an array which contains all fields that have changed
   // This is used for search, to determine if we need to re-index
   const updatedFields = ["groups"];
