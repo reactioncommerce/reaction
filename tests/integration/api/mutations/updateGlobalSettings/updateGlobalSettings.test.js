1bf5d3749a182eeca1343d2e30809bc58f6ea6b5
Will Lopez <will.lopez77@gmail.com>
2019-12-19 12:34:06 -0600

    chore: add updateGlobalSettings mutation integration test
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

---
+++ tests/integration/api/mutations/updateGlobalSettings/updateGlobalSettings.test.js
@@ -0,0 +1,91 @@
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+import Factory from "/tests/util/factory.js";
+import TestApp from "/tests/util/TestApp.js";
+
+const updateGlobalSettings = importAsString("./updateGlobalSettings.graphql");
+const TestGlobalSettingSchema = ` 
+  extend type GlobalSettings {
+    canSellDigitalProducts: Boolean
+  }
+
+  extend input GlobalSettingsUpdates {
+    canSellDigitalProducts: Boolean
+  }
+
+  extend input UpdateGlobalSettingsInput {
+    """
+    If true a shop can sell digital products
+    """
+    canSellDigitalProducts: Boolean
+  }
+`;
+
+jest.setTimeout(300000);
+
+const shopId = "123";
+const shopName = "Test Shop";
+let testApp;
+let globalSettingsMutation;
+
+const mockGlobalSetting = {
+  shopId,
+  canSellDigitalProducts: false
+};
+
+const mockAdminAccount = Factory.Account.makeOne({
+  roles: {
+    [shopId]: ["admin"]
+  }
+});
+
+beforeAll(async () => {
+  testApp = new TestApp();
+
+  testApp.registerPlugin({
+    name: "testGlobalSetting",
+    graphQL: {
+      schemas: [TestGlobalSettingSchema]
+    },
+    globalSettingsConfig: {
+      canSellDigitalProducts: {
+        rolesThatCanEdit: ["admin"],
+        simpleSchema: {
+          type: Boolean
+        }
+      }
+    }
+  });
+
+  await testApp.start();
+  await testApp.insertPrimaryShop({ _id: shopId, name: shopName });
+  await testApp.createUserAndAccount(mockAdminAccount);
+  await testApp.collections.AppSettings.insertOne(mockGlobalSetting);
+  globalSettingsMutation = testApp.query(updateGlobalSettings);
+});
+
+afterAll(async () => {
+  await testApp.collections.AppSettings.deleteMany({});
+  await testApp.collections.Shops.deleteMany({});
+  await testApp.stop();
+});
+
+test("global settings can be updated", async () => {
+  let result;
+  testApp.setLoggedInUser(mockAdminAccount);
+
+  try {
+    result = await globalSettingsMutation({
+      input: {
+        settingsUpdates: {
+          canSellDigitalProducts: true
+        }
+      }
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  console.log(JSON.stringify(result));
+  expect(result.globalSettings.canSellDigitalProducts).toEqual(true);
+});
