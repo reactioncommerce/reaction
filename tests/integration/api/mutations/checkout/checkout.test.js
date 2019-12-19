328133ececaa47126a7e9aea71da817fe38bdf42
Mike Murray <hellomikemurray@gmail.com>
2019-12-19 14:22:04 -0800

    test: add full cart integration test
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ tests/integration/api/mutations/checkout/checkout.test.js
@@ -0,0 +1,263 @@
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+import TestApp from "/tests/util/TestApp.js";
+
+const AddCartItemsMutation = importAsString("./AddCartItemsMutation.graphql");
+const RemoveCartItemsMutation = importAsString("./RemoveCartItemsMutation.graphql");
+const CreateCartMutation = importAsString("./CreateCartMutation.graphql");
+const SetShippingAddressOnCartMutation = importAsString("./SetShippingAddressOnCartMutation.graphql");
+const UpdateCartItemsQuantityMutation = importAsString("./UpdateCartItemsQuantityMutation.graphql");
+const PublishProductToCatalogMutation = importAsString("./PublishProductsToCatalogMutation.graphql");
+
+jest.setTimeout(300000);
+
+const encodeProductOpaqueId = encodeOpaqueId("reaction/product");
+
+const internalShopId = "123";
+const internalProductId = "999";
+const opaqueProductId = "cmVhY3Rpb24vcHJvZHVjdDo5OTk="; // reaction/product:999
+const internalTagIds = ["923", "924"];
+const opaqueShopId = "cmVhY3Rpb24vc2hvcDoxMjM="; // reaction/shop:123
+const internalVariantIds = ["875", "874", "925"];
+
+const shopName = "Test Shop";
+
+const mockProduct = {
+  _id: internalProductId,
+  ancestors: [],
+  title: "Fake Product",
+  shopId: internalShopId,
+  isDeleted: false,
+  isVisible: true,
+  supportedFulfillmentTypes: ["shipping"]
+};
+
+const mockVariant = {
+  _id: internalVariantIds[0],
+  ancestors: [internalProductId],
+  attributeLabel: "Variant",
+  title: "Fake Product Variant",
+  shopId: internalShopId,
+  isDeleted: false,
+  isVisible: true
+};
+
+const mockOptionOne = {
+  _id: internalVariantIds[1],
+  ancestors: [internalProductId, internalVariantIds[0]],
+  attributeLabel: "Option",
+  title: "Fake Product Option One",
+  shopId: internalShopId,
+  isDeleted: false,
+  isVisible: true,
+  price: 19.99
+};
+
+const mockOptionTwo = {
+  _id: internalVariantIds[2],
+  ancestors: [internalProductId, internalVariantIds[0]],
+  attributeLabel: "Option",
+  title: "Fake Product Option Two",
+  shopId: internalShopId,
+  isDeleted: false,
+  isVisible: true,
+  price: 29.99
+};
+
+let testApp;
+let addCartItems;
+let createCart;
+let removeCartItems;
+let publishProducts;
+let setShippingAddressOnCart;
+let updateCartItemsQuantity;
+
+beforeAll(async () => {
+  testApp = new TestApp();
+  await testApp.start();
+
+  addCartItems = testApp.mutate(AddCartItemsMutation);
+  createCart = testApp.mutate(CreateCartMutation);
+  removeCartItems = testApp.mutate(RemoveCartItemsMutation);
+  publishProducts = testApp.mutate(PublishProductToCatalogMutation);
+  removeCartItems = testApp.mutate(RemoveCartItemsMutation);
+  setShippingAddressOnCart = testApp.mutate(SetShippingAddressOnCartMutation);
+  updateCartItemsQuantity = testApp.mutate(UpdateCartItemsQuantityMutation);
+
+  await testApp.insertPrimaryShop({ _id: internalShopId, name: shopName });
+
+  // Add Tags and products
+  await Promise.all(internalTagIds.map((_id) => testApp.collections.Tags.insertOne({ _id, shopId: internalShopId, slug: `slug${_id}` })));
+  await testApp.collections.Products.insertOne(mockProduct);
+  await testApp.collections.Products.insertOne(mockVariant);
+  await testApp.collections.Products.insertOne(mockOptionOne);
+  await testApp.collections.Products.insertOne(mockOptionTwo);
+
+  await testApp.setLoggedInUser({
+    _id: "123",
+    roles: { [internalShopId]: ["createProduct"] }
+  });
+
+  // Publish products to the catalog
+  await publishProducts({ productIds: [opaqueProductId] });
+});
+
+afterAll(async () => {
+  await testApp.collections.Shops.deleteOne({ _id: internalShopId });
+  await testApp.collections.Products.deleteOne({ _id: internalProductId });
+  await testApp.clearLoggedInUser();
+  await testApp.stop();
+});
+
+describe("as a signed in user", () => {
+  let opaqueCartId;
+  let opaqueCartItemId;
+  let opaqueCartItemIdToRemove;
+
+  // create a new cart
+  test("create a new cart with one item", async () => {
+    let result;
+    try {
+      result = await createCart({
+        createCartInput: {
+          shopId: opaqueShopId,
+          items: {
+            price: {
+              amount: 19.99,
+              currencyCode: "USD"
+            },
+            productConfiguration: {
+              productId: opaqueProductId,
+              productVariantId: encodeProductOpaqueId(internalVariantIds[1])
+            },
+            quantity: 1
+          }
+        }
+      });
+    } catch (error) {
+      expect(error).toBeUndefined();
+      return;
+    }
+
+    opaqueCartId = result.createCart.cart._id;
+    opaqueCartItemId = result.createCart.cart.items.nodes[0]._id;
+
+    expect(result.createCart.cart.items.nodes[0].quantity).toEqual(1);
+    expect(result.createCart.cart.items.nodes[0].price.amount).toEqual(19.99);
+  });
+
+  test("add 1 more quantity to an existing cart item", async () => {
+    let result;
+    try {
+      result = await updateCartItemsQuantity({
+        updateCartItemsQuantityInput: {
+          cartId: opaqueCartId,
+          items: [
+            {
+              cartItemId: opaqueCartItemId,
+              quantity: 2
+            }
+          ]
+        }
+      });
+    } catch (error) {
+      expect(error).toBeUndefined();
+      return;
+    }
+
+    expect(result.updateCartItemsQuantity.cart.items.nodes[0].quantity).toEqual(2);
+    expect(result.updateCartItemsQuantity.cart.items.nodes[0].price.amount).toEqual(19.99);
+  });
+
+  test("add another item to the cart with a quantity of 5", async () => {
+    let result;
+    try {
+      result = await addCartItems({
+        addCartItemsInput: {
+          cartId: opaqueCartId,
+          items: {
+            price: {
+              amount: 29.99,
+              currencyCode: "USD"
+            },
+            productConfiguration: {
+              productId: opaqueProductId,
+              productVariantId: encodeProductOpaqueId(internalVariantIds[2])
+            },
+            quantity: 5
+          }
+        }
+      });
+    } catch (error) {
+      expect(error).toBeUndefined();
+      return;
+    }
+
+    opaqueCartItemIdToRemove = result.addCartItems.cart.items.nodes[0]._id;
+
+    expect(result.addCartItems.cart.items.nodes[0].quantity).toEqual(5);
+    expect(result.addCartItems.cart.items.nodes[0].price.amount).toEqual(29.99);
+    expect(result.addCartItems.cart.items.nodes[1].quantity).toEqual(2);
+    expect(result.addCartItems.cart.items.nodes[1].price.amount).toEqual(19.99);
+  });
+
+  test("remove recently added item from cart", async () => {
+    let result;
+    try {
+      result = await removeCartItems({
+        removeCartItemsInput: {
+          cartId: opaqueCartId,
+          cartItemIds: [opaqueCartItemIdToRemove]
+        }
+      });
+    } catch (error) {
+      expect(error).toBeUndefined();
+      return;
+    }
+    expect(result.removeCartItems.cart.items.nodes.length).toEqual(1);
+    expect(result.removeCartItems.cart.items.nodes[0].quantity).toEqual(2);
+    expect(result.removeCartItems.cart.items.nodes[0].price.amount).toEqual(19.99);
+  });
+
+  test("set shipping address on cart", async () => {
+    let result;
+    try {
+      result = await setShippingAddressOnCart({
+        input: {
+          cartId: opaqueCartId,
+          address: {
+            address1: "12345 Drive Lane",
+            city: "The city",
+            country: "USA",
+            firstName: "FName",
+            fullName: "FName LName",
+            lastName: "LName",
+            phone: "5555555555",
+            postal: "97878",
+            region: "CA"
+          }
+        }
+      });
+    } catch (error) {
+      expect(error).toBeUndefined();
+      return;
+    }
+
+    expect(result.setShippingAddressOnCart.cart.checkout.fulfillmentGroups[0].shippingAddress).toEqual({
+      address1: "12345 Drive Lane",
+      address2: null,
+      city: "The city",
+      company: null,
+      country: "USA",
+      firstName: "FName",
+      fullName: "FName LName",
+      isBillingDefault: false,
+      isCommercial: false,
+      isShippingDefault: false,
+      lastName: "LName",
+      phone: "5555555555",
+      postal: "97878",
+      region: "CA"
+    });
+  });
+});
