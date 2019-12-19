f443c5f8af19162b2c51485247f595732c6a6323
trojanh <rajan.tiwari@kiprosh.com>
2019-12-19 19:47:39 +0530

    test: fix eslint
    Signed-off-by: trojanh <rajan.tiwari@kiprosh.com>

--- tests/integration/api/mutations/shippingRates/createFlatRateFulfillmentMethod.test.js
+++ tests/integration/api/mutations/shippingRates/createFlatRateFulfillmentMethod.test.js
@@ -1,4 +1,3 @@
-import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
 import importAsString from "@reactioncommerce/api-utils/importAsString.js";
 import Factory from "/tests/util/factory.js";
 import TestApp from "/tests/util/TestApp.js";
