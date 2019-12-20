98bb64b5c68fff9021ccffc465e659b0152e2d35
trojanh <rajan.tiwari@kiprosh.com>
2019-12-20 20:41:02 +0530

    test: add integration test for deleteFlatRateFulfillmentRestriction mutation
    Signed-off-by: trojanh <rajan.tiwari@kiprosh.com>

---
+++ tests/integration/api/mutations/shippingRates/deleteFlatRateFulfillmentRestriction.test.js
@@ -0,0 +1,93 @@
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+import Factory from "/tests/util/factory.js";
+import TestApp from "/tests/util/TestApp.js";
+
+const DeleteFlatRateFulfillmentRestrictionMutation = importAsString("./DeleteFlatRateFulfillmentRestrictionMutation.graphql");
+
+jest.setTimeout(300000);
+
+const internalShopId = "123";
+const internalRestrictionId = "456";
+const opaqueShopId = encodeOpaqueId("reaction/shop", internalShopId); // reaction/shop:123
+const opaqueRestrictionId = encodeOpaqueId("reaction/flatRateFulfillmentRestriction", internalRestrictionId);
+const shopName = "Test Shop";
+let deleteFlatRateFulfillmentRestriction;
+let testApp;
+
+const mockFulfillmentRestriction = {
+  _id: internalRestrictionId,
+  shopId: internalShopId,
+  attributes: null,
+  type: "allow",
+  destination: {
+    country: ["US"],
+    postal: ["90817"],
+    region: ["CA"]
+  }
+};
+
+const mockOwnerAccount = Factory.Account.makeOne({
+  roles: {
+    [internalShopId]: ["owner"]
+  },
+  shopId: internalShopId
+});
+
+beforeAll(async () => {
+  testApp = new TestApp();
+  await testApp.start();
+
+  await testApp.insertPrimaryShop({ _id: internalShopId, name: shopName });
+  await testApp.collections.FlatRateFulfillmentRestrictions.insertOne(mockFulfillmentRestriction);
+  await testApp.createUserAndAccount(mockOwnerAccount);
+  deleteFlatRateFulfillmentRestriction = testApp.mutate(DeleteFlatRateFulfillmentRestrictionMutation);
+});
+
+afterAll(async () => {
+  await testApp.collections.Accounts.deleteMany({});
+  await testApp.collections.users.deleteMany({});
+  await testApp.collections.FlatRateFulfillmentRestrictions.deleteMany({});
+  await testApp.collections.Shops.deleteMany({});
+  await testApp.stop();
+});
+
+afterEach(async () => {
+  await testApp.clearLoggedInUser();
+});
+
+test("shop owner can delete flat rate fulfillment restriction", async () => {
+  let result;
+  await testApp.setLoggedInUser(mockOwnerAccount);
+
+  try {
+    result = await deleteFlatRateFulfillmentRestriction({
+      input: {
+        shopId: opaqueShopId,
+        restrictionId: opaqueRestrictionId
+      }
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
+
+  expect(result.deleteFlatRateFulfillmentRestriction.restriction).toEqual({
+    ...mockFulfillmentRestriction,
+    _id: opaqueRestrictionId,
+    shopId: opaqueShopId
+  });
+});
+
+test("shop owner cannot delete flat rate fulfillment restriction if not logged in", async () => {
+  try {
+    await deleteFlatRateFulfillmentRestriction({
+      input: {
+        shopId: opaqueShopId,
+        restrictionId: opaqueRestrictionId
+      }
+    });
+  } catch (error) {
+    expect(error).toMatchSnapshot();
+  }
+});
