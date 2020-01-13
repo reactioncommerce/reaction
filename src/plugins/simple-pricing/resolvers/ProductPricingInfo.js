b8d775c5f248c717c505bdf6adc16a08a5407aa9
Peter Lyons <pete@peterlyons.com>
2020-01-13 13:50:31 -0700

    feat(simple-pricing): Compute displayPrice as resolver as needed
    This creates compatibility with both the core catalog plugin and
    catalog publisher.
    
    Signed-off-by: Peter Lyons <pete@peterlyons.com>

--- src/plugins/simple-pricing/resolvers/ProductPricingInfo.js
+++ src/plugins/simple-pricing/resolvers/ProductPricingInfo.js
@@ -1,6 +1,20 @@
 import getCurrencyDefinitionByCode from "@reactioncommerce/api-utils/getCurrencyDefinitionByCode.js";
+import getDisplayPrice from "../util/getDisplayPrice.js";
 
 export default {
   currency: (node) => getCurrencyDefinitionByCode(node.currencyCode),
-  compareAtPrice: ({ compareAtPrice: amount, currencyCode }) => (typeof amount === "number" && amount > 0 ? { amount, currencyCode } : null)
+  compareAtPrice: ({ compareAtPrice: amount, currencyCode }) =>
+    typeof amount === "number" && amount > 0 ? { amount, currencyCode } : null,
+  displayPrice: (node) => {
+    if (node.displayPrice) {
+      // Operating in core catalog plugin mode.
+      // Use displayPrice directly from mongo.
+      // It was computed at publish time.
+      return node.displayPrice;
+    }
+    // Operating in catalog publisher mode.
+    // displayPrice was not computed ahead of time.
+    // Compute it on the fly now.
+    return getDisplayPrice(node.minPrice, node.maxPrice, getCurrencyDefinitionByCode(node.currencyCode));
+  }
 };
