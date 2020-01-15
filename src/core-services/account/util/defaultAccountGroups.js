7611ec38c16b31627d4cd37e54aed0a67a6a8607
Mike Murray <hellomikemurray@gmail.com>
2020-01-15 15:23:56 -0800

    fix: update shop manager slug
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- src/core-services/account/util/defaultAccountGroups.js
+++ src/core-services/account/util/defaultAccountGroups.js
@@ -2,7 +2,7 @@
 export const defaultCustomerGroupSlug = "customer";
 export const defaultGuestGroupSlug = "guest";
 export const defaultOwnerGroupSlug = "owner";
-export const defaultShopManagerGroupSlug = "shop-manager";
+export const defaultShopManagerGroupSlug = "shop manager";
 
 export default [
   defaultCustomerGroupSlug,
