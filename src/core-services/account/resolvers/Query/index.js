b70575134d2a8943ec9a0c594b995953ef3ddc36
Erik Kieckhafer <ek@ato.la>
2020-01-20 13:00:25 -0800

    refactor: remove administrators queires as they are no longer relevent
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/core-services/account/resolvers/Query/index.js
+++ src/core-services/account/resolvers/Query/index.js
@@ -1,6 +1,5 @@
 import account from "./account.js";
 import accounts from "./accounts.js";
-import administrators from "./administrators.js";
 import group from "./group.js";
 import groups from "./groups.js";
 import roles from "./roles.js";
@@ -9,7 +8,6 @@ import viewer from "./viewer.js";
 export default {
   account,
   accounts,
-  administrators,
   group,
   groups,
   roles,
--- src/core-services/account/resolvers/Shop/index.js
+++ src/core-services/account/resolvers/Shop/index.js
@@ -1,9 +1,7 @@
-import administrators from "./administrators.js";
 import groups from "./groups.js";
 import roles from "./roles.js";
 
 export default {
-  administrators,
   groups,
   roles
 };
