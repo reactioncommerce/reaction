ffd67693cbccca3d7f8328e2822423c9db9b8ac0
Erik Kieckhafer <ek@ato.la>
2020-01-21 18:53:12 -0800

    refactor: remove auth check in appSettings
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/core-services/settings/queries/appSettings.js
+++ src/core-services/settings/queries/appSettings.js
@@ -12,10 +12,5 @@ export default async function appSettings(context, shopId = null) {
 
   const settings = (await AppSettings.findOne({ shopId })) || {};
 
-  shopId ?
-    await context.validatePermissions(`reaction:legacy:shops:${shopId}`, "read", { shopId })
-    :
-    await context.validatePermissions("reaction:legacy:shops", "read", { shopId });
-
   return shopId ? addShopSettingDefaults(settings) : addGlobalSettingDefaults(settings);
 }
