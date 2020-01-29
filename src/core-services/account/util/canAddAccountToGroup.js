620ca46891957ea4b47a4f5f2a3a212686be3dfb
Erik Kieckhafer <ek@ato.la>
2020-01-29 15:38:42 -0800

    refactor: remove isOwnerAccount logic from `canAddAccountToGroup`
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/core-services/account/util/canAddAccountToGroup.js
+++ src/core-services/account/util/canAddAccountToGroup.js
@@ -7,8 +7,6 @@
  */
 export default async function canAddAccountToGroup(context, group) {
   const {
-    account: contextUserAccount,
-    collections: { Groups },
     isInternalCall,
     userHasPermission
   } = context;
@@ -17,18 +15,9 @@ export default async function canAddAccountToGroup(context, group) {
 
   const { shopId } = group;
 
-  // An account can add another account to a group as long as the person adding
-  // has all permissions granted by that group.
-  // We can't use `userHasPermission` here because we want to make sure they
-  // have ALL the permissions rather than ANY.
-  // Accounts in the "owner" group and users with the global "owner" permission
-  // are able to add any user to any group, regardless of other permissions.
-  const ownerGroup = await Groups.findOne({ name: "owner", shopId });
-  const isOwnerAccount = (
-    !!ownerGroup && contextUserAccount && contextUserAccount.groups.includes(ownerGroup._id)) ||
-    userHasPermission("reaction:legacy:shops", "owner", { shopId }); // TODO(pod-auth): update this to figure out what to do with "owner"
-
-  const hasPermissionToAddAccountToGroup = await isOwnerAccount || await userHasPermission("reaction:legacy:groups", "manage:accounts", { shopId });
+  // An account can add another account to a group as long as the account adding
+  // has `reaction:legacy:groups/manage:accounts` permissions
+  const hasPermissionToAddAccountToGroup = await userHasPermission("reaction:legacy:groups", "manage:accounts", { shopId });
 
   return hasPermissionToAddAccountToGroup;
 }
