6819dafd9b721ef63d2e51d1d91ee58f6cf4c14a
Erik Kieckhafer <ek@ato.la>
2020-01-31 00:13:10 -0800

    style: lint fix
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- tests/integration/api/mutations/updateAccount/updateAccount.test.js
+++ tests/integration/api/mutations/updateAccount/updateAccount.test.js
@@ -52,42 +52,42 @@ afterEach(async () => {
   await testApp.clearLoggedInUser();
 });
 
-// test("user can update their own account", async () => {
-//   await testApp.setLoggedInUser(mockOtherUserAccount);
+test("user can update their own account", async () => {
+  await testApp.setLoggedInUser(mockOtherUserAccount);
 
-//   let result;
-//   try {
-//     result = await updateAccount({
-//       input: {
-//         accountId: otherAccountOpaqueId,
-//         currencyCode: "USD",
-//         language: "en",
-//         firstName: "FIRST",
-//         lastName: "LAST",
-//         name: "FIRST LAST",
-//         note: "This is a note",
-//         username: "emanresu",
-//         picture: "https://foo.bar.com/me.jpg",
-//         bio: "Test account"
-//       }
-//     });
-//   } catch (error) {
-//     expect(error).toBeUndefined();
-//     return;
-//   }
+  let result;
+  try {
+    result = await updateAccount({
+      input: {
+        accountId: otherAccountOpaqueId,
+        currencyCode: "USD",
+        language: "en",
+        firstName: "FIRST",
+        lastName: "LAST",
+        name: "FIRST LAST",
+        note: "This is a note",
+        username: "emanresu",
+        picture: "https://foo.bar.com/me.jpg",
+        bio: "Test account"
+      }
+    });
+  } catch (error) {
+    expect(error).toBeUndefined();
+    return;
+  }
 
-//   expect(result.updateAccount.account).toEqual({
-//     currency: { code: "USD" },
-//     language: "en",
-//     firstName: "FIRST",
-//     lastName: "LAST",
-//     name: "FIRST LAST",
-//     note: "This is a note",
-//     username: "emanresu",
-//     picture: "https://foo.bar.com/me.jpg",
-//     bio: "Test account"
-//   });
-// });
+  expect(result.updateAccount.account).toEqual({
+    currency: { code: "USD" },
+    language: "en",
+    firstName: "FIRST",
+    lastName: "LAST",
+    name: "FIRST LAST",
+    note: "This is a note",
+    username: "emanresu",
+    picture: "https://foo.bar.com/me.jpg",
+    bio: "Test account"
+  });
+});
 
 test("accounts admin can update any other account", async () => {
   await testApp.setLoggedInUser(mockAdminUserAccount);
@@ -126,24 +126,24 @@ test("accounts admin can update any other account", async () => {
   });
 });
 
-// test("user cannot update an account if not logged in", async () => {
-//   try {
-//     await updateAccount({
-//       input: { accountId: otherAccountOpaqueId, currencyCode: "INR" }
-//     });
-//   } catch (errors) {
-//     expect(errors[0].message).toBe("Access Denied");
-//   }
-// });
+test("user cannot update an account if not logged in", async () => {
+  try {
+    await updateAccount({
+      input: { accountId: otherAccountOpaqueId, currencyCode: "INR" }
+    });
+  } catch (errors) {
+    expect(errors[0].message).toBe("Access Denied");
+  }
+});
 
-// test("user cannot update another account if they don't have permission", async () => {
-//   await testApp.setLoggedInUser(mockOtherUserAccount);
+test("user cannot update another account if they don't have permission", async () => {
+  await testApp.setLoggedInUser(mockOtherUserAccount);
 
-//   try {
-//     await updateAccount({
-//       input: { accountId: adminAccountOpaqueId, currencyCode: "INR" }
-//     });
-//   } catch (errors) {
-//     expect(errors[0].message).toBe("Access Denied");
-//   }
-// });
+  try {
+    await updateAccount({
+      input: { accountId: adminAccountOpaqueId, currencyCode: "INR" }
+    });
+  } catch (errors) {
+    expect(errors[0].message).toBe("Access Denied");
+  }
+});
