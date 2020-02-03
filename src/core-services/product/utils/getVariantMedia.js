18546b5623d323a6a728673a3a94d4b965955077
Mike Murray <hellomikemurray@gmail.com>
2020-02-03 15:23:30 -0800

    feat: return opaque id
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- src/core-services/product/utils/getVariantMedia.js
+++ src/core-services/product/utils/getVariantMedia.js
@@ -1,3 +1,5 @@
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
+
 /**
  *
  * @method getVariantMedia
@@ -29,6 +31,7 @@ export default async function getVariantMedia(variant, context) {
       const { priority, variantId: variantIdLocal } = metadata || {};
 
       return {
+        _id: encodeOpaqueId("reaction/mediaRecord", media._id),
         priority,
         variantId: variantIdLocal,
         URLs: {
