90be6dffec5e0cbdb554a23ea047d1a22d01e748
Ross Hadden <rosshadden@gmail.com>
2020-02-07 11:55:38 -0500

    refactor: renamed plugin keys
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/registerPlugins.js
+++ src/registerPlugins.js
@@ -8,102 +8,102 @@ export default {
   /**
    * CORE
    */
-  registerSimpleSchemaPlugin: "./plugins/simple-schema/index.js", // REQUIRED
-  registerJobQueuePlugin: "./plugins/job-queue/index.js", // REQUIRED
-  registerFilesPlugin: "./core-services/files/index.js", // REQUIRED
-  registerShopPlugin: "./core-services/shop/index.js", // REQUIRED
-  registerSettingsPlugin: "./core-services/settings/index.js", // REQUIRED
-  registerI18nPlugin: "./core-services/i18n/index.js", // REQUIRED
-  registerEmailPlugin: "./core-services/email/index.js", // REQUIRED
-  registerAddressPlugin: "./core-services/address/index.js", // REQUIRED
-  registerTranslationsPlugin: "./plugins/translations/index.js", // OPTIONAL
-  registerSystemInformationPlugin: "./plugins/system-info/index.js", // OPTIONAL
+  simpleSchema: "./plugins/simple-schema/index.js", // REQUIRED
+  jobQueue: "./plugins/job-queue/index.js", // REQUIRED
+  files: "./core-services/files/index.js", // REQUIRED
+  shop: "./core-services/shop/index.js", // REQUIRED
+  settings: "./core-services/settings/index.js", // REQUIRED
+  i18: "./core-services/i18n/index.js", // REQUIRED
+  email: "./core-services/email/index.js", // REQUIRED
+  address: "./core-services/address/index.js", // REQUIRED
+  translations: "./plugins/translations/index.js", // OPTIONAL
+  systemInfo: "./plugins/system-info/index.js", // OPTIONAL
 
   /**
    * Email
    */
-  registerEmailTemplatesPlugin: "./plugins/email-templates/index.js", // OPTIONAL
-  registerSMTPEmailPlugin: "./plugins/email-smtp/index.js", // OPTIONAL
+  emailTemplates: "./plugins/email-templates/index.js", // OPTIONAL
+  sMTPEmail: "./plugins/email-smtp/index.js", // OPTIONAL
 
   /**
    * Accounts
    */
-  registerAccountsPlugin: "./core-services/account/index.js", // REQUIRED
+  accounts: "./core-services/account/index.js", // REQUIRED
 
   /**
    * Authentication and Authorization
    */
-  registerAuthenticationPlugin: "./plugins/authentication/index.js", // REQUIRED
-  // registerAuthorizationPlugin: "@reactioncommerce/reaction-plugin-authorization", // REQUIRED // TODO(pod-auth): uncomment when `reaction-plugin-authorization` when available
-  registerLegacyAuthorizationPlugin: "./plugins/legacy-authorization/index.js", // REQUIRED
+  authentication: "./plugins/authentication/index.js", // REQUIRED
+  // authorization: "@reactioncommerce/reaction-plugin-authorization", // REQUIRED // TODO(pod-auth): uncomment when `reaction-plugin-authorization` when available
+  legacyAuthorization: "./plugins/legacy-authorization/index.js", // REQUIRED
 
   /**
    * Catalog
    */
-  registerProductPlugin: "./core-services/product/index.js", // REQUIRED
-  registerCatalogPlugin: "./core-services/catalog/index.js", // REQUIRED
-  registerTagsPlugin: "./core-services/tags/index.js", // REQUIRED
+  product: "./core-services/product/index.js", // REQUIRED
+  catalog: "./core-services/catalog/index.js", // REQUIRED
+  tags: "./core-services/tags/index.js", // REQUIRED
 
   /**
    * Pricing
    */
-  registerSimplePricingPlugin: "./plugins/simple-pricing/index.js", // OPTIONAL
+  simplePricing: "./plugins/simple-pricing/index.js", // OPTIONAL
 
   /**
    * Inventory
    */
-  registerInventoryPlugin: "./core-services/inventory/index.js", // REQUIRED
-  registerSimpleInventoryPlugin: "./plugins/simple-inventory/index.js", // OPTIONAL
+  inventory: "./core-services/inventory/index.js", // REQUIRED
+  simpleInventory: "./plugins/simple-inventory/index.js", // OPTIONAL
 
   /**
    * Cart
    */
-  registerCartPlugin: "./core-services/cart/index.js", // REQUIRED
+  cart: "./core-services/cart/index.js", // REQUIRED
 
   /**
    * Orders
    */
-  registerOrdersPlugin: "./core-services/orders/index.js", // REQUIRED
+  orders: "./core-services/orders/index.js", // REQUIRED
 
   /**
    * Payments
    */
-  registerPaymentsPlugin: "./core-services/payments/index.js", // REQUIRED
-  registerExamplePaymentsPlugin: "./plugins/payments-example/index.js", // OPTIONAL
-  registerStripePaymentsPlugin: "./plugins/payments-stripe/index.js", // OPTIONAL
+  payments: "./core-services/payments/index.js", // REQUIRED
+  examplePayments: "./plugins/payments-example/index.js", // OPTIONAL
+  stripePayments: "./plugins/payments-stripe/index.js", // OPTIONAL
 
   /**
    * Discounts
    */
-  registerDiscountsPlugin: "./core-services/discounts/index.js", // REQUIRED
-  registerDiscountCodesPlugin: "./plugins/discount-codes/index.js", // OPTIONAL
+  discounts: "./core-services/discounts/index.js", // REQUIRED
+  discountCodes: "./plugins/discount-codes/index.js", // OPTIONAL
 
   /**
    * Surcharges
    */
-  registerSurchargesPlugin: "./plugins/surcharges/index.js", // OPTIONAL
+  surcharges: "./plugins/surcharges/index.js", // OPTIONAL
 
   /**
    * Shipping
    */
-  registerShippingPlugin: "./core-services/shipping/index.js", // REQUIRED
-  registerShippingRatesPlugin: "./plugins/shipping-rates/index.js", // OPTIONAL
+  shipping: "./core-services/shipping/index.js", // REQUIRED
+  shippingRates: "./plugins/shipping-rates/index.js", // OPTIONAL
 
   /**
    * Taxes
    */
-  registerTaxesPlugin: "./core-services/taxes/index.js", // REQUIRED
-  registerTaxesRatesPlugin: "./plugins/taxes-rates/index.js", // OPTIONAL
+  taxes: "./core-services/taxes/index.js", // REQUIRED
+  taxesRates: "./plugins/taxes-rates/index.js", // OPTIONAL
 
   /**
    * Navigation
    */
-  registerNavigationPlugin: "./plugins/navigation/index.js", // OPTIONAL
-  registerSitemapGeneratorPlugin: "./plugins/sitemap-generator/index.js", // OPTIONAL
+  navigation: "./plugins/navigation/index.js", // OPTIONAL
+  sitemapGenerator: "./plugins/sitemap-generator/index.js", // OPTIONAL
 
   /**
    * Miscellaneous
    */
-  registerNotificationsPlugin: "./plugins/notifications/index.js", // OPTIONAL
-  registerTestAddressValidationPlugin: "./plugins/address-validation-test/index.js" // OPTIONAL
+  notifications: "./plugins/notifications/index.js", // OPTIONAL
+  testAddressValidation: "./plugins/address-validation-test/index.js" // OPTIONAL
 };
