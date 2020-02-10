2f2f0a6d25a656da3d86f5cac7ac53f76ff63a78
Erik Kieckhafer <ek@ato.la>
2020-02-10 14:56:46 -0800

    use original name for migrations
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- migrations/index.js
+++ migrations/index.js
@@ -3,7 +3,7 @@ import migration2 from "./2.js";
 export default {
   tracks: [
     {
-      namespace: "simple-authorization",
+      namespace: "legacy-authorization",
       migrations: {
         2: migration2
       }
