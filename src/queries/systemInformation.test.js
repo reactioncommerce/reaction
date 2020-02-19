921c962873009ce0e404bf99d1d0df453108dbcc
Erik Kieckhafer <ek@ato.la>
2020-02-18 23:11:54 -0800

    tests: add unit tests
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/queries/systemInformation.test.js
@@ -0,0 +1,19 @@
+import mockContext from "@reactioncommerce/api-utils/tests/mockContext.js";
+import ReactionError from "@reactioncommerce/reaction-error";
+import systemInformation from "./systemInformation.js";
+
+const fakeShopId = "FAKE_SHOP_ID";
+mockContext.validatePermissions = jest.fn("validatePermissions");
+mockContext.validatePermissions.mockReturnValueOnce(Promise.resolve(undefined));
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test("throws access-denied if not allowed", async () => {
+  mockContext.validatePermissions.mockImplementation(() => {
+    throw new ReactionError("access-denied", "Access Denied");
+  });
+  const result = systemInformation(mockContext, fakeShopId);
+  return expect(result).rejects.toThrowErrorMatchingSnapshot();
+});
