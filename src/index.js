cf64f13eebe85e261a50f4938799d630c10e7669
Erik Kieckhafer <ek@ato.la>
2020-02-19 22:10:38 -0800

    feat: initial commit of plugin files
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import pkg from "../package.json";
+import mutations from "./mutations/index.js";
+import tokenMiddleware from "./util/tokenMiddleware.js";
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {ReactionAPI} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "Authentication",
+    name: "reaction-authentication",
+    version: pkg.version,
+    collections: {
+      users: {
+        name: "users"
+      }
+    },
+    mutations,
+    expressMiddleware: [
+      {
+        route: "graphql",
+        stage: "authenticate",
+        fn: tokenMiddleware
+      }
+    ]
+  });
+}
