ee4b467df58e8c5080aec431bc685f33a7d510de
Eric Dobbertin <eric@dairystatedesigns.com>
2020-02-25 17:58:00 -0600

    refactor: remove empty Mutation ref
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,8 @@
 import getConnectionTypeResolvers from "@reactioncommerce/api-utils/graphql/getConnectionTypeResolvers.js";
-import Mutation from "./Mutation/index.js";
 import Query from "./Query/index.js";
 import Shop from "./Shop/index.js";
 
 export default {
-  Mutation,
   Query,
   Shop,
   ...getConnectionTypeResolvers("Role")
