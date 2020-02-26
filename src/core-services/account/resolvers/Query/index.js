5d3aca5f9766527024f3b14eb5061ecf682807b3
Eric Dobbertin <eric@dairystatedesigns.com>
2020-02-25 19:13:10 -0600

    feat: remove roles queries from account plugin
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/account/resolvers/Query/index.js
+++ src/core-services/account/resolvers/Query/index.js
@@ -2,7 +2,6 @@ import account from "./account.js";
 import accounts from "./accounts.js";
 import group from "./group.js";
 import groups from "./groups.js";
-import roles from "./roles.js";
 import viewer from "./viewer.js";
 
 export default {
@@ -10,6 +9,5 @@ export default {
   accounts,
   group,
   groups,
-  roles,
   viewer
 };
