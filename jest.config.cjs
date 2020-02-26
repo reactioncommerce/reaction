accea3603e1f1743796c4da219ab2cbb58d98355
Eric Dobbertin <eric@dairystatedesigns.com>
2020-02-25 18:24:21 -0600

    test: fix test config
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -81,7 +81,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@reactioncommerce/api-utils/(.*)$": "@reactioncommerce/api-utils/lib/$1"
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -174,7 +176,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
     // Any packages that are published only as ESM need to be listed here
-    "node_modules/(?!(@reactioncommerce/api-utils|@reactioncommerce/db-version-check|@reactioncommerce/plugin-simple-authorization)/)"
+    "node_modules/(?!(@reactioncommerce/api-utils|@reactioncommerce/db-version-check)/)"
   ]
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
