2474f7abd4291f0dbd7cf671b855e81b207cd967
Erik Kieckhafer <ek@ato.la>
2020-03-06 16:07:17 -0800

    feat: install sitemap-generator from npm
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/registerPlugins.js
+++ src/registerPlugins.js
@@ -1,6 +1,7 @@
 /* npm package imports */
 import registerAuthenticationPlugin from "@reactioncommerce/plugin-authentication";
 import registerSimpleAuthorizationPlugin from "@reactioncommerce/plugin-simple-authorization";
+import registerSitemapGeneratorPlugin from "@reactioncommerce/plugin-sitemap-generator";
 import registerSystemInformationPlugin from "@reactioncommerce/plugin-system-information";
 
 /* node-app imports */
@@ -33,7 +34,6 @@ import registerShippingRatesPlugin from "./plugins/shipping-rates/index.js";
 import registerSimpleInventoryPlugin from "./plugins/simple-inventory/index.js";
 import registerSimplePricingPlugin from "./plugins/simple-pricing/index.js";
 import registerSimpleSchemaPlugin from "./plugins/simple-schema/index.js";
-import registerSitemapGeneratorPlugin from "./plugins/sitemap-generator/index.js";
 import registerSMTPEmailPlugin from "./plugins/email-smtp/index.js";
 import registerStripePaymentsPlugin from "./plugins/payments-stripe/index.js";
 import registerSurchargesPlugin from "./plugins/surcharges/index.js";
