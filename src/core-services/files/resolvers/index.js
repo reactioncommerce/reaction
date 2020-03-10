2da271e088f9371f81a5187564cd67ddaf3f83fe
Eric Dobbertin <eric@dairystatedesigns.com>
2020-03-10 16:50:57 -0500

    refactor: combine all xformCatalogProductMedia
    into files plugin
    
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/files/resolvers/index.js
+++ src/core-services/files/resolvers/index.js
@@ -1,5 +1,8 @@
+import CatalogProductAndVariant from "./CatalogProductAndVariant.js";
 import Mutation from "./Mutation/index.js";
 
 export default {
+  CatalogProduct: CatalogProductAndVariant,
+  CatalogProductVariant: CatalogProductAndVariant,
   Mutation
 };
--- src/core-services/files/util/xformItemsAddImageUrls.js
+++ src/core-services/files/util/xformItemsAddImageUrls.js
@@ -1,26 +1,5 @@
 import getCatalogProductMedia from "./getCatalogProductMedia.js";
-
-/**
- * @name xformCatalogProductMedia
- * @method
- * @memberof GraphQL/Transforms
- * @summary Transforms DB media object to final GraphQL result. Calls functions plugins have registered for type
- *  "xformCatalogProductMedia". First to return an object is returned here
- * @param {Object} mediaItem Media item object. See ImageInfo SimpleSchema
- * @param {Object} context Request context
- * @returns {Object} Transformed media item
- */
-async function xformCatalogProductMedia(mediaItem, context) {
-  const xformCatalogProductMediaFuncs = context.getFunctionsOfType("xformCatalogProductMedia");
-  for (const func of xformCatalogProductMediaFuncs) {
-    const xformedMediaItem = await func(mediaItem, context); // eslint-disable-line no-await-in-loop
-    if (xformedMediaItem) {
-      return xformedMediaItem;
-    }
-  }
-
-  return mediaItem;
-}
+import xformCatalogProductMedia from "./xformCatalogProductMedia.js";
 
 /**
  * @summary Mutates an array of CartItem or OrderItem to add image fields at read time
