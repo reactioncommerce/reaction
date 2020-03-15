1fb65da9b3fa72c2517442cf80231ae41232c162
Eric Dobbertin <eric@dairystatedesigns.com>
2020-03-15 13:11:01 -0500

    feat: add more createProduct fields
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/core-services/product/mutations/createProduct.js
+++ src/core-services/product/mutations/createProduct.js
@@ -1,8 +1,14 @@
 import SimpleSchema from "simpl-schema";
 import Random from "@reactioncommerce/random";
-import { Product } from "../simpleSchemas.js";
+import ReactionError from "@reactioncommerce/reaction-error";
+import cleanProductInput from "../utils/cleanProductInput.js";
 
 const inputSchema = new SimpleSchema({
+  product: {
+    type: Object,
+    blackbox: true,
+    optional: true
+  },
   shopId: String
 });
 
@@ -11,19 +17,33 @@ const inputSchema = new SimpleSchema({
  * @summary creates an empty product, with an empty variant
  * @param {Object} context - an object containing the per-request state
  * @param {Object} input - Input arguments for the operation
+ * @param {String} [input.product] - product data
  * @param {String} input.shopId - the shop to create the product for
  * @return {String} created productId
  */
 export default async function createProduct(context, input) {
   inputSchema.validate(input);
-  const { collections } = context;
+
+  const { appEvents, collections, simpleSchemas } = context;
+  const { Product } = simpleSchemas;
   const { Products } = collections;
-  const { shopId } = input;
+  const { product: productInput, shopId } = input;
 
   // Check that user has permission to create product
   await context.validatePermissions("reaction:legacy:products", "create", { shopId });
 
   const newProductId = Random.id();
+
+  const initialProductData = await cleanProductInput(context, {
+    productId: newProductId,
+    productInput,
+    shopId
+  });
+
+  if (initialProductData.isDeleted) {
+    throw new ReactionError("invalid-param", "Creating a deleted product is not allowed");
+  }
+
   const createdAt = new Date();
   const newProduct = {
     _id: newProductId,
@@ -40,7 +60,8 @@ export default async function createProduct(context, input) {
     updatedAt: createdAt,
     workflow: {
       status: "new"
-    }
+    },
+    ...initialProductData
   };
 
   // Apply custom transformations from plugins.
@@ -61,6 +82,8 @@ export default async function createProduct(context, input) {
     productId: newProductId,
     shopId
   });
+
+  await appEvents.emit("afterProductCreate", { product: newProduct });
 
   return newProduct;
 }
--- src/core-services/product/resolvers/Mutation/createProduct.js
+++ src/core-services/product/resolvers/Mutation/createProduct.js
@@ -1,4 +1,4 @@
-import { decodeShopOpaqueId } from "../../xforms/id.js";
+import { decodeShopOpaqueId, decodeTagOpaqueId } from "../../xforms/id.js";
 
 /**
  *
@@ -7,7 +7,8 @@ import { decodeShopOpaqueId } from "../../xforms/id.js";
  * @param {Object} _ - unused
  * @param {Object} args - The input arguments
  * @param {Object} args.input - mutation input object
- * @param {String} args.input.clientMutationId - The mutation id
+ * @param {String} [args.input.clientMutationId] - The mutation id
+ * @param {String} [args.input.product] - product data
  * @param {String} args.input.shopId - shopId of shop to create product for
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Object>} createProduct payload
@@ -15,10 +16,17 @@ import { decodeShopOpaqueId } from "../../xforms/id.js";
 export default async function createProduct(_, { input }, context) {
   const {
     clientMutationId = null,
+    product: productInput,
     shopId
   } = input;
 
+  if (productInput && Array.isArray(productInput.tagIds)) {
+    productInput.hashtags = productInput.tagIds.map(decodeTagOpaqueId);
+    delete productInput.tagIds;
+  }
+
   const product = await context.mutations.createProduct(context, {
+    product: productInput,
     shopId: decodeShopOpaqueId(shopId)
   });
 
