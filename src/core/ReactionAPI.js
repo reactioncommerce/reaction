e28187ecf52ac59f21fe9788a0c58403cddc850c
Ross Hadden <rosshadden@gmail.com>
2020-03-20 11:52:41 -0400

    feat: logged debug message on plugin import, and sorted plugins
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/core/ReactionAPI.js
+++ src/core/ReactionAPI.js
@@ -479,6 +479,7 @@ export default class ReactionAPI {
       } else {
         await import(path.join("..", pluginPath));
       }
+      Logger.debug(`Imported plugin ${pluginPath}`);
     }
     /* eslint-enable no-await-in-loop */
 
