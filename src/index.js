272c2cabfaec69ac1cdf2bdd1748d3b60e68c7a3
Erik Kieckhafer <ek@ato.la>
2020-03-25 15:35:20 -0700

    feat: move plugin files from internal package
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import pkg from "../package.json";
+import getSurcharges from "./getSurcharges.js";
+import mutations from "./mutations/index.js";
+import policies from "./policies.json";
+import queries from "./queries/index.js";
+import resolvers from "./resolvers/index.js";
+import schemas from "./schemas/index.js";
+import setSurchargesOnCart from "./util/setSurchargesOnCart.js";
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {ReactionAPI} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "Surcharges",
+    name: "plugin-surcharges",
+    version: pkg.version,
+    collections: {
+      Surcharges: {
+        name: "Surcharges",
+        indexes: [
+          [{ shopId: 1 }]
+        ]
+      }
+    },
+    graphQL: {
+      resolvers,
+      schemas
+    },
+    mutations,
+    policies,
+    queries,
+    functionsByType: {
+      getSurcharges: [getSurcharges]
+    },
+    cart: {
+      transforms: [
+        {
+          name: "setSurchargesOnCart",
+          fn: setSurchargesOnCart,
+          priority: 20
+        }
+      ]
+    }
+  });
+}
