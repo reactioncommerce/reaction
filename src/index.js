7a9fc091f5719f414f8735e9056c607223752b5c
Erik Kieckhafer <ek@ato.la>
2020-03-24 21:57:43 -0700

    feat: move plugin files into npm package
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import pkg from "../package.json";
+import i18n from "./i18n/index.js";
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {ReactionAPI} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "Translations",
+    name: "plugin-translations",
+    version: pkg.version,
+    i18n
+  });
+}
