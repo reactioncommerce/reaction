80b0bfe805cc6247d5e8500e43dbf79d16dc9c6f
Erik Kieckhafer <ek@ato.la>
2020-03-30 15:34:43 -0700

    chore: set up repository
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import pkg from "../package.json";
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {ReactionAPI} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "Simple Schema",
+    name: "simple-schema",
+    version: pkg.version,
+  });
+}
