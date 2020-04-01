d55c4c01cea1ed071a3fcb584a08fac4d3b9c2a2
Erik Kieckhafer <ek@ato.la>
2020-03-31 17:18:06 -0700

    feat: move package files to plugin
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import pkg from "../package.json";
+import i18n from "./i18n/index.js";
+import mutations from "./mutations/index.js";
+import policies from "./policies.json";
+import queries from "./queries/index.js";
+import resolvers from "./resolvers/index.js";
+import schemas from "./schemas/index.js";
+import accountByUserId from "./util/accountByUserId.js";
+import { Account, Group } from "./simpleSchemas.js";
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -9,6 +17,43 @@ export default async function register(app) {
   await app.registerPlugin({
     label: "Account",
     name: "account",
-    version: pkg.version
+    version: pkg.version,
+    i18n,
+    collections: {
+      Accounts: {
+        name: "Accounts",
+        indexes: [
+          // Create indexes. We set specific names for backwards compatibility
+          // with indexes created by the aldeed:schema-index Meteor package.
+          [{ groups: 1 }, { name: "c2_groups" }],
+          [{ shopId: 1 }, { name: "c2_shopId" }],
+          [{ userId: 1 }, { name: "c2_userId" }],
+          [{ shopId: 1, slug: 1 }]
+        ]
+      },
+      AccountInvites: {
+        name: "AccountInvites",
+        indexes: [
+          [{ email: 1 }]
+        ]
+      },
+      Groups: {
+        name: "Groups"
+      }
+    },
+    auth: {
+      accountByUserId
+    },
+    graphQL: {
+      resolvers,
+      schemas
+    },
+    mutations,
+    queries,
+    policies,
+    simpleSchemas: {
+      Account,
+      Group
+    }
   });
 }
---
+++ src/queries/index.js
@@ -0,0 +1,13 @@
+import accounts from "./accounts.js";
+import group from "./group.js";
+import groups from "./groups.js";
+import groupsByAccount from "./groupsByAccount.js";
+import userAccount from "./userAccount.js";
+
+export default {
+  accounts,
+  group,
+  groups,
+  groupsByAccount,
+  userAccount
+};
