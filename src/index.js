6e53288d7b89b9d599eff394b3c7a9c8e25c1d09
Erik Kieckhafer <ek@ato.la>
2020-04-01 15:22:39 -0700

    chore: prepare for moving plugin
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import pkg from "../package.json";
  */
 export default async function register(app) {
   await app.registerPlugin({
-    label: "Plugin Example",
-    name: "plugin-example",
+    label: "Taxes",
+    name: "taxes",
     version: pkg.version
   });
 }
