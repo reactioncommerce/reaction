3729674d24e98d4da3f44e87dad0635cd8baff9a
Erik Kieckhafer <ek@ato.la>
2020-04-02 15:11:38 -0700

    refactor: update name of taxes-rates
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import pkg from "../package.json";
  */
 export default async function register(app) {
   await app.registerPlugin({
-    label: "Tax Rates",
-    name: "taxes-rates",
-    version: pkg.version,
+    label: "Flat Rate Taxes",
+    name: "taxes-flat-rate",
+    version: pkg.version
   });
 }
