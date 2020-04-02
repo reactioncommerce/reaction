77d37473a005082772b04c5d3d1102b2cef2523d
Erik Kieckhafer <ek@ato.la>
2020-04-02 14:26:50 -0700

    chore: prepare for moving service
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import pkg from "../package.json";
  */
 export default async function register(app) {
   await app.registerPlugin({
-    label: "Plugin Example",
-    name: "plugin-example",
+    label: "Carts",
+    name: "carts",
     version: pkg.version
   });
 }
