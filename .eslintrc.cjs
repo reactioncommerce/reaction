82960cfb87d13f2d27a7c23383805755bc0f26b3
Erik Kieckhafer <ek@ato.la>
2020-04-03 14:41:07 -0700

    refactor: use api-utils config
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,3 @@
 module.exports = {
-  extends: "@reactioncommerce",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      impliedStrict: true
-    }
-  },
-  env: {
-    es6: true,
-    jasmine: true
-  },
-  rules: {
-    "node/no-missing-import": "off",
-    "node/no-missing-require": "off",
-    "node/no-unsupported-features/es-syntax": "off",
-    "node/no-unpublished-import": "off",
-    "node/no-unpublished-require": "off"
-  }
+  extends: "./node_modules/@reactioncommerce/api-utils/lib/configs/eslintrc.cjs"
 };
