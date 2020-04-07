2a0ed9d1c2f9c6119859df0185eec81b4be05047
Erik Kieckhafer <ek@ato.la>
2020-04-07 16:01:21 -0700

    refactor: use accounts plugin from npm
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/core-services/account/resolvers/Group/index.js
+++
@@ -1,9 +0,0 @@
-import resolveShopFromShopId from "@reactioncommerce/api-utils/graphql/resolveShopFromShopId.js";
-import { encodeGroupOpaqueId } from "../../xforms/id.js";
-import createdBy from "./createdBy.js";
-
-export default {
-  _id: (node) => encodeGroupOpaqueId(node._id),
-  createdBy,
-  shop: resolveShopFromShopId
-};
