c623f9611586a22308ad80679f678bd030557901
Will Lopez <will.lopez77@gmail.com>
2020-04-14 17:03:33 -0700

    refactor: add searchField param to discountCodes query
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

--- src/queries/discountCodes.js
+++ src/queries/discountCodes.js
@@ -5,15 +5,37 @@
  * @summary Query the Discounts collection
  * @param {Object} context - an object containing the per-request state
  * @param {String} shopId - ID of Shop to query against
+ * @param {Object} filters - filters to be applied
  * @returns {Promise<Object>} DiscountCodes object Promise
  */
-export default async function discountCodes(context, shopId) {
+export default async function discountCodes(context, shopId, filters) {
   const { collections } = context;
   const { Discounts } = collections;
 
   await context.validatePermissions("reaction:legacy:discounts", "read", { shopId });
 
-  return Discounts.find({
+  // Create the mongo selector from the provided filters
+  let selector = { 
     shopId
-  });
+  }
+
+  // filter by searchField
+  if (filters.searchField) {
+    const cond = {
+      $regex: filters.searchField,
+      $options: "i"
+    };
+    selector = {
+      ...selector,
+      $or: [{
+        code: cond
+      }, {
+        label: cond
+      }, {
+        description: cond
+      }]
+    };
+  }
+
+  return Discounts.find(selector);
 }
