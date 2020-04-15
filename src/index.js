27b86b0429c942a30d166e562702c7e38a77247b
Maria Paktiti <maria.paktiti@gmail.com>
2020-04-14 19:33:50 -0500

    feat: move source code and enable semantic-release
    Signed-off-by: Maria Paktiti <maria.paktiti@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import pkg from "../package.json";
+import mutations from "./mutations/index.js";
+import queries from "./queries/index.js";
+import resolvers from "./resolvers/index.js";
+import schemas from "./schemas/index.js";
+import { registerPluginHandlerForAppSettings } from "./util/settingsConfig.js";
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -9,6 +14,21 @@ export default async function register(app) {
   await app.registerPlugin({
     label: "Settings",
     name: "settings",
-    version: pkg.version
+    version: pkg.version,
+    collections: {
+      AppSettings: {
+        name: "AppSettings",
+        indexes: [[{ shopId: 1 }, { unique: true }]],
+      },
+    },
+    functionsByType: {
+      registerPluginHandler: [registerPluginHandlerForAppSettings],
+    },
+    mutations,
+    queries,
+    graphQL: {
+      resolvers,
+      schemas,
+    },
   });
 }
