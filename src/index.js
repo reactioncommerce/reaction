76e7a59b92b5a94beafe1a46604f6a7767886314
Erik Kieckhafer <ek@ato.la>
2020-04-16 11:50:13 -0700

    feat: move files to plugin
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import pkg from "../package.json";
+import i18n from "./i18n/index.js";
+import queries from "./queries/index.js";
+import setDiscountsOnCart from "./util/setDiscountsOnCart.js";
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -9,6 +12,27 @@ export default async function register(app) {
   await app.registerPlugin({
     label: "Discounts",
     name: "discounts",
-    version: pkg.version
+    version: pkg.version,
+    i18n,
+    collections: {
+      Discounts: {
+        name: "Discounts",
+        indexes: [
+          // Create indexes. We set specific names for backwards compatibility
+          // with indexes created by the aldeed:schema-index Meteor package.
+          [{ shopId: 1 }, { name: "c2_shopId" }]
+        ]
+      }
+    },
+    queries,
+    cart: {
+      transforms: [
+        {
+          name: "setDiscountsOnCart",
+          fn: setDiscountsOnCart,
+          priority: 10
+        }
+      ]
+    }
   });
 }
