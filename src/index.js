94ae1db7b5d52e13de48a52c5099c32a47b118b8
Erik Kieckhafer <ek@ato.la>
2020-04-16 11:43:33 -0700

    feat: move files to plugin
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import pkg from "../package.json";
+import i18n from "./i18n/index.js";
+import mutations from "./mutations/index.js";
+import queries from "./queries/index.js";
+import { registerPluginHandlerForPayments } from "./registration.js";
+import resolvers from "./resolvers/index.js";
+import schemas from "./schemas/index.js";
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -9,6 +15,16 @@ export default async function register(app) {
   await app.registerPlugin({
     label: "Payments",
     name: "payments",
-    version: pkg.version
+    version: pkg.version,
+    i18n,
+    functionsByType: {
+      registerPluginHandler: [registerPluginHandlerForPayments]
+    },
+    graphQL: {
+      resolvers,
+      schemas
+    },
+    queries,
+    mutations
   });
 }
