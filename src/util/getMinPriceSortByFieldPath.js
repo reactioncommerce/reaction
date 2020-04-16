851278a720101d3663387e3a3c74a85e1c141bbb
Erik Kieckhafer <ek@ato.la>
2020-04-16 10:10:26 -0700

    feat: move package to plugin
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ src/util/getMinPriceSortByFieldPath.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} connectionArgs - an object of all arguments that were sent by the client
+ * @param {String} connectionArgs.sortPriceByCurrencyCode - currency code
+ * @returns {Number} minimum price
+ */
+export default function getMinPriceSortByFieldPathForSimplePricing(context, { connectionArgs }) {
+  const { sortByPriceCurrencyCode } = connectionArgs || {};
+
+  if (typeof sortByPriceCurrencyCode !== "string") {
+    throw new Error("sortByPriceCurrencyCode is required when sorting by minPrice");
+  }
+
+  return `product.pricing.${sortByPriceCurrencyCode}.minPrice`;
+}
---
+++ src/util/mutateNewProductBeforeCreate.js
@@ -0,0 +1,14 @@
+/**
+ * @summary Mutates a new top-level Product being created
+ * @param {Object} product Product object to mutate
+ * @returns {undefined}
+ */
+export default function mutateNewProductBeforeCreateForSimplePricing(product) {
+  if (!product.price) {
+    product.price = {
+      range: "0.00 - 0.00",
+      min: 0,
+      max: 0
+    };
+  }
+}
