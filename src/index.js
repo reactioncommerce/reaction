a5236036e5d17c20db386cb1f27986d79654ca2b
Erik Kieckhafer <ek@ato.la>
2020-04-17 16:44:36 -0700

    feat: move files to plugin
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import pkg from "../package.json";
+import i18n from "./i18n/index.js";
+import mutations from "./mutations/index.js";
+import queries from "./queries/index.js";
+import policies from "./policies.json";
+import resolvers from "./resolvers/index.js";
+import schemas from "./schemas/index.js";
+import startup from "./startup.js";
+import inventoryForProductConfigurations from "./utils/inventoryForProductConfigurations.js";
+import createDataLoaders from "./utils/createDataLoaders.js";
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -6,9 +15,36 @@ import pkg from "../package.json";
  * @returns {undefined}
  */
 export default async function register(app) {
+  /**
+   * Simple Inventory plugin
+   * Isolates the get/set of inventory data to this plugin.
+   */
   await app.registerPlugin({
     label: "Simple Inventory",
     name: "inventory-simple",
-    version: pkg.version
+    version: pkg.version,
+    i18n,
+    collections: {
+      SimpleInventory: {
+        name: "SimpleInventory",
+        indexes: [
+          [{ "productConfiguration.productVariantId": 1, "shopId": 1 }, { unique: true }],
+          // Use _id as second sort to force full stability
+          [{ updatedAt: 1, _id: 1 }]
+        ]
+      }
+    },
+    functionsByType: {
+      inventoryForProductConfigurations: [inventoryForProductConfigurations],
+      startup: [startup],
+      createDataLoaders: [createDataLoaders]
+    },
+    graphQL: {
+      resolvers,
+      schemas
+    },
+    mutations,
+    policies,
+    queries
   });
 }
