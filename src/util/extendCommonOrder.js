0280d0d2dbac789feb17642d084da3017d67a6cf
Eric Dobbertin <eric@dairystatedesigns.com>
2020-04-21 19:48:23 -0500

    fix: keep item props when extending order items
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- src/util/extendCommonOrder.js
+++ src/util/extendCommonOrder.js
@@ -40,17 +40,16 @@ function mergeProductAndVariants(productAndVariants) {
 
 /**
  * @name extendCommonOrder
- * @summary Get shipping attributes for a fulfillment group that will be used to
- * determine any applicable shipping restrictions.
- * @param {Object} context -  an object containing the per-request state
+ * @summary Add extra properties to all CommonOrder items, which will be used to
+ *   determine any applicable shipping restrictions.
+ * @param {Object} context - an object containing the per-request state
  * @param {Object} commonOrder - details about the purchase a user wants to make.
- * @param {Array} fulfillmentGroup.items - the items in the cart
- * @returns {Object|null} shipping restriction attributes for the provided fulfillment group
+ * @returns {Object|null} The CommonOrder, with each item in `.items` extended.
  */
 export default async function extendCommonOrder(context, commonOrder) {
   const { collections: { Tags }, getFunctionsOfType, queries } = context;
   const { items: orderItems } = commonOrder;
-  const products = [];
+  const extendedOrderItems = [];
 
   // Products in the Catalog collection are the source of truth, therefore use them
   // as the source of data instead of what is coming from the client.
@@ -64,15 +63,16 @@ export default async function extendCommonOrder(context, commonOrder) {
       throw new ReactionError("not-found", "Catalog product not found");
     }
 
-    const flattenProduct = mergeProductAndVariants(productAndVariants);
+    const extendedOrderItem = {
+      ...orderLineItem,
+      ...mergeProductAndVariants(productAndVariants)
+    };
 
     // Fetch product tags
-    allProductsTags.find((tag) => {
-      if (tag.productId === productAndVariants.product.productId) {
-        flattenProduct.tags = tag.tags;
-      }
-      return null;
-    });
+    const tagInfo = allProductsTags.find((info) => info.productId === extendedOrderItem.productId);
+    if (tagInfo) {
+      extendedOrderItem.tags = tagInfo.tags;
+    }
 
     // Fetch custom attributes
     // We need to run each of these functions in a series, rather than in parallel, because
@@ -80,14 +80,14 @@ export default async function extendCommonOrder(context, commonOrder) {
     // eslint rules when the output of one iteration might be used as input in another iteration, such as this case here.
     // See https://eslint.org/docs/rules/no-await-in-loop#when-not-to-use-it
     for (const customAttributesFunc of getFunctionsOfType("addShippingRestrictionCustomAttributes")) {
-      await customAttributesFunc(flattenProduct, productAndVariants); // eslint-disable-line
+      await customAttributesFunc(extendedOrderItem, productAndVariants); // eslint-disable-line
     }
 
-    products.push(flattenProduct);
+    extendedOrderItems.push(extendedOrderItem);
   }
 
   return {
     ...commonOrder,
-    items: products
+    items: extendedOrderItems
   };
 }
