e64d50bbeb368ad3f1923e033011a64f3809d4c5
Loan Laux <loan@outgrow.io>
2020-05-03 00:56:37 +0200

    feat: add customers query
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,4 +1,5 @@
 import accounts from "./accounts.js";
+import customers from "./customers.js";
 import group from "./group.js";
 import groups from "./groups.js";
 import groupsByAccount from "./groupsByAccount.js";
@@ -6,6 +7,7 @@ import userAccount from "./userAccount.js";
 
 export default {
   accounts,
+  customers,
   group,
   groups,
   groupsByAccount,
