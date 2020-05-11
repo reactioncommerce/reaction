673d8325aec8f2e82607ea3f35e703ca6ea1068d
Loan Laux <loan@outgrow.io>
2020-05-11 12:42:22 +0200

    feat: add shops query and resolver
    Signed-off-by: Loan Laux <loan@outgrow.io>

---
+++ src/queries/shops.js
@@ -0,0 +1,29 @@
+/**
+ * @name shops
+ * @summary Query the Shops collection for a list of shops.
+ *   If shopIds are provided, return the matching shops. Otherwise, return all the shops.
+ * @param {Object} context - an object containing the per-request state
+ * @param {Array} shopIds - optional array of shop IDs to return
+ * @returns {Promise<Object>} Products object Promise
+ */
+export default async function shops(context, shopIds) {
+  const { collections } = context;
+  const { Shops } = collections;
+
+  if (Array.isArray(shopIds) && shopIds.length > 0) {
+    // make sure we're authorized to read all the requested shopIds
+    for (const shopId of shopIds) {
+      await context.validatePermissions(`reaction:legacy:shops:${shopId}`, "read", { shopId });
+    }
+
+    return Shops.find({
+      _id: {
+        $in: shopIds
+      }
+    });
+  }
+
+  await context.validatePermissions("reaction:legacy:shops", "read", { shopId: null });
+
+  return Shops.find();
+}
---
+++ src/resolvers/Query/shops.js
@@ -0,0 +1,31 @@
+import getPaginatedResponse from "@reactioncommerce/api-utils/graphql/getPaginatedResponse.js";
+import wasFieldRequested from "@reactioncommerce/api-utils/graphql/wasFieldRequested.js";
+import { decodeShopOpaqueId } from "../../xforms/id";
+
+/**
+ * @name Shop/shops
+ * @method
+ * @memberof Shop/GraphQL
+ * @summary find and return the shops for a shop
+ * @param {Object} _ - unused
+ * @param {Object} args - an object of all arguments that were sent by the client
+ * @param {String} args.shopIds - Optional shopIds to filter by
+ * @param {String} args.after - Connection argument
+ * @param {String} args.before - Connection argument
+ * @param {Number} args.first - Connection argument
+ * @param {Number} args.last - Connection argument
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} info Info about the GraphQL request
+ * @returns {Promise<Object[]>} Promise that resolves with array of shop objects
+ */
+export default async function shops(_, { shopIds, ...connectionArgs }, context, info) {
+  const decodedShopIds = shopIds.map((shopId) => decodeShopOpaqueId(shopId));
+
+  const query = await context.queries.shops(context, decodedShopIds);
+
+  return getPaginatedResponse(query, connectionArgs, {
+    includeHasNextPage: wasFieldRequested("pageInfo.hasNextPage", info),
+    includeHasPreviousPage: wasFieldRequested("pageInfo.hasPreviousPage", info),
+    includeTotalCount: wasFieldRequested("totalCount", info)
+  });
+}
