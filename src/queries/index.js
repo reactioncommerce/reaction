544177678646ee784a3993bbd05adc9f1b46e27b
Loan Laux <loan@outgrow.io>
2020-05-11 13:24:00 +0200

    fix: update schema to allow pagination
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,6 +1,9 @@
+import shops from "./shops.js";
+
 export default {
   shopById: (context, _id) => context.dataLoaders.Shops.load(_id),
   shopBySlug: (context, slug) => context.collections.Shops.findOne({ slug }),
+  shops,
   primaryShop: (context) => context.collections.Shops.findOne({ shopType: "primary" }),
   async primaryShopId(context) {
     const shop = await context.collections.Shops.findOne({
