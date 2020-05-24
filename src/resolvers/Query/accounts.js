a3db5e8c6d70c636d983b08b19933db198e0f325
Loan Laux <loan@outgrow.io>
2020-05-24 20:16:04 +0200

    feat: add notInAnyGroups arg to accounts query
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- src/resolvers/Query/accounts.js
+++ src/resolvers/Query/accounts.js
@@ -10,19 +10,20 @@ import { decodeGroupOpaqueId } from "../../xforms/id.js";
  * @param {Object} _ - unused
  * @param {Object} args - an object of all arguments that were sent by the client
  * @param {String} [args.groupIds] - Array of group IDs
+ * @param {Boolean} [args.notInAnyGroups] - Return accounts that aren't part of any groups
  * @param {Object} context - an object containing the per-request state
  * @param {Object} info Info about the GraphQL request
  * @returns {Promise<Object>} Promise containing queried accounts
  */
 export default async function accounts(_, args, context, info) {
-  const { groupIds: opaqueGroupIds, ...connectionArgs } = args;
+  const { groupIds: opaqueGroupIds, notInAnyGroups, ...connectionArgs } = args;
 
   let groupIds;
   if (opaqueGroupIds) {
     groupIds = opaqueGroupIds.map((opaqueGroupId) => decodeGroupOpaqueId(opaqueGroupId));
   }
 
-  const query = await context.queries.accounts(context, { groupIds });
+  const query = await context.queries.accounts(context, { groupIds, notInAnyGroups });
 
   return getPaginatedResponse(query, connectionArgs, {
     includeHasNextPage: wasFieldRequested("pageInfo.hasNextPage", info),
