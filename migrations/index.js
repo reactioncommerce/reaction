98f011a7c30b038a9b4901186f16d386fd9448e4
Loan Laux <loan@outgrow.io>
2020-10-09 14:51:59 +0400

    chore: add migration for invitations/read role
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- migrations/index.js
+++ migrations/index.js
@@ -1,12 +1,14 @@
 import { migrationsNamespace } from "./migrationsNamespace.js";
 import migration2 from "./2.js";
+import migration3 from "./3.js";
 
 export default {
   tracks: [
     {
       namespace: migrationsNamespace,
       migrations: {
-        2: migration2
+        2: migration2,
+        3: migration3
       }
     }
   ]
