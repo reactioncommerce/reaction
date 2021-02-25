86aa9bbb35e2debe256ec243620eb35d0c24f0cb
Loan Laux <loan@outgrow.io>
2021-02-25 19:38:44 +0400

    feat: add support for Edges on discountCodes query
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,3 +1,4 @@
+import getConnectionTypeResolvers from "@reactioncommerce/api-utils/graphql/getConnectionTypeResolvers.js";
 import Mutation from "./Mutation/index.js";
 import Query from "./Query/index.js";
 import DiscountCode from "./DiscountCode/index.js";
@@ -5,5 +6,6 @@ import DiscountCode from "./DiscountCode/index.js";
 export default {
   DiscountCode,
   Mutation,
-  Query
+  Query,
+  ...getConnectionTypeResolvers("DiscountCode")
 };
