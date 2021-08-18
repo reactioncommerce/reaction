5f5182d0ab725d36b37948fae3db2449fcd8c952
Alvaro Bueno <alvaro.bueno@mailchimp.com>
2021-08-18 18:15:26 -0400

    feat: update Stripe SCA payments plugin
    Signed-off-by: Alvaro Bueno <alvaro.bueno@mailchimp.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -26,6 +26,7 @@ const externalNodeModules = [
   "@reactioncommerce/api-plugin-orders",
   "@reactioncommerce/api-plugin-payments-example",
   "@reactioncommerce/api-plugin-payments-stripe",
+  "@reactioncommerce/api-plugin-payments-stripe-scas",
   "@reactioncommerce/api-plugin-payments",
   "@reactioncommerce/api-plugin-pricing-simple",
   "@reactioncommerce/api-plugin-products",
