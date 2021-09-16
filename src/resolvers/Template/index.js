b0cd524b8355d355d422bb27221e196464b9ce67
Mohan Narayana <mohan.narayana@mailchimp.com>
2021-09-16 13:05:09 -0500

    fix: change getConnectionTypeResolvers parameter
    Signed-off-by: Mohan Narayana <mohan.narayana@mailchimp.com>
    Signed-off-by: Alvaro Bueno <alvaro.bueno@mailchimp.com>

--- src/resolvers/Template/index.js
+++
@@ -1,6 +0,0 @@
-import { encodeShopOpaqueId, encodeTemplateOpaqueId } from "../../xforms/id.js";
-
-export default {
-  _id: (node) => encodeTemplateOpaqueId(node._id),
-  shopId: (node) => encodeShopOpaqueId(node.shopId)
-};
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,11 +1,9 @@
 import getConnectionTypeResolvers from "@reactioncommerce/api-utils/graphql/getConnectionTypeResolvers.js";
 import Query from "./Query/index.js";
 import Mutation from "./Mutation/index.js";
-import Template from "./Template/index.js";
 
 export default {
-  ...getConnectionTypeResolvers(Template),
   Query,
   Mutation,
-  Template
+  ...getConnectionTypeResolvers("Template")
 };
