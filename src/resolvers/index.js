e4ff4938e1b79e206482f8ac66e4d91d5d616493
Alvaro Bueno <alvaro.bueno@mailchimp.com>
2021-09-16 16:30:15 +0200

    fix: add getConnectionTypeResolvers to index

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,8 +1,10 @@
+import getConnectionTypeResolvers from "@reactioncommerce/api-utils/graphql/getConnectionTypeResolvers.js";
 import Query from "./Query/index.js";
 import Mutation from "./Mutation/index.js";
 import Template from "./Template/index.js";
 
 export default {
+  ...getConnectionTypeResolvers(Template),
   Query,
   Mutation,
   Template
