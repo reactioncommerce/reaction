6747cf178d7571f2436429e53d7c04d939687917
Alvaro Bueno <alvaro.bueno@mailchimp.com>
2021-12-20 18:54:45 -0500

    fix: condition on settingsConfig

--- src/util/settingsConfig.js
+++ src/util/settingsConfig.js
@@ -15,7 +15,7 @@ export function addGlobalSettingDefaults(settings) {
     const value = settings[field];
     if (value === undefined || value === null) {
       const config = globalSettingsSchema[field];
-      if (config && config.defaultValue !== undefined) {
+      if (config !== null && config.defaultValue !== undefined) {
         settings[field] = config.defaultValue;
       }
     }
