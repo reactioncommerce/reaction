c2de5d44971d77634a357d4eaa0b2cada306f0e0
tedraykov <tedraykov@gmail.com>
2022-01-19 11:20:30 +0200

    fix: skip adding user to context if auth token has expired
    Signed-off-by: tedraykov <tedraykov@gmail.com>

--- src/util/getUserFromAuthToken.js
+++ src/util/getUserFromAuthToken.js
@@ -1,10 +1,14 @@
 import getAccounts from "./accountServer.js";
+import hasTokenExpired from "./hasTokenExpired.js";
+
 /**
  * Given an Authorization Bearer token and the current context, returns the user document
  * for that token after performing token checks.
  *
- * If the provided token is not associated with any user or is associated but is
- * expired, this function throws an "access-denied" ReactionError.
+ * If the provided token is not associated with any user, this function throws an
+ * "access-denied" ReactionError.
+ * If the provided token has expired, the function skips the user lookup and proceeds
+ * as if no token has been provided in the request
  *
  * @name getUserFromAuthToken
  * @method
@@ -16,8 +20,10 @@ import getAccounts from "./accountServer.js";
 async function getUserFromAuthToken(loginToken) {
   const { accountsServer } = await getAccounts();
   const authToken = loginToken.replace(/bearer\s/gi, "");
-  const user = await accountsServer.resumeSession(authToken);
-  return user;
+
+  if (hasTokenExpired(authToken)) return null;
+
+  return accountsServer.resumeSession(authToken);
 }
 
 export default getUserFromAuthToken;
