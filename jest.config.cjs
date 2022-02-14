d0aa0005310a54c93e76098a078055a455cec257
Brent Hoover <brent@thebuddhalodge.com>
2022-02-14 12:41:26 +0800

    feat: add coverage thresholds to jest config
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -233,6 +233,14 @@ const jestConfig = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
+  collectCoverage: true,
+  coverageThreshold: {
+    global: 50,
+    functions: 50,
+    lines: 50,
+    statements: 50
+  }
 };
 
 module.exports = jestConfig;
