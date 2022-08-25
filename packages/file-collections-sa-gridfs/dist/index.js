9f43b092c8f09a0c6b86c9d43bc76ec3fa43aa79
Brian Nguyen <vanpho02@gmail.com>
2022-08-25 14:41:15 +0700

    feat: udd dist to root .gitignore

--- packages/file-collections-sa-gridfs/dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";var _Object$defineProperty = require("@babel/runtime-corejs2/core-js/object/define-property");var _interopRequireDefault = require("@babel/runtime-corejs2/helpers/interopRequireDefault");_Object$defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _GridFSStore = _interopRequireDefault(require("./GridFSStore"));var _default =
-
-_GridFSStore.default;exports.default = _default;
--- packages/file-collections/dist/common/FileRecord/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";var _Object$defineProperty = require("@babel/runtime-corejs2/core-js/object/define-property");var _interopRequireDefault = require("@babel/runtime-corejs2/helpers/interopRequireDefault");_Object$defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _FileRecord = _interopRequireDefault(require("./FileRecord"));var _default =
-
-_FileRecord.default;exports.default = _default;
