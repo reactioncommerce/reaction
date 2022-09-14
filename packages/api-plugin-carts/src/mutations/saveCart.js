5f798fceb7bb84dedc95684cf1ca25ecb31150d4
Brent Hoover <brent@thebuddhalodge.com>
2022-09-14 13:23:24 +0800

    feat: pass on "emitted by" parameter
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-carts/src/mutations/saveCart.js
+++ packages/api-plugin-carts/src/mutations/saveCart.js
@@ -5,11 +5,11 @@ import ReactionError from "@reactioncommerce/reaction-error";
  *   validates, and upserts to database.
  * @param {Object} context - App context
  * @param {Object} cart - The cart to transform and insert or replace
+ * @param {Boolean} emittedBy - Who emitted the event
  * @returns {Object} Transformed and saved cart
  */
-export default async function saveCart(context, cart) {
+export default async function saveCart(context, cart, emittedBy) {
   const { appEvents, collections: { Cart }, userId = null } = context;
-
   // These will mutate `cart`
   await context.mutations.removeMissingItemsFromCart(context, cart);
   await context.mutations.transformAndValidateCart(context, cart);
@@ -20,12 +20,8 @@ export default async function saveCart(context, cart) {
   if (upsertedCount === 1) {
     appEvents.emit("afterCartCreate", {
       cart,
-      createdBy: userId
-    });
-  } else {
-    appEvents.emit("afterCartUpdate", {
-      cart,
-      updatedBy: userId
+      createdBy: userId,
+      emittedBy
     });
   }
 
