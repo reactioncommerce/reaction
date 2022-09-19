65479f677c1acd2b97fbb1cb2243c09ceaf426f2
Sujith <mail.sujithvn@gmail.com>
2022-09-20 01:44:14 +0530

    feat(fulfillment)!: fulfillment shipping flat rate
    First commit - api-plugin-fulfillment-shipping-flat-rate
    
    Signed-off-by: Sujith <mail.sujithvn@gmail.com>

---
+++ packages/api-plugin-fulfillment-method-shipping-flat-rate/src/queries/getFlatRateFulfillmentRestriction.js
@@ -0,0 +1,21 @@
+/**
+ * @name getFlatRateFulfillmentRestriction
+ * @method
+ * @memberof Fulfillment/NoMeteorQueries
+ * @summary Query the FulfillmentRestrictions collection for restrictions with the provided shopId
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} params - request parameters
+ * @param {String} params.shopId - Shop ID for the shop that owns the restriction
+ * @returns {Promise<Object>|undefined} - A restrictions document, if one is found
+ */
+export default async function getFlatRateFulfillmentRestriction(context, { restrictionId, shopId } = {}) {
+  const { collections } = context;
+  const { FulfillmentRestrictions } = collections;
+
+  await context.validatePermissions(`reaction:legacy:shippingRestrictions:${restrictionId}`, "read", { shopId });
+
+  return FulfillmentRestrictions.findOne({
+    _id: restrictionId,
+    shopId
+  });
+}
