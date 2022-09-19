87a28d7cd494ce831d45fc41dcb0076f780d99f3
Sujith <mail.sujithvn@gmail.com>
2022-09-20 01:44:14 +0530

    feat(fulfillment)!: fulfillment shipping flat rate
    First commit - api-plugin-fulfillment-shipping-flat-rate
    
    Signed-off-by: Sujith <mail.sujithvn@gmail.com>

---
+++ packages/api-plugin-fulfillment-method-shipping-flat-rate/src/mutations/updateFlatRateFulfillmentMethod.js
@@ -0,0 +1,55 @@
+import SimpleSchema from "simpl-schema";
+import ReactionError from "@reactioncommerce/reaction-error";
+import methodSchema from "../util/methodSchema.js";
+
+const inputSchema = new SimpleSchema({
+  method: methodSchema,
+  methodId: String,
+  shopId: String
+});
+
+/**
+ * @method updateFlatRateFulfillmentMethodMutation
+ * @summary updates a flat rate fulfillment method
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} input - Input (see SimpleSchema)
+ * @returns {Promise<Object>} An object with a `method` property containing the updated method
+ */
+export default async function updateFlatRateFulfillmentMethodMutation(context, input) {
+  const cleanedInput = inputSchema.clean(input); // add default values and such
+  inputSchema.validate(cleanedInput);
+
+  const { method: inputMethod, methodId, shopId } = cleanedInput;
+  const { collections } = context;
+  const { Fulfillment } = collections;
+  const method = { ...inputMethod };
+
+  if (!methodId) throw new ReactionError("invalid-param", "Method ID to be updated not provided");
+  await context.validatePermissions(`reaction:legacy:shippingMethods:${methodId}`, "update", { shopId });
+  // await context.validatePermissions(`reaction:legacy:fulfillmentMethods:${methodId}`, "update", { shopId });
+
+  // MongoDB schema still uses `enabled` rather than `isEnabled`
+  method.enabled = method.isEnabled;
+  delete method.isEnabled;
+
+  // Hardcoded field, each ff-method plugin has to introduce this field for grouping purpose
+  // Schema defined as optional=true for backward compatibility
+  method.fulfillmentMethod = "flatRate";
+
+  const { matchedCount } = await Fulfillment.updateOne({
+    "methods._id": methodId,
+    shopId
+  }, {
+    $set: {
+      "methods.$": {
+        ...method,
+        _id: methodId
+      }
+    }
+  });
+  if (matchedCount === 0) throw new ReactionError("not-found", "Not found");
+
+  inputMethod._id = methodId;
+
+  return { method: inputMethod };
+}
---
+++ packages/api-plugin-fulfillment-method-shipping-flat-rate/src/queries/flatRateFulfillmentMethods.js
@@ -0,0 +1,49 @@
+/**
+ * @name flatRateFulfillmentMethods
+ * @method
+ * @memberof Fulfillment/Queries
+ * @summary Query the Fulfillment collection for a list of fulfillment methods
+ * @param {Object} context - an object containing the per-request state
+ * @param {Object} input - Request input
+ * @param {String} input.shopId - The shop id of the fulfillment methods
+ * @returns {Promise<Object>} Mongo cursor
+ */
+export default async function flatRateFulfillmentMethods(context, input) {
+  const { collections } = context;
+  const { Fulfillment } = collections;
+  const { shopId } = input;
+  const fulfillmentTypeName = "shipping";
+  const fulfillmentMethodName = "flatRate";
+
+  await context.validatePermissions("reaction:legacy:shippingMethods", "read", { shopId });
+  await context.validatePermissions("reaction:legacy:fulfillmentMethods", "read", { shopId });
+
+  // aggregate pipeline to extract fulfillment methods inside shippment
+  return {
+    collection: Fulfillment,
+    pipeline: [
+      {
+        $match: {
+          "fulfillmentType": fulfillmentTypeName,
+          "methods.fulfillmentMethod": fulfillmentMethodName,
+          shopId
+        }
+      },
+      {
+        $unwind: "$methods"
+      },
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: [
+              "$methods",
+              {
+                shopId: "$$ROOT.shopId"
+              }
+            ]
+          }
+        }
+      }
+    ]
+  };
+}
