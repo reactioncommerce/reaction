1b51c301d0a73308d1750c94d1206563dc0a01fb
Sujith <mail.sujithvn@gmail.com>
2022-09-20 01:25:44 +0530

    feat(fulfillment): fulfillment-pickup-store commit
    First commit of the api-plugin-fulfillment-pickup-store
    
    Signed-off-by: Sujith <mail.sujithvn@gmail.com>

---
+++ packages/api-plugin-fulfillment-method-pickup-store/src/index.js
@@ -0,0 +1,36 @@
+import { createRequire } from "module";
+import fulfillmentMethodPickupStorePreStartup from "./preStartup.js";
+import fulfillmentMethodPickupStoreStartup from "./startup.js";
+import { MethodStoreData } from "./simpleSchemas.js";
+import schemas from "./schemas/index.js";
+import getFulfillmentMethodsWithQuotesPickupStore from "./getFulfillmentMethodsWithQuotesPickupStore.js";
+import validateOrderMethodsstore from "./util/validateOrderMethodsstore.js";
+
+const require = createRequire(import.meta.url);
+const pkg = require("../package.json");
+
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {Object} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: "Fulfillment Method Pickup Store",
+    name: "fulfillment-method-pickup-store",
+    version: pkg.version,
+    graphQL: {
+      schemas
+    },
+    simpleSchemas: {
+      MethodStoreData
+    },
+    functionsByType: {
+      preStartup: [fulfillmentMethodPickupStorePreStartup],
+      startup: [fulfillmentMethodPickupStoreStartup],
+      validateOrderMethods: [validateOrderMethodsstore],
+      getFulfillmentMethodsWithQuotes: [getFulfillmentMethodsWithQuotesPickupStore],
+      getFulfillmentMethodsWithQuotespickup: [getFulfillmentMethodsWithQuotesPickupStore]
+    }
+  });
+}
