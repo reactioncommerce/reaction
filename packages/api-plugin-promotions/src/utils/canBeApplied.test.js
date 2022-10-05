df19a0ebb3078760fe51b6c02ba69b75e6831830
vanpho93 <vanpho02@gmail.com>
2022-10-05 17:00:16 +0700

    feat: add canBeApplied and enhanceCart functions
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

---
+++ packages/api-plugin-promotions/src/utils/canBeApplied.test.js
@@ -0,0 +1,47 @@
+import canBeApplied from "./canBeApplied.js";
+
+const promotion = {
+  _id: "test id",
+  actions: [{ actionKey: "test" }],
+  triggers: [{ triggerKey: "test", triggerParameters: { name: "test trigger" } }],
+  stackAbility: "none"
+};
+
+test("should return true when the cart don't have promotion are applied", () => {
+  const cart = {
+    _id: "cartId"
+  };
+
+  // when appliedPromotions is undefined
+  expect(canBeApplied(cart.appliedPromotions, promotion));
+
+  // when appliedPromotions is empty
+  cart.appliedPromotions = [];
+  expect(canBeApplied(cart.appliedPromotions, promotion));
+});
+
+test("should return false when cart has first promotion applied with stackAbility is none", () => {
+  const cart = {
+    _id: "cartId",
+    appliedPromotions: [promotion]
+  };
+  const secondPromotion = {
+    ...promotion,
+    _id: "promotion 2",
+    stackAbility: "all"
+  };
+  expect(canBeApplied(cart.appliedPromotions, secondPromotion)).toBe(false);
+});
+
+test("should return false when the 2nd promotion has stackAbility is none", () => {
+  const cart = {
+    _id: "cartId",
+    appliedPromotions: [promotion]
+  };
+  const secondPromotion = {
+    ...promotion,
+    _id: "promotion 2",
+    stackAbility: "none"
+  };
+  expect(canBeApplied(cart.appliedPromotions, secondPromotion)).toBe(false);
+});
