219ee7d66dff946cfbc89e8d580e0005570d5913
vanpho93 <vanpho02@gmail.com>
2022-10-10 16:01:09 +0700

    feat: apply coupon code to the cart
    Signed-off-by: vanpho93 <vanpho02@gmail.com>

--- packages/api-plugin-promotions-coupons/src/triggers/couponsTriggerHandler.js
+++ packages/api-plugin-promotions-coupons/src/triggers/couponsTriggerHandler.js
@@ -1,17 +1,4 @@
-import { createRequire } from "module";
-import Logger from "@reactioncommerce/logger";
-import { Engine } from "json-rules-engine";
 import { CouponTriggerParameters } from "../simpleSchemas.js";
-
-const require = createRequire(import.meta.url);
-const pkg = require("../../package.json");
-
-const { name, version } = pkg;
-const logCtx = {
-  name,
-  version,
-  file: "couponsTriggerHandler.js"
-};
 
 /**
  * @summary a no-op function for testing of promotions
@@ -21,22 +8,7 @@ const logCtx = {
  * @returns {Boolean} - Whether the promotion can be applied to the cart
  */
 export async function couponTriggerHandler(context, enhancedCart, { triggerParameters }) {
-  const {
-    promotions: { operators }
-  } = context;
-
-  const engine = new Engine();
-  Object.keys(operators).forEach((operatorKey) => {
-    engine.addOperator(operatorKey, operators[operatorKey]);
-  });
-  engine.addRule(triggerParameters);
-  const facts = { cart: enhancedCart };
-
-  // eslint-disable-next-line no-await-in-loop
-  const results = await engine.run(facts);
-  const { failureResults } = results;
-  Logger.debug({ ...logCtx, ...results }, "Coupon trigger handler called");
-  return failureResults.length === 0;
+  return triggerParameters.couponCode === "code";
 }
 
 export default {
