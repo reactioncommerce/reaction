569963de010e803fb9bce137df82d16852a6dbf2
vanpho93 <vanpho02@gmail.com>
2022-10-12 09:41:37 +0700

    fix: linting

--- packages/api-plugin-promotions/src/mutations/applyExplicitPromotionToCart.js
+++ packages/api-plugin-promotions/src/mutations/applyExplicitPromotionToCart.js
@@ -3,7 +3,7 @@ import applyExplicitPromotion from "../handlers/applyExplicitPromotion.js";
 /**
  * @method applyExplicitPromotion
  * @summary Apply a coupon code to a cart
- * @param {Object} context
+ * @param {Object} context - The application context
  * @param {Object} cart - The cart to apply the promotion to
  * @param {Object} promotion - The promotion to apply
  * @returns {Promise<Object>} with cart
