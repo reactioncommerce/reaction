226149befa82b22c890c51cfdf44c860312d0cb4
apadhi <aditya_padhi@intuit.com>
2022-10-17 17:57:31 +0530

    moved jest process env to a separate file
    Signed-off-by: apadhi <aditya_padhi@intuit.com>

--- apps/reaction/tests/util/setupJestTests.js
+++ apps/reaction/tests/util/setupJestTests.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-undef */
-
-require("../../src/checkNodeVersion.cjs");
-
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const jestProcessEnv = require("./jestProcessEnv.json");
 process.env = Object.assign(process.env, {
-  MAIL_URL: "smtp://user:pass@email-smtp.us-west-2.amazonaws.com:465",
-  REACTION_LOG_LEVEL: "ERROR",
-  REACTION_WORKERS_ENABLED: false
+  ...jestProcessEnv
 });
 
 process.on("unhandledRejection", (err) => {
