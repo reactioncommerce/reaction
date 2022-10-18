8e43c08271729648492e6848825c1699e6e1e54e
apadhi <aditya_padhi@intuit.com>
2022-10-18 09:38:38 +0530

    fix: fixed lint error
    Signed-off-by: apadhi <aditya_padhi@intuit.com>

--- apps/reaction/tests/util/setupJestTests.js
+++ apps/reaction/tests/util/setupJestTests.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 import { createRequire } from "module";
+
 const require = createRequire(import.meta.url);
 const jestProcessEnv = require("./jestProcessEnv.json");
+
 process.env = Object.assign(process.env, {
   ...jestProcessEnv
 });
