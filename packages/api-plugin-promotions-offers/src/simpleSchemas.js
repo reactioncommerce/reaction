19b3d7847fe1b0ce5558d0150c077ae72d37de4c
vanpho93 <vanpho02@gmail.com>
2022-11-01 18:30:03 +0700

    feat: add rules for promotion trigger (uncompleted)
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- packages/api-plugin-promotions-offers/src/simpleSchemas.js
+++ packages/api-plugin-promotions-offers/src/simpleSchemas.js
@@ -1,9 +1,40 @@
 import SimpleSchema from "simpl-schema";
 
-export const OfferTriggerParameters = new SimpleSchema({
+const OfferTriggerFact = new SimpleSchema({
   name: String,
+  handlerName: String,
+  fromFact: {
+    type: String,
+    optional: true
+  }
+});
+
+const Rules = new SimpleSchema({
   conditions: {
     type: Object,
     blackbox: true
+  }
+});
+
+export const OfferTriggerParameters = new SimpleSchema({
+  "name": String,
+  "conditions": {
+    type: Object,
+    blackbox: true
+  },
+  "facts": {
+    type: Array,
+    optional: true
+  },
+  "facts.$": {
+    type: OfferTriggerFact
+  },
+  "inclusionRule": {
+    type: Rules,
+    optional: true
+  },
+  "exclusionRule": {
+    type: Rules,
+    optional: true
   }
 });
