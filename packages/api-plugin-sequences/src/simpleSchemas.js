d9339d9e965347b9431d475a2867b6abfd816209
Brent Hoover <brent@thebuddhalodge.com>
2022-11-11 07:59:04 +0000

    feat: refactoring plus adds setting start sequence via env var
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-sequences/src/simpleSchemas.js
+++ packages/api-plugin-sequences/src/simpleSchemas.js
@@ -3,5 +3,8 @@ import SimpleSchema from "simpl-schema";
 export const Sequence = new SimpleSchema({
   shopId: String,
   entity: String,
-  value: SimpleSchema.Integer
+  value: {
+    type: SimpleSchema.Integer,
+    min: 0
+  }
 });
