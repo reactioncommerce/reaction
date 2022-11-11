4a8f10977db4d61c78c22b54891fe7c380856354
Brent Hoover <brent@thebuddhalodge.com>
2022-11-11 07:59:04 +0000

    feat: refactoring plus adds setting start sequence via env var
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- packages/api-plugin-sequences/src/mutations/incrementSequence.js
+++ packages/api-plugin-sequences/src/mutations/incrementSequence.js
@@ -5,16 +5,12 @@
  * @param {String} entity - The entity (normally a collection) that you are tracking the ID for
  * @return {Promise<Number>} - The auto-incrementing ID to use
  */
-export default async function getNextSequence(context, shopId, entity) {
+export default async function incrementSequence(context, shopId, entity) {
   const { collections: { Sequences } } = context;
-  const { value: { value } } = await Sequences.findOneAndUpdate({ shopId, entity }, { $inc: { value: 1 } }, { returnDocument: "after" });
-  if (!value) {
-    await Sequences.insert({
-      shopId,
-      entity,
-      value: 1
-    });
-    return 1;
-  }
+  const { value: { value } } = await Sequences.findOneAndUpdate(
+    { shopId, entity },
+    { $inc: { value: 1 } },
+    { returnDocument: "after" }
+  );
   return value;
 }
