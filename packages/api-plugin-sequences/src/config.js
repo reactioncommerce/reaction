c85e095e879b41b123403f3f71f45dec9214eef5
Brent Hoover <brent@thebuddhalodge.com>
2022-11-14 09:54:36 +0000

    fix: change import style to appease Jest
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-sequences/src/config.js
+++ packages/api-plugin-sequences/src/config.js
@@ -1,11 +1,10 @@
-import envalid from "envalid";
+import { cleanEnv, json } from "envalid";
 import * as dotenv from "dotenv";
 
-const { json } = envalid;
 
 // this is required for envalid 7 or greater which was required to make json work
 dotenv.config();
 
-export default envalid.cleanEnv(process.env, {
+export default cleanEnv(process.env, {
   SEQUENCE_INITIAL_VALUES: json({ default: { entity: 999 } })
 });
