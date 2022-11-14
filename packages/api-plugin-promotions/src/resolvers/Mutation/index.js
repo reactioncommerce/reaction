12df8811d951494e4a7cecd2e1ce70738dda8dab
Brent Hoover <brent@thebuddhalodge.com>
2022-11-14 08:52:08 +0000

    feat: add duplicatePromotion plus name field
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-promotions/src/resolvers/Mutation/index.js
+++ packages/api-plugin-promotions/src/resolvers/Mutation/index.js
@@ -1,7 +1,9 @@
 import updatePromotion from "./updatePromotion.js";
 import createPromotion from "./createPromotion.js";
+import duplicatePromotion from "./duplicatePromotion.js";
 
 export default {
   updatePromotion,
-  createPromotion
+  createPromotion,
+  duplicatePromotion
 };
