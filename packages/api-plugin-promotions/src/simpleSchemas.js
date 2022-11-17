dea1748cdcd86ccb913fac0c29b8dd37ba8bd75a
Brent Hoover <brent@thebuddhalodge.com>
2022-11-17 07:23:43 +0000

    feat: add state field and archivePromotion endpoint
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-promotions/src/simpleSchemas.js
+++ packages/api-plugin-promotions/src/simpleSchemas.js
@@ -73,6 +73,11 @@ export const Promotion = new SimpleSchema({
     type: Boolean,
     defaultValue: false
   },
+  "state": {
+    type: String,
+    allowedValues: ["created", "active", "completed", "archived"],
+    defaultValue: "created"
+  },
   "triggers": {
     type: Array
   },
