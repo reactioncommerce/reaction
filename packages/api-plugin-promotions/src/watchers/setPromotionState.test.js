2992e5449aff67998b4705f6660d9683f5395a07
Brent Hoover <brent@thebuddhalodge.com>
2022-11-21 09:03:13 +0000

    feat: more test fixes
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-promotions/src/watchers/setPromotionState.test.js
+++ packages/api-plugin-promotions/src/watchers/setPromotionState.test.js
@@ -6,7 +6,6 @@ import setPromotionState from "./setPromotionState.js";
 let con;
 let mongoServer;
 let db;
-let col;
 let promotions;
 
 const promotionShouldBeActive = {
@@ -26,17 +25,14 @@ const promotionShouldBeCompleted = {
 
 jest.mock("../utils/getCurrentShopTime.js", () => () => ({ shop1: new Date("2022/12/31") }));
 
+
 describe("setPromotionState", () => {
   beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
     con = await MongoClient.connect(mongoServer.getUri(), {});
     db = con.db(mongoServer.instanceInfo.dbName);
-    col = db.collection("Promotions");
-    mockContext.collections.Promotions = col;
-  });
-
-  afterEach(async () => {
-    col.removeMany({});
+    promotions = db.collection("Promotions");
+    mockContext.collections.Promotions = promotions;
   });
 
   afterAll(async () => {
@@ -49,6 +45,7 @@ describe("setPromotionState", () => {
   });
 
   it("should return 0 when no records match", async () => {
+    promotions.removeMany({});
     const { totalMarkedCompleted, totalMadeActive } = await setPromotionState(mockContext);
     expect(totalMarkedCompleted).toEqual(0);
     expect(totalMadeActive).toEqual(0);
@@ -56,16 +53,16 @@ describe("setPromotionState", () => {
 
 
   it("should return 1 for made active when 1 valid record exists", async () => {
+    promotions.removeMany({});
     await promotions.insertOne(promotionShouldBeActive);
-    mockContext.collections.Promotions = promotions;
     const { totalMarkedCompleted, totalMadeActive } = await setPromotionState(mockContext);
     expect(totalMarkedCompleted).toEqual(0);
     expect(totalMadeActive).toEqual(1);
   });
 
   it("should return 1 for marked completed when 1 valid record exists", async () => {
+    promotions.removeMany({});
     await promotions.insertOne(promotionShouldBeCompleted);
-    mockContext.collections.Promotions = promotions;
     const { totalMarkedCompleted, totalMadeActive } = await setPromotionState(mockContext);
     expect(totalMarkedCompleted).toEqual(1);
     expect(totalMadeActive).toEqual(0);
