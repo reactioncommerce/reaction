3ba4b7fc5db9517e47fee93ac61f3938b1583ce9
Brent Hoover <brent@thebuddhalodge.com>
2022-11-21 09:08:24 +0000

    feat: updates from C/R
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-promotions/src/mutations/duplicatePromotion.js
+++ packages/api-plugin-promotions/src/mutations/duplicatePromotion.js
@@ -5,13 +5,14 @@ import validateTriggerParams from "./validateTriggerParams.js";
 /**
  * @summary duplicate an existing promotion to a new one
  * @param {Object} context - the per-request application context
+ * @param {String} shopId - The shop id of the promotion to duplicate
  * @param {String} promotionId - The ID of the promotion you want to duplicate
  * @return {Promise<{success: boolean, promotion: *}|{success: boolean, errors: [{message: string}]}>} - return the newly created promotion or an array of errors
  */
-export default async function duplicatePromotion(context, promotionId) {
+export default async function duplicatePromotion(context, { shopId, promotionId }) {
   const { collections: { Promotions }, simpleSchemas: { Promotion: PromotionSchema } } = context;
   const now = new Date();
-  const existingPromotion = await Promotions.findOne({ _id: promotionId });
+  const existingPromotion = await Promotions.findOne({ shopId, _id: promotionId });
   const newPromotion = _.cloneDeep(existingPromotion);
   newPromotion._id = Random.id();
   newPromotion.createdAt = now;
