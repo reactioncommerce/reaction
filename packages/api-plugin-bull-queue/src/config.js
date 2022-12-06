8644f8bbd727a34b75be2ba5fe9f86e160a2883d
Brent Hoover <brent@reactioncommerce.com>
2022-12-06 08:34:34 +0000

    feat: add defaults set by env var for adding jobs
    Signed-off-by: Brent Hoover <brent@reactioncommerce.com>

--- packages/api-plugin-bull-queue/src/config.js
+++ packages/api-plugin-bull-queue/src/config.js
@@ -3,7 +3,12 @@ import envalid from "envalid";
 export default envalid.cleanEnv(process.env, {
   REACTION_WORKERS_ENABLED: envalid.bool({ default: true }),
   VERBOSE_JOBS: envalid.bool({ default: false }),
-  REDIS_SERVER: envalid.str({ default: "redis://127.0.0.1:6379" })
+  REDIS_SERVER: envalid.str({ default: "redis://127.0.0.1:6379" }),
+  JOBS_SERVER_REMOVE_ON_COMPLETE: envalid.bool({ default: false }),
+  JOBS_SERVER_REMOVE_ON_FAIL: envalid.bool({ default: false }),
+  JOBS_SERVER_DEFAULT_ATTEMPTS: envalid.num({ default: 5 }),
+  JOBS_SERVER_BACKOFF_MS: envalid.num({ default: 5000 }),
+  JOBS_SERVER_BACKOFF_STRATEGY: envalid.str({ default: "exponential" })
 }, {
   dotEnvPath: null
 });
