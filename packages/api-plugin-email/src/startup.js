5c1ef28101abcd0dbe0d73f334396dd7fa98a61e
Brent Hoover <brent@reactioncommerce.com>
2022-12-07 07:34:23 +0000

    feat: don't start queues/jobs if in test mode
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- packages/api-plugin-email/src/startup.js
+++ packages/api-plugin-email/src/startup.js
@@ -1,5 +1,7 @@
 import returnEmailProcessor from "./util/returnEmailProcessor.js";
+import config from "./config.js";
 
+const { REACTION_WORKERS_ENABLED } = config;
 /**
  * @summary Called on startup
  * @param {Object} context Startup context
@@ -7,6 +9,7 @@ import returnEmailProcessor from "./util/returnEmailProcessor.js";
  * @returns {undefined}
  */
 export default async function emailStartup(context) {
+  if (!REACTION_WORKERS_ENABLED) return;
   const { bullQueue } = context;
   bullQueue.createQueue(context, "sendEmail", {}, returnEmailProcessor(context));
 }
