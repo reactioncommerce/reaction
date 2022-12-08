41500b6766da4bb5a1d599634903ba9eb3f2a0d5
Brent Hoover <brent@reactioncommerce.com>
2022-12-08 06:51:31 +0000

    feat: remove unneeded reference to REACTION_WORKERS_ENABLED
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- packages/api-plugin-promotions/src/startup.js
+++ packages/api-plugin-promotions/src/startup.js
@@ -1,10 +1,6 @@
 import { createRequire } from "module";
 import Logger from "@reactioncommerce/logger";
 import setPromotionState from "./watchers/setPromotionState.js";
-import config from "./config.js";
-
-const { REACTION_WORKERS_ENABLED } = config;
-
 
 const require = createRequire(import.meta.url);
 
@@ -23,9 +19,6 @@ const logCtx = {
  * @return {Boolean} - true if success
  */
 export default async function startupPromotions(context) {
-  if (!REACTION_WORKERS_ENABLED) {
-    return false;
-  }
   const { bullQueue } = context;
   await bullQueue.createQueue(context, "setPromotionState", {}, setPromotionState(context));
   await bullQueue.scheduleJob(context, "setPromotionState", "checkForChangedStates", {}, "*/5 * * * *");
