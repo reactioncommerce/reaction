967f4f6eab3a8af1718555c2d290b8b1fca9f0a3
Brent Hoover <brent@reactioncommerce.com>
2022-12-08 05:37:33 +0000

    feat: changes from c/r
    Signed-off-by: Brent Hoover <brent@reactioncommerce.com>

--- packages/api-plugin-email/src/startup.js
+++ packages/api-plugin-email/src/startup.js
@@ -1,7 +1,5 @@
 import returnEmailProcessor from "./util/returnEmailProcessor.js";
-import config from "./config.js";
 
-const { REACTION_WORKERS_ENABLED } = config;
 /**
  * @summary Called on startup
  * @param {Object} context Startup context
@@ -9,7 +7,6 @@ const { REACTION_WORKERS_ENABLED } = config;
  * @returns {undefined}
  */
 export default async function emailStartup(context) {
-  if (!REACTION_WORKERS_ENABLED) return;
   const { bullQueue } = context;
   bullQueue.createQueue(context, "sendEmail", {}, returnEmailProcessor(context));
 }
