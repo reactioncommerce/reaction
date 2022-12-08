cf5508389451642398e110c678237470d9c9b2be
Brent Hoover <brent@reactioncommerce.com>
2022-12-08 06:22:55 +0000

    feat: remove scheduled job when job with same name is added
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-bull-queue/src/api/scheduleJob.js
+++ packages/api-plugin-bull-queue/src/api/scheduleJob.js
@@ -12,22 +12,30 @@ const logCtx = {
   file: "api/scheduleJob.js"
 };
 
+
 /**
  * @summary create a scheduled job
  * @param {Object} context - The application context
  * @param {String} queueName - The queue to add this job
+ * @param {String} jobName - The unique name of the job
  * @param {Object} jobData - Data to be passed to the worker
  * @param {Object} schedule - The schedule as a crontab
  * @return {Boolean} - true if success
  */
-export default async function scheduleJob(context, queueName, jobData, schedule) {
+export default async function scheduleJob(context, queueName, jobName, jobData, schedule) {
   if (typeof jobData !== "object" || typeof schedule !== "string") {
     Logger.error(logCtx, "Invalid parameters supplied to scheduleJob");
     return false;
   }
   if (context.bullQueue.jobQueues[queueName]) {
     const thisQueue = context.bullQueue.jobQueues[queueName];
-    await thisQueue.add(jobData, { repeat: { cron: schedule } });
+    const repeatableJobs = await thisQueue.getRepeatableJobs();
+    const jobToRemove = repeatableJobs.find((jbName) => jobName === jbName.name);
+    if (jobToRemove) {
+      await thisQueue.removeRepeatable(jobToRemove);
+      Logger.info({ queueName, jobName, ...logCtx }, "Removed repeatable job");
+    }
+    await thisQueue.add(jobName, jobData, { repeat: { cron: schedule } });
     return true;
   }
   Logger.error({ queueName, ...logCtx }, "Could not schedule job as the queue was not found");
