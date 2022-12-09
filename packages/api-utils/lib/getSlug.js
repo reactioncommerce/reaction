8e58e4bb1c4420978de3c56aedf54f21087a14dd
Brent Hoover <brent@reactioncommerce.com>
2022-12-09 00:54:05 +0000

    feat: allow for second parameter on getSlug function for allowed characters
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-utils/lib/getSlug.js
+++ packages/api-utils/lib/getSlug.js
@@ -10,8 +10,9 @@ const { slugify } = require("transliteration");
  * @see https://www.npmjs.com/package/transliteration
  * @memberof Utils
  * @param {String} slugString - string to slugify
+ * @param {String|Boolean} allowedChars - specify extra characters that are not removed by slugify
  * @returns {String} slugified string
  */
-export default function getSlug(slugString) {
-  return (typeof slugString === "string" && slugify(slugString, { allowedChars: "a-zA-Z0-9-/" })) || "";
+export default function getSlug(slugString, allowedChars = false) {
+  return (typeof slugString === "string" && slugify(slugString, { allowedChars })) || "";
 }
