7e7e00ab4909fd9af489c0f0d82916564f79058d
Brent Hoover <brent@reactioncommerce.com>
2022-12-13 09:47:18 +0000

    feat: add handlers that reprocess carts when promotion state changes
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-promotions/src/startup.js
+++ packages/api-plugin-promotions/src/startup.js
@@ -1,6 +1,7 @@
 import { createRequire } from "module";
 import Logger from "@reactioncommerce/logger";
 import setPromotionState from "./watchers/setPromotionState.js";
+import saveListOfCarts from "./utils/resaveListOfCarts.js";
 
 const require = createRequire(import.meta.url);
 
@@ -21,7 +22,8 @@ const logCtx = {
 export default async function startupPromotions(context) {
   const { bullQueue } = context;
   await bullQueue.createQueue(context, "setPromotionState", {}, setPromotionState(context));
-  await bullQueue.scheduleJob(context, "setPromotionState", "checkForChangedStates", {}, "*/5 * * * *");
+  await bullQueue.scheduleJob(context, "setPromotionState", "checkForChangedStates", {}, "*/1 * * * *");
   Logger.info(logCtx, "Add setPromotionState queue and job");
+  await bullQueue.createQueue(context, "checkExistingCarts", {}, saveListOfCarts(context));
   return true;
 }
