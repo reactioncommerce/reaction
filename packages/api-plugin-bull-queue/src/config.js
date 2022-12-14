882ed76c035477245c1435201faffa32c73a437b
Brent Hoover <brent@reactioncommerce.com>
2022-12-14 08:30:28 +0000

    fix: bump dotenv to 7 and add dotenv Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-bull-queue/src/config.js
+++ packages/api-plugin-bull-queue/src/config.js
@@ -1,4 +1,7 @@
 import envalid from "envalid";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default envalid.cleanEnv(process.env, {
   REACTION_WORKERS_ENABLED: envalid.bool({ default: true }),
@@ -10,6 +13,4 @@ export default envalid.cleanEnv(process.env, {
   JOBS_SERVER_BACKOFF_STRATEGY: envalid.str({ default: "exponential" }),
   JOBS_SERVER_REMOVE_COMPLETED_JOBS_AFTER: envalid.str({ default: "3 days" }),
   JOBS_SERVER_REMOVE_FAILED_JOBS_AFTER: envalid.str({ default: "30 days" })
-}, {
-  dotEnvPath: null
 });
