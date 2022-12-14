51aaa796b731ca3ac7061e530850674c2aba8999
Brent Hoover <brent@reactioncommerce.com>
2022-12-14 11:01:30 +0000

    fix: non-working code Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- packages/api-plugin-promotions/src/startup.js
+++ packages/api-plugin-promotions/src/startup.js
@@ -1,7 +1,7 @@
 import { createRequire } from "module";
 import Logger from "@reactioncommerce/logger";
 import setPromotionState from "./watchers/setPromotionState.js";
-import saveListOfCarts from "./utils/resaveListOfCarts.js";
+import checkCartForPromotionChange from "./utils/checkCartForPromotionChange.js";
 
 const require = createRequire(import.meta.url);
 
@@ -24,6 +24,6 @@ export default async function startupPromotions(context) {
   await bullQueue.createQueue(context, "setPromotionState", { jobName: "checkForChangedStates" }, setPromotionState(context));
   await bullQueue.scheduleJob(context, "setPromotionState", "checkForChangedStates", {}, "*/5 * * * *");
   Logger.info(logCtx, "Add setPromotionState queue and job");
-  await bullQueue.createQueue(context, "checkExistingCarts", {}, saveListOfCarts(context));
+  await bullQueue.createQueue(context, "checkExistingCarts", {}, checkCartForPromotionChange(context));
   return true;
 }
