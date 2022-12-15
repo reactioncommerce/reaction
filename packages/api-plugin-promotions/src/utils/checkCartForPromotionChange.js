a49b60b1ed34220379458f5cb2bc98a61f2b282f
Brent Hoover <brent@reactioncommerce.com>
2022-12-15 05:03:53 +0000

    fix: fully working check and update carts in batches
    Signed-off-by: Brent Hoover <brent@reactioncommerce.com>

--- packages/api-plugin-promotions/src/utils/checkCartForPromotionChange.js
+++ packages/api-plugin-promotions/src/utils/checkCartForPromotionChange.js
@@ -1,29 +1,44 @@
+import { createRequire } from "module";
 import _ from "lodash";
+import Logger from "@reactioncommerce/logger";
 import applyPromotions from "../handlers/applyPromotions.js";
+
+
+const require = createRequire(import.meta.url);
+
+const pkg = require("../../package.json");
+
+const { name, version } = pkg;
+const logCtx = {
+  name,
+  version,
+  file: "checkForPromotionChange.js"
+};
 
 
 /**
  * @summary check if the cart promotion state has changed
  * @param {Object} context - The application context
- * @param {Object} Carts - The carts collection from the context
+ * @param {Object} Cart - The carts collection from the context
  * @param {String} cartId - The id of the cart to check
  * @return {Promise<{reason: null, updated: boolean, cart: *}|{reason: string, updated: boolean, cart: *}>} - Whether its changed and how
  */
-export async function checkForChangedCart(context, Carts, cartId) {
+export async function checkForChangedCart(context, Cart, cartId) {
   let updated = false;
   let reason = null;
   // eslint-disable-next-line no-await-in-loop
-  const cart = await Carts.findOne({ _id: cartId });
+  const cart = await Cart.findOne({ _id: cartId });
   // eslint-disable-next-line no-await-in-loop
-  const updatedCart = await applyPromotions(context, cart);
+  const cartToMutate = _.cloneDeep(cart); // can't pass in cart since applyPromotion mutates
+  const updatedCart = await applyPromotions(context, cartToMutate);
   if (cart.appliedPromotions || updatedCart.appliedPromotions) {
     if (!updatedCart.appliedPromotions) updatedCart.appliedPromotions = [];
     if (!cart.appliedPromotions) cart.appliedPromotions = [];
     if (cart.appliedPromotions.length !== updatedCart.appliedPromotions.length) {
       updated = true;
-      reason = "different array lengths";
+      reason = "different number of promotions";
     } else {
-      // length didn't change so now we need to check each item
+      // length didn't change, so now we need to check each item
       for (const promotion of cart.appliedPromotions) {
         delete promotion.updatedAt;
         const samePromotion = updatedCart.appliedPromotions.find((pr) => pr._id === promotion._id);
@@ -54,15 +69,24 @@ export default function wrapper(context) {
    * @param {Array<String>} arrayOfCartIds - An array of cart ids
    * @return {undefined} undefined
    */
-  async function saveListOfCarts(arrayOfCartIds) {
-    const { collections: { Carts } } = context;
+  async function checkCartsForPromotionChange(arrayOfCartIds) {
+    let totalModified = 0;
+    let totalUnchanged = 0;
+    const { collections: { Cart } } = context;
     for (const cartId of arrayOfCartIds) {
       // eslint-disable-next-line no-await-in-loop
-      const { updated, cart } = await checkForChangedCart(Carts, cartId, context);
+      const { updated, cart } = await checkForChangedCart(context, Cart, cartId);
       if (updated) { // something about promotions on the cart have changed so trigger a full update
         context.mutations.saveCart(context, cart);
+        totalModified += 1;
+      } else {
+        totalUnchanged += 1;
       }
     }
+    Logger.info(
+      { totalModified, totalUnchanged, numberOfCarts: arrayOfCartIds.length, ...logCtx },
+      "Completed processing batch of cart promotion checks"
+    );
   }
-  return saveListOfCarts;
+  return checkCartsForPromotionChange;
 }
