b36d4a46d92bf7f99eabc49653e6ca5baddb7116
Brent Hoover <brent@reactioncommerce.com>
2022-12-15 11:32:34 +0000

    fix: handle promotion without triggers Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- packages/api-plugin-promotions/src/mutations/createPromotion.js
+++ packages/api-plugin-promotions/src/mutations/createPromotion.js
@@ -12,10 +12,13 @@ export default async function createPromotion(context, promotion) {
   promotion._id = Random.id();
   promotion.referenceId = await context.mutations.incrementSequence(context, promotion.shopId, "Promotions");
   const now = new Date();
-  const [firstTrigger] = promotion.triggers; // currently support only one trigger
-  const { triggerKey } = firstTrigger;
-  const trigger = promotions.triggers.find((tr) => tr.key === triggerKey);
-  promotion.triggerType = trigger.type;
+  let triggerKey;
+  if (promotion.triggers && promotion.triggers.length) { // if there are no triggers, this is an error, but we'll let schema validation catch it
+    const [firstTrigger] = promotion.triggers; // currently support only one trigger
+    ({ triggerKey } = firstTrigger);
+    const trigger = promotions.triggers.find((tr) => tr.key === triggerKey);
+    promotion.triggerType = trigger.type;
+  }
   promotion.state = "created";
   promotion.createdAt = now;
   promotion.updatedAt = now;
