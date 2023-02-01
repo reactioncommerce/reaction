ab8f8a4d3550ba8aeff4c8c37fe65bf875444c78
vanpho93 <vanpho02@gmail.com>
2023-02-01 09:46:40 +0700

    feat: add additional coupon validation
    Signed-off-by: vanpho93 <vanpho02@gmail.com>

--- packages/api-plugin-promotions-coupons/src/mutations/index.js
+++ packages/api-plugin-promotions-coupons/src/mutations/index.js
@@ -1,9 +1,11 @@
 import applyCouponToCart from "./applyCouponToCart.js";
 import createStandardCoupon from "./createStandardCoupon.js";
+import updateStandardCoupon from "./updateStandardCoupon.js";
 import removeCouponFromCart from "./removeCouponFromCart.js";
 
 export default {
   applyCouponToCart,
   createStandardCoupon,
+  updateStandardCoupon,
   removeCouponFromCart
 };
