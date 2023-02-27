f17ecf22f4d7a90cdf35f715f66a825452b4ec49
Sujith <mail.sujithvn@gmail.com>
2023-02-27 16:56:05 +0530

    fix: encode set to true for test, remove in v6
    Signed-off-by: Sujith <mail.sujithvn@gmail.com>

--- packages/api-utils/lib/config.js
+++ packages/api-utils/lib/config.js
@@ -5,6 +5,7 @@ const { bool } = envalid;
 export default envalid.cleanEnv(process.env, {
   REACTION_SHOULD_ENCODE_IDS: bool({
     default: false,
+    devDefault: envalid.testOnly(true), // #TODO Remove this in v6 when we remove the encoding of IDs
     desc: "Setting this to false makes the `encodeOpaqueId` function pass through the ID unchanged."
   })
 }, {
