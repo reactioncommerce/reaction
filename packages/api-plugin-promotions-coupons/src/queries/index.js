6be616a68232b65715bad2ef85d9ef9e518e3884
vanpho93 <vanpho02@gmail.com>
2023-02-27 21:20:02 +0700

    feat: add additional redeemed coupon information
    Signed-off-by: vanpho93 <vanpho02@gmail.com>

--- packages/api-plugin-promotions-coupons/src/queries/index.js
+++ packages/api-plugin-promotions-coupons/src/queries/index.js
@@ -1,7 +1,13 @@
 import coupon from "./coupon.js";
 import coupons from "./coupons.js";
+import couponLog from "./couponLog.js";
+import couponLogs from "./couponLogs.js";
+import couponLogByOrderId from "./couponLogByOrderId.js";
 
 export default {
   coupon,
-  coupons
+  coupons,
+  couponLog,
+  couponLogs,
+  couponLogByOrderId
 };
--- packages/api-plugin-promotions-coupons/src/resolvers/index.js
+++ packages/api-plugin-promotions-coupons/src/resolvers/index.js
@@ -1,8 +1,10 @@
 import Promotion from "./Promotion/index.js";
 import Mutation from "./Mutation/index.js";
 import Query from "./Query/index.js";
+import Order from "./Order/index.js";
 
 export default {
+  Order,
   Promotion,
   Mutation,
   Query
