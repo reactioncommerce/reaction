dc2704a2f49481799ff1c459ea95e0eaa2211a12
vanpho93 <vanpho02@gmail.com>
2023-02-27 10:03:48 +0700

    feat: deprecate discounts plugins
    Signed-off-by: vanpho93 <vanpho02@gmail.com>

--- apps/reaction/tests/integration/api/queries/discountCodes/discountCodes.test.js
+++ apps/reaction/tests/integration/api/queries/discountCodes/discountCodes.test.js
@@ -12,41 +12,41 @@ const opaqueShopId = "cmVhY3Rpb24vc2hvcDoxMjM="; // reaction/shop:123
 const shopName = "Test Shop";
 const discountCodeDocuments = [];
 
-for (let index = 10; index < 25; index += 1) {
-  const doc = Factory.Discounts.makeOne({
-    _id: `discountCode-${index}`,
-    shopId: internalShopId,
-    code: `${index}OFF`,
-    label: `${index} Off`,
-    description: `Take $${index} off on all orders over $${index}`,
-    discount: `${index}`,
-    discountMethod: "code",
-    calculation: {
-      method: "discount"
-    },
-    conditions: {
-      accountLimit: 1,
-      order: {
-        min: index,
-        startDate: "2019-11-14T18:30:03.658Z",
-        endDate: "2021-01-01T08:00:00.000Z"
-      },
-      redemptionLimit: 0,
-      audience: ["customer"],
-      permissions: ["guest", "anonymous"],
-      products: ["product-id"],
-      tags: ["tag-id"],
-      enabled: true
-    },
-    transactions: [{
-      cartId: "cart-id",
-      userId: "user-id",
-      appliedAt: "2019-11-18T18:30:03.658Z"
-    }]
-  });
+// for (let index = 10; index < 25; index += 1) {
+//   const doc = Factory.Discounts.makeOne({
+//     _id: `discountCode-${index}`,
+//     shopId: internalShopId,
+//     code: `${index}OFF`,
+//     label: `${index} Off`,
+//     description: `Take $${index} off on all orders over $${index}`,
+//     discount: `${index}`,
+//     discountMethod: "code",
+//     calculation: {
+//       method: "discount"
+//     },
+//     conditions: {
+//       accountLimit: 1,
+//       order: {
+//         min: index,
+//         startDate: "2019-11-14T18:30:03.658Z",
+//         endDate: "2021-01-01T08:00:00.000Z"
+//       },
+//       redemptionLimit: 0,
+//       audience: ["customer"],
+//       permissions: ["guest", "anonymous"],
+//       products: ["product-id"],
+//       tags: ["tag-id"],
+//       enabled: true
+//     },
+//     transactions: [{
+//       cartId: "cart-id",
+//       userId: "user-id",
+//       appliedAt: "2019-11-18T18:30:03.658Z"
+//     }]
+//   });
 
-  discountCodeDocuments.push(doc);
-}
+//   discountCodeDocuments.push(doc);
+// }
 
 const adminGroup = Factory.Group.makeOne({
   _id: "adminGroup",
@@ -110,7 +110,7 @@ beforeAll(async () => {
 // test file gets its own test database.
 afterAll(() => testApp.stop());
 
-test("throws access-denied when getting discount codes if not an admin", async () => {
+test.skip("throws access-denied when getting discount codes if not an admin", async () => {
   await testApp.setLoggedInUser(mockCustomerAccount);
 
   try {
@@ -122,7 +122,7 @@ test("throws access-denied when getting discount codes if not an admin", async (
   }
 });
 
-test("returns discount records if user is an admin", async () => {
+test.skip("returns discount records if user is an admin", async () => {
   await testApp.setLoggedInUser(mockAdminAccount);
 
   const result = await discountCodes({
@@ -136,7 +136,7 @@ test("returns discount records if user is an admin", async () => {
 });
 
 
-test("returns discount records on second page if user is an admin", async () => {
+test.skip("returns discount records on second page if user is an admin", async () => {
   await testApp.setLoggedInUser(mockAdminAccount);
 
   const result = await discountCodes({
