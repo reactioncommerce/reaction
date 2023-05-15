cd2f570a9cd1b2f4f7ff72ba484d9ce65d074da0
tedraykov <tedraykov@gmail.com>
2023-05-15 19:56:13 +0300

    feat: support dynamic password reset url
    Signed-off-by: tedraykov <tedraykov@gmail.com>

--- packages/api-plugin-authentication/src/config.js
+++ packages/api-plugin-authentication/src/config.js
@@ -5,6 +5,7 @@ const { str } = envalid;
 export default envalid.cleanEnv(
   process.env,
   {
+    PASSWORD_RESET_PATH_FRAGMENT: str({ default: "?resetToken=" }),
     STORE_URL: str({ devDefault: "http://localhost:4000" }),
     TOKEN_SECRET: str({ default: "UPDATE_THIS_SECRET" })
   },
